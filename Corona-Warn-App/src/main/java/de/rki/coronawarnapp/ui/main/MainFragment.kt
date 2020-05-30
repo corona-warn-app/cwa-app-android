dcc0b5e04b0d6755619437d644aa6e6942c2e5ca
Tim Brüggenthies <tim.brueggenthies@outlook.de>
2020-05-30 20:15:21 +0200

    Moves creation of notification channel to app startup

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/MainFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/MainFragment.kt
@@ -150,7 +150,6 @@ class MainFragment : BaseFragment() {
                         NotificationManagerCompat.from(requireContext()).areNotificationsEnabled()
                             .toString()
                     )
-                    NotificationHelper.createNotificationChannel()
                     NotificationHelper.sendNotification(
                         getString(R.string.notification_headline),
                         getString(R.string.notification_body),
