33d098bc431bc36bfc5561131100277cf749bbc8
Ooi, Hee Tatt <hee.tatt.ooi@sap.com>
2020-06-02 08:12:29 +0200

    comments

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/CoronaWarnApplication.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/CoronaWarnApplication.kt
@@ -13,7 +13,14 @@ class CoronaWarnApplication : Application(), LifecycleObserver {
     companion object {
         val TAG: String? = CoronaWarnApplication::class.simpleName
         private lateinit var instance: CoronaWarnApplication
+
+        /* describes if the app is in foreground
+         * Initialized to false, because app could also be started by a background job.
+         * For the cases where the app is started via the launcher icon, the onAppForegrounded
+         * event will be called, setting it to true
+         */
         var isAppInForeground = false
+
         fun getAppContext(): Context =
             instance.applicationContext
     }
@@ -24,12 +31,18 @@ class CoronaWarnApplication : Application(), LifecycleObserver {
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 
+    /**
+     * Callback when the app is open but backgrounded
+     */
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onAppBackgrounded() {
         isAppInForeground = false
         Log.v(TAG, "App backgrounded")
     }
 
+    /**
+     * Callback when the app is foregrounded
+     */
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onAppForegrounded() {
         isAppInForeground = true
