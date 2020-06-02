864c75548db2e7d574b3ff74ef148254d14fa6e6
Philipp Woessner <philipp.woessner@sap.com>
2020-06-02 14:27:44 +0200

    added restriction to portrait and reversed portrait orientation only

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/CoronaWarnApplication.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/CoronaWarnApplication.kt
@@ -1,10 +1,13 @@
 package de.rki.coronawarnapp
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import de.rki.coronawarnapp.notification.NotificationHelper
 
-class CoronaWarnApplication : Application() {
+class CoronaWarnApplication : Application(), Application.ActivityLifecycleCallbacks {
 
     companion object {
         private lateinit var instance: CoronaWarnApplication
@@ -16,5 +19,23 @@ class CoronaWarnApplication : Application() {
         instance = this
         NotificationHelper.createNotificationChannel()
         super.onCreate()
+
+        registerActivityLifecycleCallbacks(this)
     }
+
+    override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+        p0.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
+    }
+
+    override fun onActivityPaused(p0: Activity) {}
+
+    override fun onActivityStarted(p0: Activity) {}
+
+    override fun onActivityDestroyed(p0: Activity) {}
+
+    override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {}
+
+    override fun onActivityStopped(p0: Activity) {}
+
+    override fun onActivityResumed(p0: Activity) {}
 }
