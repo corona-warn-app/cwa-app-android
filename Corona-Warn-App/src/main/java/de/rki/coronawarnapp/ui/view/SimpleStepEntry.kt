4511c4c919703308520b58092ca0580b8e936ac9
Fabian-K <fabian.kajzar@sap.com>
2020-06-02 16:12:46 +0200

    Submission Contact Fragment & StepView Custom View (#102)
    * - submission-contact fragment added
    - StepEntry custom view added to unify various step entry views
    - SimpleStepEntry as a simple implementation of a StepEntry with title and text added
    
    * - apply naming conventions for ids
    - use CallHelper to trigger a call
    - access views using kotlinx.android.synthetic
    
    Co-authored-by: harambasicluka <64483219+harambasicluka@users.noreply.github.com>

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/view/SimpleStepEntry.kt
@@ -0,0 +1,29 @@
+package de.rki.coronawarnapp.ui.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.core.content.withStyledAttributes
+import de.rki.coronawarnapp.R
+import kotlinx.android.synthetic.main.include_step_entry_simple_body.view.*
+
+/**
+ * The [SimpleStepEntry] is a step entry with title and text as content.
+ */
+class SimpleStepEntry @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : StepEntry(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.include_step_entry_simple_body, this)
+
+        context.withStyledAttributes(attrs, R.styleable.SimpleStepEntry) {
+            simple_step_entry_title.text =
+                getText(R.styleable.SimpleStepEntry_simple_step_entry_title)
+
+            simple_step_entry_body.text =
+                getText(R.styleable.SimpleStepEntry_simple_step_entry_text)
+        }
+    }
+}
