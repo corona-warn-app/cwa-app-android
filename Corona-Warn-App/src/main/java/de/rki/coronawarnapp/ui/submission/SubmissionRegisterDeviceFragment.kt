c36256fe659b0d67aaf353abbbe9921ba420368f
Kolya Opahle <k.opahle@sap.com>
2020-06-03 16:21:35 +0200

    Added error dialogs to RegisterDeviceFragment screen

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/SubmissionRegisterDeviceFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/SubmissionRegisterDeviceFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import com.android.volley.TimeoutError
+import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentSubmissionRegisterDeviceBinding
+import de.rki.coronawarnapp.exception.TestAlreadyPairedException
 import de.rki.coronawarnapp.ui.BaseFragment
 import de.rki.coronawarnapp.ui.viewmodel.SubmissionViewModel
+import de.rki.coronawarnapp.util.DialogHelper
 
 class SubmissionRegisterDeviceFragment : BaseFragment() {
     private val viewModel: SubmissionViewModel by activityViewModels()
@@ -24,6 +28,38 @@ class SubmissionRegisterDeviceFragment : BaseFragment() {
         return binding.root
     }
 
+    private fun navigateToDispatchScreen() = doNavigate(
+        SubmissionRegisterDeviceFragmentDirections
+            .actionSubmissionRegisterDeviceFragmentToSubmissionDispatcherFragment()
+    )
+
+    private fun buildErrorDialog(exception: Exception): DialogHelper.DialogInstance {
+        return when (exception) {
+            is TimeoutError -> DialogHelper.DialogInstance(
+                R.string.submission_error_dialog_web_generic_timeout_title,
+                R.string.submission_error_dialog_web_generic_timeout_body,
+                R.string.submission_error_dialog_web_generic_timeout_button_positive,
+                R.string.submission_error_dialog_web_generic_timeout_button_negative,
+                viewModel::doDeviceRegistration,
+                ::navigateToDispatchScreen
+            )
+            is TestAlreadyPairedException -> DialogHelper.DialogInstance(
+                R.string.submission_error_dialog_web_test_paired_title,
+                R.string.submission_error_dialog_web_test_paired_body,
+                R.string.submission_error_dialog_web_test_paired_button_positive,
+                null,
+                ::navigateToDispatchScreen
+            )
+            else -> DialogHelper.DialogInstance(
+                R.string.submission_error_dialog_web_generic_error_title,
+                R.string.submission_error_dialog_web_generic_error_body,
+                R.string.submission_error_dialog_web_generic_error_button_positive,
+                null,
+                ::navigateToDispatchScreen
+            )
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -33,6 +69,12 @@ class SubmissionRegisterDeviceFragment : BaseFragment() {
                     SubmissionRegisterDeviceFragmentDirections
                         .actionSubmissionRegisterDeviceFragmentToSubmissionResultFragment()
                 )
+            }
+        })
+
+        viewModel.registrationError.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                DialogHelper.showDialog(requireActivity(), buildErrorDialog(it))
             }
         })
     }
