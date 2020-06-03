88d9027e24dd8b8c0baae7478c15cb23af403a0f
Kolya Opahle <k.opahle@sap.com>
2020-06-03 13:34:41 +0200

    Added specific exceptions to all submission web requests

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/WebRequestBuilder.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/WebRequestBuilder.kt
@@ -23,11 +23,14 @@ import KeyExportFormat
 import android.util.Log
 import com.android.volley.DefaultRetryPolicy
 import com.android.volley.Response
+import com.android.volley.TimeoutError
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import de.rki.coronawarnapp.BuildConfig
+import de.rki.coronawarnapp.exception.SubmissionTanInvalidException
 import de.rki.coronawarnapp.exception.TestAlreadyPairedException
+import de.rki.coronawarnapp.exception.TestPairingInvalidException
 import de.rki.coronawarnapp.exception.WebRequestException
 import de.rki.coronawarnapp.exception.report
 import de.rki.coronawarnapp.http.request.ApplicationConfigurationRequest
@@ -154,7 +157,7 @@ object WebRequestBuilder {
                         )
                         cont.resume(response)
                     },
-                    RegistrationTokenRequestErrorListener(requestID, cont)
+                    RegistrationTokenRequestErrorListener(cont)
                 )
             RequestQueueHolder.addToRequestQueue(getRegistrationTokenRequest)
             Log.d(TAG, "$requestID: Added $url to queue.")
@@ -208,7 +211,7 @@ object WebRequestBuilder {
                         )
                         cont.resume(response)
                     },
-                    RequestErrorListener(requestID, cont)
+                    GetTanRequestErrorListener(cont)
                 )
             RequestQueueHolder.addToRequestQueue(getTANRequest)
             Log.d(TAG, "$requestID: Added $url to queue.")
@@ -242,14 +245,13 @@ object WebRequestBuilder {
                         )
                         cont.resume(response)
                     },
-                    RequestErrorListener(requestID, cont)
+                    SubmitKeysToServerRequestErrorListener(cont)
                 )
             RequestQueueHolder.addToRequestQueue(submitKeysRequest)
             Log.d(TAG, "$requestID: Added $url to queue.")
         }
 
     private class RequestErrorListener<T>(
-        private val requestID: UUID,
         private val cont: Continuation<T>
     ) :
         Response.ErrorListener {
@@ -265,21 +267,74 @@ object WebRequestBuilder {
     }
 
     private class RegistrationTokenRequestErrorListener<T>(
-        private val requestID: UUID,
         private val cont: Continuation<T>
     ) :
         Response.ErrorListener {
         override fun onErrorResponse(error: VolleyError?) {
-            if (error != null) {
-                val resultingException = when(error.networkResponse.statusCode) {
-                    400 -> TestAlreadyPairedException("the test was already paired to a different device", error)
+            val resultingException = when (error) {
+                is TimeoutError -> error
+                null -> WebRequestException(
+                    "an error occurred during a webrequest",
+                    NullPointerException("The volley exception was null")
+                )
+                else -> when (error.networkResponse.statusCode) {
+                    400 -> TestAlreadyPairedException(
+                        "the test was already paired to a different device",
+                        error
+                    )
                     else -> WebRequestException("an error occurred during a webrequest", error)
                 }
-                resultingException.report(de.rki.coronawarnapp.exception.ExceptionCategory.HTTP)
-                cont.resumeWithException(resultingException)
-            } else {
-                cont.resumeWithException(NullPointerException("the provided exception from volley was null"))
             }
+            resultingException.report(de.rki.coronawarnapp.exception.ExceptionCategory.HTTP)
+            cont.resumeWithException(resultingException)
+        }
+    }
+
+    private class GetTanRequestErrorListener<T>(
+        private val cont: Continuation<T>
+    ) :
+        Response.ErrorListener {
+        override fun onErrorResponse(error: VolleyError?) {
+            val resultingException = when (error) {
+                is TimeoutError -> error
+                null -> WebRequestException(
+                    "an error occurred during a webrequest",
+                    NullPointerException("The volley exception was null")
+                )
+                else -> when (error.networkResponse.statusCode) {
+                    400 -> TestPairingInvalidException(
+                        "the test paring to the device is invalid",
+                        error
+                    )
+                    else -> WebRequestException("an error occurred during a webrequest", error)
+                }
+            }
+            resultingException.report(de.rki.coronawarnapp.exception.ExceptionCategory.HTTP)
+            cont.resumeWithException(resultingException)
+        }
+    }
+
+    private class SubmitKeysToServerRequestErrorListener<T>(
+        private val cont: Continuation<T>
+    ) :
+        Response.ErrorListener {
+        override fun onErrorResponse(error: VolleyError?) {
+            val resultingException = when (error) {
+                is TimeoutError -> error
+                null -> WebRequestException(
+                    "an error occurred during a webrequest",
+                    NullPointerException("The volley exception was null")
+                )
+                else -> when (error.networkResponse.statusCode) {
+                    403 -> SubmissionTanInvalidException(
+                        "the tan used for submission is invalid",
+                        error
+                    )
+                    else -> WebRequestException("an error occurred during a webrequest", error)
+                }
+            }
+            resultingException.report(de.rki.coronawarnapp.exception.ExceptionCategory.HTTP)
+            cont.resumeWithException(resultingException)
         }
     }
 }
