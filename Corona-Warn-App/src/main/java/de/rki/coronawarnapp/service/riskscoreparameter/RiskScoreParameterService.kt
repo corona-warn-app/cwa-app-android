c0a668f5fc741a02091da71d444dd57ab85e6294
Philipp Woessner <philipp.woessner@sap.com>
2020-06-03 11:36:25 +0200

    added the minRiskScore parameter to the ExposureConfiguration

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/service/riskscoreparameter/RiskScoreParameterService.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/service/riskscoreparameter/RiskScoreParameterService.kt
@@ -2,6 +2,7 @@ package de.rki.coronawarnapp.service.riskscoreparameter
 
 import com.google.android.gms.nearby.exposurenotification.ExposureConfiguration
 import de.rki.coronawarnapp.http.WebRequestBuilder
+import de.rki.coronawarnapp.server.protocols.ApplicationConfigurationOuterClass
 import de.rki.coronawarnapp.server.protocols.ApplicationConfigurationOuterClass.RiskScoreParameters
 import de.rki.coronawarnapp.service.diagnosiskey.DiagnosisKeyConstants
 
@@ -9,56 +10,60 @@ object RiskScoreParameterService {
     suspend fun asyncRetrieveRiskScoreParameters(): ExposureConfiguration =
         WebRequestBuilder
             .asyncGetApplicationConfigurationFromServer(DiagnosisKeyConstants.COUNTRY_APPCONFIG_DOWNLOAD_URL)
-            .exposureConfig
+            .configValues()
             .mapRiskScoreToExposureConfiguration()
+
+    private fun ApplicationConfigurationOuterClass.ApplicationConfiguration.configValues(): Pair<RiskScoreParameters, Int> =
+        Pair(this.exposureConfig, this.minRiskScore)
 
     // TODO also add minimumRiskScore as soon as the backend will provide it in the correct proto
     // right now it is available but only in the parent ApplicationConfiguration
-    private fun RiskScoreParameters.mapRiskScoreToExposureConfiguration(): ExposureConfiguration =
+    private fun Pair<RiskScoreParameters, Int>.mapRiskScoreToExposureConfiguration(): ExposureConfiguration =
         ExposureConfiguration
             .ExposureConfigurationBuilder()
-            .setAttenuationWeight(this.attenuationWeight.toInt())
+            .setMinimumRiskScore(this.second)
+            .setAttenuationWeight(this.first.attenuationWeight.toInt())
             .setAttenuationScores(
-                this.attenuation.gt73DbmValue,
-                this.attenuation.gt63Le73DbmValue,
-                this.attenuation.gt51Le63DbmValue,
-                this.attenuation.gt33Le51DbmValue,
-                this.attenuation.gt27Le33DbmValue,
-                this.attenuation.gt15Le27DbmValue,
-                this.attenuation.gt10Le15DbmValue,
-                this.attenuation.lt10DbmValue
+                this.first.attenuation.gt73DbmValue,
+                this.first.attenuation.gt63Le73DbmValue,
+                this.first.attenuation.gt51Le63DbmValue,
+                this.first.attenuation.gt33Le51DbmValue,
+                this.first.attenuation.gt27Le33DbmValue,
+                this.first.attenuation.gt15Le27DbmValue,
+                this.first.attenuation.gt10Le15DbmValue,
+                this.first.attenuation.lt10DbmValue
             )
-            .setDaysSinceLastExposureWeight(this.daysWeight.toInt())
+            .setDaysSinceLastExposureWeight(this.first.daysWeight.toInt())
             .setDaysSinceLastExposureScores(
-                this.daysSinceLastExposure.ge14DaysValue,
-                this.daysSinceLastExposure.ge12Lt14DaysValue,
-                this.daysSinceLastExposure.ge10Lt12DaysValue,
-                this.daysSinceLastExposure.ge8Lt10DaysValue,
-                this.daysSinceLastExposure.ge6Lt8DaysValue,
-                this.daysSinceLastExposure.ge4Lt6DaysValue,
-                this.daysSinceLastExposure.ge2Lt4DaysValue,
-                this.daysSinceLastExposure.ge0Lt2DaysValue
+                this.first.daysSinceLastExposure.ge14DaysValue,
+                this.first.daysSinceLastExposure.ge12Lt14DaysValue,
+                this.first.daysSinceLastExposure.ge10Lt12DaysValue,
+                this.first.daysSinceLastExposure.ge8Lt10DaysValue,
+                this.first.daysSinceLastExposure.ge6Lt8DaysValue,
+                this.first.daysSinceLastExposure.ge4Lt6DaysValue,
+                this.first.daysSinceLastExposure.ge2Lt4DaysValue,
+                this.first.daysSinceLastExposure.ge0Lt2DaysValue
             )
-            .setDurationWeight(this.durationWeight.toInt())
+            .setDurationWeight(this.first.durationWeight.toInt())
             .setDurationScores(
-                this.duration.eq0MinValue,
-                this.duration.gt0Le5MinValue,
-                this.duration.gt5Le10MinValue,
-                this.duration.gt10Le15MinValue,
-                this.duration.gt15Le20MinValue,
-                this.duration.gt20Le25MinValue,
-                this.duration.gt25Le30MinValue,
-                this.duration.gt30MinValue
+                this.first.duration.eq0MinValue,
+                this.first.duration.gt0Le5MinValue,
+                this.first.duration.gt5Le10MinValue,
+                this.first.duration.gt10Le15MinValue,
+                this.first.duration.gt15Le20MinValue,
+                this.first.duration.gt20Le25MinValue,
+                this.first.duration.gt25Le30MinValue,
+                this.first.duration.gt30MinValue
             )
-            .setTransmissionRiskWeight(this.transmissionWeight.toInt())
+            .setTransmissionRiskWeight(this.first.transmissionWeight.toInt())
             .setTransmissionRiskScores(
-                this.transmission.appDefined1Value,
-                this.transmission.appDefined2Value,
-                this.transmission.appDefined3Value,
-                this.transmission.appDefined4Value,
-                this.transmission.appDefined5Value,
-                this.transmission.appDefined6Value,
-                this.transmission.appDefined7Value,
-                this.transmission.appDefined8Value
+                this.first.transmission.appDefined1Value,
+                this.first.transmission.appDefined2Value,
+                this.first.transmission.appDefined3Value,
+                this.first.transmission.appDefined4Value,
+                this.first.transmission.appDefined5Value,
+                this.first.transmission.appDefined6Value,
+                this.first.transmission.appDefined7Value,
+                this.first.transmission.appDefined8Value
             ).build()
 }
