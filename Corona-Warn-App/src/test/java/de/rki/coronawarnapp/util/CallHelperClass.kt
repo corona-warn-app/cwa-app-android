bc875a73fd1f1baf378df325c299b22e77b69761
Kirill <kirill.sergeev@sap.com>
2020-06-04 09:47:18 +0300

    util tests

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/CallHelperClass.kt
@@ -0,0 +1,23 @@
+package de.rki.coronawarnapp.util
+
+import de.rki.coronawarnapp.ui.BaseFragment
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+/**
+ * CallHelper test.
+ */
+class CallHelperClass {
+
+    @Test
+    fun testCall() {
+        val fragment = mockk<BaseFragment>()
+        every { fragment.startActivity(any()) } just Runs
+        CallHelper.call(fragment, "+77777777777")
+        verify(exactly = 1) { fragment.startActivity(any()) }
+    }
+}
---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/ConnectivityHelperTest.kt
@@ -0,0 +1,137 @@
+package de.rki.coronawarnapp.util
+
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkRequest
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkClass
+import io.mockk.mockkConstructor
+import io.mockk.mockkObject
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class ConnectivityHelperTest {
+
+    @MockK
+    private lateinit var bAdapter: BluetoothAdapter
+
+    @MockK
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        mockkStatic(BluetoothAdapter::class)
+    }
+
+    @Test
+    fun testNetworkCallback() {
+        var registered: Boolean? = null
+        var available: Boolean? = null
+        val callback = object : ConnectivityHelper.NetworkCallback() {
+            override fun onNetworkAvailable() {
+                available = true
+            }
+            override fun onNetworkUnavailable() {
+                available = false
+            }
+        }
+        mockkConstructor(NetworkRequest.Builder::class)
+        mockkObject(NetworkRequest.Builder())
+        val request = mockkClass(NetworkRequest::class)
+        val manager = mockkClass(ConnectivityManager::class)
+        every { anyConstructed<NetworkRequest.Builder>().addCapability(any()).addCapability(any()).build() } returns request
+        every { context.getSystemService(Context.CONNECTIVITY_SERVICE) } returns manager
+        every { manager.registerNetworkCallback(any(), callback) } answers { registered = true }
+        every { manager.unregisterNetworkCallback(callback) } answers { registered = false }
+
+        // register
+        ConnectivityHelper.registerNetworkStatusCallback(context, callback)
+
+        assertEquals(registered, true)
+        assertEquals(available, false)
+
+        // network found
+        callback.onAvailable(mockk())
+        assertEquals(available, true)
+
+        // loss of network
+        callback.onLost(mockk())
+        assertEquals(available, false)
+
+        // unregister
+        ConnectivityHelper.unregisterNetworkStatusCallback(context, callback)
+        assertEquals(registered, false)
+    }
+
+    @Test
+    fun testBluetoothCallback() {
+        var registered: Boolean? = null
+        var available: Boolean? = null
+        val callback = object : ConnectivityHelper.BluetoothCallback() {
+            override fun onBluetoothAvailable() {
+                available = true
+            }
+
+            override fun onBluetoothUnavailable() {
+                available = false
+            }
+        }
+
+        every { BluetoothAdapter.getDefaultAdapter() } returns bAdapter
+        every { context.registerReceiver(any(), any()) } answers {
+            registered = true
+            mockk()
+        }
+        every { context.unregisterReceiver(any()) } answers { registered = false }
+
+        // turned on
+        every { bAdapter.isEnabled } returns true
+
+        // register
+        ConnectivityHelper.registerBluetoothStatusCallback(context, callback)
+
+        assertNotNull(callback.recevier)
+        assertEquals(registered, true)
+        assertEquals(available, true)
+
+        // unregister
+        ConnectivityHelper.unregisterBluetoothStatusCallback(context, callback)
+
+        assertNull(callback.recevier)
+        assertEquals(registered, false)
+    }
+
+    @Test
+    fun testBluetoothState() {
+        // no bluetooth hardware
+        assertFalse(ConnectivityHelper.isBluetoothEnabled())
+
+        every { BluetoothAdapter.getDefaultAdapter() } returns bAdapter
+
+        // check turned off
+        every { bAdapter.isEnabled } returns false
+        assertFalse(ConnectivityHelper.isBluetoothEnabled())
+
+        // check turned on
+        every { bAdapter.isEnabled } returns true
+        assertTrue(ConnectivityHelper.isBluetoothEnabled())
+    }
+
+    @After
+    fun cleanUp() {
+        unmockkAll()
+    }
+}
