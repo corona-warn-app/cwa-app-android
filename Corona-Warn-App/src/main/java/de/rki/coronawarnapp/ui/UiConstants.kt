bfa5d299dd3082327b98b12cdfc0d130b40ffed1
marcmuschko <marc.muschko@sap.com>
2020-06-04 15:47:26 +0200

    Small design / layout adjustments, translatable support number / FAQ link (#163)
    * added translatable website for FAQ and technical contact
    
    * final strings for main overview added
    
    * added spacing to scroll views to bottom buttons / guidelines
    
    * restrucured app information menu order
    
    * fixed missing text in app information, added divider to contact page
    
    * text style adjustment onboarding
    
    * text style adjustment app information menu and main overview risk list

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/UiConstants.kt
+++
@@ -1,8 +0,0 @@
-package de.rki.coronawarnapp.ui
-
-object UiConstants {
-    const val INFORMATION_URI = "https://www.bundesregierung.de/c19app-intern"
-
-    // todo move to strings if translatable is needed? if yes include regex in CallHelper to filter non-numerical chars excluding '+'
-    const val TECHNICAL_HOTLINE = "tel:+49 800 7540001"
-}
