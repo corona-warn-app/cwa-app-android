f083c1fda00c4cbdef74072257ac455989b63dc1
Ooi, Hee Tatt <hee.tatt.ooi@sap.com>
2020-06-07 13:27:17 +0200

    catch CwaSecurityException for update

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/update/UpdateChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/update/UpdateChecker.kt
@@ -32,11 +32,13 @@ class UpdateChecker(private val activity: LauncherActivity) {
     suspend fun checkForUpdate() {
 
         // check if an update is needed based on server config
-        // TODO replace with signature exception
         val updateNeededFromServer: Boolean = try {
             checkIfUpdatesNeededFromServer()
         } catch (exception: CwaSecurityException) {
             Log.e(TAG, "CwaSecurityException caught:" + exception.localizedMessage)
+            true
+        } catch (exception: Exception) {
+            Log.e(TAG, "Exception caught:" + exception.localizedMessage)
             false
         }
 
