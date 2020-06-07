2fb27665fcb83aed63961e56d61779ee4a035a26
Thomas Klingbeil <thomas.klingbeil@sap.com>
2020-06-07 19:19:53 +0200

    extend tests for submissionTanViewModel

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/SubmissionTanViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/SubmissionTanViewModelTest.kt
@@ -1,9 +1,11 @@
 package de.rki.coronawarnapp.ui.submission
 
+import com.google.android.gms.nearby.exposurenotification.ExposureSummary
+import de.rki.coronawarnapp.storage.SubmissionRepository
 import de.rki.coronawarnapp.util.TanHelper
+import io.mockk.*
+import org.junit.Assert.*
 import org.junit.Test
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
 
 class SubmissionTanViewModelTest {
     private var viewModel: SubmissionTanViewModel = SubmissionTanViewModel()
@@ -21,24 +23,39 @@ class SubmissionTanViewModelTest {
     @Test
     fun tanFormatValid() {
         viewModel.tan.postValue("ZWFPC7NG47")
-        viewModel.tanCharactersValid.value?.let { assertTrue(it) }
+        viewModel.isValidTanFormat.value?.let { assertTrue(it) }
 
         viewModel.tan.postValue("ABC")
-        viewModel.tanCharactersValid.value?.let { assertFalse(it) }
+        viewModel.isValidTanFormat.value?.let { assertFalse(it) }
 
         viewModel.tan.postValue("ZWFPC7NG48")
-        viewModel.tanCharactersValid.value?.let { assertFalse(it) }
+        viewModel.isValidTanFormat.value?.let { assertFalse(it) }
 
         viewModel.tan.postValue("ZWFPC7NG4A")
-        viewModel.tanCharactersValid.value?.let { assertFalse(it) }
+        viewModel.isValidTanFormat.value?.let { assertFalse(it) }
     }
 
     @Test
     fun checksumValid() {
         viewModel.tan.postValue("ZWFPC7NG47")
-        viewModel.tanCharactersValid.value?.let { assertTrue(it) }
+        viewModel.tanChecksumValid.value?.let { assertTrue(it) }
 
         viewModel.tan.postValue("ZWFPC7NG48")
-        viewModel.tanCharactersValid.value?.let { assertFalse(it) }
+        viewModel.tanChecksumValid.value?.let { assertFalse(it) }
+    }
+
+    @Test
+    fun testTanStorage() {
+        val sr = mockk<SubmissionRepository> {
+            every { setTeletan(any()) } just Runs
+        }
+        val tan = "ZWFPC7NG47";
+        sr.setTeletan(tan)
+
+        verify (exactly = 1) { sr.setTeletan(
+            withArg {
+                assertEquals(it, tan)
+            })
+        }
     }
 }
