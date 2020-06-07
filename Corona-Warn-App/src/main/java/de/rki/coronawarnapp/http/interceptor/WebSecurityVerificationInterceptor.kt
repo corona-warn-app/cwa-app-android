59cf00a16fb0a6604c18ba80a9d5670bcf07acce
Jakob Möller <jakob.moeller@sap.com>
2020-06-07 17:20:06 +0200

    Setup Intermediate Pin (#217)
    * Setup Intermediate Pin
    
    Signed-off-by: d067928 <jakob.moeller@sap.com>
    
    * Setup Trust Anchors
    
    Signed-off-by: d067928 <jakob.moeller@sap.com>
    
    * Service Factory Merge Resolve
    
    Signed-off-by: d067928 <jakob.moeller@sap.com>
    
    * Add URLs for Submission and Distribution
    
    Signed-off-by: d067928 <jakob.moeller@sap.com>

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/interceptor/WebSecurityVerificationInterceptor.kt
@@ -0,0 +1,14 @@
+package de.rki.coronawarnapp.http.interceptor
+
+import de.rki.coronawarnapp.exception.CwaWebSecurityException
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.net.ssl.SSLException
+
+class WebSecurityVerificationInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        try { return chain.proceed(chain.request()) } catch (e: SSLException) {
+            throw CwaWebSecurityException(e)
+        }
+    }
+}
