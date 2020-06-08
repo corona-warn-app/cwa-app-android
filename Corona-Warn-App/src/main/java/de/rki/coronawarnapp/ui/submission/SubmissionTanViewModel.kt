4dc73f1a4403c0f58a950450b570c470473217e8
Thomas Klingbeil <64434904+tklingbeil@users.noreply.github.com>
2020-06-08 09:22:29 +0200

    Revert "Extend teleTAN to 10 characters + input validation (#252)" (#267)
    This reverts commit 8577f8d4c3e70ded43a693df9ac67d4bd6c48808.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/SubmissionTanViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/SubmissionTanViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import de.rki.coronawarnapp.storage.SubmissionRepository
-import de.rki.coronawarnapp.util.TanHelper
 
 class SubmissionTanViewModel : ViewModel() {
 
@@ -17,21 +16,7 @@ class SubmissionTanViewModel : ViewModel() {
 
     val isValidTanFormat =
         Transformations.map(tan) {
-            it != null &&
-            it.length == TanConstants.MAX_LENGTH &&
-            TanHelper.isChecksumValid(it) &&
-            TanHelper.allCharactersValid(it)
-        }
-
-    val tanChecksumValid =
-        Transformations.map(tan) {
-            ((it !== null && it.trim().length == TanConstants.MAX_LENGTH) &&
-                    TanHelper.isChecksumValid(it).not()).not()
-        }
-
-    val tanCharactersValid =
-        Transformations.map(tan) {
-            !((it != null) && TanHelper.allCharactersValid(it).not())
+            it != null && it.length == TanConstants.MAX_LENGTH
         }
 
     fun storeTeletan() {
