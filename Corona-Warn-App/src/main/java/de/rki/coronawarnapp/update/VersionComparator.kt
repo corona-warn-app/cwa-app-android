f896427b057b5b64ed1d22f76abec20faa06cbf1
DanCuprum <kopperfeld@gmail.com>
2020-06-09 09:01:38 +0200

    Update VersionComparator.kt
    Dear Team, 
    
    I just wish to notify you that I think to have found a (logical) bug in your above code. 
    In my opinion the following code snippet below will fail, under the following constructed example condition:
    
            if (versionToCompareMajor > currentVersionMajor) {
                isVersionOlder = true
            } else if (versionToCompareMinor > currentVersionMinor) {
                isVersionOlder = true
    
    currentMajor version = 3, compareMajor version = 1
    currentMinor version = 1, compareMinor version = 2
    
    In other words: If the major version on the server is two major steps older than the installed one, 
    the function "isVersionOlder" will the faulty return true, which in turn would update the App to an actually *older* version. 
    Thus the app will effectively be downgraded instead of upgraded, so to say.
    If this behaviour is intended, then please skip my remarks. 
    (It just appeared slightly odd to me, alone from the viewpoint of the logical construction of the code.)
    Hope this was a bit helpful in any case.
    
    Kind Regards
    Dan(iel)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/update/VersionComparator.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/update/VersionComparator.kt
@@ -16,6 +16,8 @@ object VersionComparator {
         val versionToCompareMajor = versionToCompareParts[0].toInt()
         val versionToCompareMinor = versionToCompareParts[1].toInt()
         val versionToComparePatch = versionToCompareParts[2].toInt()
+        
+        // ** please check my attached comments to the following code block =>
 
         if (versionToCompareMajor > currentVersionMajor) {
             isVersionOlder = true
@@ -28,3 +30,28 @@ object VersionComparator {
         return isVersionOlder
     }
 }
+
+/*
+** Dear Team, 
+
+I just wish to notify you that I think to have found a (logical) bug in your above code. 
+In my opinion the following code snippet below will fail, under the following constructed example condition:
+
+        if (versionToCompareMajor > currentVersionMajor) {
+            isVersionOlder = true
+        } else if (versionToCompareMinor > currentVersionMinor) {
+            isVersionOlder = true
+
+currentMajor version = 3, compareMajor version = 1
+currentMinor version = 1, compareMinor version = 2
+
+In other words: If the major version on the server is two major steps older than the installed one, 
+the function "isVersionOlder" will the faulty return true, which in turn would update the App to an actually *older* version. 
+Thus the app will effectively be downgraded instead of upgraded, so to say.
+If this behaviour is intended, then please skip my remarks. 
+(It just appeared slightly odd to me, alone from the viewpoint of the logical construction of the code.)
+Hope this was a bit helpful in any case.
+
+Kind Regards
+Dan(iel)
+*/
