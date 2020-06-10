66fa704a0c1f9108eb52afa5c5a151d7fcadbe9c
Fabian Kajzar <fabian.kajzar@sap.com>
2020-06-10 13:51:58 +0200

    TAN Input - adjust stroke width to match design - increase spacing before tan input - wrap blocks to next line if not enough space - include errors in TalkBack

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/TanInput.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/TanInput.kt
@@ -4,31 +4,25 @@ import android.content.Context
 import android.os.Handler
 import android.text.InputFilter
 import android.util.AttributeSet
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.FrameLayout
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.children
 import androidx.core.widget.doOnTextChanged
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.ui.submission.TanConstants.MAX_LENGTH
 import de.rki.coronawarnapp.util.TanHelper
-import kotlinx.android.synthetic.main.view_tan_input.view.dash_view_1
-import kotlinx.android.synthetic.main.view_tan_input.view.dash_view_2
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_edittext
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_1
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_10
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_2
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_3
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_4
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_5
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_6
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_7
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_8
-import kotlinx.android.synthetic.main.view_tan_input.view.tan_input_textview_9
+import kotlinx.android.synthetic.main.view_tan_input_edittext.view.tan_input_edittext
 import java.util.Locale
+import kotlin.math.max
 
-class TanInput(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+class TanInput(context: Context, attrs: AttributeSet) : ViewGroup(context, attrs) {
 
     companion object {
         private const val KEYBOARD_TRIGGER_DELAY = 100L
+        private const val EMPTY_STRING = ""
     }
 
     private val whitespaceFilter =
@@ -44,13 +38,20 @@ class TanInput(context: Context, attrs: AttributeSet) : FrameLayout(context, att
 
     private var tan: String? = null
 
+    private val lineSpacing: Int
+
     init {
-        inflate(context, R.layout.view_tan_input, this)
+        // add "hidden" edittext for input handling
+        inflate(context, R.layout.view_tan_input_edittext, this)
+
+        // add 3 blocks of digits
+        inflate(context, R.layout.view_tan_input_group_3, this)
+        inflate(context, R.layout.view_tan_input_group_3, this)
+        inflate(context, R.layout.view_tan_input_group_4, this)
+
+        lineSpacing = context.resources.getDimension(R.dimen.submission_tan_line_spacing).toInt()
 
         tan_input_edittext.filters = arrayOf(whitespaceFilter, alphaNumericFilter, lengthFilter)
-
-        dash_view_1.text = "-"
-        dash_view_2.text = "-"
 
         // register listener
         tan_input_edittext.doOnTextChanged { text, _, _, _ -> updateTan(text) }
@@ -86,32 +87,106 @@ class TanInput(context: Context, attrs: AttributeSet) : FrameLayout(context, att
 
     private fun notifyListener() = listener?.invoke(tan)
 
-    private fun updateDigits() = listOf(
-        tan_input_textview_1,
-        tan_input_textview_2,
-        tan_input_textview_3,
-        tan_input_textview_4,
-        tan_input_textview_5,
-        tan_input_textview_6,
-        tan_input_textview_7,
-        tan_input_textview_8,
-        tan_input_textview_9,
-        tan_input_textview_10
-    ).forEachIndexed { i, tanDigit ->
-        tanDigit.text = digitAtIndex(i)
-        tanDigit.background =
-            if (digitAtIndex(i) == "")
-                resources.getDrawable(R.drawable.tan_input_digit, null)
-            else if (TanHelper.isTanCharacterValid(digitAtIndex(i)))
-                resources.getDrawable(R.drawable.tan_input_digit_entered, null)
-            else resources.getDrawable(R.drawable.tan_input_digit_error, null)
+    // returns all digits
+    private fun digits(): List<TextView> = children
+        .filterIsInstance(LinearLayout::class.java) // filter to 3 groups, ignoring edittext
+        .flatMap {
+            it.children.filterIsInstance(TextView::class.java) // ignore separators
+        }
+        .toList()
+
+    private fun updateDigits() = digits().forEachIndexed { i, tanDigit ->
+        val text = digitAtIndex(i)
+        tanDigit.text = text
+        tanDigit.background = when {
+            text == EMPTY_STRING -> resources.getDrawable(R.drawable.tan_input_digit, null)
+            TanHelper.isTanCharacterValid(text) -> resources.getDrawable(
+                R.drawable.tan_input_digit_entered,
+                null
+            )
+            else -> resources.getDrawable(R.drawable.tan_input_digit_error, null)
+        }
 
         tanDigit.setTextColor(
-            if (TanHelper.isTanCharacterValid(digitAtIndex(i)))
-                resources.getColor(R.color.colorTextSemanticNeutral, null)
-            else resources.getColor(R.color.colorTextSemanticRed, null)
+            if (TanHelper.isTanCharacterValid(text))
+                resources.getColor(R.color.colorTextPrimary1, null)
+            else
+                resources.getColor(R.color.colorTextSemanticRed, null)
         )
     }
 
-    private fun digitAtIndex(index: Int): String = tan?.getOrNull(index)?.toString() ?: ""
+    private fun digitAtIndex(index: Int): String = tan?.getOrNull(index)?.toString() ?: EMPTY_STRING
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val availableWith = MeasureSpec.getSize(widthMeasureSpec)
+
+        var lines = 1
+        var remainingWidthInLine = availableWith
+        var longestLineWidth = 0
+
+        // calculate number of lines and longest line
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            measureChild(child, widthMeasureSpec, heightMeasureSpec)
+
+            if (child is EditText) {
+                // no need to place "hidden" input in line
+                continue
+            }
+
+            // is there sill enough place at current line?
+            if (child.measuredWidth <= remainingWidthInLine) {
+                // child can be placed at current line
+                remainingWidthInLine -= child.measuredWidth
+                longestLineWidth = max(longestLineWidth, availableWith - remainingWidthInLine)
+            } else {
+                // child needs to be placed at next line
+                lines++
+                remainingWidthInLine = availableWith - child.measuredWidth
+                longestLineWidth = max(longestLineWidth, availableWith - remainingWidthInLine)
+            }
+        }
+
+        // calculate height assuming all groups have same height
+        val childrenHeight = children.first { it is LinearLayout }.measuredHeight
+        val resultingHeight = lines * childrenHeight + (lineSpacing * (lines - 1))
+
+        val resultingWidth = longestLineWidth
+
+        setMeasuredDimension(resultingWidth, resultingHeight)
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        val availableWidth = right - left
+        var remainingWidthInLine = availableWidth
+        var currentLineStart = 0
+
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+
+            if (child is EditText) {
+                // layout "hidden" edittext
+                child.layout(0, 0, 1, 1)
+                continue
+            }
+
+            val childWidth = child.measuredWidth
+
+            // if not enough space in current line, target next
+            if (childWidth > remainingWidthInLine) {
+                currentLineStart += (child.measuredHeight + lineSpacing)
+                remainingWidthInLine = availableWidth
+            }
+
+            // layout child at line
+            val childTop = currentLineStart
+            val childBottom = currentLineStart + child.measuredHeight
+            val childStart = availableWidth - remainingWidthInLine
+            val childEnd = childStart + childWidth
+            remainingWidthInLine -= childWidth
+            child.layout(childStart, childTop, childEnd, childBottom)
+        }
+    }
 }
