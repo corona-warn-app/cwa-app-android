2a14347d3ff940b9f52b67a1f08137d8c5b339b0
Kolya Opahle <k.opahle@sap.com>
2020-06-12 12:07:19 +0200

    Added exception handler for UnknownHostException's
    Signed-off-by: Kolya Opahle <k.opahle@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/HttpErrorParser.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/HttpErrorParser.kt
@@ -25,40 +25,47 @@ import de.rki.coronawarnapp.exception.http.UnauthorizedException
 import de.rki.coronawarnapp.exception.http.UnsupportedMediaTypeException
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.net.UnknownHostException
 
 class HttpErrorParser : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val response = chain.proceed(chain.request())
-        return when (val code = response.code) {
-            200 -> response
-            201 -> response
-            202 -> response
-            204 -> response
-            400 -> throw BadRequestException()
-            401 -> throw UnauthorizedException()
-            403 -> throw ForbiddenException()
-            404 -> throw NotFoundException()
-            409 -> throw ConflictException()
-            410 -> throw GoneException()
-            415 -> throw UnsupportedMediaTypeException()
-            429 -> throw TooManyRequestsException()
-            500 -> throw InternalServerErrorException()
-            501 -> throw NotImplementedException()
-            502 -> throw BadGatewayException()
-            503 -> throw ServiceUnavailableException()
-            504 -> throw GatewayTimeoutException()
-            505 -> throw HTTPVersionNotSupported()
-            511 -> throw NetworkAuthenticationRequiredException()
-            598 -> throw NetworkReadTimeoutException()
-            599 -> throw NetworkConnectTimeoutException()
-            else -> {
-                if (code in 100..199) throw CwaInformationalNotSupportedError(code)
-                if (code in 200..299) throw CwaSuccessResponseWithCodeMismatchNotSupportedError(code)
-                if (code in 300..399) throw CwaRedirectNotSupportedError(code)
-                if (code in 400..499) throw CwaClientError(code)
-                if (code in 500..599) throw CwaServerError(code)
-                throw CwaWebException(code)
+        try {
+            val response = chain.proceed(chain.request())
+            return when (val code = response.code) {
+                200 -> response
+                201 -> response
+                202 -> response
+                204 -> response
+                400 -> throw BadRequestException()
+                401 -> throw UnauthorizedException()
+                403 -> throw ForbiddenException()
+                404 -> throw NotFoundException()
+                409 -> throw ConflictException()
+                410 -> throw GoneException()
+                415 -> throw UnsupportedMediaTypeException()
+                429 -> throw TooManyRequestsException()
+                500 -> throw InternalServerErrorException()
+                501 -> throw NotImplementedException()
+                502 -> throw BadGatewayException()
+                503 -> throw ServiceUnavailableException()
+                504 -> throw GatewayTimeoutException()
+                505 -> throw HTTPVersionNotSupported()
+                511 -> throw NetworkAuthenticationRequiredException()
+                598 -> throw NetworkReadTimeoutException()
+                599 -> throw NetworkConnectTimeoutException()
+                else -> {
+                    if (code in 100..199) throw CwaInformationalNotSupportedError(code)
+                    if (code in 200..299) throw CwaSuccessResponseWithCodeMismatchNotSupportedError(
+                        code
+                    )
+                    if (code in 300..399) throw CwaRedirectNotSupportedError(code)
+                    if (code in 400..499) throw CwaClientError(code)
+                    if (code in 500..599) throw CwaServerError(code)
+                    throw CwaWebException(code)
+                }
             }
+        } catch (err: UnknownHostException) {
+            throw BadGatewayException()
         }
     }
 }
