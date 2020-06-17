fa8882e9ce8ed5383911f3c57125f7d7f9af6a6d
Kolya Opahle <k.opahle@sap.com>
2020-06-17 13:55:33 +0200

    Added import for AppCompatEditText

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/view/UnscrollableEditText.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/view/UnscrollableEditText.kt
@@ -2,9 +2,10 @@ package de.rki.coronawarnapp.ui.view
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
 
 class UnscrollableEditText(context: Context, attributeSet: AttributeSet) :
-    androidx.appcompat.widget.AppCompatEditText(context, attributeSet) {
+    AppCompatEditText(context, attributeSet) {
     override fun onSelectionChanged(selStart: Int, selEnd: Int) {
         super.onSelectionChanged(selStart, selEnd)
         text?.length?.let { setSelection(it) }
