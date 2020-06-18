cbdfb71b87799c2a330e7861f377bfb11423c0a1
Fabus1184 <fabianlippold1184@gmail.com>
2020-06-18 19:16:50 +0200

    added floating button to print privacy consent

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/information/InformationPrivacyFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/information/InformationPrivacyFragment.kt
@@ -1,13 +1,29 @@
 package de.rki.coronawarnapp.ui.information
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.res.AssetManager
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintDocumentAdapter
+import android.print.PrintManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.accessibility.AccessibilityEvent
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentInformationPrivacyBinding
 import de.rki.coronawarnapp.ui.main.MainActivity
+import kotlinx.coroutines.channels.ActorScope
+import java.io.InputStreamReader
+import java.util.*
 
 /**
  * Basic Fragment which only displays static content.
@@ -48,5 +64,61 @@ class InformationPrivacyFragment : Fragment() {
         binding.informationPrivacyHeader.headerButtonBack.buttonIcon.setOnClickListener {
             (activity as MainActivity).goBack()
         }
+
+        binding.savePrivacyConsentButton.setOnClickListener {
+
+            var mWebView: WebView? = null
+            val webView = WebView(activity)
+
+            webView.webViewClient = object : WebViewClient() {
+
+                override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest) = false
+
+                override fun onPageFinished(view: WebView, url: String) {
+                    createWebPrintJob(view)
+                    mWebView = null
+                }
+            }
+
+            if(Locale.getDefault().language == "de"){
+                webView.loadUrl("file:///android_asset/privacy_de.html")
+            }else{
+                webView.loadUrl("file:///android_asset/privacy_en.html")
+            }
+
+
+
+
+            mWebView = webView
+
+        }
+
+
+
     }
+
+    private fun createWebPrintJob(webView: WebView) {
+
+        // Get a PrintManager instance
+        (activity?.getSystemService(Context.PRINT_SERVICE) as? PrintManager)?.let { printManager ->
+
+            val jobName = "${getString(R.string.app_name)} Document"
+
+            // Get a print adapter instance
+            val printAdapter = webView.createPrintDocumentAdapter(jobName)
+
+            // Create a print job with name and adapter instance
+            printManager.print(
+                jobName,
+                printAdapter,
+                PrintAttributes.Builder().build()
+            ).also { printJob ->
+
+                // Save the job object for later status checking
+                var printJobs = printJob
+            }
+        }
+    }
+
+
 }
