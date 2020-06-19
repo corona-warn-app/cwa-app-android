4932235ee6bbe0fe3dc9a2a52a09fda84719752f
Tobias Preuss <tobias.preuss@googlemail.com>
2020-06-19 17:11:34 +0200

    Lift-out return.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ConnectivityHelper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ConnectivityHelper.kt
@@ -137,9 +137,9 @@ object ConnectivityHelper {
      */
     fun isBackgroundJobEnabled(context: Context): Boolean {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            return !activityManager.isBackgroundRestricted
-        } else return true
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            !activityManager.isBackgroundRestricted
+        } else true
     }
 
     /**
