4c217aedd0010837c264c1d6e3b923123aea1bed
Kolya Opahle <k.opahle@sap.com>
2020-07-29 17:43:06 +0200

    basic playbook implementation with fake and real requests

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/playbook/PlaybookImpl.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/http/playbook/PlaybookImpl.kt
@@ -1,38 +1,74 @@
 package de.rki.coronawarnapp.http.playbook
 
 import KeyExportFormat
+import de.rki.coronawarnapp.http.WebRequestBuilder
 import de.rki.coronawarnapp.service.submission.KeyType
 import de.rki.coronawarnapp.util.formatter.TestResult
 
 class PlaybookImpl : Playbook {
     override suspend fun initialRegistration(key: String, keyType: KeyType): String {
-        // register
-        // dummy
-        // dummy
-        TODO("Not yet implemented")
+        // real registration
+        val registrationToken =
+            WebRequestBuilder.getInstance().asyncGetRegistrationToken(key, keyType)
+        // fake test result
+        WebRequestBuilder.getInstance().asyncFakeGetTestResult(registrationToken, "?????????")
+        // fake auth code
+        // TODO: Proper request padding
+        val fakeAuthCode =
+            WebRequestBuilder.getInstance().asyncFakeGetTan(registrationToken, "?????????")
+        // fake submission
+        // TODO: Generate fake keys
+        WebRequestBuilder.getInstance().asyncFakeSubmitKeysToServer(fakeAuthCode, listOf())
+        return registrationToken
     }
 
     override suspend fun testResult(registrationToken: String): TestResult {
-        // check
-        // dummy
-        // dummy
-        TODO("Not yet implemented")
+        // fake registration
+        // TODO: Decision about fake key type and padding
+        val fakeRegistrationToken = WebRequestBuilder.getInstance()
+            .asyncFakeGetRegistrationToken("0123456789", KeyType.TELETAN, "?????????")
+        // real test result
+        val testResult = WebRequestBuilder.getInstance().asyncGetTestResult(registrationToken)
+        // fake auth code
+        // TODO: Proper request padding
+        val fakeAuthCode =
+            WebRequestBuilder.getInstance().asyncFakeGetTan(fakeRegistrationToken, "?????????")
+        // fake submission
+        // TODO: Generate fake keys
+        WebRequestBuilder.getInstance().asyncFakeSubmitKeysToServer(fakeAuthCode, listOf())
+        return TestResult.fromInt(testResult)
     }
 
     override suspend fun submission(
         registrationToken: String,
         keys: List<KeyExportFormat.TemporaryExposureKey>
     ) {
-        // tan
-        // dummy
-        // upload
-        TODO("Not yet implemented")
+        // fake registration
+        // TODO: Decision about fake key type and padding
+        val fakeRegistrationToken = WebRequestBuilder.getInstance()
+            .asyncFakeGetRegistrationToken("0123456789", KeyType.TELETAN, "?????????")
+        // fake test result
+        // TODO: Proper request padding
+        WebRequestBuilder.getInstance().asyncFakeGetTestResult(fakeRegistrationToken, "?????????")
+        // real auth code
+        val authCode = WebRequestBuilder.getInstance().asyncGetTan(registrationToken)
+        // real submission
+        WebRequestBuilder.getInstance().asyncFakeSubmitKeysToServer(authCode, keys)
     }
 
     override suspend fun dummy() {
-        // dummy
-        // dummy
-        // dummy
-        TODO("Not yet implemented")
+        // fake registration
+        // TODO: Decision about fake key type and padding
+        val fakeRegistrationToken = WebRequestBuilder.getInstance()
+            .asyncFakeGetRegistrationToken("0123456789", KeyType.TELETAN, "?????????")
+        // fake test result
+        WebRequestBuilder.getInstance().asyncFakeGetTestResult(fakeRegistrationToken, "?????????")
+        // fake auth code
+        // TODO: Proper request padding
+        val fakeAuthCode =
+            WebRequestBuilder.getInstance().asyncFakeGetTan(fakeRegistrationToken, "?????????")
+        // fake submission
+        // TODO: Generate fake keys
+        WebRequestBuilder.getInstance().asyncFakeSubmitKeysToServer(fakeAuthCode, listOf())
     }
 }
