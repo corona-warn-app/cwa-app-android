c333b7a87c4f59ac87494b1659dc31e0600d8619
Rituraj Sambherao <git.rituraj.sambherao@gmail.com>
2020-08-11 17:28:46 +0100

    initial commit : feature / fix complete
    1. Formatter Added
    2. include layout added
    3. visilbity logic based on default device language has been added
    4. links for english and german forms are added based on device language
    5. phone number has been removed

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/information/InformationLegalFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/information/InformationLegalFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.accessibility.AccessibilityEvent
 import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentInformationLegalBinding
 import de.rki.coronawarnapp.ui.main.MainActivity
+import de.rki.coronawarnapp.util.ExternalActionHelper
+import java.util.Locale
 
 /**
  * Basic Fragment which only displays static content.
@@ -37,6 +40,26 @@ class InformationLegalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setButtonOnClickListener()
+        setContactFormLink(view)
+    }
+
+    private fun setContactFormLink(view: View) {
+        if (Locale.getDefault().language == Locale.ENGLISH.language ||
+            Locale.getDefault().language == Locale.GERMAN.language) {
+            binding.informationLegalContactForm.informationLegalContactForm.setOnClickListener {
+                ExternalActionHelper.openUrl(
+                    this, requireContext().getString(R.string.information_legal_subtitle_contact_form_link_default))
+            }
+        } else {
+            binding.informationLegalContactForm.informationLegalContactFormEn.setOnClickListener {
+                ExternalActionHelper.openUrl(
+                    this, requireContext().getString(R.string.information_legal_subtitle_contact_form_link_en))
+            }
+            binding.informationLegalContactForm.informationLegalContactFormDe.setOnClickListener {
+                ExternalActionHelper.openUrl(
+                    this, requireContext().getString(R.string.information_legal_subtitle_contact_form_link_de))
+            }
+        }
     }
 
     override fun onResume() {
