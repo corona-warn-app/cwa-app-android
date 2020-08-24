92da178d6fb92b932cfe5d81448ddafc9439d576
Rituraj Sambherao <git.rituraj.sambherao@gmail.com>
2020-08-24 18:16:43 +0100

    logic for getting device language improved

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterInformationLegalHelper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterInformationLegalHelper.kt
@@ -1,6 +1,8 @@
 @file:JvmName("FormatterInformationLegalHelper")
 
 package de.rki.coronawarnapp.util.formatter
+import android.content.res.Resources
+import android.os.Build
 import android.view.View
 import java.util.Locale
 
@@ -11,6 +13,7 @@ import java.util.Locale
  * @param isContactFormView
  * @return
  */
+
 fun formatVisibilityLanguageBased(defaultLanguageEnglishOrGerman: Boolean, isContactFormView: Boolean?): Int {
     if (defaultLanguageEnglishOrGerman) {
         return if (isContactFormView == true) {
@@ -29,6 +32,38 @@ fun formatVisibilityLanguageBased(defaultLanguageEnglishOrGerman: Boolean, isCon
  * @param isContactFormView
  * @return
  */
-fun formatContactForm(isContactFormView: Boolean?): Int =
-    formatVisibilityLanguageBased(Locale.getDefault().language == Locale.ENGLISH.language ||
-        Locale.getDefault().language == Locale.GERMAN.language, isContactFormView)
+fun formatContactForm(isContactFormView: Boolean?): Int {
+    var locale: Locale = getSysLocale()
+    return formatVisibilityLanguageBased(
+        locale.language == Locale.ENGLISH.language ||
+                locale.language == Locale.GERMAN.language, isContactFormView)
+}
+
+/**
+ * Returns the device Locale
+ * @see Locale
+ *
+ * @return
+ */
+fun getSysLocale(): Locale {
+    return if (hasAndroidNOrHigher()) {
+        @Suppress("NewApi")
+        Resources.getSystem().configuration.locales[0]
+    } else {
+        @Suppress("DEPRECATION")
+        Resources.getSystem().configuration.locale
+    }
+}
+
+/**
+ * checks if the Android version is N (API version 24) or higher
+ * Returns boolean value
+ *
+ * @return
+ */
+fun hasAndroidNOrHigher(): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return true
+    }
+    return false
+}
