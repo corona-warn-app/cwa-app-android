c078158a74f754fe7d7c4b34ffa1a5e6462daab3
Rituraj Sambherao <54317407+ritsam@users.noreply.github.com>
2020-08-27 18:07:43 +0100

    Fix/alternate contact possibility (EXPOSUREAPP-2297, EXPOSUREAPP-2308) (#1055)
    * changed the links to English contact form
    
    * logic for getting device language improved
    
    * String corrections
    
    * manually creating contact form link
    
    * provide locale to everyone
    
    * test contact form logic
    
    * fixed MaxLineLength
    
    * removed not needed resources for lint
    
    Co-authored-by: Philipp Woessner <64482866+pwoessner@users.noreply.github.com>
    Co-authored-by: Jakob Möller <jakob.moeller@sap.com>
    Co-authored-by: chris-cwa <chris.cwa.sap@gmail.com>
    Co-authored-by: chris-cwa <69595386+chris-cwa@users.noreply.github.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/di/ApplicationComponent.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/di/ApplicationComponent.kt
@@ -8,6 +8,7 @@ import de.rki.coronawarnapp.receiver.ReceiverBinder
 import de.rki.coronawarnapp.risk.RiskModule
 import de.rki.coronawarnapp.service.ServiceBinder
 import de.rki.coronawarnapp.ui.ActivityBinder
+import de.rki.coronawarnapp.util.device.DeviceModule
 import javax.inject.Singleton
 
 @Singleton
@@ -18,6 +19,7 @@ import javax.inject.Singleton
         ReceiverBinder::class,
         ServiceBinder::class,
         ActivityBinder::class,
+        DeviceModule::class,
         RiskModule::class
     ]
 )
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterInformationLegalHelper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterInformationLegalHelper.kt
@@ -1,34 +1,30 @@
 @file:JvmName("FormatterInformationLegalHelper")
 
 package de.rki.coronawarnapp.util.formatter
+
 import android.view.View
-import java.util.Locale
+import de.rki.coronawarnapp.CoronaWarnApplication
+import de.rki.coronawarnapp.ui.information.InformationLegalPresentation
+import de.rki.coronawarnapp.util.device.DefaultSystemInfoProvider
 
 /**
  * Language based format visibility
  *
- * @param defaultLanguageEnglishOrGerman
  * @param isContactFormView
  * @return
  */
-fun formatVisibilityLanguageBased(defaultLanguageEnglishOrGerman: Boolean, isContactFormView: Boolean?): Int {
-    if (defaultLanguageEnglishOrGerman) {
-        return if (isContactFormView == true) {
+
+fun formatVisibilityLanguageBased(
+    isContactFormView: Boolean?
+): Int {
+    InformationLegalPresentation(DefaultSystemInfoProvider(CoronaWarnApplication.getAppContext())).apply {
+        if (!showBackupLinkToContactForm) {
+            return if (isContactFormView == true) {
+                View.VISIBLE
+            } else View.GONE
+        }
+        return if (isContactFormView == false) {
             View.VISIBLE
         } else View.GONE
     }
-    return if (isContactFormView == false) {
-        View.VISIBLE
-    } else View.GONE
 }
-
-/**
- * checks the default language of the device and formats the visibility
- * Returns visibility value
- *
- * @param isContactFormView
- * @return
- */
-fun formatContactForm(isContactFormView: Boolean?): Int =
-    formatVisibilityLanguageBased(Locale.getDefault().language == Locale.ENGLISH.language ||
-        Locale.getDefault().language == Locale.GERMAN.language, isContactFormView)
