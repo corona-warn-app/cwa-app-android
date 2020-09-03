e9d2838c78d9f5e763fdfda6d6ab231aa42eb797
chris-cwa <chris.cwa.sap@gmail.com>
2020-09-03 09:32:20 +0200

    fixed test

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/formatter/FormatterRiskHelperTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/formatter/FormatterRiskHelperTest.kt
@@ -93,17 +93,19 @@ class FormatterRiskHelperTest {
         )
     }
 
-//    private fun formatRiskContactBase(iRiskLevelScore: Int?, iMatchedKeysCount: Int?, sValue: String) {
-//        every { context.getString(R.string.risk_card_body_contact) } returns R.string.risk_card_body_contact.toString()
-//
-//        val result = formatRiskContact(riskLevelScore = iRiskLevelScore, matchedKeysCount = iMatchedKeysCount)
-//        assertThat(
-//            result, `is`(sValue)
-//        )
-//    }
+    private fun formatRiskContactBase(iRiskLevelScore: Int?, iMatchedKeysCount: Int?, sValue: String) {
+        every { context.getString(R.string.risk_card_body_contact) } returns R.string.risk_card_body_contact.toString()
+        every { context.getString(R.string.risk_card_body_contact_low_risk) } returns R.string.risk_card_body_contact_low_risk.toString()
+
+        val result = formatRiskContact(riskLevelScore = iRiskLevelScore, matchedKeysCount = iMatchedKeysCount)
+        assertThat(
+            result, `is`(sValue)
+        )
+    }
 
     private fun formatRiskContactLastBase(iRiskLevelScore: Int?, iDaysSinceLastExposure: Int?, sValue: String) {
         every { context.getString(R.string.risk_card_body_contact) } returns R.string.risk_card_body_contact.toString()
+        every { context.getString(R.string.risk_card_body_contact_low_risk) } returns R.string.risk_card_body_contact_low_risk.toString()
 
         val result =
             formatRiskContactLast(riskLevelScore = iRiskLevelScore, daysSinceLastExposure = iDaysSinceLastExposure)
@@ -577,70 +579,70 @@ class FormatterRiskHelperTest {
 
     @Test
     fun formatRiskContact() {
-//        formatRiskContactBase(iRiskLevelScore = null, iMatchedKeysCount = null, sValue = "")
-//        formatRiskContactBase(iRiskLevelScore = null, iMatchedKeysCount = 0, sValue = "")
-//
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.INCREASED_RISK,
-//            iMatchedKeysCount = 0,
-//            sValue = context.getString(R.string.risk_card_body_contact)
-//        )
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.INCREASED_RISK,
-//            iMatchedKeysCount = 2,
-//            sValue = context.resources.getQuantityString(
-//                R.plurals.risk_card_body_contact_value,
-//                2,
-//                2
-//            )
-//        )
-//
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.LOW_LEVEL_RISK,
-//            iMatchedKeysCount = 0,
-//            sValue = context.getString(R.string.risk_card_body_contact)
-//        )
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.LOW_LEVEL_RISK,
-//            iMatchedKeysCount = 2,
-//            sValue = context.resources.getQuantityString(
-//                R.plurals.risk_card_body_contact_value,
-//                2,
-//                2
-//            )
-//        )
-//
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_OUTDATED_RESULTS,
-//            iMatchedKeysCount = 0,
-//            sValue = ""
-//        )
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.NO_CALCULATION_POSSIBLE_TRACING_OFF,
-//            iMatchedKeysCount = 0,
-//            sValue = ""
-//        )
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_INITIAL,
-//            iMatchedKeysCount = 0,
-//            sValue = ""
-//        )
-//
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_OUTDATED_RESULTS,
-//            iMatchedKeysCount = 2,
-//            sValue = ""
-//        )
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.NO_CALCULATION_POSSIBLE_TRACING_OFF,
-//            iMatchedKeysCount = 2,
-//            sValue = ""
-//        )
-//        formatRiskContactBase(
-//            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_INITIAL,
-//            iMatchedKeysCount = 2,
-//            sValue = ""
-//        )
+        formatRiskContactBase(iRiskLevelScore = null, iMatchedKeysCount = null, sValue = "")
+        formatRiskContactBase(iRiskLevelScore = null, iMatchedKeysCount = 0, sValue = "")
+
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.INCREASED_RISK,
+            iMatchedKeysCount = 0,
+            sValue = context.getString(R.string.risk_card_body_contact)
+        )
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.INCREASED_RISK,
+            iMatchedKeysCount = 2,
+            sValue = context.resources.getQuantityString(
+                R.plurals.risk_card_body_contact_value,
+                2,
+                2
+            )
+        )
+
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.LOW_LEVEL_RISK,
+            iMatchedKeysCount = 0,
+            sValue = context.getString(R.string.risk_card_body_contact_low_risk)
+        )
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.LOW_LEVEL_RISK,
+            iMatchedKeysCount = 2,
+            sValue = context.resources.getQuantityString(
+                R.plurals.risk_card_body_contact_value,
+                2,
+                2
+            )
+        )
+
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_OUTDATED_RESULTS,
+            iMatchedKeysCount = 0,
+            sValue = ""
+        )
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.NO_CALCULATION_POSSIBLE_TRACING_OFF,
+            iMatchedKeysCount = 0,
+            sValue = ""
+        )
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_INITIAL,
+            iMatchedKeysCount = 0,
+            sValue = ""
+        )
+
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_OUTDATED_RESULTS,
+            iMatchedKeysCount = 2,
+            sValue = ""
+        )
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.NO_CALCULATION_POSSIBLE_TRACING_OFF,
+            iMatchedKeysCount = 2,
+            sValue = ""
+        )
+        formatRiskContactBase(
+            iRiskLevelScore = RiskLevelConstants.UNKNOWN_RISK_INITIAL,
+            iMatchedKeysCount = 2,
+            sValue = ""
+        )
     }
 
     @Test
