709c8fbbab48ce674c3c906cdbc4106e0c90bf06
Matthias Urhahn <matthias.urhahn@sap.com>
2020-09-09 23:17:28 +0200

    CRUD (instrumentation) test for `KeyCacheDatabase`

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/diagnosiskeys/storage/KeyCacheDatabaseTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/diagnosiskeys/storage/KeyCacheDatabaseTest.kt
@@ -1,40 +1,79 @@
 package de.rki.coronawarnapp.diagnosiskeys.storage
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import de.rki.coronawarnapp.diagnosiskeys.server.LocationCode
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.runBlocking
+import org.joda.time.Instant
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class KeyCacheDatabaseTest {
+    private val database = KeyCacheDatabase.Factory(
+        ApplicationProvider.getApplicationContext<Context>()
+    ).create()
+    private val dao = database.cachedKeyFiles()
 
     @Test
-    fun createEntry() {
-        TODO()
-    }
+    fun crud() {
+        val keyDay = CachedKeyInfo(
+            type = CachedKeyInfo.Type.COUNTRY_DAY,
+            location = LocationCode("DE"),
+            day = LocalDate.now(),
+            hour = null,
+            createdAt = Instant.now()
+        )
+        val keyHour = CachedKeyInfo(
+            type = CachedKeyInfo.Type.COUNTRY_HOUR,
+            location = LocationCode("DE"),
+            day = LocalDate.now(),
+            hour = LocalTime.now(),
+            createdAt = Instant.now()
+        )
+        runBlocking {
+            dao.clear()
 
-    @Test
-    fun deleteEntry() {
-        TODO()
-    }
+            dao.insertEntry(keyDay)
+            dao.insertEntry(keyHour)
+            dao.getAllEntries() shouldBe listOf(keyDay, keyHour)
+            dao.getEntriesForType(CachedKeyInfo.Type.COUNTRY_DAY.typeValue) shouldBe listOf(keyDay)
+            dao.getEntriesForType(CachedKeyInfo.Type.COUNTRY_HOUR.typeValue) shouldBe listOf(keyHour)
 
-    @Test
-    fun clear() {
-        TODO()
-    }
+            dao.updateDownloadState(keyDay.toDownloadUpdate("coffee"))
+            dao.getEntriesForType(CachedKeyInfo.Type.COUNTRY_DAY.typeValue).single().apply {
+                isDownloadComplete shouldBe true
+                checksumMD5 shouldBe "coffee"
+            }
+            dao.getEntriesForType(CachedKeyInfo.Type.COUNTRY_HOUR.typeValue).single().apply {
+                isDownloadComplete shouldBe false
+                checksumMD5 shouldBe null
+            }
 
-    @Test
-    fun getAll() {
-        TODO()
-    }
+            dao.updateDownloadState(keyHour.toDownloadUpdate("with milk"))
+            dao.getEntriesForType(CachedKeyInfo.Type.COUNTRY_DAY.typeValue).single().apply {
+                isDownloadComplete shouldBe true
+                checksumMD5 shouldBe "coffee"
+            }
+            dao.getEntriesForType(CachedKeyInfo.Type.COUNTRY_HOUR.typeValue).single().apply {
+                isDownloadComplete shouldBe true
+                checksumMD5 shouldBe "with milk"
+            }
 
-    @Test
-    fun getAllDayData() {
-        TODO()
-    }
+            dao.deleteEntry(keyDay)
+            dao.getAllEntries() shouldBe listOf(
+                keyHour.copy(
+                    isDownloadComplete = true,
+                    checksumMD5 = "with milk"
+                )
+            )
 
-    @Test
-    fun getAllHourData() {
-        TODO()
+            dao.clear()
+            dao.getAllEntries() shouldBe emptyList()
+        }
     }
-
 }
