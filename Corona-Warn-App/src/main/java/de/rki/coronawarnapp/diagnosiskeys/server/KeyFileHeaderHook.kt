5b87f7f6f8f8e8abbbda3e255f8333d21260b346
Matthias Urhahn <matthias.urhahn@sap.com>
2020-09-11 10:36:18 +0200

    Add comment with reference to ticket regarding follow up on the other headers.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/diagnosiskeys/server/KeyFileHeaderHook.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/diagnosiskeys/server/KeyFileHeaderHook.kt
@@ -11,6 +11,7 @@ class KeyFileHeaderHook(
     fun Headers.getPayloadChecksumMD5(): String? {
         // TODO Ping backend regarding alternative checksum sources
         var fileMD5 = values("ETag").singleOrNull()
+        // The hash from these headers doesn't match, TODO EXPOSUREBACK-178
 //                var fileMD5 = headers.values("x-amz-meta-cwa-hash-md5").singleOrNull()
 //                if (fileMD5 == null) {
 //                    headers.values("x-amz-meta-cwa-hash").singleOrNull()
