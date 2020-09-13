6ff60a208229141fa1f9c370fd6ba85fd8a974cb
chris-cwa <chris.cwa.sap@gmail.com>
2020-09-13 23:39:57 +0200

    represent date by long in millis

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/StartOfSymptoms.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/StartOfSymptoms.kt
@@ -2,7 +2,7 @@ package de.rki.coronawarnapp.submission
 
 sealed class StartOfSymptoms {
 
-    data class Date(val date: Int) : StartOfSymptoms()
+    data class Date(val millis: Long) : StartOfSymptoms()
     object LastSevenDays : StartOfSymptoms()
     object OneToTwoWeeksAgo : StartOfSymptoms()
     object MoreThanTwoWeeks : StartOfSymptoms()
