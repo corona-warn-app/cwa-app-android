d945179bfcbed4f38fc5b20ac7ac919ba23cb3dd
BerndMitter <Berndus@gmx.de>
2020-09-14 10:05:14 +0200

    Better SubmissionOutOfGermanyViewModel testing

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/LiveDataTextUtil.kt
@@ -0,0 +1,57 @@
+/**
+ * Credits to: https://github.com/android/architecture-components-samples/blob/master/LiveDataSample/app/src/test/java/com/android/example/livedatabuilder/util/LiveDataTestUtil.kt
+ */
+package de.rki.coronawarnapp.util
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+/**
+ * Gets the value of a [LiveData] or waits for it to have one, with a timeout.
+ *
+ * Use this extension from host-side (JVM) tests. It's recommended to use it alongside
+ * `InstantTaskExecutorRule` or a similar mechanism to execute tasks synchronously.
+ */
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    afterObserve: () -> Unit = {}
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+
+    afterObserve.invoke()
+
+    // Don't wait indefinitely if the LiveData is not set.
+    if (!latch.await(time, timeUnit)) {
+        this.removeObserver(observer)
+        throw TimeoutException("LiveData value was never set.")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
+}
+
+/**
+ * Observes a [LiveData] until the `block` is done executing.
+ */
+fun <T> LiveData<T>.observeForTesting(block: () -> Unit) {
+    val observer = Observer<T> { }
+    try {
+        observeForever(observer)
+        block()
+    } finally {
+        removeObserver(observer)
+    }
+}
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/viewmodel/SubmissionOutOfGermanyViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/viewmodel/SubmissionOutOfGermanyViewModelTest.kt
@@ -1,60 +1,26 @@
 package de.rki.coronawarnapp.ui.submission.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
+import de.rki.coronawarnapp.util.getOrAwaitValue
 import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.reset
-import org.mockito.Mockito.verify
-import org.mockito.junit.MockitoJUnitRunner
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 
-@RunWith(MockitoJUnitRunner::class)
 class SubmissionOutOfGermanyViewModelTest {
 
     private val submissionOutOfGermanyViewModel: SubmissionOutOfGermanyViewModel =
         SubmissionOutOfGermanyViewModel()
-
-    @Mock
-    lateinit var yesAnswerActiveObserver: Observer<Boolean>
-
-    @Mock
-    lateinit var noAnswerActiveObserver: Observer<Boolean>
-
-    @Mock
-    lateinit var notSpecifiedActiveObserver: Observer<Boolean>
-
-    @Mock
-    lateinit var nextActiveObserver: Observer<Boolean>
 
     // @JvmField could be removed when using @get:Rule
     @Rule
     @JvmField
     var instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Before
-    fun setupBeforeEachTest() {
-        submissionOutOfGermanyViewModel.yesAnswerActive.observeForever(yesAnswerActiveObserver)
-        submissionOutOfGermanyViewModel.noAnswerActive.observeForever(noAnswerActiveObserver)
-        submissionOutOfGermanyViewModel.notSpecifiedActive.observeForever(notSpecifiedActiveObserver)
-        submissionOutOfGermanyViewModel.nextActive.observeForever(nextActiveObserver)
-
-        reset(yesAnswerActiveObserver)
-        reset(noAnswerActiveObserver)
-        reset(notSpecifiedActiveObserver)
-        reset(nextActiveObserver)
-    }
-
     @After
     fun runAfterEachTest() {
-        submissionOutOfGermanyViewModel.yesAnswerActive.removeObserver(yesAnswerActiveObserver)
-        submissionOutOfGermanyViewModel.noAnswerActive.removeObserver(noAnswerActiveObserver)
-        submissionOutOfGermanyViewModel.notSpecifiedActive.removeObserver(notSpecifiedActiveObserver)
-        submissionOutOfGermanyViewModel.nextActive.removeObserver(nextActiveObserver)
-        
+        // Reset button clicked to default value
         submissionOutOfGermanyViewModel.buttonClicked(ButtonClicked.NONE)
     }
 
@@ -62,39 +28,39 @@ class SubmissionOutOfGermanyViewModelTest {
     fun textButtonClickedNone() {
         submissionOutOfGermanyViewModel.buttonClicked(ButtonClicked.NONE)
 
-        verify(yesAnswerActiveObserver).onChanged(false)
-        verify(noAnswerActiveObserver).onChanged(false)
-        verify(notSpecifiedActiveObserver).onChanged(false)
-        verify(nextActiveObserver).onChanged(false)
+        assertFalse(submissionOutOfGermanyViewModel.yesAnswerActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.noAnswerActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.notSpecifiedActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.nextActive.getOrAwaitValue())
     }
 
     @Test
     fun textButtonClickedYes() {
         submissionOutOfGermanyViewModel.buttonClicked(ButtonClicked.YES)
 
-        verify(yesAnswerActiveObserver).onChanged(true)
-        verify(noAnswerActiveObserver).onChanged(false)
-        verify(notSpecifiedActiveObserver).onChanged(false)
-        verify(nextActiveObserver).onChanged(true)
+        assertTrue(submissionOutOfGermanyViewModel.yesAnswerActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.noAnswerActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.notSpecifiedActive.getOrAwaitValue())
+        assertTrue(submissionOutOfGermanyViewModel.nextActive.getOrAwaitValue())
     }
 
     @Test
     fun textButtonClickedNo() {
         submissionOutOfGermanyViewModel.buttonClicked(ButtonClicked.NO)
 
-        verify(yesAnswerActiveObserver).onChanged(false)
-        verify(noAnswerActiveObserver).onChanged(true)
-        verify(notSpecifiedActiveObserver).onChanged(false)
-        verify(nextActiveObserver).onChanged(true)
+        assertFalse(submissionOutOfGermanyViewModel.yesAnswerActive.getOrAwaitValue())
+        assertTrue(submissionOutOfGermanyViewModel.noAnswerActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.notSpecifiedActive.getOrAwaitValue())
+        assertTrue(submissionOutOfGermanyViewModel.nextActive.getOrAwaitValue())
     }
 
     @Test
     fun textButtonClickedNotSpecified() {
         submissionOutOfGermanyViewModel.buttonClicked(ButtonClicked.NOT_SPECIFIED)
 
-        verify(yesAnswerActiveObserver).onChanged(false)
-        verify(noAnswerActiveObserver).onChanged(false)
-        verify(notSpecifiedActiveObserver).onChanged(true)
-        verify(nextActiveObserver).onChanged(true)
+        assertFalse(submissionOutOfGermanyViewModel.yesAnswerActive.getOrAwaitValue())
+        assertFalse(submissionOutOfGermanyViewModel.noAnswerActive.getOrAwaitValue())
+        assertTrue(submissionOutOfGermanyViewModel.notSpecifiedActive.getOrAwaitValue())
+        assertTrue(submissionOutOfGermanyViewModel.nextActive.getOrAwaitValue())
     }
 }
