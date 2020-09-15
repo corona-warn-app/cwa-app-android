04fe098be44d7a7553a8a415bff3d476fddaf781
Oliver Zimmerman <oezimmerman@gmail.com>
2020-09-15 15:50:15 +0100

    removed unused events

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/SymptomCalendarEvent.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/SymptomCalendarEvent.kt
@@ -5,8 +5,4 @@ import de.rki.coronawarnapp.submission.StartOfSymptoms
 sealed class SymptomCalendarEvent {
     object NavigateToNext : SymptomCalendarEvent()
     object NavigateToPrevious : SymptomCalendarEvent()
-    object SelectLastSeven : SymptomCalendarEvent()
-    object SelectOneToTwoWeeks : SymptomCalendarEvent()
-    object SelectMoreThanTwoWeeks : SymptomCalendarEvent()
-    object SelectNoInformation : SymptomCalendarEvent()
 }
