dc08374d1d86eb04e7f68f89a582befef670cdd2
chris-cwa <chris.cwa.sap@gmail.com>
2020-09-25 15:43:56 +0200

    https://github.com/corona-warn-app/cwa-app-android/pull/1223#discussion_r494957683

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/DefaultKeyConverter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/DefaultKeyConverter.kt
@@ -5,11 +5,6 @@ import com.google.android.gms.nearby.exposurenotification.TemporaryExposureKey
 import com.google.protobuf.ByteString
 
 class DefaultKeyConverter : KeyConverter {
-
-    companion object {
-        private const val ROLLING_PERIOD = 144
-    }
-
     override fun toExternalFormat(
         key: TemporaryExposureKey,
         riskValue: Int,
@@ -18,7 +13,7 @@ class DefaultKeyConverter : KeyConverter {
         KeyExportFormat.TemporaryExposureKey.newBuilder()
             .setKeyData(ByteString.readFrom(key.keyData.inputStream()))
             .setRollingStartIntervalNumber(key.rollingStartIntervalNumber)
-            .setRollingPeriod(ROLLING_PERIOD)
+            .setRollingPeriod(key.rollingPeriod)
             .setTransmissionRiskLevel(riskValue)
             .setDaysSinceOnsetOfSymptoms(daysSinceOnsetOfSymptoms)
             .build()
