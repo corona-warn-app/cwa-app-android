7b4d6d9246e4d0ee4620a4d5259c6e8a6b392b84
Matthias Urhahn <matthias.urhahn@sap.com>
2020-09-28 17:22:00 +0200

    Fixed detekt issue

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/environment/BaseEnvironmentModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/environment/BaseEnvironmentModule.kt
@@ -1,6 +1,6 @@
 package de.rki.coronawarnapp.environment
 
-abstract class BaseEnvironmentModule {
+open class BaseEnvironmentModule {
     fun requireValidUrl(url: String) {
         if (!url.startsWith("https://")) throw IllegalStateException("Invalid: $url")
     }
