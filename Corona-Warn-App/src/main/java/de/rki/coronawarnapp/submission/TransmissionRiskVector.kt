ed9bb2f36f642801339f228ebf0ec3700f6fd208
chris-cwa <chris.cwa.sap@gmail.com>
2020-09-30 14:17:11 +0200

    End of class please.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/TransmissionRiskVector.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/TransmissionRiskVector.kt
@@ -2,13 +2,13 @@ package de.rki.coronawarnapp.submission
 
 class TransmissionRiskVector(private val values: IntArray) {
 
-    companion object {
-        private const val DEFAULT_TRANSMISSION_RISK_LEVEL = 1
-    }
-
     val raw: IntArray
         get() = values
 
     operator fun get(index: Int) =
         if (index < values.size) values[index] else DEFAULT_TRANSMISSION_RISK_LEVEL
+
+    companion object {
+        private const val DEFAULT_TRANSMISSION_RISK_LEVEL = 1
+    }
 }
