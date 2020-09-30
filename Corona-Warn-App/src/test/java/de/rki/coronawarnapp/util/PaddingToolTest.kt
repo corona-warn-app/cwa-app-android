884d9734d6e083631880f3acf9d5eb9d0489320c
Matthias Urhahn <matthias.urhahn@sap.com>
2020-09-30 15:19:33 +0200

    Fix unreliable test. Random could be randomly 0 which would fail the test.

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/PaddingToolTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/PaddingToolTest.kt
@@ -3,6 +3,7 @@ package de.rki.coronawarnapp.util
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
+import timber.log.Timber
 import kotlin.math.abs
 import kotlin.random.Random
 
@@ -12,10 +13,11 @@ class PaddingToolTest : BaseTest() {
 
     @Test
     fun `verify padding patterns`() {
-        repeat(100) {
-            val randomLength = abs(Random.nextInt(1024))
+        repeat(1000) {
+            val randomLength = abs(Random.nextInt(1, 1024))
             PaddingTool.requestPadding(randomLength).apply {
                 length shouldBe randomLength
+                Timber.v("RandomLength: %d, Padding: %s", randomLength, this)
                 validPattern.matches(this) shouldBe true
             }
         }
