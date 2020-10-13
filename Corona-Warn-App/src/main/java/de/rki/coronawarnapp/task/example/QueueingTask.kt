1cfff398ffb9143746fd5e49d1bb985402ed8091
chris-cwa <chris.cwa.sap@gmail.com>
2020-10-13 19:30:45 +0200

    updated the example

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/task/example/QueueingTask.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/task/example/QueueingTask.kt
@@ -63,6 +63,8 @@ open class QueueingTask @Inject constructor() : Task<DefaultProgress, QueueingTa
     ) : Task.Result
 
     data class Config(
+        override val timeout: Long? = null,
+
         override val collisionBehavior: TaskFactory.Config.CollisionBehavior =
             TaskFactory.Config.CollisionBehavior.ENQUEUE
 
