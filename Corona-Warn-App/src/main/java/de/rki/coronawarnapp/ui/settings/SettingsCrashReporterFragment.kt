68d16b38a05bcd03497ab0a8abf7a4dbcb1512ce
BMItter <Berndus@gmx.de>
2020-10-15 16:33:24 +0200

    use separated Adapter in ReporterFragment - cleanup

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/SettingsCrashReporterFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/SettingsCrashReporterFragment.kt
@@ -5,11 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import de.rki.coronawarnapp.R
-import de.rki.coronawarnapp.crash.CrashModel
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.fragment_crashreporter_overview.*
 
 class SettingsCrashReporterFragment : Fragment() {
 
@@ -24,7 +20,7 @@ class SettingsCrashReporterFragment : Fragment() {
             }
     }
 
-    private lateinit var adapter: MyCrashesAdapter
+    private lateinit var adapter: CrashReporterAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,51 +36,5 @@ class SettingsCrashReporterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_crashreporter_overview, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        val items = listOf(
-            CrashModel("Crash #0", "Nullpointer wie in alten Zeiten"),
-            CrashModel("Crash #1", "Source"),
-            CrashModel("Crash #2", "Source"),
-            CrashModel("Crash #3", "Source"),
-            CrashModel("Crash #4", "Source"),
-            CrashModel("Crash #5", "Source")
-        )
-
-        adapter = MyCrashesAdapter()
-        adapter.replaceItems(items)
-        list.adapter = adapter
-    }
-
-    // ****************ADAPTER***************
-
-    class MyCrashesAdapter : RecyclerView.Adapter<MyCrashesAdapter.ViewHolder>() {
-        private var items = listOf<CrashModel>()
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.view_crashreport_list_item, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = items[position]
-
-            // holder.contentTextView.text = item.content
-            // holder.sourceTextView.text = item.source
-        }
-
-        fun replaceItems(items: List<CrashModel>) {
-            this.items = items
-            notifyDataSetChanged()
-        }
-
-        override fun getItemCount(): Int = items.size
-
-        inner class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
-            LayoutContainer
     }
 }
