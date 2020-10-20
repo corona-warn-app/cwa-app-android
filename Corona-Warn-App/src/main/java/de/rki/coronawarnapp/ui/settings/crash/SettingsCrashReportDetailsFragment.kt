20033a64877e2a0e92570f12c4499275478467ae
BMItter <Berndus@gmx.de>
2020-10-20 16:31:20 +0200

    details added to report - wip

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/crash/SettingsCrashReportDetailsFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/crash/SettingsCrashReportDetailsFragment.kt
@@ -32,7 +32,18 @@ class SettingsCrashReportDetailsFragment :
 
         vm.selectedCrashReport.observe2(this) {
             fragmentSettingsCrashReportDetailsBinding.textViewCrashReportDetails.text =
-                "Selected crash report ${it.id}"
+                "Selected crash report ${it.id} \n" +
+                    " # appeared at: ${it.crashedAt} \n\n" +
+                    " # Device: ${it.deviceInfo} \n" +
+                    " # Android Version ${it.androidVersion} \n" +
+                    " # Android API-Level ${it.apiLevel} \n\n" +
+
+                    " # AppVersion: ${it.appVersionName} \n" +
+                    " # AppVercionCode ${it.appVersionCode} \n" +
+                    " # C-Hash ${it.shortID} \n\n\n" +
+
+                    " ${it.stackTrace}"
+
         }
     }
 }
