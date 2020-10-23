75a67d3441e49cc28f2305efa6a6d4d95cfadc25
BMItter <Berndus@gmx.de>
2020-10-23 23:59:31 +0200

    quickfix - wip

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/bugreporting/event/BugEvent.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/bugreporting/event/BugEvent.kt
@@ -14,6 +14,7 @@ interface BugEvent {
     val stackTrace: String
     val appVersionName: String
     val appVersionCode: Int
+    val deviceInfo: String
     val apiLevel: Int
     val androidVersion: String
     val shortID: String
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterCrashReport.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterCrashReport.kt
@@ -2,10 +2,13 @@
 
 package de.rki.coronawarnapp.util.formatter
 
+import android.os.Build
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import de.rki.coronawarnapp.R
 import java.text.SimpleDateFormat
+import java.time.Instant
 import java.util.Date
 import java.util.Locale
 
@@ -14,10 +17,12 @@ fun formatCrashReportId(textView: TextView, id: Int) {
     textView.text = textView.context.getString(R.string.crash_report_title, id)
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @BindingAdapter("crashReportDate")
-fun formatCrashReportDate(textView: TextView, date: Date) {
+fun formatCrashReportDate(textView: TextView, dateInstant: Instant) {
+    val myDate = Date.from(dateInstant)
     val sdf = SimpleDateFormat("Edd.MM.yyyy, HH:mm:ss", Locale.getDefault())
-    textView.text = sdf.format(date)
+    textView.text = sdf.format(myDate)
 }
 
 @BindingAdapter("crashReportShortMessage")
