994d6976c7aee3e3db3d4a201a8eb36bf9536aec
chris-cwa <chris.cwa.sap@gmail.com>
2020-10-28 12:43:12 +0100

    map task factory

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/RiskModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/RiskModule.kt
@@ -2,10 +2,21 @@ package de.rki.coronawarnapp.risk
 
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import de.rki.coronawarnapp.task.Task
+import de.rki.coronawarnapp.task.TaskFactory
+import de.rki.coronawarnapp.task.TaskTypeKey
 import javax.inject.Singleton
 
 @Module
 abstract class RiskModule {
+
+    @Binds
+    @IntoMap
+    @TaskTypeKey(RiskLevelTask::class)
+    abstract fun riskLevelTaskFactory(
+        factory: RiskLevelTask.Factory
+    ): TaskFactory<out Task.Progress, out Task.Result>
 
     @Binds
     @Singleton
