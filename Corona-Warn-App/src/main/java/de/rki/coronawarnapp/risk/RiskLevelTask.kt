1a16472384484902e95fe4de25e4c25f57e3406c
chris-cwa <chris.cwa.sap@gmail.com>
2020-10-30 14:10:48 +0100

    reduced static access

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/RiskLevelTask.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/RiskLevelTask.kt
@@ -2,20 +2,25 @@ package de.rki.coronawarnapp.risk
 
 import android.content.Context
 import com.google.android.gms.nearby.exposurenotification.ExposureSummary
-import de.rki.coronawarnapp.CoronaWarnApplication
 import de.rki.coronawarnapp.exception.ExceptionCategory
 import de.rki.coronawarnapp.exception.RiskLevelCalculationException
 import de.rki.coronawarnapp.exception.reporting.report
 import de.rki.coronawarnapp.nearby.ENFClient
 import de.rki.coronawarnapp.nearby.InternalExposureNotificationClient
-import de.rki.coronawarnapp.risk.RiskLevel.*
+import de.rki.coronawarnapp.risk.RiskLevel.INCREASED_RISK
+import de.rki.coronawarnapp.risk.RiskLevel.LOW_LEVEL_RISK
+import de.rki.coronawarnapp.risk.RiskLevel.NO_CALCULATION_POSSIBLE_TRACING_OFF
+import de.rki.coronawarnapp.risk.RiskLevel.UNDETERMINED
+import de.rki.coronawarnapp.risk.RiskLevel.UNKNOWN_RISK_INITIAL
+import de.rki.coronawarnapp.risk.RiskLevel.UNKNOWN_RISK_OUTDATED_RESULTS
+import de.rki.coronawarnapp.risk.RiskLevel.UNKNOWN_RISK_OUTDATED_RESULTS_MANUAL
 import de.rki.coronawarnapp.storage.LocalData
 import de.rki.coronawarnapp.storage.RiskLevelRepository
 import de.rki.coronawarnapp.task.Task
 import de.rki.coronawarnapp.task.TaskCancellationException
 import de.rki.coronawarnapp.task.TaskFactory
 import de.rki.coronawarnapp.task.common.DefaultProgress
-import de.rki.coronawarnapp.util.ConnectivityHelper
+import de.rki.coronawarnapp.util.BackgroundModeStatus
 import de.rki.coronawarnapp.util.ConnectivityHelper.isNetworkEnabled
 import de.rki.coronawarnapp.util.TimeStamper
 import de.rki.coronawarnapp.util.di.AppContext
@@ -32,7 +37,8 @@ class RiskLevelTask @Inject constructor(
     private val riskLevels: RiskLevels,
     @AppContext private val context: Context,
     private val enfClient: ENFClient,
-    private val timeStamper: TimeStamper
+    private val timeStamper: TimeStamper,
+    private val backgroundModeStatus: BackgroundModeStatus
 ) : Task<DefaultProgress, RiskLevelTask.Result> {
 
     private val internalProgress = ConflatedBroadcastChannel<DefaultProgress>()
@@ -63,7 +69,7 @@ class RiskLevelTask @Inject constructor(
 
                         calculationNotPossibleBecauseOfOutdatedResults().also {
                             checkCancel()
-                        } -> if (backgroundJobsEnabled) {
+                        } -> if (backgroundJobsEnabled()) {
                             UNKNOWN_RISK_OUTDATED_RESULTS
                         } else {
                             UNKNOWN_RISK_OUTDATED_RESULTS_MANUAL
@@ -117,19 +123,19 @@ class RiskLevelTask @Inject constructor(
         if (isCanceled) throw TaskCancellationException()
     }
 
-    private val backgroundJobsEnabled: Boolean
-        get() = ConnectivityHelper.autoModeEnabled(CoronaWarnApplication.getAppContext()).also {
-            if (it) {
-                Timber.tag(TAG)
-                    .v("diagnosis keys outdated and active tracing time is above threshold")
-                Timber.tag(TAG)
-                    .v("manual mode not active (background jobs enabled)")
-            } else {
-                Timber.tag(TAG)
-                    .v("diagnosis keys outdated and active tracing time is above threshold")
-                Timber.tag(TAG).v("manual mode active (background jobs disabled)")
+    private suspend fun backgroundJobsEnabled() =
+            backgroundModeStatus.isAutoModeEnabled.first().also {
+                if (it) {
+                    Timber.tag(TAG)
+                            .v("diagnosis keys outdated and active tracing time is above threshold")
+                    Timber.tag(TAG)
+                            .v("manual mode not active (background jobs enabled)")
+                } else {
+                    Timber.tag(TAG)
+                            .v("diagnosis keys outdated and active tracing time is above threshold")
+                    Timber.tag(TAG).v("manual mode active (background jobs disabled)")
+                }
             }
-        }
 
     override suspend fun cancel() {
         Timber.w("cancel() called.")
