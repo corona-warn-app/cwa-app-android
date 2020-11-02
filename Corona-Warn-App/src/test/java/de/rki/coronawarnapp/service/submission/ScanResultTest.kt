95c40a1056d8801543682b6d28bf17f848fa70b6
Rituraj Sambherao <git.rituraj.sambherao@gmail.com>
2020-11-02 16:33:50 +0000

    test cases added as new potential guidelines (DO NOT MERGE YET)

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/service/submission/ScanResultTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/service/submission/ScanResultTest.kt
@@ -10,6 +10,8 @@ import org.junit.Test
 
 class ScanResultTest {
     private val guid = "123456-12345678-1234-4DA7-B166-B86D85475064"
+    private val lowerCaseGuid = "123456-12345678-1234-4da7-b166-b86d85475064"
+    private val mixedCaseGuid = "123456-12345678-1234-4dA7-b166-B86d85475064"
 
     @MockK
     private lateinit var scanResult: QRScanResult
@@ -26,6 +28,20 @@ class ScanResultTest {
         // valid test
         scanResult = QRScanResult("https://localhost/?$guid")
         scanResult.isValid shouldBe true
+
+        //UPPERCASE and MIXED Cases Strings
+        scanResult = QRScanResult("https://localhost/?$lowerCaseGuid")
+        scanResult.isValid shouldBe true
+
+        scanResult = QRScanResult("HTTPS://LOCALHOST/?$lowerCaseGuid")
+        scanResult.isValid shouldBe true
+
+        scanResult = QRScanResult("https://localhost/?$mixedCaseGuid")
+        scanResult.isValid shouldBe true
+
+        scanResult = QRScanResult("HTTPS://LOCALHOST/?$mixedCaseGuid")
+        scanResult.isValid shouldBe true
+
 
         // more invalid tests checks
         scanResult = QRScanResult("http://localhost/?$guid")
