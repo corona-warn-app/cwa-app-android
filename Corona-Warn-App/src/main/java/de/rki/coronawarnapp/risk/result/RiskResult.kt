f4884a35c259715cbcc36213940f840e906aa92d
BMItter <Berndus@gmx.de>
2020-11-03 16:14:25 +0100

    ktlint

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/result/RiskResult.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/result/RiskResult.kt
@@ -2,7 +2,7 @@ package de.rki.coronawarnapp.risk.result
 
 import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass
 
-//TODO("Adjust Types")
+// TODO("Adjust Types")
 data class RiskResult(
     val transmissionRiskLevel: Any,
     val normalizedTime: Int,
