3d5d4e311d43399a4e436672a2c6d4d8c2928432
BMItter <Berndus@gmx.de>
2020-11-03 15:52:53 +0100

    Implementing steps to aggregate results form exposure windows - wip

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/DefaultRiskLevelCalculation.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/DefaultRiskLevelCalculation.kt
@@ -1,13 +1,17 @@
 package de.rki.coronawarnapp.risk
 
+import android.text.TextUtils
 import com.google.android.gms.nearby.exposurenotification.ExposureSummary
 import com.google.android.gms.nearby.exposurenotification.ExposureWindow
 import de.rki.coronawarnapp.risk.result.AggregatedRiskResult
+import de.rki.coronawarnapp.risk.result.ExposureData
 import de.rki.coronawarnapp.risk.result.RiskResult
-import de.rki.coronawarnapp.server.protocols.internal.AttenuationDurationOuterClass.AttenuationDuration
+import de.rki.coronawarnapp.server.protocols.internal.AttenuationDurationOuterClass
 import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass
 
 import timber.log.Timber
+import java.lang.Exception
+import java.lang.IllegalArgumentException
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.math.round
@@ -23,7 +27,7 @@ class DefaultRiskLevelCalculation @Inject constructor() : RiskLevelCalculation {
     }
 
     override fun calculateRiskScore(
-        attenuationParameters: AttenuationDuration,
+        attenuationParameters: AttenuationDurationOuterClass.AttenuationDuration,
         exposureSummary: ExposureSummary
     ): Double {
 
@@ -60,6 +64,7 @@ class DefaultRiskLevelCalculation @Inject constructor() : RiskLevelCalculation {
 
         return round(riskScore.times(DECIMAL_MULTIPLIER)).div(DECIMAL_MULTIPLIER)
     }
+
     private fun Int.capped(): Int {
         return if (this > TimeVariables.getMaxAttenuationDuration()) {
             TimeVariables.getMaxAttenuationDuration()
@@ -79,6 +84,108 @@ class DefaultRiskLevelCalculation @Inject constructor() : RiskLevelCalculation {
         exposureWindowsAndResult: Map<ExposureWindow, RiskResult>,
         riskCalculationParameters: RiskCalculationParametersOuterClass.RiskCalculationParameters
     ): AggregatedRiskResult {
-        TODO("Not yet implemented")
+        val uniqueDates = exposureWindowsAndResult.keys
+            .map { it.dateMillisSinceEpoch }
+            .toSet()
+        Timber.d("uniqueDates: ${TextUtils.join(System.lineSeparator(), uniqueDates)}")
+
+        val exposureHistory = uniqueDates.map {
+            exposureDataMapper(
+                it,
+                exposureWindowsAndResult,
+                riskCalculationParameters
+            )
+        }
+
+        exposureHistory.forEach { Timber.d("(date=${it.date}, riskLevel=${it.riskLevel}, minimumDistinctEncountersWithLowRisk=${it.minimumDistinctEncountersWithLowRisk}, minimumDistinctEncountersWithHighRisk=${it.minimumDistinctEncountersWithHighRisk})") }
+
+        // 6. Determine `Total Risk`
+        val totalRiskLevel =
+            if (exposureHistory.any { it.riskLevel == RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.HIGH }) {
+                RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.HIGH
+            } else {
+                RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.LOW
+            }
+
+        Timber.d("totalRiskLevel: ${totalRiskLevel.name} (${totalRiskLevel.ordinal})")
+
+        // 7. Determine `Date of Most Recent Date with Low Risk`
+
+        // 8. Determine `Date of Most Recent Date with High Risk`
+
+        // 9. Determine `Total Minimum Distinct Encounters With Low Risk`
+
+        // 10. Determine `Total Minimum Distinct Encounters With High Risk`
+
+        TODO("Implement last steps and adjust values")
+
+        return AggregatedRiskResult(
+            totalRiskLevel,
+            totalMinimumDistinctEncountersWithLowRisk = 1,
+            totalMinimumDistinctEncountersWithHighRisk = 1,
+            mostRecentDateWithLowRisk = 1,
+            mostRecentDateWithHighRisk = 1
+        )
+    }
+
+    private fun exposureDataMapper(
+        date: Long,
+        exposureWindowsAndResult: Map<ExposureWindow, RiskResult>,
+        riskCalculationParameters: RiskCalculationParametersOuterClass.RiskCalculationParameters
+    ): ExposureData {
+        // 1. Group `Exposure Windows by Date`
+        val exposureWindowsAndResultForDate = exposureWindowsAndResult
+            .filter { it.key.dateMillisSinceEpoch == date }
+
+        // 2. Determine `Normalized Time per Date`
+        val normalizedTime = exposureWindowsAndResultForDate.values
+            .sumOf { it.normalizedTime }
+
+        Timber.d("normalizedTime: $normalizedTime")
+
+        // 3. Determine `Risk Level per Date`
+        val riskLevel = try {
+            riskCalculationParameters.normalizedTimePerDayToRiskLevelMappingList
+                .filter { inRange(it.normalizedTimeRange, normalizedTime.toLong()) }
+                .map { it.riskLevel }
+                .first()
+        } catch (e: Exception) {
+            throw IllegalArgumentException("Invalid config for normalizedTimePerDayToRiskLevelMapping")
+        }
+
+        Timber.d("riskLevel: ${riskLevel.name} (${riskLevel.ordinal})")
+
+        // 4. Determine `Minimum Distinct Encounters With Low Risk per Date`
+        val minimumDistinctEncountersWithLowRisk = exposureWindowsAndResultForDate
+            .filter { it.value.riskLevel == RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.LOW }
+            .map { "${it.value.transmissionRiskLevel}_${it.key.calibrationConfidence}" }
+            .distinct()
+            .size
+
+        Timber.d("minimumDistinctEncountersWithLowRisk: $minimumDistinctEncountersWithLowRisk")
+
+        // 5. Determine `Minimum Distinct Encounters With High Risk per Date`
+        val minimumDistinctEncountersWithHighRisk = exposureWindowsAndResultForDate
+            .filter { it.value.riskLevel == RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.HIGH }
+            .map { "${it.value.transmissionRiskLevel}_${it.key.calibrationConfidence}" }
+            .distinct()
+            .size
+
+        Timber.d("minimumDistinctEncountersWithHighRisk: $minimumDistinctEncountersWithHighRisk")
+
+        return ExposureData(
+            date,
+            riskLevel,
+            minimumDistinctEncountersWithLowRisk,
+            minimumDistinctEncountersWithHighRisk
+        )
+    }
+
+    private fun inRange(range: RiskCalculationParametersOuterClass.Range, value: Long): Boolean {
+        if (range.minExclusive && value <= range.min) return false
+        else if (!range.minExclusive && value < range.min) return false
+        if (range.maxExclusive && value >= range.max) return false
+        else if (!range.maxExclusive && value > range.max) return false
+        return true
     }
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/result/RiskResult.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/result/RiskResult.kt
@@ -1,8 +1,10 @@
 package de.rki.coronawarnapp.risk.result
 
+import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass
+
 //TODO("Adjust Types")
 data class RiskResult(
     val transmissionRiskLevel: Any,
-    val normalizedTime: Any,
-    val riskLevel: Int
+    val normalizedTime: Int,
+    val riskLevel: RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel
 )
