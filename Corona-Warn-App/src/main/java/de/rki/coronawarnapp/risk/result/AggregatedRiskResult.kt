c8b49c840ff5bc12d3340e846519749d6456461c
BMItter <Berndus@gmx.de>
2020-11-05 16:05:37 +0100

    Implementing steps to aggregate results form exposure windows

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/result/AggregatedRiskResult.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/result/AggregatedRiskResult.kt
@@ -2,11 +2,10 @@ package de.rki.coronawarnapp.risk.result
 
 import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass
 
-// TODO("Adjust Types")
 data class AggregatedRiskResult(
     val totalRiskLevel: RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel,
     val totalMinimumDistinctEncountersWithLowRisk: Int,
     val totalMinimumDistinctEncountersWithHighRisk: Int,
-    val mostRecentDateWithLowRisk: Int?,
-    val mostRecentDateWithHighRisk: Int?
+    val mostRecentDateWithLowRisk: Long?,
+    val mostRecentDateWithHighRisk: Long?
 )
