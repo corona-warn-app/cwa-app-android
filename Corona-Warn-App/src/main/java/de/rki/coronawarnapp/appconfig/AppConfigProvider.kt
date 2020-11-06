f20959fb00714192a973d96feb77b6c383d6dbcb
Matthias Urhahn <matthias.urhahn@sap.com>
2020-11-06 22:29:42 +0100

    Run the app config retrieval on the app scope, otherwise UI unsubscription could cancel the app config retrieval.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/AppConfigProvider.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/AppConfigProvider.kt
@@ -2,12 +2,11 @@ package de.rki.coronawarnapp.appconfig
 
 import de.rki.coronawarnapp.util.coroutine.AppScope
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import de.rki.coronawarnapp.util.flow.HotDataFlow
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.SharingStarted
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -19,24 +18,31 @@ class AppConfigProvider @Inject constructor(
     @AppScope private val scope: CoroutineScope
 ) {
 
-    private val mutex = Mutex()
-    private val currentConfigInternal = MutableStateFlow<ConfigData?>(null)
-
-    val currentConfig: Flow<ConfigData?> = currentConfigInternal
-
-    suspend fun clear() = mutex.withLock {
-        Timber.tag(TAG).v("clear()")
-        source.clear()
-        currentConfigInternal.value = null
+    private val configHolder = HotDataFlow(
+        loggingTag = "AppConfigProvider",
+        scope = scope,
+        coroutineContext = dispatcherProvider.IO,
+        sharingBehavior = SharingStarted.Lazily
+    ) {
+        source.retrieveConfig()
     }
 
-    suspend fun getAppConfig(): ConfigData = mutex.withLock {
-        Timber.tag(TAG).v("getAppConfig()")
-        withContext(context = scope.coroutineContext + dispatcherProvider.IO) {
-            source.retrieveConfig().also {
-                currentConfigInternal.emit(it)
+    val currentConfig: Flow<ConfigData> = configHolder.data
+
+    suspend fun getAppConfig(): ConfigData {
+        // Switch scope so the app config can't get canceled due to unsubscription,
+        // we'd still like to have that new config in any case.
+        val deferred = scope.async(context = dispatcherProvider.IO) {
+            configHolder.updateBlocking {
+                source.retrieveConfig()
             }
         }
+        return deferred.await()
+    }
+
+    suspend fun clear() {
+        Timber.tag(TAG).v("clear()")
+        source.clear()
     }
 
     companion object {
