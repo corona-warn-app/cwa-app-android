31f7f76fb168dadc89beb9ea6c4d8bbb5c13eaf0
ralfgehrer <mail@ralfgehrer.com>
2020-11-10 11:51:52 +0100

    add more logs

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/notification/NotificationReceiver.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/notification/NotificationReceiver.kt
@@ -18,10 +18,12 @@ class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         AndroidInjection.inject(this, context)
         when (val notificationId = intent.getIntExtra(NOTIFICATION_ID, Int.MIN_VALUE)) {
-            POSITIVE_RESULT_NOTIFICATION_ID ->
+            POSITIVE_RESULT_NOTIFICATION_ID -> {
+                Timber.tag(TAG).v("NotificationReceiver received intent to show a positive test result notification")
                 testResultNotificationService.showPositiveTestResultNotification(notificationId)
+            }
             else ->
-                Timber.tag(TAG).v("NotificationReceiver received an undefined notificationId: %s", notificationId)
+                Timber.tag(TAG).d("NotificationReceiver received an undefined notificationId: %s", notificationId)
         }
     }
 
