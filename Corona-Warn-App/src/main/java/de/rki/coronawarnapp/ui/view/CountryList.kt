dccb06e4b4e3590e9f735459d1f6e1df91a693fa
Rituraj Sambherao <git.rituraj.sambherao@gmail.com>
2020-11-17 00:01:36 +0000

    Country Names list implemented

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/view/CountryList.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/view/CountryList.kt
@@ -7,13 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.core.content.withStyledAttributes
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.ui.Country
-import kotlinx.android.synthetic.main.view_country_list_entry_flag.view.*
+import kotlinx.android.synthetic.main.view_country_list_entry_flag_container.view.*
 import java.text.Collator
 
 class CountryList(context: Context, attrs: AttributeSet) :
@@ -44,21 +43,30 @@ class CountryList(context: Context, attrs: AttributeSet) :
                 Collator.getInstance().compare(a.first, b.first)
             }
 
-        view = inflate(context, R.layout.view_country_list_entry_flag, this)
+        view = inflate(context, R.layout.view_country_list_entry_flag_container, this)
         val flagNumberOfColumns = 8
-        val namesNumberOfColumns = 6
         var adapterCountryFlags = CountryAdapterFlags(context, _list)
-//        var adapterCountryNames = CountryAdapterNames(context, _list)
         context.withStyledAttributes(attributeSet, R.styleable.SimpleStepEntry) {
+
             flagGrid.layoutManager = GridLayoutManager(context, flagNumberOfColumns)
             flagGrid.adapter = adapterCountryFlags
-//
-//            countryNamesGrid.layoutManager = GridLayoutManager(context, namesNumberOfColumns)
-//            countryNamesGrid.adapter = adapterCountryNames
+
+            var countryNames = ""
+            val iterator: Iterator<Country> = list!!.iterator()
+            while (iterator.hasNext()) {
+                countryNames += resources.getString(iterator.next().labelRes)
+                if (iterator.hasNext()) {
+                    countryNames += ", "
+                }
+            }
+            country_list_entry_label.text = countryNames
         }
     }
 }
 
+/**
+ * Adapter for the country flags grid
+ */
 class CountryAdapterFlags internal constructor(
     context: Context?,
     countryList: List<Country>?
@@ -87,36 +95,5 @@ class CountryAdapterFlags internal constructor(
     // stores and recycles views as they are scrolled off screen
     inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var flagImage: ImageView = itemView.findViewById(R.id.country_list_entry_image)
-    }
-}
-
-
-class CountryAdapterNames internal constructor(
-    context: Context?,
-    countryList: List<Country>?
-) :
-    RecyclerView.Adapter<CountryAdapterNames.ViewHolder>() {
-    private val mInflater: LayoutInflater = LayoutInflater.from(context)
-    private val countryList: List<Country>? = countryList
-
-    // inflates the cell layout from xml when needed
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view: View = mInflater.inflate(R.layout.view_country_list_entry_flag_item_name, parent, false)
-        return ViewHolder(view)
-    }
-
-    // total number of cells
-    override fun getItemCount(): Int {
-        return countryList!!.size
-    }
-
-    // stores and recycles views as they are scrolled off screen
-    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var flagName: TextView = itemView.findViewById(R.id.country_list_entry_label)
-    }
-
-    override fun onBindViewHolder(holder: CountryAdapterNames.ViewHolder, position: Int) {
-        val country = this.countryList!![position]
-        holder.flagName.setText(country.labelRes)
     }
 }
