feeabf0d42894ccf7e96e90ba2bc6ec68d11417d
BMItter <berndus@gmx.de>
2020-11-18 23:04:50 +0100

    fix multiple instances for ExposureResultStore

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/ExposureResultStore.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/ExposureResultStore.kt
@@ -4,7 +4,9 @@ import com.google.android.gms.nearby.exposurenotification.ExposureWindow
 import de.rki.coronawarnapp.risk.result.AggregatedRiskResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ExposureResultStore @Inject constructor() {
 
     val entities = MutableStateFlow<ExposureResult>(Pair(emptyList(), null))
