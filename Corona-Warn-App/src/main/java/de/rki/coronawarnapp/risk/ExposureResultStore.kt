92a675dcde6b1bb6f323bab358cdfc5b88206b6f
chris-cwa <chris.cwa.sap@gmail.com>
2020-11-18 21:23:09 +0100

    flow for windows

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/ExposureResultStore.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/ExposureResultStore.kt
@@ -11,12 +11,16 @@ class ExposureResultStore @Inject constructor() {
     private val internalResult = MutableStateFlow<AggregatedRiskResult?>(null)
     val result: Flow<AggregatedRiskResult?> = internalResult
 
+    private val internalWindows = MutableStateFlow<List<ExposureWindow>>(emptyList())
+    val windows: Flow<List<ExposureWindow>> = internalWindows
+
     private var entities: Pair<List<ExposureWindow>, AggregatedRiskResult?> = Pair(emptyList(), null)
 
     var exposureWindowEntities: Pair<List<ExposureWindow>, AggregatedRiskResult?>
         get() = entities
         set(value) {
             entities = value
+            internalWindows.value = value.first
             internalResult.value = value.second
         }
 }
