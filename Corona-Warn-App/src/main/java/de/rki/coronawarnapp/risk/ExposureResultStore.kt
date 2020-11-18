ea62e49f92d64c4c7b848a1b1dab1ae0cfc4fe83
chris-cwa <chris.cwa.sap@gmail.com>
2020-11-18 21:11:43 +0100

    flow for aggregated result

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/ExposureResultStore.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/ExposureResultStore.kt
@@ -2,9 +2,14 @@ package de.rki.coronawarnapp.risk
 
 import com.google.android.gms.nearby.exposurenotification.ExposureWindow
 import de.rki.coronawarnapp.risk.result.AggregatedRiskResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 class ExposureResultStore @Inject constructor() {
+
+    private val internalResult = MutableStateFlow<AggregatedRiskResult?>(null)
+    val result: Flow<AggregatedRiskResult?> = internalResult
 
     private var entities: Pair<List<ExposureWindow>, AggregatedRiskResult?> = Pair(emptyList(), null)
 
@@ -12,5 +17,6 @@ class ExposureResultStore @Inject constructor() {
         get() = entities
         set(value) {
             entities = value
+            internalResult.value = value.second
         }
 }
