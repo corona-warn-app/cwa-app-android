8bb3fc64bf9a26bdb56f9dfa6cf6134aa56d02b4
Kolya Opahle <k.opahle@sap.com>
2020-11-23 14:07:46 +0100

    Added more unit testing for DefaultDiagnosisKeysDataMapper and fixed TestForAPIFragment
    Signed-off-by: Kolya Opahle <k.opahle@sap.com>

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/api/ui/TestForAPIFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/api/ui/TestForAPIFragment.kt
@@ -26,6 +26,7 @@ import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import com.google.zxing.qrcode.QRCodeWriter
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.databinding.FragmentTestForAPIBinding
 import de.rki.coronawarnapp.exception.ExceptionCategory
 import de.rki.coronawarnapp.exception.ExceptionCategory.INTERNAL
@@ -66,6 +67,9 @@ class TestForAPIFragment : Fragment(R.layout.fragment_test_for_a_p_i),
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
     @Inject lateinit var enfClient: ENFClient
     @Inject lateinit var exposureResultStore: ExposureResultStore
+
+    // TODO: This is ugly, remove when refactoring the fragment
+    @Inject lateinit var appConfigProvider: AppConfigProvider
     private val vm: TestForApiFragmentViewModel by cwaViewModels { viewModelFactory }
 
     companion object {
@@ -290,9 +294,10 @@ class TestForAPIFragment : Fragment(R.layout.fragment_test_for_a_p_i),
                 Timber.i("Provide ${googleFileList.count()} files with ${appleKeyList.size} keys")
                 try {
                     // only testing implementation: this is used to wait for the broadcastreceiver of the OS / EN API
-                    // enfClient.provideDiagnosisKeys(
-                    //     googleFileList,
-                    // )
+                    enfClient.provideDiagnosisKeys(
+                        googleFileList,
+                        appConfigProvider.getAppConfig().diagnosisKeysDataMapping
+                    )
                     showToast("Provided ${appleKeyList.size} keys to Google API")
                 } catch (e: Exception) {
                     e.report(ExceptionCategory.EXPOSURENOTIFICATION)
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/nearby/modules/diagnosiskeysdatamapper/DefaultDiagnosisKeysDataMapperTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/nearby/modules/diagnosiskeysdatamapper/DefaultDiagnosisKeysDataMapperTest.kt
@@ -1,14 +1,95 @@
 package de.rki.coronawarnapp.nearby.modules.diagnosiskeysdatamapper
 
 import com.google.android.gms.nearby.exposurenotification.DiagnosisKeysDataMapping
+import com.google.android.gms.nearby.exposurenotification.ExposureNotificationClient
 import com.google.android.gms.nearby.exposurenotification.Infectiousness
 import com.google.android.gms.nearby.exposurenotification.ReportType
 import de.rki.coronawarnapp.nearby.modules.diagnosiskeysdatamapper.DefaultDiagnosisKeysDataMapper.Companion.hasChanged
 import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
+import testhelpers.coroutines.runBlockingTest2
+import testhelpers.gms.MockGMSTask
 
 class DefaultDiagnosisKeysDataMapperTest : BaseTest() {
+    @MockK lateinit var googleENFClient: ExposureNotificationClient
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @AfterEach
+    fun teardown() {
+        clearAllMocks()
+    }
+
+    private fun createMapper() = DefaultDiagnosisKeysDataMapper(
+        client = googleENFClient
+    )
+
+    @Test
+    fun `set mapping is invoked`() {
+        val firstMapping = DiagnosisKeysDataMapping.DiagnosisKeysDataMappingBuilder().apply {
+            setReportTypeWhenMissing(ReportType.CONFIRMED_TEST)
+            setInfectiousnessWhenDaysSinceOnsetMissing(Infectiousness.HIGH)
+            setDaysSinceOnsetToInfectiousness(mapOf(0 to Infectiousness.STANDARD, 1 to Infectiousness.HIGH))
+        }.build()
+
+        val secondMapping = DiagnosisKeysDataMapping.DiagnosisKeysDataMappingBuilder().apply {
+            setReportTypeWhenMissing(ReportType.CONFIRMED_TEST)
+            setInfectiousnessWhenDaysSinceOnsetMissing(Infectiousness.HIGH)
+            setDaysSinceOnsetToInfectiousness(mapOf(0 to Infectiousness.HIGH, 1 to Infectiousness.STANDARD))
+        }.build()
+
+        coEvery { googleENFClient.diagnosisKeysDataMapping } returns MockGMSTask.forValue(firstMapping)
+        coEvery { googleENFClient.setDiagnosisKeysDataMapping(any()) } returns MockGMSTask.forValue(null)
+
+        val mapper = createMapper()
+
+        runBlockingTest2 {
+            mapper.updateDiagnosisKeysDataMapping(secondMapping)
+        }
+
+        verify {
+            googleENFClient.setDiagnosisKeysDataMapping(secondMapping)
+        }
+    }
+
+    @Test
+    fun `set mapping is not invoked`() {
+        val firstMapping = DiagnosisKeysDataMapping.DiagnosisKeysDataMappingBuilder().apply {
+            setReportTypeWhenMissing(ReportType.CONFIRMED_TEST)
+            setInfectiousnessWhenDaysSinceOnsetMissing(Infectiousness.HIGH)
+            setDaysSinceOnsetToInfectiousness(mapOf(0 to Infectiousness.STANDARD, 1 to Infectiousness.HIGH))
+        }.build()
+
+        val secondMapping = DiagnosisKeysDataMapping.DiagnosisKeysDataMappingBuilder().apply {
+            setReportTypeWhenMissing(ReportType.CONFIRMED_TEST)
+            setInfectiousnessWhenDaysSinceOnsetMissing(Infectiousness.HIGH)
+            setDaysSinceOnsetToInfectiousness(mapOf(0 to Infectiousness.STANDARD, 1 to Infectiousness.HIGH))
+        }.build()
+
+        coEvery { googleENFClient.diagnosisKeysDataMapping } returns MockGMSTask.forValue(firstMapping)
+        coEvery { googleENFClient.setDiagnosisKeysDataMapping(any()) } returns MockGMSTask.forValue(null)
+
+        val mapper = createMapper()
+
+        runBlockingTest2 {
+            mapper.updateDiagnosisKeysDataMapping(secondMapping)
+        }
+
+        verify(exactly = 0) {
+            googleENFClient.setDiagnosisKeysDataMapping(secondMapping)
+        }
+    }
 
     @Test
     fun `mapping change detection works`() {
