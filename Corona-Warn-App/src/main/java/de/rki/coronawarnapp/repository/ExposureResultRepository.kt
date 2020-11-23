bfce18df8a05e9a29ea165e373fa7ba53087fe19
chris-cwa <chris.cwa.sap@gmail.com>
2020-11-23 13:44:29 +0100

    + convenience

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/repository/ExposureResultRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/repository/ExposureResultRepository.kt
@@ -1,33 +1,16 @@
 package de.rki.coronawarnapp.repository
 
-import com.google.gson.Gson
 import de.rki.coronawarnapp.fileio.CWAFileIO
 import de.rki.coronawarnapp.risk.ExposureResult
-import de.rki.coronawarnapp.util.serialization.fromJson
-import java.io.FileWriter
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ExposureResultRepository @Inject constructor(fileio: CWAFileIO) {
+class ExposureResultRepository @Inject constructor(fileio: CWAFileIO) : FileRepository() {
 
     private val file = fileio.exposureResultFile
 
-    fun load(): ExposureResult? {
-        if (!file.exists()) return null
+    fun load(): ExposureResult? = loadOrNull(file)
 
-        return Gson().fromJson(file)
-    }
-
-    fun upsert(exposureResult: ExposureResult?) {
-        if (exposureResult == null) {
-            if (file.exists()) {
-                file.delete()
-            }
-        } else {
-            FileWriter(file).use {
-                Gson().toJson(exposureResult, it)
-            }
-        }
-    }
+    fun upsert(exposureResult: ExposureResult?) = writeOrDelete(exposureResult, file)
 }
---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/repository/FileRepository.kt
@@ -0,0 +1,27 @@
+package de.rki.coronawarnapp.repository
+
+import com.google.gson.Gson
+import de.rki.coronawarnapp.util.serialization.fromJson
+import java.io.File
+import java.io.FileWriter
+
+abstract class FileRepository {
+
+    inline fun <reified T> loadOrNull(file: File): T? {
+        if (!file.exists()) return null
+
+        return Gson().fromJson(file)
+    }
+
+    fun <T> writeOrDelete(t: T?, file: File) {
+        if (t == null) {
+            if (file.exists()) {
+                file.delete()
+            }
+        } else {
+            FileWriter(file).use {
+                Gson().toJson(t, it)
+            }
+        }
+    }
+}
