6dc0ab61264e1f83b2ffc73fdfd8206be25aaab8
BMItter <46747780+BMItter@users.noreply.github.com>
2020-11-27 18:56:41 +0100

    Fix - Exposure windows keep adding (EXPOSUREAPP-4020) (#1741)
    * Use lastRiskResult from DB
    
    * use correct format for windows and timestamp
    
    * klint
    
    * Reduce RiskLevelResult Storage API and fill exposure windows in risk result if available.
    
    * Default exposure windows to null while mapping
    
    * Don't add empty exposure windows list to risk results
    
    * fix broken tests
    
    * Added tests for exposure windows
    
    Co-authored-by: Matthias Urhahn <matthias.urhahn@sap.com>

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/risk/storage/internal/PersistedRiskResultDaoTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/risk/storage/internal/PersistedRiskResultDaoTest.kt
@@ -2,6 +2,8 @@ package de.rki.coronawarnapp.risk.storage.internal
 
 import de.rki.coronawarnapp.risk.RiskLevel
 import de.rki.coronawarnapp.risk.RiskLevelResult
+import de.rki.coronawarnapp.risk.storage.RiskStorageTestData.testExposureWindow
+import de.rki.coronawarnapp.risk.storage.RiskStorageTestData.testExposureWindowDaoWrapper
 import de.rki.coronawarnapp.risk.storage.internal.riskresults.PersistedRiskLevelResultDao
 import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass
 import io.kotest.matchers.shouldBe
@@ -11,8 +13,44 @@ import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
 
 class PersistedRiskResultDaoTest : BaseTest() {
+
     @Test
-    fun `mapping succesful result`() {
+    fun `mapping successful result`() {
+        PersistedRiskLevelResultDao(
+            id = "",
+            calculatedAt = Instant.ofEpochMilli(931161601L),
+            failureReason = null,
+            aggregatedRiskResult = PersistedRiskLevelResultDao.PersistedAggregatedRiskResult(
+                totalRiskLevel = RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.LOW,
+                totalMinimumDistinctEncountersWithLowRisk = 89,
+                totalMinimumDistinctEncountersWithHighRisk = 59,
+                mostRecentDateWithLowRisk = Instant.ofEpochMilli(852191241L),
+                mostRecentDateWithHighRisk = Instant.ofEpochMilli(790335113L),
+                numberOfDaysWithLowRisk = 52,
+                numberOfDaysWithHighRisk = 81
+            )
+        ).toRiskResult(listOf(testExposureWindowDaoWrapper)).apply {
+            riskLevel shouldBe RiskLevel.LOW_LEVEL_RISK
+            calculatedAt.millis shouldBe 931161601L
+            exposureWindows shouldBe listOf(testExposureWindow)
+            failureReason shouldBe null
+            aggregatedRiskResult shouldNotBe null
+            aggregatedRiskResult?.apply {
+                totalRiskLevel shouldBe RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.LOW
+                totalMinimumDistinctEncountersWithLowRisk shouldBe 89
+                totalMinimumDistinctEncountersWithHighRisk shouldBe 59
+                mostRecentDateWithLowRisk shouldNotBe null
+                mostRecentDateWithLowRisk?.millis shouldBe 852191241L
+                mostRecentDateWithHighRisk shouldNotBe null
+                mostRecentDateWithHighRisk?.millis shouldBe 790335113L
+                numberOfDaysWithLowRisk shouldBe 52
+                numberOfDaysWithHighRisk shouldBe 81
+            }
+        }
+    }
+
+    @Test
+    fun `mapping successful result with exposure windows`() {
         PersistedRiskLevelResultDao(
             id = "",
             calculatedAt = Instant.ofEpochMilli(931161601L),
