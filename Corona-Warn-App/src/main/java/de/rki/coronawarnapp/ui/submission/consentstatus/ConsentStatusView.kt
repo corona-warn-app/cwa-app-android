d7132461b4721e994923f5ff8a64e3db9301abc7
Matthias Urhahn <matthias.urhahn@sap.com>
2020-11-29 18:50:29 +0100

    Flatten view hierarchy.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/consentstatus/ConsentStatusView.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/consentstatus/ConsentStatusView.kt
@@ -2,9 +2,11 @@ package de.rki.coronawarnapp.ui.submission.consentstatus
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
 import de.rki.coronawarnapp.R
 
 class ConsentStatusView @JvmOverloads constructor(
@@ -16,17 +18,26 @@ class ConsentStatusView @JvmOverloads constructor(
     private var titleTextView: TextView
 
     init {
-        LayoutInflater.from(context)
-            .inflate(R.layout.view_consent_status, this, true)
+        LayoutInflater.from(context).inflate(R.layout.view_consent_status, this, true)
 
-        titleTextView = findViewById(R.id.consent_status_title)
+        val outValue = TypedValue()
+        getContext().theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+        setBackgroundResource(outValue.resourceId)
+
+        titleTextView = findViewById(R.id.consent_status_message)
     }
 
-    fun updateConsent(consent: Boolean) {
-        if (consent) {
-            titleTextView.setText(R.string.submission_consent_view_consent_given)
-        } else {
-            titleTextView.setText(R.string.submission_consent_view_consent_not_given)
+    var consent: Boolean = false
+        set(value) {
+            when (value) {
+                true -> R.string.submission_consent_view_consent_given
+                false -> R.string.submission_consent_view_consent_not_given
+            }.let { titleTextView.setText(it) }
+            field = value
         }
-    }
+}
+
+@BindingAdapter("consent")
+fun ConsentStatusView.consent(newConsent: Boolean) {
+    consent = newConsent
 }
