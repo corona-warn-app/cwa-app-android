ec9d52b308adf07d9a191e6c97c0ce8e0ccc90cf
Alexander Alferov <a.alferov@sap.com>
2020-11-30 18:23:16 +0300

    Unit test fix

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/yourconsent/SubmissionYourConsentViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/yourconsent/SubmissionYourConsentViewModelTest.kt
@@ -6,6 +6,7 @@ import de.rki.coronawarnapp.ui.Country
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
+import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
@@ -13,6 +14,7 @@ import io.mockk.just
 import io.mockk.verify
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -38,6 +40,11 @@ class SubmissionYourConsentViewModelTest {
         viewModel = SubmissionYourConsentViewModel(interoperabilityRepository, submissionRepository)
     }
 
+    @AfterEach
+    fun teardown() {
+        clearAllMocks()
+    }
+
     @Test
     fun testCountryList() {
         viewModel.countryList.observeForever { }
@@ -61,6 +68,7 @@ class SubmissionYourConsentViewModelTest {
     @Test
     fun testConsentGiven() {
         coEvery { submissionRepository.hasGivenConsentToSubmission } returns flowOf(false)
+        viewModel = SubmissionYourConsentViewModel(interoperabilityRepository, submissionRepository)
         viewModel.switchConsent()
         verify(exactly = 1) { submissionRepository.giveConsentToSubmission() }
         viewModel.clickEvent.value shouldBe SubmissionYourConsentEvents.SwitchConsent
