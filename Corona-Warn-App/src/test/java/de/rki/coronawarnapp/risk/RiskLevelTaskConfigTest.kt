371a19b5f96d3e00dbe289767056bcf4d2dd4018
chris-cwa <chris.cwa.sap@gmail.com>
2020-11-30 13:12:03 +0100

    updated tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/risk/RiskLevelTaskConfigTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/risk/RiskLevelTaskConfigTest.kt
@@ -1,10 +1,15 @@
 package de.rki.coronawarnapp.risk
 
 import de.rki.coronawarnapp.nearby.modules.detectiontracker.ExposureDetectionTracker
+import de.rki.coronawarnapp.nearby.modules.detectiontracker.TrackedExposureDetection
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
 import org.joda.time.Duration
+import org.joda.time.Instant
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
@@ -20,7 +25,66 @@ class RiskLevelTaskConfigTest : BaseTest() {
 
     @Test
     fun `risk level task max execution time is not above 9 minutes`() {
-        val config = RiskLevelTask.Config(exposureDetectionTracker)
-        config.executionTimeout.isShorterThan(Duration.standardMinutes(9)) shouldBe true
+        RiskLevelTask.Config(exposureDetectionTracker)
+            .executionTimeout
+            .isShorterThan(Duration.standardMinutes(9)) shouldBe true
+    }
+
+    @Test
+    fun `risk level preconditions are met`() {
+        every { exposureDetectionTracker.calculations } returns MutableStateFlow(mapOf("" to TrackedExposureDetection(
+            identifier = "",
+            startedAt = Instant(),
+            result = TrackedExposureDetection.Result.NO_MATCHES,
+            enfVersion = TrackedExposureDetection.EnfVersion.V2_WINDOW_MODE
+        )))
+        runBlocking {
+            RiskLevelTask.Config(exposureDetectionTracker)
+                .preconditions.fold(true) { result, precondition ->
+                    result && precondition()
+                } shouldBe true
+        }
+    }
+
+    @Test
+    fun `risk level preconditions are not met, because there are no detections`() {
+        every { exposureDetectionTracker.calculations } returns MutableStateFlow(emptyMap())
+            runBlocking {
+            RiskLevelTask.Config(exposureDetectionTracker)
+                .preconditions.fold(true) { result, precondition ->
+                    result && precondition()
+                } shouldBe false
+        }
+    }
+
+    @Test
+    fun `risk level preconditions are not met, because there are no enf V2 detections`() {
+        every { exposureDetectionTracker.calculations } returns MutableStateFlow(mapOf("" to TrackedExposureDetection(
+            identifier = "",
+            startedAt = Instant(),
+            result = TrackedExposureDetection.Result.NO_MATCHES,
+            enfVersion = TrackedExposureDetection.EnfVersion.V1_LEGACY_MODE
+        )))
+        runBlocking {
+            RiskLevelTask.Config(exposureDetectionTracker)
+                .preconditions.fold(true) { result, precondition ->
+                    result && precondition()
+                } shouldBe false
+        }
+    }
+
+    @Test
+    fun `risk level preconditions are not met, because detection is not finished yet`() {
+        every { exposureDetectionTracker.calculations } returns MutableStateFlow(mapOf("" to TrackedExposureDetection(
+            identifier = "",
+            startedAt = Instant(),
+            enfVersion = TrackedExposureDetection.EnfVersion.V2_WINDOW_MODE
+        )))
+        runBlocking {
+            RiskLevelTask.Config(exposureDetectionTracker)
+                .preconditions.fold(true) { result, precondition ->
+                    result && precondition()
+                } shouldBe false
+        }
     }
 }
