14b0751e7cff87fbff432ef838262a8b53a09c17
Kolya Opahle <k.opahle@sap.com>
2020-12-07 15:22:36 +0100

    Added main activity and nav graph for contact diary
    Signed-off-by: Kolya Opahle <k.opahle@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/ui/ContactDiaryActivity.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/ui/ContactDiaryActivity.kt
@@ -1,3 +1,46 @@
 package de.rki.coronawarnapp.contactdiary.ui
 
-class ContactDiaryActivity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.util.di.AppInjector
+import javax.inject.Inject
+
+/**
+ * This activity holds all the contact diary fragments
+ */
+class ContactDiaryActivity : AppCompatActivity(), HasAndroidInjector {
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, ContactDiaryActivity::class.java))
+        }
+    }
+
+    @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
+
+    private val FragmentManager.currentNavigationFragment: Fragment?
+        get() = primaryNavigationFragment?.childFragmentManager?.fragments?.first()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AppInjector.setup(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contact_diary_activity)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        supportFragmentManager.currentNavigationFragment?.onActivityResult(
+            requestCode,
+            resultCode,
+            data
+        )
+    }
+}
