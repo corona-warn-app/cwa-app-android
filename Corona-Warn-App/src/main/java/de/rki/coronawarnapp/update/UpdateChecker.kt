9135bbe968b435af2709876344d25ebe23a0f0ba
Matthias Urhahn <matthias.urhahn@sap.com>
2020-12-10 11:22:33 +0100

    Don't execute the update checker (and it updating the config) on the main thread.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/update/UpdateChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/update/UpdateChecker.kt
@@ -2,85 +2,69 @@ package de.rki.coronawarnapp.update
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat.startActivity
+import dagger.Reusable
 import de.rki.coronawarnapp.BuildConfig
-import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.appconfig.CWAConfig
 import de.rki.coronawarnapp.appconfig.internal.ApplicationConfigurationCorruptException
-import de.rki.coronawarnapp.ui.LauncherActivity
-import de.rki.coronawarnapp.util.di.AppInjector
 import timber.log.Timber
+import javax.inject.Inject
 
-class UpdateChecker(private val activity: LauncherActivity) {
+@Reusable
+class UpdateChecker @Inject constructor(
+    private val appConfigProvider: AppConfigProvider
+) {
 
-    companion object {
-        val TAG: String? = UpdateChecker::class.simpleName
+    suspend fun checkForUpdate(): Result = try {
+        val isUpdateNeeded = checkIfUpdatesNeededFromServer()
+        Result(isUpdateNeeded = isUpdateNeeded, updateIntent = createUpdateIntent())
+    } catch (exception: ApplicationConfigurationCorruptException) {
+        Timber.e(
+            "ApplicationConfigurationCorruptException caught:%s",
+            exception.localizedMessage
+        )
 
-        const val STORE_PREFIX = "https://play.google.com/store/apps/details?id="
-        const val COM_ANDROID_VENDING = "com.android.vending"
-    }
-
-    suspend fun checkForUpdate() {
-        // check if an update is needed based on server config
-        val updateNeededFromServer: Boolean = try {
-            checkIfUpdatesNeededFromServer()
-        } catch (exception: ApplicationConfigurationCorruptException) {
-            Timber.e(
-                "ApplicationConfigurationCorruptException caught:%s",
-                exception.localizedMessage
-            )
-            true
-        } catch (exception: Exception) {
-            Timber.e("Exception caught:%s", exception.localizedMessage)
-            false
-        }
-
-        if (updateNeededFromServer) {
-            showUpdateNeededDialog()
-        } else {
-            activity.navigateToActivities()
-        }
-    }
-
-    /**
-     * Show dialog there an update is needed and links to the play store
-     */
-    private fun showUpdateNeededDialog() {
-        AlertDialog.Builder(activity)
-            .setTitle(activity.getString(R.string.update_dialog_title))
-            .setMessage(activity.getString(R.string.update_dialog_message))
-            .setCancelable(false)
-            .setPositiveButton(activity.getString(R.string.update_dialog_button)) { _, _ ->
-
-                val uriStringInPlayStore = STORE_PREFIX + BuildConfig.APPLICATION_ID
-                val intent = Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse(
-                        uriStringInPlayStore
-                    )
-                    setPackage(COM_ANDROID_VENDING)
-                }
-                startActivity(activity, intent, null)
-            }
-            .create().show()
+        Result(isUpdateNeeded = true, updateIntent = createUpdateIntent())
+    } catch (exception: Exception) {
+        Timber.tag(TAG).e("Exception caught:%s", exception.localizedMessage)
+        Result(isUpdateNeeded = false, updateIntent = null)
     }
 
     private suspend fun checkIfUpdatesNeededFromServer(): Boolean {
-        val cwaAppConfig: CWAConfig = AppInjector.component.appConfigProvider.getAppConfig()
+        val cwaAppConfig: CWAConfig = appConfigProvider.getAppConfig()
 
         val minVersionFromServer = cwaAppConfig.minVersionCode
 
-        Timber.d(
-            "minVersionFromServer:%s",
-            minVersionFromServer
-        )
-        Timber.d("Current app version:%s", BuildConfig.VERSION_CODE)
+        Timber.tag(TAG).d("minVersionFromServer:%s", minVersionFromServer)
+        Timber.tag(TAG).d("Current app version:%s", BuildConfig.VERSION_CODE)
 
         val needsImmediateUpdate = VersionComparator.isVersionOlder(
             BuildConfig.VERSION_CODE.toLong(),
             minVersionFromServer
         )
-        Timber.e("needs update:$needsImmediateUpdate")
+        Timber.tag(TAG).e("needs update:$needsImmediateUpdate")
         return needsImmediateUpdate
+    }
+
+    private fun createUpdateIntent(): Intent {
+        val uriStringInPlayStore = STORE_PREFIX + BuildConfig.APPLICATION_ID
+        return Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(
+                uriStringInPlayStore
+            )
+            setPackage(COM_ANDROID_VENDING)
+        }
+    }
+
+    data class Result(
+        val isUpdateNeeded: Boolean,
+        val updateIntent: Intent?
+    )
+
+    companion object {
+        private const val TAG: String = "UpdateChecker"
+
+        private const val STORE_PREFIX = "https://play.google.com/store/apps/details?id="
+        private const val COM_ANDROID_VENDING = "com.android.vending"
     }
 }
