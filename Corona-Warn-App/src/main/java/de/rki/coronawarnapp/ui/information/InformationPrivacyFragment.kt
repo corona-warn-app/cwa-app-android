be05b86949a5199adb852f82067a6324e85243e9
I502720 <axel.herbstreith@sap.com>
2020-12-14 17:18:07 +0100

    Aligned back navigation

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/information/InformationPrivacyFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/information/InformationPrivacyFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentInformationPrivacyBinding
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
@@ -26,7 +27,7 @@ class InformationPrivacyFragment : Fragment(R.layout.fragment_information_privac
 
     private fun setButtonOnClickListener() {
         binding.informationPrivacyHeader.headerButtonBack.buttonIcon.setOnClickListener {
-            activity?.onBackPressed()
+            findNavController().popBackStack()
         }
     }
 }
