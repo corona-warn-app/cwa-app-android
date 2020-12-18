a87b44dbb02c0fd8d3fd7508b13ad7f07e551525
BMItter <46747780+BMItter@users.noreply.github.com>
2020-12-18 16:10:13 +0100

    Code smells diary (EXPOSUREAPP-4152) (#1943)
    * move companions object to the end of ContactDiaryActivity
    
    * format clean
    
    * Init contact diary database lazily in repo, Added fun clear to contact diary repo, Use repo clear in data reset
    
    * Use unified string for date
    
    * Corrected date format
    
    * clean

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/ContactDiaryExtensions.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/ContactDiaryExtensions.kt
@@ -5,6 +5,8 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import org.joda.time.LocalDate
+import java.util.Locale
 
 fun ViewPager2.registerOnPageChangeCallback(cb: (position: Int) -> Unit) {
     this.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
@@ -13,6 +15,9 @@ fun ViewPager2.registerOnPageChangeCallback(cb: (position: Int) -> Unit) {
         }
     })
 }
+
+// According to tech spec german locale only
+fun LocalDate.toFormattedDay(): String = toString("EEEE, dd.MM.yy", Locale.GERMAN)
 
 fun EditText.showKeyboard() = post {
         if (requestFocus()) context.inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
