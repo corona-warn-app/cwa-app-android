fa594d38f5a99e21e5b87fa1d9a25ad57f223536
BMItter <46747780+BMItter@users.noreply.github.com>
2020-12-22 13:45:26 +0100

    Fix for not showing keyboard on all devices on edittext focus (EXPOSUREAPP-4152) (#1972)
    * fix for not showing keyboard on all devices on edittext focus
    
    * show keyboard and focus on all devices

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/ContactDiaryExtensions.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/ContactDiaryExtensions.kt
@@ -1,9 +1,9 @@
 package de.rki.coronawarnapp.contactdiary.util
 
 import android.content.Context
+import android.view.View
+import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import org.joda.time.LocalDate
 import java.util.Locale
@@ -19,18 +19,6 @@ fun ViewPager2.registerOnPageChangeCallback(cb: (position: Int) -> Unit) {
 // According to tech spec german locale only
 fun LocalDate.toFormattedDay(): String = toString("EEEE, dd.MM.yy", Locale.GERMAN)
 
-fun EditText.showKeyboard() = post {
-        if (requestFocus()) context.inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-    }
-
-fun Fragment.hideKeyboard() {
-    context?.inputMethodManager?.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, 0)
-    view?.clearFocus()
-}
-
-private val Context.inputMethodManager
-    get() = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
 fun String.formatContactDiaryNameField(maxLength: Int): String {
     val newName = if (isNotBlank()) {
         trim()
@@ -39,4 +27,33 @@ fun String.formatContactDiaryNameField(maxLength: Int): String {
         this
     }
     return newName.take(maxLength)
+}
+
+fun View.focusAndShowKeyboard() {
+    /**
+     * This is to be called when the window already has focus.
+     */
+    fun View.showTheKeyboardNow() {
+        if (isFocused) {
+            post {
+                val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+    }
+
+    requestFocus()
+    if (hasWindowFocus()) {
+        showTheKeyboardNow()
+    } else {
+        viewTreeObserver.addOnWindowFocusChangeListener(
+            object : ViewTreeObserver.OnWindowFocusChangeListener {
+                override fun onWindowFocusChanged(hasFocus: Boolean) {
+                    if (hasFocus) {
+                        this@focusAndShowKeyboard.showTheKeyboardNow()
+                        viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                    }
+                }
+            })
+    }
 }
