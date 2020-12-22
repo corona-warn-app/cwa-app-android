5ac9e2c664c221e51a1e38a3e01a8cb8003e38f8
Matthias Urhahn <matthias.urhahn@sap.com>
2020-12-22 13:30:50 +0100

    Add test menu option to disable device time check via affecting the config mapping.

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/appconfig/ui/AppConfigTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/appconfig/ui/AppConfigTestFragment.kt
@@ -43,6 +43,13 @@ class AppConfigTestFragment : Fragment(R.layout.fragment_test_appconfig), AutoIn
 
         binding.downloadAction.setOnClickListener { vm.download() }
         binding.deleteAction.setOnClickListener { vm.clearConfig() }
+
+        vm.deviceTimeCheckDisabled.observe2(this) {
+            binding.deviceTimeCheckDisabledToggle.isChecked = it
+        }
+        binding.deviceTimeCheckDisabledToggle.setOnClickListener {
+            vm.toggleDeviceTimeCheckDisabled()
+        }
     }
 
     companion object {
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/mapping/CWAConfigMapper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/mapping/CWAConfigMapper.kt
@@ -3,17 +3,22 @@ package de.rki.coronawarnapp.appconfig.mapping
 import dagger.Reusable
 import de.rki.coronawarnapp.appconfig.CWAConfig
 import de.rki.coronawarnapp.server.protocols.internal.v2.AppConfigAndroid.ApplicationConfigurationAndroid
+import de.rki.coronawarnapp.storage.TestSettings
+import de.rki.coronawarnapp.util.CWADebug
 import timber.log.Timber
 import javax.inject.Inject
 
 @Reusable
-class CWAConfigMapper @Inject constructor() : CWAConfig.Mapper {
+class CWAConfigMapper @Inject constructor(
+    private val testSettings: TestSettings
+) : CWAConfig.Mapper {
+
     override fun map(rawConfig: ApplicationConfigurationAndroid): CWAConfig {
         return CWAConfigContainer(
             latestVersionCode = rawConfig.latestVersionCode,
             minVersionCode = rawConfig.minVersionCode,
             supportedCountries = rawConfig.getMappedSupportedCountries(),
-            isDeviceTimeCheckEnabled = !rawConfig.isDeviceTimeCheckDisabled(),
+            isDeviceTimeCheckEnabled = !rawConfig.isDeviceTimeCheckDisabled()
         )
     }
 
@@ -28,6 +33,10 @@ class CWAConfigMapper @Inject constructor() : CWAConfig.Mapper {
         }
 
     private fun ApplicationConfigurationAndroid.isDeviceTimeCheckDisabled(): Boolean {
+        if (CWADebug.isDeviceForTestersBuild && testSettings.isDeviceTimeCheckDisabled.value) {
+            Timber.w("Device time check is disabled via test settings!")
+            return true
+        }
         if (!hasAppFeatures()) return false
 
         return try {
