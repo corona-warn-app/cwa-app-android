73a027af504dc267257514d54764a597d081c0c3
Mohamed Metwalli <mohamed.metwalli@sap.com>
2020-12-24 11:24:19 +0100

    Rename c to context

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/backgroundpriority/BackgroundPriorityState.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/backgroundpriority/BackgroundPriorityState.kt
@@ -8,17 +8,17 @@ import de.rki.coronawarnapp.R
 data class BackgroundPriorityState(
     val isBackgroundPriorityEnabled: Boolean
 ) {
-    fun getButtonStateLabel(c: Context): String = c.getString(
+    fun getButtonStateLabel(context: Context): String = context.getString(
         if (isBackgroundPriorityEnabled) R.string.settings_on
         else R.string.settings_off
     )
 
-    fun getHeaderIllustration(c: Context): Drawable? {
+    fun getHeaderIllustration(context: Context): Drawable? {
         val illustrationId = if (isBackgroundPriorityEnabled) {
             R.drawable.ic_settings_illustration_background_priority_enabled
         } else {
             R.drawable.ic_settings_illustration_background_priority_disabled
         }
-        return ContextCompat.getDrawable(c, illustrationId)
+        return ContextCompat.getDrawable(context, illustrationId)
     }
 }
