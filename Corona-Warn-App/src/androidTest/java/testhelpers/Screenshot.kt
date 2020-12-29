a3b9c976e4809933786e06dc182c62485cdf5f9c
Mohamed Metwalli <mohamed.metwalli@sap.com>
2020-12-29 20:14:27 +0100

    Define custom annotation for tests

---
+++ Corona-Warn-App/src/androidTest/java/testhelpers/Screenshot.kt
@@ -0,0 +1,8 @@
+package testhelpers
+
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.CLASS
+)
+annotation class Screenshot
