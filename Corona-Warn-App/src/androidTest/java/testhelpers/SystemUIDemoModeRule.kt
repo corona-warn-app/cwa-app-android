5d2a48be266cdf21418a0166acc3e478da5fa4ee
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-01-05 09:18:16 +0100

    Correct docs

--- Corona-Warn-App/src/androidTest/java/testhelpers/SystemUIDemoModeRule.kt
+++ Corona-Warn-App/src/androidTest/java/testhelpers/SystemUIDemoModeRule.kt
@@ -6,7 +6,7 @@ import org.junit.runners.model.Statement
 
 /**
  * Enters UI demo mode (clean up device status bar)
- * before running screenshot tests and exists after it afterwards
+ * before running screenshot tests and exists it afterwards
  */
 class SystemUIDemoModeRule : TestRule {
 
