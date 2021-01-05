1b066ff218342cfad132782a96f5f37f27fbbff0
Fynn Godau <fynngodau@mailbox.org>
2021-01-05 18:24:52 +0100

    Add compat extension to Context

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ContextExtensions.kt
@@ -0,0 +1,11 @@
+package de.rki.coronawarnapp.util
+
+import android.content.Context
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+
+object ContextExtensions {
+
+    fun Context.getColorCompat(@ColorRes id: Int) = ContextCompat.getColor(this, id)
+
+}
