45a8ab5e9760b8f7309b2ee2c5a3f981b88abdd8
BMItter <berndus@gmx.de>
2021-01-07 16:23:44 +0100

    merge resolve

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/ui/edit/adapter/PersonEditAdapter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/ui/edit/adapter/PersonEditAdapter.kt
@@ -4,12 +4,16 @@ import android.view.ViewGroup
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.contactdiary.model.ContactDiaryPerson
 import de.rki.coronawarnapp.contactdiary.util.AbstractAdapter
+import de.rki.coronawarnapp.contactdiary.util.setClickLabel
 import de.rki.coronawarnapp.databinding.ContactDiaryEditListItemBinding
 import de.rki.coronawarnapp.ui.lists.BaseAdapter
 import de.rki.coronawarnapp.util.lists.BindableVH
 
-internal class PersonEditAdapter(private val onItemClicked: (item: ContactDiaryPerson) -> Unit) :
-    AbstractAdapter<ContactDiaryPerson, PersonEditAdapter.ViewHolder>() {
+internal class PersonEditAdapter(
+    private val clickLabelString: String,
+    private val getContentDescriptionString: (ContactDiaryPerson) -> String,
+    private val onItemClicked: (item: ContactDiaryPerson) -> Unit
+) : AbstractAdapter<ContactDiaryPerson, PersonEditAdapter.ViewHolder>() {
 
     override fun onCreateBaseVH(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(parent)
 
@@ -18,15 +22,17 @@ internal class PersonEditAdapter(private val onItemClicked: (item: ContactDiaryP
 
     inner class ViewHolder(parent: ViewGroup) : BaseAdapter.VH(R.layout.contact_diary_edit_list_item, parent),
         BindableVH<ContactDiaryPerson, ContactDiaryEditListItemBinding> {
-        override val viewBinding:
-            Lazy<ContactDiaryEditListItemBinding> =
+        override val viewBinding: Lazy<ContactDiaryEditListItemBinding> =
             lazy { ContactDiaryEditListItemBinding.bind(itemView) }
 
-        override val onBindData:
-            ContactDiaryEditListItemBinding.(item: ContactDiaryPerson, payloads: List<Any>) -> Unit =
-            { key, _ ->
-                name.text = key.fullName
-                itemContainer.setOnClickListener { onItemClicked(key) }
+        override val onBindData: ContactDiaryEditListItemBinding.(item: ContactDiaryPerson, payloads: List<Any>) -> Unit =
+            { person, _ ->
+                name.text = person.fullName
+                itemContainer.apply {
+                    setOnClickListener { onItemClicked(person) }
+                    contentDescription = getContentDescriptionString(person)
+                    setClickLabel(clickLabelString)
+                }
             }
     }
 }
