affa324d4efa9329f63e52fd4e123321c800a8f7
Matthias Urhahn <matthias.urhahn@sap.com>
2021-01-12 21:22:28 +0100

    Task for running screenshots only.

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/main/home/HomeFragmentTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/main/home/HomeFragmentTest.kt
@@ -1,14 +1,8 @@
 package de.rki.coronawarnapp.ui.main.home
 
-import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import io.mockk.MockKAnnotations
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.just
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -18,25 +12,25 @@ import testhelpers.BaseUITest
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest : BaseUITest() {
 
-    @MockK lateinit var viewModel: HomeFragmentViewModel
+//    @MockK lateinit var viewModel: HomeFragmentViewModel
 
     @Before
     fun setup() {
-        MockKAnnotations.init(this, relaxed = true)
-
-        every { viewModel.tracingHeaderState } returns MutableLiveData()
-        every { viewModel.homeItems } returns MutableLiveData(emptyList())
-        every { viewModel.refreshRequiredData() } just Runs
-
-        setupMockViewModel(object : HomeFragmentViewModel.Factory {
-            // override fun create(handle: SavedStateHandle){} HomeFragmentViewModel = viewModel}
-            override fun create(): HomeFragmentViewModel = viewModel
-        })
+//        MockKAnnotations.init(this, relaxed = true)
+//
+//        every { viewModel.tracingHeaderState } returns MutableLiveData()
+//        every { viewModel.homeItems } returns MutableLiveData(emptyList())
+//        every { viewModel.refreshRequiredData() } just Runs
+//
+//        setupMockViewModel(object : HomeFragmentViewModel.Factory {
+//            // override fun create(handle: SavedStateHandle){} HomeFragmentViewModel = viewModel}
+//            override fun create(): HomeFragmentViewModel = viewModel
+//        })
     }
 
     @After
     fun teardown() {
-        clearAllViewModels()
+//        clearAllViewModels()
     }
 
     @Test
