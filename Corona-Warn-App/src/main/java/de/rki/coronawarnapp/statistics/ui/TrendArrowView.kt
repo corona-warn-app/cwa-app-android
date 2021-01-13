008d74b7c857c2a2833a325615e5d934e320d719
Matthias Urhahn <matthias.urhahn@sap.com>
2021-01-13 17:52:36 +0100

    Add placeholders for statistic UIs.

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/TrendArrowView.kt
@@ -0,0 +1,19 @@
+package de.rki.coronawarnapp.statistics.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass
+
+class TrendArrowView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    fun setTrend(
+        trend: KeyFigureCardOuterClass.KeyFigure.Trend,
+        trendSemantic: KeyFigureCardOuterClass.KeyFigure.TrendSemantic
+    ) {
+    }
+}
