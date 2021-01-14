ef6401e6e20d1e59c82bb46a4a41e3e30b75857f
BMItter <berndus@gmx.de>
2021-01-14 23:46:52 +0100

    Save aggregated risk per date results

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/storage/RiskLevelStorage.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/risk/storage/RiskLevelStorage.kt
@@ -1,6 +1,7 @@
 package de.rki.coronawarnapp.risk.storage
 
 import de.rki.coronawarnapp.risk.RiskLevelResult
+import de.rki.coronawarnapp.risk.result.AggregatedRiskPerDateResult
 import kotlinx.coroutines.flow.Flow
 
 interface RiskLevelStorage {
@@ -27,6 +28,14 @@ interface RiskLevelStorage {
      * Newest item first.
      */
     val latestAndLastSuccessful: Flow<List<RiskLevelResult>>
+
+    /**
+     * Risk level per date/day
+     * Used by contact diary overview
+     * Can be up to 14 Items.
+     * Item with newest date first.
+     */
+    val aggregatedRiskPerDateResults: Flow<List<AggregatedRiskPerDateResult>>
 
     suspend fun storeResult(result: RiskLevelResult)
 
