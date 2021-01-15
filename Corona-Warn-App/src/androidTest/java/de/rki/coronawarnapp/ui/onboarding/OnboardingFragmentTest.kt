b4417ed0b9bcb5518f0135d0e6803611a22d5dd1
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-01-15 15:42:05 +0100

    Specify screenshots path

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/onboarding/OnboardingFragmentTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/onboarding/OnboardingFragmentTest.kt
@@ -1,11 +1,14 @@
 package de.rki.coronawarnapp.ui.onboarding
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import androidx.fragment.app.testing.launchFragment
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import de.rki.coronawarnapp.R
@@ -15,22 +18,32 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import testhelpers.BaseUITest
+import testhelpers.CWScreenshotCallback
 import testhelpers.SCREENSHOT_DELAY_TIME
 import testhelpers.Screenshot
 import testhelpers.SystemUIDemoModeRule
 import testhelpers.launchFragmentInContainer2
 import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.ScreenshotStrategy
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
 import tools.fastlane.screengrab.locale.LocaleTestRule
+import java.util.jar.Manifest
 
 @RunWith(AndroidJUnit4::class)
 class OnboardingFragmentTest : BaseUITest() {
 
-    @Rule
-    @JvmField
-    val localeTestRule = LocaleTestRule()
-
+//    @Rule
+//    @JvmField
+//    val localeTestRule = LocaleTestRule()
+//
     @get:Rule
     val systemUIDemoModeRule = SystemUIDemoModeRule()
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        WRITE_EXTERNAL_STORAGE,
+        READ_EXTERNAL_STORAGE
+    )
 
     @Before
     fun setUp() {
@@ -57,7 +70,11 @@ class OnboardingFragmentTest : BaseUITest() {
         Screengrab.screenshot(OnboardingFragment::class.simpleName)
 
         onView(withId(R.id.onboarding_easy_language)).perform(scrollTo(), click())
-        Screengrab.screenshot(OnboardingFragment::class.simpleName.plus("2"))
+        Screengrab.screenshot(
+            OnboardingFragment::class.simpleName.plus("2"),
+            UiAutomatorScreenshotStrategy(),
+            CWScreenshotCallback()
+        )
     }
 }
 
