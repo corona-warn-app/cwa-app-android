bf6f88fca859d9d0565b5fe644365671228df975
Matthias Urhahn <matthias.urhahn@sap.com>
2021-01-15 19:20:35 +0100

    LINTING

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/security/InvalidSignatureException.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/security/InvalidSignatureException.kt
@@ -5,7 +5,7 @@ import de.rki.coronawarnapp.exception.reporting.ReportedException
 open class InvalidSignatureException(
     code: Int,
     message: String,
-    cause: Throwable? = null,
+    cause: Throwable? = null
 ) : ReportedException(
     code = code,
     message = message,
