05412ac7e571e6470d7b301ad4e1b8c8fad3433f
chris-cwa <chris.cwa.sap@gmail.com>
2021-01-15 14:48:32 +0100

    trying to live with different implementations

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/CWADateTimeFormatPatternFactory.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/CWADateTimeFormatPatternFactory.kt
@@ -7,11 +7,15 @@ object CWADateTimeFormatPatternFactory {
 
     fun Locale.shortDatePattern(): String {
         DateTimeFormat.patternForStyle("S-", this).also {
+            /**
+             * since results differ depending on the implementation of the underlying platform,
+             * we make sure that we get, what we expect
+             */
             return when {
                 (this == Locale.UK || this.language.equals("pl", true)) && it.contains("yyyy").not() ->
                     it.replace("yy", "yyyy")
                 this.language == "tr" -> it.replace("dd", "d")
-                this.language == "bg" -> it.replace("dd", "d") + " г."
+                this.language == "bg" -> (if (it.endsWith('.')) it else "$it г.").replace("dd", "d")
                 else -> it
             }
         }
