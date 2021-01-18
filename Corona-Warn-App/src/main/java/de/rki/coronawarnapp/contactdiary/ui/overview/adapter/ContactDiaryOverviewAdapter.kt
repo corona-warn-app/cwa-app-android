7a2e17683d39b09054f4feb906a49a241b93fa29
BMItter <berndus@gmx.de>
2021-01-18 19:15:41 +0100

    Show risk in overview

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/ui/overview/adapter/ContactDiaryOverviewAdapter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/ui/overview/adapter/ContactDiaryOverviewAdapter.kt
@@ -5,7 +5,9 @@ import androidx.core.view.isGone
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.contactdiary.util.clearAndAddAll
 import de.rki.coronawarnapp.databinding.ContactDiaryOverviewListItemBinding
+import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass
 import de.rki.coronawarnapp.ui.lists.BaseAdapter
+import de.rki.coronawarnapp.util.ContextExtensions.getDrawableCompat
 import de.rki.coronawarnapp.util.lists.BindableVH
 import org.joda.time.LocalDate
 import timber.log.Timber
@@ -34,20 +36,46 @@ class ContactDiaryOverviewAdapter(
         BaseAdapter.VH(R.layout.contact_diary_overview_list_item, parent),
         BindableVH<ListItem, ContactDiaryOverviewListItemBinding> {
 
+        private val nestedItemAdapter by lazy { ContactDiaryOverviewNestedAdapter() }
+
         override val viewBinding: Lazy<ContactDiaryOverviewListItemBinding> =
             lazy { ContactDiaryOverviewListItemBinding.bind(itemView) }
 
         override val onBindData: ContactDiaryOverviewListItemBinding.(item: ListItem, payloads: List<Any>) -> Unit =
             { item, _ ->
-                val nestedItemAdapter = ContactDiaryOverviewNestedAdapter(item, onItemSelectionListener)
-                viewBinding.value.contactDiaryOverviewNestedRecyclerView.adapter = nestedItemAdapter
-                contactDiaryOverviewElementName.text = dateFormatter(item.date)
-                contactDiaryOverviewElementName.contentDescription = dateFormatterForAccessibility(item.date)
+                contactDiaryOverviewNestedRecyclerView.adapter = nestedItemAdapter
+                contactDiaryOverviewNestedRecyclerView.suppressLayout(true)
                 contactDiaryOverviewElementBody.setOnClickListener { onItemSelectionListener(item) }
+
+                contactDiaryOverviewElementName.apply {
+                    text = dateFormatter(item.date)
+                    contentDescription = dateFormatterForAccessibility(item.date)
+                }
+
                 contactDiaryOverviewNestedElementGroup.isGone = item.data.isEmpty()
                 nestedItemAdapter.setItems(item.data)
-                item.riskLevel?.let { Timber.i("Risk is ${it.riskLevel.name} for ${item.date}") }
-                    ?: run { Timber.i("No risk level found for ${item.date}") }
+
+                item.riskLevel?.let {
+                    contactDiaryOverviewNestedListItemRisk.apply {
+                        this.contactDiaryOverviewRiskItem.isGone = false
+
+                        if (it.riskLevel == RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.HIGH) {
+                            this.contactDiaryOverviewRiskItemImage.setImageDrawable(context.getDrawableCompat(R.drawable.ic_high_risk_alert))
+                            this.contactDiaryOverviewItemRiskTitle.text =
+                                context.getString(R.string.contact_diary_high_risk_title)
+                        } else {
+                            this.contactDiaryOverviewRiskItemImage.setImageDrawable(context.getDrawableCompat(R.drawable.ic_low_risk_alert))
+                            this.contactDiaryOverviewItemRiskTitle.text =
+                                context.getString(R.string.contact_diary_low_risk_title)
+                        }
+
+                        this.contactDiaryOverviewItemRiskBody.text = if (item.data.isEmpty()) {
+                            context.getString(R.string.contact_diary_risk_body)
+                        } else {
+                            context.getString(R.string.contact_diary_risk_body_extended)
+                        }
+                    }
+                } ?: run { contactDiaryOverviewNestedListItemRisk.contactDiaryOverviewRiskItem.isGone = true }
             }
     }
 }
