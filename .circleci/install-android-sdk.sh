708ad1028e7bca37ae3821efebc994835e3581c2
Matthias Urhahn <matthias.urhahn@sap.com>
2021-01-18 18:28:02 +0100

    Iterating

--- .circleci/install-android-sdk.sh
+++ .circleci/install-android-sdk.sh
@@ -1,6 +1,6 @@
 if [ -d $ANDROID_SDK_ROOT ]
 then
-    echo "Directory $ANDROID_SDK_ROOT already exists so we're skipping the install. If you'd like to install fresh tools, edit this script to invalidate the CI cache."
+    echo "Directory $ANDROID_SDK_ROOT already exists so we're skipping the install. If you'd like to install fresh tools, edit this script to invalidate the CI cache..."
     exit 0
 fi
 
