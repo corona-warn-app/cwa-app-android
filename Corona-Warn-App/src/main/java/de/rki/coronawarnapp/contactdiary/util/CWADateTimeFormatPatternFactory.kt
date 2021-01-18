bf889a5450621c7beca35e0e03074906bd2e678b
chris-cwa <chris.cwa.sap@gmail.com>
2021-01-18 09:57:58 +0100

    team wants to hardcode everything

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/CWADateTimeFormatPatternFactory.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/util/CWADateTimeFormatPatternFactory.kt
@@ -1,42 +1,20 @@
 package de.rki.coronawarnapp.contactdiary.util
 
+import de.rki.coronawarnapp.contactdiary.util.CWADateTimeFormatPatternFactory.shortDatePattern
 import org.joda.time.format.DateTimeFormat
 import java.util.Locale
 
 object CWADateTimeFormatPatternFactory {
 
-    fun Locale.shortDatePattern(): String {
-        val language = language
-        DateTimeFormat.patternForStyle("S-", this).normalizeYear().apply {
-            /**
-             * since results differ depending on the implementation of the underlying platform,
-             * we make sure that we get, what we expect
-             */
-            return when {
-                fourDigitYearRequired && !yearHas4Digits -> makeYear4Digits()
-                language == "tr" -> noLeadingZeroInDayOfMonth()
-                language == "bg" -> ensureYearSuffixAbbrev().noLeadingZeroInDayOfMonth()
-                else -> this
-            }
-        }
+    fun Locale.shortDatePattern() = when {
+        this == Locale.GERMANY -> "dd.MM.yy"
+        this == Locale.UK -> "dd/MM/yyyy"
+        this == Locale.US -> "M/d/yy"
+        this == Locale("bg", "BG") -> "d.MM.yy 'г'."
+        this == Locale("ro", "RO") -> "dd.MM.yyyy"
+        this == Locale("pl", "PL") -> "dd.MM.yyyy"
+        this == Locale("tr", "TR") -> "d.MM.yyyy"
+        else -> DateTimeFormat.patternForStyle("S-", this)
     }
 
-    private fun String.ensureYearSuffixAbbrev() = (if (endsWith('.')) this else "$this 'г'.")
-
-    private val String.yearHas4Digits: Boolean
-        get() = contains("yyyy")
-
-    private fun String.makeYear4Digits() = replace("yy", "yyyy")
-
-    private val Locale.fourDigitYearRequired: Boolean
-        get() = this == Locale.UK || this.language.equals("pl", true)
-
-    private fun String.noLeadingZeroInDayOfMonth() = replace("dd", "d")
-
-    private fun String.normalizeYear() =
-        if (this.contains("y") && this.contains("yy").not()) {
-            replace("y", "yyyy")
-        } else {
-            this
-        }
 }
