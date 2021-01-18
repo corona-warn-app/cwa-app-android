25176a1a5295ba228cc9abfde40b09da5d5b1620
Juraj Kusnier <jurajkusnier@gmail.com>
2021-01-18 15:11:24 +0100

    update getFormattedDate()

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterStatistics.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/formatter/FormatterStatistics.kt
@@ -1,48 +1,38 @@
-@file:JvmName("FormatterStatistics")
-
 package de.rki.coronawarnapp.util.formatter
 
 import android.content.Context
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.contactdiary.util.getLocale
-import de.rki.coronawarnapp.contactdiary.util.toFormattedDay
 import de.rki.coronawarnapp.statistics.IncidenceStats
 import de.rki.coronawarnapp.statistics.InfectionStats
 import de.rki.coronawarnapp.statistics.KeySubmissionsStats
-import de.rki.coronawarnapp.statistics.ReproductionNumberStats
+import de.rki.coronawarnapp.statistics.SevenDayRValue
 import de.rki.coronawarnapp.statistics.StatsItem
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 
-fun StatsItem.getFormattedDate(context: Context): String {
+fun StatsItem.getPrimaryLabel(context: Context): String {
     val today = LocalDate()
     val yesterday = today.minusDays(1)
     val day = LocalDate(updatedAt)
+    val dateTimeFormatter = DateTimeFormat.mediumDate().withLocale(context.getLocale())
 
     return when (this) {
         is InfectionStats,
-        is KeySubmissionsStats -> {
-            when (day) {
-                today -> context.getString(R.string.statistics_primary_value_today)
-                yesterday -> context.getString(R.string.statistics_primary_value_yesterday)
-                else -> DateTimeFormat.mediumDate().withLocale(context.getLocale()).print(day)
-            }
+        is KeySubmissionsStats -> when (day) {
+            today -> context.getString(R.string.statistics_primary_value_today)
+            yesterday -> context.getString(R.string.statistics_primary_value_yesterday)
+            else -> dateTimeFormatter.print(day)
         }
         is IncidenceStats -> when (day) {
             today -> context.getString(R.string.statistics_primary_value_until_today)
             yesterday -> context.getString(R.string.statistics_primary_value_until_yesterday)
-            else -> context.getString(
-                R.string.statistics_primary_value_until,
-                DateTimeFormat.mediumDate().withLocale(context.getLocale()).print(day)
-            )
+            else -> context.getString(R.string.statistics_primary_value_until, dateTimeFormatter.print(day))
         }
-        is ReproductionNumberStats -> when (day) {
+        is SevenDayRValue -> when (day) {
             today -> context.getString(R.string.statistics_primary_value_curret)
             yesterday -> context.getString(R.string.statistics_primary_value_yesterday)
-            else -> context.getString(
-                R.string.statistics_primary_value_until,
-                DateTimeFormat.mediumDate().withLocale(context.getLocale()).print(day)
-            )
+            else -> context.getString(R.string.statistics_primary_value_until, dateTimeFormatter.print(day))
         }
     }
 }
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/formatter/FormatterStatisticsHelperTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/formatter/FormatterStatisticsHelperTest.kt
@@ -3,13 +3,19 @@ package de.rki.coronawarnapp.util.formatter
 import android.content.Context
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.contactdiary.util.getLocale
+import de.rki.coronawarnapp.statistics.IncidenceStats
+import de.rki.coronawarnapp.statistics.InfectionStats
+import de.rki.coronawarnapp.statistics.KeySubmissionsStats
+import de.rki.coronawarnapp.statistics.SevenDayRValue
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockkStatic
+import io.mockk.slot
 import io.mockk.unmockkAll
-import org.joda.time.LocalDate
+import org.joda.time.DateTime
+import org.joda.time.Instant
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -20,8 +26,9 @@ class FormatterStatisticsHelperTest {
     @MockK
     private lateinit var context: Context
 
-    private val today = LocalDate()
-    private val yesterday = LocalDate().minusDays(1)
+    private val slot = slot<String>()
+    private val today = Instant()
+    private val yesterday = DateTime().minusDays(1).toInstant()
 
     @Before
     fun setUp() {
@@ -29,40 +36,58 @@ class FormatterStatisticsHelperTest {
         mockkStatic("de.rki.coronawarnapp.contactdiary.util.ContactDiaryExtensionsKt")
         with(context) {
             every { context.getLocale() } returns Locale.GERMANY
+            every { getString(R.string.statistics_primary_value_curret) } returns CURRENT
             every { getString(R.string.statistics_primary_value_today) } returns TODAY
             every { getString(R.string.statistics_primary_value_yesterday) } returns YESTERDAY
             every { getString(R.string.statistics_primary_value_until_today) } returns UNTIL_TODAY
             every { getString(R.string.statistics_primary_value_until_yesterday) } returns UNTIL_YESTERDAY
+            every {
+                getString(
+                    R.string.statistics_primary_value_until,
+                    capture(slot)
+                )
+            } answers { "Until ${slot.captured}" }
         }
     }
 
     @Test
-    fun `formatStatisticsDate test`() {
-        formatStatisticsDate(
-            context = context,
-            localDate = today
-        ) shouldBe TODAY
-        formatStatisticsDate(
-            context = context,
-            localDate = yesterday
-        ) shouldBe YESTERDAY
-        formatStatisticsDate(context = context, localDate = LocalDate("2021-01-02")) shouldBe "Samstag, 02.01.21"
+    fun `InfectionStats primary label test`() {
+        InfectionStats(today, listOf()).getPrimaryLabel(context) shouldBe TODAY
+        InfectionStats(yesterday, listOf()).getPrimaryLabel(context) shouldBe YESTERDAY
+        InfectionStats(
+            Instant.parse("2021-01-13T00:00:00Z"),
+            listOf()
+        ).getPrimaryLabel(context) shouldBe "13.01.2021"
     }
 
     @Test
-    fun `formatStatisticsDateInterval test`() {
-        formatStatisticsDateInterval(
-            context = context,
-            localDate = today
-        ) shouldBe UNTIL_TODAY
-        formatStatisticsDateInterval(
-            context = context,
-            localDate = yesterday
-        ) shouldBe UNTIL_YESTERDAY
-        formatStatisticsDateInterval(
-            context = context,
-            localDate = LocalDate("2021-01-02")
-        ) shouldBe "Samstag, 02.01.21"
+    fun `KeySubmissionsStats primary label test`() {
+        KeySubmissionsStats(today, listOf()).getPrimaryLabel(context) shouldBe TODAY
+        KeySubmissionsStats(yesterday, listOf()).getPrimaryLabel(context) shouldBe YESTERDAY
+        KeySubmissionsStats(
+            Instant.parse("2021-01-13T00:00:00Z"),
+            listOf()
+        ).getPrimaryLabel(context) shouldBe "13.01.2021"
+    }
+
+    @Test
+    fun `IncidenceStats primary label test`() {
+        IncidenceStats(today, listOf()).getPrimaryLabel(context) shouldBe UNTIL_TODAY
+        IncidenceStats(yesterday, listOf()).getPrimaryLabel(context) shouldBe UNTIL_YESTERDAY
+        IncidenceStats(
+            Instant.parse("2021-01-13T00:00:00Z"),
+            listOf()
+        ).getPrimaryLabel(context) shouldBe "Until 13.01.2021"
+    }
+
+    @Test
+    fun `SevenDayRValue primary label test`() {
+        SevenDayRValue(today, listOf()).getPrimaryLabel(context) shouldBe CURRENT
+        SevenDayRValue(yesterday, listOf()).getPrimaryLabel(context) shouldBe YESTERDAY
+        SevenDayRValue(
+            Instant.parse("2021-01-13T00:00:00Z"),
+            listOf()
+        ).getPrimaryLabel(context) shouldBe "Until 13.01.2021"
     }
 
     @After
@@ -72,6 +97,7 @@ class FormatterStatisticsHelperTest {
 
     companion object {
         private const val TODAY = "Today"
+        private const val CURRENT = "Current"
         private const val YESTERDAY = "Yesterday"
         private const val UNTIL_TODAY = "Until today"
         private const val UNTIL_YESTERDAY = "Until yesterday"
