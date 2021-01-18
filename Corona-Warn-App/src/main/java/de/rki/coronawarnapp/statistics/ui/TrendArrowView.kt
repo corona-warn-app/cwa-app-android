08e031017986fe2917f7dd32f8df90d1c66af52b
chilja <chiljamgossow@gmail.com>
2021-01-18 13:56:12 +0100

    trend view

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/TrendArrowView.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/TrendArrowView.kt
@@ -2,20 +2,41 @@ package de.rki.coronawarnapp.statistics.ui
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import androidx.core.content.ContextCompat
+import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass
-import timber.log.Timber
+import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass.KeyFigure.Trend.DECREASING
+import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass.KeyFigure.Trend.INCREASING
+import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass.KeyFigure.TrendSemantic.NEGATIVE
+import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass.KeyFigure.TrendSemantic.POSITIVE
 
 class TrendArrowView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
+
+    init {
+        scaleType = ScaleType.CENTER
+        setTrend(INCREASING, NEGATIVE)
+    }
 
     fun setTrend(
         trend: KeyFigureCardOuterClass.KeyFigure.Trend,
         trendSemantic: KeyFigureCardOuterClass.KeyFigure.TrendSemantic
     ) {
-        Timber.d("setTrend(trend=$trend, trendSemantic=$trendSemantic)")
+        rotation = when (trend) {
+            INCREASING -> -45F
+            DECREASING -> 45F
+            else -> 0F
+        }
+
+        background = ContextCompat.getDrawable(
+            context, when (trendSemantic) {
+                POSITIVE -> R.drawable.bg_statistics_trend_positive
+                NEGATIVE -> R.drawable.bg_statistics_trend_negative
+                else -> R.drawable.bg_statistics_trend_neutral
+            }
+        )
     }
 }
