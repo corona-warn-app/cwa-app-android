a1502b5768fcf1b5a669fe403dc8192c9a7378c7
Lukas Lechner <office@lukaslechner.com>
2021-01-19 12:55:29 +0100

    Implement layout and bind data to it

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/cards/IncidenceCard.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/cards/IncidenceCard.kt
@@ -3,8 +3,12 @@ package de.rki.coronawarnapp.statistics.ui.homecards.cards
 import android.view.ViewGroup
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.HomeStatisticsCardsIncidenceLayoutBinding
+import de.rki.coronawarnapp.server.protocols.internal.stats.KeyFigureCardOuterClass
 import de.rki.coronawarnapp.statistics.IncidenceStats
 import de.rki.coronawarnapp.statistics.ui.homecards.StatisticsCardAdapter
+import de.rki.coronawarnapp.statistics.util.formatStatisticalValue
+import de.rki.coronawarnapp.statistics.util.getLocalizedSpannableString
+import de.rki.coronawarnapp.util.formatter.getPrimaryLabel
 
 class IncidenceCard(parent: ViewGroup) :
     StatisticsCardAdapter.ItemVH<StatisticsCardItem, HomeStatisticsCardsIncidenceLayoutBinding>(
@@ -23,6 +27,23 @@ class IncidenceCard(parent: ViewGroup) :
         item: StatisticsCardItem,
         payloads: List<Any>
     ) -> Unit = { item, payloads ->
-        item.stats as IncidenceStats
+
+        viewBinding.value.iconInfo.setOnClickListener {
+            item.onHelpAction.invoke(item.stats)
+        }
+
+        val stats = item.stats as IncidenceStats
+
+        val primaryFigure =
+            stats.keyFigures.firstOrNull { it.rank == KeyFigureCardOuterClass.KeyFigure.Rank.PRIMARY }
+
+        if (primaryFigure != null) {
+            with(viewBinding.value) {
+                valueLabel.text = stats.getPrimaryLabel(context)
+                val formattedValue = formatStatisticalValue(context, primaryFigure.value, primaryFigure.decimals)
+                valuePrimary.text = getLocalizedSpannableString(context, formattedValue)
+                trendArrowView.setTrend(primaryFigure.trend, primaryFigure.trendSemantic)
+            }
+        }
     }
 }
