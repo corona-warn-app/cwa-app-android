814969f3fa46f78ed6bd341f5357b64b032e1f61
BMItter <berndus@gmx.de>
2021-01-19 22:53:52 +0100

    We dislike flaky tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/risk/result/AggregatedRiskPerDateResultTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/risk/result/AggregatedRiskPerDateResultTest.kt
@@ -9,49 +9,35 @@ import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
 import timber.log.Timber
 
-class AggregatedRiskPerDateResultTest: BaseTest() {
+class AggregatedRiskPerDateResultTest : BaseTest() {
 
     @Test
     fun `day is correct`() {
-        val riskLevel = RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.LOW
-        val minimumDistinctEncountersWithLowRisk = 0
-        val minimumDistinctEncountersWithHighRisk = 0
-
         val oneDay = Days.ONE.toStandardDuration()
-        val todayInstant = Instant.now()
-        val todayLocalDate = LocalDate.now()
-        val yesterdayInstant = Instant.now().minus(oneDay)
-        val yesterdayLocalDate = LocalDate.now().minusDays(1)
-        val tomorrowInstant = Instant.now().plus(oneDay)
-        val tomorrowLocalDate = LocalDate.now().plusDays(1)
+        val todayInstant = Instant.parse("2020-08-20T23:00:00.000Z")
+        val todayLocalDate = LocalDate.parse("2020-08-20")
+        val yesterdayInstant = todayInstant.minus(oneDay)
+        val yesterdayLocalDate = todayLocalDate.minusDays(1)
+        val tomorrowInstant = todayInstant.plus(oneDay)
+        val tomorrowLocalDate = todayLocalDate.plusDays(1)
 
         Timber.i("Today as LocalDate $todayLocalDate and as Instant $todayInstant")
         Timber.i("Yesterday as LocalDate $yesterdayLocalDate and as Instant $yesterdayInstant")
         Timber.i("Tomorrow as LocalDate $tomorrowLocalDate and as Instant $tomorrowInstant")
 
-        val todayAggregatedRiskPerDateResult = AggregatedRiskPerDateResult(
-            dateMillisSinceEpoch = todayInstant.millis,
-            riskLevel = riskLevel,
-            minimumDistinctEncountersWithLowRisk = minimumDistinctEncountersWithLowRisk,
-            minimumDistinctEncountersWithHighRisk = minimumDistinctEncountersWithHighRisk
-        )
-
-        val yesterdayAggregatedRiskPerDateResult = AggregatedRiskPerDateResult(
-            dateMillisSinceEpoch = yesterdayInstant.millis,
-            riskLevel = riskLevel,
-            minimumDistinctEncountersWithLowRisk = minimumDistinctEncountersWithLowRisk,
-            minimumDistinctEncountersWithHighRisk = minimumDistinctEncountersWithHighRisk
-        )
-
-        val tomorrowAggregatedRiskPerDateResult = AggregatedRiskPerDateResult(
-            dateMillisSinceEpoch = tomorrowInstant.millis,
-            riskLevel = riskLevel,
-            minimumDistinctEncountersWithLowRisk = minimumDistinctEncountersWithLowRisk,
-            minimumDistinctEncountersWithHighRisk = minimumDistinctEncountersWithHighRisk
-        )
+        val todayAggregatedRiskPerDateResult = createAggregatedRiskPerDateResult(todayInstant)
+        val yesterdayAggregatedRiskPerDateResult = createAggregatedRiskPerDateResult(yesterdayInstant)
+        val tomorrowAggregatedRiskPerDateResult = createAggregatedRiskPerDateResult(tomorrowInstant)
 
         todayAggregatedRiskPerDateResult.day shouldBe todayLocalDate
         yesterdayAggregatedRiskPerDateResult.day shouldBe yesterdayLocalDate
         tomorrowAggregatedRiskPerDateResult.day shouldBe tomorrowLocalDate
     }
+
+    private fun createAggregatedRiskPerDateResult(date: Instant) = AggregatedRiskPerDateResult(
+        dateMillisSinceEpoch = date.millis,
+        riskLevel = RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel.HIGH,
+        minimumDistinctEncountersWithLowRisk = 0,
+        minimumDistinctEncountersWithHighRisk = 0
+    )
 }
