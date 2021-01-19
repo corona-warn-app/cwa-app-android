295d07764acce431b1aea3a8443e38ed30528e00
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-01-19 18:26:08 +0100

    Extend from CwaClientError

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/exception/TanPairingException.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/exception/TanPairingException.kt
@@ -2,6 +2,7 @@ package de.rki.coronawarnapp.exception
 
 import android.content.Context
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.exception.http.CwaClientError
 import de.rki.coronawarnapp.util.HasHumanReadableError
 import de.rki.coronawarnapp.util.HumanReadableError
 
@@ -9,7 +10,8 @@ import de.rki.coronawarnapp.util.HumanReadableError
  * Specific Exception type to identify an error case happening when TAN is retrieved.
  * @see <a href="https://jira-ibs.wbs.net.sap/browse/EXPOSUREAPP-4515">EXPOSUREAPP-4515</a>
  */
-class TanPairingException(val code: Int, override val cause: Throwable?) : Exception(cause), HasHumanReadableError {
+class TanPairingException(override val code: Int, override val cause: Throwable?) :
+    CwaClientError(code, cause?.message, cause), HasHumanReadableError {
     override fun toHumanReadableError(context: Context): HumanReadableError {
         return HumanReadableError(
             title = context.getString(R.string.submission_error_dialog_web_paring_invalid_title),
