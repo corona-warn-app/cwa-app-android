ad3b63c8bd0ec98f978e55af3cfbbe250a437f54
Juraj Kusnier <jurajkusnier@users.noreply.github.com>
2021-01-20 15:41:45 +0100

    Polishing statistics (EXPOSUREAPP-4693) (#2167)
    * Unify cards design
    
    * Change cards height
    
    * Layout update after merge
    
    * Revert layout changes

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/cards/SevenDayRValueCard.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/cards/SevenDayRValueCard.kt
@@ -6,6 +6,7 @@ import de.rki.coronawarnapp.databinding.HomeStatisticsCardsSevendayrvalueLayoutB
 import de.rki.coronawarnapp.statistics.SevenDayRValue
 import de.rki.coronawarnapp.statistics.ui.homecards.StatisticsCardAdapter
 import de.rki.coronawarnapp.statistics.util.formatStatisticalValue
+import de.rki.coronawarnapp.util.formatter.getPrimaryLabel
 import de.rki.coronawarnapp.statistics.util.getLocalizedSpannableString
 
 class SevenDayRValueCard(parent: ViewGroup) :
@@ -25,12 +26,16 @@ class SevenDayRValueCard(parent: ViewGroup) :
         item: StatisticsCardItem,
         payloads: List<Any>
     ) -> Unit = { item, payloads ->
-        val reproductionNumber = (item.stats as SevenDayRValue).reproductionNumber
-        val valueString = formatStatisticalValue(context, reproductionNumber.value, reproductionNumber.decimals)
-        primaryValue.text = getLocalizedSpannableString(context, valueString)
-        trendView.setTrend(reproductionNumber.trend, reproductionNumber.trendSemantic)
+
         infoStatistics.setOnClickListener {
             item.onHelpAction.invoke(item.stats)
         }
+
+        val reproductionNumber = (item.stats as SevenDayRValue).reproductionNumber
+        val valueString = formatStatisticalValue(context, reproductionNumber.value, reproductionNumber.decimals)
+
+        primaryLabel.text = item.stats.getPrimaryLabel(context)
+        primaryValue.text = getLocalizedSpannableString(context, valueString)
+        trendView.setTrend(reproductionNumber.trend, reproductionNumber.trendSemantic)
     }
 }
