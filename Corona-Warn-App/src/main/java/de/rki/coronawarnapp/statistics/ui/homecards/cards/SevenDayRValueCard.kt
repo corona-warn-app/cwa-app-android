7d9db51e4a751f55db313bbb09313e0165251ba7
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-01-20 12:11:45 +0100

    fix 7-day-value and adjust layout (#2163)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/cards/SevenDayRValueCard.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/cards/SevenDayRValueCard.kt
@@ -6,6 +6,7 @@ import de.rki.coronawarnapp.databinding.HomeStatisticsCardsSevendayrvalueLayoutB
 import de.rki.coronawarnapp.statistics.SevenDayRValue
 import de.rki.coronawarnapp.statistics.ui.homecards.StatisticsCardAdapter
 import de.rki.coronawarnapp.statistics.util.formatStatisticalValue
+import de.rki.coronawarnapp.statistics.util.getLocalizedSpannableString
 
 class SevenDayRValueCard(parent: ViewGroup) :
     StatisticsCardAdapter.ItemVH<StatisticsCardItem, HomeStatisticsCardsSevendayrvalueLayoutBinding>(
@@ -25,10 +26,11 @@ class SevenDayRValueCard(parent: ViewGroup) :
         payloads: List<Any>
     ) -> Unit = { item, payloads ->
         val reproductionNumber = (item.stats as SevenDayRValue).reproductionNumber
-        primaryValue.text = formatStatisticalValue(context, reproductionNumber.value, reproductionNumber.decimals)
+        val valueString = formatStatisticalValue(context, reproductionNumber.value, reproductionNumber.decimals)
+        primaryValue.text = getLocalizedSpannableString(context, valueString)
         trendView.setTrend(reproductionNumber.trend, reproductionNumber.trendSemantic)
         infoStatistics.setOnClickListener {
-            // TODO navigate to statistics info
+            item.onHelpAction.invoke(item.stats)
         }
     }
 }
