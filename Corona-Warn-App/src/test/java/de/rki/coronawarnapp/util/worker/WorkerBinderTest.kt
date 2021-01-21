7df9c119719712d590fb47959b10a06f0b3772b2
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-01-21 15:36:43 +0100

    Fix Worker test

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/worker/WorkerBinderTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/worker/WorkerBinderTest.kt
@@ -17,6 +17,7 @@ import de.rki.coronawarnapp.task.TaskController
 import de.rki.coronawarnapp.util.di.AppContext
 import de.rki.coronawarnapp.util.serialization.BaseGson
 import io.github.classgraph.ClassGraph
+import io.kotest.matchers.collections.shouldContainAll
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
@@ -66,7 +67,7 @@ class WorkerBinderTest : BaseTest() {
 }
 
 @Singleton
-@Component(modules = [AssistedInjectModule::class, WorkerBinder::class, MockProvider::class])
+@Component(modules = [WorkerBinder::class, MockProvider::class])
 interface WorkerTestComponent {
 
     val factories: @JvmSuppressWildcards Map<Class<out ListenableWorker>, Provider<InjectedWorkerFactory<out ListenableWorker>>>
