73b423a53d18ef4eb8792d087828f537bcc240d6
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-01-28 18:21:07 +0100

    Enhance findNavController

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ui/FragmentExtensions.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ui/FragmentExtensions.kt
@@ -1,13 +1,14 @@
 package de.rki.coronawarnapp.util.ui
 
+import android.app.Activity
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.ui.doNavigate
 import timber.log.Timber
 
@@ -23,7 +24,14 @@ fun Fragment.popBackStack(): Boolean {
     return findNavController().popBackStack()
 }
 
+/**
+ * [FragmentContainerView] does not access [NavController] in [Activity.onCreate]
+ * as workaround [FragmentManager] is used to get the [NavController]
+ * @param id [Int] NavFragment id
+ * @see <a href="https://issuetracker.google.com/issues/142847973">issue-142847973</a>
+ */
+@Throws(IllegalStateException::class)
 fun FragmentManager.findNavController(@IdRes id: Int): NavController {
-    val fragment = findFragmentById(id) as NavHostFragment
-    return fragment.navController
+    val fragment = findFragmentById(id) ?: throw IllegalStateException("Fragment is not found for id:$id")
+    return NavHostFragment.findNavController(fragment)
 }
