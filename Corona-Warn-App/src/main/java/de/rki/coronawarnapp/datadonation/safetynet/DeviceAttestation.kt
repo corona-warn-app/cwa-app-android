ff22356cff38ab66693dd1d71d6ab3043bfdbb8c
Matthias Urhahn <matthias.urhahn@sap.com>
2021-01-28 12:36:44 +0100

    Don't return salt/nonce, just offer the caller directly the PPACAndroid protobuf class.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/safetynet/DeviceAttestation.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/safetynet/DeviceAttestation.kt
@@ -1,18 +1,26 @@
 package de.rki.coronawarnapp.datadonation.safetynet
 
 import de.rki.coronawarnapp.appconfig.SafetyNetRequirements
+import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpacAndroid
 
 interface DeviceAttestation {
 
+    /**
+     * Will return the attestation result against which you can check your requirements.
+     * Will throw an exception if no attestation could be obtained (e.g. no network)
+     */
+    @Throws(SafetyNetException::class)
     suspend fun attest(request: Request): Result
 
     interface Request {
+        /**
+         * e.g. for EventSurvey, a UUID, base64 encoded.
+         */
         val scenarioPayload: ByteArray
     }
 
     interface Result {
-        val nonce: ByteArray
-        val salt: ByteArray
+        val accessControlProtoBuf: PpacAndroid.PPACAndroid
 
         /**
          * If the attestation does not match the safetynet requirements, this will throw an exception
