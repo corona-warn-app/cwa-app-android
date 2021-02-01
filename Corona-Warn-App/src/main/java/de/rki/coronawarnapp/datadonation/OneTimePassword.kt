29aee953a7e9102ce20a0fe8df91ae5664e96886
chris-cwa <chris.cwa.sap@gmail.com>
2021-02-01 14:39:10 +0100

    + timestamp

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/OneTimePassword.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/OneTimePassword.kt
@@ -1,8 +1,12 @@
 package de.rki.coronawarnapp.datadonation
 
+import org.joda.time.Instant
 import java.util.UUID
 
-data class OneTimePassword(val uuid: UUID = UUID.randomUUID()) {
+data class OneTimePassword(
+    val uuid: UUID = UUID.randomUUID(),
+    val time: Instant = Instant.now()
+) {
 
     val payloadForRequest = uuid.toString().toByteArray()
 }
