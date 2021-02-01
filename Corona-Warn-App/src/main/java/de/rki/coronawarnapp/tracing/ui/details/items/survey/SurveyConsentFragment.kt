65b43f73cff5a0bf13fbb61524a62fe0ad52ea40
BMItter <berndus@gmx.de>
2021-02-01 18:47:37 +0100

    new layouts for survey, vectors, fragment, viewmodel - wip

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/items/survey/SurveyConsentFragment.kt
@@ -0,0 +1,31 @@
+package de.rki.coronawarnapp.tracing.ui.details.items.survey
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import de.rki.coronawarnapp.R
+
+class SurveyConsentFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SurveyConsentFragment()
+    }
+
+    private lateinit var viewModel: SurveyConsentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.survey_consent_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SurveyConsentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/items/survey/SurveyConsentViewModel.kt
@@ -0,0 +1,7 @@
+package de.rki.coronawarnapp.tracing.ui.details.items.survey
+
+import androidx.lifecycle.ViewModel
+
+class SurveyConsentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
