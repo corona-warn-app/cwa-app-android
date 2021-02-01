12760f5952aad3faed0fe24c4dc0d7d43e64312f
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-02-01 15:35:29 +0100

    Add new google apis aar

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/qrcode/consent/SubmissionConsentViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/qrcode/consent/SubmissionConsentViewModel.kt
@@ -1,6 +1,8 @@
 package de.rki.coronawarnapp.ui.submission.qrcode.consent
 
 import androidx.lifecycle.asLiveData
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.nearby.exposurenotification.ExposureNotificationClient
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.storage.interoperability.InteroperabilityRepository
@@ -10,11 +12,13 @@ import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
+import timber.log.Timber
 
 class SubmissionConsentViewModel @AssistedInject constructor(
     private val submissionRepository: SubmissionRepository,
     interoperabilityRepository: InteroperabilityRepository,
-    dispatcherProvider: DispatcherProvider
+    dispatcherProvider: DispatcherProvider,
+    private val exposureNotificationClient: ExposureNotificationClient,
 ) : CWAViewModel(dispatcherProvider = dispatcherProvider) {
 
     val routeToScreen: SingleLiveEvent<SubmissionNavigationEvents> = SingleLiveEvent()
@@ -24,7 +28,15 @@ class SubmissionConsentViewModel @AssistedInject constructor(
 
     fun onConsentButtonClick() {
         submissionRepository.giveConsentToSubmission()
-        routeToScreen.postValue(SubmissionNavigationEvents.NavigateToQRCodeScan)
+        //routeToScreen.postValue(SubmissionNavigationEvents.NavigateToQRCodeScan)
+        exposureNotificationClient.requestPreAuthorizedTemporaryExposureKeyHistory().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Timber.tag("onConsentButtonClick").d(task.result.toString())
+            } else {
+                Timber.tag("onConsentButtonClick").e(task.exception)
+
+            }
+        }
     }
 
     fun onBackButtonClick() {
