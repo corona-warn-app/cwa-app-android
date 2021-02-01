9219b9a777f3e7aeff020edef4ed487a7ffdfb72
Kolya Opahle <k.opahle@sap.com>
2021-02-01 14:58:07 +0100

    added implementation for ExposureRiskMetadataDonor,
    the de.rki.coronawarnapp.server.protocols.internal.ppdd.PpaData namespaces is populated by protos that are not yet present but will be added soon
    
    Signed-off-by: Kolya Opahle <k.opahle@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/AnalyticsSettings.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/AnalyticsSettings.kt
@@ -1,7 +1,35 @@
 package de.rki.coronawarnapp.datadonation.analytics
 
+import android.content.Context
+import com.google.protobuf.ByteString
+import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpaData.ExposureRiskMetadata
+import de.rki.coronawarnapp.util.di.AppContext
+import de.rki.coronawarnapp.util.preferences.FlowPreference
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AnalyticsSettings @Inject constructor()
+class AnalyticsSettings @Inject constructor(
+    @AppContext private val context: Context
+) {
+    private val prefs by lazy {
+        context.getSharedPreferences("analytics_localdata", Context.MODE_PRIVATE)
+    }
+
+    val previousExposureRiskMetadata = FlowPreference(
+        preferences = prefs,
+        key = PREVIOUS_EXPOSURE_RISK_METADATA,
+        reader = { key ->
+            getString(key, null)?.let {
+                ExposureRiskMetadata.parseFrom(ByteString.copyFromUtf8(it))
+            }
+        },
+        writer = { key, value ->
+            putString(key, value?.toByteString()?.toStringUtf8())
+        }
+    )
+
+    companion object {
+        private const val PREVIOUS_EXPOSURE_RISK_METADATA = "exposurerisk.metadata.previous"
+    }
+}
