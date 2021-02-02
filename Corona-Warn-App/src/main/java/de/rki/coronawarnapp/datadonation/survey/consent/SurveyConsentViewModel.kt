dec6f4ba9bbbf6de59f225b7334952e6a811dae3
BMItter <berndus@gmx.de>
2021-02-02 16:43:55 +0100

    Add click listener for next button, Request url from surveys (example)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentViewModel.kt
@@ -1,14 +1,18 @@
 package de.rki.coronawarnapp.datadonation.survey.consent
 
+import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.datadonation.survey.Surveys
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
-import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
 
 class SurveyConsentViewModel @AssistedInject constructor(
-    dispatcherProvider: DispatcherProvider
+    dispatcherProvider: DispatcherProvider,
+    private val surveys: Surveys,
+    @Assisted private val surveyType: Surveys.Type
 ) : CWAViewModel(dispatcherProvider = dispatcherProvider) {
 
     val routeToScreen: SingleLiveEvent<SurveyConsentNavigationEvents> = SingleLiveEvent()
@@ -17,6 +21,15 @@ class SurveyConsentViewModel @AssistedInject constructor(
         routeToScreen.postValue(SurveyConsentNavigationEvents.NavigateBack)
     }
 
+    fun onNextButtonPressed() = launch {
+        surveys.requestDetails(surveyType)
+            .also {
+                routeToScreen.postValue(SurveyConsentNavigationEvents.NavigateToWebView(it.surveyLink))
+            }
+    }
+
     @AssistedFactory
-    interface Factory : SimpleCWAViewModelFactory<SurveyConsentViewModel>
+    interface Factory : CWAViewModelFactory<SurveyConsentViewModel> {
+        fun create(type: Surveys.Type): SurveyConsentViewModel
+    }
 }
