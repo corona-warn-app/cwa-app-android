c0efda7003bf99f98174c4a01db7de97c8d1796f
BMItter <berndus@gmx.de>
2021-02-02 17:15:15 +0100

    Added states

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentViewModel.kt
@@ -1,13 +1,19 @@
 package de.rki.coronawarnapp.datadonation.survey.consent
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.datadonation.survey.SurveyException
 import de.rki.coronawarnapp.datadonation.survey.Surveys
+import de.rki.coronawarnapp.exception.ExceptionCategory
+import de.rki.coronawarnapp.exception.reporting.report
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class SurveyConsentViewModel @AssistedInject constructor(
     dispatcherProvider: DispatcherProvider,
@@ -15,6 +21,10 @@ class SurveyConsentViewModel @AssistedInject constructor(
     @Assisted private val surveyType: Surveys.Type
 ) : CWAViewModel(dispatcherProvider = dispatcherProvider) {
 
+    private val internalLoadingState = MutableStateFlow(false)
+
+    val showLoadingIndicator: LiveData<Boolean> = internalLoadingState.asLiveData()
+    val showErrorDialog: SingleLiveEvent<SurveyException> = SingleLiveEvent()
     val routeToScreen: SingleLiveEvent<SurveyConsentNavigationEvents> = SingleLiveEvent()
 
     fun onBackButtonPressed() {
@@ -22,10 +32,15 @@ class SurveyConsentViewModel @AssistedInject constructor(
     }
 
     fun onNextButtonPressed() = launch {
-        surveys.requestDetails(surveyType)
-            .also {
-                routeToScreen.postValue(SurveyConsentNavigationEvents.NavigateToWebView(it.surveyLink))
-            }
+        internalLoadingState.emit(true)
+        try {
+            val surveyLink = surveys.requestDetails(surveyType).surveyLink
+            routeToScreen.postValue(SurveyConsentNavigationEvents.NavigateToWebView(surveyLink))
+        } catch (surveyException: SurveyException) {
+            surveyException.report(ExceptionCategory.INTERNAL)
+            showErrorDialog.postValue(surveyException)
+        }
+        internalLoadingState.emit(false)
     }
 
     @AssistedFactory
