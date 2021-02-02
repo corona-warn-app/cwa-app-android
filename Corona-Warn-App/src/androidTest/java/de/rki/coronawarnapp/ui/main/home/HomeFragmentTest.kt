04562aeed55cdca32271034165d38f49b2f3b682
Lukas Lechner <lukas.lechner@sap.com>
2021-02-02 15:50:39 +0100

    Delta-Onboarding interoperability info not shown (EXPOSUREAPP-4910) (#2257)
    * Fix issue of not showing delta onboarding about interoperability before showing release notes
    
    * Suppress failure to give the chance for other steps to continue in Firebase TestLab
    
    * Fix instrumentation test
    
    * Revert "Suppress failure to give the chance for other steps to continue in Firebase TestLab"
    
    This reverts commit 5d642b66
    
    * Use when statement instead of if-else conditions
    
    Co-authored-by: harambasicluka <64483219+harambasicluka@users.noreply.github.com>
    Co-authored-by: Alex Paulescu <alex.paulescu@gmail.com>

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/main/home/HomeFragmentTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/main/home/HomeFragmentTest.kt
@@ -86,6 +86,7 @@ class HomeFragmentTest : BaseUITest() {
             every { showLoweredRiskLevelDialog } returns MutableLiveData()
             every { homeItems } returns MutableLiveData(emptyList())
             every { popupEvents } returns SingleLiveEvent()
+            every { showPopUpsOrNavigate() } just Runs
         }
 
         setupMockViewModel(
