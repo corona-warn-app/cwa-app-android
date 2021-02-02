e6e7e4f0e751bd9e4b21fa732be0c746885e98ed
chris-cwa <chris.cwa.sap@gmail.com>
2021-02-02 21:16:41 +0100

    https://github.com/corona-warn-app/cwa-app-android/pull/2232#discussion_r567892814

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/OneTimePasswordTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/OneTimePasswordTest.kt
@@ -8,9 +8,45 @@ class OneTimePasswordTest {
 
     @Test
     fun test() {
-        "923c977a-617b-11eb-ae93-0242ac130002".apply {
-            OneTimePassword(UUID.fromString(this))
-                .payloadForRequest shouldBe encodeToByteArray()
-        }
+        OneTimePassword(UUID.fromString("923c977a-617b-11eb-ae93-0242ac130002"))
+            .payloadForRequest shouldBe
+            byteArrayOf(
+                57,
+                50,
+                51,
+                99,
+                57,
+                55,
+                55,
+                97,
+                45,
+                54,
+                49,
+                55,
+                98,
+                45,
+                49,
+                49,
+                101,
+                98,
+                45,
+                97,
+                101,
+                57,
+                51,
+                45,
+                48,
+                50,
+                52,
+                50,
+                97,
+                99,
+                49,
+                51,
+                48,
+                48,
+                48,
+                50
+            )
     }
 }
