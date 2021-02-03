da192e1fdc8028bceab30767ff93406d5aef3f67
BMItter <46747780+BMItter@users.noreply.github.com>
2021-02-03 15:54:40 +0100

    Survey Event UI Base (EXPOSUREAPP-4807, 4808 ) (#2271)
    * tracing survey card implemented - wip
    
    * Added user survey to risk details
    
    * Fixed button noch clickable
    
    * use card as parent, minor adjustments
    
    * new layouts for survey, vectors, fragment, viewmodel - wip
    
    * Moved fragment and viewmodel to correct spot
    
    * Adjusted fragment and vm
    
    * align with package structure
    
    * Added module
    
    * event handling for upcomiing navigation
    
    * Navigation from tracing details to survey consent fragment
    
    * ktlint, detekt
    
    * adjusted res
    
    * Pass type of survey
    
    * klint
    
    * Add click listener for next button, Request url from surveys (example)
    
    * ktlint
    
    * string res cleanup
    
    * Added states
    
    * delete obsolet layout
    
    * empty space removed
    
    * comment- data class added
    
    * addressed reformatting issues
    
    * bigger headline for survey headline
    
    * adjusted view behaviour to match figma

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentFragment.kt
@@ -0,0 +1,57 @@
+package de.rki.coronawarnapp.datadonation.survey.consent
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.SurveyConsentFragmentBinding
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.observe2
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
+import javax.inject.Inject
+
+class SurveyConsentFragment : Fragment(R.layout.survey_consent_fragment), AutoInject {
+
+    private val navArgs by navArgs<SurveyConsentFragmentArgs>()
+
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val vm: SurveyConsentViewModel by cwaViewModelsAssisted(
+        factoryProducer = { viewModelFactory },
+        constructorCall = { factory, _ ->
+            factory as SurveyConsentViewModel.Factory
+            factory.create(navArgs.SurveyType)
+        }
+    )
+
+    private val binding: SurveyConsentFragmentBinding by viewBindingLazy()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            toolbar.setNavigationOnClickListener { vm.onBackButtonPressed() }
+            surveyNextButton.setOnClickListener { vm.onNextButtonPressed() }
+        }
+
+        vm.routeToScreen.observe2(this) {
+            when (it) {
+                is SurveyConsentNavigationEvents.NavigateBack ->
+                    activity?.onBackPressed()
+                is SurveyConsentNavigationEvents.NavigateToWebView ->
+                    Toast.makeText(requireContext(), "Open ${it.url} (WIP, next PR))", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        vm.showLoadingIndicator.observe2(this) {
+            // TODO
+        }
+
+        vm.showErrorDialog.observe2(this) {
+            // TODO
+        }
+    }
+}
