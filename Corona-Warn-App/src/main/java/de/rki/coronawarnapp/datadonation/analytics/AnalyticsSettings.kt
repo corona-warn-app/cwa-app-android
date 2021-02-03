c2fc2620b79623937749a661199f9dfad623265e
Kolya Opahle <k.opahle@sap.com>
2021-02-03 15:39:36 +0100

    switched to kotlin only solution for proto <-> settings string conversion
    Signed-off-by: Kolya Opahle <k.opahle@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/AnalyticsSettings.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/AnalyticsSettings.kt
@@ -1,12 +1,13 @@
 package de.rki.coronawarnapp.datadonation.analytics
 
 import android.content.Context
-import com.google.protobuf.ByteString
 import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpaData
 import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpaData.ExposureRiskMetadata
 import de.rki.coronawarnapp.util.di.AppContext
 import de.rki.coronawarnapp.util.preferences.FlowPreference
 import de.rki.coronawarnapp.util.preferences.createFlowPreference
+import okio.ByteString.Companion.decodeBase64
+import okio.ByteString.Companion.toByteString
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -22,12 +23,14 @@ class AnalyticsSettings @Inject constructor(
         preferences = prefs,
         key = PREVIOUS_EXPOSURE_RISK_METADATA,
         reader = { key ->
-            getString(key, null)?.let {
-                ExposureRiskMetadata.parseFrom(ByteString.copyFromUtf8(it))
+            getString(key, null)?.let { prefString ->
+                prefString.decodeBase64()?.toByteArray()?.let {
+                    ExposureRiskMetadata.parseFrom(it)
+                }
             }
         },
         writer = { key, value ->
-            putString(key, value?.toByteString()?.toStringUtf8())
+            putString(key, value?.toByteArray()?.toByteString()?.base64())
         }
     )
 
