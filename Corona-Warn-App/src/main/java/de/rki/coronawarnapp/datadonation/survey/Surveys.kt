be37bb616ff4f70a2e2c4023a2d14791aec926d1
BMItter <berndus@gmx.de>
2021-02-04 17:30:41 +0100

    Create available surveys lazily, run flow on coroutine context

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/Surveys.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/Surveys.kt
@@ -2,7 +2,9 @@ package de.rki.coronawarnapp.datadonation.survey
 
 import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.datadonation.safetynet.DeviceAttestation
+import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,15 +13,20 @@ import javax.inject.Singleton
 class Surveys @Inject constructor(
     private val deviceAttestation: DeviceAttestation,
     private val settings: SurveySettings,
-    private val appConfigProvider: AppConfigProvider
+    private val appConfigProvider: AppConfigProvider,
+    dispatcherProvider: DispatcherProvider
 ) {
 
-    val availableSurveys: Flow<Collection<Type>> = appConfigProvider.currentConfig.map {
-        mutableListOf<Type>().apply {
-            if (it.survey.surveyOnHighRiskEnabled) {
-                add(Type.HIGH_RISK_ENCOUNTER)
+    val availableSurveys: Flow<Collection<Type>> by lazy {
+        appConfigProvider.currentConfig
+            .flowOn(dispatcherProvider.Default)
+            .map {
+                mutableListOf<Type>().apply {
+                    if (it.survey.surveyOnHighRiskEnabled) {
+                        add(Type.HIGH_RISK_ENCOUNTER)
+                    }
+                }
             }
-        }
     }
 
     suspend fun requestDetails(type: Type): Survey {
