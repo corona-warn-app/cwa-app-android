c033fefb1ac361166f7644746170b01e1ecc0690
BMItter <berndus@gmx.de>
2021-02-04 14:48:55 +0100

    Implemented survey mapping

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/SurveyConfig.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/SurveyConfig.kt
@@ -3,7 +3,18 @@ package de.rki.coronawarnapp.appconfig
 import de.rki.coronawarnapp.appconfig.mapping.ConfigMapper
 
 interface SurveyConfig {
-    val enableSurveyLinkOnHighRiskCard: Boolean
+    /** The name of the query parameter to pass the OTP to the survey */
+    val otpQueryParameterName: String
+
+    /** True to enable the survey in case of a high risk card, false otherwise */
+    val surveyOnHighRiskEnabled: Boolean
+
+    /**
+     * The url (without query parameters) to access the survey in case of a high risk
+     * card
+     */
+    val surveyOnHighRiskUrl: String
+
     val safetyNetRequirements: SafetyNetRequirements
 
     interface Mapper : ConfigMapper<SurveyConfig>
