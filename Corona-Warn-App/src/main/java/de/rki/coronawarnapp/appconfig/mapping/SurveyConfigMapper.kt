48a7987a706c83796e7e7485d0cc04253244156f
BMItter <berndus@gmx.de>
2021-02-05 11:07:28 +0100

    Use default params

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/mapping/SurveyConfigMapper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/mapping/SurveyConfigMapper.kt
@@ -11,50 +11,40 @@ import javax.inject.Inject
 @Reusable
 class SurveyConfigMapper @Inject constructor() : SurveyConfig.Mapper {
     override fun map(rawConfig: AppConfigAndroid.ApplicationConfigurationAndroid): SurveyConfig {
-        var otpQueryParameterName = ""
-        var enableSurveyLinkOnHighRiskCard = false
-        var surveyOnHighRiskUrl = ""
-        var requireBasicIntegrity = false
-        var requireCTSProfileMatch = false
-        var requireEvaluationTypeBasic = false
-        var requireEvaluationTypeHardwareBacked = false
+        var surveyConfigContainer = SurveyConfigContainer()
 
         if (rawConfig.hasEventDrivenUserSurveyParameters()) {
             if (rawConfig.eventDrivenUserSurveyParameters.hasCommon()) {
                 rawConfig.eventDrivenUserSurveyParameters.common.also {
-                    otpQueryParameterName = it.otpQueryParameterName
-                    enableSurveyLinkOnHighRiskCard = it.surveyOnHighRiskEnabled
-                    surveyOnHighRiskUrl = it.surveyOnHighRiskUrl
+                    surveyConfigContainer = SurveyConfigContainer(
+                        otpQueryParameterName = it.otpQueryParameterName,
+                        surveyOnHighRiskEnabled = it.surveyOnHighRiskEnabled,
+                        surveyOnHighRiskUrl = it.surveyOnHighRiskUrl
+                    )
                 }
             }
 
             if (rawConfig.eventDrivenUserSurveyParameters.hasPpac()) {
                 rawConfig.eventDrivenUserSurveyParameters.ppac.also {
-                    requireBasicIntegrity = it.requireBasicIntegrity
-                    requireCTSProfileMatch = it.requireCTSProfileMatch
-                    requireEvaluationTypeBasic = it.requireEvaluationTypeBasic
-                    requireEvaluationTypeHardwareBacked = it.requireEvaluationTypeHardwareBacked
+                    surveyConfigContainer = surveyConfigContainer.copy(
+                        safetyNetRequirements = SafetyNetRequirementsContainer(
+                            requireBasicIntegrity = it.requireBasicIntegrity,
+                            requireCTSProfileMatch = it.requireCTSProfileMatch,
+                            requireEvaluationTypeBasic = it.requireEvaluationTypeBasic,
+                            requireEvaluationTypeHardwareBacked = it.requireEvaluationTypeHardwareBacked
+                        )
+                    )
                 }
             }
         }
 
-        return SurveyConfigContainer(
-            otpQueryParameterName = otpQueryParameterName,
-            surveyOnHighRiskEnabled = enableSurveyLinkOnHighRiskCard,
-            surveyOnHighRiskUrl = surveyOnHighRiskUrl,
-            safetyNetRequirements = SafetyNetRequirementsContainer(
-                requireBasicIntegrity = requireBasicIntegrity,
-                requireCTSProfileMatch = requireCTSProfileMatch,
-                requireEvaluationTypeBasic = requireEvaluationTypeBasic,
-                requireEvaluationTypeHardwareBacked = requireEvaluationTypeHardwareBacked
-            )
-        ).also { Timber.v("SurveyConfig=%s", it) }
+        return surveyConfigContainer.also { Timber.v("SurveyConfig=%s", it) }
     }
 
     data class SurveyConfigContainer(
-        override val otpQueryParameterName: String,
-        override val surveyOnHighRiskEnabled: Boolean,
-        override val surveyOnHighRiskUrl: String,
-        override val safetyNetRequirements: SafetyNetRequirements
+        override val otpQueryParameterName: String = "",
+        override val surveyOnHighRiskEnabled: Boolean = false,
+        override val surveyOnHighRiskUrl: String = "",
+        override val safetyNetRequirements: SafetyNetRequirements = SafetyNetRequirementsContainer()
     ) : SurveyConfig
 }
