d378ffc81b53e1fd0ff83845d7b779b50fae1386
chilja <chiljamgossow@gmail.com>
2021-02-08 17:03:24 +0100

    show normal behavior box when tracing disabled independent of risk status use toolbar

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/TracingDetailsItemProvider.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/TracingDetailsItemProvider.kt
@@ -48,8 +48,9 @@ class TracingDetailsItemProvider @Inject constructor(
                 add(AdditionalInfoLowRiskBox.Item)
             }
 
-            when (latestCalc.riskState) {
-                RiskState.INCREASED_RISK -> BehaviorIncreasedRiskBox.Item
+            when {
+                status != Status.TRACING_INACTIVE && latestCalc.riskState == RiskState.INCREASED_RISK ->
+                    BehaviorIncreasedRiskBox.Item
                 else -> BehaviorNormalRiskBox.Item(
                     tracingStatus = status,
                     riskState = latestCalc.riskState
