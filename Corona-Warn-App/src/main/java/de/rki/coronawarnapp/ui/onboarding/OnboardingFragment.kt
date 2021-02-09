422317895dffc46b506e2beb2ead561b8d00ce31
chilja <chiljamgossow@gmail.com>
2021-02-09 19:02:41 +0100

    show link for German only simplify layout and fragment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/onboarding/OnboardingFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/onboarding/OnboardingFragment.kt
@@ -9,44 +9,45 @@ import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentOnboardingBinding
 import de.rki.coronawarnapp.util.di.AutoInject
-import de.rki.coronawarnapp.util.ui.observe2
-import de.rki.coronawarnapp.util.ui.viewBindingLazy
-import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
-import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
 import de.rki.coronawarnapp.util.ui.doNavigate
-import javax.inject.Inject
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
+import java.util.Locale
 
 /**
  * Onboarding starting point.
  */
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding), AutoInject {
 
-    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
-    private val vm: OnboardingFragmentViewModel by cwaViewModels { viewModelFactory }
     private val binding: FragmentOnboardingBinding by viewBindingLazy()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            onboardingButtonNext.setOnClickListener { vm.onNextButtonClick() }
-            onboardingInclude.onboardingEasyLanguage.setOnClickListener { vm.onEasyLanguageClick() }
-        }
-        vm.routeToScreen.observe2(this) {
-            when (it) {
-                is OnboardingNavigationEvents.NavigateToOnboardingPrivacy ->
-                    doNavigate(
-                        OnboardingFragmentDirections
-                            .actionOnboardingFragmentToOnboardingPrivacyFragment()
-                    )
-                is OnboardingNavigationEvents.NavigateToEasyLanguageUrl ->
-                    startActivity(
-                        Intent(
-                            Intent.ACTION_VIEW,
-                            Uri.parse(getString(R.string.onboarding_tracing_easy_language_explanation_url))
-                        )
-                    )
+            onboardingButtonNext.setOnClickListener { goToOnboardingPrivacyFragment() }
+            // only show link for German
+            if (Locale.getDefault().language == "de") {
+                onboardingEasyLanguage.visibility = View.VISIBLE
+                onboardingEasyLanguage.setOnClickListener { openEasyLanguageLink() }
+            } else {
+                onboardingEasyLanguage.visibility = View.GONE
             }
         }
+    }
+
+    private fun goToOnboardingPrivacyFragment() {
+        doNavigate(
+            OnboardingFragmentDirections
+                .actionOnboardingFragmentToOnboardingPrivacyFragment()
+        )
+    }
+
+    private fun openEasyLanguageLink() {
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(getString(R.string.onboarding_tracing_easy_language_explanation_url))
+            )
+        )
     }
 
     override fun onResume() {
