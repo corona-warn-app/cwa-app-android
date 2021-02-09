201588893809cf883c95eb657b6e3a5612923549
Kolya Opahle <k.opahle@sap.com>
2021-02-09 18:38:25 +0100

    Implemented File based storage of Last submitted analytics file on tester devices
    Signed-off-by: Kolya Opahle <k.opahle@sap.com>

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/datadonation/analytics/storage/DefaultLastAnalyticsSubmissionLogger.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/datadonation/analytics/storage/DefaultLastAnalyticsSubmissionLogger.kt
@@ -1,12 +1,36 @@
 package de.rki.coronawarnapp.datadonation.analytics.storage
 
+import android.content.Context
 import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpaData
+import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import de.rki.coronawarnapp.util.di.AppContext
+import kotlinx.coroutines.withContext
+import java.io.File
 import javax.inject.Inject
 
-class DefaultLastAnalyticsSubmissionLogger @Inject constructor() : LastAnalyticsSubmissionLogger {
-    override suspend fun storeAnalyticsData(analyticsProto: PpaData.PPADataAndroid) {
-        // Do not store past analytics submissions in Production
-    }
+class DefaultLastAnalyticsSubmissionLogger @Inject constructor(
+    @AppContext private val context: Context,
+    private val dispatcherProvider: DispatcherProvider
+) : LastAnalyticsSubmissionLogger {
+    private val analyticsDir = File(context.cacheDir, "analytics_storage")
+    private val analyticsFile = File(analyticsDir, "analytics.bin")
 
-    override suspend fun getLastAnalyticsData(): PpaData.PPADataAndroid? = null
+    override suspend fun storeAnalyticsData(analyticsProto: PpaData.PPADataAndroid) =
+        withContext(dispatcherProvider.IO) {
+            if (!analyticsDir.exists()) {
+                analyticsDir.mkdirs()
+            }
+
+            analyticsFile.writeBytes(analyticsProto.toByteArray())
+        }
+
+    override suspend fun getLastAnalyticsData(): PpaData.PPADataAndroid? = withContext(dispatcherProvider.IO) {
+        try {
+            analyticsFile.readBytes().let {
+                PpaData.PPADataAndroid.parseFrom(it)
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
