5049591a4f606bc377e971c2196f5cbdc6a2de62
Matthias Urhahn <matthias.urhahn@sap.com>
2021-02-12 18:08:10 +0100

    Use a specific time object, as Long and just `getDelay` could be confusing. Android mostly uses long=miliseconds and not hours.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/worker/DataDonationAnalyticsWorkBuilder.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/worker/DataDonationAnalyticsWorkBuilder.kt
@@ -6,17 +6,18 @@ import androidx.work.PeriodicWorkRequestBuilder
 import dagger.Reusable
 import de.rki.coronawarnapp.worker.BackgroundConstants
 import org.joda.time.DateTimeConstants
+import org.joda.time.Duration
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @Reusable
 class DataDonationAnalyticsWorkBuilder @Inject constructor() {
-    fun buildPeriodicWork(additionalDelay: Long): PeriodicWorkRequest =
+    fun buildPeriodicWork(additionalDelay: Duration): PeriodicWorkRequest =
         PeriodicWorkRequestBuilder<DataDonationAnalyticsPeriodicWorker>(
             DateTimeConstants.HOURS_PER_DAY.toLong(), TimeUnit.HOURS
         )
             .setInitialDelay(
-                DateTimeConstants.HOURS_PER_DAY.toLong() + additionalDelay,
+                DateTimeConstants.HOURS_PER_DAY.toLong() + additionalDelay.standardHours,
                 TimeUnit.HOURS
             )
             .setBackoffCriteria(
