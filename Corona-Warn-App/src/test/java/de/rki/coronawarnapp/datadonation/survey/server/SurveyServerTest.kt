3baf695628c8317a8f8b8f32bd513b124baf98bc
chris-cwa <chris.cwa.sap@gmail.com>
2021-02-12 12:41:28 +0100

    fixed tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/survey/server/SurveyServerTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/survey/server/SurveyServerTest.kt
@@ -1,75 +1,119 @@
 package de.rki.coronawarnapp.datadonation.survey.server
 
 import android.content.Context
+import com.google.protobuf.ByteString
 import de.rki.coronawarnapp.datadonation.OneTimePassword
+import de.rki.coronawarnapp.datadonation.safetynet.DeviceAttestation
 import de.rki.coronawarnapp.server.protocols.internal.ppdd.EdusOtp
+import de.rki.coronawarnapp.server.protocols.internal.ppdd.EdusOtpRequestAndroid
+import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpacAndroid
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import okio.ByteString.Companion.decodeBase64
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import testhelpers.BaseIOTest
 import testhelpers.BaseTest
 import testhelpers.TestDispatcherProvider
-import java.io.File
 import java.util.UUID
 
 class SurveyServerTest : BaseTest() {
+
     @MockK lateinit var surveyApi: SurveyApiV1
     @MockK lateinit var context: Context
-
-    private val testDir = File(BaseIOTest.IO_TEST_BASEDIR, this::class.java.simpleName)
+    @MockK lateinit var attestationResult: DeviceAttestation.Result
 
     @BeforeEach
     fun setup() {
         MockKAnnotations.init(this)
-        testDir.mkdirs()
-        testDir.exists() shouldBe true
     }
 
     @AfterEach
     fun teardown() {
         clearAllMocks()
-        testDir.deleteRecursively()
     }
 
     @Test
-    fun `valid otp`(): Unit = runBlocking {
-        val server = SurveyServer(surveyApi = { surveyApi }, TestDispatcherProvider())
-        coEvery { surveyApi.authOTP(any()) } answers {
-            arg<EdusOtp.EDUSOneTimePassword>(0).apply {
-                otp shouldBe "15cff19f-af26-41bc-94f2-c1a65075e894"
+    fun `valid otp`() {
+        val otpData = "15cff19f-af26-41bc-94f2-c1a65075e894"
+        val build = PpacAndroid.PPACAndroid.newBuilder()
+            .setSafetyNetJws("abc")
+            .setSalt("def")
+            .build()
+        every { attestationResult.accessControlProtoBuf } returns build
+
+        runBlocking {
+            val server = SurveyServer(surveyApi = { surveyApi }, TestDispatcherProvider())
+            coEvery { surveyApi.authOTP(any()) } answers {
+                arg<EdusOtpRequestAndroid.EDUSOneTimePasswordRequestAndroid>(0).payload shouldBe
+                    EdusOtpRequestAndroid.EDUSOneTimePasswordRequestAndroid.newBuilder()
+                        .setPayload(
+                            EdusOtp.EDUSOneTimePassword.newBuilder()
+                                .setOtp(otpData)
+                                .setOtpBytes(
+                                    ByteString.copyFrom(
+                                        "MTVjZmYxOWYtYWYyNi00MWJjLTk0ZjItYzFhNjUwNzVlODk0".decodeBase64()!!
+                                            .toByteArray()
+                                    )
+                                )
+                        )
+                        .setAuthentication(build)
+                        .build()
+                SurveyApiV1.DataDonationResponse(null)
             }
-            SurveyApiV1.DataDonationResponse(null)
+
+            server.authOTP(
+                OneTimePassword(UUID.fromString(otpData)),
+                attestationResult
+            ).errorCode shouldBe null
+
+            coVerify { surveyApi.authOTP(any()) }
         }
-
-        val data = OneTimePassword(UUID.fromString("15cff19f-af26-41bc-94f2-c1a65075e894"))
-        server.authOTP(data, mockk()).errorCode shouldBe null
-
-        coVerify { surveyApi.authOTP(any()) }
     }
 
     @Test
-    fun `invalid otp`(): Unit = runBlocking {
-        val server = SurveyServer(surveyApi = { surveyApi }, TestDispatcherProvider())
-        coEvery { surveyApi.authOTP(any()) } answers {
-            arg<EdusOtp.EDUSOneTimePassword>(0).apply {
-                otp shouldBe "15cff19f-af26-41bc-94f2-c1a65075e894"
+    fun `invalid otp`() {
+        val otpData = "15cff19f-af26-41bc-94f2-c1a65075e894"
+        val build = PpacAndroid.PPACAndroid.newBuilder()
+            .setSafetyNetJws("abc")
+            .setSalt("def")
+            .build()
+        every { attestationResult.accessControlProtoBuf } returns build
+
+        runBlocking {
+            val server = SurveyServer(surveyApi = { surveyApi }, TestDispatcherProvider())
+            coEvery { surveyApi.authOTP(any()) } answers {
+                arg<EdusOtpRequestAndroid.EDUSOneTimePasswordRequestAndroid>(0).payload shouldBe
+                    EdusOtpRequestAndroid.EDUSOneTimePasswordRequestAndroid.newBuilder()
+                        .setPayload(
+                            EdusOtp.EDUSOneTimePassword.newBuilder()
+                                .setOtp(otpData)
+                                .setOtpBytes(
+                                    ByteString.copyFrom(
+                                        "MTVjZmYxOWYtYWYyNi00MWJjLTk0ZjItYzFhNjUwNzVlODk0".decodeBase64()!!
+                                            .toByteArray()
+                                    )
+                                )
+                        )
+                        .setAuthentication(build)
+                        .build()
+                SurveyApiV1.DataDonationResponse("API_TOKEN_ALREADY_ISSUED")
             }
-            SurveyApiV1.DataDonationResponse("API_TOKEN_ALREADY_ISSUED")
+
+            server.authOTP(
+                OneTimePassword(UUID.fromString(otpData)),
+                attestationResult
+            ).errorCode shouldBe "API_TOKEN_ALREADY_ISSUED"
+
+            coVerify { surveyApi.authOTP(any()) }
         }
-
-        val data = OneTimePassword(UUID.fromString("15cff19f-af26-41bc-94f2-c1a65075e894"))
-        server.authOTP(data, mockk()).errorCode shouldBe "API_TOKEN_ALREADY_ISSUED"
-
-        coVerify { surveyApi.authOTP(any()) }
     }
 
     @Test
@@ -79,7 +123,7 @@ class SurveyServerTest : BaseTest() {
         val data = OneTimePassword(UUID.fromString("15cff19f-af26-41bc-94f2-c1a65075e894"))
 
         shouldThrowAny {
-            server.authOTP(data, mockk())
+            server.authOTP(data, attestationResult)
         }
     }
 }
