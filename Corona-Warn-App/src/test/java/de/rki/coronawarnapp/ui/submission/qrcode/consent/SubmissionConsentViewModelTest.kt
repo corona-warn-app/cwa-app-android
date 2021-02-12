f6cdfc177fc8dbad1f0c44c556af5b2d5818f58b
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-02-12 10:03:20 +0100

    Add tests for consent viewmodel

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/qrcode/consent/SubmissionConsentViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/qrcode/consent/SubmissionConsentViewModelTest.kt
@@ -1,5 +1,7 @@
 package de.rki.coronawarnapp.ui.submission.qrcode.consent
 
+import com.google.android.gms.common.api.ApiException
+import de.rki.coronawarnapp.nearby.modules.tekhistory.TEKHistoryProvider
 import de.rki.coronawarnapp.storage.interoperability.InteroperabilityRepository
 import de.rki.coronawarnapp.submission.SubmissionRepository
 import de.rki.coronawarnapp.ui.Country
@@ -7,6 +9,8 @@ import de.rki.coronawarnapp.ui.submission.viewmodel.SubmissionNavigationEvents
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
@@ -18,12 +22,14 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import testhelpers.TestDispatcherProvider
 import testhelpers.extensions.InstantExecutorExtension
+import java.io.PrintWriter
 
 @ExtendWith(InstantExecutorExtension::class)
 class SubmissionConsentViewModelTest {
 
     @MockK lateinit var submissionRepository: SubmissionRepository
     @MockK lateinit var interoperabilityRepository: InteroperabilityRepository
+    @MockK lateinit var tekHistoryProvider: TEKHistoryProvider
 
     lateinit var viewModel: SubmissionConsentViewModel
 
@@ -38,7 +44,7 @@ class SubmissionConsentViewModelTest {
             submissionRepository,
             interoperabilityRepository,
             dispatcherProvider = TestDispatcherProvider(),
-            mockk()
+            tekHistoryProvider
         )
     }
 
@@ -64,5 +70,46 @@ class SubmissionConsentViewModelTest {
     fun testCountryList() {
         viewModel.countries.observeForever { }
         viewModel.countries.value shouldBe countryList
+    }
+
+    @Test
+    fun `giveGoogleConsentResult when user Allows routes to QR Code scan`() {
+        viewModel.giveGoogleConsentResult(true)
+        viewModel.routeToScreen.value shouldBe SubmissionNavigationEvents.NavigateToQRCodeScan
+    }
+
+    @Test
+    fun `giveGoogleConsentResult when user Doesn't Allow routes to QR Code scan`() {
+        viewModel.giveGoogleConsentResult(false)
+        viewModel.routeToScreen.value shouldBe SubmissionNavigationEvents.NavigateToQRCodeScan
+    }
+
+    @Test
+    fun `onConsentButtonClick sets normal consent and request new Google consent Api`() {
+        viewModel.onConsentButtonClick()
+        verify(exactly = 1) { submissionRepository.giveConsentToSubmission() }
+        coVerify(exactly = 1) { tekHistoryProvider.preAuthorizeExposureKeyHistory() }
+    }
+
+    @Test
+    fun `onConsentButtonClick triggers new Google consent window`() {
+        val apiException = mockk<ApiException>().apply {
+            every { status.hasResolution() } returns true
+            every { printStackTrace(any<PrintWriter>()) } just Runs
+        }
+        coEvery { tekHistoryProvider.preAuthorizeExposureKeyHistory() } throws apiException
+
+        viewModel.onConsentButtonClick()
+
+        viewModel.routeToScreen.value shouldBe SubmissionNavigationEvents.ResolvePlayServicesException(apiException)
+    }
+
+    @Test
+    fun `onConsentButtonClick routes to QR scan when unrecoverable Error is thrown`() {
+        coEvery { tekHistoryProvider.preAuthorizeExposureKeyHistory() } throws Exception("Unrecoverable Error")
+
+        viewModel.onConsentButtonClick()
+
+        viewModel.routeToScreen.value shouldBe SubmissionNavigationEvents.NavigateToQRCodeScan
     }
 }
