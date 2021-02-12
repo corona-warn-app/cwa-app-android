164bfa93f2e9c34f3e2550a658c8bec051f8fb01
chris-cwa <chris.cwa.sap@gmail.com>
2021-02-12 09:44:07 +0100

    auth otp and handle result

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/Surveys.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/Surveys.kt
@@ -1,14 +1,16 @@
 package de.rki.coronawarnapp.datadonation.survey
 
 import de.rki.coronawarnapp.appconfig.AppConfigProvider
+import de.rki.coronawarnapp.datadonation.OTPAuthorizationResult
 import de.rki.coronawarnapp.datadonation.safetynet.DeviceAttestation
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import org.joda.time.Seconds
+import de.rki.coronawarnapp.datadonation.storage.OTPRepository
+import de.rki.coronawarnapp.datadonation.survey.server.SurveyServer
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import java.util.UUID
+import org.joda.time.Instant
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,6 +19,8 @@ class Surveys @Inject constructor(
     private val deviceAttestation: DeviceAttestation,
     private val settings: SurveySettings,
     private val appConfigProvider: AppConfigProvider,
+    private val surveyServer: SurveyServer,
+    private val otpRepo: OTPRepository,
     dispatcherProvider: DispatcherProvider,
     private val urlProvider: SurveyUrlProvider
 ) {
@@ -34,18 +38,39 @@ class Surveys @Inject constructor(
     }
 
     suspend fun requestDetails(type: Type): Survey {
+        val surveyConfig = appConfigProvider.getAppConfig().survey
+        Timber.v("Requested survey: %s", surveyConfig)
+        if (type == Type.HIGH_RISK_ENCOUNTER && !surveyConfig.surveyOnHighRiskEnabled) {
+            // FIXME
+            throw IllegalArgumentException("not enabled")
+        }
+        otpRepo.otpAuthorizationResult?.apply {
+            if (authorized && redeemedAt.toDateTime().monthOfYear() == Instant.now().toDateTime().monthOfYear()) {
+                // FIXME
+                throw IllegalArgumentException("already did this year")
+            }
+        }
+        val otp = otpRepo.otp ?: otpRepo.generateOTP()
+        val errorCode = surveyServer.authOTP(
+            otp,
+            deviceAttestation.attest(object : DeviceAttestation.Request {
+                override val scenarioPayload: ByteArray
+                    get() = otp.payloadForRequest
+            })
+        ).errorCode
 
-        // TODO adjust for server com
-        // Just to have a glimpse at the loading spinner
-        delay(Seconds.THREE.toStandardDuration().millis)
+        val result = OTPAuthorizationResult(otp.uuid, errorCode == null, Instant.now())
+        otpRepo.otpAuthorizationResult = result
 
-        // TODO: generate and authenticate real otp
-        val otp = UUID.randomUUID()
-
-        return Survey(
-            type = Type.HIGH_RISK_ENCOUNTER,
-            surveyLink = urlProvider.provideUrl(type, otp)
-        )
+        if (result.authorized) {
+            return Survey(
+                type = Type.HIGH_RISK_ENCOUNTER,
+                surveyLink = urlProvider.provideUrl(type, otp.uuid)
+            )
+        } else {
+            //FIXME
+            throw IllegalArgumentException(errorCode)
+        }
     }
 
     suspend fun resetSurvey(type: Type) {
