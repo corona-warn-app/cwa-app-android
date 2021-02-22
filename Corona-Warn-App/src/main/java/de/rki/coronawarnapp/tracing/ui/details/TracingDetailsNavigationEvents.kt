9da8be3f23097a223f02a639793ece5b0686f4eb
Lukas Lechner <office@lukaslechner.com>
2021-02-22 15:39:36 +0100

    Skip consent screen on subsequent survey participates

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/TracingDetailsNavigationEvents.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/TracingDetailsNavigationEvents.kt
@@ -3,5 +3,6 @@ package de.rki.coronawarnapp.tracing.ui.details
 import de.rki.coronawarnapp.datadonation.survey.Surveys
 
 sealed class TracingDetailsNavigationEvents {
-    class NavigateToSurveyConsentFragment(val type: Surveys.Type) : TracingDetailsNavigationEvents()
+    data class NavigateToSurveyConsentFragment(val type: Surveys.Type) : TracingDetailsNavigationEvents()
+    data class NavigateToSurveyUrlInBrowser(val url: String) : TracingDetailsNavigationEvents()
 }
