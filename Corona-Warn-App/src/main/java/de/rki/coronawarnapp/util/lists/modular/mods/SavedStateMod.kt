97e4b27d918e91ca58659f7d27fe70f79b479077
Matthias Urhahn <matthias.urhahn@sap.com>
2021-02-23 14:15:01 +0100

    Fix comment field not saving on app moving into the background (EXPOSUREAPP-5108) #2424
    * Generalize the lifecycle callback extension on views and extract it.
    
    * Use `addOnWindowFocusChangeListener` to detect when the user puts the app into the background.
    
    Co-authored-by: Ralf Gehrer <ralfgehrer@users.noreply.github.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/lists/modular/mods/SavedStateMod.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/lists/modular/mods/SavedStateMod.kt
@@ -2,13 +2,10 @@ package de.rki.coronawarnapp.util.lists.modular.mods
 
 import android.os.Parcelable
 import androidx.annotation.Keep
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.findFragment
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 import androidx.recyclerview.widget.RecyclerView
 import de.rki.coronawarnapp.util.lists.modular.ModularAdapter
+import de.rki.coronawarnapp.util.ui.addLifecycleEventCallback
 import timber.log.Timber
 
 @Keep
@@ -27,37 +24,24 @@ class SavedStateMod<T : ModularAdapter.VH> :
     }
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
-        val hostLifecycle = recyclerView.hostLifecycle ?: return
+        recyclerView.addLifecycleEventCallback(type = Lifecycle.Event.ON_STOP) {
+            savedStates.clear()
 
-        val observer = object : LifecycleObserver {
-            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-            fun onStop() {
-                savedStates.clear()
-
-                getAllViewHolders(recyclerView).filterIsInstance<StateSavingVH>().forEach { vh ->
-                    val key = vh.savedStateKey
-                    val state = vh.onSaveState()
-                    if (key != null && state != null) {
-                        savedStates[key] = state
-                    }
+            getAllViewHolders(recyclerView).filterIsInstance<StateSavingVH>().forEach { vh ->
+                val key = vh.savedStateKey
+                val state = vh.onSaveState()
+                if (key != null && state != null) {
+                    savedStates[key] = state
                 }
-
-                hostLifecycle.removeObserver(this)
             }
+
+            true
         }
-        hostLifecycle.addObserver(observer)
     }
 
     override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
         // NOOP
     }
-
-    private val RecyclerView.hostLifecycle: Lifecycle?
-        get() = try {
-            findFragment<Fragment>().viewLifecycleOwner.lifecycle
-        } catch (e: Exception) {
-            null
-        }
 
     private fun getAllViewHolders(recyclerView: RecyclerView): List<RecyclerView.ViewHolder> = try {
         (0..recyclerView.childCount)
