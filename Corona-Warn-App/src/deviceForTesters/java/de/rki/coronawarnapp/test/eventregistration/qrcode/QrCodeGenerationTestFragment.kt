719b5ac605c2108e26f0dabc7f85719ee065d7cb
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-02-27 12:12:31 +0100

    Share internal file

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/qrcode/QrCodeGenerationTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/qrcode/QrCodeGenerationTestFragment.kt
@@ -1,12 +1,10 @@
 package de.rki.coronawarnapp.test.eventregistration.qrcode
 
-import android.graphics.pdf.PdfDocument
 import android.os.Bundle
-import android.os.Environment
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTestQrcodeGenerationBinding
 import de.rki.coronawarnapp.test.menu.ui.TestMenuItem
@@ -15,10 +13,6 @@ import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
-import kotlinx.coroutines.launch
-import timber.log.Timber
-import java.io.File
-import java.io.FileOutputStream
 import javax.inject.Inject
 
 class QrCodeGenerationTestFragment : Fragment(R.layout.fragment_test_qrcode_generation), AutoInject {
@@ -29,10 +23,9 @@ class QrCodeGenerationTestFragment : Fragment(R.layout.fragment_test_qrcode_gene
     private val binding: FragmentTestQrcodeGenerationBinding by viewBindingLazy()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.generateQrCode.setOnClickListener {
-            viewModel.generateQrCode(
-                getString(R.string.qr_code_input)
-            )
+
+        viewModel.intentLiveDate.observe2(this) {
+            startActivity(it.get(requireActivity()))
         }
 
         viewModel.bitmapLiveDate.observe2(this) {
@@ -40,24 +33,18 @@ class QrCodeGenerationTestFragment : Fragment(R.layout.fragment_test_qrcode_gene
             binding.printPDF.isVisible = it != null
         }
 
-        binding.printPDF.setOnClickListener { generatePDF() }
-    }
+        viewModel.errorLiveDate.observe2(this) {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+        }
 
-    private fun generatePDF() {
-        val pdfPage = binding.pdfPage
-        lifecycleScope.launch {
-            try {
-                val file = File(Environment.getExternalStorageDirectory().path + "/cwa_event.pdf")
-                val document = PdfDocument()
-                val pageInfo = PdfDocument.PageInfo.Builder(pdfPage.width, pdfPage.height, 1).create()
-                val page = document.startPage(pageInfo)
-                pdfPage.draw(page.canvas)
-                document.finishPage(page)
-                document.writeTo(FileOutputStream(file))
-                document.close()
-            } catch (e: Exception) {
-                Timber.d(e, "Generating PDF Failed")
-            }
+        binding.generateQrCode.setOnClickListener {
+            viewModel.generateQrCode(
+                getString(R.string.qr_code_input)
+            )
+        }
+
+        binding.printPDF.setOnClickListener {
+            viewModel.generatePDF(requireContext(), binding.pdfPage)
         }
     }
 
--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/qrcode/QrCodeGenerationTestFragmentViewModel.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/qrcode/QrCodeGenerationTestFragmentViewModel.kt
@@ -1,26 +1,37 @@
 package de.rki.coronawarnapp.test.eventregistration.qrcode
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color.BLACK
 import android.graphics.Color.WHITE
-import androidx.lifecycle.MutableLiveData
+import android.graphics.pdf.PdfDocument
+import android.view.View
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
-import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.ui.SingleLiveEvent
+import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import de.rki.coronawarnapp.util.sharing.FileSharing
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
 
-class QrCodeGenerationTestFragmentViewModel @AssistedInject constructor() : CWAViewModel() {
+class QrCodeGenerationTestFragmentViewModel @AssistedInject constructor(
+    private val dispatcher: DispatcherProvider,
+    private val fileSharing: FileSharing,
+) : CWAViewModel(dispatcher) {
 
-    val bitmapLiveDate = MutableLiveData<Bitmap>()
+    val bitmapLiveDate = SingleLiveEvent<Bitmap>()
+    val errorLiveDate = SingleLiveEvent<String>()
+    val intentLiveDate = SingleLiveEvent<FileSharing.ShareIntentProvider>()
 
-    fun generateQrCode(input: String) = launch {
+    fun generateQrCode(input: String) = launch(dispatcher.IO) {
         bitmapLiveDate.postValue(encodeAsBitmap(input))
     }
 
@@ -39,7 +50,38 @@ class QrCodeGenerationTestFragmentViewModel @AssistedInject constructor() : CWAV
             ).toBitmap()
         } catch (e: Exception) {
             Timber.d(e, "Qr Code generation failed")
+            errorLiveDate.postValue(e.localizedMessage ?: "Qr Code generation failed")
             null
+        }
+    }
+
+    fun generatePDF(
+        context: Context,
+        view: View
+    ) = launch(dispatcher.IO) {
+        try {
+            val dir = File(context.filesDir, "events")
+            if (!dir.exists()) {
+                dir.mkdirs()
+            }
+            val file = File(dir, "CoronaWarnApp-Event.pdf")
+            val document = PdfDocument()
+            val pageInfo = PdfDocument.PageInfo.Builder(
+                view.width,
+                view.height,
+                1
+            ).create()
+            val page = document.startPage(pageInfo)
+            view.draw(page.canvas)
+            document.finishPage(page)
+            document.writeTo(FileOutputStream(file))
+            document.close()
+            intentLiveDate.postValue(
+                fileSharing.getIntentProvider(file, "Scan and Share")
+            )
+        } catch (e: Exception) {
+            errorLiveDate.postValue(e.localizedMessage ?: "Generating PDF Failed")
+            Timber.d(e, "Generating PDF Failed")
         }
     }
 
