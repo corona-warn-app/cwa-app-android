984a6547fd6bbd05b9c4bfb891ca91b54a8cc5af
chilja <chiljamgossow@gmail.com>
2021-03-01 15:06:49 +0100

    add unit tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/analytics/modules/keysubmission/AnalyticsKeySubmissionCollectorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/analytics/modules/keysubmission/AnalyticsKeySubmissionCollectorTest.kt
@@ -1,3 +1,101 @@
 package de.rki.coronawarnapp.datadonation.analytics.modules.keysubmission
 
-class AnalyticsKeySubmissionCollectorTest
+import de.rki.coronawarnapp.datadonation.analytics.storage.AnalyticsSettings
+import de.rki.coronawarnapp.risk.RiskLevelResult
+import de.rki.coronawarnapp.risk.RiskLevelSettings
+import de.rki.coronawarnapp.risk.RiskState
+import de.rki.coronawarnapp.risk.storage.RiskLevelStorage
+import de.rki.coronawarnapp.util.TimeStamper
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.joda.time.Hours
+import org.joda.time.Instant
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+import testhelpers.preferences.mockFlowPreference
+
+class AnalyticsKeySubmissionCollectorTest : BaseTest() {
+
+    @MockK lateinit var timeStamper: TimeStamper
+    @MockK lateinit var analyticsSettings: AnalyticsSettings
+    @MockK lateinit var analyticsKeySubmissionStorage: AnalyticsKeySubmissionStorage
+    @MockK lateinit var riskLevelStorage: RiskLevelStorage
+    @MockK lateinit var riskLevelSettings: RiskLevelSettings
+    @MockK lateinit var riskLevelResult: RiskLevelResult
+
+    private val now = Instant.now()
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+        every { timeStamper.nowUTC } returns now
+    }
+
+    @Test
+    fun testReportTestRegistered() {
+        coEvery { analyticsSettings.analyticsEnabled.value } returns true
+
+        every { riskLevelResult.riskState } returns RiskState.INCREASED_RISK
+        coEvery {
+            riskLevelStorage
+                .latestAndLastSuccessful
+        } returns flowOf(listOf(riskLevelResult))
+        every { riskLevelSettings.lastChangeToHighRiskLevelTimestamp } returns now.minus(
+            Hours.hours(2).toStandardDuration()
+        )
+        val testRegisteredAt = mockFlowPreference(now.millis)
+        coEvery { analyticsKeySubmissionStorage.testRegisteredAt } returns testRegisteredAt
+        every { riskLevelResult.wasSuccessfullyCalculated } returns true
+        val riskLevelAtTestRegistration = mockFlowPreference(-1)
+        every { analyticsKeySubmissionStorage.riskLevelAtTestRegistration } returns riskLevelAtTestRegistration
+        val hoursSinceHighRiskWarningAtTestRegistration = mockFlowPreference(-1)
+        every { analyticsKeySubmissionStorage.hoursSinceHighRiskWarningAtTestRegistration } returns hoursSinceHighRiskWarningAtTestRegistration
+        runBlockingTest {
+            val collector = createInstance()
+            collector.reportTestRegistered()
+            verify { testRegisteredAt.update(any()) }
+            verify { riskLevelAtTestRegistration.update(any()) }
+            verify { hoursSinceHighRiskWarningAtTestRegistration.update(any()) }
+        }
+    }
+
+    @Test
+    fun testNoCollectionIfDisabled() {
+        coEvery { analyticsSettings.analyticsEnabled.value } returns false
+        runBlockingTest {
+            val collector = createInstance()
+            collector.reportTestRegistered()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.testRegisteredAt }
+            verify(exactly = 0) { analyticsKeySubmissionStorage.riskLevelAtTestRegistration }
+            verify(exactly = 0) { analyticsKeySubmissionStorage.hoursSinceHighRiskWarningAtTestRegistration }
+            collector.reportSubmitted()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.submitted }
+            collector.reportSubmittedInBackground()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.submittedInBackground }
+            collector.reportAdvancedConsentGiven()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.advancedConsentGiven }
+            collector.reportConsentWithdrawn()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.advancedConsentGiven }
+            collector.reportLastSubmissionFlowScreen(Screen.UNKNOWN)
+            verify(exactly = 0) { analyticsKeySubmissionStorage.lastSubmissionFlowScreen }
+            collector.reportPositiveTestResultReceived()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.testResultReceivedAt }
+            collector.reportSubmittedAfterCancel()
+            verify(exactly = 0) { analyticsKeySubmissionStorage.submittedAfterCancel }
+        }
+    }
+
+    fun createInstance() = AnalyticsKeySubmissionCollector(
+        timeStamper,
+        analyticsSettings,
+        analyticsKeySubmissionStorage,
+        riskLevelStorage,
+        riskLevelSettings
+    )
+}
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/analytics/modules/keysubmission/AnalyticsKeySubmissionDonorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/analytics/modules/keysubmission/AnalyticsKeySubmissionDonorTest.kt
@@ -1,3 +1,68 @@
 package de.rki.coronawarnapp.datadonation.analytics.modules.keysubmission
 
-class AnalyticsKeySubmissionDonorTest
+import de.rki.coronawarnapp.appconfig.ConfigData
+import de.rki.coronawarnapp.datadonation.analytics.modules.DonorModule
+import de.rki.coronawarnapp.util.TimeStamper
+import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runBlockingTest
+import org.joda.time.Duration
+import org.joda.time.Instant
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+
+class AnalyticsKeySubmissionDonorTest : BaseTest() {
+    @MockK lateinit var repository: AnalyticsKeySubmissionRepository
+    @MockK lateinit var timeStamper: TimeStamper
+    @MockK lateinit var configData: ConfigData
+    val request = object : DonorModule.Request {
+        override val currentConfig: ConfigData
+            get() = configData
+    }
+    private val now = Instant.now()
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+        every { timeStamper.nowUTC } returns now
+        every { configData.analytics.hoursSinceTestResultToSubmitKeySubmissionMetadata } returns 6
+    }
+
+    @Test
+    fun testNoContributionWithoutTestResult() {
+        every { repository.testResultReceivedAt } returns -1
+        every { repository.submitted } returns false
+        runBlockingTest {
+            val donor = createInstance()
+            donor.beginDonation(request) shouldBe AnalyticsKeySubmissionNoContribution
+        }
+    }
+
+    @Test
+    fun testNoContributionWhenNotSubmitted() {
+        every { repository.testResultReceivedAt } returns now.minus(Duration.standardHours(4)).millis
+        every { repository.submitted } returns false
+        runBlockingTest {
+            val donor = createInstance()
+            donor.beginDonation(request) shouldBe AnalyticsKeySubmissionNoContribution
+        }
+    }
+
+    @Test
+    fun testSubmitContributionAfterEnoughTimeHasPassed() {
+        every { repository.testResultReceivedAt } returns now.minus(Duration.standardHours(4)).millis
+        every { repository.submitted } returns true
+        every { repository.testResultReceivedAt } returns 1
+        val minTimePassedToSubmit = Duration.standardHours(3)
+        runBlockingTest {
+            val donor = createInstance()
+            donor.enoughTimeHasPassedSinceResult(Duration.standardHours(3)) shouldBe true
+            donor.shouldSubmitData(minTimePassedToSubmit) shouldBe true
+        }
+    }
+
+    fun createInstance() = AnalyticsKeySubmissionDonor(repository, timeStamper)
+}
