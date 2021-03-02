5c008d45ec1bd37f9430ef0bfeb4f84db976eaa3
Mohamed Metwalli <Mohamed.Metwalli@sap.com>
2021-03-02 13:43:42 +0100

    Create ViewModel

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/EventRegistrationUIModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/EventRegistrationUIModule.kt
@@ -2,6 +2,9 @@ package de.rki.coronawarnapp.ui.eventregistration
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import de.rki.coronawarnapp.ui.eventregistration.checkin.ConfirmCheckInFragment
+import de.rki.coronawarnapp.ui.eventregistration.checkin.ConfirmCheckInModule
+import de.rki.coronawarnapp.ui.eventregistration.checkin.ConfirmCheckInViewModel
 import de.rki.coronawarnapp.ui.eventregistration.scan.ScanCheckInQrCodeFragment
 import de.rki.coronawarnapp.ui.eventregistration.scan.ScanCheckInQrCodeModule
 
@@ -10,4 +13,7 @@ internal abstract class EventRegistrationUIModule {
 
     @ContributesAndroidInjector(modules = [ScanCheckInQrCodeModule::class])
     abstract fun scanCheckInQrCodeFragment(): ScanCheckInQrCodeFragment
+
+    @ContributesAndroidInjector(modules = [ConfirmCheckInModule::class])
+    abstract fun confirmCheckInFragment(): ConfirmCheckInFragment
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/checkin/ConfirmCheckInFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/checkin/ConfirmCheckInFragment.kt
@@ -1,4 +1,4 @@
-package de.rki.coronawarnapp.ui.eventregistration
+package de.rki.coronawarnapp.ui.eventregistration.checkin
 
 import android.os.Bundle
 import android.view.View
@@ -6,11 +6,17 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentConfrimCheckInBinding
-import de.rki.coronawarnapp.eventregistration.common.decodeBase32
-import de.rki.coronawarnapp.server.protocols.internal.evreg.EventOuterClass
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import javax.inject.Inject
 
-class ConfirmCheckInFragment : Fragment(R.layout.fragment_confrim_check_in) {
+class ConfirmCheckInFragment : Fragment(R.layout.fragment_confrim_check_in), AutoInject {
+
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val viewModel: ConfirmCheckInViewModel by cwaViewModels { viewModelFactory }
 
     private val binding: FragmentConfrimCheckInBinding by viewBindingLazy()
     private val args by navArgs<ConfirmCheckInFragmentArgs>()
@@ -18,19 +24,17 @@ class ConfirmCheckInFragment : Fragment(R.layout.fragment_confrim_check_in) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val event = args.event ?: return
-        val decodeBase32 = event.split(".")[0].decodeBase32()
-
-        val parsedEvent = EventOuterClass.Event.parseFrom(decodeBase32.toByteArray())
-
-        binding.encodedEvent.text = with(parsedEvent) {
-            """
+        viewModel.decodeEvent(args.encodedEvent)
+        viewModel.eventData.observe2(this) {
+            binding.encodedEvent.text = with(it) {
+                """
             guid=${String(guid.toByteArray())}
             desc=$description
             start=$start
             end=$end
             defaultCheckInLengthInMinutes=$defaultCheckInLengthInMinutes
             """.trimIndent()
+            }
         }
     }
 }
