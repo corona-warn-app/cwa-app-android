ebf95a33b0585635a6a576616a0bc8ef7df245f1
Lukas Lechner <office@lukaslechner.com>
2021-03-02 16:45:43 +0100

    Create empty fragments and implement navigation

---
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/CreateEventTestFragment.kt
@@ -0,0 +1,11 @@
+package de.rki.coronawarnapp.test.eventregistration.ui
+
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FragmentTestEventregistrationBinding
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
+
+class CreateEventTestFragment : Fragment(R.layout.fragment_test_createevent) {
+
+    private val binding: FragmentTestEventregistrationBinding by viewBindingLazy()
+}
---
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/ShowStoredEventsTestFragment.kt
@@ -0,0 +1,11 @@
+package de.rki.coronawarnapp.test.eventregistration.ui
+
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FragmentTestShowstoredeventsBinding
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
+
+class ShowStoredEventsTestFragment : Fragment(R.layout.fragment_test_showstoredevents) {
+
+    private val binding: FragmentTestShowstoredeventsBinding by viewBindingLazy()
+}
