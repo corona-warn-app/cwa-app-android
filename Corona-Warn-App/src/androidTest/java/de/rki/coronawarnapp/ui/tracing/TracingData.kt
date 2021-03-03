574610eb54284d256b06767660cfe401498e039c
chris-cwa <chris.cwa.sap@gmail.com>
2021-03-03 16:42:26 +0100

    fixed instrumented tests

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/tracing/TracingData.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/tracing/TracingData.kt
@@ -41,7 +41,6 @@ object TracingData {
             ),
 
             PeriodLoggedBox.Item(
-                activeTracingDaysInRetentionPeriod = 0,
                 tracingStatus = GeneralTracingStatus.Status.TRACING_INACTIVE
             ),
             DetailsLowRiskBox.Item(riskState = RiskState.LOW_RISK, matchedKeyCount = 0)
@@ -62,7 +61,6 @@ object TracingData {
                     lastExposureDetectionTime = Instant.now(),
                     allowManualUpdate = false,
                     daysWithEncounters = 0,
-                    activeTracingDays = 0,
                     lastEncounterAt = Instant.now()
                 )
             ),
@@ -72,7 +70,6 @@ object TracingData {
             ),
 
             PeriodLoggedBox.Item(
-                activeTracingDaysInRetentionPeriod = 0,
                 tracingStatus = GeneralTracingStatus.Status.TRACING_ACTIVE
             ),
             DetailsLowRiskBox.Item(riskState = RiskState.LOW_RISK, matchedKeyCount = 0)
@@ -93,13 +90,11 @@ object TracingData {
                     lastExposureDetectionTime = Instant.now(),
                     allowManualUpdate = false,
                     daysWithEncounters = 1,
-                    activeTracingDays = 5,
                     lastEncounterAt = Instant.now()
                 )
             ),
             BehaviorIncreasedRiskBox.Item,
             PeriodLoggedBox.Item(
-                activeTracingDaysInRetentionPeriod = 5,
                 tracingStatus = GeneralTracingStatus.Status.TRACING_ACTIVE
             ),
             DetailsIncreasedRiskBox.Item(
