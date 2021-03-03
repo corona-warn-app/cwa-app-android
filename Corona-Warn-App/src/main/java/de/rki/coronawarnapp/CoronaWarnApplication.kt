72ad146523aaa1957c2929cede41d67f1b15c1e6
Juraj Kusnier <jurajkusnier@gmail.com>
2021-03-03 11:39:51 +0100

    EncryptedPreferencesMigration skeleton class

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/CoronaWarnApplication.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/CoronaWarnApplication.kt
@@ -22,6 +22,7 @@ import de.rki.coronawarnapp.exception.reporting.ReportingConstants.ERROR_REPORT_
 import de.rki.coronawarnapp.notification.NotificationHelper
 import de.rki.coronawarnapp.risk.RiskLevelChangeDetector
 import de.rki.coronawarnapp.storage.LocalData
+import de.rki.coronawarnapp.storage.preferences.EncryptedPreferencesMigration
 import de.rki.coronawarnapp.submission.auto.AutoSubmission
 import de.rki.coronawarnapp.task.TaskController
 import de.rki.coronawarnapp.util.CWADebug
@@ -57,6 +58,7 @@ class CoronaWarnApplication : Application(), HasAndroidInjector {
     @Inject lateinit var notificationHelper: NotificationHelper
     @Inject lateinit var deviceTimeHandler: DeviceTimeHandler
     @Inject lateinit var autoSubmission: AutoSubmission
+    @Inject lateinit var encryptedPreferencesMigration: EncryptedPreferencesMigration
 
     @LogHistoryTree @Inject lateinit var rollingLogHistory: Timber.Tree
 
@@ -73,6 +75,8 @@ class CoronaWarnApplication : Application(), HasAndroidInjector {
         Timber.plant(rollingLogHistory)
 
         Timber.v("onCreate(): WorkManager setup done: $workManager")
+
+        encryptedPreferencesMigration.doMigration()
 
         notificationHelper.createNotificationChannel()
 
---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/storage/preferences/EncryptedPreferencesMigration.kt
@@ -0,0 +1,77 @@
+package de.rki.coronawarnapp.storage.preferences
+
+import android.content.SharedPreferences
+import de.rki.coronawarnapp.storage.EncryptedPreferences
+import timber.log.Timber
+import javax.inject.Inject
+
+class EncryptedPreferencesMigration @Inject constructor(
+    private val encryptedPreferencesHelper: EncryptedPreferencesHelper,
+    @EncryptedPreferences private val encryptedSharedPreferences: SharedPreferences
+) {
+
+    fun doMigration() {
+        try {
+            copyData()
+        } catch (e: Exception) {
+            Timber.e(e, "Migration was not successful")
+        }
+    }
+
+    private fun copyData() {
+        Timber.d("EncryptedPreferencesMigration START")
+        if (encryptedPreferencesHelper.isAvailable()) {
+            Timber.d("EncryptedPreferences are available")
+            SettingsLocalData(encryptedSharedPreferences).apply {
+                // TODO: setting migration
+            }
+
+            OnboardingLocalData(encryptedSharedPreferences).apply {
+                // TODO: onboarding migration
+            }
+
+            TracingLocalData(encryptedSharedPreferences).apply {
+                // TODO: tracing migration
+            }
+
+            val submissionLocalData = SubmissionLocalData(encryptedSharedPreferences).apply {
+                // TODO: submission migration
+            }
+
+            encryptedPreferencesHelper.clean()
+        }
+        Timber.d("EncryptedPreferencesMigration END")
+    }
+
+    // Moving LocalData methods here to prevent accidental misuse.
+    // TODO: delete or update LocalData, SecurityConstants, SecurityHelper after all methods are migrated
+    private class SettingsLocalData(private val sharedPreferences: SharedPreferences) {
+
+        // TODO: remove methods from LocalData object
+        fun isNotificationsRiskEnabled(): Boolean = sharedPreferences.getBoolean(PKEY_NOTIFICATIONS_RISK_ENABLED, true)
+
+        fun isNotificationsTestEnabled(): Boolean = sharedPreferences.getBoolean(PKEY_NOTIFICATIONS_TEST_ENABLED, true)
+
+        fun numberOfRemainingSharePositiveTestResultReminders(): Int =
+            sharedPreferences.getInt(PKEY_POSITIVE_TEST_RESULT_REMINDER_COUNT, Int.MIN_VALUE)
+
+        companion object {
+            private const val PKEY_NOTIFICATIONS_RISK_ENABLED = "preference_notifications_risk_enabled"
+            private const val PKEY_NOTIFICATIONS_TEST_ENABLED = "preference_notifications_test_enabled"
+            private const val PKEY_POSITIVE_TEST_RESULT_REMINDER_COUNT =
+                "preference_positive_test_result_reminder_count"
+        }
+    }
+
+    private class OnboardingLocalData(private val sharedPreferences: SharedPreferences) {
+        // TODO:
+    }
+
+    private class TracingLocalData(private val sharedPreferences: SharedPreferences) {
+        // TODO:
+    }
+
+    private class SubmissionLocalData(private val sharedPreferences: SharedPreferences) {
+        // TODO:
+    }
+}
