14fd131adc6b21126d7f2cb076e42628dc019acd
Mohamed <mohamed.metwalli@sap.com>
2021-03-04 17:49:12 +0100

    QR code and  pdf creation (EXPOSUREAPP-5430) (#2490)
    * Generate QR-Code PDF
    
    * lint
    
    * Update QrCodeGenerationTestFragmentViewModel.kt
    
    * Share internal file
    
    * Move qr code under event registration
    
    * Reduce size
    
    * Refactoring
    
    * Remove padding
    
    * Fix unresolved reference
    
    * Lint
    
    * Increase qr-code quality
    
    * Merge two actions in Event main screen
    
    * klint
    
    * Adjust sizes

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/EventRegistrationTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/EventRegistrationTestFragment.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTestEventregistrationBinding
 import de.rki.coronawarnapp.test.menu.ui.TestMenuItem
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.doNavigate
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
@@ -18,13 +18,25 @@ import javax.inject.Inject
 class EventRegistrationTestFragment : Fragment(R.layout.fragment_test_eventregistration), AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
-    private val vm: EventRegistrationTestFragmentViewModel by cwaViewModels { viewModelFactory }
+    private val viewModel: EventRegistrationTestFragmentViewModel by cwaViewModels { viewModelFactory }
 
     private val binding: FragmentTestEventregistrationBinding by viewBindingLazy()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.scanCheckInQrCode.setOnClickListener {
-            findNavController().navigate(R.id.scanCheckInQrCodeFragment)
+        with(binding) {
+            scanCheckInQrCode.setOnClickListener {
+                doNavigate(
+                    EventRegistrationTestFragmentDirections
+                        .actionEventRegistrationTestFragmentToScanCheckInQrCodeFragment()
+                )
+            }
+
+            testQrCodeCreation.setOnClickListener {
+                doNavigate(
+                    EventRegistrationTestFragmentDirections
+                        .actionEventRegistrationTestFragmentToTestQrCodeCreationFragment()
+                )
+            }
         }
     }
 
