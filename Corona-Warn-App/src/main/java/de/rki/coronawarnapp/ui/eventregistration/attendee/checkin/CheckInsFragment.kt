1067a026005f9a5274abf6fb068ef62e6d7d821b
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-04 23:10:18 +0100

    Connect scan fragment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/attendee/checkin/CheckInsFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/attendee/checkin/CheckInsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FragmentCheckInsBinding
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.doNavigate
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
 import javax.inject.Inject
@@ -13,8 +16,15 @@ class CheckInsFragment : Fragment(R.layout.fragment_check_ins), AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
     private val viewModel: CheckInsViewModel by cwaViewModels { viewModelFactory }
+    private val binding: FragmentCheckInsBinding by viewBindingLazy()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.scanCheckinQrcodeFab.setOnClickListener {
+            doNavigate(
+                CheckInsFragmentDirections.actionCheckInsFragmentToScanCheckInQrCodeFragment()
+            )
+        }
     }
 }
