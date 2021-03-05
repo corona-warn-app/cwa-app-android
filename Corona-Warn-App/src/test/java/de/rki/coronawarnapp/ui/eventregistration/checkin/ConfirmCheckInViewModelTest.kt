8b88e6ce2c7558f77f70b161dd15f910761cd1d4
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-05 18:28:24 +0100

    Update ConfirmCheckInViewModelTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/eventregistration/checkin/ConfirmCheckInViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/eventregistration/checkin/ConfirmCheckInViewModelTest.kt
@@ -1,8 +1,15 @@
 package de.rki.coronawarnapp.ui.eventregistration.checkin
 
 import de.rki.coronawarnapp.eventregistration.checkins.qrcode.EventQRCode
+import de.rki.coronawarnapp.eventregistration.checkins.qrcode.QRCodeVerifier
+import de.rki.coronawarnapp.server.protocols.internal.evreg.EventOuterClass
 import io.kotest.matchers.shouldBe
-import org.joda.time.Instant
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -16,23 +23,30 @@ class ConfirmCheckInViewModelTest : BaseTest() {
 
     private lateinit var viewModel: ConfirmCheckInViewModel
 
+    @MockK lateinit var qrCodeVerifier: QRCodeVerifier
+
     @BeforeEach
     fun setUp() {
-        viewModel = ConfirmCheckInViewModel()
+        MockKAnnotations.init(this)
+        coEvery { qrCodeVerifier.verify(any()) } returns object : EventQRCode {
+            override val event: EventOuterClass.Event
+                get() = mockk<EventOuterClass.Event>().apply {
+                    every { description } returns "CWA Event"
+                }
+        }
+
+        viewModel = ConfirmCheckInViewModel(qrCodeVerifier)
     }
 
     @Test
     fun decodeEvent() {
         val decodedEvent =
-            "BIPEY33SMVWSA2LQON2W2IDEN5WG64RAONUXIIDBNVSXILBAMNXRBCM4UQARRKM6UQASAHRKCC7CTDWGQ4JCO7RVZSWVIMQK4UPA" +
-                ".GBCAEIA7TEORBTUA25QHBOCWT26BCA5PORBS2E4FFWMJ3UU3P6SXOL7SHUBCA7UEZBDDQ2R6VRJH7WBJKVF7GZYJA6YMRN27IPEP7NKGGJSWX3XQ"
+            "BIYAUEDBZY6EIWF7QX6JOKSRPAGEB3H7CIIEGV2BEBG"
         viewModel.decodeEvent(decodedEvent)
-        viewModel.eventData.getOrAwaitValue() shouldBe EventQRCode(
-            guid = "Lorem ipsum dolor sit amet, co",
-            description = "",
-            start = Instant.parse("1970-01-01T00:44:50.857Z"),
-            end = Instant.parse("1970-01-01T00:00:00.030Z")
-        )
+        viewModel.eventData.getOrAwaitValue().apply {
+            event.description shouldBe "CWA Event"
+        }
+        coVerify { qrCodeVerifier.verify(any()) }
     }
 
     @Test
