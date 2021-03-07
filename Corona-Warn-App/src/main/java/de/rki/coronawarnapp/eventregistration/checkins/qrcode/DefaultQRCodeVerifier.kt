df7857ecbc50b4b86c99117e847b4836f7f255ed
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-07 15:52:00 +0100

    Parse signed event

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/qrcode/DefaultQRCodeVerifier.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/qrcode/DefaultQRCodeVerifier.kt
@@ -2,14 +2,17 @@ package de.rki.coronawarnapp.eventregistration.checkins.qrcode
 
 import de.rki.coronawarnapp.eventregistration.common.decodeBase32
 import de.rki.coronawarnapp.server.protocols.internal.evreg.EventOuterClass
+import de.rki.coronawarnapp.server.protocols.internal.evreg.SignedEventOuterClass
+import timber.log.Timber
 import javax.inject.Inject
 
 class DefaultQRCodeVerifier @Inject constructor() : QRCodeVerifier {
     override suspend fun verify(encodedEvent: String): EventQRCode {
+        Timber.i("encodedEvent: $encodedEvent")
         // TODO Implement verification
         //  For now just parse
         return EventQRCode(
-            event = EventOuterClass.Event.parseFrom(encodedEvent.decodeBase32().toByteArray())
+            event = SignedEventOuterClass.SignedEvent.parseFrom(encodedEvent.decodeBase32().toByteArray()).event
         )
     }
 }
