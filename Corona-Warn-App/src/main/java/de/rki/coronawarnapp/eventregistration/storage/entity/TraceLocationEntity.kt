40bfcf5468917d6fa090d33b849c6dc50d99b98a
Lukas Lechner <office@lukaslechner.com>
2021-03-08 16:03:54 +0100

    Remove parcelable from TraceLocationEntity.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationEntity.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationEntity.kt
@@ -1,28 +1,25 @@
 package de.rki.coronawarnapp.eventregistration.storage.entity
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import de.rki.coronawarnapp.eventregistration.events.TraceLocation
-import kotlinx.parcelize.Parcelize
 import org.joda.time.Instant
 
-@Parcelize
 @Entity(tableName = "traceLocations")
 data class TraceLocationEntity(
 
-    @PrimaryKey @ColumnInfo(name = "guid") override val guid: String,
-    @ColumnInfo(name = "version") override val version: Int,
-    @ColumnInfo(name = "type") override val type: TraceLocation.Type,
-    @ColumnInfo(name = "description") override val description: String,
-    @ColumnInfo(name = "location") override val address: String,
-    @ColumnInfo(name = "startTime") override val startDate: Instant?,
-    @ColumnInfo(name = "endTime") override val endDate: Instant?,
-    @ColumnInfo(name = "defaultCheckInLength") override val defaultCheckInLengthInMinutes: Int?,
-    @ColumnInfo(name = "signature") override val signature: String
+    @PrimaryKey @ColumnInfo(name = "guid") val guid: String,
+    @ColumnInfo(name = "version") val version: Int,
+    @ColumnInfo(name = "type") val type: TraceLocation.Type,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "location") val address: String,
+    @ColumnInfo(name = "startTime") val startDate: Instant?,
+    @ColumnInfo(name = "endTime") val endDate: Instant?,
+    @ColumnInfo(name = "defaultCheckInLength") val defaultCheckInLengthInMinutes: Int?,
+    @ColumnInfo(name = "signature") val signature: String
 
-) : TraceLocation, Parcelable
+)
 
 fun TraceLocation.toTraceLocationEntity(): TraceLocationEntity =
     TraceLocationEntity(
