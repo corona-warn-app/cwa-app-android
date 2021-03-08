1ef4dacf4d31449aa1af72ab7918103211e5c550
chris-cwa <chris.cwa.sap@gmail.com>
2021-03-08 13:07:17 +0100

    added more crud methods

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/storage/dao/EventCheckInDao.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/storage/dao/EventCheckInDao.kt
@@ -1,7 +1,9 @@
 package de.rki.coronawarnapp.eventregistration.checkins.storage.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import de.rki.coronawarnapp.contactdiary.storage.dao.BaseRoomDao
 import de.rki.coronawarnapp.eventregistration.checkins.storage.entity.EventCheckInEntity
 import kotlinx.coroutines.flow.Flow
@@ -11,6 +13,12 @@ abstract class EventCheckInDao : BaseRoomDao<EventCheckInEntity, EventCheckInEnt
 
     @Query("SELECT * FROM checkin")
     abstract override fun allEntries(): Flow<List<EventCheckInEntity>>
+
+    @Insert
+    abstract override suspend fun insert(entity: EventCheckInEntity): Long
+
+    @Update
+    abstract override suspend fun update(entity: EventCheckInEntity)
 
     @Query("DELETE FROM checkin")
     abstract override suspend fun deleteAll()
