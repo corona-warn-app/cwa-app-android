fc496f65debe6fdcee034a87df06185f6d9a1446
Lukas Lechner <office@lukaslechner.com>
2021-03-08 09:17:18 +0100

    Make event list more pretty

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/showevents/ShowStoredEventsTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/showevents/ShowStoredEventsTestFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTestShowstoredeventsBinding
+import de.rki.coronawarnapp.eventregistration.events.HostedEvent
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
@@ -22,8 +23,20 @@ class ShowStoredEventsTestFragment : Fragment(R.layout.fragment_test_showstorede
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        vm.storedEvents.observe2(this) {
-            binding.storedEvents.text = it.toString()
+        vm.storedEvents.observe2(this) { events ->
+            binding.storedEvents.text = events.joinToString(separator = "\n\n") { it.getSimpleUIString() }
         }
+    }
+
+    private fun HostedEvent.getSimpleUIString(): String {
+        return listOf(
+            "guid = $guid",
+            "description = $description",
+            "location = $location",
+            "startTime = $startTime",
+            "endTime = $endTime",
+            "defaultCheckInLengthInMinutes = $defaultCheckInLengthInMinutes",
+            "signature = $signature"
+        ).joinToString(separator = "\n")
     }
 }
