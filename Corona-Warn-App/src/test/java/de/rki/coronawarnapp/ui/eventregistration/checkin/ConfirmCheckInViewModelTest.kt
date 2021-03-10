c5268000774341ba1ebf0284188346554257da2c
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-10 15:01:00 +0100

    Update ConfirmCheckInViewModelTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/eventregistration/checkin/ConfirmCheckInViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/eventregistration/checkin/ConfirmCheckInViewModelTest.kt
@@ -1,16 +1,9 @@
 package de.rki.coronawarnapp.ui.eventregistration.checkin
 
 import de.rki.coronawarnapp.eventregistration.checkins.qrcode.QRCodeVerifier
-import de.rki.coronawarnapp.eventregistration.checkins.qrcode.QRCodeVerifyResult
-import de.rki.coronawarnapp.server.protocols.internal.evreg.EventOuterClass
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.MockKAnnotations
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -29,26 +22,7 @@ class ConfirmCheckInViewModelTest : BaseTest() {
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
-        coEvery { qrCodeVerifier.verify(any()) } returns QRCodeVerifyResult.Success(
-            mockk<EventOuterClass.Event>().apply {
-                every { description } returns "CWA Event"
-            }
-        )
-
         viewModel = ConfirmCheckInViewModel(qrCodeVerifier)
-    }
-
-    @Test
-    fun decodeEvent() {
-        val decodedEvent =
-            "BIYAUEDBZY6EIWF7QX6JOKSRPAGEB3H7CIIEGV2BEBG"
-        viewModel.decodeEvent(decodedEvent)
-        val verifyResult = viewModel.verifyResult.getOrAwaitValue()
-        verifyResult.shouldBeInstanceOf<QRCodeVerifyResult.Success>()
-        verifyResult.apply {
-            event.description shouldBe "CWA Event"
-        }
-        coVerify { qrCodeVerifier.verify(any()) }
     }
 
     @Test
