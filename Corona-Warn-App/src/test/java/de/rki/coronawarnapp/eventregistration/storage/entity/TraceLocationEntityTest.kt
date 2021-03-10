f2047206ec03628e27478fdf3f606fde753eab03
Lukas Lechner <office@lukaslechner.com>
2021-03-10 17:10:50 +0100

    Add unit tests for TraceLocationEntity mapping

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationEntityTest.kt
@@ -0,0 +1,60 @@
+package de.rki.coronawarnapp.eventregistration.storage.entity
+
+import de.rki.coronawarnapp.eventregistration.events.DefaultTraceLocation
+import de.rki.coronawarnapp.eventregistration.events.TraceLocation
+import io.kotest.matchers.shouldBe
+import org.joda.time.Instant
+import org.junit.jupiter.api.Test
+
+internal class TraceLocationEntityTest {
+
+    @Test
+    fun `toTraceLocationEntity() should map to TraceLocationEntity correctly with all arguments`() {
+        DefaultTraceLocation(
+            guid = "TestGuid",
+            version = 1,
+            type = TraceLocation.Type.TEMPORARY_OTHER,
+            description = "TestTraceLocation",
+            address = "TestTraceLocationAddress",
+            startDate = Instant.parse("2021-01-01T12:00:00.000Z"),
+            endDate = Instant.parse("2021-01-01T18:00:00.000Z"),
+            defaultCheckInLengthInMinutes = 15,
+            signature = "signature"
+        ).toTraceLocationEntity() shouldBe TraceLocationEntity(
+            guid = "TestGuid",
+            version = 1,
+            type = TraceLocation.Type.TEMPORARY_OTHER,
+            description = "TestTraceLocation",
+            address = "TestTraceLocationAddress",
+            startDate = Instant.parse("2021-01-01T12:00:00.000Z"),
+            endDate = Instant.parse("2021-01-01T18:00:00.000Z"),
+            defaultCheckInLengthInMinutes = 15,
+            signature = "signature"
+        )
+    }
+
+    @Test
+    fun `toTraceLocationEntity() should map to TraceLocationEntity correctly with some arguments as null`() {
+        DefaultTraceLocation(
+            guid = "TestGuid",
+            version = 1,
+            type = TraceLocation.Type.PERMANENT_OTHER,
+            description = "TestTraceLocation",
+            address = "TestTraceLocationAddress",
+            startDate = null,
+            endDate = null,
+            defaultCheckInLengthInMinutes = null,
+            signature = "signature"
+        ).toTraceLocationEntity() shouldBe TraceLocationEntity(
+            guid = "TestGuid",
+            version = 1,
+            type = TraceLocation.Type.PERMANENT_OTHER,
+            description = "TestTraceLocation",
+            address = "TestTraceLocationAddress",
+            startDate = null,
+            endDate = null,
+            defaultCheckInLengthInMinutes = null,
+            signature = "signature"
+        )
+    }
+}
