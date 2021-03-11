daa6c773fbc73b4f11c9532e8ba4f82da263cb19
Lukas Lechner <lukas.lechner@sap.com>
2021-03-11 19:31:33 +0100

    Create events repository (EXPOSUREAPP-5470) (EXPOSUREAPP-5482)(EXPOSUREAPP-5484) (#2549)
    * Add events card in event registration test fragment
    
    * Format
    
    * Create empty fragments and implement navigation
    
    * Create layout for CreateEventTestFragment.kt
    
    * Add spinner for event type
    
    * Build event object from input
    
    * Create database for event registration and store events in it
    
    * Fix date issue
    
    * Show stored events in a simple way
    
    * Make event list more pretty
    
    * Add button to delete all events in ShowStoredEventsTestFragment.kt
    
    * Change the functions of HostedEventRepository.kt to regular functions instead of suspend functions
    
    * Change text in EventRegistration TestFragment
    
    * Fix merge issues
    
    * Add parameters from new TechSpec
    
    * Change wording from 'hosted Events' to 'TraceLocation'
    
    * Delete event registration database on app reset
    
    * Fix detect issue 'MaxLineLength'
    
    * Remove parcelable from TraceLocationEntity.kt
    
    * Map TraceLocationEntity to TraceLocation in TraceLocationRepository
    
    * Use the Instant.seconds extension function
    
    * Use existing EventRegistrationModule.kt instead of creating a new one
    
    * Only delete traceLocations on App reset instead of all tables in TraceLocationRepository.kt
    
    * Add named parameters to TraceLocation mapping
    
    * Add unit test for TraceLocation Mapper
    
    * Add unit tests for EventRegistrationConverters
    
    * Add unit tests for TraceLocationEntity mapping
    
    * Add events database table to 'TraceLocation' database and remove 'EventRegistration' database
    
    * Add database tests for TraceLocations
    
    * Fix issue in EventRegistrationModule.kt
    
    * Add database tests for checkInDao
    
    * Fix detekt maxLineLength issue
    
    * Fix inconsistency in TraceLocationEntity.kt
    
    * Fix ktlint issues
    
    * Inherit from BaseTest or BaseTestInstrumentation in tests
    
    * Fix issue in TraceLocationConverters.kt

---
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/showevents/ShowStoredEventsTestFragment.kt
@@ -0,0 +1,48 @@
+package de.rki.coronawarnapp.test.eventregistration.ui.showevents
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FragmentTestShowstoredeventsBinding
+import de.rki.coronawarnapp.eventregistration.events.TraceLocation
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.observe2
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import javax.inject.Inject
+
+class ShowStoredEventsTestFragment : Fragment(R.layout.fragment_test_showstoredevents), AutoInject {
+
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val vm: ShowStoredEventsTestViewModel by cwaViewModels { viewModelFactory }
+
+    private val binding: FragmentTestShowstoredeventsBinding by viewBindingLazy()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vm.storedEvents.observe2(this) { events ->
+            binding.storedEvents.text = events.joinToString(separator = "\n\n") { it.getSimpleUIString() }
+        }
+
+        binding.deleteAllEvents.setOnClickListener {
+            vm.deleteAllEvents()
+        }
+    }
+
+    private fun TraceLocation.getSimpleUIString(): String {
+        return listOf(
+            "guid = $guid",
+            "type = $type",
+            "description = $description",
+            "location = $address",
+            "startTime = $startDate",
+            "endTime = $endDate",
+            "defaultCheckInLengthInMinutes = $defaultCheckInLengthInMinutes",
+            "signature = $signature",
+            "version = $version"
+        ).joinToString(separator = "\n")
+    }
+}
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/EventRegistrationModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/EventRegistrationModule.kt
@@ -4,9 +4,16 @@ import dagger.Binds
 import dagger.Module
 import de.rki.coronawarnapp.eventregistration.checkins.qrcode.DefaultQRCodeVerifier
 import de.rki.coronawarnapp.eventregistration.checkins.qrcode.QRCodeVerifier
+import de.rki.coronawarnapp.eventregistration.storage.repo.DefaultTraceLocationRepository
+import de.rki.coronawarnapp.eventregistration.storage.repo.TraceLocationRepository
 
 @Module
 abstract class EventRegistrationModule {
+
     @Binds
     abstract fun qrCodeVerifier(qrCodeVerifier: DefaultQRCodeVerifier): QRCodeVerifier
+
+    @Binds
+    abstract fun traceLocationRepository(defaultTraceLocationRepo: DefaultTraceLocationRepository):
+        TraceLocationRepository
 }
