13a58b299ffca7ecc2158de96056e651f4bb2f45
Mohamed <mohamed.metwalli@sap.com>
2021-03-11 18:39:42 +0100

    Printing option (EXPOSUREAPP-5675) (#2574)
    * Create PrintingAdapter.kt
    
    * Update PrintingAdapter.kt
    
    * Test printing
    
    * Update QrCodeCreationTestViewModel.kt
    
    * Remove Charset causing crash only older versions
    
    * Log
    
    * Add logs and specify media size

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/qrcode/QrCodeCreationTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/qrcode/QrCodeCreationTestFragment.kt
@@ -2,17 +2,22 @@ package de.rki.coronawarnapp.test.eventregistration.ui.qrcode
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintManager
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.getSystemService
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTestQrcodeCreationBinding
+import de.rki.coronawarnapp.test.eventregistration.ui.PrintingAdapter
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import timber.log.Timber
 import javax.inject.Inject
 
 class QrCodeCreationTestFragment : Fragment(R.layout.fragment_test_qrcode_creation), AutoInject {
@@ -25,13 +30,42 @@ class QrCodeCreationTestFragment : Fragment(R.layout.fragment_test_qrcode_creati
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        viewModel.sharingIntent.observe2(this) {
-            startActivity(it.get(requireActivity()))
+        viewModel.sharingIntent.observe2(this) { fileIntent ->
+
+            binding.printPDF.isVisible = true
+            binding.printPDF.setOnClickListener {
+                // Context must be an Activity context
+                val printingManger = context?.getSystemService<PrintManager>()
+                Timber.i("PrintingManager: $printingManger")
+                printingManger?.apply {
+                    val printingJob = print(
+                        "CoronaWarnApp",
+                        PrintingAdapter(fileIntent.file),
+                        PrintAttributes
+                            .Builder()
+                            .setMediaSize(PrintAttributes.MediaSize.ISO_A3)
+                            .build()
+                    )
+
+                    Timber.i("PrintingJob:$printingJob")
+                    Timber.i("PrintingJob isBlocked:${printingJob.isBlocked}")
+                    Timber.i("PrintingJob isCancelled:${printingJob.isCancelled}")
+                    Timber.i("PrintingJob isCompleted:${printingJob.isCompleted}")
+                    Timber.i("PrintingJob isFailed:${printingJob.isFailed}")
+                    Timber.i("PrintingJob info:${printingJob.info}")
+                }
+            }
+            binding.sharePDF.isVisible = true
+            binding.sharePDF.setOnClickListener {
+                startActivity(fileIntent.intent(requireActivity()))
+            }
         }
 
         viewModel.qrCodeBitmap.observe2(this) {
             binding.qrCodeImage.setImageBitmap(it)
-            binding.printPDF.isVisible = it != null
+            if (it != null) {
+                viewModel.createPDF(binding.pdfPage)
+            }
         }
 
         viewModel.errorMessage.observe2(this) {
@@ -45,10 +79,6 @@ class QrCodeCreationTestFragment : Fragment(R.layout.fragment_test_qrcode_creati
         )
         binding.generateQrCode.setOnClickListener {
             viewModel.createQrCode(binding.qrCodeText.text.toString())
-        }
-
-        binding.printPDF.setOnClickListener {
-            viewModel.createPDF(binding.pdfPage)
         }
     }
 }
