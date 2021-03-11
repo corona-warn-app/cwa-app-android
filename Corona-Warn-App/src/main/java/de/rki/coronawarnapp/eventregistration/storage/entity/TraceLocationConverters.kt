307daa65c3666cec67009821fc918e8ad158a106
Lukas Lechner <office@lukaslechner.com>
2021-03-11 18:11:56 +0100

    Fix issue in TraceLocationConverters.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationConverters.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationConverters.kt
@@ -6,7 +6,7 @@ import de.rki.coronawarnapp.eventregistration.events.TraceLocation
 class TraceLocationConverters {
 
     @TypeConverter
-    fun toTraceLocationType(value: Int) = enumValues<TraceLocation.Type>()[value]
+    fun toTraceLocationType(value: Int) = enumValues<TraceLocation.Type>().single { it.value == value }
 
     @TypeConverter
     fun fromTraceLocationType(type: TraceLocation.Type) = type.value
