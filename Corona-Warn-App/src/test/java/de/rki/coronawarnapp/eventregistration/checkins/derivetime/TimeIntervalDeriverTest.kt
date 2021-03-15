9331a8488d0c5c8d8ae3409dc8a5728b25fbee5c
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-15 12:10:51 +0100

    Add test class

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/eventregistration/checkins/derivetime/TimeIntervalDeriverTest.kt
@@ -0,0 +1,23 @@
+package de.rki.coronawarnapp.eventregistration.checkins.derivetime
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import testhelpers.BaseTest
+
+internal class TimeIntervalDeriverTest : BaseTest() {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun deriveTime() {
+    }
+}
