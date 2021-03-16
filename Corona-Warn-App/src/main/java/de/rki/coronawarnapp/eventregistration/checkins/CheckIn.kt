21f1624729755825a2a64a0ffd4eff65940b1e73
chris-cwa <chris.cwa.sap@gmail.com>
2021-03-16 09:12:48 +0100

    - unnecessary equals override

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/CheckIn.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/CheckIn.kt
@@ -18,29 +18,4 @@ data class CheckIn(
     val checkInEnd: Instant?,
     val targetCheckInEnd: Instant?,
     val createJournalEntry: Boolean
-) {
-    @Suppress("ComplexMethod")
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CheckIn
-
-        if (id != other.id) return false
-        if (guid != other.guid) return false
-        if (version != other.version) return false
-        if (type != other.type) return false
-        if (description != other.description) return false
-        if (address != other.address) return false
-        if (traceLocationStart != other.traceLocationStart) return false
-        if (traceLocationEnd != other.traceLocationEnd) return false
-        if (defaultCheckInLengthInMinutes != other.defaultCheckInLengthInMinutes) return false
-        if (signature != other.signature) return false
-        if (checkInStart != other.checkInStart) return false
-        if (checkInEnd != other.checkInEnd) return false
-        if (targetCheckInEnd != other.targetCheckInEnd) return false
-        if (createJournalEntry != other.createJournalEntry) return false
-
-        return true
-    }
-}
+)
