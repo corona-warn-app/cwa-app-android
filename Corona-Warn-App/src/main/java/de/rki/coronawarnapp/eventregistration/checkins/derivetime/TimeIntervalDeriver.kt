c26696e7da60a1e24f27629f01a53127504e3d51
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-16 12:50:01 +0100

    Use common in Range - specify the timestamp in seconds

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/derivetime/TimeIntervalDeriver.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/derivetime/TimeIntervalDeriver.kt
@@ -1,7 +1,7 @@
 package de.rki.coronawarnapp.eventregistration.checkins.derivetime
 
 import de.rki.coronawarnapp.appconfig.PresenceTracingSubmissionParamContainer
-import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass.Range
+import de.rki.coronawarnapp.risk.DefaultRiskLevels.Companion.inRange
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import kotlin.math.max
@@ -12,32 +12,16 @@ private val INTERVAL_LENGTH_IN_SECONDS = TimeUnit.MINUTES.toSeconds(10L)
 private fun alignToInterval(timestamp: Long) =
     (timestamp / INTERVAL_LENGTH_IN_SECONDS) * INTERVAL_LENGTH_IN_SECONDS
 
-private fun Range.inRange(value: Long): Boolean {
-    val inRage = when {
-        minExclusive && value <= min -> false
-        !minExclusive && value < min -> false
-        maxExclusive && value >= max -> false
-        !maxExclusive && value > max -> false
-        else -> true
-    }
-
-    Timber.d(
-        "Value:%s isInRange:%s - Range{min:%s,max:%s,minExclusive:%s,maxExclusive:%s}",
-        value,
-        inRage,
-        min,
-        max,
-        minExclusive,
-        maxExclusive
-    )
-    return inRage
-}
-
+/**
+ * Derive CheckIn start and end times
+ * @param startTimestampInSeconds [Long] timestamp in seconds
+ * @param endTimestampInSeconds [Long] timestamp in seconds
+ */
 fun PresenceTracingSubmissionParamContainer.deriveTime(
-    startTimestamp: Long,
-    endTimestamp: Long
+    startTimestampInSeconds: Long,
+    endTimestampInSeconds: Long
 ): Pair<Long, Long>? {
-    val durationInSeconds = max(0, endTimestamp - startTimestamp)
+    val durationInSeconds = max(0, endTimestampInSeconds - startTimestampInSeconds)
     Timber.d("durationInSeconds: $durationInSeconds")
 
     val durationInMinutes = TimeUnit.SECONDS.toMinutes(durationInSeconds)
@@ -58,10 +42,10 @@ fun PresenceTracingSubmissionParamContainer.deriveTime(
     val aerosoleDecayInSeconds = aerosoleDecays.firstOrNull() ?: 0.0 // Default: zero, i.e. 'no decay'
     Timber.d("aerosoleDecayInSeconds: $aerosoleDecayInSeconds")
 
-    val relevantEndTimestamp = endTimestamp + aerosoleDecayInSeconds.toLong()
-    val relevantStartIntervalTimestamp = alignToInterval(startTimestamp)
+    val relevantEndTimestamp = endTimestampInSeconds + aerosoleDecayInSeconds.toLong()
+    val relevantStartIntervalTimestamp = alignToInterval(startTimestampInSeconds)
     val relevantEndIntervalTimestamp = alignToInterval(relevantEndTimestamp)
-    val overlapWithStartInterval = relevantStartIntervalTimestamp + INTERVAL_LENGTH_IN_SECONDS - startTimestamp
+    val overlapWithStartInterval = relevantStartIntervalTimestamp + INTERVAL_LENGTH_IN_SECONDS - startTimestampInSeconds
     val overlapWithEndInterval = relevantEndTimestamp - relevantEndIntervalTimestamp
     Timber.d("overlapWithStartInterval: $overlapWithStartInterval")
     Timber.d("overlapWithEndInterval: $overlapWithEndInterval")
