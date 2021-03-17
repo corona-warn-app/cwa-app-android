e5e3af02143062482aecfeb40e20a2ed980bd77d
Lukas Lechner <office@lukaslechner.com>
2021-03-17 10:28:19 +0100

    Move CategoryItemList to ViewModel

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/events/ui/category/TraceLocationCategoryFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/events/ui/category/TraceLocationCategoryFragment.kt
@@ -6,13 +6,9 @@ import android.view.accessibility.AccessibilityEvent
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.EventRegistrationCategoryFragmentBinding
-import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.CategoryItem
 import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.TraceLocationCategoryAdapter
-import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.category.TraceLocationUIType
-import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.category.traceLocationCategories
-import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.header.TraceLocationHeaderItem
-import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.separator.TraceLocationSeparatorItem
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
@@ -32,20 +28,13 @@ class TraceLocationCategoryFragment : Fragment(R.layout.event_registration_categ
 
         binding.toolbar.setNavigationOnClickListener { popBackStack() }
 
-        val categoryList = mutableListOf<CategoryItem>().apply {
-            add(TraceLocationHeaderItem(R.string.tracelocation_organizer_category_type_location_header))
-            addAll(traceLocationCategories.filter { it.uiType == TraceLocationUIType.LOCATION })
-            add(TraceLocationSeparatorItem)
-            add(TraceLocationHeaderItem(R.string.tracelocation_organizer_category_type_event_header))
-            addAll(traceLocationCategories.filter { it.uiType == TraceLocationUIType.EVENT })
+        vm.categoryItems.observe2(this){ categoryItems ->
+            val adapter = TraceLocationCategoryAdapter(categoryItems) {
+                // TODO: Set click-listener - Continue with event creation flow in next PR
+                Timber.d("Clicked on TraceLocationCategory: $it")
+            }
+            binding.recyclerViewCategories.adapter = adapter
         }
-
-        val adapter = TraceLocationCategoryAdapter(categoryList) {
-            // TODO: Set click-listener - Continue with event creation flow in next PR
-            Timber.d("Clicked on TraceLocationCategory: $it")
-        }
-
-        binding.recyclerViewCategories.adapter = adapter
     }
 
     override fun onResume() {
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/events/ui/category/TraceLocationCategoryViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/events/ui/category/TraceLocationCategoryViewModel.kt
@@ -1,7 +1,15 @@
 package de.rki.coronawarnapp.eventregistration.events.ui.category
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.CategoryItem
+import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.category.TraceLocationUIType
+import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.category.traceLocationCategories
+import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.header.TraceLocationHeaderItem
+import de.rki.coronawarnapp.eventregistration.events.ui.category.adapter.separator.TraceLocationSeparatorItem
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 
@@ -9,4 +17,14 @@ class TraceLocationCategoryViewModel @AssistedInject constructor() : CWAViewMode
 
     @AssistedFactory
     interface Factory : SimpleCWAViewModelFactory<TraceLocationCategoryViewModel>
+
+    private val categoryItemList = mutableListOf<CategoryItem>().apply {
+        add(TraceLocationHeaderItem(R.string.tracelocation_organizer_category_type_location_header))
+        addAll(traceLocationCategories.filter { it.uiType == TraceLocationUIType.LOCATION })
+        add(TraceLocationSeparatorItem)
+        add(TraceLocationHeaderItem(R.string.tracelocation_organizer_category_type_event_header))
+        addAll(traceLocationCategories.filter { it.uiType == TraceLocationUIType.EVENT })
+    }.toList()
+
+    val categoryItems: LiveData<List<CategoryItem>> = MutableLiveData(categoryItemList)
 }
