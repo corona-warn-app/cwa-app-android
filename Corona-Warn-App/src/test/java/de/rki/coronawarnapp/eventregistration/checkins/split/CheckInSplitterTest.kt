beffe0d0a07d71eaa1b8470e7a1c33d3f94baedb
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-17 17:54:52 +0100

    Add some edge cases

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/eventregistration/checkins/split/CheckInSplitterTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/eventregistration/checkins/split/CheckInSplitterTest.kt
@@ -69,7 +69,7 @@ class CheckInSplitterTest : BaseTest() {
     @Test
     fun `Scenario 3`() {
         /*
-           Example 3 - same-start-end-time check-in
+           Example 3 - same-start-end-times check-in
            localCheckIn = { start: '2021-03-04 09:30', end: '2021-03-04 09:30' }
            splitInto = [{ start: '2021-03-04 09:30', end: '2021-03-04 09:30' } // no split
         */
@@ -87,7 +87,7 @@ class CheckInSplitterTest : BaseTest() {
     @Test
     fun `Scenario 4`() {
         /*
-        // Example 2 - 2-days check-in
+        // Example 4 - 2-days check-in
         localCheckIn = { start: '2021-03-04 09:30', end: '2021-03-05 09:45' }
         splitInto = [
             { start: '2021-03-04 09:30', end: '2021-03-05 00:00' },
@@ -117,7 +117,7 @@ class CheckInSplitterTest : BaseTest() {
     @Test
     fun `Scenario 5`() {
         /*
-         // Example 3 - 3-day check-in
+         // Example 5 - 3-days check-in
          localCheckIn = { start: '2021-03-04 09:30', end: '2021-03-06 09:45' }
          splitInto = [
             { start: '2021-03-04 09:30', end: '2021-03-05 00:00' },
@@ -145,6 +145,102 @@ class CheckInSplitterTest : BaseTest() {
             get(2) shouldBe checkIn.copy(
                 checkInStart = Instant.parse("2021-03-06T00:00:00Z"),
                 checkInEnd = Instant.parse("2021-03-06T09:45:00Z")
+            )
+        }
+    }
+
+    @Test
+    fun `Scenario 6`() {
+        /*
+        // Example 6 - 2-days-different-months check-in
+        localCheckIn = { start: '2021-02-28 09:30', end: '2021-03-01 12:45' }
+        splitInto = [
+            { start: '2021-02-28 09:30', end: '2021-03-01 00:00' },
+            { start: '2021-03-01 00:00', end: '2021-03-01 12:45' }
+        ]
+        */
+
+        val checkIn = defaultCheckIn.copy(
+            checkInStart = Instant.parse("2021-02-28T09:30:00Z"),
+            checkInEnd = Instant.parse("2021-03-01T12:45:00Z")
+        )
+
+        checkIn.splitByMidnightUTC().apply {
+            size shouldBe 2
+            get(0) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2021-02-28T09:30:00Z"),
+                checkInEnd = Instant.parse("2021-03-01T00:00:00Z")
+            )
+
+            get(1) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2021-03-01T00:00:00Z"),
+                checkInEnd = Instant.parse("2021-03-01T12:45:00Z")
+            )
+        }
+    }
+
+    @Test
+    fun `Scenario 7`() {
+        /*
+        // Example 7 - 2-days-different-years check-in
+        localCheckIn = { start: '2021-12-31 09:30', end: '2022-01-01 12:45' }
+        splitInto = [
+            { start: '2021-12-31 09:30', end: '2022-01-01 00:00' },
+            { start: '2022-01-01 00:00', end: '2022-01-01 12:45' }
+        ]
+        */
+
+        val checkIn = defaultCheckIn.copy(
+            checkInStart = Instant.parse("2021-12-31T09:30:00Z"),
+            checkInEnd = Instant.parse("2022-01-01T12:45:00Z")
+        )
+
+        checkIn.splitByMidnightUTC().apply {
+            size shouldBe 2
+            get(0) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2021-12-31T09:30:00Z"),
+                checkInEnd = Instant.parse("2022-01-01T00:00:00Z")
+            )
+
+            get(1) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2022-01-01T00:00:00Z"),
+                checkInEnd = Instant.parse("2022-01-01T12:45:00Z")
+            )
+        }
+    }
+
+    @Test
+    fun `Scenario 8`() {
+        /*
+        // Example 8 - 3-days-different-months-leap-year check-in
+        localCheckIn = { start: '2020-02-28 09:30', end: '2020-03-01 12:45' }
+        splitInto = [
+            { start: '2020-02-28 09:30', end: '2020-02-29 00:00' },
+            { start: '2020-02-29 00:00', end: '2020-03-01 00:00' },
+            { start: '2020-03-01 00:00', end: '2020-03-01 12:45' }
+        ]
+        */
+
+        val checkIn = defaultCheckIn.copy(
+            checkInStart = Instant.parse("2020-02-28T09:30:00Z"),
+            checkInEnd = Instant.parse("2020-03-01T12:45:00Z")
+        )
+
+        checkIn.splitByMidnightUTC().apply {
+            size shouldBe 3
+            get(0) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2020-02-28T09:30:00Z"),
+                checkInEnd = Instant.parse("2020-02-29T00:00:00Z")
+            )
+
+            get(1) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2020-02-29T00:00:00Z"),
+                checkInEnd = Instant.parse("2020-03-01T00:00:00Z")
+            )
+
+            get(2) shouldBe checkIn.copy(
+                checkInStart = Instant.parse("2020-03-01T00:00:00Z"),
+                checkInEnd = Instant.parse("2020-03-01T12:45:00Z")
             )
         }
     }
