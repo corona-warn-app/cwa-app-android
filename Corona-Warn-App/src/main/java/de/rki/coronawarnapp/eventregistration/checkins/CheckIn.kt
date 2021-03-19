d4a7f88794ee836eecf65b2dab137ba671654541
chris-cwa <chris.cwa.sap@gmail.com>
2021-03-19 09:15:15 +0100

    no more use of blob

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/CheckIn.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/CheckIn.kt
@@ -7,7 +7,7 @@ import org.joda.time.Instant
 data class CheckIn(
     val id: Long,
     val guid: String,
-    val guidHash: ByteArray,
+    val guidHash: ByteString,
     val version: Int,
     val type: Int,
     val description: String,
@@ -15,39 +15,10 @@ data class CheckIn(
     val traceLocationStart: Instant?,
     val traceLocationEnd: Instant?,
     val defaultCheckInLengthInMinutes: Int?,
-    val traceLocationBytes: ByteArray,
+    val traceLocationBytes: ByteString,
     val signature: ByteString,
     val checkInStart: Instant,
     val checkInEnd: Instant,
     val completed: Boolean,
     val createJournalEntry: Boolean
-) {
-
-    // we have to override it because of the array fields
-    @SuppressWarnings("ComplexMethod")
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CheckIn
-
-        if (id != other.id) return false
-        if (guid != other.guid) return false
-        if (!guidHash.contentEquals(other.guidHash)) return false
-        if (version != other.version) return false
-        if (type != other.type) return false
-        if (description != other.description) return false
-        if (address != other.address) return false
-        if (traceLocationStart != other.traceLocationStart) return false
-        if (traceLocationEnd != other.traceLocationEnd) return false
-        if (defaultCheckInLengthInMinutes != other.defaultCheckInLengthInMinutes) return false
-        if (!traceLocationBytes.contentEquals(other.traceLocationBytes)) return false
-        if (signature != other.signature) return false
-        if (checkInStart != other.checkInStart) return false
-        if (checkInEnd != other.checkInEnd) return false
-        if (completed != other.completed) return false
-        if (createJournalEntry != other.createJournalEntry) return false
-
-        return true
-    }
-}
+)
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationCheckInEntity.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/storage/entity/TraceLocationCheckInEntity.kt
@@ -9,7 +9,7 @@ import org.joda.time.Instant
 data class TraceLocationCheckInEntity(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Long = 0L,
     @ColumnInfo(name = "guid") val guid: String,
-    @ColumnInfo(name = "guidHash") val guidHash: ByteArray,
+    @ColumnInfo(name = "guidHash") val guidHashBase64: String,
     @ColumnInfo(name = "version") val version: Int,
     @ColumnInfo(name = "type") val type: Int,
     @ColumnInfo(name = "description") val description: String,
@@ -17,39 +17,10 @@ data class TraceLocationCheckInEntity(
     @ColumnInfo(name = "traceLocationStart") val traceLocationStart: Instant?,
     @ColumnInfo(name = "traceLocationEnd") val traceLocationEnd: Instant?,
     @ColumnInfo(name = "defaultCheckInLengthInMinutes") val defaultCheckInLengthInMinutes: Int?,
-    @ColumnInfo(name = "traceLocationBytes") val traceLocationBytes: ByteArray,
+    @ColumnInfo(name = "traceLocationBytes") val traceLocationBytesBase64: String,
     @ColumnInfo(name = "signature") val signatureBase64: String,
     @ColumnInfo(name = "checkInStart") val checkInStart: Instant,
     @ColumnInfo(name = "checkInEnd") val checkInEnd: Instant,
     @ColumnInfo(name = "completed") val completed: Boolean,
     @ColumnInfo(name = "createJournalEntry") val createJournalEntry: Boolean
-) {
-
-    // we have to override it because of the array fields
-    @SuppressWarnings("ComplexMethod")
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as TraceLocationCheckInEntity
-
-        if (id != other.id) return false
-        if (guid != other.guid) return false
-        if (!guidHash.contentEquals(other.guidHash)) return false
-        if (version != other.version) return false
-        if (type != other.type) return false
-        if (description != other.description) return false
-        if (address != other.address) return false
-        if (traceLocationStart != other.traceLocationStart) return false
-        if (traceLocationEnd != other.traceLocationEnd) return false
-        if (defaultCheckInLengthInMinutes != other.defaultCheckInLengthInMinutes) return false
-        if (!traceLocationBytes.contentEquals(other.traceLocationBytes)) return false
-        if (signatureBase64 != other.signatureBase64) return false
-        if (checkInStart != other.checkInStart) return false
-        if (checkInEnd != other.checkInEnd) return false
-        if (completed != other.completed) return false
-        if (createJournalEntry != other.createJournalEntry) return false
-
-        return true
-    }
-}
+)
