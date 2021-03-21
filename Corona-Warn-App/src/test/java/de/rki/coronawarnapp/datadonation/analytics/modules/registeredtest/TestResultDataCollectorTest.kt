8760162572e7277ec2eb635dcf6067735d54d554
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-21 11:38:37 +0100

    Add tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/analytics/modules/registeredtest/TestResultDataCollectorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/datadonation/analytics/modules/registeredtest/TestResultDataCollectorTest.kt
@@ -37,6 +37,7 @@ class TestResultDataCollectorTest : BaseTest() {
         MockKAnnotations.init(this)
 
         every { timeStamper.nowUTC } returns Instant.now()
+        every { testResultDonorSettings.clear() } just Runs
         testResultDataCollector = TestResultDataCollector(
             analyticsSettings,
             testResultDonorSettings,
@@ -46,49 +47,115 @@ class TestResultDataCollectorTest : BaseTest() {
     }
 
     @Test
-    fun `saveTestResultAnalyticsSettings does not save anything when no user consent`() = runBlockingTest {
-        every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(false)
-        testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.POSITIVE)
+    fun `saveTestResultAnalyticsSettings does not save anything when no user consent`() =
+        runBlockingTest {
+            every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(false)
+            testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.POSITIVE)
 
-        verify(exactly = 0) {
-            testResultDonorSettings.saveTestResultDonorDataAtRegistration(any(), any())
+            verify(exactly = 0) {
+                testResultDonorSettings.saveTestResultDonorDataAtRegistration(any(), any())
+            }
         }
-    }
 
     @Test
-    fun `saveTestResultAnalyticsSettings saves data when user gave consent`() = runBlockingTest {
-        every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(true)
+    fun `saveTestResultAnalyticsSettings saves data when user gave consent`() =
+        runBlockingTest {
+            every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(true)
 
-        val mockRiskLevelResult = mockk<RiskLevelResult>().apply {
-            every { calculatedAt } returns Instant.now()
-            every { wasSuccessfullyCalculated } returns true
-        }
-        every { riskLevelStorage.latestAndLastSuccessful } returns flowOf(listOf(mockRiskLevelResult))
-        every { testResultDonorSettings.saveTestResultDonorDataAtRegistration(any(), any()) } just Runs
-        testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.POSITIVE)
+            val mockRiskLevelResult = mockk<RiskLevelResult>().apply {
+                every { calculatedAt } returns Instant.now()
+                every { wasSuccessfullyCalculated } returns true
+            }
+            every { riskLevelStorage.latestAndLastSuccessful } returns flowOf(listOf(mockRiskLevelResult))
+            every { testResultDonorSettings.saveTestResultDonorDataAtRegistration(any(), any()) } just Runs
+            testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.POSITIVE)
 
-        verify(exactly = 1) {
-            testResultDonorSettings.saveTestResultDonorDataAtRegistration(any(), any())
+            verify(exactly = 1) {
+                testResultDonorSettings.saveTestResultDonorDataAtRegistration(any(), any())
+            }
         }
-    }
 
     @Test
-    fun `saveTestResultAnalyticsSettings does not save data when TestResult is INVALID`() = runBlockingTest {
-        every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(false)
-        testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.INVALID)
+    fun `saveTestResultAnalyticsSettings does not save data when TestResult is INVALID`() =
+        runBlockingTest {
+            every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(false)
+            testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.INVALID)
 
-        verify {
-            analyticsSettings.analyticsEnabled wasNot Called
+            verify {
+                analyticsSettings.analyticsEnabled wasNot Called
+            }
         }
-    }
 
     @Test
-    fun `saveTestResultAnalyticsSettings does not save data when TestResult is REDEEMED`() = runBlockingTest {
-        every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(false)
-        testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.REDEEMED)
+    fun `saveTestResultAnalyticsSettings does not save data when TestResult is REDEEMED`() =
+        runBlockingTest {
+            every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(false)
+            testResultDataCollector.saveTestResultAnalyticsSettings(TestResult.REDEEMED)
 
-        verify {
-            analyticsSettings.analyticsEnabled wasNot Called
+            verify {
+                analyticsSettings.analyticsEnabled wasNot Called
+            }
         }
+
+    @Test
+    fun `updatePendingTestResultReceivedTime doesn't update when TestResult isn't POS or NEG`() =
+        runBlockingTest {
+            for (testResult in listOf(TestResult.REDEEMED, TestResult.INVALID, TestResult.PENDING)) {
+                every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(true)
+                every { testResultDonorSettings.testScannedAfterConsent } returns mockFlowPreference(true)
+                every { testResultDonorSettings.testResultAtRegistration } returns mockFlowPreference(TestResult.PENDING)
+                testResultDataCollector.updatePendingTestResultReceivedTime(testResult)
+
+                verify {
+                    analyticsSettings.analyticsEnabled
+                    testResultDonorSettings.testScannedAfterConsent
+                    testResultDonorSettings.testResultAtRegistration
+                    testResultDonorSettings.finalTestResultReceivedAt wasNot Called
+                    testResultDonorSettings.testResultAtRegistration wasNot Called
+                }
+            }
+        }
+
+    @Test
+    fun `updatePendingTestResultReceivedTime doesn't update when Test is not scanned after consent`() =
+        runBlockingTest {
+            every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(true)
+            every { testResultDonorSettings.testScannedAfterConsent } returns mockFlowPreference(false)
+            every { testResultDonorSettings.testResultAtRegistration } returns mockFlowPreference(TestResult.PENDING)
+            testResultDataCollector.updatePendingTestResultReceivedTime(TestResult.NEGATIVE)
+
+            verify {
+                analyticsSettings.analyticsEnabled
+                testResultDonorSettings.testScannedAfterConsent
+                testResultDonorSettings.testResultAtRegistration wasNot Called
+                testResultDonorSettings.finalTestResultReceivedAt wasNot Called
+                testResultDonorSettings.testResultAtRegistration wasNot Called
+            }
+        }
+
+    @Test
+    fun `updatePendingTestResultReceivedTime update when TestResult is POS or NEG`() =
+        runBlockingTest {
+            for (testResult in listOf(TestResult.NEGATIVE, TestResult.POSITIVE)) {
+                every { analyticsSettings.analyticsEnabled } returns mockFlowPreference(true)
+                every { testResultDonorSettings.testScannedAfterConsent } returns mockFlowPreference(true)
+                every { testResultDonorSettings.testResultAtRegistration } returns mockFlowPreference(TestResult.PENDING)
+                every { testResultDonorSettings.finalTestResultReceivedAt } returns mockFlowPreference(Instant.EPOCH)
+                testResultDataCollector.updatePendingTestResultReceivedTime(testResult)
+
+                verify {
+                    analyticsSettings.analyticsEnabled
+                    testResultDonorSettings.testScannedAfterConsent
+                    testResultDonorSettings.testResultAtRegistration
+                    testResultDonorSettings.finalTestResultReceivedAt
+                    testResultDonorSettings.testResultAtRegistration
+                }
+            }
+        }
+
+    @Test
+    fun `clear is clearing saved data`() {
+        testResultDataCollector.clear()
+        verify { testResultDonorSettings.clear() }
     }
 }
