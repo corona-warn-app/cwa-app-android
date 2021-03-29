184b161e9855a82773ac095de873f57ff968e1d6
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-29 17:28:53 +0200

    Add test

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/eventregistration/attendee/checkins/CheckInsViewModelTest.kt
@@ -0,0 +1,26 @@
+package de.rki.coronawarnapp.ui.eventregistration.attendee.checkins
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import testhelpers.BaseTest
+
+class CheckInsViewModelTest : BaseTest() {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun onRemoveCheckInConfirmed() {
+    }
+
+    @Test
+    fun onRemoveAllCheckIns() {
+    }
+}
