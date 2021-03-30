874902b1f0d21b5143c864eca62ce1aa9e782ba9
chris-cwa <chris.cwa.sap@gmail.com>
2021-03-30 09:15:45 +0200

    moved preferences into settings class

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/TraceLocationSettings.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/TraceLocationSettings.kt
@@ -1,23 +1,38 @@
 package de.rki.coronawarnapp.eventregistration
 
-import de.rki.coronawarnapp.eventregistration.storage.TraceLocationPreferences
+import android.content.Context
+import de.rki.coronawarnapp.util.di.AppContext
+import de.rki.coronawarnapp.util.preferences.clearAndNotify
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TraceLocationSettings @Inject constructor(val preferences: TraceLocationPreferences) {
+class TraceLocationSettings @Inject constructor(
+    @AppContext val context: Context
+) {
+
+    private val preferences by lazy {
+        context.getSharedPreferences(name, Context.MODE_PRIVATE)
+    }
 
     var onboardingStatus: OnboardingStatus
         get() {
-            val order = preferences.onboardingStatusOrder.value
+            val order = preferences.getInt(key_status, OnboardingStatus.NOT_ONBOARDED.order)
             return OnboardingStatus.values().find { it.order == order } ?: OnboardingStatus.NOT_ONBOARDED
         }
-        set(value) = preferences.onboardingStatusOrder.update { value.order }
+        set(value) = preferences.edit().putInt(key_status, value.order).apply()
 
     inline val isOnboardingDone get() = onboardingStatus == OnboardingStatus.ONBOARDED_2_0
+
+    fun clear() {
+        preferences.clearAndNotify()
+    }
 
     enum class OnboardingStatus(val order: Int) {
         NOT_ONBOARDED(0),
         ONBOARDED_2_0(1)
     }
 }
+
+private const val key_status = "trace_location_onboardingstatus"
+private const val name = "trace_location_localdata"
