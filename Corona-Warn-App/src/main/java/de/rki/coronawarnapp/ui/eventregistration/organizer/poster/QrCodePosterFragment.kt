1aecfc404a2bb555870724ba2ad17263d12dc100
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-03-31 23:36:36 +0200

    Print PDF

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/organizer/poster/QrCodePosterFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/organizer/poster/QrCodePosterFragment.kt
@@ -1,16 +1,23 @@
 package de.rki.coronawarnapp.ui.eventregistration.organizer.poster
 
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintManager
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.core.content.getSystemService
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.QrCodePosterFragmentBinding
+import de.rki.coronawarnapp.test.eventregistration.ui.PrintingAdapter
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.files.FileSharing
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
+import timber.log.Timber
 import javax.inject.Inject
 
 class QrCodePosterFragment : Fragment(R.layout.qr_code_poster_fragment), AutoInject {
@@ -37,6 +44,40 @@ class QrCodePosterFragment : Fragment(R.layout.qr_code_poster_fragment), AutoInj
                 posterImage.setImageBitmap(poster.template.image)
                 qrCodeImage.setImageBitmap(poster.qrCode)
                 progressBar.hide()
+
+                viewModel.createPDF(binding.qrCodePoster)
+            }
+        }
+
+        viewModel.sharingIntent.observe(viewLifecycleOwner) {
+            onShareIntent(it)
+        }
+    }
+
+    private fun onShareIntent(fileIntent: FileSharing.FileIntentProvider) {
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_print -> {
+                    val printingManger = context?.getSystemService<PrintManager>()
+                    Timber.i("PrintingManager=$printingManger")
+                    if (printingManger != null) {
+                        printingManger.print(
+                            "CoronaWarnApp",
+                            PrintingAdapter(fileIntent.file),
+                            PrintAttributes
+                                .Builder()
+                                .setMediaSize(PrintAttributes.MediaSize.ISO_A3)
+                                .build()
+                        )
+                    } else {
+                        // TODO
+                    }
+
+                    true
+                }
+
+                R.id.action_share -> startActivity(fileIntent.intent(requireActivity())).run { true }
+                else -> false
             }
         }
     }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/organizer/poster/QrCodePosterViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/eventregistration/organizer/poster/QrCodePosterViewModel.kt
@@ -1,6 +1,8 @@
 package de.rki.coronawarnapp.ui.eventregistration.organizer.poster
 
 import android.graphics.Bitmap
+import android.graphics.pdf.PdfDocument
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import dagger.assisted.Assisted
@@ -12,23 +14,64 @@ import de.rki.coronawarnapp.eventregistration.checkins.qrcode.Template
 import de.rki.coronawarnapp.exception.ExceptionCategory
 import de.rki.coronawarnapp.exception.reporting.report
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import de.rki.coronawarnapp.util.files.FileSharing
+import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
 import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
 
 class QrCodePosterViewModel @AssistedInject constructor(
     @Assisted private val locationID: String,
     private val dispatcher: DispatcherProvider,
     private val qrCodeGenerator: QrCodeGenerator,
-    private val posterImageProvider: PosterImageProvider
+    private val posterImageProvider: PosterImageProvider,
+    private val fileSharing: FileSharing
 
 ) : CWAViewModel(dispatcher) {
 
     private val posterLiveData = MutableLiveData<Poster>()
     val poster: LiveData<Poster> = posterLiveData
+    val sharingIntent = SingleLiveEvent<FileSharing.FileIntentProvider>()
 
     init {
         generatePoster()
+    }
+
+    /**
+     * Create a new PDF file and result is delivered by [sharingIntent]
+     * as a sharing [FileSharing.ShareIntentProvider]
+     */
+    @Suppress("BlockingMethodInNonBlockingContext")
+    fun createPDF(view: View) = launch(context = dispatcher.IO) {
+        try {
+            val file = File(view.context.cacheDir, "CoronaWarnApp-Event.pdf")
+            val pageInfo = PdfDocument.PageInfo.Builder(
+                view.width,
+                view.height,
+                1
+            ).create()
+
+            PdfDocument().apply {
+                startPage(pageInfo).apply {
+                    view.draw(canvas)
+                    finishPage(this)
+                }
+
+                FileOutputStream(file).use {
+                    writeTo(it)
+                    close()
+                }
+            }
+
+            sharingIntent.postValue(
+                fileSharing.getFileIntentProvider(file, "Scan and Help")
+            )
+        } catch (e: Exception) {
+            Timber.d(e, "Creating pdf failed")
+            e.report(ExceptionCategory.INTERNAL)
+        }
     }
 
     private fun generatePoster() = launch(context = dispatcher.IO) {
