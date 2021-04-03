9b0850a5f17279d355a30e384e3b47ce75c7abcd
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-03 11:27:32 +0200

    Use config regex

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/qrcode/QRCodeUriParser.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eventregistration/checkins/qrcode/QRCodeUriParser.kt
@@ -1,46 +1,53 @@
 package de.rki.coronawarnapp.eventregistration.checkins.qrcode
 
 import dagger.Reusable
+import de.rki.coronawarnapp.appconfig.AppConfigProvider
+import de.rki.coronawarnapp.server.protocols.internal.v2.PresenceTracingParametersOuterClass.PresenceTracingQRCodeDescriptor.PayloadEncoding
 import de.rki.coronawarnapp.util.decodeBase32
 import okio.ByteString
+import okio.ByteString.Companion.decodeBase64
 import timber.log.Timber
 import java.net.URI
 import javax.inject.Inject
 
 @Reusable
-class QRCodeUriParser @Inject constructor() {
+class QRCodeUriParser @Inject constructor(
+    private val appConfigProvider: AppConfigProvider
+) {
 
-    /**
-     * Validate that QRCode scanned uri matches the following formulas:
-     * https://e.coronawarn.app/c1/SIGNED_TRACE_LOCATION_BASE32
-     * HTTPS://E.CORONAWARN.APP/C1/SIGNED_TRACE_LOCATION_BASE32
-     */
-    fun getQrCodePayload(maybeUri: String): ByteString? = URI.create(maybeUri).run {
-        if (!scheme.equals(SCHEME, true)) return@run null
-        if (!authority.equals(AUTHORITY, true)) return@run null
+    suspend fun getQrCodePayload(input: String): ByteString? {
+        Timber.d("input=$input")
+        URI.create(input) // Just to verify it is a valid url
 
-        if (!path.substringBeforeLast("/").equals(PATH_PREFIX, true)) return@run null
-
-        val rawData = path.substringAfterLast("/")
-        val paddingDiff = 8 - (rawData.length % 8)
-        val maybeBase32 = rawData + createPadding(paddingDiff)
-
-        if (!maybeBase32.matches(BASE32_REGEX)) return@run null
-
-        return@run try {
-            maybeBase32.decodeBase32()
-        } catch (e: Exception) {
-            Timber.w(e, "Data wasn't base32: %s", maybeBase32)
-            null
+        val descriptors = appConfigProvider.getAppConfig().presenceTracing.qrCodeDescriptors
+        val descriptor = descriptors.find { it.regexPattern.toRegex(RegexOption.IGNORE_CASE).matches(input) }
+        if (descriptor == null) {
+            Timber.d("Invalid URI - no matchedDescriptor")
+            throw IllegalArgumentException("Invalid URI - no matchedDescriptor")
         }
-    }
+        Timber.d("descriptor=$descriptor")
 
-    companion object {
-        private fun createPadding(length: Int) = (0 until length).joinToString(separator = "") { "=" }
+        val payloadGroupIndex = descriptor.encodedPayloadGroupIndex
+        val groups = descriptor.regexPattern
+            .toRegex(RegexOption.IGNORE_CASE).find(input) // Find matched result [MatchResult]
+            ?.destructured?.toList().orEmpty() // Destructured groups - excluding the zeroth group (Whole String)
+        Timber.d("groups=$groups")
 
-        private const val SCHEME = "https"
-        private const val AUTHORITY = "e.coronawarn.app"
-        private const val PATH_PREFIX = "/c1"
-        private val BASE32_REGEX = "^([A-Z2-7=]{8})+$".toRegex(RegexOption.IGNORE_CASE)
+        if (payloadGroupIndex !in groups.indices) {
+            Timber.d("Invalid payload - group index is out of bounds")
+            throw IllegalArgumentException("Invalid payload - group index is out of bounds")
+        }
+
+        val payload = groups[payloadGroupIndex]
+        Timber.d("payload=$payload")
+
+        val encoding = PayloadEncoding.forNumber(descriptor.payloadEncoding.number)
+        Timber.d("encoding=$encoding")
+
+        return when (encoding) {
+            PayloadEncoding.BASE32 -> payload.decodeBase32()
+            PayloadEncoding.BASE64 -> payload.decodeBase64()
+            else -> null
+        }
     }
 }
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/eventregistration/checkins/qrcode/QRCodeUriParserTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/eventregistration/checkins/qrcode/QRCodeUriParserTest.kt
@@ -1,24 +1,46 @@
 package de.rki.coronawarnapp.eventregistration.checkins.qrcode
 
+import de.rki.coronawarnapp.appconfig.AppConfigProvider
+import de.rki.coronawarnapp.appconfig.ConfigData
+import de.rki.coronawarnapp.appconfig.PresenceTracingConfigContainer
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
 import testhelpers.BaseTest
 
 class QRCodeUriParserTest : BaseTest() {
 
-    fun createInstance() = QRCodeUriParser()
+    @MockK lateinit var appConfigProvider: AppConfigProvider
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+        coEvery { appConfigProvider.getAppConfig() } returns mockk<ConfigData>().apply {
+            every { presenceTracing } returns PresenceTracingConfigContainer(
+                qrCodeDescriptors = listOf()
+            )
+        }
+    }
+
+    fun createInstance() = QRCodeUriParser(appConfigProvider)
 
     @ParameterizedTest
     @ArgumentsSource(ValidUrlProvider::class)
-    fun `Valid URLs`(input: String) {
+    fun `Valid URLs`(input: String) = runBlockingTest {
         createInstance().getQrCodePayload(input) shouldNotBe null
     }
 
     @ParameterizedTest
     @ArgumentsSource(InvalidUrlProvider::class)
-    fun `Invalid URLs`(input: String) {
+    fun `Invalid URLs`(input: String) = runBlockingTest {
         createInstance().getQrCodePayload(input) shouldBe null
     }
 }
