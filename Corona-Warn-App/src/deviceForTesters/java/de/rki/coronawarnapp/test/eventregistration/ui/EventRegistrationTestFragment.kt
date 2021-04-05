08abf5d939752baf88e22153377a91c924f48ef9
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-05 08:45:08 +0200

    Style text

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/EventRegistrationTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/eventregistration/ui/EventRegistrationTestFragment.kt
@@ -2,14 +2,20 @@ package de.rki.coronawarnapp.test.eventregistration.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.SpannedString
 import android.view.View
 import android.widget.Toast
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
+import androidx.core.text.scale
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTestEventregistrationBinding
 import de.rki.coronawarnapp.test.menu.ui.TestMenuItem
+import de.rki.coronawarnapp.util.ContextExtensions.getColorCompat
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.doNavigate
 import de.rki.coronawarnapp.util.ui.observe2
@@ -84,9 +90,9 @@ class EventRegistrationTestFragment : Fragment(R.layout.fragment_test_eventregis
             binding.lastOrganiserLocationCard.isVisible = it != null
             it?.let { data ->
                 with(binding) {
-                    lastOrganiserLocation.text = data.traceLocation.toString()
-                    lastOrganiserLocationId.text = "ID: " + data.id
-                    lastOrganiserLocationUrl.text = "URL: " + data.url
+                    lastOrganiserLocation.text = traceLocation(data)
+                    lastOrganiserLocationId.text = styleText("ID", data.id)
+                    lastOrganiserLocationUrl.text = styleText("URL", data.url)
                 }
             }
         }
@@ -95,13 +101,46 @@ class EventRegistrationTestFragment : Fragment(R.layout.fragment_test_eventregis
             binding.lastAttendeeLocationCard.isVisible = it != null
             it?.let { data ->
                 with(binding) {
-                    lastAttendeeLocation.text = data.traceLocation.toString()
-                    lastAttendeeLocationId.text = "ID: " + data.id
-                    lastAttendeeLocationUrl.text = "URL: " + data.url
+                    lastAttendeeLocation.text = traceLocation(data)
+                    lastAttendeeLocationId.text = styleText("ID", data.id)
+                    lastAttendeeLocationUrl.text = styleText("URL", data.url)
                 }
             }
         }
     }
+
+    private fun traceLocation(data: LastLocationData): SpannedString = with(data.traceLocation) {
+        buildSpannedString {
+            append("TraceLocation [\n")
+            append(styleText("Id", id))
+            append(styleText("type", type))
+            append(styleText("version", version))
+            append(styleText("address", address))
+            append(styleText("description", description))
+            append(styleText("startDate", startDate))
+            append(styleText("endDate", endDate))
+            append(styleText("defaultCheckInLengthInMinutes", defaultCheckInLengthInMinutes))
+            append(styleText("cnPublicKey", cnPublicKey))
+            append(styleText("cryptographicSeed", cryptographicSeed.base64()))
+            append("]")
+        }
+    }
+
+    private fun styleText(key: String, value: Any?): SpannedString =
+        buildSpannedString {
+            bold {
+                color(requireContext().getColorCompat(R.color.colorAccent)) {
+                    append("$key=")
+                }
+            }
+
+            scale(0.85f) {
+                color(requireContext().getColorCompat(R.color.colorTextPrimary1)) {
+                    append(value.toString())
+                }
+            }
+            append("\n")
+        }
 
     companion object {
         val MENU_ITEM = TestMenuItem(
