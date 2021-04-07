4ee8314b0808766071160725d731ee81f9d929e3
Matthias Urhahn <matthias.urhahn@sap.com>
2021-04-07 14:29:33 +0200

    Use TimeUnit.MINUTES

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/risk/calculation/PresenceTracingConversions.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/risk/calculation/PresenceTracingConversions.kt
@@ -2,11 +2,10 @@ package de.rki.coronawarnapp.presencetracing.risk.calculation
 
 import de.rki.coronawarnapp.risk.RiskState
 import de.rki.coronawarnapp.server.protocols.internal.v2.RiskCalculationParametersOuterClass.NormalizedTimeToRiskLevelMapping.RiskLevel
+import java.util.concurrent.TimeUnit
 
 // converts number of 10min intervals into milliseconds
-internal fun Int.tenMinIntervalToMillis() = this * MILLIS_IN_TEN_MIN
-
-private const val MILLIS_IN_TEN_MIN = 600000L
+internal fun Int.tenMinIntervalToMillis() = this * TimeUnit.MINUTES.toMillis(10L)
 
 fun RiskLevel.mapToRiskState(): RiskState {
     return when (this) {
