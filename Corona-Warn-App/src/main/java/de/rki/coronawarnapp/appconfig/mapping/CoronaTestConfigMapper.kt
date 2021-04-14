38f69359537bd4f481557f6d69bc30c50036d124
Mohamed <mohamed.metwalli@sap.com>
2021-04-14 10:55:18 +0200

    Corona Test Config (EXPOSUREAPP-6047) (#2823)
    * Map CoronaTestConfig
    
    * Set default value
    
    * Remove todo , aligned

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/mapping/CoronaTestConfigMapper.kt
@@ -0,0 +1,36 @@
+package de.rki.coronawarnapp.appconfig.mapping
+
+import dagger.Reusable
+import de.rki.coronawarnapp.appconfig.CoronaRapidAntigenTestParametersContainer
+import de.rki.coronawarnapp.appconfig.CoronaTestConfig
+import de.rki.coronawarnapp.appconfig.CoronaTestConfigContainer
+import de.rki.coronawarnapp.server.protocols.internal.v2.AppConfigAndroid.ApplicationConfigurationAndroid
+import timber.log.Timber
+import javax.inject.Inject
+
+@Reusable
+class CoronaTestConfigMapper @Inject constructor() : CoronaTestConfig.Mapper {
+    override fun map(
+        rawConfig: ApplicationConfigurationAndroid
+    ): CoronaTestConfig {
+
+        if (!rawConfig.hasCoronaTestParameters()) {
+            Timber.d("coronaTestParameters are missing")
+            return CoronaTestConfigContainer()
+        }
+
+        return rawConfig.mapCoronaTestParameters()
+    }
+
+    private fun ApplicationConfigurationAndroid.mapCoronaTestParameters(): CoronaTestConfig {
+        val coronaRapidAntigenTestParameters = if (coronaTestParameters.hasCoronaRapidAntigenTestParameters()) {
+            CoronaRapidAntigenTestParametersContainer(
+                coronaTestParameters.coronaRapidAntigenTestParameters.hoursToDeemTestOutdated
+            )
+        } else {
+            Timber.d("coronaRapidAntigenTestParameters is missing")
+            CoronaRapidAntigenTestParametersContainer()
+        }
+        return CoronaTestConfigContainer(coronaRapidAntigenTestParameters)
+    }
+}
