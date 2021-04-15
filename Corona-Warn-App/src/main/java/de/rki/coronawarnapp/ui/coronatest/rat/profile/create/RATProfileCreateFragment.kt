11e0ff8f7a0cb97dc1b5d1ade90ef45035bbfa97
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-15 18:10:12 +0200

    Create Profile screen

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/create/RATProfileCreateFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/create/RATProfileCreateFragment.kt
@@ -1,6 +1,31 @@
 package de.rki.coronawarnapp.ui.coronatest.rat.profile.create
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.RatProfileCreateFragmentBinding
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.popBackStack
+import de.rki.coronawarnapp.util.ui.viewBindingLazy
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import javax.inject.Inject
 
-class RATProfileCreateFragment : Fragment(R.layout.rat_profile_create_fragment)
+class RATProfileCreateFragment : Fragment(R.layout.rat_profile_create_fragment), AutoInject {
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+
+    private val binding: RatProfileCreateFragmentBinding by viewBindingLazy()
+    private val viewModel: RATProfileCreateFragmentViewModel by cwaViewModels { viewModelFactory }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) =
+        with(binding) {
+            toolbar.setNavigationOnClickListener {
+                popBackStack()
+            }
+
+            profileSaveButton.setOnClickListener {
+                viewModel.saveProfile()
+            }
+        }
+}
---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/create/RATProfileCreateFragmentViewModel.kt
@@ -0,0 +1,22 @@
+package de.rki.coronawarnapp.ui.coronatest.rat.profile.create
+
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.coronatest.antigen.profile.RATProfile
+import de.rki.coronawarnapp.coronatest.antigen.profile.RATProfileSettings
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
+import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
+
+class RATProfileCreateFragmentViewModel @AssistedInject constructor(
+    private val ratProfileSettings: RATProfileSettings
+) : CWAViewModel() {
+    fun saveProfile() {
+
+        ratProfileSettings.profile.update {
+            RATProfile("Max", "Mustermann", "01.10.2000")
+        }
+    }
+
+    @AssistedFactory
+    interface Factory : SimpleCWAViewModelFactory<RATProfileCreateFragmentViewModel>
+}
