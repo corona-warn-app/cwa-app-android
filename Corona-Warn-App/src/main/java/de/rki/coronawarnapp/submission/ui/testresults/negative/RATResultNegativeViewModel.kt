b1baf41bc76eae083541e7451395ba6359ca7f0a
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-21 21:21:01 +0200

    Refactoring

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/ui/testresults/negative/RATResultNegativeViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/submission/ui/testresults/negative/RATResultNegativeViewModel.kt
@@ -22,6 +22,7 @@ import org.joda.time.format.PeriodFormatter
 import org.joda.time.format.PeriodFormatterBuilder
 import timber.log.Timber
 import java.lang.Exception
+import java.util.concurrent.TimeUnit
 
 class RATResultNegativeViewModel @AssistedInject constructor(
     dispatcherProvider: DispatcherProvider,
@@ -36,26 +37,36 @@ class RATResultNegativeViewModel @AssistedInject constructor(
         intervalFlow(1),
         coronaTestRepository.coronaTests
     ) { _, tests ->
-        val rapidTest = tests.firstOrNull { it.type == CoronaTest.Type.RAPID_ANTIGEN }
-        testAge(rapidTest)
+        val rapidTest = tests.firstOrNull {
+            it.type == CoronaTest.Type.RAPID_ANTIGEN
+        }
+
+        rapidTest?.testAge()
     }.asLiveData(context = dispatcherProvider.Default)
 
-    private suspend fun testAge(rapidTest: CoronaTest?): TestAge? {
-        if (rapidTest !is RACoronaTest) {
+    private suspend fun CoronaTest.testAge(): TestAge? {
+        if (this !is RACoronaTest) {
             Timber.d("Rapid test is missing")
             return null
         }
 
-        // TODO redeem test
         val hours = appConfigProvider.getAppConfig()
             .coronaTestParameters
             .coronaRapidAntigenTestParameters
             .hoursToDeemTestOutdated
+            .toLong()
 
-        val duration = Duration(rapidTest.testedAt, timeStamper.nowUTC)
-        val ageText = formatter.print(duration.toPeriod())
-
-        return TestAge(test = rapidTest, ageText)
+        val nowUTC = timeStamper.nowUTC
+        val age = nowUTC.millis - testedAt.millis
+        val redeemed = age > TimeUnit.HOURS.toMillis(hours)
+        val ageText = if (redeemed) {
+            Timber.d("Test (%s) is redeemed", registrationToken)
+            // TODO show it is redeemed
+            "__:__:__"
+        } else {
+            formatter.print(Duration(age).toPeriod())
+        }
+        return TestAge(test = this, ageText)
     }
 
     fun deleteTest() {
@@ -82,14 +93,15 @@ class RATResultNegativeViewModel @AssistedInject constructor(
     )
 
     companion object {
-        private val formatter: PeriodFormatter = PeriodFormatterBuilder().apply {
-            printZeroAlways()
-            minimumPrintedDigits(2)
-            appendHours()
-            appendSuffix(":")
-            appendMinutes()
-            appendSuffix(":")
-            appendSeconds()
-        }.toFormatter()
+        private val formatter: PeriodFormatter =
+            PeriodFormatterBuilder().apply {
+                printZeroAlways()
+                minimumPrintedDigits(2)
+                appendHours()
+                appendSuffix(":")
+                appendMinutes()
+                appendSuffix(":")
+                appendSeconds()
+            }.toFormatter()
     }
 }
