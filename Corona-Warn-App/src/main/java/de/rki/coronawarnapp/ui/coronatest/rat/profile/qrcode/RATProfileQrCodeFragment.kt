5528aed89d9d7c6844c40f7e03190198f3d9760f
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-26 16:09:32 +0200

    UI

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/qrcode/RATProfileQrCodeFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/qrcode/RATProfileQrCodeFragment.kt
@@ -2,18 +2,47 @@ package de.rki.coronawarnapp.ui.coronatest.rat.profile.qrcode
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.AppBarLayout
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.RatProfileQrCodeFragmentBinding
+import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
+import kotlin.math.abs
 
-class RATProfileQrCodeFragment : Fragment(R.layout.rat_profile_qr_code_fragment) {
+class RATProfileQrCodeFragment : Fragment(R.layout.rat_profile_qr_code_fragment), AutoInject {
 
     private val binding: RatProfileQrCodeFragmentBinding by viewBindingLazy()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setToolbarOverlay()
+        binding.apply {
+            appBarLayout.addOnOffsetChangedListener(
+                AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+                    val alpha = 1.0f - abs(verticalOffset / (appBarLayout.totalScrollRange.toFloat() * 0.5f))
+                    title.alpha = alpha
+                }
+            )
 
-        binding.closeButton.setOnClickListener { popBackStack() }
+            closeButton.setOnClickListener { popBackStack() }
+            toolbar.setNavigationOnClickListener { popBackStack() }
+        }
+    }
+
+    private fun setToolbarOverlay() {
+        val width = requireContext().resources.displayMetrics.widthPixels
+
+        val params: CoordinatorLayout.LayoutParams = binding.nestedScrollView.layoutParams
+            as (CoordinatorLayout.LayoutParams)
+
+        val textParams = binding.title.layoutParams as (LinearLayout.LayoutParams)
+        textParams.bottomMargin = ((width) / 2) - 24 /* 24 is space between screen border and QrCode */
+        binding.title.requestLayout() /* 24 is space between screen border and QrCode */
+
+        val behavior: AppBarLayout.ScrollingViewBehavior = params.behavior as ((AppBarLayout.ScrollingViewBehavior))
+        behavior.overlayTop = ((width) / 2) - 24
     }
 }
