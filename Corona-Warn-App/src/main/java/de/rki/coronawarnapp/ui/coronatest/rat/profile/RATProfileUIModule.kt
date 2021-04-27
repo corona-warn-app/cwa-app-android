40ddda52b3be967e3229379fbcc7148ee374b455
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-27 17:42:05 +0200

    Adjust Onboarding navigation

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/RATProfileUIModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/RATProfileUIModule.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import de.rki.coronawarnapp.ui.coronatest.rat.profile.create.RATProfileCreateFragment
 import de.rki.coronawarnapp.ui.coronatest.rat.profile.create.RATProfileCreateFragmentModule
+import de.rki.coronawarnapp.ui.coronatest.rat.profile.onboarding.RATProfileOnboardingFragment
+import de.rki.coronawarnapp.ui.coronatest.rat.profile.onboarding.RATProfileOnboardingFragmentModule
 import de.rki.coronawarnapp.ui.coronatest.rat.profile.qrcode.RATProfileQrCodeFragment
 import de.rki.coronawarnapp.ui.coronatest.rat.profile.qrcode.RATProfileQrCodeFragmentModule
 
@@ -15,4 +17,7 @@ internal abstract class RATProfileUIModule {
 
     @ContributesAndroidInjector(modules = [RATProfileQrCodeFragmentModule::class])
     abstract fun ratProfileQrCodeFragment(): RATProfileQrCodeFragment
+
+    @ContributesAndroidInjector(modules = [RATProfileOnboardingFragmentModule::class])
+    abstract fun ratProfileOnboardingFragment(): RATProfileOnboardingFragment
 }
