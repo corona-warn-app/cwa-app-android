2f4736245b0d1350dc32f134d8c6dcbbc29a1ee0
Matthias Urhahn <matthias.urhahn@sap.com>
2021-04-28 16:05:25 +0200

    Introduce "KnownError" concept, to remove some of the error dialog chaos.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/onboarding/OnboardingTracingFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/onboarding/OnboardingTracingFragment.kt
@@ -4,17 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.bugreporting.ui.toErrorDialogBuilder
 import de.rki.coronawarnapp.databinding.FragmentOnboardingTracingBinding
-import de.rki.coronawarnapp.exception.reporting.ErrorCodes
 import de.rki.coronawarnapp.ui.doNavigate
 import de.rki.coronawarnapp.util.DialogHelper
 import de.rki.coronawarnapp.util.di.AutoInject
-import de.rki.coronawarnapp.util.errors.stackTraceToStringList
-import de.rki.coronawarnapp.util.tryHumanReadableError
 import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
@@ -67,20 +64,7 @@ class OnboardingTracingFragment : Fragment(R.layout.fragment_onboarding_tracing)
             permissionRequest.invoke(requireActivity())
         }
         vm.ensErrorEvents.observe2(this) { error ->
-            AlertDialog.Builder(requireContext()).apply {
-                val humanReadable = error.tryHumanReadableError(requireContext())
-                setTitle(
-                    humanReadable.title
-                        ?: getString(R.string.errors_generic_headline_short) + " ${ErrorCodes.EN_PERMISSION_PROBLEM.code}"
-                )
-                setMessage(humanReadable.description.replace(". ", ".\n"))
-                setPositiveButton(R.string.submission_test_result_dialog_tracing_required_button) { _, _ -> }
-                setNeutralButton(R.string.errors_generic_button_negative) { _, _ ->
-                    AlertDialog.Builder(requireContext()).apply {
-                        setMessage(error.stackTraceToStringList().joinToString("\n\n"))
-                    }.show()
-                }
-            }.show()
+            error.toErrorDialogBuilder(requireContext()).show()
         }
     }
 
