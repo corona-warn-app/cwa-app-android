45130941b93af5226e8f80b4bf17b63d81f4aabd
Matthias Urhahn <matthias.urhahn@sap.com>
2021-04-28 10:53:07 +0200

    Incompatibility warning card on main screen (EXPOSUREAPP-5464)(COMMUNITY) (#2873)
    * Incompatibility warning card on main screen
    
    * Fix lint
    
    * Fix lint further
    
    * Really fix lint
    
    * Refactor BluetoothSupport.kt to be injectable, to allow mocking.
    
    * Unit test draft.
    
    * adapt tests
    
    * tests and refactoring
    
    * tests and refactoring
    
    * screenshots
    
    * add icon
    
    * add dimen again
    
    * change text
    
    * klint
    
    Co-authored-by: Fynn Godau <fynngodau@mailbox.org>
    Co-authored-by: chilja <chiljamgossow@gmail.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/home/HomeAdapter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/home/HomeAdapter.kt
@@ -29,6 +29,7 @@ import de.rki.coronawarnapp.tracing.ui.homecards.TracingProgressCard
 import de.rki.coronawarnapp.ui.main.home.items.CreateTraceLocationCard
 import de.rki.coronawarnapp.ui.main.home.items.FAQCard
 import de.rki.coronawarnapp.ui.main.home.items.HomeItem
+import de.rki.coronawarnapp.ui.main.home.items.IncompatibleCard
 import de.rki.coronawarnapp.util.lists.BindableVH
 import de.rki.coronawarnapp.util.lists.diffutil.AsyncDiffUtilAdapter
 import de.rki.coronawarnapp.util.lists.diffutil.AsyncDiffer
@@ -50,6 +51,7 @@ class HomeAdapter :
                 StableIdMod(data),
                 DataBinderMod<HomeItem, HomeItemVH<HomeItem, ViewBinding>>(data),
                 TypedVHCreatorMod({ data[it] is FAQCard.Item }) { FAQCard(it) },
+                TypedVHCreatorMod({ data[it] is IncompatibleCard.Item }) { IncompatibleCard(it) },
                 TypedVHCreatorMod({ data[it] is CreateTraceLocationCard.Item }) { CreateTraceLocationCard(it) },
                 TypedVHCreatorMod({ data[it] is IncreasedRiskCard.Item }) { IncreasedRiskCard(it) },
                 TypedVHCreatorMod({ data[it] is LowRiskCard.Item }) { LowRiskCard(it) },
