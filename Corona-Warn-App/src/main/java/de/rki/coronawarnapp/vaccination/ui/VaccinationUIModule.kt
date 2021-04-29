692c6a8607d7493dd318f3a66a1b5768f311c42e
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-04-29 16:04:25 +0200

    Update VaccinationUIModule.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/ui/VaccinationUIModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/ui/VaccinationUIModule.kt
@@ -2,9 +2,11 @@ package de.rki.coronawarnapp.vaccination.ui
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import de.rki.coronawarnapp.vaccination.ui.details.VaccinationDetailsFragment
+import de.rki.coronawarnapp.vaccination.ui.details.VaccinationDetailsFragmentModule
 
 @Module
 abstract class VaccinationUIModule {
-    @ContributesAndroidInjector(modules = [VaccinationTestFragmentModule::class])
-    abstract fun vaccinationTestFragment(): VaccinationTestFragment
+    @ContributesAndroidInjector(modules = [VaccinationDetailsFragmentModule::class])
+    abstract fun vaccinationDetailsFragment(): VaccinationDetailsFragment
 }
