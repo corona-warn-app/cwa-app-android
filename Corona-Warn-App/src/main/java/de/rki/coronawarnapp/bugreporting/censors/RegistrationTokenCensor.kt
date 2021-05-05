172e74965dd0c6d9f54a90f451d0c604a91971bf
Kolya Opahle <k.opahle@sap.com>
2021-05-05 15:45:12 +0200

    Removed debug check in censors that acted differently in debug vs prod mode (prod mode is new default)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/bugreporting/censors/RegistrationTokenCensor.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/bugreporting/censors/RegistrationTokenCensor.kt
@@ -4,7 +4,6 @@ import dagger.Reusable
 import de.rki.coronawarnapp.bugreporting.censors.BugCensor.Companion.toNewLogLineIfDifferent
 import de.rki.coronawarnapp.bugreporting.debuglog.LogLine
 import de.rki.coronawarnapp.coronatest.CoronaTestRepository
-import de.rki.coronawarnapp.util.CWADebug
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
@@ -22,18 +21,13 @@ class RegistrationTokenCensor @Inject constructor(
         for (token in tokens) {
             if (!entry.message.contains(token)) continue
 
-            newMessage = if (CWADebug.isDeviceForTestersBuild) {
-                newMessage.replace(token, PLACEHOLDER_TESTER + token.takeLast(27))
-            } else {
-                newMessage.replace(token, PLACEHOLDER + token.takeLast(4))
-            }
+            newMessage = newMessage.replace(token, PLACEHOLDER + token.takeLast(4))
         }
 
         return entry.toNewLogLineIfDifferent(newMessage)
     }
 
     companion object {
-        private const val PLACEHOLDER_TESTER = "########-"
         private const val PLACEHOLDER = "########-####-####-####-########"
     }
 }
