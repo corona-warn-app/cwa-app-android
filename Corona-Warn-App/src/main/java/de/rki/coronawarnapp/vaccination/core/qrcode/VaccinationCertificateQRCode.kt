d12b1af178dfe93b88e555162a44aa565406fb2f
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-05 17:08:00 +0200

    Vaccination Repository Part 1 (EXPOSUREAPP-6729) (#3058)
    * Repository WIP
    
    * Add vaccination location
    
    * Adjust label and type for country.
    
    * Vaccination storage WIP, Draft 2
    
    * Vaccination storage WIP, Draft 5
    
    * Vaccination storage WIP, Draft 6
    
    * Revert gradle change.
    
    * Vaccination storage WIP, Draft 7
    
    * Adjust vaccination certificate json schema.
    
    * Extend proof certificate with known attributes, adjust unit tests.
    
    * Error handling, TODOs, more structure, storage and tests.
    
    * LINTs
    
    * Restructure storage approach, reparsing instead of duplicate data.
    
    * Fix temporary issues.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/qrcode/VaccinationCertificateQRCode.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/qrcode/VaccinationCertificateQRCode.kt
@@ -1,9 +1,13 @@
 package de.rki.coronawarnapp.vaccination.core.qrcode
 
+import okio.ByteString
+
 // TODO
 data class VaccinationCertificateQRCode(
-    // Vaccine or prophylaxis
-    val vaccineNameId: String,
-    val vaccineMedicinalProduct: String,
-    val marketAuthorizationHolder: String,
-)
+    val parsedData: VaccinationCertificateData,
+    // COSE representation of the vaccination certificate (as byte sequence)
+    val certificateCOSE: ByteString,
+) {
+    val uniqueCertificateIdentifier: String
+        get() = parsedData.vaccinationCertificate.vaccinationDatas.single().uniqueCertificateIdentifier
+}
