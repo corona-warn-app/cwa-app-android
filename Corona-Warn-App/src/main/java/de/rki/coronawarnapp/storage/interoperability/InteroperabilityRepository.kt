a1d78aa5184f26924e45376a4c49ac3205d62fb8
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-06 16:42:46 +0200

    Improve network availability checks. * Remove static access ConnectivityHelper.kt * Add `isInternetAvailable` to NetworkStateProvider.kt * Improve NetworkStateProvider.kt unit tests * Add unit tests for Android 5 specific behavior

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/storage/interoperability/InteroperabilityRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/storage/interoperability/InteroperabilityRepository.kt
@@ -3,6 +3,9 @@ package de.rki.coronawarnapp.storage.interoperability
 import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.main.CWASettings
 import de.rki.coronawarnapp.ui.Country
+import de.rki.coronawarnapp.util.network.NetworkStateProvider
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
@@ -13,31 +16,41 @@ import javax.inject.Singleton
 @Singleton
 class InteroperabilityRepository @Inject constructor(
     private val appConfigProvider: AppConfigProvider,
-    private val settings: CWASettings
+    private val settings: CWASettings,
+    networkStateProvider: NetworkStateProvider,
 ) {
 
-    val countryList = appConfigProvider.currentConfig
-        .map { configData ->
-            try {
-                configData
-                    .supportedCountries
-                    .mapNotNull { rawCode ->
-                        val countryCode = rawCode.toLowerCase(Locale.ROOT)
-
-                        val mappedCountry = Country.values().singleOrNull { it.code == countryCode }
-                        if (mappedCountry == null) Timber.e("Unknown countrycode: %s", rawCode)
-                        mappedCountry
-                    }
-            } catch (e: Exception) {
-                Timber.e(e, "Failed to map country list.")
-                emptyList()
+    private val hasInternetFlow = networkStateProvider.networkState
+        .map { it.isInternetAvailable }
+        .distinctUntilChanged()
+        .onEach { hasInternet ->
+            // Refresh appConfig on false -> true changes
+            if (hasInternet) {
+                Timber.v("Trying app config refresh for interop country list.")
+                appConfigProvider.getAppConfig()
             }
         }
-        .onEach { Timber.d("Country list: %s", it.joinToString(",")) }
 
-    suspend fun refreshCountries() {
-        appConfigProvider.getAppConfig()
+    val countryList = combine(
+        appConfigProvider.currentConfig,
+        hasInternetFlow,
+    ) { configData, _ ->
+        try {
+            configData
+                .supportedCountries
+                .mapNotNull { rawCode ->
+                    val countryCode = rawCode.toLowerCase(Locale.ROOT)
+
+                    val mappedCountry = Country.values().singleOrNull { it.code == countryCode }
+                    if (mappedCountry == null) Timber.e("Unknown countrycode: %s", rawCode)
+                    mappedCountry
+                }
+        } catch (e: Exception) {
+            Timber.e(e, "Failed to map country list.")
+            emptyList()
+        }
     }
+        .onEach { Timber.d("Country list: %s", it.joinToString(",")) }
 
     fun saveInteroperabilityUsed() {
         settings.wasInteroperabilityShownAtLeastOnce = true
