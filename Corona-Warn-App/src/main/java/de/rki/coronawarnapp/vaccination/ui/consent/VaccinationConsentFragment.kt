9f63a8f12ad03620ab821a2410fd7997ad89fcba
Juraj Kusnier <jurajkusnier@gmail.com>
2021-05-06 07:32:52 +0200

    update VaccinationConsentFragment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/ui/consent/VaccinationConsentFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/ui/consent/VaccinationConsentFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.VaccinationConsentFragmentBinding
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBindingLazy
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
@@ -19,11 +20,16 @@ class VaccinationConsentFragment : Fragment(R.layout.vaccination_consent_fragmen
 
     private val binding: VaccinationConsentFragmentBinding by viewBindingLazy()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) =
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         with(binding) {
             toolbar.setNavigationOnClickListener { popBackStack() }
             buttonConsent.setOnClickListener {
                 viewModel.doConsent()
             }
         }
+
+        viewModel.countryList.observe2(this) {
+            binding.countryList.countries = it
+        }
+    }
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/ui/consent/VaccinationConsentViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/ui/consent/VaccinationConsentViewModel.kt
@@ -1,23 +1,27 @@
 package de.rki.coronawarnapp.vaccination.ui.consent
 
+import androidx.lifecycle.asLiveData
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.storage.interoperability.InteroperabilityRepository
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
-import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
+import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 import de.rki.coronawarnapp.vaccination.core.repository.VaccinationRepository
 
 class VaccinationConsentViewModel @AssistedInject constructor(
     private val vaccinationRepository: VaccinationRepository,
+    interoperabilityRepository: InteroperabilityRepository,
     dispatcherProvider: DispatcherProvider
-) : CWAViewModel() {
+) : CWAViewModel(dispatcherProvider) {
+
+    val countryList = interoperabilityRepository.countryList
+        .asLiveData(context = dispatcherProvider.Default)
 
     fun doConsent() {
-     //TODO: implement me
+        //TODO: implement me
     }
 
     @AssistedFactory
-    interface Factory : CWAViewModelFactory<VaccinationConsentViewModel> {
-        fun create(): VaccinationConsentViewModel
-    }
+    interface Factory : SimpleCWAViewModelFactory<VaccinationConsentViewModel>
 }
