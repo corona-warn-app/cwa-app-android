886e786c1483c0876407b45ee33a266e84b1dc46
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-07 18:08:04 +0200

    Unit tests for RA/PCRCoronaTest.isFinal

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/decoder/Base45Decoder.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/decoder/Base45Decoder.kt
@@ -1,10 +1,29 @@
+/*
+    Copyright 2021 A-SIT Plus GmbH
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Modifications Copyright (c) 2021 SAP SE or an SAP affiliate company.
+*/
 package de.rki.coronawarnapp.vaccination.decoder
 
 import java.math.BigInteger
 import javax.inject.Inject
 
-// TODO:licence?
-
+/**
+ * Based on
+ * https://github.com/ehn-digital-green-development/hcert-kotlin/blob/23203fbb71f53524ee643a9df116264f87b5b32a/src/main/kotlin/ehn/techiop/hcert/kotlin/chain/common/Base45Encoder.kt
+ */
 @OptIn(ExperimentalUnsignedTypes::class)
 class Base45Decoder @Inject constructor() {
     private val alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:"
