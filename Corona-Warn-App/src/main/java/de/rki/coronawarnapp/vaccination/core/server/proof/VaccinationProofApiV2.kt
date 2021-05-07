20ad731bbc1055c88f333527236ce7c7ba274fb2
BMItter <berndus@gmx.de>
2021-05-07 15:53:43 +0200

    Added new optional api

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofApiV2.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofApiV2.kt
@@ -8,7 +8,16 @@ import retrofit2.http.POST
 
 interface VaccinationProofApiV2 {
 
+    // Returns COSE representation (as byte sequence) of a Proof Certificate
     @Headers("Content-Type: application/cbor")
     @POST("/api/certify/v2/reissue/cbor")
     suspend fun obtainProofCertificate(@Body cose: ByteString): ByteString
+
+    // Returns string as for the QR Code of a Proof Certificate (starting with HC1: )
+    @Headers(
+        "Content-Type: application/cbor",
+        "Accept: application/cbor+base45"
+    )
+    @POST("/api/certify/v2/reissue/cbor")
+    suspend fun obtainProofCertificateBase45(@Body cose: ByteString): String
 }
