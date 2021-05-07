9e2b687b3c2faba8212f82dbe98d5f6a22e820f1
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-05-07 16:39:07 +0200

    Update QrCodeFullScreenFragment.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/qrcode/fullscreen/QrCodeFullScreenFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/qrcode/fullscreen/QrCodeFullScreenFragment.kt
@@ -9,6 +9,8 @@ import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+import android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 import android.view.View.SYSTEM_UI_FLAG_VISIBLE
 import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
 import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
@@ -45,10 +47,13 @@ class QrCodeFullScreenFragment : Fragment(R.layout.fragment_qr_code_full_screen)
 
         sharedElementEnterTransition = MaterialContainerTransform()
         sharedElementReturnTransition = MaterialContainerTransform()
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             requireActivity().window.attributes.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
+        super.onActivityCreated(savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) =
@@ -103,7 +108,7 @@ class QrCodeFullScreenFragment : Fragment(R.layout.fragment_qr_code_full_screen)
     }
 
     private fun hideSystemUI() {
-        requireActivity().window.decorView.systemUiVisibility = (
+        var flags = (
             SYSTEM_UI_FLAG_IMMERSIVE
                 or SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -111,13 +116,25 @@ class QrCodeFullScreenFragment : Fragment(R.layout.fragment_qr_code_full_screen)
                 or SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or SYSTEM_UI_FLAG_FULLSCREEN
             )
+        if (resources.getBoolean(R.bool.lightSystemUI)) {
+            flags = flags or SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) flags = flags or SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+        requireActivity().window.decorView.systemUiVisibility = flags
     }
 
     private fun showSystemUI() {
-        requireActivity().window.decorView.systemUiVisibility = (
+        var flags = (
             SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             )
+
+        if (resources.getBoolean(R.bool.lightSystemUI)) {
+            flags = flags or SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) flags = flags or SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+
+        requireActivity().window.decorView.systemUiVisibility = flags
     }
 }
