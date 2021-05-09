9ac5cd3751f53fcc306a86344b7a074e0dd7333c
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-05-09 22:22:36 +0200

    Material Alert

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentBlockingProgressDialogFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/survey/consent/SurveyConsentBlockingProgressDialogFragment.kt
@@ -2,15 +2,15 @@ package de.rki.coronawarnapp.datadonation.survey.consent
 
 import android.app.Dialog
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import de.rki.coronawarnapp.R
 
 class SurveyConsentBlockingProgressDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         isCancelable = false
-        return AlertDialog.Builder(requireContext())
+        return MaterialAlertDialogBuilder(requireContext())
             .setView(R.layout.survey_consent_blocking_progress_dialog)
             .setCancelable(false)
             .create()
