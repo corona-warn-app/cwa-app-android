36c059a3fd71c62c166d0a41a833c6893db4cfad
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-05-10 10:42:21 +0200

    merge

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/repository/storage/VaccinationContainer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/repository/storage/VaccinationContainer.kt
@@ -13,10 +13,8 @@ import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateCOSEPa
 import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateData
 import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateQRCode
 import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateV1
-import de.rki.coronawarnapp.vaccination.core.server.valueset.VaccinationValueSet
-
 import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateV1Parser
-import de.rki.coronawarnapp.vaccination.core.server.VaccinationValueSet
+import de.rki.coronawarnapp.vaccination.core.server.valueset.VaccinationValueSet
 import org.joda.time.Instant
 import org.joda.time.LocalDate
 
