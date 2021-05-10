a3b96d6fee32649cb946c37fce17a310a4be99a9
chris-cwa <chris.cwa.sap@gmail.com>
2021-05-10 17:45:06 +0200

    fixed types

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofServer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofServer.kt
@@ -3,6 +3,7 @@ package de.rki.coronawarnapp.vaccination.core.server.proof
 import dagger.Lazy
 import dagger.Reusable
 import de.rki.coronawarnapp.vaccination.core.qrcode.RawCOSEObject
+import okio.ByteString
 import javax.inject.Inject
 
 @Reusable
@@ -11,7 +12,7 @@ class VaccinationProofServer @Inject constructor(
 ) {
 
     suspend fun getProofCertificate(vaccinationCertificate: RawCOSEObject) =
-        api.get().obtainProofCertificate(vaccinationCertificate).let {
+        api.get().obtainProofCertificate(ByteString.of(*vaccinationCertificate)).let {
             object : ProofCertificateResponse {
                 override val proofCertificateData = ProofCertificateCOSEParser().parse(it)
                 override val proofCertificateCOSE = it
