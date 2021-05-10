9ac41746f2278ddc8a27a84629550d4f11e4d243
chris-cwa <chris.cwa.sap@gmail.com>
2021-05-10 15:56:08 +0200

    use api

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofServer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofServer.kt
@@ -1,18 +1,19 @@
 package de.rki.coronawarnapp.vaccination.core.server.proof
 
+import dagger.Lazy
 import dagger.Reusable
 import okio.ByteString
 import javax.inject.Inject
 
-/**
- * Talks with IBM servers?
- */
 @Reusable
-class VaccinationProofServer @Inject constructor() {
+class VaccinationProofServer @Inject constructor(
+    private val api: Lazy<VaccinationProofApiV2>
+) {
 
     suspend fun getProofCertificate(
         vaccinationCertificate: ByteString
     ): ProofCertificateResponse {
+        val obtainProofCertificateBase45: String = api.get().obtainProofCertificateBase45(vaccinationCertificate)
         throw NotImplementedError()
     }
 }
