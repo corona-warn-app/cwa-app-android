44a4ce5b7b4e95c7a980218ddfbd045e8031d79a
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-11 19:17:51 +0200

    Add test for magic header check.

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/compression/ZLIBCompressionTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/compression/ZLIBCompressionTest.kt
@@ -1,15 +1,26 @@
 package de.rki.coronawarnapp.util.compression
 
+import de.rki.coronawarnapp.util.errors.causes
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import okio.ByteString.Companion.decodeBase64
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
+import java.util.zip.DataFormatException
 
 class ZLIBCompressionTest : BaseTest() {
 
     @Test
     fun `basic decompression`() {
         ZLIBCompression().decompress(compressed).utf8() shouldBe "The Cake Is A Lie"
+    }
+
+    @Test
+    fun `invalid decompression`() {
+        val error = shouldThrow<InvalidInputException> {
+            ZLIBCompression().decompress(compressed.substring(5))
+        }
+        error.causes().first { it is DataFormatException }.message shouldBe "incorrect header check"
     }
 
     val compressed = "eJwLyUhVcE7MTlXwLFZwVPDJTAUAL3sFLQ==".decodeBase64()!!
