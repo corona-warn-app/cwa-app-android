a0cab558f51d46a71756da9fc6f696f04a62b310
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-11 11:52:39 +0200

    wip

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofApiV2.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofApiV2.kt
@@ -1,9 +1,10 @@
 package de.rki.coronawarnapp.vaccination.core.server.proof
 
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import okio.ByteString
 import retrofit2.http.Body
 import retrofit2.http.Headers
-
 import retrofit2.http.POST
 
 interface VaccinationProofApiV2 {
@@ -11,7 +12,7 @@ interface VaccinationProofApiV2 {
     // Returns COSE representation (as byte sequence) of a Proof Certificate
     @Headers("Content-Type: application/cbor")
     @POST("/api/certify/v2/reissue/cbor")
-    suspend fun obtainProofCertificate(@Body cose: ByteString): ByteString
+    suspend fun obtainProofCertificate(@Body cose: RequestBody): ResponseBody
 
     // Returns string as for the QR Code of a Proof Certificate (starting with HC1: )
     @Headers(
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofServer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/proof/VaccinationProofServer.kt
@@ -3,6 +3,11 @@ package de.rki.coronawarnapp.vaccination.core.server.proof
 import dagger.Lazy
 import dagger.Reusable
 import de.rki.coronawarnapp.vaccination.core.common.RawCOSEObject
+import de.rki.coronawarnapp.vaccination.core.qrcode.HealthCertificateCOSEDecoder
+import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateCOSEParser
+import de.rki.coronawarnapp.vaccination.core.qrcode.VaccinationCertificateV1Parser
+import okio.ByteString
+import timber.log.Timber
 import javax.inject.Inject
 
 @Reusable
@@ -10,11 +15,21 @@ class VaccinationProofServer @Inject constructor(
     private val api: Lazy<VaccinationProofApiV2>
 ) {
 
-    suspend fun getProofCertificate(vaccinationCertificate: RawCOSEObject) =
-        api.get().obtainProofCertificate(vaccinationCertificate).let {
-            object : ProofCertificateResponse {
-                override val proofCertificateData = ProofCertificateCOSEParser().parse(it)
-                override val proofCertificateCOSE = it
+    suspend fun getProofCertificate(vaccinationCertificate: RawCOSEObject): ProofCertificateResponse =
+        api.get().obtainProofCertificate(
+            vaccinationCertificate.toRequestBody()
+        ).let {
+            val data = it.byteString()
+            val data2 = VaccinationCertificateCOSEParser(
+                HealthCertificateCOSEDecoder(),
+                VaccinationCertificateV1Parser()
+            ).parse(data.toByteArray())
+            Timber.v("%s", data2)
+            return object : ProofCertificateResponse {
+                override val proofCertificateData: ProofCertificateData
+                    get() = TODO("Not yet implemented")
+                override val proofCertificateCOSE: ByteString
+                    get() = TODO("Not yet implemented")
             }
         }
 }
