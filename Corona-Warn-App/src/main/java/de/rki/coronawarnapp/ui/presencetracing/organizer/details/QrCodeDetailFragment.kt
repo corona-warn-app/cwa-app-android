81840731df96c88cd3d6073b21439af01793f6b4
Mohamed <mohamed.metwalli@sap.com>
2021-05-12 14:50:06 +0200

    Qr Code partially scrolled (EXPOSUREAPP-6541) (#3110)
    * Full screen qr-code that adapts to theme
    
    * Revert color inversion
    
    * Immersive mode
    
    * Small cleaning
    
    * Reset flags
    
    * Handle cutout
    
    * Lint
    
    * Update QrCodeFullScreenFragment.kt
    
    * Finally works
    
    * :japanese_goblin: lint
    
    * Fix navigation bar color on old Android versions
    
    * Polishing
    
    * Revert "Polishing"
    
    This reverts commit e2f2be1e4b298ca0731d0b0688a6607cd6cabaa2.
    
    * Revert "Revert "Polishing""
    
    This reverts commit c99408dc5a46ec53161aac02f6cbebdd3f9c0486.
    
    * Clear flags
    
    * Show toolbar
    
    * Improving
    
    * Shadow view
    
    * finally works
    
    * works now?
    
    * Animate
    
    * Code 🧑‍💻 polishing
    
    * Remove margin
    
    * PR comments
    
    * update shadow
    
    * Use divider
    
    Co-authored-by: Juraj Kusnier <jurajkusnier@gmail.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/presencetracing/organizer/details/QrCodeDetailFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/presencetracing/organizer/details/QrCodeDetailFragment.kt
@@ -7,7 +7,10 @@ import android.widget.LinearLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.core.view.isGone
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.transition.MaterialContainerTransform
@@ -15,6 +18,7 @@ import com.google.android.material.transition.MaterialSharedAxis
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.TraceLocationOrganizerQrCodeDetailFragmentBinding
 import de.rki.coronawarnapp.ui.view.onOffsetChange
+import de.rki.coronawarnapp.ui.qrcode.fullscreen.QrCodeFullScreenFragmentArgs
 import de.rki.coronawarnapp.util.ContextExtensions.getDrawableCompat
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.doNavigate
@@ -57,7 +61,9 @@ class QrCodeDetailFragment : Fragment(R.layout.trace_location_organizer_qr_code_
             appBarLayout.onOffsetChange { titleAlpha, subtitleAlpha ->
                 title.alpha = titleAlpha
                 subtitle.alpha = subtitleAlpha
+                checkShadowVisibility()
             }
+            root.viewTreeObserver.addOnGlobalLayoutListener { checkShadowVisibility() }
 
             toolbar.apply {
                 navigationIcon = context.getDrawableCompat(R.drawable.ic_close_white)
@@ -77,6 +83,10 @@ class QrCodeDetailFragment : Fragment(R.layout.trace_location_organizer_qr_code_
             }
 
             root.transitionName = navArgs.traceLocationId.toString()
+
+            qrCodeImage.setOnClickListener {
+                viewModel.openFullScreen()
+            }
         }
 
         viewModel.routeToScreen.observe2(this) {
@@ -92,6 +102,12 @@ class QrCodeDetailFragment : Fragment(R.layout.trace_location_organizer_qr_code_
 
                 is QrCodeDetailNavigationEvents.NavigateToQrCodePosterFragment -> doNavigate(
                     QrCodeDetailFragmentDirections.actionQrCodeDetailFragmentToQrCodePosterFragment(it.locationId)
+                )
+                is QrCodeDetailNavigationEvents.NavigateToFullScreenQrCode -> findNavController().navigate(
+                    R.id.action_global_qrCodeFullScreenFragment,
+                    QrCodeFullScreenFragmentArgs(it.qrcodeText).toBundle(),
+                    null,
+                    FragmentNavigatorExtras(binding.qrCodeImage to binding.qrCodeImage.transitionName)
                 )
             }
         }
@@ -142,6 +158,10 @@ class QrCodeDetailFragment : Fragment(R.layout.trace_location_organizer_qr_code_
                 }
             }
         }
+    }
+
+    private fun TraceLocationOrganizerQrCodeDetailFragmentBinding.checkShadowVisibility() {
+        shadowView.isInvisible = nestedScrollView.bottom <= shadowView.y
     }
 
     private fun setToolbarOverlay() {
