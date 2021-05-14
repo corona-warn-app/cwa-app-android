7029d6212c61d5aeaae724a26a84d11aa2e7bf8d
BMItter <berndus@gmx.de>
2021-05-14 17:37:56 +0200

    user errors wont crash the app

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/repository/ValueSetsRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/repository/ValueSetsRepository.kt
@@ -2,21 +2,30 @@ package de.rki.coronawarnapp.vaccination.core.repository
 
 import dagger.Reusable
 import de.rki.coronawarnapp.util.coroutine.AppScope
+import de.rki.coronawarnapp.util.flow.shareLatest
 import de.rki.coronawarnapp.vaccination.core.repository.storage.ValueSetsStorage
 import de.rki.coronawarnapp.vaccination.core.server.valueset.DefaultVaccinationValueSet
 import de.rki.coronawarnapp.vaccination.core.server.valueset.VaccinationServer
 import de.rki.coronawarnapp.vaccination.core.server.valueset.VaccinationValueSet
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.util.Locale
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @Reusable
 class ValueSetsRepository @Inject constructor(
@@ -30,8 +39,7 @@ class ValueSetsRepository @Inject constructor(
     val latestValueSet: Flow<VaccinationValueSet?> = internalFlow
         .filterNotNull()
         .map {
-            fromServer(it) ?: fromLocalStorage() ?: fromServer(Locale.ENGLISH) ?: createEmptyValueSet(it)
-            // Return empty value set as last resort
+            fromServer(it) ?: fromLocalStorage() ?: fromServer(Locale.ENGLISH) ?: createEmptyValueSet(it) // Return empty value set as last resort
         }
         .stateIn(
             scope = appScope,
@@ -44,17 +52,23 @@ class ValueSetsRepository @Inject constructor(
         internalFlow.value = languageCode
     }
 
-    private suspend fun fromServer(languageCode: Locale): VaccinationValueSet? {
+    private suspend fun fromServer(languageCode: Locale): VaccinationValueSet? = try {
         Timber.d("fromServer(languageCode=%s)", languageCode)
-        return vaccinationServer.getVaccinationValueSets(languageCode = languageCode)?.also {
+        vaccinationServer.getVaccinationValueSets(languageCode = languageCode)?.also {
             Timber.d("Saving new value sets %s", it)
             valueSetsStorage.vaccinationValueSet = it
         }
+    } catch (e: Exception) {
+        Timber.w(e, "fromServer(): %s", e.message)
+        null
     }
 
-    private fun fromLocalStorage(): VaccinationValueSet? {
+    private fun fromLocalStorage(): VaccinationValueSet? = try {
         Timber.d("fromLocalStorage()")
-        return valueSetsStorage.vaccinationValueSet
+        valueSetsStorage.vaccinationValueSet
+    } catch (e: Exception) {
+        Timber.w(e, "fromLocalStorage(): %s", e.message)
+        null
     }
 
     private fun createEmptyValueSet(languageCode: Locale) = DefaultVaccinationValueSet(
