390a6e7e59a0f936c8da70a9751084e46e17b1a0
BMItter <berndus@gmx.de>
2021-05-17 12:00:59 +0200

    Ignore server cache headers

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/valueset/VaccinationValueSetModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/vaccination/core/server/valueset/VaccinationValueSetModule.kt
@@ -8,9 +8,13 @@ import de.rki.coronawarnapp.environment.download.DownloadCDNHttpClient
 import de.rki.coronawarnapp.environment.download.DownloadCDNServerUrl
 import de.rki.coronawarnapp.util.di.AppContext
 import okhttp3.Cache
+import okhttp3.CacheControl
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 @Module
 class VaccinationValueSetModule {
@@ -33,6 +37,7 @@ class VaccinationValueSetModule {
         @ValueSet cache: Cache
     ): VaccinationValueSetApiV1 {
         val client = httpClient.newBuilder()
+            .addNetworkInterceptor(CacheInterceptor())
             .cache(cache)
             .build()
 
@@ -41,6 +46,24 @@ class VaccinationValueSetModule {
             .baseUrl(url)
             .build()
             .create(VaccinationValueSetApiV1::class.java)
+    }
+
+    private class CacheInterceptor() : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val response = chain.proceed(chain.request())
+
+            val cacheControl = CacheControl.Builder()
+                .maxAge(300, TimeUnit.SECONDS)
+                .build()
+
+            // We cache as we please
+            val cacheHeader = "Cache-Control"
+            return response.newBuilder()
+                .removeHeader("Pragma")
+                .removeHeader(cacheHeader)
+                .addHeader(cacheHeader, cacheControl.toString())
+                .build()
+        }
     }
 
     companion object {
