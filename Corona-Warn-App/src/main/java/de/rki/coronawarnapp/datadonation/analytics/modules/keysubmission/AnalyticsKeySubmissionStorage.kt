0fe87ae980510dd3ce605b5aa43fd497cdab8569
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-05-21 16:01:48 +0200

    fix shared prefs

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/modules/keysubmission/AnalyticsKeySubmissionStorage.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/datadonation/analytics/modules/keysubmission/AnalyticsKeySubmissionStorage.kt
@@ -8,102 +8,99 @@ import javax.inject.Inject
 
 class AnalyticsPCRKeySubmissionStorage @Inject constructor(
     @AppContext context: Context
-) : AnalyticsKeySubmissionStorage(context) {
-    override val sharedPrefKey = "analytics_key_submission_localdata"
-}
+) : AnalyticsKeySubmissionStorage(context, "")
 
 class AnalyticsRAKeySubmissionStorage @Inject constructor(
     @AppContext context: Context
-) : AnalyticsKeySubmissionStorage(context) {
-    override val sharedPrefKey = "analytics_ra_key_submission_localdata"
-}
+) : AnalyticsKeySubmissionStorage(context, "_RAT")
 
 abstract class AnalyticsKeySubmissionStorage(
-    val context: Context
+    val context: Context,
+    val sharedPrefKeySuffix: String
 ) {
-    abstract val sharedPrefKey: String
+    private val sharedPrefKey: String = "analytics_key_submission_localdata"
 
     private val prefs by lazy {
-        context.getSharedPreferences(sharedPrefKey, Context.MODE_PRIVATE)
+        context.getSharedPreferences(sharedPrefKey + sharedPrefKeySuffix, Context.MODE_PRIVATE)
     }
 
     val testResultReceivedAt = prefs.createFlowPreference(
-        key = "analytics_key_submission_testResultReceivedAt",
+        key = "analytics_key_submission_testResultReceivedAt" + sharedPrefKeySuffix,
         defaultValue = -1L
     )
 
     val testRegisteredAt = prefs.createFlowPreference(
-        key = "analytics_key_submission_testRegisteredAt",
+        key = "analytics_key_submission_testRegisteredAt" + sharedPrefKeySuffix,
         defaultValue = -1L
     )
 
     val submitted = prefs.createFlowPreference(
-        key = "analytics_key_submission_submitted",
+        key = "analytics_key_submission_submitted" + sharedPrefKeySuffix,
         defaultValue = false
     )
 
     val submittedAt = prefs.createFlowPreference(
-        key = "analytics_key_submission_submittedAt",
+        key = "analytics_key_submission_submittedAt" + sharedPrefKeySuffix,
         defaultValue = -1L
     )
 
     val submittedInBackground = prefs.createFlowPreference(
-        key = "analytics_key_submission_submittedInBackground",
+        key = "analytics_key_submission_submittedInBackground" + sharedPrefKeySuffix,
         defaultValue = false
     )
 
     val submittedAfterCancel = prefs.createFlowPreference(
-        key = "analytics_key_submission_submittedAfterCancel",
+        key = "analytics_key_submission_submittedAfterCancel" + sharedPrefKeySuffix,
         defaultValue = false
     )
 
     val submittedAfterSymptomFlow = prefs.createFlowPreference(
-        key = "analytics_key_submission_submittedAfterSymptomFlow",
+        key = "analytics_key_submission_submittedAfterSymptomFlow" + sharedPrefKeySuffix,
         defaultValue = false
     )
 
     val lastSubmissionFlowScreen = prefs.createFlowPreference(
-        key = "analytics_key_submission_lastSubmissionFlowScreen",
+        key = "analytics_key_submission_lastSubmissionFlowScreen" + sharedPrefKeySuffix,
         defaultValue = Screen.UNKNOWN.code
     )
 
     val advancedConsentGiven = prefs.createFlowPreference(
-        key = "analytics_key_submission_advancedConsentGiven",
+        key = "analytics_key_submission_advancedConsentGiven" + sharedPrefKeySuffix,
         defaultValue = false
     )
 
     val registeredWithTeleTAN = prefs.createFlowPreference(
-        key = "analytics_key_submission_registeredWithTeleTAN",
+        key = "analytics_key_submission_registeredWithTeleTAN" + sharedPrefKeySuffix,
         defaultValue = false
     )
 
     val riskLevelAtTestRegistration = prefs.createFlowPreference(
-        key = "analytics_key_submission_riskLevelAtTestRegistration",
+        key = "analytics_key_submission_riskLevelAtTestRegistration" + sharedPrefKeySuffix,
         defaultValue = -1
     )
 
     val ewHoursSinceHighRiskWarningAtTestRegistration = prefs.createFlowPreference(
-        key = "analytics_key_submission_hoursSinceHighRiskWarningAtTestRegistration",
+        key = "analytics_key_submission_hoursSinceHighRiskWarningAtTestRegistration" + sharedPrefKeySuffix,
         defaultValue = -1
     )
 
     val ewDaysSinceMostRecentDateAtRiskLevelAtTestRegistration = prefs.createFlowPreference(
-        key = "analytics_key_submission_daysSinceMostRecentDateAtRiskLevelAtTestRegistration",
+        key = "analytics_key_submission_daysSinceMostRecentDateAtRiskLevelAtTestRegistration" + sharedPrefKeySuffix,
         defaultValue = -1
     )
 
     val ptHoursSinceHighRiskWarningAtTestRegistration = prefs.createFlowPreference(
-        key = "analytics_key_submission_hoursSincePtHighRiskWarningAtTestRegistration",
+        key = "analytics_key_submission_hoursSincePtHighRiskWarningAtTestRegistration" + sharedPrefKeySuffix,
         defaultValue = -1
     )
 
     val ptDaysSinceMostRecentDateAtRiskLevelAtTestRegistration = prefs.createFlowPreference(
-        key = "analytics_key_submission_daysSinceMostRecentDateAtPtRiskLevelAtTestRegistration",
+        key = "analytics_key_submission_daysSinceMostRecentDateAtPtRiskLevelAtTestRegistration$sharedPrefKeySuffix",
         defaultValue = -1
     )
 
     val submittedWithCheckIns = prefs.createFlowPreference(
-        key = "analytics_key_submission_submittedWithCheckIns",
+        key = "analytics_key_submission_submittedWithCheckIns$sharedPrefKeySuffix",
         defaultValue = false
     )
 
