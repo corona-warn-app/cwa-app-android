77775df05ac804fffd8d20f0ecf7d651f04cb115
Lukas Lechner <office@lukaslechner.com>
2021-05-24 14:53:41 +0200

    Adjust RatProfileCensorTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/submission/RatProfileCensorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/submission/RatProfileCensorTest.kt
@@ -8,6 +8,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.joda.time.format.DateTimeFormat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -61,9 +62,17 @@ internal class RatProfileCensorTest : BaseTest() {
         censor.checkLog(logLine)!!.string shouldBe
             "Mister RAT-Profile/FirstName who is also known as RAT-Profile/LastName and is born on RAT-Profile/DateOfBirth lives in RAT-Profile/Street, " +
             "RAT-Profile/Zip-Code in the beautiful city of RAT-Profile/City. You can reach him by phone: RAT-Profile/Phone or email: RAT-Profile/eMail"
+        }
 
-        // censoring should still work after the user deletes his profile
-        every { ratProfileSettings.profile.flow } returns flowOf(null)
+    @Test
+    fun `censoring should still work after the user deletes his profile`() = runBlockingTest {
+        every { ratProfileSettings.profile.flow } returns flowOf(profile, null)
+
+        val censor = createInstance()
+
+        val logLine =
+            "Mister First name who is also known as Last name and is born on 1950-08-01 lives in Main street, " +
+                "12132 in the beautiful city of London. You can reach him by phone: 111111111 or email: email@example.com"
 
         censor.checkLog(logLine)!!.string shouldBe
             "Mister RAT-Profile/FirstName who is also known as RAT-Profile/LastName and is born on RAT-Profile/DateOfBirth lives in RAT-Profile/Street, " +
