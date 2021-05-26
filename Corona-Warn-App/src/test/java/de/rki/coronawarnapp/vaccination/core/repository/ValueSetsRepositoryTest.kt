d36678b83fe1cc63fe97fd6cae5c55f63b6493a4
BMItter <Berndus@gmx.de>
2021-05-26 11:16:07 +0200

    Improved preference handling / thread safety (DEV) (#3252)
    * Change to HotDataFlow
    
    * Created test data for value sets, Adjusted storage tests, Also save null values
    
    * Adjusted repo test
    
    * Only emit when content changed
    
    * ktlint & sourcecheck
    
    * Thread safety
    
    * Return empty value set as default
    
    * detekt and sourceclean
    
    Co-authored-by: harambasicluka <64483219+harambasicluka@users.noreply.github.com>

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/vaccination/core/repository/ValueSetsRepositoryTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/vaccination/core/repository/ValueSetsRepositoryTest.kt
@@ -1,11 +1,11 @@
 package de.rki.coronawarnapp.vaccination.core.repository
 
-import de.rki.coronawarnapp.util.preferences.FlowPreference
+import de.rki.coronawarnapp.vaccination.core.ValueSetTestData.emptyValueSetEn
+import de.rki.coronawarnapp.vaccination.core.ValueSetTestData.valueSetDe
+import de.rki.coronawarnapp.vaccination.core.ValueSetTestData.valueSetEn
 import de.rki.coronawarnapp.vaccination.core.repository.storage.ValueSetsStorage
-import de.rki.coronawarnapp.vaccination.core.server.valueset.DefaultVaccinationValueSet
 import de.rki.coronawarnapp.vaccination.core.server.valueset.VaccinationServer
-import de.rki.coronawarnapp.vaccination.core.server.valueset.VaccinationValueSet
-import io.kotest.matchers.shouldBe
+import de.rki.coronawarnapp.vaccination.core.validateValues
 import io.mockk.MockKAnnotations
 import io.mockk.Ordering
 import io.mockk.coEvery
@@ -14,13 +14,13 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.runs
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
-import testhelpers.preferences.mockFlowPreference
+import testhelpers.TestDispatcherProvider
+import testhelpers.coroutines.runBlockingTest2
 import java.util.Locale
 
 class ValueSetsRepositoryTest : BaseTest() {
@@ -28,124 +28,68 @@ class ValueSetsRepositoryTest : BaseTest() {
     @MockK lateinit var vaccinationServer: VaccinationServer
     @MockK lateinit var valueSetsStorage: ValueSetsStorage
 
-    private val testScope = TestCoroutineScope()
+    private val testDispatcherProvider = TestDispatcherProvider()
 
-    private val emptyValueSetEN = createValueSet(languageCode = Locale.ENGLISH)
-    private val valueSetPref: FlowPreference<ValueSetsStorage.StoredVaccinationValueSet> =
-        mockFlowPreference(emptyValueSetEN.toStoredVaccinationValueSet())
-
-    private val valueSetEN = createValueSet(
-        languageCode = Locale.ENGLISH,
-        vpItems = listOf(
-            DefaultVaccinationValueSet.DefaultValueSet.DefaultItem(
-                key = "1119305005",
-                displayText = "Vaccine-Name"
-            )
-        ),
-        mpItems = listOf(
-            DefaultVaccinationValueSet.DefaultValueSet.DefaultItem(
-                key = "EU/1/21/1529",
-                displayText = "MedicalProduct-Name"
-            )
-        ),
-        maItems = listOf(
-            DefaultVaccinationValueSet.DefaultValueSet.DefaultItem(
-                key = "ORG-100001699",
-                displayText = "Manufactorer-Name"
-            )
-        )
-    )
-
-    private val valueSetDE = createValueSet(
-        languageCode = Locale.GERMAN,
-        vpItems = listOf(
-            DefaultVaccinationValueSet.DefaultValueSet.DefaultItem(
-                key = "1119305005",
-                displayText = "Impfstoff-Name"
-            )
-        ),
-        mpItems = listOf(
-            DefaultVaccinationValueSet.DefaultValueSet.DefaultItem(
-                key = "EU/1/21/1529",
-                displayText = "Arzneimittel-Name"
-            )
-        ),
-        maItems = listOf(
-            DefaultVaccinationValueSet.DefaultValueSet.DefaultItem(
-                key = "ORG-100001699",
-                displayText = "Hersteller-Name"
-            )
-        )
-    )
-
-    private fun createValueSet(
-        languageCode: Locale,
-        vpItems: List<DefaultVaccinationValueSet.DefaultValueSet.DefaultItem> = emptyList(),
-        mpItems: List<DefaultVaccinationValueSet.DefaultValueSet.DefaultItem> = emptyList(),
-        maItems: List<DefaultVaccinationValueSet.DefaultValueSet.DefaultItem> = emptyList()
-    ) = DefaultVaccinationValueSet(
-        languageCode = languageCode,
-        vp = DefaultVaccinationValueSet.DefaultValueSet(items = vpItems),
-        mp = DefaultVaccinationValueSet.DefaultValueSet(items = mpItems),
-        ma = DefaultVaccinationValueSet.DefaultValueSet(items = maItems)
-    )
-
-    private fun createInstance() = ValueSetsRepository(
+    private fun createInstance(scope: CoroutineScope) = ValueSetsRepository(
         vaccinationServer = vaccinationServer,
         valueSetsStorage = valueSetsStorage,
-        scope = testScope
+        scope = scope,
+        dispatcherProvider = testDispatcherProvider
     )
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
         coEvery { vaccinationServer.getVaccinationValueSets(any()) } returns null
-        coEvery { vaccinationServer.getVaccinationValueSets(languageCode = Locale.ENGLISH) } returns valueSetEN
-        coEvery { vaccinationServer.getVaccinationValueSets(languageCode = Locale.GERMAN) } returns valueSetDE
+        coEvery { vaccinationServer.getVaccinationValueSets(languageCode = Locale.ENGLISH) } returns valueSetEn
+        coEvery { vaccinationServer.getVaccinationValueSets(languageCode = Locale.GERMAN) } returns valueSetDe
         every { vaccinationServer.clear() } just runs
 
-        every { valueSetsStorage.valueSet } returns valueSetPref
-        every { valueSetsStorage.clear() } just runs
+        every { valueSetsStorage.vaccinationValueSet = any() } just runs
+        every { valueSetsStorage.vaccinationValueSet } returns emptyValueSetEn
     }
 
     @Test
-    fun `successful update for de`() = runBlockingTest {
-        createInstance().run {
+    fun `successful update for de`() = runBlockingTest2(ignoreActive = true) {
+        createInstance(this).run {
             triggerUpdateValueSet(languageCode = Locale.GERMAN)
             latestValueSet.first()
-        }.also { it.validateValues(valueSetDE) }
+        }.also { it.validateValues(valueSetDe) }
 
-        coVerify(exactly = 1) {
+        coVerify {
             vaccinationServer.getVaccinationValueSets(languageCode = Locale.GERMAN)
+            valueSetsStorage.vaccinationValueSet = valueSetDe
         }
 
         coVerify(exactly = 0) {
             vaccinationServer.getVaccinationValueSets(languageCode = Locale.ENGLISH)
+            valueSetsStorage.vaccinationValueSet = valueSetEn
         }
     }
 
     @Test
-    fun `fallback to en`() = runBlockingTest {
-        createInstance().run {
+    fun `fallback to en`() = runBlockingTest2(ignoreActive = true) {
+        createInstance(this).run {
             triggerUpdateValueSet(languageCode = Locale.FRENCH)
             latestValueSet.first()
-        }.also { it.validateValues(valueSetEN) }
+        }.also { it.validateValues(valueSetEn) }
 
         coVerify(ordering = Ordering.ORDERED) {
             vaccinationServer.getVaccinationValueSets(languageCode = Locale.FRENCH)
             vaccinationServer.getVaccinationValueSets(languageCode = Locale.ENGLISH)
+            valueSetsStorage.vaccinationValueSet = valueSetEn
         }
     }
 
     @Test
-    fun `server returns nothing`() = runBlockingTest {
+    fun `server returns nothing`() = runBlockingTest2(ignoreActive = true) {
         coEvery { vaccinationServer.getVaccinationValueSets(languageCode = Locale.GERMAN) } returns null
         coEvery { vaccinationServer.getVaccinationValueSets(languageCode = Locale.ENGLISH) } returns null
 
-        createInstance().run {
+        createInstance(this).run {
             triggerUpdateValueSet(languageCode = Locale.GERMAN)
             latestValueSet.first()
-        }.also { it.validateValues(emptyValueSetEN) }
+        }.also { it.validateValues(emptyValueSetEn) }
 
         coVerify(ordering = Ordering.ORDERED) {
             vaccinationServer.getVaccinationValueSets(languageCode = Locale.GERMAN)
@@ -154,37 +98,16 @@ class ValueSetsRepositoryTest : BaseTest() {
     }
 
     @Test
-    fun `mapping is to stored version is correct`() {
-        valueSetDE.also { it.toStoredVaccinationValueSet().validateValues(it) }
-        valueSetEN.also { it.toStoredVaccinationValueSet().validateValues(it) }
-        emptyValueSetEN.also { it.toStoredVaccinationValueSet().validateValues(it) }
-    }
-
-    @Test
-    fun `clear data of server and local storage`() {
-        createInstance().run {
+    fun `clear data of server and local storage`() = runBlockingTest2(ignoreActive = true) {
+        createInstance(this).run {
             clear()
 
-            coVerify(exactly = 1) {
+            latestValueSet.first().validateValues(emptyValueSetEn)
+
+            coVerify {
                 vaccinationServer.clear()
-                valueSetsStorage.clear()
+                valueSetsStorage.vaccinationValueSet = emptyValueSetEn
             }
-        }
-    }
-
-    private fun VaccinationValueSet.validateValues(v2: VaccinationValueSet) {
-        languageCode shouldBe v2.languageCode
-        vp.validateValues(v2.vp)
-        mp.validateValues(v2.mp)
-        ma.validateValues(v2.ma)
-    }
-
-    private fun VaccinationValueSet.ValueSet.validateValues(v2: VaccinationValueSet.ValueSet) {
-        items.forEachIndexed { index, item1 ->
-            val item2 = v2.items[index]
-
-            item1.key shouldBe item2.key
-            item1.displayText shouldBe item2.displayText
         }
     }
 }
