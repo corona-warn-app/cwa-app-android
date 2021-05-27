c0c5e1f7a043821fe4dc8f9036dd1c00807327f8
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-05-27 16:59:27 +0200

    Show dialogs and adjust text

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/greencertificate/RequestGreenCertificateFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/greencertificate/RequestGreenCertificateFragment.kt
@@ -3,15 +3,23 @@ package de.rki.coronawarnapp.ui.submission.greencertificate
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.coronatest.type.CoronaTest
 import de.rki.coronawarnapp.databinding.FragmentRequestGreenCertificateBinding
+import de.rki.coronawarnapp.util.TimeAndDateExtensions.toDayFormat
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
+import org.joda.time.LocalDate
 import javax.inject.Inject
 
-class RequestGreenCertificateFragment : Fragment(R.layout.fragment_request_green_certificate) {
+class RequestGreenCertificateFragment : Fragment(R.layout.fragment_request_green_certificate), AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
     private val viewModel by cwaViewModelsAssisted<RequestGreenCertificateViewModel>(
@@ -24,6 +32,39 @@ class RequestGreenCertificateFragment : Fragment(R.layout.fragment_request_green
     private val binding by viewBinding<FragmentRequestGreenCertificateBinding>()
     private val args by navArgs<RequestGreenCertificateFragmentArgs>()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) =
+        with(binding) {
+            val isPCR = args.testType == CoronaTest.Type.PCR
+            birthDateGroup.isVisible = isPCR
+            privacyCard.pcrExtraBullet.isVisible = isPCR
+
+            toolbar.setNavigationOnClickListener { showDialog() }
+            buttonConsent.setOnClickListener { popBackStack() }
+            buttonCancel.setOnClickListener { popBackStack() }
+            dateInputEdit.setOnClickListener { openDatePicker() }
+        }
+
+    private fun showDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.request_gc_dialog_title)
+            .setMessage(R.string.request_gc_dialog_message)
+            .setNegativeButton(R.string.request_gc_dialog_positive_button) { _, _ -> }
+            .setPositiveButton(R.string.request_gc_dialog_negative_button) { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun openDatePicker() {
+        MaterialDatePicker.Builder
+            .datePicker()
+            .build()
+            .apply {
+                addOnPositiveButtonClickListener { timestamp ->
+                    val localDate = LocalDate(timestamp)
+                    binding.dateInputEdit.setText(localDate.toDayFormat())
+                    viewModel.birthDateChanged(localDate)
+                }
+            }
+            .show(childFragmentManager, "RATProfileCreateFragment.MaterialDatePicker")
     }
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/greencertificate/RequestGreenCertificateViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/greencertificate/RequestGreenCertificateViewModel.kt
@@ -6,10 +6,14 @@ import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.coronatest.type.CoronaTest
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
+import org.joda.time.LocalDate
 
 class RequestGreenCertificateViewModel @AssistedInject constructor(
     @Assisted private val testType: CoronaTest.Type,
 ) : CWAViewModel() {
+
+    fun birthDateChanged(localDate: LocalDate) {
+    }
 
     @AssistedFactory
     interface Factory : CWAViewModelFactory<RequestGreenCertificateViewModel> {
