ea85ac503376f1081594319bcb05db8e0c761dd4
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-28 13:18:11 +0200

    Fix date parser pattern.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/coronatest/type/common/DateOfBirthKey.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/coronatest/type/common/DateOfBirthKey.kt
@@ -1,11 +1,9 @@
 package de.rki.coronawarnapp.coronatest.type.common
 
-import dagger.Reusable
 import de.rki.coronawarnapp.util.HashExtensions.toSHA256
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 
-@Reusable
 data class DateOfBirthKey constructor(
     private val testGuid: String,
     private val dateOfBirth: LocalDate,
@@ -17,11 +15,11 @@ data class DateOfBirthKey constructor(
 
     val key by lazy {
         val dobFormatted = dateOfBirth.toString(DOB_FORMATTER)
-        val keyHash = "$testGuid$dobFormatted".toSHA256()
+        val keyHash = "${testGuid}$dobFormatted".toSHA256()
         "x${keyHash.substring(1)}"
     }
 
     companion object {
-        private val DOB_FORMATTER = DateTimeFormat.forPattern("DDMMYYYY")
+        private val DOB_FORMATTER = DateTimeFormat.forPattern("ddMMYYYY")
     }
 }
