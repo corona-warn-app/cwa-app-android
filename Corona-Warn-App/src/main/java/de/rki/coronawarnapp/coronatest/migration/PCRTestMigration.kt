d114420b321a23bb6342e9665c48d931ad817bdb
Matthias Urhahn <matthias.urhahn@sap.com>
2021-05-28 12:26:32 +0200

    Remove explicit assignment, defaults are sufficient.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/coronatest/migration/PCRTestMigration.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/coronatest/migration/PCRTestMigration.kt
@@ -75,8 +75,6 @@ class PCRTestMigration @Inject constructor(
             isViewed = hasViewedTestResult,
             isAdvancedConsentGiven = hasGivenConsent,
             isResultAvailableNotificationSent = testResultNotificationSent,
-            isDccConsentGiven = false,
-            isDccDataSetCreated = false,
         )
         return setOf(legacyPCRTest).also {
             Timber.tag(TAG).d("Offering converted legacy CoronaTest: %s", it)
