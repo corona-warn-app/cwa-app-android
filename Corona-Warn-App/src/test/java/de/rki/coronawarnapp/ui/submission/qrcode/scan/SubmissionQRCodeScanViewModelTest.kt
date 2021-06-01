c8b5b6fd9441dce0c2d6fb3d74df4fe6a54149d1
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-06-01 13:10:33 +0200

    Fix test

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/qrcode/scan/SubmissionQRCodeScanViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/qrcode/scan/SubmissionQRCodeScanViewModelTest.kt
@@ -22,6 +22,7 @@ import io.mockk.just
 import io.mockk.verify
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runBlockingTest
+import org.joda.time.Instant
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -110,8 +111,8 @@ class SubmissionQRCodeScanViewModelTest : BaseTest() {
 
             createViewModel().registerCoronaTest(rawResult = "")
 
-            verify(exactly = 1) { analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.PCR) }
             verify(exactly = 0) {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.PCR)
                 analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.RAPID_ANTIGEN)
             }
         }
@@ -127,9 +128,52 @@ class SubmissionQRCodeScanViewModelTest : BaseTest() {
 
             createViewModel().registerCoronaTest(rawResult = "")
 
-            verify(exactly = 1) { analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.PCR) }
+            verify(exactly = 0) {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.PCR)
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.RAPID_ANTIGEN)
+            }
+        }
+
+    @Test
+    fun `registerCoronaTest() should call analyticsKeySubmissionCollector for RAT tests - no-dcc support`() =
+        runBlockingTest {
+            val coronaTestQRCode = CoronaTestQRCode.RapidAntigen(
+                hash = "123456-12345678-1234-4DA7-B166-B86D85475064",
+                createdAt = Instant.EPOCH,
+                isDccSupportedByPoc = false
+            )
+
+            every { qrCodeValidator.validate(any()) } returns coronaTestQRCode
+            every { analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any()) } just Runs
+            coEvery { qrCodeRegistrationStateProcessor.startQrCodeRegistration(any(), any()) } just Runs
+
+            createViewModel().registerCoronaTest(rawResult = "")
+
+            verify(exactly = 1) {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.RAPID_ANTIGEN)
+            }
+            verify(exactly = 0) {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.PCR)
+            }
+        }
+
+    @Test
+    fun `registerCoronaTest() should Not call analyticsKeySubmissionCollector for RAT tests - dcc support`() =
+        runBlockingTest {
+            val coronaTestQRCode = CoronaTestQRCode.RapidAntigen(
+                hash = "123456-12345678-1234-4DA7-B166-B86D85475064",
+                createdAt = Instant.EPOCH,
+                isDccSupportedByPoc = true
+            )
+
+            every { qrCodeValidator.validate(any()) } returns coronaTestQRCode
+            every { analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any()) } just Runs
+            coEvery { qrCodeRegistrationStateProcessor.startQrCodeRegistration(any(), any()) } just Runs
+
+            createViewModel().registerCoronaTest(rawResult = "")
             verify(exactly = 0) {
                 analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.RAPID_ANTIGEN)
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(CoronaTest.Type.PCR)
             }
         }
 }
