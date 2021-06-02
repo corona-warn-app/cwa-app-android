0dd927ed14ac0e9a7d2cf95e38241c0b38bad198
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-06-02 12:52:57 +0200

    Update RequestCovidCertificateViewModelTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/greencertificate/RequestCovidCertificateViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/submission/greencertificate/RequestCovidCertificateViewModelTest.kt
@@ -3,6 +3,7 @@ package de.rki.coronawarnapp.ui.submission.greencertificate
 import androidx.lifecycle.MutableLiveData
 import de.rki.coronawarnapp.coronatest.CoronaTestRepository
 import de.rki.coronawarnapp.coronatest.qrcode.CoronaTestQRCode
+import de.rki.coronawarnapp.coronatest.type.CoronaTest
 import de.rki.coronawarnapp.datadonation.analytics.modules.keysubmission.AnalyticsKeySubmissionCollector
 import de.rki.coronawarnapp.submission.SubmissionRepository
 import de.rki.coronawarnapp.ui.submission.qrcode.QrCodeRegistrationStateProcessor
@@ -11,9 +12,12 @@ import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
-import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import org.joda.time.Instant
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 import org.junit.jupiter.api.Test
@@ -30,6 +34,15 @@ internal class RequestCovidCertificateViewModelTest : BaseTest() {
     @MockK lateinit var analyticsKeySubmissionCollector: AnalyticsKeySubmissionCollector
     @MockK lateinit var submissionRepository: SubmissionRepository
     @MockK lateinit var coronaTestRepository: CoronaTestRepository
+    @MockK lateinit var coronaTest: CoronaTest
+
+    private val date = LocalDate.parse(
+        "01.01.1987",
+        DateTimeFormat.forPattern("dd.MM.yyyy")
+    )
+
+    private val ratQRCode = CoronaTestQRCode.RapidAntigen(hash = "hash", dateOfBirth = date, createdAt = Instant.EPOCH)
+    private val pcrQRCode = CoronaTestQRCode.PCR("GUID")
 
     @BeforeEach
     fun setup() {
@@ -42,15 +55,19 @@ internal class RequestCovidCertificateViewModelTest : BaseTest() {
             coEvery { registrationState } returns MutableLiveData()
         }
 
-        coEvery { submissionRepository.registerTest(any()) } returns mockk()
+        submissionRepository.apply {
+            coEvery { registerTest(any()) } returns coronaTest
+            coEvery { testForType(any()) } returns flowOf(coronaTest)
+        }
+
+        coEvery { coronaTestRepository.removeTest(any()) } returns coronaTest
+
+        every { coronaTest.identifier } returns "identifier"
+        every { analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any()) } just Runs
     }
 
     @Test
     fun birthDateChanged() {
-        val date = LocalDate.parse(
-            "01.01.1987",
-            DateTimeFormat.forPattern("dd.MM.yyyy")
-        )
         createInstance().apply {
             birthDateChanged(date)
             birthDate.getOrAwaitValue() shouldBe date
@@ -58,30 +75,221 @@ internal class RequestCovidCertificateViewModelTest : BaseTest() {
     }
 
     @Test
-    fun onAgreeGC() {
-        createInstance().apply {
+    fun `PCR onAgreeGC removes and registers new test`() {
+        createInstance(deleteOldTest = true).apply {
+            birthDateChanged(date)
             onAgreeGC()
+
+            coVerify {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    pcrQRCode.copy(isDccConsentGiven = true, dateOfBirth = date),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
         }
     }
 
     @Test
-    fun onDisagreeGC() {
+    fun `PCR onAgreeGC registers new test and does not remove old Test`() {
+        createInstance(deleteOldTest = false).apply {
+            birthDateChanged(date)
+            onAgreeGC()
+
+            coVerify {
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    pcrQRCode.copy(isDccConsentGiven = true, dateOfBirth = date),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+
+            coVerify(exactly = 0) {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+            }
+        }
+    }
+
+    @Test
+    fun `PCR onDisagreeGC removes and registers new test`() {
+        createInstance(deleteOldTest = true).apply {
+            onDisagreeGC()
+
+            coVerify {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    pcrQRCode.copy(isDccConsentGiven = false),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
+    }
+
+    @Test
+    fun `PCR onDisagreeGC registers new test and does not remove old Test`() {
+        createInstance(deleteOldTest = false).apply {
+            onDisagreeGC()
+
+            coVerify {
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    pcrQRCode.copy(isDccConsentGiven = false),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+
+            coVerify(exactly = 0) {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+            }
+        }
+    }
+
+    @Test
+    fun `RAT onAgreeGC removes and registers new test`() {
+        createInstance(coronaTestQRCode = ratQRCode, deleteOldTest = true).apply {
+            onAgreeGC()
+
+            coVerify {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    ratQRCode.copy(isDccConsentGiven = true, dateOfBirth = date),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
+    }
+
+    @Test
+    fun `RAT onAgreeGC registers new test and does not remove old Test`() {
+        createInstance(coronaTestQRCode = ratQRCode, deleteOldTest = false).apply {
+            onAgreeGC()
+
+            coVerify {
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    ratQRCode.copy(isDccConsentGiven = true),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+
+            coVerify(exactly = 0) {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+            }
+        }
+    }
+
+    @Test
+    fun `RAT onDisagreeGC removes and registers new test`() {
+        createInstance(coronaTestQRCode = ratQRCode, deleteOldTest = true).apply {
+            onDisagreeGC()
+
+            coVerify {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    ratQRCode.copy(isDccConsentGiven = false),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
+    }
+
+    @Test
+    fun `RAT onDisagreeGC registers new test and does not remove old Test`() {
+        createInstance(coronaTestQRCode = ratQRCode, deleteOldTest = false).apply {
+            onDisagreeGC()
+
+            coVerify {
+                qrCodeRegistrationStateProcessor.startQrCodeRegistration(
+                    ratQRCode.copy(isDccConsentGiven = false),
+                    any()
+                )
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+
+            coVerify(exactly = 0) {
+                submissionRepository.testForType(any())
+                coronaTestRepository.removeTest(any())
+            }
+        }
     }
 
     @Test
     fun navigateBack() {
+        createInstance().apply {
+            navigateBack()
+            events.getOrAwaitValue() shouldBe Back
+        }
     }
 
     @Test
     fun navigateToHomeScreen() {
+        createInstance().apply {
+            navigateToHomeScreen()
+            events.getOrAwaitValue() shouldBe ToHomeScreen
+        }
     }
 
     @Test
     fun navigateToDispatcherScreen() {
+        createInstance().apply {
+            navigateToDispatcherScreen()
+            events.getOrAwaitValue() shouldBe ToDispatcherScreen
+        }
+    }
+
+    @Test
+    fun `onAgreeGC reports analytics`() {
+        createInstance(coronTestConsent = true).apply {
+            onAgreeGC()
+            coVerify {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
+    }
+
+    @Test
+    fun `onDisagreeGC reports analytics`() {
+        createInstance(coronTestConsent = true).apply {
+            onDisagreeGC()
+            coVerify {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
+    }
+
+    @Test
+    fun `onAgreeGC does not report analytics`() {
+        createInstance(coronTestConsent = false).apply {
+            onAgreeGC()
+            coVerify(exactly = 0) {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
+    }
+
+    @Test
+    fun `onDisagreeGC does not report analytics`() {
+        createInstance(coronTestConsent = false).apply {
+            onDisagreeGC()
+            coVerify(exactly = 0) {
+                analyticsKeySubmissionCollector.reportAdvancedConsentGiven(any())
+            }
+        }
     }
 
     private fun createInstance(
-        coronaTestQRCode: CoronaTestQRCode = CoronaTestQRCode.PCR("GUID"),
+        coronaTestQRCode: CoronaTestQRCode = pcrQRCode,
         coronTestConsent: Boolean = true,
         deleteOldTest: Boolean = false
     ) = RequestCovidCertificateViewModel(
