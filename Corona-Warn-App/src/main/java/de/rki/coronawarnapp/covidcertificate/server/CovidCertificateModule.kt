5cc04b48132345ee88ecdb861d614b2fd0133e89
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-04 11:23:00 +0200

    merge 2.4 add di module

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/server/CovidCertificateModule.kt
@@ -0,0 +1,26 @@
+package de.rki.coronawarnapp.covidcertificate.server
+
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import de.rki.coronawarnapp.environment.download.DownloadCDNHttpClient
+import de.rki.coronawarnapp.environment.download.DownloadCDNServerUrl
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+@Module
+class CovidCertificateModule {
+
+    @Reusable
+    @Provides
+    fun apiV1(
+        @DownloadCDNHttpClient httpClient: OkHttpClient,
+        @DownloadCDNServerUrl url: String,
+    ): CovidCertificateApiV1 {
+        return Retrofit.Builder()
+            .client(httpClient)
+            .baseUrl(url)
+            .build()
+            .create(CovidCertificateApiV1::class.java)
+    }
+}
