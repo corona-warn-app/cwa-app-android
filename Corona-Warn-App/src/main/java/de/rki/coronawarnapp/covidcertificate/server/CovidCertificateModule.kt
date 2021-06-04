ead89fa79976e1a290d6436cee93570681b720f2
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-04 14:26:43 +0200

    add server environment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/server/CovidCertificateModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/server/CovidCertificateModule.kt
@@ -3,20 +3,19 @@ package de.rki.coronawarnapp.covidcertificate.server
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
-import de.rki.coronawarnapp.environment.download.DownloadCDNHttpClient
-import de.rki.coronawarnapp.environment.download.DownloadCDNServerUrl
+import de.rki.coronawarnapp.environment.covidcertificate.DCCHttpClient
+import de.rki.coronawarnapp.environment.covidcertificate.DCCServerUrl
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 @Module
 class CovidCertificateModule {
 
-    // TODO check environment for DCC server
     @Reusable
     @Provides
     fun apiV1(
-        @DownloadCDNHttpClient httpClient: OkHttpClient,
-        @DownloadCDNServerUrl url: String,
+        @DCCHttpClient httpClient: OkHttpClient,
+        @DCCServerUrl url: String,
     ): CovidCertificateApiV1 {
         return Retrofit.Builder()
             .client(httpClient)
