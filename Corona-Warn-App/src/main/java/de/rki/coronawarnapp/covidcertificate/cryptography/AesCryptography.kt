25049e56f1fcbadf30d0f21e58498df0d8d2a3fe
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-07 11:44:02 +0200

    simplify decryption

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/cryptography/AesCryptography.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/cryptography/AesCryptography.kt
@@ -1,16 +1,12 @@
 package de.rki.coronawarnapp.covidcertificate.cryptography
 
 import com.google.android.gms.common.util.Hex
-import dagger.Reusable
-import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.util.encoders.Base64.decode
-import java.security.Security
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import javax.inject.Inject
 
-@Reusable
 class AesCryptography @Inject constructor() {
 
     private val ivParameterSpec
@@ -20,15 +16,11 @@ class AesCryptography @Inject constructor() {
         decryptionKey: ByteArray,
         encryptedData: ByteArray
     ): ByteArray {
-        Security.addProvider(BouncyCastleProvider())
         val keySpec = SecretKeySpec(decode(decryptionKey), ALGORITHM)
         val input = decode(encryptedData)
         return with(Cipher.getInstance(TRANSFORMATION)) {
             init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec)
-            val output = ByteArray(getOutputSize(input.size))
-            var outputLength = update(input, 0, input.size, output, 0)
-            outputLength += doFinal(output, outputLength)
-            output.copyOfRange(0, outputLength)
+            doFinal(input)
         }
     }
 }
