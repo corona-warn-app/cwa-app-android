6eccf4adf3c2f66795d6588a855f8bf63548ec80
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-07 15:23:03 +0200

    Error handling (EXPOSURERAPP-7508) (#3358)
    * wip
    
    * wiring
    
    * wip
    
    * testing
    
    * refactor exception
    tests
    
    * tests
    
    * decompression
    
    * decompression
    
    * fix tests
    
    * encrypt
    
    * second test case
    
    * add error codes
    
    * fix tests
    
    * move exception
    
    * tests
    
    * error codes
    
    * error codes
    
    * error codes
    
    * address comments
    
    * add more logging
    
    * reverse string file
    
    * error handling server
    
    * error handling certificate
    
    * simplify decryption
    
    * klint
    
    * fix decoding
    
    * klint
    
    * change to unit
    
    * refactor

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/exception/ErrorMessage.kt
@@ -0,0 +1,14 @@
+package de.rki.coronawarnapp.covidcertificate.exception
+
+import de.rki.coronawarnapp.R
+
+const val ERROR_MESSAGE_GENERIC = R.string.errors_generic_text_unknown_error_cause
+
+// TODO change to correct error message once provided
+const val ERROR_MESSAGE_TRY_AGAIN = ERROR_MESSAGE_GENERIC
+const val ERROR_MESSAGE_DCC_NOT_SUPPORTED_BY_LAB = ERROR_MESSAGE_GENERIC
+const val ERROR_MESSAGE_NO_NETWORK = ERROR_MESSAGE_GENERIC
+const val ERROR_MESSAGE_E2E_ERROR_CALL_HOTLINE = ERROR_MESSAGE_GENERIC
+const val ERROR_MESSAGE_TRY_AGAIN_DCC_NOT_AVAILABLE_YET = ERROR_MESSAGE_GENERIC
+const val ERROR_MESSAGE_CLIENT_ERROR_CALL_HOTLINE = ERROR_MESSAGE_GENERIC
+const val ERROR_MESSAGE_DCC_EXPIRED = ERROR_MESSAGE_GENERIC
