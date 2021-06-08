afb80860adb21c347dd0914c3586b0351270ddf1
Matthias Urhahn <matthias.urhahn@sap.com>
2021-06-08 17:21:29 +0200

    Add 202 response-empty body handling.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/server/CovidCertificateModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/server/CovidCertificateModule.kt
@@ -6,11 +6,32 @@ import dagger.Reusable
 import de.rki.coronawarnapp.environment.covidcertificate.DCCHttpClient
 import de.rki.coronawarnapp.environment.covidcertificate.DCCServerUrl
 import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
 
 @Module
 class CovidCertificateModule {
+
+    /**
+     * Handles DCC server 202 "retry later" response with 0-byte bodies.
+     */
+    private val nullConverter = object : Converter.Factory() {
+        fun factoryRef() = this
+        override fun responseBodyConverter(
+            type: Type,
+            annotations: Array<out Annotation>,
+            retrofit: Retrofit
+        ) = object : Converter<ResponseBody, Any?> {
+            val nextConverter = retrofit.nextResponseBodyConverter<Any?>(factoryRef(), type, annotations)
+
+            override fun convert(value: ResponseBody): Any? {
+                return if (value.contentLength() != 0L) nextConverter.convert(value) else null
+            }
+        }
+    }
 
     @Reusable
     @Provides
@@ -18,12 +39,11 @@ class CovidCertificateModule {
         @DCCHttpClient httpClient: OkHttpClient,
         @DCCServerUrl url: String,
         gsonConverterFactory: GsonConverterFactory
-    ): CovidCertificateApiV1 {
-        return Retrofit.Builder()
-            .client(httpClient)
-            .baseUrl(url)
-            .addConverterFactory(gsonConverterFactory)
-            .build()
-            .create(CovidCertificateApiV1::class.java)
-    }
+    ): CovidCertificateApiV1 = Retrofit.Builder()
+        .client(httpClient)
+        .baseUrl(url)
+        .addConverterFactory(nullConverter)
+        .addConverterFactory(gsonConverterFactory)
+        .build()
+        .create(CovidCertificateApiV1::class.java)
 }
