2838e743d89c21aaeb29597733d77726ecff7dd8
Kolya Opahle <k.opahle@sap.com>
2021-06-14 16:14:21 +0200

    Missing text for test certificate on negative test result screen (EXPOSUREAPP-7753) (#3424)
    * Added Ability to check registrationToken to TestCertificateWrapper RATResultNegativeFragment now displays if a certificate is assigned to the test and if it is pending the icon and is final state for StepEntry's can now be set via code added new strings for RATResultNegativeFragment
    
    * SubmissionTestResultNegativeFragment now displays if a certificate is assigned to the test and if it is pending moved some strings around
    
    * linting
    
    * Fixed Integration tests
    
    Co-authored-by: harambasicluka <64483219+harambasicluka@users.noreply.github.com>
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/testresult/negative/SubmissionTestResultNegativeViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/submission/testresult/negative/SubmissionTestResultNegativeViewModel.kt
@@ -1,6 +1,5 @@
 package de.rki.coronawarnapp.ui.submission.testresult.negative
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import androidx.navigation.NavDirections
 import dagger.assisted.Assisted
@@ -8,19 +7,20 @@ import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.coronatest.type.CoronaTest
 import de.rki.coronawarnapp.coronatest.type.pcr.notification.PCRTestResultAvailableNotificationService
+import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificateRepository
 import de.rki.coronawarnapp.submission.SubmissionRepository
-import de.rki.coronawarnapp.ui.submission.testresult.TestResultUIState
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.map
 import timber.log.Timber
 
 class SubmissionTestResultNegativeViewModel @AssistedInject constructor(
     dispatcherProvider: DispatcherProvider,
     private val submissionRepository: SubmissionRepository,
+    certificateRepository: TestCertificateRepository,
     private val testResultAvailableNotificationService: PCRTestResultAvailableNotificationService,
     @Assisted private val testType: CoronaTest.Type
 ) : CWAViewModel(dispatcherProvider = dispatcherProvider) {
@@ -30,11 +30,25 @@ class SubmissionTestResultNegativeViewModel @AssistedInject constructor(
     }
 
     val routeToScreen = SingleLiveEvent<NavDirections?>()
-    val testResult: LiveData<TestResultUIState> = submissionRepository.testForType(type = testType)
-        .filterNotNull()
-        .map { test ->
-            TestResultUIState(coronaTest = test)
-        }.asLiveData(context = dispatcherProvider.Default)
+    val testResult = combine(
+        submissionRepository.testForType(type = testType).filterNotNull(),
+        certificateRepository.certificates
+    ) { test, certs ->
+        val cert = certs.firstOrNull {
+            it.registrationToken == test.registrationToken
+        }
+
+        val certificateState: CertificateState = when (cert?.isCertificateRetrievalPending) {
+            true -> CertificateState.PENDING
+            false -> CertificateState.AVAILABLE
+            else -> CertificateState.NOT_REQUESTED
+        }
+
+        UIState(
+            coronaTest = test,
+            certificateState = certificateState
+        )
+    }.asLiveData(context = dispatcherProvider.Default)
 
     fun deregisterTestFromDevice() = launch {
         Timber.tag(TAG).d("deregisterTestFromDevice()")
@@ -48,6 +62,17 @@ class SubmissionTestResultNegativeViewModel @AssistedInject constructor(
         submissionRepository.setViewedTestResult(type = testType)
         testResultAvailableNotificationService.cancelTestResultAvailableNotification()
     }
+
+    enum class CertificateState {
+        NOT_REQUESTED,
+        PENDING,
+        AVAILABLE
+    }
+
+    data class UIState(
+        val coronaTest: CoronaTest,
+        val certificateState: CertificateState
+    )
 
     @AssistedFactory
     interface Factory : CWAViewModelFactory<SubmissionTestResultNegativeViewModel> {
