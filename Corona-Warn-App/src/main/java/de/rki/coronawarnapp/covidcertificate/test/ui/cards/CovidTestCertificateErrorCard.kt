551e3b52d1f8d770e1dd99c43aac9c6ab9e969e6
Juraj Kusnier <jurajkusnier@gmail.com>
2021-06-15 14:57:20 +0200

    update displayed time

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/ui/cards/CovidTestCertificateErrorCard.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/ui/cards/CovidTestCertificateErrorCard.kt
@@ -6,16 +6,17 @@ import androidx.core.view.isInvisible
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.covidcertificate.test.ui.CertificatesAdapter
 import de.rki.coronawarnapp.databinding.CovidTestErrorCardBinding
-import de.rki.coronawarnapp.util.TimeAndDateExtensions.toShortDayFormat
-import de.rki.coronawarnapp.util.TimeAndDateExtensions.toShortTimeFormat
 import de.rki.coronawarnapp.util.lists.diffutil.HasPayloadDiffer
-import org.joda.time.Instant
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 class CovidTestCertificateErrorCard(parent: ViewGroup) :
     CertificatesAdapter.CertificatesItemVH<CovidTestCertificateErrorCard.Item, CovidTestErrorCardBinding>(
         R.layout.home_card_container_layout,
         parent
     ) {
+
+    private val shortTime = DateTimeFormat.shortTime()
 
     override val viewBinding = lazy {
         CovidTestErrorCardBinding.inflate(layoutInflater, itemView.findViewById(R.id.card_container), true)
@@ -28,8 +29,8 @@ class CovidTestCertificateErrorCard(parent: ViewGroup) :
 
         testTime.text = context.getString(
             R.string.test_certificate_time,
-            item.testDate.toShortDayFormat(),
-            item.testDate.toShortTimeFormat(),
+            item.testDate.toString(DATE_FORMAT),
+            item.testDate.toString(shortTime)
         )
 
         retryButton.setOnClickListener {
@@ -53,11 +54,15 @@ class CovidTestCertificateErrorCard(parent: ViewGroup) :
     }
 
     data class Item(
-        override val testDate: Instant,
+        override val testDate: DateTime,
         val onRetryAction: (Item) -> Unit,
         val onDeleteAction: (Item) -> Unit,
         val isUpdatingData: Boolean,
     ) : CovidCertificateTestItem, HasPayloadDiffer {
         override fun diffPayload(old: Any, new: Any): Any? = if (old::class == new::class) new else null
+    }
+
+    companion object {
+        private const val DATE_FORMAT = "dd.MM.yyyy"
     }
 }
