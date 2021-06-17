00925a52a9562ceffdc5f661ae9402f573907004
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-06-17 18:48:35 +0200

    Remove test data

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/core/PersonCertificatesProvider.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/core/PersonCertificatesProvider.kt
@@ -3,17 +3,12 @@ package de.rki.coronawarnapp.covidcertificate.person.core
 import dagger.Reusable
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CertificatePersonIdentifier
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
-import de.rki.coronawarnapp.covidcertificate.common.qrcode.QrCodeString
 import de.rki.coronawarnapp.covidcertificate.recovery.core.RecoveryCertificateRepository
-import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificate
 import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificateRepository
-import de.rki.coronawarnapp.covidcertificate.vaccination.core.VaccinationCertificate
 import de.rki.coronawarnapp.covidcertificate.vaccination.core.repository.VaccinationRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
-import org.joda.time.Instant
-import org.joda.time.LocalDate
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -49,7 +44,6 @@ class PersonCertificatesProvider @Inject constructor(
             Timber.tag(TAG).v("PersonCertificates for %s with %d certs.", personIdentifier, certs.size)
             PersonCertificates(certificates = certs.toPrioritySortOrder())
         }.toSet()
-//        + testData // TODO remove
     }
 
     fun Collection<CwaCovidCertificate>.toPrioritySortOrder(): List<CwaCovidCertificate> {
@@ -66,119 +60,7 @@ class PersonCertificatesProvider @Inject constructor(
         // TODO
     }
 
-    // TODO remove
-    private val testData = (0..30).map { PersonCertificates(listOf(testCertificate(it))) }.toSet() +
-        PersonCertificates(listOf(testCertificate(31, true, false))) +
-        PersonCertificates(listOf(testCertificate(32, true, true))) +
-        PersonCertificates(listOf(testCertificate(33)), true)
-
-    // TODO remove
-    private fun testCertificate(
-        index: Int,
-        isCertificateRetrievalPending: Boolean = false,
-        isUpdating: Boolean = false
-    ) =
-        object : TestCertificate {
-            override val targetName: String
-                get() = "targetName"
-            override val testType: String
-                get() = "testType"
-            override val testResult: String
-                get() = "testResult"
-            override val testName: String?
-                get() = "testName"
-            override val testNameAndManufacturer: String?
-                get() = "testNameAndManufacturer"
-            override val sampleCollectedAt: Instant
-                get() = Instant.now()
-            override val testCenter: String?
-                get() = "testCenter"
-            override val registeredAt: Instant
-                get() = Instant.now()
-            override val isUpdatingData: Boolean
-                get() = isUpdating
-            override val isCertificateRetrievalPending: Boolean
-                get() = isCertificateRetrievalPending
-            override val issuer: String
-                get() = "issuer"
-            override val issuedAt: Instant
-                get() = Instant.now()
-            override val expiresAt: Instant
-                get() = Instant.now()
-            override val qrCode: QrCodeString
-                get() = (0..20).fold("") { it, _ -> it + "qrCode$index" }
-
-            override val firstName: String
-                get() = "Max"
-            override val lastName: String
-                get() = "Mustermann"
-            override val fullName: String
-                get() = "Max Mustermann $index"
-            override val dateOfBirth: LocalDate
-                get() = LocalDate.now()
-            override val personIdentifier: CertificatePersonIdentifier
-                get() = CertificatePersonIdentifier(
-                    LocalDate.now(),
-                    "last$index",
-                    "firs$index"
-                )
-            override val certificateIssuer: String
-                get() = "certificateIssuer"
-            override val certificateCountry: String
-                get() = "certificateCountry"
-            override val certificateId: String
-                get() = "certificateId"
-        }
-
     companion object {
         private val TAG = PersonCertificatesProvider::class.simpleName!!
-    }
-    // TODO remove
-    fun vaccinationCertificate(
-        index: Int,
-        isCertificateRetrievalPending: Boolean = false,
-        isUpdating: Boolean = false
-    ) = object : VaccinationCertificate {
-        override val vaccinatedAt: LocalDate
-            get() = LocalDate.now()
-        override val vaccineTypeName: String
-            get() = "vaccineTypeName"
-        override val vaccineManufacturer: String
-            get() = "vaccineManufacturer"
-        override val medicalProductName: String
-            get() = "medicalProductName"
-        override val doseNumber: Int
-            get() = 2
-        override val totalSeriesOfDoses: Int
-            get() = 2
-        override val issuer: String
-            get() = "issuer"
-        override val issuedAt: Instant
-            get() = Instant.now()
-        override val expiresAt: Instant
-            get() = Instant.now()
-        override val qrCode: QrCodeString
-            get() = (0..20).fold("") { it, _ -> it + "qrCode$index" }
-        override val firstName: String
-        ?
-            get() = "firstName"
-        override val lastName: String
-            get() = "lastName"
-        override val fullName: String
-            get() = "fullName"
-        override val dateOfBirth: LocalDate
-            get() = LocalDate.now()
-        override val personIdentifier: CertificatePersonIdentifier
-            get() = CertificatePersonIdentifier(
-                LocalDate.now(),
-                "last$index",
-                "firs$index"
-            )
-        override val certificateIssuer: String
-            get() = "certificateIssuer"
-        override val certificateCountry: String
-            get() = "certificateCountry"
-        override val certificateId: String
-            get() = "certificateId"
     }
 }
