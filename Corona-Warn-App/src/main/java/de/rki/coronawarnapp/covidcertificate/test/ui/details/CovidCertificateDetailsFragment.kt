a33edb91aeaa725822c8c01d8675f48661611f80
Juraj Kusnier <jurajkusnier@gmail.com>
2021-06-17 12:04:56 +0200

    use local time

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/ui/details/CovidCertificateDetailsFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/ui/details/CovidCertificateDetailsFragment.kt
@@ -19,8 +19,8 @@ import de.rki.coronawarnapp.ui.qrcode.fullscreen.QrCodeFullScreenFragmentArgs
 import de.rki.coronawarnapp.ui.view.onOffsetChange
 import de.rki.coronawarnapp.util.DialogHelper
 import de.rki.coronawarnapp.util.TimeAndDateExtensions.toDayFormat
-import de.rki.coronawarnapp.util.TimeAndDateExtensions.toShortDayFormat
 import de.rki.coronawarnapp.util.TimeAndDateExtensions.toShortTimeFormat
+import de.rki.coronawarnapp.util.TimeAndDateExtensions.toUserTimeZone
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.setUrl
 import de.rki.coronawarnapp.util.ui.popBackStack
@@ -65,6 +65,8 @@ class CovidCertificateDetailsFragment : Fragment(R.layout.fragment_covid_certifi
     private fun FragmentCovidCertificateDetailsBinding.onCertificateReady(
         testCertificate: TestCertificate
     ) {
+        val testDateTime = testCertificate.sampleCollectedAt.toUserTimeZone()
+
         name.text = testCertificate.run { "$lastName, $firstName" }
         birthDate.text = testCertificate.dateOfBirth.toDayFormat()
         diseaseType.text = testCertificate.targetName
@@ -72,8 +74,8 @@ class CovidCertificateDetailsFragment : Fragment(R.layout.fragment_covid_certifi
         testName.text = testCertificate.testName
         testManufacturer.text = testCertificate.testNameAndManufactor
         testDate.text = "%s %s".format(
-            testCertificate.sampleCollectedAt.toShortDayFormat(),
-            testCertificate.sampleCollectedAt.toShortTimeFormat()
+            testDateTime.toDayFormat(),
+            testDateTime.toShortTimeFormat()
         )
         testResult.text = testCertificate.testResult
         testCenter.text = testCertificate.testCenter
