1b3ab3bf8c971f3ac2b5bbdeb805eb8e2a9c1fad
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-17 08:26:12 +0200

    change naming

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/common/certificate/DccData.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/common/certificate/DccData.kt
@@ -1,6 +1,6 @@
 package de.rki.coronawarnapp.covidcertificate.common.certificate
 
-data class DccData<C : DccV1.MetaData>(
+data class DccData<CertT : DccV1.MetaData>(
     val header: DccHeader,
-    val certificate: C,
+    val certificate: CertT,
 )
