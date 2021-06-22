59b391fec33f3f95e6c2f7af53b9a8bb8e59556a
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-06-22 19:41:00 +0200

    Fix tests

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/covidcertificate/person/ui/details/PersonDetailsFragmentTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/covidcertificate/person/ui/details/PersonDetailsFragmentTest.kt
@@ -14,6 +14,12 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CertificatePersonIdentifier
+import de.rki.coronawarnapp.covidcertificate.common.certificate.DccV1
+import de.rki.coronawarnapp.covidcertificate.common.certificate.TestDccV1
+import de.rki.coronawarnapp.covidcertificate.common.certificate.VaccinationDccV1
+import de.rki.coronawarnapp.covidcertificate.common.repository.RecoveryCertificateContainerId
+import de.rki.coronawarnapp.covidcertificate.common.repository.TestCertificateContainerId
+import de.rki.coronawarnapp.covidcertificate.common.repository.VaccinationCertificateContainerId
 import de.rki.coronawarnapp.covidcertificate.person.core.PersonCertificates
 import de.rki.coronawarnapp.covidcertificate.person.ui.details.items.CertificateItem
 import de.rki.coronawarnapp.covidcertificate.person.ui.details.items.CwaUserCard
@@ -21,8 +27,10 @@ import de.rki.coronawarnapp.covidcertificate.person.ui.details.items.PersonDetai
 import de.rki.coronawarnapp.covidcertificate.person.ui.details.items.RecoveryCertificateCard
 import de.rki.coronawarnapp.covidcertificate.person.ui.details.items.TestCertificateCard
 import de.rki.coronawarnapp.covidcertificate.person.ui.details.items.VaccinationCertificateCard
+import de.rki.coronawarnapp.covidcertificate.person.ui.overview.PersonColorShade
 import de.rki.coronawarnapp.covidcertificate.recovery.core.RecoveryCertificate
 import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificate
+import de.rki.coronawarnapp.covidcertificate.vaccination.core.VaccinatedPerson
 import de.rki.coronawarnapp.covidcertificate.vaccination.core.VaccinationCertificate
 import de.rki.coronawarnapp.util.TimeAndDateExtensions.toLocalDateUserTz
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
@@ -48,6 +56,9 @@ class PersonDetailsFragmentTest : BaseUITest() {
     @MockK lateinit var viewModel: PersonDetailsViewModel
     private lateinit var bitmap: Bitmap
     private val args = PersonDetailsFragmentArgs("code").toBundle()
+    private val vcContainerId = VaccinationCertificateContainerId("1")
+    private val tcsContainerId = TestCertificateContainerId("2")
+    private val rcContainerId = RecoveryCertificateContainerId("3")
 
     @Before
     fun setUp() {
@@ -63,7 +74,10 @@ class PersonDetailsFragmentTest : BaseUITest() {
         )
         setupMockViewModel(
             object : PersonDetailsViewModel.Factory {
-                override fun create(personIdentifierCode: String): PersonDetailsViewModel = viewModel
+                override fun create(
+                    personIdentifierCode: String,
+                    colorShade: PersonColorShade
+                ): PersonDetailsViewModel = viewModel
             }
         )
     }
@@ -105,49 +119,86 @@ class PersonDetailsFragmentTest : BaseUITest() {
 
             add(PersonDetailsQrCard.Item(testCertificate, bitmap))
             add(CwaUserCard.Item(personCertificates) {})
-            add(VaccinationCertificateCard.Item(vaccinationCertificate1, isCurrentCertificate = false) {})
-            add(VaccinationCertificateCard.Item(vaccinationCertificate2, isCurrentCertificate = false) {})
-            add(TestCertificateCard.Item(testCertificate, isCurrentCertificate = true) {})
-            add(RecoveryCertificateCard.Item(recoveryCertificate, isCurrentCertificate = false) {})
+            add(
+                VaccinationCertificateCard.Item(
+                    vaccinationCertificate1,
+                    isCurrentCertificate = false,
+                    VaccinatedPerson.Status.COMPLETE,
+                    PersonColorShade.COLOR_1
+                ) {})
+            add(
+                VaccinationCertificateCard.Item(
+                    vaccinationCertificate2,
+                    isCurrentCertificate = false,
+                    VaccinatedPerson.Status.COMPLETE,
+                    PersonColorShade.COLOR_1
+                ) {})
+            add(TestCertificateCard.Item(testCertificate, isCurrentCertificate = true, PersonColorShade.COLOR_1) {})
+            add(
+                RecoveryCertificateCard.Item(
+                    recoveryCertificate,
+                    isCurrentCertificate = false,
+                    PersonColorShade.COLOR_1
+                ) {})
         }
     )
 
     private fun mockTestCertificate(): TestCertificate = mockk<TestCertificate>().apply {
         every { certificateId } returns "testCertificateId"
         every { fullName } returns "Andrea Schneider"
+        every { rawCertificate } returns mockk<TestDccV1>().apply {
+            every { test } returns mockk<DccV1.TestCertificateData>().apply {
+                every { testType } returns "LP6464-4"
+                every { sampleCollectedAt } returns Instant.parse("2021-05-31T11:35:00.000Z")
+            }
+        }
+        every { containerId } returns tcsContainerId
         every { testType } returns "PCR-Test"
         every { dateOfBirth } returns LocalDate.parse("18.04.1943", DateTimeFormat.forPattern("dd.MM.yyyy"))
         every { sampleCollectedAt } returns Instant.parse("2021-05-31T11:35:00.000Z")
         every { registeredAt } returns Instant.parse("2021-05-21T11:35:00.000Z")
-        every { personIdentifier } returns CertificatePersonIdentifier(
-            firstNameStandardized = "firstNameStandardized",
-            lastNameStandardized = "lastNameStandardized",
-            dateOfBirth = LocalDate.now()
-        )
+        every { personIdentifier } returns certificatePersonIdentifier
+        every { qrCode } returns "qrCode"
     }
 
-    private fun mockVaccinationCertificate(
-        number: Int = 1,
-        final: Boolean = false
-    ): VaccinationCertificate =
+    private fun mockVaccinationCertificate(number: Int = 1, final: Boolean = false): VaccinationCertificate =
         mockk<VaccinationCertificate>().apply {
+            val localDate = Instant.parse("2021-06-01T11:35:00.000Z").toLocalDateUserTz()
+            every { fullName } returns "Andrea Schneider"
             every { certificateId } returns "vaccinationCertificateId$number"
-            every { vaccinatedAt } returns Instant.parse("2021-06-01T11:35:00.000Z").toLocalDateUserTz()
-            every { personIdentifier } returns CertificatePersonIdentifier(
-                firstNameStandardized = "firstNameStandardized",
-                lastNameStandardized = "lastNameStandardized",
-                dateOfBirth = LocalDate.now()
-            )
+            every { rawCertificate } returns mockk<VaccinationDccV1>().apply {
+                every { vaccination } returns mockk<DccV1.VaccinationData>().apply {
+                    every { doseNumber } returns number
+                    every { totalSeriesOfDoses } returns 2
+                    every { vaccinatedAt } returns localDate
+                }
+            }
+            every { containerId } returns vcContainerId
+            every { vaccinatedAt } returns localDate
+            every { personIdentifier } returns certificatePersonIdentifier
             every { doseNumber } returns number
             every { totalSeriesOfDoses } returns 2
+            every { dateOfBirth } returns LocalDate.now()
             every { isFinalShot } returns final
+            every { qrCode } returns "qrCode"
         }
 
     private fun mockRecoveryCertificate(): RecoveryCertificate =
         mockk<RecoveryCertificate>().apply {
+            every { fullName } returns "Andrea Schneider"
             every { certificateId } returns "recoveryCertificateId"
+            every { dateOfBirth } returns LocalDate.now()
             every { validUntil } returns Instant.parse("2021-05-31T11:35:00.000Z").toLocalDateUserTz()
+            every { personIdentifier } returns certificatePersonIdentifier
+            every { qrCode } returns "qrCode"
+            every { containerId } returns rcContainerId
         }
+
+    private val certificatePersonIdentifier = CertificatePersonIdentifier(
+        dateOfBirth = LocalDate.parse("01.01.2020", DateTimeFormat.forPattern("dd.MM.yyyy")),
+        firstNameStandardized = "firstNameStandardized",
+        lastNameStandardized = "lastNameStandardized",
+    )
 
     @After
     fun tearDown() {
