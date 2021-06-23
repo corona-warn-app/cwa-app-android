613d5fbc7485a1603139433ad558bac3d48f809c
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-23 12:27:49 +0200

    merge conflict

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/storage/StoredRecoveryCertificateData.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/storage/StoredRecoveryCertificateData.kt
@@ -3,7 +3,7 @@ package de.rki.coronawarnapp.covidcertificate.recovery.core.storage
 import com.google.gson.annotations.SerializedName
 
 data class StoredRecoveryCertificateData(
-    @SerializedName("recoveryCertificateQrCode") override val recoveryCertificateQrCode: String?,
+    @SerializedName("recoveryCertificateQrCode") override val recoveryCertificateQrCode: String,
 ) : StoredRecoveryCertificate
 
 interface StoredRecoveryCertificate {
