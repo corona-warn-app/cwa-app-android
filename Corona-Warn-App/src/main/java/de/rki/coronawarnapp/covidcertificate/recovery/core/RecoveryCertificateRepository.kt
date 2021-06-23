05fd1849019911e6dd48e3e867e577440fac7e09
chris-cwa <chris.cwa.sap@gmail.com>
2021-06-23 09:04:17 +0200

    removed redundant identifier

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/RecoveryCertificateRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/RecoveryCertificateRepository.kt
@@ -85,7 +85,6 @@ class RecoveryCertificateRepository @Inject constructor(
 
     private fun RecoveryCertificateQRCode.toContainer() = RecoveryCertificateContainer(
         data = StoredRecoveryCertificateData(
-            identifier = uniqueCertificateIdentifier,
             recoveryCertificateQrCode = qrCode
         ),
         qrCodeExtractor = qrCodeExtractor,
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/storage/StoredRecoveryCertificateData.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/storage/StoredRecoveryCertificateData.kt
@@ -3,11 +3,9 @@ package de.rki.coronawarnapp.covidcertificate.recovery.core.storage
 import com.google.gson.annotations.SerializedName
 
 data class StoredRecoveryCertificateData(
-    @SerializedName("identifier") override val identifier: String,
     @SerializedName("recoveryCertificateQrCode") override val recoveryCertificateQrCode: String?,
 ) : StoredRecoveryCertificate
 
 interface StoredRecoveryCertificate {
-    val identifier: String
     val recoveryCertificateQrCode: String?
 }
