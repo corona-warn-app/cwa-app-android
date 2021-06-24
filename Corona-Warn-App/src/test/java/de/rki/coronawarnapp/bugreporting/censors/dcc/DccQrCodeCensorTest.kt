eddec511227a2fc10d981d739c94a94df2d3220b
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-06-24 13:42:45 +0200

    fix test

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/dcc/DccQrCodeCensorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/dcc/DccQrCodeCensorTest.kt
@@ -15,9 +15,9 @@ import org.junit.jupiter.api.Test
 @Suppress("MaxLineLength")
 internal class DccQrCodeCensorTest {
 
-    private val testRawString =
+    private val vaccinationQrCode =
         "HC1:6BFOXN*TS0BI\$ZD.P9UOL97O4-2HH77HRM3DSPTLRR+%3.ZH9M9ESIGUBA KWML/O6HXK 0D+4O5VC9:BPCNYKMXEE1JAA/CZIK0JK1WL260X638J3-E3GG396B-43FZT-43:S0X37*ZV+FNI6HXY0ZSVILVQJF//05MVZJ5V.499TXY9KK9+OC+G9QJPNF67J6QW67KQY466PPM4MLJE+.PDB9L6Q2+PFQ5DB96PP5/P-59A%N+892 7J235II3NJ7PK7SLQMIJSBHVA7UJQWT.+S+ND%%M%331BH.IA.C8KRDL4O54O4IGUJKJGI0JAXD15IAXMFU*GSHGHD63DAOC9JU0H11+*4.\$S6ZC0JBZAB-C3QHISKE MCAOI8%M3V96-PY\$N6XOWLIBPIAYU:*JIRHUF2XZQ4H9 XJ72WG1K36VF/9BL56%E8T1OEEG%5TW5A 6YO67N6UCE:WT6BT-UMM:ABJK2TMDN1:FW-%T+\$D78NDSC3%5F61NYS-P9LOE0%J/ZAY:N5L4H-H/LH:AO3FU JHG7K46IOIMT.RE%PHLA21JRI3HTC\$AH"
-    private val testCertificateData = DccData(
+    private val vaccinationCertificateData = DccData(
         header = mockk(),
         certificate = VaccinationDccV1(
             version = "1",
@@ -63,12 +63,12 @@ internal class DccQrCodeCensorTest {
 
     @Test
     fun `checkLog() should return censored LogLine`() = runBlockingTest {
-        DccQrCodeCensor.addQRCodeStringToCensor(testRawString)
-        DccQrCodeCensor.addCertificateToCensor(testCertificateData)
+        DccQrCodeCensor.addQRCodeStringToCensor(vaccinationQrCode)
+        DccQrCodeCensor.addCertificateToCensor(vaccinationCertificateData)
 
         val censor = createInstance()
 
-        val logLineToCensor = "Here comes the rawString: $testRawString of the vaccine certificate"
+        val logLineToCensor = "Here comes the rawString: $vaccinationQrCode of the vaccine certificate"
 
         censor.checkLog(logLineToCensor)!!
             .compile()!!.censored shouldBe "Here comes the rawString: ###C\$AH of the vaccine certificate"
@@ -79,30 +79,30 @@ internal class DccQrCodeCensorTest {
 
         censor.checkLog(certDataToCensor)!!
             .compile()!!.censored shouldBe "Hello my name is nameData/familyName nameData/givenName, i was born at " +
-            "covidCertificate/dateOfBirth, i have been vaccinated with: vaccinationData/targetId " +
-            "vaccinationData/vaccineId vaccinationData/medicalProductId" +
-            " vaccinationData/marketAuthorizationHolderId vaccinationData/vaccinatedOnFormatted" +
-            " vaccinationData/certificateCountry vaccinationData/certificateIssuer" +
-            " vaccinationData/uniqueCertificateIdentifier"
+            "dcc/dateOfBirth, i have been vaccinated with: vaccination/targetId " +
+            "vaccination/vaccineId vaccination/medicalProductId" +
+            " vaccination/marketAuthorizationHolderId vaccination/vaccinatedOnFormatted" +
+            " vaccination/certificateCountry vaccination/certificateIssuer" +
+            " vaccination/uniqueCertificateIdentifier"
     }
 
     @Test
     fun `checkLog() should return null if no data to censor was set`() = runBlockingTest {
         val censor = createInstance()
 
-        val logLineNotToCensor = "Here comes the rawData: $testRawString"
+        val logLineNotToCensor = "Here comes the rawData: $vaccinationQrCode"
 
         censor.checkLog(logLineNotToCensor) shouldBe null
     }
 
     @Test
     fun `checkLog() should return null if nothing should be censored`() = runBlockingTest {
-        DccQrCodeCensor.addQRCodeStringToCensor(testRawString.replace("1", "2"))
-        DccQrCodeCensor.addCertificateToCensor(testCertificateData)
+        DccQrCodeCensor.addQRCodeStringToCensor(vaccinationQrCode.replace("1", "2"))
+        DccQrCodeCensor.addCertificateToCensor(vaccinationCertificateData)
 
         val censor = createInstance()
 
-        val logLineNotToCensor = "Here comes the rawString: $testRawString"
+        val logLineNotToCensor = "Here comes the rawString: $vaccinationQrCode"
 
         censor.checkLog(logLineNotToCensor) shouldBe null
     }
