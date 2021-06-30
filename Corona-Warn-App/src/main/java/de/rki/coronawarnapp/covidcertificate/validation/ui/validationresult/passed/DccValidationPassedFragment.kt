44ad07c08bca7f3cf9b3dec04692fd13fa34f224
BMItter <berndus@gmx.de>
2021-06-30 15:46:36 +0200

    Bind viewmodel

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/passed/DccValidationPassedFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/passed/DccValidationPassedFragment.kt
@@ -9,12 +9,13 @@ import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
 import javax.inject.Inject
 
-class DccValidationPassedFragment : Fragment(R.layout.validation_rules_result_valid_screen), AutoInject
-{
+class DccValidationPassedFragment : Fragment(R.layout.validation_rules_result_valid_screen), AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val viewModel: DccValidationPassedViewmodel by cwaViewModels { viewModelFactory }
     private val binding: ValidationRulesResultValidScreenBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +29,4 @@ class DccValidationPassedFragment : Fragment(R.layout.validation_rules_result_va
             }
         }
     }
-
 }
