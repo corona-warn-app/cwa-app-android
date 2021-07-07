2943eff83e919812f9dffd0df2d6644898fa59e1
Matthias Urhahn <matthias.urhahn@sap.com>
2021-07-07 16:04:32 +0200

    Additional edgecase handling for failed public key registration (EXPOSUREAPP-8162) (#3656)
    * Additional edge-case handling for #3650.
    Moving the rsa keypair to `TestCertificateRepository.requestCertificate` created an additional edgecase:
    Users that updated the CWA to the new code, after `requestCertificate` (so they don't have keypairs yet),
    and before they registered a public key (so they need to register one, but did not generate one).
    
    New handling now detects this and generates a keypair in any-case, and checks if the server returns HTTP 409.
    For additional details see comments in `TestCertificateProcessor`
    
    * Fix refresh loop.
    
    * Fix TestCertificateServer error mapping.
    
    Co-authored-by: harambasicluka <64483219+harambasicluka@users.noreply.github.com>
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/core/TestCertificateProcessor.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/core/TestCertificateProcessor.kt
@@ -5,8 +5,8 @@ import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccQrCodeExtractor
 import de.rki.coronawarnapp.covidcertificate.common.exception.InvalidHealthCertificateException
 import de.rki.coronawarnapp.covidcertificate.common.exception.InvalidTestCertificateException
-import de.rki.coronawarnapp.covidcertificate.common.exception.TestCertificateServerException
-import de.rki.coronawarnapp.covidcertificate.common.exception.TestCertificateServerException.ErrorCode
+import de.rki.coronawarnapp.covidcertificate.common.exception.TestCertificateException
+import de.rki.coronawarnapp.covidcertificate.common.exception.TestCertificateException.ErrorCode
 import de.rki.coronawarnapp.covidcertificate.test.core.server.TestCertificateComponents
 import de.rki.coronawarnapp.covidcertificate.test.core.server.TestCertificateServer
 import de.rki.coronawarnapp.covidcertificate.test.core.storage.types.PCRCertificateData
@@ -14,6 +14,7 @@ import de.rki.coronawarnapp.covidcertificate.test.core.storage.types.RACertifica
 import de.rki.coronawarnapp.covidcertificate.test.core.storage.types.RetrievedTestCertificate
 import de.rki.coronawarnapp.util.TimeStamper
 import de.rki.coronawarnapp.util.encryption.rsa.RSACryptography
+import de.rki.coronawarnapp.util.encryption.rsa.RSAKeyPairGenerator
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import okio.ByteString.Companion.decodeBase64
@@ -28,12 +29,14 @@ class TestCertificateProcessor @Inject constructor(
     private val rsaCryptography: RSACryptography,
     private val appConfigProvider: AppConfigProvider,
     private val qrCodeExtractor: DccQrCodeExtractor,
+    private val rsaKeyPairGenerator: RSAKeyPairGenerator,
 ) {
 
     /**
      * Register the public key with the server, a shortwhile later,
      * the test certificate components should be available, via [obtainCertificate].
      */
+    @Suppress("ComplexMethod")
     internal suspend fun registerPublicKey(
         data: RetrievedTestCertificate
     ): RetrievedTestCertificate {
@@ -41,7 +44,7 @@ class TestCertificateProcessor @Inject constructor(
 
         if (data is PCRCertificateData && data.labId.isNullOrBlank()) {
             Timber.tag(TAG).e("PCR certificate is missing valid labId: %s", data)
-            throw TestCertificateServerException(ErrorCode.DCC_NOT_SUPPORTED_BY_LAB)
+            throw TestCertificateException(ErrorCode.DCC_NOT_SUPPORTED_BY_LAB)
         }
 
         if (data.publicKeyRegisteredAt != null) {
@@ -49,32 +52,80 @@ class TestCertificateProcessor @Inject constructor(
             return data
         }
 
-        val publicKey = data.rsaPublicKey ?: throw IllegalArgumentException("rsaPublicKey was null")
-        requireNotNull(data.rsaPrivateKey) { "We have a public key without private key?" }
+        /*
+            https://github.com/corona-warn-app/cwa-app-android/pull/3650
+            ^ prior to the PR, a client/server error could lead to a stalemate:
+            Server says a public key was registered; App has not stored KeyPair
+            ^ the PR changes key generation to only happen once, on test registration.
+            Only users that were already affected by the bug will run into this case.
+
+            Users that upgraded right after registering for a test certificate,
+            and before retrieving a test certificate are also in this state.
+         */
+        val isMissingKeyPairEdgeCase = data.publicKeyRegisteredAt == null &&
+            data.rsaPublicKey == null &&
+            data.rsaPrivateKey == null
+
+        val (publicKey, privateKey) = when {
+            data.rsaPublicKey != null && data.rsaPrivateKey != null -> {
+                // Normal case, since ~2.5.1 (PR #3650)
+                Timber.tag(TAG).v("Using existing RSA KeyPair.")
+                data.rsaPublicKey!! to data.rsaPrivateKey!!
+            }
+            !isMissingKeyPairEdgeCase -> {
+                // Shouldn't happen
+                throw IllegalArgumentException("Missing keypair")
+            }
+            else -> {
+                // Either affected by #3638 or was "unlucky user" that updated the app,
+                // while the test was already registered for a certificate, but no publickey was retrieved yet.
+                // "unlucky user"'s are missing the rsa keypairs. So we generate a pair and just try.
+                Timber.tag(TAG).w("Possible edgecase, RSA KeyPair is missing.")
+                val keyPair = rsaKeyPairGenerator.generate()
+                keyPair.publicKey to keyPair.privateKey
+            }
+        }
+
+        val dataToSave = if (isMissingKeyPairEdgeCase) {
+            // Could be #3638 or could be user that updated CWA while trying to register key.
+            // So "unlucky users" don't become "#3638 users", we store the keypair we used.
+            // For #3638 users, the case won't become worse, their server request returns HTTP409 in any case.
+            when (data) {
+                is PCRCertificateData -> data.copy(rsaPublicKey = publicKey, rsaPrivateKey = privateKey)
+                is RACertificateData -> data.copy(rsaPublicKey = publicKey, rsaPrivateKey = privateKey)
+            }
+        } else {
+            data
+        }
 
         try {
             certificateServer.registerPublicKeyForTest(
-                testRegistrationToken = data.registrationToken,
+                testRegistrationToken = dataToSave.registrationToken,
                 publicKey = publicKey,
             )
-            Timber.tag(TAG).i("Public key successfully registered for %s", data)
-        } catch (e: TestCertificateServerException) {
-            if (e.errorCode == ErrorCode.PKR_409) {
-                Timber.tag(TAG).w("PublicKey already registered, assuming we can go ahead.")
-            } else {
-                throw e
+            Timber.tag(TAG).i("PublicKey successfully registered for %s", dataToSave)
+        } catch (e: TestCertificateException) {
+            when {
+                e.errorCode == ErrorCode.PKR_409 && isMissingKeyPairEdgeCase -> {
+                    // User was affected by bug #3638
+                    throw TestCertificateException(ErrorCode.BUG_3638_KEYPAIR_LOST)
+                }
+                e.errorCode == ErrorCode.PKR_409 && !isMissingKeyPairEdgeCase -> {
+                    Timber.tag(TAG).w("PublicKey already registered, assuming we can go ahead.")
+                }
+                e.errorCode != ErrorCode.PKR_409 && isMissingKeyPairEdgeCase -> {
+                    Timber.tag(TAG).w("EdgeCase: 'unlucky user' saving keypair for retry.")
+                    return dataToSave
+                }
+                else -> throw e
             }
         }
 
         val nowUTC = timeStamper.nowUTC
 
-        return when (data) {
-            is PCRCertificateData -> data.copy(
-                publicKeyRegisteredAt = nowUTC,
-            )
-            is RACertificateData -> data.copy(
-                publicKeyRegisteredAt = nowUTC,
-            )
+        return when (dataToSave) {
+            is PCRCertificateData -> dataToSave.copy(publicKeyRegisteredAt = nowUTC)
+            is RACertificateData -> dataToSave.copy(publicKeyRegisteredAt = nowUTC)
         }
     }
 
@@ -92,7 +143,7 @@ class TestCertificateProcessor @Inject constructor(
 
         if (data is PCRCertificateData && data.labId.isNullOrBlank()) {
             Timber.tag(TAG).e("PCR certificate is missing valid labId: %s", data)
-            throw TestCertificateServerException(ErrorCode.DCC_NOT_SUPPORTED_BY_LAB)
+            throw TestCertificateException(ErrorCode.DCC_NOT_SUPPORTED_BY_LAB)
         }
 
         if (data.publicKeyRegisteredAt == null) {
@@ -122,7 +173,7 @@ class TestCertificateProcessor @Inject constructor(
 
         val components = try {
             executeRequest()
-        } catch (e: TestCertificateServerException) {
+        } catch (e: TestCertificateException) {
             if (e.errorCode == ErrorCode.DCC_COMP_202) {
                 delay(certConfig.waitForRetry.millis)
                 executeRequest()
