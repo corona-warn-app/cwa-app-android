5f5201774c055f083f3e98d5616074859aae4060
Matthias Urhahn <matthias.urhahn@sap.com>
2021-07-08 17:37:10 +0200

    Finish wiring and list layout.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/failed/DccValidationFailedFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/failed/DccValidationFailedFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.ValidationResultAdapter
-import de.rki.coronawarnapp.databinding.CovidCertificateValidationFailedFragmentBinding
+import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.setHeaderForState
+import de.rki.coronawarnapp.databinding.CovidCertificateValidationResultFragmentsBinding
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.lists.decorations.RecylerViewPaddingDecorator
 import de.rki.coronawarnapp.util.lists.diffutil.update
 import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBinding
@@ -15,10 +17,10 @@ import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
 import javax.inject.Inject
 
-class DccValidationFailedFragment : Fragment(R.layout.covid_certificate_validation_failed_fragment), AutoInject {
+class DccValidationFailedFragment : Fragment(R.layout.covid_certificate_validation_result_fragments), AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
-    private val binding by viewBinding<CovidCertificateValidationFailedFragmentBinding>()
+    private val binding by viewBinding<CovidCertificateValidationResultFragmentsBinding>()
     private val args by navArgs<DccValidationFailedFragmentArgs>()
     private val viewModel: DccValidationFailedViewModel by cwaViewModelsAssisted(
         factoryProducer = { viewModelFactory },
@@ -33,7 +35,20 @@ class DccValidationFailedFragment : Fragment(R.layout.covid_certificate_validati
     @Inject lateinit var validationResultAdapter: ValidationResultAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.list.adapter = validationResultAdapter
+        binding.apply {
+            setHeaderForState(args.validation.state)
+            list.apply {
+                adapter = validationResultAdapter
+                addItemDecoration(
+                    RecylerViewPaddingDecorator(
+                        topPadding = R.dimen.spacing_small,
+                        leftPadding = R.dimen.spacing_small,
+                        rightPadding = R.dimen.spacing_small,
+                    )
+                )
+            }
+        }
+
         viewModel.listItems.observe2(this) {
             validationResultAdapter.update(it)
         }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/failed/DccValidationFailedViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/failed/DccValidationFailedViewModel.kt
@@ -5,6 +5,7 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.covidcertificate.validation.core.DccValidation
+import de.rki.coronawarnapp.covidcertificate.validation.core.rule.DccValidationRule
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.BusinessRuleFailedVH
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.BusinessRuleOpenVH
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.RuleHeaderVH
@@ -12,12 +13,12 @@ import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.comm
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.ValidationFaqVH
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.ValidationInputVH
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.ValidationOverallResultVH
-import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.ValidationPassedHintVH
 import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.listitem.ValidationResultItem
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 
 class DccValidationFailedViewModel @AssistedInject constructor(
     @Assisted private val validation: DccValidation,
@@ -29,17 +30,41 @@ class DccValidationFailedViewModel @AssistedInject constructor(
     }.asLiveData2()
 
     private suspend fun generateItems(): List<ValidationResultItem> {
-        return listOf(
+        val items = mutableListOf(
             ValidationInputVH.Item(validation),
-            ValidationOverallResultVH.Item(validation.state),
-            TechnicalValidationFailedVH.Item(validation),
-            RuleHeaderVH.Item(DccValidation.State.FAILURE),
-            BusinessRuleFailedVH.Item(validation.invalidationRules.first()),
-            RuleHeaderVH.Item(DccValidation.State.OPEN),
-            BusinessRuleOpenVH.Item(validation.acceptanceRules.first()),
-            ValidationFaqVH.Item,
-            ValidationPassedHintVH.Item,
+            ValidationOverallResultVH.Item(DccValidation.State.FAILURE)
         )
+
+        Timber.d("Generating items for state ${validation.state}")
+        when (validation.state) {
+            DccValidation.State.PASSED -> {
+                Timber.e("State PASSED but we are on screen 'FAILED', wrong navigation?")
+            }
+            DccValidation.State.OPEN -> {
+                Timber.e("State OPEN but we are on screen 'FAILED', wrong navigation?")
+            }
+            DccValidation.State.TECHNICAL_FAILURE -> {
+                items.add(RuleHeaderVH.Item(DccValidation.State.TECHNICAL_FAILURE))
+                items.add(TechnicalValidationFailedVH.Item(validation))
+            }
+            DccValidation.State.FAILURE -> {
+                val failedRules = validation.rules.filter { it.result == DccValidationRule.Result.FAILED }
+                if (failedRules.isNotEmpty()) {
+                    items.add(RuleHeaderVH.Item(DccValidation.State.FAILURE))
+                    failedRules.forEach { items.add(BusinessRuleFailedVH.Item(it)) }
+                }
+
+                val openRules = validation.rules.filter { it.result == DccValidationRule.Result.OPEN }
+                if (openRules.isNotEmpty()) {
+                    items.add(RuleHeaderVH.Item(DccValidation.State.OPEN))
+                    openRules.forEach { items.add(BusinessRuleOpenVH.Item(it)) }
+                }
+            }
+        }
+
+        items.add(ValidationFaqVH.Item)
+
+        return items.toList()
     }
 
     @AssistedFactory
