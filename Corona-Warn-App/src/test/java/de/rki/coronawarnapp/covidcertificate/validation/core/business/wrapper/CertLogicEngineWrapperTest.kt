a0f1d81d5f4cef391bc21f4247c7a7f5150eee5d
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-07-08 07:58:11 +0200

    klint

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/validation/core/business/wrapper/CertLogicEngineWrapperTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/validation/core/business/wrapper/CertLogicEngineWrapperTest.kt
@@ -6,6 +6,11 @@ import de.rki.coronawarnapp.covidcertificate.common.certificate.DccQrCodeExtract
 import de.rki.coronawarnapp.covidcertificate.vaccination.core.VaccinationQrCodeTestData
 import de.rki.coronawarnapp.covidcertificate.validation.core.rule.DccValidationRule
 import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runBlockingTest
 import org.joda.time.Instant
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -14,18 +19,24 @@ import javax.inject.Inject
 
 @Suppress("MaxLineLength")
 class CertLogicEngineWrapperTest : BaseTest() {
+    @MockK lateinit var valueSetWrapper: ValueSetWrapper
 
-    @Inject lateinit var wrapper: CertLogicEngineWrapper
+    lateinit var wrapper: CertLogicEngineWrapper
     @Inject lateinit var extractor: DccQrCodeExtractor
     @Inject lateinit var dccJsonSchema: DccJsonSchema
 
+    private val vaccinationValueMap = mapOf(countryCodes)
+
     @BeforeEach
     fun setup() {
+        MockKAnnotations.init(this)
         DaggerCovidCertificateTestComponent.factory().create().inject(this)
+        wrapper = CertLogicEngineWrapper(valueSetWrapper)
+        coEvery { valueSetWrapper.valueSetVaccination } returns flowOf(vaccinationValueMap)
     }
 
     @Test
-    fun `valid certificate passes`() {
+    fun `valid certificate passes`() = runBlockingTest {
         val rule = createVaccinationRule(
             validFrom = "2021-05-27T07:46:40Z",
             validTo = "2022-08-01T07:46:40Z",
