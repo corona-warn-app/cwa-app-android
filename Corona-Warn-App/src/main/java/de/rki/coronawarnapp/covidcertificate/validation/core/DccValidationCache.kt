a3538e4dd2073ba6ab112f0da4013ebbd0a96e6e
chris-cwa <chris.cwa.sap@gmail.com>
2021-07-09 15:43:06 +0200

    using the filename to differentiate log messages

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/core/DccValidationCache.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/core/DccValidationCache.kt
@@ -17,39 +17,38 @@ class DccValidationCache @Inject constructor(
     private val acceptanceRulesCacheFile = File(cacheDir, "dcc_validation_cache_acc_rules_raw")
     private val invalidationRulesCacheFile = File(cacheDir, "dcc_validation_cache_inv_rules_raw")
 
-    suspend fun loadCountryJson(): String? = countryCacheFile.load("raw dcc countries")
+    suspend fun loadCountryJson(): String? = countryCacheFile.load()
 
-    suspend fun loadAcceptanceRuleJson(): String? = acceptanceRulesCacheFile.load("raw acceptance rules")
+    suspend fun loadAcceptanceRuleJson(): String? = acceptanceRulesCacheFile.load()
 
-    suspend fun loadInvalidationRuleJson(): String? = invalidationRulesCacheFile.load("raw invalidation rules")
+    suspend fun loadInvalidationRuleJson(): String? = invalidationRulesCacheFile.load()
 
-    suspend fun saveCountryJson(data: String?) = countryCacheFile.save(data, "raw dcc countries")
+    suspend fun saveCountryJson(data: String?) = countryCacheFile.save(data)
 
-    suspend fun saveAcceptanceRulesJson(data: String?) = acceptanceRulesCacheFile.save(data, "raw acceptance rules")
+    suspend fun saveAcceptanceRulesJson(data: String?) = acceptanceRulesCacheFile.save(data)
 
-    suspend fun saveInvalidationRulesJson(data: String?) =
-        invalidationRulesCacheFile.save(data, "raw invalidation rules")
+    suspend fun saveInvalidationRulesJson(data: String?) = invalidationRulesCacheFile.save(data)
 
     @VisibleForTesting
-    internal suspend fun File.load(descr: String): String? = mutex.withLock {
+    internal suspend fun File.load(): String? = mutex.withLock {
         try {
             if (exists()) readText() else null
         } catch (e: Exception) {
-            Timber.tag(TAG).e(e, "Failed to load $descr from cache.")
+            Timber.tag(TAG).e(e, "Failed to load $name from cache.")
             null
         }
     }
 
     @VisibleForTesting
-    internal suspend fun File.save(data: String?, descr: String) = mutex.withLock {
+    internal suspend fun File.save(data: String?) = mutex.withLock {
         if (data == null) {
             if (exists() && delete()) {
-                Timber.tag(TAG).d("Cache file for $descr was deleted.")
+                Timber.tag(TAG).d("Cache file for $name was deleted.")
             }
             return
         }
         if (exists()) {
-            Timber.tag(TAG).d("Overwriting with new data for $descr (size=%d)", data.length)
+            Timber.tag(TAG).d("Overwriting with new data for $name (size=%d)", data.length)
         }
         parentFile?.mkdirs()
         writeText(data)
