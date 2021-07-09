98322c4832c8e8a52c5e3a968dbfa28feb278fa4
Matthias Urhahn <matthias.urhahn@sap.com>
2021-07-09 16:27:42 +0200

    🤷

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/covidcertificate/validation/core/rule/DccValidationRuleParcelTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/covidcertificate/validation/core/rule/DccValidationRuleParcelTest.kt
@@ -2,15 +2,56 @@ package de.rki.coronawarnapp.covidcertificate.validation.core.rule
 
 import android.os.Bundle
 import android.os.Parcel
+import de.rki.coronawarnapp.util.serialization.SerializationModule
 import io.kotest.matchers.shouldBe
 import org.junit.Test
 import testhelpers.BaseUITest
+import java.time.ZonedDateTime
 
 class DccValidationRuleParcelTest : BaseUITest() {
 
     @Test
     fun parcelization() {
-        val dccValidationRule = DccValidationRuleTest().createOne()
+        val dccValidationRule = DccValidationRule(
+            identifier = "identifier",
+            typeDcc = DccValidationRule.Type.ACCEPTANCE,
+            version = "1.0.0",
+            schemaVersion = "1.0.0",
+            engine = "engine",
+            engineVersion = "1.0.0",
+            certificateType = "general",
+            description = mapOf("a" to "b"),
+            validFrom = ZonedDateTime.parse("2021-05-20T07:46:40Z").toString(),
+            validTo = ZonedDateTime.parse("2021-05-27T07:46:40Z").toString(),
+            affectedFields = listOf("aField"),
+            logic = SerializationModule.jacksonBaseMapper.readTree(
+                """
+                    {
+                        "and": [
+                            {
+                                ">": [
+                                    {
+                                        "var": "hcert.v.0.dn"
+                                    },
+                                    0
+                                ]
+                            },
+                            {
+                                ">=": [
+                                    {
+                                        "var": "hcert.v.0.dn"
+                                    },
+                                    {
+                                        "var": "hcert.v.0.sd"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                """.trimIndent()
+            ),
+            country = "de",
+        )
 
         val bundle = Bundle().apply {
             putParcelable("dccValidationRule", dccValidationRule)
