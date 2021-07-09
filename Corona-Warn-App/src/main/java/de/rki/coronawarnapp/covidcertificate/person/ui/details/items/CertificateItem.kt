fc0e9421808ce8688cb3cf0883d88dfc1c5bc15c
Matthias Urhahn <matthias.urhahn@sap.com>
2021-07-09 15:59:45 +0200

    Improve `stableId` for person details list. * VaccinationInfo can be static * Previously we used just the certificate ID, but it's possible that the CWA contains two different types of certificates with the same ID.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/details/items/CertificateItem.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/details/items/CertificateItem.kt
@@ -1,5 +1,13 @@
 package de.rki.coronawarnapp.covidcertificate.person.ui.details.items
 
+import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
 import de.rki.coronawarnapp.util.lists.HasStableId
 
-interface CertificateItem : HasStableId
+interface CertificateItem : HasStableId {
+
+    fun CwaCovidCertificate.toStableListId(): Long = run {
+        var result = this::class.hashCode()
+        result = 31 * result + certificateId.hashCode()
+        result.toLong()
+    }
+}
