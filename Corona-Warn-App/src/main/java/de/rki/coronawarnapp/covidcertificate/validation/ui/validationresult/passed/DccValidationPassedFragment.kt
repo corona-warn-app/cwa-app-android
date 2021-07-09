e50abfbc4900d7d59e6062fcc608b9508832bf58
BMItr <Berndus@gmx.de>
2021-07-09 17:01:51 +0200

    Validation Result Common passed (EXPOSUREAPP-8031) (#3678)
    * Skeletons for validation result recyclerview items.
    
    * Remove accidental file.
    
    * update ValidationFaqVH
    
    * update ValidationFaqVH
    
    * update ValidationInputVH
    
    * update ValidationOverallResultVH
    
    * update strings
    
    * update RuleHeaderVH
    
    * fixing merge conflicts
    
    * update TechnicalValidationFailedVH
    
    * Introduce common adapter for validation results.
    
    * Adjust `DccValidation` and make user input part of the result data.
    Change UI navigation to allow passing the result via arguments.
    
    * Make `DccValidation` parcelable so we can take it with us when navigating.
    
    * Graphics and header+toolbar
    
    * REVERT ME before PR merge
    Setup for easier WIP
    
    * More list setup with test data.
    
    * Refactoring, determine list items async.
    
    * update layouts
    
    * small updates
    
    * add RecyclerViewPaddingDecorator
    
    * remove unused items
    
    * Remove unused singleton and fix usage of `ObjectMapper` to all use the same instance.
    
    * remove duplicated strings
    
    * fix unit tests
    
    * Always use mapper from `SerializationModule().jacksonObjectMapper()` to prevent tests from behaving differently in the future.
    
    * Implemented passed fragment
    
    * Added layout, Added strings, Added drawable
    
    * build again
    
    * remove comma
    
    * Fix instrumentation test.
    
    * Finish wiring and list layout.
    
    * Revert "REVERT ME before PR merge Setup for easier WIP"
    
    This reverts commit 98c0ee67
    
    * Update Corona-Warn-App/src/main/res/values-de/covid_certificate_strings.xml
    
    Co-authored-by: Natalia Lemmerth <83214960+NataliaLemmerth@users.noreply.github.com>
    
    * Update Corona-Warn-App/src/main/res/values/covid_certificate_strings.xml
    
    Co-authored-by: Natalia Lemmerth <83214960+NataliaLemmerth@users.noreply.github.com>
    
    * Add toolbar close listener.
    
    * Include covid_certificate_validation_result_fragments
    
    * Fixed little merge error
    
    * Removed margin
    
    * Added navigation
    
    * clean source
    
    * added NEW rule text to string res
    
    * added NEW no rules available strings
    
    * added darkmode elements
    
    * harmonized header naming
    
    * Added rule header to passed screen
    
    * adjusted specific textsize to 14sp according to figma
    
    * updated listener
    
    Co-authored-by: Matthias Urhahn <matthias.urhahn@sap.com>
    Co-authored-by: Juraj Kusnier <jurajkusnier@users.noreply.github.com>
    Co-authored-by: Juraj Kusnier <jurajkusnier@gmail.com>
    Co-authored-by: Natalia Lemmerth <83214960+NataliaLemmerth@users.noreply.github.com>
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/passed/DccValidationPassedFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/passed/DccValidationPassedFragment.kt
@@ -1,5 +1,73 @@
 package de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.passed
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.ValidationResultAdapter
+import de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.common.setHeaderForState
+import de.rki.coronawarnapp.databinding.CovidCertificateValidationPassedFragmentBinding
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.lists.decorations.RecylerViewPaddingDecorator
+import de.rki.coronawarnapp.util.lists.diffutil.update
+import de.rki.coronawarnapp.util.ui.observe2
+import de.rki.coronawarnapp.util.ui.popBackStack
+import de.rki.coronawarnapp.util.ui.viewBinding
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
+import javax.inject.Inject
 
-class DccValidationPassedFragment : Fragment()
+class DccValidationPassedFragment : Fragment(R.layout.covid_certificate_validation_passed_fragment), AutoInject {
+
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val binding: CovidCertificateValidationPassedFragmentBinding by viewBinding()
+    private val args: DccValidationPassedFragmentArgs by navArgs()
+    private val viewModel: DccValidationPassedViewModel by cwaViewModelsAssisted(
+        factoryProducer = { viewModelFactory },
+        constructorCall = { factory, _ ->
+            factory as DccValidationPassedViewModel.Factory
+            factory.create(validation = args.validation)
+        }
+    )
+
+    @Inject lateinit var validationResultAdapter: ValidationResultAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            covidCertificateValidationResultFragments.apply {
+                setHeaderForState(args.validation.state)
+                list.apply {
+                    adapter = validationResultAdapter
+                    val padding = R.dimen.spacing_small
+                    addItemDecoration(
+                        RecylerViewPaddingDecorator(
+                            topPadding = padding,
+                            leftPadding = padding,
+                            rightPadding = padding
+                        )
+                    )
+                }
+
+                toolbar.setNavigationOnClickListener { viewModel.onCloseClicked() }
+            }
+
+            checkAnotherCountryButton.setOnClickListener { viewModel.onCheckAnotherCountryClicked() }
+        }
+
+        viewModel.items.observe2(this) {
+            validationResultAdapter.update(it)
+        }
+
+        viewModel.navigation.observe2(this) {
+            handleNavigation(it)
+        }
+    }
+
+    private fun handleNavigation(navigation: DccValidationPassedNavigation) {
+        when (navigation) {
+            DccValidationPassedNavigation.Back -> popBackStack()
+        }
+    }
+}
