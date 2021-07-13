cec453fa49ce8dda54f60358c948dc8e1e7e4a1a
I502720 <axel.herbstreith@sap.com>
2021-07-13 16:47:50 +0200

    update failed vh

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/common/listitem/BusinessRuleFailedVH.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationresult/common/listitem/BusinessRuleFailedVH.kt
@@ -2,8 +2,10 @@ package de.rki.coronawarnapp.covidcertificate.validation.ui.validationresult.com
 
 import android.view.ViewGroup
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.coronatest.type.CoronaTest
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
 import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificate
+import de.rki.coronawarnapp.covidcertificate.validation.core.country.DccCountry
 import de.rki.coronawarnapp.covidcertificate.validation.core.rule.EvaluatedDccRule
 import de.rki.coronawarnapp.databinding.CovidCertificateValidationResultRuleFailedItemBinding
 import de.rki.coronawarnapp.util.lists.diffutil.HasPayloadDiffer
@@ -27,11 +29,32 @@ class BusinessRuleFailedVH(
         item: Item,
         payloads: List<Any>,
     ) -> Unit = { item, _ ->
-        ruleId.text = item.evaluatedDccRule.rule.identifier
 
         when (item.certificate) {
             is TestCertificate -> {
-                testCenterName.text = item.certificate.testCenter
+                if(
+                    item.certificate.testType != CoronaTest.Type.PCR.raw &&
+                    item.certificate.testType != CoronaTest.Type.RAPID_ANTIGEN.raw
+                ) {
+
+                    title.text = context.getString(R.string.validation_rules_failed_vh_title_wrong_test_type)
+                    subtitle.text = context.getString(
+                        R.string.validation_rules_open_vh_subtitle,
+                        DccCountry(item.evaluatedDccRule.rule.country).displayName()
+                    )
+                    line1.text = context.getString(R.string.validation_rules_failed_vh_test_type)
+                    line2.text = item.certificate.testType
+                    line3.text = context.getString(R.string.validation_rules_failed_vh_rule_id)
+                    line4.text = item.evaluatedDccRule.rule.identifier
+
+                } else {
+                    title.text = context.getString(R.string.validation_rules_failed_vh_title_uncertified_test_center)
+                    subtitle.text = context.getString(R.string.validation_rules_failed_vh_subtitle_uncertified_test_center)
+                    line1.text = context.getString(R.string.validation_rules_failed_vh_test_center_title)
+                    line2.text = item.certificate.testCenter
+                    line3.text = context.getString(R.string.validation_rules_failed_vh_rule_id)
+                    line4.text = item.evaluatedDccRule.rule.identifier
+                }
             }
         }
     }
