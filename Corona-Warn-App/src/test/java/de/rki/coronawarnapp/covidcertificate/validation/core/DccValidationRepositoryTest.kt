326531dede889474df4125e41b2fb1ea4599f20f
chris-cwa <69595386+chris-cwa@users.noreply.github.com>
2021-07-13 09:38:36 +0200

    Validation repo (EXPOSUREAPP-7618) (#3658)
    * + rules set data structure
    
    * adopted api, because we expect a zip file
    
    * unzip rule bin
    
    * cache rules
    
    * clear rules
    
    * parsing data
    
    * updated data refreshing
    
    * updated access to repo
    
    * using the filename to differentiate log messages
    
    * removed unused class
    
    * fixed coroutine context
    
    * removed duplicate code
    
    * fixed unit test
    
    * additional tests
    
    * unused import :O
    
    * code formatting
    
    * additional tests
    
    * Extract GSON JsonNodeAdapter and add tests.
    
    * Cleanup use of JSONObject: JSONObject.NULL -> JsonToken.NULL as we use JsonObject (GSON) not JSONObject (android).
    
    * Add country filtering to rules.
    
    * Rule data is CBOR encoded too.
    
    * Adapt rule parsing and tests to new test data.
    
    * Align dccCountry error handling with rules retrieval.
    
    * If the cached json does not match our expected serialization format, throw it away.
    
    * Only save the rawJson after successful parsing.
    
    * Use SerializedName for enums of custom type adapter.
    
    * Add error handling for invalid json formats.
    
    * Fix logtag
    
    * Additional logging.
    
    * Nobody expects the instrumentation tests...
    
    Co-authored-by: Matthias Urhahn <matthias.urhahn@sap.com>

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/validation/core/DccValidationRepositoryTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/validation/core/DccValidationRepositoryTest.kt
@@ -1,8 +1,13 @@
 package de.rki.coronawarnapp.covidcertificate.validation.core
 
+import de.rki.coronawarnapp.covidcertificate.validation.core.common.exception.DccValidationException
 import de.rki.coronawarnapp.covidcertificate.validation.core.country.DccCountry
+import de.rki.coronawarnapp.covidcertificate.validation.core.rule.DccValidationRule
+import de.rki.coronawarnapp.covidcertificate.validation.core.rule.DccValidationRule.Description
+import de.rki.coronawarnapp.covidcertificate.validation.core.rule.DccValidationRule.Type
 import de.rki.coronawarnapp.covidcertificate.validation.core.server.DccValidationServer
 import de.rki.coronawarnapp.util.serialization.SerializationModule
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
@@ -23,40 +28,132 @@ class DccValidationRepositoryTest : BaseTest() {
     @MockK lateinit var localCache: DccValidationCache
 
     private val testCountryData = "[\"DE\",\"NL\"]"
+    private val testAcceptanceRulesData =
+        """
+            [
+                {
+                    "Type": "Acceptance",
+                    "Logic": {
+                        "!": [
+                            {
+                                "var": "payload.v.1"
+                            }
+                        ]
+                    },
+                    "Engine": "CERTLOGIC",
+                    "Country": "LT",
+                    "ValidTo": "2023-07-04T00:00:00Z",
+                    "Version": "1.0.0",
+                    "ValidFrom": "2021-07-04T15:00:00Z",
+                    "Identifier": "VR-LT-0000",
+                    "Description": [
+                        {
+                            "desc": "One type of event of vaccination",
+                            "lang": "en"
+                        }
+                    ],
+                    "EngineVersion": "1.0.0",
+                    "SchemaVersion": "1.0.0",
+                    "AffectedFields": [
+                        "v.1"
+                    ],
+                    "CertificateType": "Vaccination"
+                }
+            ]
+        """.trimIndent()
+    private val testInvalidationRulesData =
+        """
+            [
+                {
+                    "Type": "Invalidation",
+                    "Logic": {
+                        "!": [
+                            {
+                                "var": "payload.v.1"
+                            }
+                        ]
+                    },
+                    "Engine": "CERTLOGIC",
+                    "Country": "LT",
+                    "ValidTo": "2023-07-04T00:00:00Z",
+                    "Version": "1.0.0",
+                    "ValidFrom": "2021-07-04T15:00:00Z",
+                    "Identifier": "IR-DE-0000",
+                    "Description": [
+                        {
+                            "desc": "One type of event of vaccination",
+                            "lang": "en"
+                        }
+                    ],
+                    "EngineVersion": "1.0.0",
+                    "SchemaVersion": "1.0.0",
+                    "AffectedFields": [
+                        "v.1"
+                    ],
+                    "CertificateType": "Vaccination"
+                }
+            ]
+        """.trimIndent()
 
     @BeforeEach
     fun setup() {
         MockKAnnotations.init(this)
 
         localCache.apply {
-            coEvery { loadJson() } returns null
-            coEvery { saveJson(any()) } just Runs
+            coEvery { loadCountryJson() } returns null
+            coEvery { saveCountryJson(any()) } just Runs
+            coEvery { loadAcceptanceRuleJson() } returns null
+            coEvery { saveAcceptanceRulesJson(any()) } just Runs
+            coEvery { loadInvalidationRuleJson() } returns null
+            coEvery { saveInvalidationRulesJson(any()) } just Runs
         }
 
         server.apply {
             coEvery { dccCountryJson() } returns testCountryData
+            coEvery { ruleSetJson(Type.ACCEPTANCE) } returns testAcceptanceRulesData
+            coEvery { ruleSetJson(Type.INVALIDATION) } returns testInvalidationRulesData
         }
     }
+
+    private val serializationModule = SerializationModule()
+    private val baseGson = serializationModule.baseGson()
+    private val objectMapper = serializationModule.jacksonObjectMapper()
 
     private fun createInstance(scope: CoroutineScope) = DccValidationRepository(
         appScope = scope,
         dispatcherProvider = TestDispatcherProvider(),
-        gson = SerializationModule().baseGson(),
+        gson = baseGson,
         server = server,
         localCache = localCache,
     )
 
     @Test
-    fun `local cache is loaded on init - no server request`() = runBlockingTest2(ignoreActive = true) {
+    fun `local cache is loaded on init - no server requests`() = runBlockingTest2(ignoreActive = true) {
         createInstance(this).apply {
             dccCountries.first() shouldBe emptyList()
+            acceptanceRules.first() shouldBe emptyList()
+            invalidationRules.first() shouldBe emptyList()
         }
 
         coVerify {
-            localCache.loadJson()
+            localCache.loadCountryJson()
         }
         coVerify(exactly = 0) {
             server.dccCountryJson()
+        }
+
+        coVerify {
+            localCache.loadAcceptanceRuleJson()
+        }
+        coVerify(exactly = 0) {
+            server.ruleSetJson(Type.ACCEPTANCE)
+        }
+
+        coVerify {
+            localCache.loadInvalidationRuleJson()
+        }
+        coVerify(exactly = 0) {
+            server.ruleSetJson(Type.INVALIDATION)
         }
     }
 
@@ -67,11 +164,116 @@ class DccValidationRepositoryTest : BaseTest() {
             dccCountries.first() shouldBe listOf(
                 DccCountry("DE"), DccCountry("NL")
             )
+            acceptanceRules.first() shouldBe listOf(
+                DccValidationRule(
+                    identifier = "VR-LT-0000",
+                    typeDcc = Type.ACCEPTANCE,
+                    country = "LT",
+                    version = "1.0.0",
+                    schemaVersion = "1.0.0",
+                    engine = "CERTLOGIC",
+                    engineVersion = "1.0.0",
+                    certificateType = "Vaccination",
+                    description = listOf(Description("en", "One type of event of vaccination")),
+                    validFrom = "2021-07-04T15:00:00Z",
+                    validTo = "2023-07-04T00:00:00Z",
+                    affectedFields = listOf("v.1"),
+                    logic = objectMapper.readTree("{\"!\":[{\"var\":\"payload.v.1\"}]}")
+                )
+            )
+            invalidationRules.first() shouldBe listOf(
+                DccValidationRule(
+                    identifier = "IR-DE-0000",
+                    typeDcc = Type.INVALIDATION,
+                    country = "LT",
+                    version = "1.0.0",
+                    schemaVersion = "1.0.0",
+                    engine = "CERTLOGIC",
+                    engineVersion = "1.0.0",
+                    certificateType = "Vaccination",
+                    description = listOf(Description("en", "One type of event of vaccination")),
+                    validFrom = "2021-07-04T15:00:00Z",
+                    validTo = "2023-07-04T00:00:00Z",
+                    affectedFields = listOf("v.1"),
+                    logic = objectMapper.readTree("{\"!\":[{\"var\":\"payload.v.1\"}]}")
+                )
+            )
         }
 
         coVerify {
             server.dccCountryJson()
-            localCache.saveJson(testCountryData)
+            localCache.saveCountryJson(testCountryData)
+            server.ruleSetJson(Type.ACCEPTANCE)
+            localCache.saveAcceptanceRulesJson(testAcceptanceRulesData)
+            server.ruleSetJson(Type.INVALIDATION)
+            localCache.saveInvalidationRulesJson(testInvalidationRulesData)
+        }
+    }
+
+    @Test
+    fun `bad acceptance rules yields exception`() = runBlockingTest2(ignoreActive = true) {
+        // Missing attributes
+        coEvery { server.ruleSetJson(Type.ACCEPTANCE) } returns """
+            [
+                {
+                    "Type": "Acceptance",
+                    "Engine": "CERTLOGIC",
+                    "Country": "LT",
+                    "Version": "1.0.0",
+                    "Identifier": "VR-LT-0000",
+                    "EngineVersion": "1.0.0",
+                    "SchemaVersion": "1.0.0",
+                }
+            ]
+        """.trimIndent()
+
+        val instance = createInstance(this)
+
+        shouldThrow<DccValidationException> {
+            instance.refresh()
+        }.errorCode shouldBe DccValidationException.ErrorCode.ACCEPTANCE_RULE_JSON_DECODING_FAILED
+
+        coVerify {
+            server.dccCountryJson()
+            localCache.saveCountryJson(testCountryData)
+            server.ruleSetJson(Type.ACCEPTANCE)
+        }
+        coVerify(exactly = 0) {
+            localCache.saveAcceptanceRulesJson(any())
+            server.ruleSetJson(Type.INVALIDATION)
+            localCache.saveInvalidationRulesJson(any())
+        }
+    }
+
+    @Test
+    fun `bad invaldation rules yields exception`() = runBlockingTest2(ignoreActive = true) {
+        // Missing attributes
+        coEvery { server.ruleSetJson(Type.INVALIDATION) } returns """
+            [
+                {
+                    "Type": "Invalidation",
+                    "Engine": "CERTLOGIC",
+                    "Country": "LT",
+                    "Version": "1.0.0",
+                    "Identifier": "VR-LT-0000",
+                    "EngineVersion": "1.0.0",
+                    "SchemaVersion": "1.0.0",
+                }
+            ]
+        """.trimIndent()
+        shouldThrow<DccValidationException> {
+            createInstance(this).refresh()
+        }.errorCode shouldBe DccValidationException.ErrorCode.INVALIDATION_RULE_JSON_DECODING_FAILED
+
+        coVerify {
+            server.dccCountryJson()
+            localCache.saveCountryJson(testCountryData)
+            server.ruleSetJson(Type.ACCEPTANCE)
+            localCache.saveAcceptanceRulesJson(testAcceptanceRulesData)
+            server.ruleSetJson(Type.INVALIDATION)
+        }
+        coVerify(exactly = 0) {
+            localCache.saveInvalidationRulesJson(any())
         }
     }
 }
