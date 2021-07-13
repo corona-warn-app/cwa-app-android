000beb1ed2cb54ab13d7141d60aa1d5987f0782e
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-07-13 16:42:10 +0200

    Select latest country selection

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationstart/ValidationStartViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/validation/ui/validationstart/ValidationStartViewModel.kt
@@ -38,6 +38,7 @@ class ValidationStartViewModel @AssistedInject constructor(
     private val uiState = MutableStateFlow(UIState())
     val state: LiveData<UIState> = uiState.asLiveData2()
     val currentDateTime: DateTime get() = uiState.value.dateTime
+    val currentCountryCode: String get() = uiState.value.dccCountry.countryCode
     val events = SingleLiveEvent<StartValidationNavEvent>()
     val countryList = dccValidationRepository.dccCountries.map { countryList ->
         if (countryList.isEmpty()) listOf(DccCountry(DE)) else countryList
@@ -76,7 +77,7 @@ class ValidationStartViewModel @AssistedInject constructor(
     }
 
     data class UIState(
-        val dccCountry: DccCountry = DccCountry("DE"),
+        val dccCountry: DccCountry = DccCountry(DE),
         val dateTime: DateTime = DateTime.now(),
     ) {
         fun formattedDateTime() = dateTime.run { "${toDayFormat()} ${toShortTimeFormat()}" }
