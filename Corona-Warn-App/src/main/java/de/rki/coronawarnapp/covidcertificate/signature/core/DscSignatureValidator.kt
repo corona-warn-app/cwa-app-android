b33883da1408dce065d4f8bffea0d40056a6484f
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-07-19 12:47:49 +0200

    Exception

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/DscSignatureValidator.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/DscSignatureValidator.kt
@@ -2,14 +2,19 @@ package de.rki.coronawarnapp.covidcertificate.signature.core
 
 import dagger.Reusable
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccData
+import de.rki.coronawarnapp.covidcertificate.signature.core.exception.DscSignatureValidationException
 import timber.log.Timber
 import javax.inject.Inject
 
 @Reusable
 class DscSignatureValidator @Inject constructor() {
 
+    /**
+     *
+     * @throws DscSignatureValidationException
+     */
     suspend fun isSignatureValid(dscData: DscData, certificateData: DccData<*>): Boolean {
-        Timber.tag(TAG).d("isSignatureValid(certificateData=%s)", certificateData.header)
+        Timber.tag(TAG).d("isSignatureValid(dscData=%s,certificateData=%s)", dscData, certificateData)
         // TODO
         return true
     }
