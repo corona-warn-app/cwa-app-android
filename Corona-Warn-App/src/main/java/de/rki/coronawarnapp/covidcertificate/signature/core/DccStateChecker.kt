dc4d0ce44b47f81b67e002b637956a2d2fe3e995
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-07-21 17:52:21 +0200

    clean up

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/DccStateChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/DccStateChecker.kt
@@ -5,12 +5,12 @@ import dagger.Reusable
 import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccData
+import de.rki.coronawarnapp.util.TimeAndDateExtensions.daysUntil
 import de.rki.coronawarnapp.util.TimeStamper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import org.joda.time.DateTimeZone
-import org.joda.time.Days
 import org.joda.time.Instant
 import javax.inject.Inject
 
@@ -42,7 +42,7 @@ internal fun DccData<*>.getExpirationState(
     timeZone: DateTimeZone = DateTimeZone.getDefault()
 ): CwaCovidCertificate.State {
     val expiresAt = header.expiresAt
-    val daysUntilExpiration = expiresAt.daysUntil(now, timeZone)
+    val daysUntilExpiration = now.daysUntil(expiresAt, timeZone)
     val diff = daysUntilExpiration - expirationThresholdInDays
     return when {
         daysUntilExpiration <= 0 -> CwaCovidCertificate.State.Expired(expiresAt)
@@ -50,13 +50,4 @@ internal fun DccData<*>.getExpirationState(
         diff <= 0 -> CwaCovidCertificate.State.ExpiringSoon(expiresAt)
         else -> throw IllegalArgumentException() // impossible!
     }
-}
-
-private fun Instant.daysUntil(
-    now: Instant,
-    timeZone: DateTimeZone = DateTimeZone.getDefault()
-): Int {
-    val expirationDate = toDateTime(timeZone).toLocalDate()
-    val today = now.toDateTime(timeZone).toLocalDate()
-    return Days.daysBetween(today, expirationDate).days
 }
