c18f425eae685f21970a99f51b8ec4adbecace98
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-07-22 12:21:24 +0200

    Create DscSignatureValidatorTest.kt

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/signature/core/DscSignatureValidatorTest.kt
@@ -0,0 +1,21 @@
+package de.rki.coronawarnapp.covidcertificate.signature.core
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+
+internal class DscSignatureValidatorTest : BaseTest() {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun validateSignature() {
+    }
+}
