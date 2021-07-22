616ebccce67a8ffebad90b548a90608f18365e59
Marc Auberer <marc.auberer@chillibits.com>
2021-07-22 15:30:12 +0200

    Get string resources from root

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/create/RATProfileCreateFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/coronatest/rat/profile/create/RATProfileCreateFragment.kt
@@ -70,7 +70,7 @@ class RATProfileCreateFragment : Fragment(R.layout.rat_profile_create_fragment),
             phoneInputEdit.setOnFocusChangeListener { _, hasFocus ->
                 // Validate phone number
                 if (!hasFocus && !Patterns.PHONE.matcher(phoneInputEdit.text.toString()).matches()) {
-                    phoneInputLayout.error = getString(R.string.rat_profile_create_phone_error);
+                    phoneInputLayout.error = getRoot().getResources().getString(R.string.rat_profile_create_phone_error)
                 } else {
                     phoneInputLayout.error = null
                 }
@@ -89,7 +89,7 @@ class RATProfileCreateFragment : Fragment(R.layout.rat_profile_create_fragment),
             emailInputEdit.setOnFocusChangeListener { _, hasFocus ->
                 // Validate email
                 if (!hasFocus && !Patterns.EMAIL_ADDRESS.matcher(emailInputEdit.text.toString()).matches()) {
-                    emailInputLayout.error = getString(R.string.rat_profile_create_email_error)
+                    emailInputLayout.error = getRoot().getResources().getString(R.string.rat_profile_create_email_error)
                 } else {
                     emailInputLayout.error = null
                 }
