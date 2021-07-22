ebeeae3ffe751efb4bd197d963e4af1aef4b0a8d
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-07-22 13:12:55 +0200

    check state

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/execution/DccStateCheckWorker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/execution/DccStateCheckWorker.kt
@@ -6,12 +6,15 @@ import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
+import de.rki.coronawarnapp.covidcertificate.common.repository.CertificateContainerId
 import de.rki.coronawarnapp.covidcertificate.recovery.core.RecoveryCertificateRepository
 import de.rki.coronawarnapp.covidcertificate.signature.core.DccStateChecker
 import de.rki.coronawarnapp.covidcertificate.signature.ui.notification.DscCheckNotification
 import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificateRepository
 import de.rki.coronawarnapp.covidcertificate.vaccination.core.repository.VaccinationRepository
 import de.rki.coronawarnapp.util.worker.InjectedWorkerFactory
+import kotlinx.coroutines.flow.first
 
 class DccStateCheckWorker @AssistedInject constructor(
     @Assisted val context: Context,
@@ -24,10 +27,36 @@ class DccStateCheckWorker @AssistedInject constructor(
 ) : CoroutineWorker(context, workerParams) {
 
     override suspend fun doWork(): Result {
-        // TODO
-        // Check certificate states
-        // Show notification if necessary
+
+        vaccinationRepository.vaccinationInfos.first().forEach {
+            val certificate = it.getMostRecentVaccinationCertificate
+            when (certificate.getState()) {
+                is CwaCovidCertificate.State.ExpiringSoon -> showNotificationExpiringSoon(certificate.containerId)
+                is CwaCovidCertificate.State.Expired -> showNotificationExpired(certificate.containerId)
+                is CwaCovidCertificate.State.Invalid -> TODO()
+                is CwaCovidCertificate.State.Valid -> {/*all good*/
+                }
+            }
+        }
+
+        recoveryCertificateRepository.certificates.first().forEach {
+            val certificate = it.recoveryCertificate
+            when (certificate.getState()) {
+                is CwaCovidCertificate.State.ExpiringSoon -> showNotificationExpiringSoon(certificate.containerId)
+                is CwaCovidCertificate.State.Expired -> showNotificationExpired(certificate.containerId)
+                is CwaCovidCertificate.State.Invalid -> TODO()
+                is CwaCovidCertificate.State.Valid -> {/*all good*/
+                }
+            }
+        }
+
         return Result.retry()
+    }
+
+    private fun showNotificationExpired(containerId: CertificateContainerId) {
+    }
+
+    private fun showNotificationExpiringSoon(containerId: CertificateContainerId) {
     }
 
     @AssistedFactory
