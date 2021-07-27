46fc41f8b8d24708516834c5e6d550840964e040
Juraj Kusnier <jurajkusnier@gmail.com>
2021-07-27 10:57:41 +0200

    add DefaultDscSourceTest

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/signature/core/storage/DefaultDscSourceTest.kt
@@ -0,0 +1,52 @@
+package de.rki.coronawarnapp.covidcertificate.signature.core.storage
+
+import android.content.Context
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import de.rki.coronawarnapp.covidcertificate.signature.core.DscDataParser
+import de.rki.coronawarnapp.util.HashExtensions.toSHA256
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.MockKAnnotations
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import testhelpers.BaseTest
+import testhelpers.EmptyApplication
+
+@Config(sdk = [Build.VERSION_CODES.P], application = EmptyApplication::class)
+@RunWith(RobolectricTestRunner::class)
+internal class DefaultDscSourceTest: BaseTest() {
+
+    private val dscListName = "default_dsc_list.bin"
+    private val checkSumName = "default_dsc_list.sha256"
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    val context: Context
+        get() = ApplicationProvider.getApplicationContext()
+
+    @Test
+    fun `current default matches checksum`() {
+        val dscList = context.assets.open(dscListName).readBytes()
+        val sha256 = context.assets.open(checkSumName).readBytes().toString(Charsets.UTF_8)
+        sha256 shouldBe "6fb4fd075aa5ed2f4f98640517b751dfdcb2b700a1069124449b64389087d441"
+        dscList.toSHA256() shouldBe sha256
+    }
+
+    @Test
+    fun `current default config can be parsed`() {
+        shouldNotThrowAny {
+            val rawDscList = context.assets.open(dscListName).readBytes()
+            val parsedDscList = DscDataParser().parse(rawDscList)
+            parsedDscList shouldNotBe null
+        }
+    }
+
+}
