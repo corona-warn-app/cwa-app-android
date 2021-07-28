b4ef34665875d9f6b09b9afd62e20a1da0c1bba9
Juraj Kusnier <jurajkusnier@gmail.com>
2021-07-28 18:04:10 +0200

    use AssetManager instead of context

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/storage/DefaultDscSource.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/storage/DefaultDscSource.kt
@@ -1,21 +1,20 @@
 package de.rki.coronawarnapp.covidcertificate.signature.core.storage
 
-import android.content.Context
+import android.content.res.AssetManager
 import de.rki.coronawarnapp.covidcertificate.signature.core.DscData
 import de.rki.coronawarnapp.covidcertificate.signature.core.DscDataParser
-import de.rki.coronawarnapp.util.di.AppContext
 import org.joda.time.Instant
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DefaultDscSource @Inject constructor(
-    @AppContext private val context: Context,
+    private val assets: AssetManager,
     private val dscDataParser: DscDataParser,
 ) {
 
     fun getDscData(): DscData {
-        val rawData = context.assets.open("default_dsc_list.bin").use { it.readBytes() }
+        val rawData = assets.open("default_dsc_list.bin").use { it.readBytes() }
         return dscDataParser.parse(rawData, Instant.EPOCH)
     }
 }
