842a126f762d426614a93e630d6c5e24f643550e
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-08-02 16:22:25 +0200

    Fix imports

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/DscDataParser.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/signature/core/DscDataParser.kt
@@ -1,7 +1,7 @@
 package de.rki.coronawarnapp.covidcertificate.signature.core
 
 import dagger.Reusable
-import de.rki.coronawarnapp.server.protocols.internal.dgc.DscListOuterClass
+import de.rki.coronawarnapp.server.protocols.internal.dgc.DscList
 import de.rki.coronawarnapp.util.toOkioByteString
 import org.joda.time.Instant
 import javax.inject.Inject
@@ -10,7 +10,7 @@ import javax.inject.Inject
 class DscDataParser @Inject constructor() {
 
     fun parse(rawData: ByteArray, updatedAt: Instant = Instant.now()): DscData = DscData(
-        dscList = DscListOuterClass.DscList.parseFrom(rawData).certificatesList.map {
+        dscList = DscList.DSCList.parseFrom(rawData).certificatesList.map {
             DscItem(
                 kid = it.kid.toOkioByteString().base64(),
                 data = it.data.toOkioByteString()
