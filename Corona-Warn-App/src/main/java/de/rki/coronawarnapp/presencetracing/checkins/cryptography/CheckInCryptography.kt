f5ff8dfa3bfa9eee581d7f451645a1a0a80363a5
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-08-03 10:58:20 +0200

    Common config and cryptography interface

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/checkins/cryptography/CheckInCryptography.kt
@@ -0,0 +1,25 @@
+package de.rki.coronawarnapp.presencetracing.checkins.cryptography
+
+import de.rki.coronawarnapp.presencetracing.checkins.CheckIn
+import de.rki.coronawarnapp.server.protocols.internal.pt.CheckInOuterClass.CheckInProtectedReport
+import de.rki.coronawarnapp.server.protocols.internal.pt.TraceWarning
+import de.rki.coronawarnapp.util.security.RandomStrong
+import javax.inject.Inject
+import kotlin.random.Random
+
+class CheckInCryptography @Inject constructor(
+    @RandomStrong private val randomSource: Random,
+) {
+    fun encrypt(
+        checkIn: CheckIn,
+        transmissionRiskLevel: Int
+    ): CheckInProtectedReport {
+        // TODO
+        throw NotImplementedError()
+    }
+
+    fun decrypt(checkInProtectedReport: CheckInProtectedReport): TraceWarning.TraceTimeIntervalWarning {
+        // TODO
+        throw NotImplementedError()
+    }
+}
---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/presencetracing/checkins/cryptography/CheckInCryptographyTest.kt
@@ -0,0 +1,16 @@
+package de.rki.coronawarnapp.presencetracing.checkins.cryptography
+
+import org.junit.jupiter.api.Test
+
+internal class CheckInCryptographyTest {
+
+    @Test
+    fun encrypt() {
+        // TODO
+    }
+
+    @Test
+    fun decrypt() {
+        // TODO
+    }
+}
