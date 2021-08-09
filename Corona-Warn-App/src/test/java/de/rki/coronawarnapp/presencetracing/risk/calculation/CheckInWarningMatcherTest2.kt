754ae93aea1621f78d7250729330ba71cb2f7082
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-08-09 12:25:37 +0200

    More unit tests for deriving

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/presencetracing/risk/calculation/CheckInWarningMatcherTest2.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/presencetracing/risk/calculation/CheckInWarningMatcherTest2.kt
@@ -14,6 +14,7 @@ import testhelpers.TestDispatcherProvider
 import java.security.SecureRandom
 import kotlin.random.asKotlinRandom
 
+@Suppress("MaxLineLength")
 internal class CheckInWarningMatcherTest2 : BaseTest() {
 
     private fun createInstance() = CheckInWarningMatcher(
@@ -42,7 +43,7 @@ internal class CheckInWarningMatcherTest2 : BaseTest() {
     }
 
     @Test
-    fun `deriveTraceWarnings with invalid decoding returns emptyList`() {
+    fun `deriveTraceWarnings with failed decoding returns emptyList`() {
         val checkIn1 = createCheckIn(
             id = 2L,
             traceLocationId = "fe84394e73838590cc7707aba0350c130f6d0fb6f0f2535f9735f481dee61871",
@@ -64,5 +65,68 @@ internal class CheckInWarningMatcherTest2 : BaseTest() {
             )
             .build()
         createInstance().deriveTraceWarnings(listOf(checkInReport1, checkInReport2), listOf(checkIn1)).size shouldBe 0
+    }
+
+    @Test
+    fun `deriveTraceWarnings pass`() {
+        val checkIn1 = createCheckIn(
+            id = 1L,
+            traceLocationId = "9baf3a40312f39849f46dad1040f2f039f1cffa1238c41e9db675315cfad39b6",
+            startDateStr = "2021-03-04T10:15+01:00",
+            endDateStr = "2021-03-04T10:17+01:00"
+        )
+
+        val checkIn2 = createCheckIn(
+            id = 2L,
+            traceLocationId = "03ad6b333d445099c7dfe0ff745ec5cc1330d149ef752f36c3aed4efea13f725",
+            startDateStr = "2021-03-04T10:15+01:00",
+            endDateStr = "2021-03-04T10:17+01:00"
+        )
+
+        val checkInReport1 = CheckInOuterClass.CheckInProtectedReport.parseFrom(
+            "CiAZ1gsaWtAA/s9cqffC7ZHMbdVDpRP+ftrdFu45maI48BIQ+VNLZEr+j6qotkv8v1ASlRoQ7v9eQ9R2JtDnU70gn4cidSIgg6KU9FjzawyGA5ZPiZxfgxt8Ou5pSVWqqyAaBkQEA4k=".decodeBase64()!!
+                .toByteArray()
+        )
+
+        val checkInReport2 = CheckInOuterClass.CheckInProtectedReport.parseFrom(
+            "CiAL+oCKqy4KUKKS/lH7yaD4WM2lE8O0MI67WcHZQ1h8tBIQSM6n2ApMmwWCEVwex9yrmBoQXpP2tOktT+RqKNtvv3qbxiIgS62l9vcMeisaFItADTNgTC9a6uEGYkIEL9ISx50LZSo=".decodeBase64()!!
+                .toByteArray()
+        )
+        createInstance().deriveTraceWarnings(
+            listOf(checkInReport1, checkInReport2),
+            listOf(checkIn1, checkIn2)
+        ).size shouldBe 2
+    }
+
+    @Test
+    fun `deriveTraceWarnings returns emptyList for invalid TraceWarnings`() {
+        val checkIn1 = createCheckIn(
+            id = 1L,
+            traceLocationId = "9baf3a40312f39849f46dad1040f2f039f1cffa1238c41e9db675315cfad39b6",
+            startDateStr = "2021-03-04T10:15+01:00",
+            endDateStr = "2021-03-04T10:17+01:00"
+        )
+
+        val checkIn2 = createCheckIn(
+            id = 2L,
+            traceLocationId = "03ad6b333d445099c7dfe0ff745ec5cc1330d149ef752f36c3aed4efea13f725",
+            startDateStr = "2021-03-04T10:15+01:00",
+            endDateStr = "2021-03-04T10:17+01:00"
+        )
+
+        val checkInReport1 = CheckInOuterClass.CheckInProtectedReport.parseFrom(
+            "CiAZ1gsaWtAA/s9cqffC7ZHMbdVDpRP+ftrdFu45maI48BIQ+VNLZEr+j6qotkv8v1ASlRoQoCFU/onRPTyGHAoQlg3TfyIguFGG1DysdISzetkTdAb4YRKBgEkwnGipMW6gXwNt0oo=".decodeBase64()!!
+                .toByteArray()
+        )
+
+        val checkInReport2 = CheckInOuterClass.CheckInProtectedReport.parseFrom(
+            "CiAL+oCKqy4KUKKS/lH7yaD4WM2lE8O0MI67WcHZQ1h8tBIQSM6n2ApMmwWCEVwex9yrmBoQA8Lb80cx5LhT/uhgsHTI1yIgTcZa3PaEuk2/cHZI4EKC8j3aznAsIhjWAcWNkPyPabk=".decodeBase64()!!
+                .toByteArray()
+        )
+
+        createInstance().deriveTraceWarnings(
+            listOf(checkInReport1, checkInReport2),
+            listOf(checkIn1, checkIn2)
+        ).size shouldBe 0
     }
 }
