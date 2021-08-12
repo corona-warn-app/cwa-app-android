9fe00925a7722b124ec6243cfb8b2345993cf1a3
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-08-12 17:21:52 +0200

    Fix

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionException.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionException.kt
@@ -3,7 +3,7 @@ package de.rki.coronawarnapp.presencetracing.organizer.submission
 class OrganizerSubmissionException(
     val errorCode: ErrorCode,
     override val cause: Throwable? = null
-) : Exception(cause = cause) {
+) : Exception(cause) {
 
     // TODO finalise error handling and display right messages
     enum class ErrorCode {
