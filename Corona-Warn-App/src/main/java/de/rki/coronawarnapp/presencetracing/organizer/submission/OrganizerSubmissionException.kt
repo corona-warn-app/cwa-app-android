c1e1416891d5e3f85ed48cc5eb4045b661738fca
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-08-13 11:57:47 +0200

    Refactoring

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionException.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionException.kt
@@ -1,11 +1,10 @@
 package de.rki.coronawarnapp.presencetracing.organizer.submission
 
+// TODO Use Errors from Error handling ticket
 class OrganizerSubmissionException(
     val errorCode: ErrorCode,
     override val cause: Throwable? = null
 ) : Exception(cause) {
-
-    // TODO finalise error handling and display right messages
     enum class ErrorCode {
         SUBMISSION_OB_CLIENT_ERROR,
         SUBMISSION_OB_SERVER_ERROR,
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionRepository.kt
@@ -1,8 +1,10 @@
 package de.rki.coronawarnapp.presencetracing.organizer.submission
 
 import dagger.Reusable
+import de.rki.coronawarnapp.coronatest.server.RegistrationRequest
 import de.rki.coronawarnapp.coronatest.server.VerificationServer
-import de.rki.coronawarnapp.presencetracing.checkins.CheckInsTransformer
+import de.rki.coronawarnapp.presencetracing.checkins.CheckIn
+import de.rki.coronawarnapp.presencetracing.checkins.OrganizerCheckInsTransformer
 import de.rki.coronawarnapp.presencetracing.checkins.qrcode.TraceLocation
 import de.rki.coronawarnapp.presencetracing.organizer.submission.server.OrganizerSubmissionServer
 import de.rki.coronawarnapp.util.coroutine.AppScope
@@ -13,7 +15,7 @@ import javax.inject.Inject
 @Reusable
 class OrganizerSubmissionRepository @Inject constructor(
     @AppScope private val appScope: CoroutineScope,
-    private val checkInsTransformer: CheckInsTransformer,
+    private val organizerCheckInsTransformer: OrganizerCheckInsTransformer,
     private val verificationServer: VerificationServer,
     private val organizerSubmissionServer: OrganizerSubmissionServer,
 ) {
@@ -23,7 +25,33 @@ class OrganizerSubmissionRepository @Inject constructor(
      * @throws OrganizerSubmissionException in case of failure
      */
     suspend fun submit(payload: OrganizerSubmissionPayload) {
-        // TODO
+        organizerCheckInsTransformer.transform(listOf(payload.toCheckIns()))
+    }
+}
+
+fun OrganizerSubmissionPayload.toCheckIns(): CheckIn {
+    val payload = this
+
+    return with(traceLocation) {
+        CheckIn(
+            id = id,
+            traceLocationId = locationId,
+            version = version,
+            type = traceLocation.type.number,
+            description = description,
+            address = address,
+            traceLocationStart = startDate,
+            traceLocationEnd = endDate,
+            defaultCheckInLengthInMinutes = defaultCheckInLengthInMinutes,
+            cryptographicSeed = cryptographicSeed,
+            cnPublicKey = cnPublicKey,
+            checkInStart = payload.startDate,
+            checkInEnd = payload.endDate,
+            completed = true,
+            createJournalEntry = false,
+            isSubmitted = false,
+            hasSubmissionConsent = true
+        )
     }
 }
 
