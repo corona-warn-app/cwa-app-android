48949fdcc7204bdf3b20ae122c07cae6ba5fa88e
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-08-18 09:54:59 +0200

    Add new error code and some comments

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionException.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/OrganizerSubmissionException.kt
@@ -6,8 +6,16 @@ class OrganizerSubmissionException(
     override val cause: Throwable? = null
 ) : Exception(cause) {
     enum class ErrorCode {
+        /** If the submission fails with HTTP status code `403` */
+        SUBMISSION_OB_TAN_ERROR,
+
+        /** If the submission fails with an HTTP status code `40x` */
         SUBMISSION_OB_CLIENT_ERROR,
+
+        /** If the submission fails with an HTTP status code `50x` */
         SUBMISSION_OB_SERVER_ERROR,
+
+        /** If the submission fails due to missing or poor network connection */
         SUBMISSION_OB_NO_NETWORK,
     }
 }
