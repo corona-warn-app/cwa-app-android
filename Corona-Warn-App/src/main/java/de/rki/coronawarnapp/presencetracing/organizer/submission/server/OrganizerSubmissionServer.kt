ed6db42777484dba487969df2f9e5cc6cdca56e7
BMItr <Berndus@gmx.de>
2021-08-19 21:44:10 +0200

    Transform exception to organizer submission exceptions

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/server/OrganizerSubmissionServer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/organizer/submission/server/OrganizerSubmissionServer.kt
@@ -4,12 +4,7 @@ import com.google.protobuf.ByteString
 import dagger.Lazy
 import dagger.Reusable
 import de.rki.coronawarnapp.appconfig.AppConfigProvider
-import de.rki.coronawarnapp.exception.http.CwaClientError
-import de.rki.coronawarnapp.exception.http.CwaServerError
-import de.rki.coronawarnapp.exception.http.CwaUnknownHostException
-import de.rki.coronawarnapp.exception.http.NetworkConnectTimeoutException
 import de.rki.coronawarnapp.presencetracing.checkins.CheckInsReport
-import de.rki.coronawarnapp.presencetracing.organizer.submission.OrganizerSubmissionException
 import de.rki.coronawarnapp.server.protocols.internal.SubmissionPayloadOuterClass.SubmissionPayload
 import de.rki.coronawarnapp.util.PaddingTool
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
@@ -53,23 +48,7 @@ class OrganizerSubmissionServer @Inject constructor(
             .setSubmissionType(SubmissionPayload.SubmissionType.SUBMISSION_TYPE_HOST_WARNING)
             .build()
 
-        try {
-            organizerSubmissionApiV1.submitCheckInsOnBehalf(uploadTAN, payload)
-        } catch (e: Exception) {
-            Timber.tag(TAG).e("Submission of %s failed", checkInsReport)
-            throw when (e) {
-                is CwaUnknownHostException, is NetworkConnectTimeoutException -> OrganizerSubmissionException.ErrorCode.SUBMISSION_OB_NO_NETWORK
-                is CwaClientError -> OrganizerSubmissionException.ErrorCode.SUBMISSION_OB_CLIENT_ERROR // HTTP status code 4XX
-                is CwaServerError -> OrganizerSubmissionException.ErrorCode.SUBMISSION_OB_SERVER_ERROR // HTTP status code 5XX
-                else -> {
-                    Timber.tag(TAG).d(
-                        "Mapping unknown exception %s to SUBMISSION_OB_SERVER_ERROR",
-                        e::class.java.simpleName
-                    )
-                    OrganizerSubmissionException.ErrorCode.SUBMISSION_OB_SERVER_ERROR
-                }
-            }.let { OrganizerSubmissionException(it, e) }
-        }
+        organizerSubmissionApiV1.submitCheckInsOnBehalf(uploadTAN, payload)
     }
 
     companion object {
