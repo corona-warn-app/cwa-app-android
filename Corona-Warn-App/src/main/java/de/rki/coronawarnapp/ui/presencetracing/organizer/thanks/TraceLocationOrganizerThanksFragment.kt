69ad9f8ecf715ce1cd48ba76199d353f6112a086
BMItr <Berndus@gmx.de>
2021-08-20 11:57:45 +0200

    OnBehalfWarning - Thank You Screen (EXPOSUREAPP-8950) (#3955)
    * added res
    
    * - Created thanks fragment - Created layout
    
    * Finished thank you screen
    
    * maxLine

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/presencetracing/organizer/thanks/TraceLocationOrganizerThanksFragment.kt
@@ -0,0 +1,31 @@
+package de.rki.coronawarnapp.ui.presencetracing.organizer.thanks
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.TraceLocationOrganizerThanksFragmentBinding
+import de.rki.coronawarnapp.util.ui.doNavigate
+import de.rki.coronawarnapp.util.ui.viewBinding
+import timber.log.Timber
+
+class TraceLocationOrganizerThanksFragment : Fragment(R.layout.trace_location_organizer_thanks_fragment) {
+
+    private val binding: TraceLocationOrganizerThanksFragmentBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            toolbar.setNavigationOnClickListener { navigateToHome() }
+            finishButton.setOnClickListener { navigateToHome() }
+        }
+    }
+
+    private fun navigateToHome() {
+        Timber.d("navigateToHome()")
+        doNavigate(
+            TraceLocationOrganizerThanksFragmentDirections.actionTraceLocationOrganizerThanksFragmentToHomeFragment()
+        )
+    }
+}
