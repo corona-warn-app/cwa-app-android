eb4a90eaa587fb86801280aaef2a7f49789f3a56
SamuraiKek <alex.paulescu@gmail.com>
2021-08-31 15:10:04 +0300

    Priority is determined only by latest date.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/vaccination/core/VaccinatedPerson.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/vaccination/core/VaccinatedPerson.kt
@@ -68,9 +68,7 @@ data class VaccinatedPerson(
         return IMMUNITY_WAITING_DAYS - Days.daysBetween(newestFullDose.vaccinatedOn, today).days
     }
 
-    private fun getNewestFullDose(): VaccinationCertificate? = vaccinationCertificates
-        .filter { it.doseNumber == it.totalSeriesOfDoses }
-        .maxByOrNull { it.vaccinatedOn }
+    private fun getNewestFullDose(): VaccinationCertificate? = vaccinationCertificates.maxByOrNull { it.vaccinatedOn }
 
     private fun isFirstVaccinationDoseAfterRecovery(): Boolean {
         val vaccinationDetails = getNewestFullDose()?.rawCertificate?.vaccination
