e6d7a115d8b821233bd25b1eb4654e86c8724a07
BerndMitter <Berndus@gmx.de>
2021-09-02 17:50:04 +0200

    Added booster check worker

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/booster/BoosterCheckWorker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/booster/BoosterCheckWorker.kt
@@ -1,5 +1,30 @@
 package de.rki.coronawarnapp.covidcertificate.booster
 
-class BoosterCheckWorker {
-    // TODO
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.util.worker.InjectedWorkerFactory
+import timber.log.Timber
+
+class BoosterCheckWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
+    private val boosterNotificationService: BoosterNotificationService
+) : CoroutineWorker(context, workerParameters) {
+
+    override suspend fun doWork(): Result = try {
+        Timber.d("Starting booster notification check")
+        boosterNotificationService.checkBoosterNotification()
+
+        Result.success()
+    } catch (e: Exception) {
+        Timber.e(e, "Booster notification check failed")
+        Result.retry()
+    }
+
+    @AssistedFactory
+    interface Factory : InjectedWorkerFactory<BoosterCheckWorker>
 }
