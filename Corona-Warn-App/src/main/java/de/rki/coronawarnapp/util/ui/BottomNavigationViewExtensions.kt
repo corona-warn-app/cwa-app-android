feb0edd02b6e813fcf33e3467b0467d84086fe59
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-09-07 15:55:32 +0200

    Unify badge colour (#4033)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ui/BottomNavigationViewExtensions.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/ui/BottomNavigationViewExtensions.kt
@@ -2,13 +2,16 @@ package de.rki.coronawarnapp.util.ui
 
 import androidx.annotation.IdRes
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.util.ContextExtensions.getColorCompat
 
 fun BottomNavigationView.updateCountBadge(@IdRes badgeId: Int, count: Int) {
     if (count > 0) {
-        val badge = getOrCreateBadge(badgeId)
-        badge.number = count
-        badge.badgeTextColor = context.getColorCompat(android.R.color.white)
+        getOrCreateBadge(badgeId).apply {
+            number = count
+            backgroundColor = context.getColorCompat(R.color.badgeColor)
+            badgeTextColor = context.getColorCompat(android.R.color.white)
+        }
     } else {
         removeBadge(badgeId)
     }
