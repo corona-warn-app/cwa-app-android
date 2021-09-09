75255ebd2ede921bb1adead538e27cd557998d0e
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-09-09 14:25:29 +0200

    Update LauncherActivityViewModelTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/launcher/LauncherActivityViewModelTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/launcher/LauncherActivityViewModelTest.kt
@@ -1,9 +1,14 @@
 package de.rki.coronawarnapp.ui.launcher
 
+import android.app.Activity
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import de.rki.coronawarnapp.environment.BuildConfigWrap
 import de.rki.coronawarnapp.main.CWASettings
 import de.rki.coronawarnapp.storage.OnboardingSettings
-import de.rki.coronawarnapp.update.UpdateChecker
+import de.rki.coronawarnapp.update.getUpdateInfo
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.instanceOf
 import io.mockk.MockKAnnotations
@@ -12,6 +17,8 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.mockkObject
+import io.mockk.mockkStatic
+import io.mockk.verify
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -24,39 +31,93 @@ import testhelpers.preferences.mockFlowPreference
 @ExtendWith(InstantExecutorExtension::class)
 class LauncherActivityViewModelTest : BaseTest() {
 
-    @MockK lateinit var updateChecker: UpdateChecker
+    @MockK lateinit var appUpdateManager: AppUpdateManager
     @MockK lateinit var cwaSettings: CWASettings
     @MockK lateinit var onboardingSettings: OnboardingSettings
 
     @BeforeEach
     fun setupFreshViewModel() {
         MockKAnnotations.init(this)
+        mockkStatic("de.rki.coronawarnapp.update.InAppUpdateKt")
 
         every { onboardingSettings.isOnboarded } returns false
 
         mockkObject(BuildConfigWrap)
         every { BuildConfigWrap.VERSION_CODE } returns 10L
 
-        coEvery { updateChecker.checkForUpdate() } returns UpdateChecker.Result(isUpdateNeeded = false)
+        coEvery { appUpdateManager.getUpdateInfo() } returns
+            mockk<AppUpdateInfo>().apply {
+                every { updateAvailability() } returns UpdateAvailability.UPDATE_NOT_AVAILABLE
+            }
+
+        every {
+            appUpdateManager.startUpdateFlowForResult(
+                any(),
+                AppUpdateType.IMMEDIATE,
+                any<Activity>(),
+                any()
+            )
+        } returns true
     }
 
     private fun createViewModel() = LauncherActivityViewModel(
-        updateChecker = updateChecker,
+        appUpdateManager = appUpdateManager,
         dispatcherProvider = TestDispatcherProvider(),
         cwaSettings = cwaSettings,
         onboardingSettings = onboardingSettings
     )
 
     @Test
-    fun `update is available`() = runBlockingTest {
-        coEvery { updateChecker.checkForUpdate() } returns UpdateChecker.Result(
-            isUpdateNeeded = true,
-            updateIntent = { mockk() }
-        )
-
+    fun `update is available and checked in init`() = runBlockingTest {
+        coEvery { appUpdateManager.getUpdateInfo() } returns
+            mockk<AppUpdateInfo>().apply {
+                every { updateAvailability() } returns UpdateAvailability.UPDATE_AVAILABLE
+            }
         val vm = createViewModel()
 
         vm.events.value shouldBe instanceOf(LauncherEvent.ForceUpdate::class)
+    }
+
+    @Test
+    fun `Force update event triggers update`() {
+        coEvery { appUpdateManager.getUpdateInfo() } returns
+            mockk<AppUpdateInfo>().apply {
+                every { updateAvailability() } returns UpdateAvailability.UPDATE_AVAILABLE
+            }
+        val vm = createViewModel()
+
+        (vm.events.value as LauncherEvent.ForceUpdate).apply {
+            forceUpdate(mockk())
+            verify {
+                appUpdateManager.startUpdateFlowForResult(
+                    any(),
+                    AppUpdateType.IMMEDIATE,
+                    any<Activity>(),
+                    any()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `On resume update is resumed`() {
+        coEvery { appUpdateManager.getUpdateInfo() } returns
+            mockk<AppUpdateInfo>().apply {
+                every { updateAvailability() } returns UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+            }
+        val vm = createViewModel()
+        vm.onResume()
+        (vm.events.value as LauncherEvent.ForceUpdate).apply {
+            forceUpdate(mockk())
+            verify {
+                appUpdateManager.startUpdateFlowForResult(
+                    any(),
+                    AppUpdateType.IMMEDIATE,
+                    any<Activity>(),
+                    any()
+                )
+            }
+        }
     }
 
     @Test
