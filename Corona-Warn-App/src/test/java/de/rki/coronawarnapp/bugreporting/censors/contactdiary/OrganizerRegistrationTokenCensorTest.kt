319c244e0a5fda381c589a984c18e142d7b78dac
Juraj Kusnier <jurajkusnier@gmail.com>
2021-09-17 15:33:42 +0200

    fix detekt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensorTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
+@Suppress("MaxLineLength")
 class OrganizerRegistrationTokenCensorTest {
 
     private val testRegistrationTokens = listOf(
