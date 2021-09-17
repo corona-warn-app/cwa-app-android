4ead8f7f4a2a60dcd7b4fb515dff7990880e5c6e
BerndMitter <Berndus@gmx.de>
2021-09-17 19:23:55 +0200

    emptry frag for upcoming delta informations frag

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/covpass/CovPassInfoFragment.kt
@@ -0,0 +1,10 @@
+package de.rki.coronawarnapp.covidcertificate.covpass
+
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
+
+class CovPassInfoFragment : Fragment(R.layout.fragment_cov_pass_info) {
+
+    // TODO: Will be implemented in another PR!!
+
+}
