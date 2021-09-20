14e3b37a869a84f74651d68a9f89e9d4c120dce7
I502720 <axel.herbstreith@sap.com>
2021-09-20 18:16:32 +0200

    update image alpha

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/covpass/CovPassInfoFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/covpass/CovPassInfoFragment.kt
@@ -3,8 +3,11 @@ package de.rki.coronawarnapp.covidcertificate.covpass
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.transition.MaterialSharedAxis
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentCovPassInfoBinding
+import de.rki.coronawarnapp.ui.view.onOffsetChange
+import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import setTextWithUrl
 
@@ -12,15 +15,29 @@ class CovPassInfoFragment : Fragment(R.layout.fragment_cov_pass_info) {
 
     private val binding: FragmentCovPassInfoBinding by viewBinding()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+
+            toolbar.setNavigationOnClickListener { popBackStack() }
+
             linkToFaq.setTextWithUrl(
                 R.string.cov_pass_info_faq_link,
                 R.string.cov_pass_info_faq_link,
                 R.string.vaccination_card_booster_eligible_faq_link // TBD
             )
+
+            appBarLayout.onOffsetChange { _, subtitleAlpha ->
+                headerImage.alpha = subtitleAlpha
+            }
         }
     }
 }
