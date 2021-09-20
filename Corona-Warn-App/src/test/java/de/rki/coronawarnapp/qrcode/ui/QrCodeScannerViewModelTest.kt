eadc03252dd99ef7147aeb6a4702db2a87104b90
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-09-20 17:51:32 +0200

    Create QrCodeScannerViewModelTest.kt

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/qrcode/ui/QrCodeScannerViewModelTest.kt
@@ -0,0 +1,19 @@
+package de.rki.coronawarnapp.qrcode.ui
+
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+
+class QrCodeScannerViewModelTest : BaseTest() {
+
+    @Test
+    fun onImportFile() {
+    }
+
+    @Test
+    fun onScanResult() {
+    }
+
+    @Test
+    fun setCameraDeniedPermanently() {
+    }
+}
