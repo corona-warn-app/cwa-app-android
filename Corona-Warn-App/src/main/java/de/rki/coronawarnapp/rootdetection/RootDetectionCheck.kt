cd0c83bff011093421082260b92fbdd980510534
BerndMitter <Berndus@gmx.de>
2021-09-28 12:09:34 +0200

    Added root module for RootBeer creation

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/rootdetection/RootDetectionCheck.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/rootdetection/RootDetectionCheck.kt
@@ -1,18 +1,15 @@
 package de.rki.coronawarnapp.rootdetection
 
-import android.content.Context
 import com.scottyab.rootbeer.RootBeer
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
-import de.rki.coronawarnapp.util.di.AppContext
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
 class RootDetectionCheck @Inject constructor(
-    @AppContext context: Context,
+    private val rootBeer: RootBeer,
     private val dispatcherProvider: DispatcherProvider
 ) {
-    private val rootBeer = RootBeer(context)
 
     // Check should run in a background thread cause it uses I/O
     suspend fun checkRoot() = withContext(dispatcherProvider.IO) {
