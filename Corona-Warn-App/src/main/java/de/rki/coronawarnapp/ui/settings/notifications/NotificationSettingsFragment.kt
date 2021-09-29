d7af80182db47532d27e07e56f8f59d95d965028
Juraj Kusnier <jurajkusnier@gmail.com>
2021-09-29 19:26:41 +0200

    update notifications

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/notifications/NotificationSettingsFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/settings/notifications/NotificationSettingsFragment.kt
@@ -1,59 +1,66 @@
 package de.rki.coronawarnapp.ui.settings.notifications
 
+import TextViewUrlSet
 import android.os.Bundle
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentSettingsNotificationsBinding
 import de.rki.coronawarnapp.util.ExternalActionHelper.openAppNotificationSettings
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.observe2
-import de.rki.coronawarnapp.util.ui.popBackStack
+import de.rki.coronawarnapp.util.ui.toResolvingString
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import setTextWithUrls
 import javax.inject.Inject
 
 /**
  * This is the setting notification page. Here the user sees his os notifications settings status.
- * If os notifications are disabled he can navigate to them with one click. And if the os is enabled
- * the user can decide which notifications he wants to get: risk updates and/or test results.
+ * He can navigate to system notification settings and enable/disable them with one click.
  */
 class NotificationSettingsFragment :
     Fragment(R.layout.fragment_settings_notifications),
     AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
-    private val vm: NotificationSettingsFragmentViewModel by cwaViewModels { viewModelFactory }
+    private val viewModel: NotificationSettingsFragmentViewModel by cwaViewModels { viewModelFactory }
 
     private val binding: FragmentSettingsNotificationsBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vm.notificationSettingsState.observe2(this) {
-            binding.state = it
+        with(binding) {
+            viewModel.notificationSettingsState.observe2(this@NotificationSettingsFragment) {
+                informationDetailsHeaderIllustration.apply {
+                    setImageResource(it.getNotificationsImage())
+                    contentDescription = it.getNotificationsIllustrationText(requireContext())
+                }
+                notificationStateValue.setText(it.getNotificationStatusText())
+                notificationStateHeader.setText(it.getNotificationsHeader())
+                notificationEnabledViews.isGone = !it.isNotificationsEnabled
+                notificationDisabledViews.isGone = it.isNotificationsEnabled
+            }
+
+            openSystemSettingsButton.setOnClickListener {
+                requireContext().openAppNotificationSettings()
+            }
+
+            faq.setTextWithUrls(
+                R.string.nm_faq.toResolvingString(),
+                TextViewUrlSet(
+                    labelResource = R.string.nm_faq_label,
+                    urlResource = R.string.nm_faq_link
+                )
+            )
         }
-        setButtonOnClickListener()
     }
 
     override fun onResume() {
         super.onResume()
         binding.settingsNotificationsContainer.sendAccessibilityEvent(AccessibilityEvent.TYPE_ANNOUNCEMENT)
-    }
-
-    private fun setButtonOnClickListener() {
-        binding.settingsSwitchRowNotificationsRisk.setOnClickListener {
-            vm.toggleNotificationsRiskEnabled()
-        }
-        binding.settingsSwitchRowNotificationsTest.setOnClickListener {
-            vm.toggleNotificationsTestEnabled()
-        }
-        binding.settingsNotificationsHeader.setNavigationOnClickListener {
-            popBackStack()
-        }
-        binding.settingsNotificationsCard.tracingStatusCardButton.setOnClickListener {
-            requireContext().openAppNotificationSettings()
-        }
     }
 }
