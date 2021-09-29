3be3e02f8a04b2fd7cd99209c1034b3f10d99d5f
SamuraiKek <alex.paulescu@gmail.com>
2021-09-30 00:01:33 +0300

    Adapted data source.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/StatsItem.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/StatsItem.kt
@@ -13,13 +13,13 @@ data class StatisticsData(
 
     override fun toString(): String {
         return "StatisticsData(cards=${
-        items.map {
-            when (it) {
-                is AddStatsItem -> "AddCard(${it.isEnabled})"
-                is GlobalStatsItem -> it.cardType.name + " " + it.updatedAt
-                is LocalStatsItem -> it.cardType.name + " " + it.updatedAt
+            items.map {
+                when (it) {
+                    is AddStatsItem -> "AddCard(${it.isEnabled})"
+                    is GlobalStatsItem -> it.cardType.name + " " + it.updatedAt
+                    is LocalStatsItem -> it.cardType.name + " " + it.updatedAt
+                }
             }
-        }
         })"
     }
 }
@@ -31,9 +31,9 @@ data class LocalStatisticsData(
 
     override fun toString(): String {
         return "StatisticsData(cards=${
-        items.map {
-            it.cardType.name + " " + it.updatedAt
-        }
+            items.map {
+                it.cardType.name + " " + it.updatedAt
+            }
         })"
     }
 }
@@ -58,7 +58,9 @@ sealed class GlobalStatsItem(val cardType: Type) : GenericStatsItem() {
         SEVEN_DAY_RVALUE(4),
         PERSONS_VACCINATED_ONCE(5),
         PERSONS_VACCINATED_COMPLETELY(6),
-        APPLIED_VACCINATION_RATES(7)
+        APPLIED_VACCINATION_RATES(7),
+        SEVEN_DAY_HOSPITALIZATION(8),
+        OCCUPIED_INTENSIVE_CARE_BEDS(9)
     }
 
     abstract fun requireValidity()
@@ -248,6 +250,37 @@ data class AppliedVaccinationRatesStats(
         }
         requireNotNull(keyFigures.singleOrNull { it.rank == KeyFigure.Rank.TERTIARY }) {
             Timber.w("AppliedVaccinationRatesStats is missing tertiary value")
+        }
+    }
+}
+
+data class SevenDayHospitalizationStats(
+    override val updatedAt: Instant,
+    override val keyFigures: List<KeyFigure>
+) : GlobalStatsItem(cardType = Type.SEVEN_DAY_HOSPITALIZATION) {
+
+    val sevenDayValue: KeyFigure
+        get() = keyFigures.single { it.rank == KeyFigure.Rank.PRIMARY }
+
+    override fun requireValidity() {
+        require(keyFigures.size == 1)
+        requireNotNull(keyFigures.singleOrNull { it.rank == KeyFigure.Rank.PRIMARY }) {
+            Timber.w("SevenDayHospitalizationStats is missing primary value")
+        }
+    }
+}
+
+data class OccupiedIntensiveCareStats(
+    override val updatedAt: Instant,
+    override val keyFigures: List<KeyFigure>
+) : GlobalStatsItem(cardType = Type.OCCUPIED_INTENSIVE_CARE_BEDS) {
+    val occupationRatio: KeyFigure
+        get() = keyFigures.single { it.rank == KeyFigure.Rank.PRIMARY }
+
+    override fun requireValidity() {
+        require(keyFigures.size == 1)
+        requireNotNull(keyFigures.singleOrNull { it.rank == KeyFigure.Rank.PRIMARY }) {
+            Timber.w("OccupiedIntensiveCareStats is missing primary value")
         }
     }
 }
