6c03073ad79b6190e9098ab6ad178ba997a112fd
BerndMitter <Berndus@gmx.de>
2021-09-29 15:11:00 +0200

    clean

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/rootdetection/RootDetectionModuleTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/rootdetection/RootDetectionModuleTest.kt
@@ -4,7 +4,7 @@ import io.kotest.assertions.throwables.shouldNotThrowAny
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
 
-class RootDetectionModuleTest: BaseTest() {
+class RootDetectionModuleTest : BaseTest() {
 
     @Test
     fun `instantiation without error`() {
