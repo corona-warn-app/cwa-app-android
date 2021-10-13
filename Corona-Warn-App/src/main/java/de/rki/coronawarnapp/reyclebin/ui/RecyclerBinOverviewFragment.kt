7277989b2f4c8bcf5762c7ee05a5d943628f981d
Juraj Kusnier <jurajkusnier@gmail.com>
2021-10-13 20:30:03 +0200

    RecyclerBinAdapter with OverviewSubHeaderItem

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/reyclebin/ui/RecyclerBinOverviewFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/reyclebin/ui/RecyclerBinOverviewFragment.kt
@@ -5,13 +5,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.RecyclerBinOverviewFragmentBinding
+import de.rki.coronawarnapp.reyclebin.ui.adapter.OverviewSubHeaderItem
+import de.rki.coronawarnapp.reyclebin.ui.adapter.RecyclerBinAdapter
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.lists.diffutil.update
+import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
 import javax.inject.Inject
 
-class RecyclerBinOverviewFragment : Fragment(R.layout.recycler_bin_overview_fragment) {
+class RecyclerBinOverviewFragment : Fragment(R.layout.recycler_bin_overview_fragment), AutoInject {
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
     private val viewModel: RecyclerBinOverviewViewModel by cwaViewModels { viewModelFactory }
@@ -20,8 +25,15 @@ class RecyclerBinOverviewFragment : Fragment(R.layout.recycler_bin_overview_frag
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val adapter = RecyclerBinAdapter()
+
         with(binding) {
             toolbar.setNavigationOnClickListener { popBackStack() }
+            recyclerBinList.adapter = adapter
+        }
+
+        viewModel.recycledCertificates.observe2(this) {
+            adapter.update(listOf(OverviewSubHeaderItem))
         }
     }
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/reyclebin/ui/RecyclerBinOverviewViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/reyclebin/ui/RecyclerBinOverviewViewModel.kt
@@ -2,11 +2,15 @@ package de.rki.coronawarnapp.reyclebin.ui
 
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.reyclebin.RecycledItemsProvider
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 
-class RecyclerBinOverviewViewModel @AssistedInject constructor() : CWAViewModel() {
-    // TODO: Implement the ViewModel
+class RecyclerBinOverviewViewModel @AssistedInject constructor(
+    private val recycledItemsProvider: RecycledItemsProvider
+) : CWAViewModel() {
+
+    val recycledCertificates = recycledItemsProvider.recycledCertificates.asLiveData2()
 
     @AssistedFactory
     interface Factory : SimpleCWAViewModelFactory<RecyclerBinOverviewViewModel>
