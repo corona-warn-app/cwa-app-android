6f4cc6aca2232dd2366d30c9882759c071ca7dac
BerndMitter <Berndus@gmx.de>
2021-10-15 12:54:54 +0200

    Implemented test for retentionDaysInRecycleBin

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/recyclebin/common/RecyclableExtensionsTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/recyclebin/common/RecyclableExtensionsTest.kt
@@ -1,6 +1,38 @@
 package de.rki.coronawarnapp.recyclebin.common
 
+import de.rki.coronawarnapp.reyclebin.common.Recyclable
+import de.rki.coronawarnapp.reyclebin.common.retentionDaysInRecycleBin
+import io.kotest.matchers.shouldBe
+import org.joda.time.Instant
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
 
-class RecyclableExtensionsTest: BaseTest() {
+class RecyclableExtensionsTest : BaseTest() {
+
+    private val now = Instant.parse("2021-10-13T12:00:00.000Z")
+
+    private val recyclable = object : Recyclable {
+        override var recycledAt: Instant? = null
+    }
+
+    @BeforeEach
+    fun setup() {
+        recyclable.recycledAt = null
+    }
+
+    @Test
+    fun `Not recycled returns 0`() {
+        recyclable.recycledAt shouldBe null
+        recyclable.retentionDaysInRecycleBin(now) shouldBe 0
+    }
+
+    @Test
+    fun `Days in retention calculation with ms precision`() {
+        recyclable.recycledAt = Instant.parse("2021-10-11T12:00:00.001Z")
+        recyclable.retentionDaysInRecycleBin(now) shouldBe 1
+
+        recyclable.recycledAt = Instant.parse("2021-10-11T12:00:00.000Z")
+        recyclable.retentionDaysInRecycleBin(now) shouldBe 2
+    }
 }
