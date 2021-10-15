a45b0dc538d6d1a8fe469073d3ee689fdceebcdd
BerndMitter <Berndus@gmx.de>
2021-10-15 12:47:42 +0200

    Schedule clean up when app comes into foreground

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/reyclebin/cleanup/RecycleBinCleanUpScheduler.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/reyclebin/cleanup/RecycleBinCleanUpScheduler.kt
@@ -3,6 +3,10 @@ package de.rki.coronawarnapp.reyclebin.cleanup
 import de.rki.coronawarnapp.util.coroutine.AppScope
 import de.rki.coronawarnapp.util.device.ForegroundState
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -16,5 +20,14 @@ class RecycleBinCleanUpScheduler @Inject constructor(
 
     fun setup() {
         Timber.d("setup()")
+
+        foregroundState.isInForeground
+            .distinctUntilChanged()
+            .filter { it }
+            .onEach {
+                Timber.d("Thread: ${Thread.currentThread().name}")
+                recycleBinCleanUpService.clearRecycledCertificates()
+            }
+            .launchIn(appScope)
     }
 }
