2d51c722cabb24cfb9b1b3a2df04b7806795a6d4
Juraj Kusnier <jurajkusnier@gmail.com>
2021-10-18 12:53:58 +0200

    update OrganizerRegistrationTokenCensor

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensor.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensor.kt
@@ -2,36 +2,51 @@ package de.rki.coronawarnapp.bugreporting.censors.contactdiary
 
 import dagger.Reusable
 import de.rki.coronawarnapp.bugreporting.censors.BugCensor
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import de.rki.coronawarnapp.bugreporting.censors.BugCensor.CensorContainer
+import de.rki.coronawarnapp.coronatest.server.RegistrationRequest
 import javax.inject.Inject
 
 @Reusable
 class OrganizerRegistrationTokenCensor @Inject constructor() : BugCensor {
 
-    override suspend fun checkLog(message: String): BugCensor.CensorContainer? = mutex.withLock {
-        if (registrationToken.isEmpty()) return null
+    override suspend fun checkLog(message: String): CensorContainer? {
+        var container = CensorContainer(message)
 
-        var container = BugCensor.CensorContainer(message)
+        synchronized(tan) {
+            tan.forEach {
+                container = container.censor(it, PLACEHOLDER + it.takeLast(3))
+            }
+        }
 
-        registrationToken.forEach {
-            container = container.censor(it, PLACEHOLDER + it.takeLast(3))
+        synchronized(registrationRequestToCensor) {
+            registrationRequestToCensor.forEach {
+                container = container.censor(it.key, REQUEST_PLACEHOLDER + it.key.takeLast(4))
+            }
         }
 
         return container.nullIfEmpty()
     }
 
     companion object {
-        private val mutex = Mutex()
-        private val registrationToken = mutableSetOf<String>()
-        suspend fun addRegistrationToken(tan: String) = mutex.withLock {
-            registrationToken.add(tan)
+        private val tan = mutableSetOf<String>()
+        fun addTan(tan: String) = synchronized(tan) {
+            this.tan.add(tan)
         }
 
-        suspend fun clearRegistrationTokens() = mutex.withLock {
-            registrationToken.clear()
+        fun clearTan() = synchronized(tan) {
+            tan.clear()
+        }
+
+        private val registrationRequestToCensor = mutableSetOf<RegistrationRequest>()
+        fun addRegistrationRequestToCensor(item: RegistrationRequest) = synchronized(registrationRequestToCensor) {
+            registrationRequestToCensor.add(item)
+        }
+
+        fun clearRegistrationRequests() = synchronized(registrationRequestToCensor) {
+            registrationRequestToCensor.clear()
         }
 
         private const val PLACEHOLDER = "########-####-####-####-########"
+        private const val REQUEST_PLACEHOLDER = "###-###-"
     }
 }
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensorTest.kt
@@ -1,5 +1,7 @@
 package de.rki.coronawarnapp.bugreporting.censors.contactdiary
 
+import de.rki.coronawarnapp.coronatest.server.RegistrationRequest
+import de.rki.coronawarnapp.coronatest.server.VerificationKeyType
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import kotlinx.coroutines.runBlocking
@@ -11,9 +13,14 @@ import org.junit.jupiter.api.Test
 @Suppress("MaxLineLength")
 class OrganizerRegistrationTokenCensorTest {
 
-    private val testRegistrationTokens = listOf(
+    private val tans = listOf(
         "63b4d3ff-e0de-4bd4-90c1-17c2bb683a2f",
         "13b4d3ff-e0de-4bd4-90c1-17c2bb683a2x",
+    )
+
+    private val testRegistrationRequests = listOf(
+        RegistrationRequest("1234567890",VerificationKeyType.TELETAN),
+        RegistrationRequest("9876543210",VerificationKeyType.TELETAN),
     )
 
     @BeforeEach
@@ -23,20 +30,35 @@ class OrganizerRegistrationTokenCensorTest {
 
     @AfterEach
     fun teardown() {
-        runBlocking { OrganizerRegistrationTokenCensor.clearRegistrationTokens() }
+        runBlocking {
+            OrganizerRegistrationTokenCensor.clearTan()
+            OrganizerRegistrationTokenCensor.clearRegistrationRequests()
+        }
     }
 
     private fun createInstance() = OrganizerRegistrationTokenCensor()
 
     @Test
-    fun `censoring replaces the logline message`() = runBlockingTest {
+    fun `censoring replaces the logline message for tans`() = runBlockingTest {
         val instance = createInstance()
 
-        testRegistrationTokens.forEach {
-            OrganizerRegistrationTokenCensor.addRegistrationToken(it)
-            val toCensor = "I'm a shy registrationToken: $it"
+        tans.forEach {
+            OrganizerRegistrationTokenCensor.addTan(it)
+            val toCensor = "I'm a shy TAN: $it"
             instance.checkLog(toCensor)!!
-                .compile()!!.censored shouldBe "I'm a shy registrationToken: ########-####-####-####-########${it.takeLast(3)}"
+                .compile()!!.censored shouldBe "I'm a shy TAN: ########-####-####-####-########${it.takeLast(3)}"
+        }
+    }
+
+    @Test
+    fun `censoring replaces the logline message for registration requests`() = runBlockingTest {
+        val instance = createInstance()
+
+        testRegistrationRequests.forEach {
+            OrganizerRegistrationTokenCensor.addRegistrationRequestToCensor(it)
+            val toCensor = "I'm a shy registration request key: ${it.key}"
+            instance.checkLog(toCensor)!!
+                .compile()!!.censored shouldBe "I'm a shy registration request key: ###-###-${it.key.takeLast(4)}"
         }
     }
 
@@ -44,9 +66,15 @@ class OrganizerRegistrationTokenCensorTest {
     fun `censoring aborts if no qrcode was set`() = runBlockingTest {
         val instance = createInstance()
 
-        testRegistrationTokens.forEach {
+        testRegistrationRequests.forEach {
+            val toCensor = "I'm a shy registrationToken: $it"
+            instance.checkLog(toCensor) shouldBe null
+        }
+
+        tans.forEach {
             val toCensor = "I'm a shy registrationToken: $it"
             instance.checkLog(toCensor) shouldBe null
         }
     }
+
 }
