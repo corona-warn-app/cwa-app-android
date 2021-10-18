513e174886b0471835a416ba86264a0c83ee08dc
Juraj Kusnier <jurajkusnier@gmail.com>
2021-10-18 16:15:40 +0200

    using mutex instead of synchronized

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensor.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/bugreporting/censors/contactdiary/OrganizerRegistrationTokenCensor.kt
@@ -4,45 +4,45 @@ import dagger.Reusable
 import de.rki.coronawarnapp.bugreporting.censors.BugCensor
 import de.rki.coronawarnapp.bugreporting.censors.BugCensor.CensorContainer
 import de.rki.coronawarnapp.coronatest.server.RegistrationRequest
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 @Reusable
 class OrganizerRegistrationTokenCensor @Inject constructor() : BugCensor {
 
-    override suspend fun checkLog(message: String): CensorContainer? {
+    override suspend fun checkLog(message: String): CensorContainer? = mutex.withLock {
         var container = CensorContainer(message)
 
-        synchronized(tan) {
-            tan.forEach {
-                container = container.censor(it, PLACEHOLDER + it.takeLast(3))
-            }
+        tan.forEach {
+            container = container.censor(it, PLACEHOLDER + it.takeLast(3))
         }
 
-        synchronized(registrationRequestToCensor) {
-            registrationRequestToCensor.forEach {
-                container = container.censor(it.key, REQUEST_PLACEHOLDER + it.key.takeLast(4))
-            }
+        registrationRequestToCensor.forEach {
+            container = container.censor(it.key, REQUEST_PLACEHOLDER + it.key.takeLast(4))
         }
 
         return container.nullIfEmpty()
     }
 
     companion object {
+        private val mutex = Mutex()
+
         private val tan = mutableSetOf<String>()
-        fun addTan(tan: String) = synchronized(tan) {
+        suspend fun addTan(tan: String) = mutex.withLock {
             this.tan.add(tan)
         }
 
-        fun clearTan() = synchronized(tan) {
+        suspend fun clearTan() = mutex.withLock {
             tan.clear()
         }
 
         private val registrationRequestToCensor = mutableSetOf<RegistrationRequest>()
-        fun addRegistrationRequestToCensor(item: RegistrationRequest) = synchronized(registrationRequestToCensor) {
+        suspend fun addRegistrationRequestToCensor(item: RegistrationRequest) = mutex.withLock {
             registrationRequestToCensor.add(item)
         }
 
-        fun clearRegistrationRequests() = synchronized(registrationRequestToCensor) {
+        suspend fun clearRegistrationRequests() = mutex.withLock {
             registrationRequestToCensor.clear()
         }
 
