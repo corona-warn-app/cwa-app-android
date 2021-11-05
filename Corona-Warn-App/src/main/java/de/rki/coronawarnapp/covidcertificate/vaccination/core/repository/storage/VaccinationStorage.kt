4ef02dce9a3ce3d0196cdf8a4fd24e57c0033ecb
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-05 16:36:27 +0100

    Group on load

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/vaccination/core/repository/storage/VaccinationStorage.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/vaccination/core/repository/storage/VaccinationStorage.kt
@@ -45,7 +45,7 @@ class VaccinationStorage @Inject constructor(
                 requireNotNull(personData.identifier)
             }
         }
-        return persons.toSet()
+        return persons.toSet().groupDataByIdentifier()
     }
 
     suspend fun save(persons: Set<VaccinatedPersonData>) = mutex.withLock {
@@ -56,7 +56,7 @@ class VaccinationStorage @Inject constructor(
                 Timber.tag(TAG).v("Removing data for %s", it)
                 remove(it)
             }
-            persons.groupDataByIdentifier().forEach {
+            persons.forEach {
                 if (it.vaccinations.isNotEmpty()) {
                     val raw = gson.toJson(it)
                     val identifier = it.identifier
