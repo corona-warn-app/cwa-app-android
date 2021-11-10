b5f4d3144fd51301c5a9a004263ed7cd947668a1
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-11-10 15:57:12 +0100

    Json and extractor (EXPOSUREAPP-10477) (#4355)
    * json and extractor
    
    * clean up
    
    * unit tests
    
    * detekt
    
    * adjust logic
    
    * review comments

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeExtractor.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeExtractor.kt
@@ -1,16 +1,41 @@
 package de.rki.coronawarnapp.dccticketing.core.qrcode
 
+import com.google.gson.Gson
+import de.rki.coronawarnapp.dccticketing.core.qrcode.DccTicketingInvalidQrCodeException.ErrorCode
 import de.rki.coronawarnapp.qrcode.scanner.QrCodeExtractor
+import de.rki.coronawarnapp.util.serialization.BaseGson
+import de.rki.coronawarnapp.util.serialization.fromJson
 import javax.inject.Inject
 
-class DccTicketingQrCodeExtractor @Inject constructor() : QrCodeExtractor<DccTicketingQrCode> {
+class DccTicketingQrCodeExtractor @Inject constructor(
+    @BaseGson private val gson: Gson
+) : QrCodeExtractor<DccTicketingQrCode> {
     override suspend fun canHandle(rawString: String): Boolean {
-        // TODO
-        return false
+        return rawString.startsWith(PREFIX)
     }
 
     override suspend fun extract(rawString: String): DccTicketingQrCode {
-        // TODO
-        return object : DccTicketingQrCode {}
+        return DccTicketingQrCode(
+            qrCode = rawString,
+            data = rawString.parse().validate()
+        )
+    }
+
+    private fun String.parse(): DccTicketingQrCodeData {
+        return try {
+            gson.fromJson(this)
+        } catch (e: Exception) {
+            throw DccTicketingInvalidQrCodeException(ErrorCode.INIT_DATA_PARSE_ERR)
+        }
+    }
+
+    private fun DccTicketingQrCodeData.validate(): DccTicketingQrCodeData {
+        if (protocol != PROTOCOL) throw DccTicketingInvalidQrCodeException(ErrorCode.INIT_DATA_PROTOCOL_INVALID)
+        if (subject.isNullOrBlank()) throw DccTicketingInvalidQrCodeException(ErrorCode.INIT_DATA_SUBJECT_EMPTY)
+        if (serviceProvider.isNullOrBlank()) throw DccTicketingInvalidQrCodeException(ErrorCode.INIT_DATA_SP_EMPTY)
+        return this
     }
 }
+
+private const val PREFIX = "{"
+private const val PROTOCOL = "DCCVALIDATION"
---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeExtractorTest.kt
@@ -0,0 +1,117 @@
+package de.rki.coronawarnapp.dccticketing.core.qrcode
+
+import de.rki.coronawarnapp.covidcertificate.DaggerCovidCertificateTestComponent
+import de.rki.coronawarnapp.dccticketing.core.qrcode.DccTicketingInvalidQrCodeException.ErrorCode.INIT_DATA_PARSE_ERR
+import de.rki.coronawarnapp.dccticketing.core.qrcode.DccTicketingInvalidQrCodeException.ErrorCode.INIT_DATA_PROTOCOL_INVALID
+import de.rki.coronawarnapp.dccticketing.core.qrcode.DccTicketingInvalidQrCodeException.ErrorCode.INIT_DATA_SP_EMPTY
+import de.rki.coronawarnapp.dccticketing.core.qrcode.DccTicketingInvalidQrCodeException.ErrorCode.INIT_DATA_SUBJECT_EMPTY
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+import javax.inject.Inject
+
+class DccTicketingQrCodeExtractorTest : BaseTest() {
+
+    @Inject lateinit var extractor: DccTicketingQrCodeExtractor
+
+    @BeforeEach
+    fun setup() {
+        DaggerCovidCertificateTestComponent.factory().create().inject(this)
+    }
+
+    @Test
+    fun `happy path extraction`() = runBlockingTest {
+        shouldNotThrowAny {
+            extractor.canHandle(validQrCode) shouldBe true
+            extractor.extract(validQrCode)
+        }
+    }
+
+    @Test
+    fun `missing service provider throws exception`() = runBlockingTest {
+        extractor.canHandle(invalidQrCodeMissingServiceProvider) shouldBe true
+        shouldThrow<DccTicketingInvalidQrCodeException> {
+            extractor.extract(invalidQrCodeMissingServiceProvider)
+        }.errorCode shouldBe INIT_DATA_SP_EMPTY
+    }
+
+    @Test
+    fun `wrong protocol throws exception`() = runBlockingTest {
+        extractor.canHandle(invalidQrCodeWrongProtocol) shouldBe true
+        shouldThrow<DccTicketingInvalidQrCodeException> {
+            extractor.extract(invalidQrCodeWrongProtocol)
+        }.errorCode shouldBe INIT_DATA_PROTOCOL_INVALID
+    }
+
+    @Test
+    fun `missing subject throws exception`() = runBlockingTest {
+        extractor.canHandle(invalidQrCodeMissingSubject) shouldBe true
+        shouldThrow<DccTicketingInvalidQrCodeException> {
+            extractor.extract(invalidQrCodeMissingSubject)
+        }.errorCode shouldBe INIT_DATA_SUBJECT_EMPTY
+    }
+
+    @Test
+    fun `malformed json throws exception`() = runBlockingTest {
+        extractor.canHandle(invalidJson) shouldBe true
+        shouldThrow<DccTicketingInvalidQrCodeException> {
+            extractor.extract(invalidJson)
+        }.errorCode shouldBe INIT_DATA_PARSE_ERR
+    }
+}
+
+@Suppress("MaxLineLength")
+private const val validQrCode = """{
+   "protocol": "DCCVALIDATION",
+   "protocolVersion": "1.0.0",
+   "serviceIdentity": "https://dgca-booking-demo-eu-test.cfapps.eu10.hana.ondemand.com/api/identity",
+   "privacyUrl": "https://validation-decorator.example",
+   "token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJiUzhEMi9XejV0WT0iLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RnY2EtYm9va2luZy1kZW1vLWV1LXRlc3QuY2ZhcHBzLmV1MTAuaGFuYS5vbmRlbWFuZC5jb20vYXBpL2lkZW50aXR5IiwiZXhwIjoxNjM1NDk2MzYwLCJzdWIiOiIwMDI0MWQxMS0yN2I0LTQxYWYtOWU3Ny0zNDE4YzNlY2NmZDQifQ.X0wUdET3omy3qXyOhBh1UuAUEvfYMCdapv0yVShynfZpc4yS3kH57TrPLgSqS7A9ZhbgIdCIfZwr0Chm1ELyTw",
+   "consent": "Please confirm to start the DCC Exchange flow. If you do not confirm, the flow is aborted.",
+   "subject": "00241d11-27b4-41af-9e77-3418c3eccfd4",
+   "serviceProvider": "Booking Demo"
+}"""
+
+@Suppress("MaxLineLength")
+private const val invalidQrCodeMissingServiceProvider = """{
+   "protocol": "DCCVALIDATION",
+   "protocolVersion": "1.0.0",
+   "serviceIdentity": "https://dgca-booking-demo-eu-test.cfapps.eu10.hana.ondemand.com/api/identity",
+   "privacyUrl": "https://validation-decorator.example",
+   "token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJiUzhEMi9XejV0WT0iLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RnY2EtYm9va2luZy1kZW1vLWV1LXRlc3QuY2ZhcHBzLmV1MTAuaGFuYS5vbmRlbWFuZC5jb20vYXBpL2lkZW50aXR5IiwiZXhwIjoxNjM1NDk2MzYwLCJzdWIiOiIwMDI0MWQxMS0yN2I0LTQxYWYtOWU3Ny0zNDE4YzNlY2NmZDQifQ.X0wUdET3omy3qXyOhBh1UuAUEvfYMCdapv0yVShynfZpc4yS3kH57TrPLgSqS7A9ZhbgIdCIfZwr0Chm1ELyTw",
+   "consent": "Please confirm to start the DCC Exchange flow. If you do not confirm, the flow is aborted.",
+   "subject": "00241d11-27b4-41af-9e77-3418c3eccfd4",
+   "serviceProvider": "   "
+}"""
+
+@Suppress("MaxLineLength")
+private const val invalidQrCodeWrongProtocol = """{
+   "protocol": "WRONG",
+   "protocolVersion": "1.0.0",
+   "serviceIdentity": "https://dgca-booking-demo-eu-test.cfapps.eu10.hana.ondemand.com/api/identity",
+   "privacyUrl": "https://validation-decorator.example",
+   "token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJiUzhEMi9XejV0WT0iLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RnY2EtYm9va2luZy1kZW1vLWV1LXRlc3QuY2ZhcHBzLmV1MTAuaGFuYS5vbmRlbWFuZC5jb20vYXBpL2lkZW50aXR5IiwiZXhwIjoxNjM1NDk2MzYwLCJzdWIiOiIwMDI0MWQxMS0yN2I0LTQxYWYtOWU3Ny0zNDE4YzNlY2NmZDQifQ.X0wUdET3omy3qXyOhBh1UuAUEvfYMCdapv0yVShynfZpc4yS3kH57TrPLgSqS7A9ZhbgIdCIfZwr0Chm1ELyTw",
+   "consent": "Please confirm to start the DCC Exchange flow. If you do not confirm, the flow is aborted.",
+   "subject": "00241d11-27b4-41af-9e77-3418c3eccfd4",
+   "serviceProvider": "Booking Demo"
+}"""
+
+@Suppress("MaxLineLength")
+private const val invalidQrCodeMissingSubject = """{
+   "protocol": "DCCVALIDATION",
+   "protocolVersion": "1.0.0",
+   "serviceIdentity": "https://dgca-booking-demo-eu-test.cfapps.eu10.hana.ondemand.com/api/identity",
+   "privacyUrl": "https://validation-decorator.example",
+   "token": "eyJ0eXAiOiJKV1QiLCJraWQiOiJiUzhEMi9XejV0WT0iLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RnY2EtYm9va2luZy1kZW1vLWV1LXRlc3QuY2ZhcHBzLmV1MTAuaGFuYS5vbmRlbWFuZC5jb20vYXBpL2lkZW50aXR5IiwiZXhwIjoxNjM1NDk2MzYwLCJzdWIiOiIwMDI0MWQxMS0yN2I0LTQxYWYtOWU3Ny0zNDE4YzNlY2NmZDQifQ.X0wUdET3omy3qXyOhBh1UuAUEvfYMCdapv0yVShynfZpc4yS3kH57TrPLgSqS7A9ZhbgIdCIfZwr0Chm1ELyTw",
+   "consent": "Please confirm to start the DCC Exchange flow. If you do not confirm, the flow is aborted.",
+   "serviceProvider": "Booking Demo"
+}"""
+
+private const val invalidJson = """{
+   "protocol": "DCCVALIDATION",
+   "cookie": "chocolate chip",
+}"""
