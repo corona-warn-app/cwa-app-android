a8505a1b0a0359b573276199dea2419be6324b8d
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-10 22:37:26 +0100

    Update BoosterNotificationTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/booster/BoosterNotificationTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/booster/BoosterNotificationTest.kt
@@ -61,15 +61,4 @@ class BoosterNotificationTest : BaseTest() {
         createInstance().showBoosterNotification(personIdentifier)
         verify { notificationHelper.sendNotification(any(), any()) }
     }
-
-    @Test
-    fun `cancel booster notification`() = runBlockingTest {
-        val personIdentifier = CertificatePersonIdentifier(
-            dateOfBirthFormatted = "1990-10-10",
-            firstNameStandardized = "firstNameStandardized",
-            lastNameStandardized = "lastNameStandardized"
-        )
-        createInstance().cancelNotification(personIdentifier)
-        verify { notificationHelper.cancelNotification(any()) }
-    }
 }
