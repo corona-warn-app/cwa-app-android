8e3b01425f0d804b20fd8ee04b9074bbb60d99e8
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-11 17:03:40 +0100

    Test menu generates keys (#4376)

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/dccticketing/DccTicketingTestFragment.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/dccticketing/DccTicketingTestFragment.kt
@@ -2,11 +2,15 @@ package de.rki.coronawarnapp.test.dccticketing
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTestDccTicketingBinding
 import de.rki.coronawarnapp.test.menu.ui.TestMenuItem
+import de.rki.coronawarnapp.util.ContextExtensions.getColorCompat
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.encoding.base64
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
 import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
@@ -21,6 +25,24 @@ class DccTicketingTestFragment : Fragment(R.layout.fragment_test_dcc_ticketing),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.generateEcKeyPair.setOnClickListener {
+            viewModel.generateECKeyPair()
+        }
+        val color = requireContext().getColorCompat(R.color.colorPrimary)
+        viewModel.ecKeyPair.observe(viewLifecycleOwner) {
+            binding.generatedKeyPair.text = buildSpannedString {
+                color(color) {
+                    append("PublicKeyBase64=")
+                }
+                append(it.publicKeyBase64)
+                appendLine()
+                color(color) {
+                    append("PrivateKeyBase64=")
+                }
+                append(it.privateKey.encoded.base64())
+            }
+        }
     }
 
     companion object {
--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/dccticketing/DccTicketingTestViewModel.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/dccticketing/DccTicketingTestViewModel.kt
@@ -2,10 +2,23 @@ package de.rki.coronawarnapp.test.dccticketing
 
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.util.encryption.ec.ECKeyPair
+import de.rki.coronawarnapp.util.encryption.ec.EcKeyGenerator
+import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 
-class DccTicketingTestViewModel @AssistedInject constructor() : CWAViewModel() {
+class DccTicketingTestViewModel @AssistedInject constructor(
+    private val ecKeyGenerator: EcKeyGenerator
+) : CWAViewModel() {
+
+    val ecKeyPair = SingleLiveEvent<ECKeyPair>()
+
+    fun generateECKeyPair() = launch {
+        ecKeyPair.postValue(
+            ecKeyGenerator.generateECKeyPair()
+        )
+    }
 
     @AssistedFactory
     interface Factory : SimpleCWAViewModelFactory<DccTicketingTestViewModel>
