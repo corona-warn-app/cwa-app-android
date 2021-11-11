b705b6a10a720b1bb66f904c94a03a2ae15510a1
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-11-11 17:42:18 +0100

    clean up

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/transaction/DccTicketingValidationCondition.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/transaction/DccTicketingValidationCondition.kt
@@ -7,31 +7,31 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class DccTicketingValidationCondition(
     @SerializedName("hash")
-    val hash: String?,              //(optional)	Hash of the DCC
+    val hash: String?, // (optional)	Hash of the DCC
     @SerializedName("lang")
-    val lang: String?,              //(optional)	Selected language
+    val lang: String?, // (optional)	Selected language
     @SerializedName("fnt")
-    val fnt: String?,               //(optional)	Transliterated family name
+    val fnt: String?, // (optional)	Transliterated family name
     @SerializedName("gnt")
-    val gnt: String?,               //(optional)	Transliterated given name
+    val gnt: String?, // (optional)	Transliterated given name
     @SerializedName("dob")
-    val dob: String?,               //(optional)	Date of birth
+    val dob: String?, // (optional)	Date of birth
     @SerializedName("type")
-    val type: List<String>?,        //(optional)	The acceptable type of DCC
+    val type: List<String>?, // (optional)	The acceptable type of DCC
     @SerializedName("coa")
-    val coa: String?,               //(optional)	Country of arrival
+    val coa: String?, // (optional)	Country of arrival
     @SerializedName("roa")
-    val roa: String?,               //(optional)	Region of arrival
+    val roa: String?, // (optional)	Region of arrival
     @SerializedName("cod")
-    val cod: String?,               //(optional)	Country of departure
+    val cod: String?, // (optional)	Country of departure
     @SerializedName("rod")
-    val rod: String?,               //(optional)	Region of departure
+    val rod: String?, // (optional)	Region of departure
     @SerializedName("category")
-    val category: List<String>?,    //(optional)	Category for validation
+    val category: List<String>?, // (optional)	Category for validation
     @SerializedName("validationClock")
-    val validationClock: String?,   //(optional)	ISO8601 date where the DCC must be validatable
+    val validationClock: String?, // (optional)	ISO8601 date where the DCC must be validatable
     @SerializedName("validFrom")
-    val validFrom: String?,         //(optional)	ISO8601 date where the DCC must be valid from
+    val validFrom: String?, // (optional)	ISO8601 date where the DCC must be valid from
     @SerializedName("validTo")
-    val validTo: String?,           //(optional)	ISO8601 date where the DCC must be valid to
+    val validTo: String?, // (optional)	ISO8601 date where the DCC must be valid to
 ) : Parcelable
