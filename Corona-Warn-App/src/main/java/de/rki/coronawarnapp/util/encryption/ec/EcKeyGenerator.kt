208d7d62557bb6d3a1110567836a7ad7b2bd9613
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-11 14:41:44 +0100

    Generate keys

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/encryption/ec/EcKeyGenerator.kt
@@ -0,0 +1,48 @@
+package de.rki.coronawarnapp.util.encryption.ec
+
+import com.google.common.io.BaseEncoding
+import dagger.Reusable
+import de.rki.coronawarnapp.util.encoding.base64
+import org.bouncycastle.math.ec.rfc8032.Ed25519
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.PublicKey
+import javax.inject.Inject
+
+@Reusable
+class EcKeyGenerator @Inject constructor() {
+
+    fun generateECKeyPair(): ECKeyPair {
+        val keyPair: KeyPair = KeyPairGenerator.getInstance(ALGORITHM)
+            .run {
+                initialize(KEY_SIZE)
+                generateKeyPair()
+            }
+        return ECKeyPair(
+            publicKey = keyPair.public,
+            privateKey = keyPair.private,
+            publicKeyBase64 = keyPair.public.encoded.base64()
+        )
+    }
+
+    companion object {
+        private const val KEY_SIZE = 256
+        private const val ALGORITHM = "EC"
+    }
+}
+
+data class ECKeyPair(
+    /**
+     * Native (EC) Public Key object
+     */
+    val publicKey: PublicKey,
+    /**
+     * Native (EC) Private Key object
+     */
+    val privateKey: PrivateKey,
+    /**
+     * A string that represents the base64-encoded public key (not byte sequence (Data / ByteArray))
+     */
+    val publicKeyBase64: String
+)
