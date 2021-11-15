f06526f3641678402806fb36c8b26768a6cf6345
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-15 15:37:48 +0100

    Filter JWK

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeHandler.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeHandler.kt
@@ -1,13 +1,22 @@
 package de.rki.coronawarnapp.dccticketing.core.qrcode
 
+import de.rki.coronawarnapp.dccticketing.core.allowlist.filtering.DccTicketingJwkFilter
+import de.rki.coronawarnapp.dccticketing.core.qrcode.DccTicketingInvalidQrCodeException.ErrorCode
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccTicketingTransactionContext
 import javax.inject.Inject
 
-class DccTicketingQrCodeHandler @Inject constructor() {
+class DccTicketingQrCodeHandler @Inject constructor(
+    private val dccTicketingJwkFilter: DccTicketingJwkFilter,
+) {
 
     suspend fun handleQrCode(qrCode: DccTicketingQrCode) {
         val transactionContext = DccTicketingTransactionContext(initializationData = qrCode.data)
         // todo Request Service Identity Document of Validation Decorator
         // todo Check against allowlist
+        val filteringResult = dccTicketingJwkFilter.filter(emptySet())
+        if (filteringResult.filteredJwkSet.isEmpty()) {
+            throw DccTicketingInvalidQrCodeException(ErrorCode.ALLOWLIST_NO_MATCH)
+        }
+        // todo return proper data when allow list pass
     }
 }
