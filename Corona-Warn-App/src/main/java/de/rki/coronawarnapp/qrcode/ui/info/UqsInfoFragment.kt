375918df2c26facd42d3dacbe647402d498f17df
SamuraiKek <alex.paulescu@gmail.com>
2021-11-15 22:16:09 +0200

    Fixed scrolling.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/qrcode/ui/info/UqsInfoFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/qrcode/ui/info/UqsInfoFragment.kt
@@ -5,8 +5,10 @@ import android.view.View
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialSharedAxis
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentUqsInfoBinding
+import de.rki.coronawarnapp.ui.view.onOffsetChange
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 
@@ -14,11 +16,21 @@ class UqsInfoFragment : Fragment(R.layout.fragment_uqs_info) {
 
     private val binding: FragmentUqsInfoBinding by viewBinding()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.apply {
             toolbar.setNavigationOnClickListener { popBackStack() }
             privacyInformation.setOnClickListener {
                 findNavController().navigate("coronawarnapp://contactdiary/dataPrivacy".toUri())
+            }
+            appBarLayout.onOffsetChange { _, subtitleAlpha ->
+                headerImage.alpha = subtitleAlpha
             }
         }
     }
