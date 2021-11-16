f8b4a04bbca8e35ce94cfc763cabe74d633f6693
Alexander Alferov <a.alferov@sap.com>
2021-11-16 22:00:14 +0300

    Close alert dialog

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/consent/one/DccTicketingConsentOneFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/consent/one/DccTicketingConsentOneFragment.kt
@@ -2,8 +2,10 @@ package de.rki.coronawarnapp.dccticketing.ui.consent.one
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.transition.MaterialSharedAxis
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentDccTicketingConsentOneBinding
@@ -27,7 +29,8 @@ class DccTicketingConsentOneFragment : Fragment(R.layout.fragment_dcc_ticketing_
 
         binding.apply {
 
-            toolbar.setNavigationOnClickListener { popBackStack() }
+            toolbar.setNavigationOnClickListener { showCloseAlertDialog() }
+            cancelButton.setOnClickListener { showCloseAlertDialog() }
 
             appBarLayout.onOffsetChange { _, subtitleAlpha ->
                 headerImage.alpha = subtitleAlpha
@@ -37,5 +40,16 @@ class DccTicketingConsentOneFragment : Fragment(R.layout.fragment_dcc_ticketing_
                 findNavController().navigate(R.id.informationPrivacyFragment)
             }
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) { showCloseAlertDialog() }
+    }
+
+    private fun showCloseAlertDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.dcc_ticketing_consent_one_cancel_dialog_title)
+            .setMessage(R.string.dcc_ticketing_consent_one_cancel_dialog_body)
+            .setNegativeButton(R.string.dcc_ticketing_consent_one_cancel_dialog_continue_btn) { _, _ -> }
+            .setPositiveButton(R.string.dcc_ticketing_consent_one_cancel_dialog_cancel_btn) { _, _ -> popBackStack() }
+            .show()
     }
 }
