7519e56ccc5214574a76ed311d4ea51922921acd
Alexander Alferov <a.alferov@sap.com>
2021-11-16 21:25:39 +0300

    Fragment with initial logic

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/consent/one/DccTicketingConsentOneFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/consent/one/DccTicketingConsentOneFragment.kt
@@ -1,8 +1,41 @@
 package de.rki.coronawarnapp.dccticketing.ui.consent.one
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialSharedAxis
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FragmentDccTicketingConsentOneBinding
+import de.rki.coronawarnapp.ui.view.onOffsetChange
+import de.rki.coronawarnapp.util.ui.popBackStack
+import de.rki.coronawarnapp.util.ui.viewBinding
 
 class DccTicketingConsentOneFragment : Fragment(R.layout.fragment_dcc_ticketing_consent_one) {
-    // TODO
+
+    private val binding: FragmentDccTicketingConsentOneBinding by viewBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            toolbar.setNavigationOnClickListener { popBackStack() }
+
+            appBarLayout.onOffsetChange { _, subtitleAlpha ->
+                headerImage.alpha = subtitleAlpha
+            }
+
+            privacyInformation.setOnClickListener {
+                findNavController().navigate(R.id.informationPrivacyFragment)
+            }
+        }
+    }
 }
