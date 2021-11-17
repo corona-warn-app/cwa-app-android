8724115295f7be9a1472022597c28b789aaca8f0
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-11-17 17:04:40 +0100

    rebase

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeHandler.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeHandler.kt
@@ -1,16 +1,14 @@
 package de.rki.coronawarnapp.dccticketing.core.qrcode
 
-import de.rki.coronawarnapp.dccticketing.core.service.DccTicketingRequestService
 import de.rki.coronawarnapp.dccticketing.core.allowlist.DccTicketingAllowListException
 import de.rki.coronawarnapp.dccticketing.core.allowlist.DccTicketingAllowListException.ErrorCode
 import de.rki.coronawarnapp.dccticketing.core.allowlist.filtering.DccTicketingJwkFilter
+import de.rki.coronawarnapp.dccticketing.core.service.DccTicketingRequestService
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccTicketingTransactionContext
 import javax.inject.Inject
 
 class DccTicketingQrCodeHandler @Inject constructor(
-    private val requestService: DccTicketingRequestService
-) {
-class DccTicketingQrCodeHandler @Inject constructor(
+    private val requestService: DccTicketingRequestService,
     private val dccTicketingJwkFilter: DccTicketingJwkFilter,
 ) {
 
@@ -19,7 +17,6 @@ class DccTicketingQrCodeHandler @Inject constructor(
             initializationData = qrCode.data
         ).decorate()
 
-        // todo Check against allowlist
         val filteringResult = dccTicketingJwkFilter.filter(emptySet())
         if (filteringResult.filteredJwkSet.isEmpty()) {
             throw DccTicketingAllowListException(ErrorCode.ALLOWLIST_NO_MATCH)
