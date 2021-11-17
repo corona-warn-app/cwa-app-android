f1f291de2aaf20417055196d5f039d0fcfaecd66
BerndMitter <Berndus@gmx.de>
2021-11-17 11:34:44 +0100

    Remove get request

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/server/DccTicketingApiV1.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/server/DccTicketingApiV1.kt
@@ -1,11 +1,4 @@
 package de.rki.coronawarnapp.dccticketing.core.server
 
-import de.rki.coronawarnapp.dccticketing.core.transaction.DccTicketingServiceIdentityDocument
-import retrofit2.http.GET
-import retrofit2.http.Url
-
 interface DccTicketingApiV1 {
-
-    @GET
-    suspend fun getServiceIdentityDocument(@Url url: String): DccTicketingServiceIdentityDocument
 }
