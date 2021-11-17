a76d36808c3c585aea7d1f7886a206e6a3fd22df
BerndMitter <Berndus@gmx.de>
2021-11-17 11:33:47 +0100

    Implemented first and second step

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/check/DccTicketingServerCertificateChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/check/DccTicketingServerCertificateChecker.kt
@@ -2,6 +2,8 @@ package de.rki.coronawarnapp.dccticketing.core.check
 
 import dagger.Reusable
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccJWK
+import okio.ByteString.Companion.toByteString
+import timber.log.Timber
 import java.security.cert.Certificate
 import javax.inject.Inject
 
@@ -15,10 +17,11 @@ class DccTicketingServerCertificateChecker @Inject constructor() {
      */
     @Throws(DccTicketingServerCertificateCheckException::class)
     fun checkCertificate(certificateChain: List<Certificate>, jwkSet: Set<DccJWK>) {
-
         // 1. Extract leafCertificate
+        val leafCertificate = certificateChain.first()
 
         // 2. Determine requiredKid
+        val requiredKid = leafCertificate.createKid().also { Timber.d("requiredKid=%s", it) }
 
         // 3. Find requiredJwkSet
 
@@ -28,6 +31,14 @@ class DccTicketingServerCertificateChecker @Inject constructor() {
 
         // 6. Compare fingerprints
 
-        // TODO: Implement all steps
+        //TODO: Implement all steps
     }
+
+    //Takes the first 8 bytes of the SHA-256 fingerprint of the certificate and encodes them with base64
+    private fun Certificate.createKid(): String = encoded.toByteString()
+        .sha256()
+        .substring(0, BYTE_COUNT)
+        .base64()
 }
+
+private const val BYTE_COUNT: Int = 8
