15fb4ea30f68c2f24b31355a5b4664f09bb8c188
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-11-18 09:51:22 +0100

    Request Service Identity Document of Validation Decorator (EXPOSUREAPP-10194) (#4393)
    * Added data
    
    * Updated data structure
    
    * Updated doc
    
    * rename
    
    * Added api and module
    
    * Added exception
    
    * Implemented sserver and api
    
    * renamed
    
    * Added service
    
    * Implemented ValidationDecoratorRequest
    
    * Added processors for requests
    
    * Added Di modules
    
    * Added note
    
    * adjusted naming mismatch
    
    * fix ApplicationComponent
    
    * little adjustment to accesstokenRequest
    
    * clean
    
    * cleanup - move to correct package
    
    * clean
    
    * source clean & detekt
    
    * decorator
    
    * validation service request
    
    * sourceclean
    
    * tech spec update
    
    * Added cert pinning skeletion, Adjusted error mapping
    
    * Added error messages
    
    * Added error messages
    
    * sourceclean
    
    * Implemented first and second step
    
    * Remove get request
    
    * Dummy impl to satisfy Ci
    
    * source clean
    
    * satisfy sonar
    
    * Revert "satisfy sonar"
    
    This reverts commit 32e798d82dc71a258ce22b02ccf81c467405d59c.
    
    * satisfy sonar
    
    * Revert "satisfy sonar"
    
    This reverts commit 9fb0b46a13c5f38c50b8db0dabcd9eb3fe2e803d.
    
    * silence sonar
    
    * clean up duplicates
    
    * silencium
    
    * use service
    
    * rebase
    
    * rebase
    
    * line
    
    * lines
    
    * lines
    
    Co-authored-by: BerndMitter <Berndus@gmx.de>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeHandler.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/qrcode/DccTicketingQrCodeHandler.kt
@@ -3,21 +3,35 @@ package de.rki.coronawarnapp.dccticketing.core.qrcode
 import de.rki.coronawarnapp.dccticketing.core.allowlist.DccTicketingAllowListException
 import de.rki.coronawarnapp.dccticketing.core.allowlist.DccTicketingAllowListException.ErrorCode
 import de.rki.coronawarnapp.dccticketing.core.allowlist.filtering.DccTicketingJwkFilter
+import de.rki.coronawarnapp.dccticketing.core.service.DccTicketingRequestService
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccTicketingTransactionContext
 import javax.inject.Inject
 
 class DccTicketingQrCodeHandler @Inject constructor(
+    private val requestService: DccTicketingRequestService,
     private val dccTicketingJwkFilter: DccTicketingJwkFilter,
 ) {
 
     suspend fun handleQrCode(qrCode: DccTicketingQrCode) {
-        val transactionContext = DccTicketingTransactionContext(initializationData = qrCode.data)
-        // todo Request Service Identity Document of Validation Decorator
-        // todo Check against allowlist
+        var transactionContext = DccTicketingTransactionContext(
+            initializationData = qrCode.data
+        ).decorate()
+
         val filteringResult = dccTicketingJwkFilter.filter(emptySet())
         if (filteringResult.filteredJwkSet.isEmpty()) {
             throw DccTicketingAllowListException(ErrorCode.ALLOWLIST_NO_MATCH)
         }
         // todo return proper data when allow list pass
+    }
+
+    suspend fun DccTicketingTransactionContext.decorate(): DccTicketingTransactionContext {
+        val decorator = requestService.requestValidationDecorator(initializationData.serviceIdentity)
+        return copy(
+            accessTokenService = decorator.accessTokenService,
+            accessTokenServiceJwkSet = decorator.accessTokenServiceJwkSet,
+            accessTokenSignJwkSet = decorator.accessTokenSignJwkSet,
+            validationService = decorator.validationService,
+            validationServiceJwkSet = decorator.validationServiceJwkSet,
+        )
     }
 }
