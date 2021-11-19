c1b5f455ee6a9a43ac249f5b8a0db99f1d9d2018
Alex Paulescu <alex.paulescu@gmail.com>
2021-11-19 14:19:10 +0200

    Percentage value gets formatted at runtime when changing locale. (#4412)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/util/StatisticsNumberValueFormatter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/util/StatisticsNumberValueFormatter.kt
@@ -5,6 +5,7 @@ import de.rki.coronawarnapp.contactdiary.util.getLocale
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
+import java.util.Locale
 
 fun formatStatisticalValue(
     context: Context,
@@ -27,6 +28,10 @@ fun formatStatisticalValue(
     }.format(value)
 }
 
-fun formatPercentageValue(value: Double): String = percentInstance.format(value)
+fun formatPercentageValue(value: Double, locale: Locale): String {
+    val percentInstance = getPercentInstance(locale)
+    return percentInstance.format(value)
+}
 
-private val percentInstance = NumberFormat.getPercentInstance().apply { minimumFractionDigits = 1 }
+private fun getPercentInstance(locale: Locale) =
+    NumberFormat.getPercentInstance(locale).apply { minimumFractionDigits = 1 }
