7ba01fcd82b8c6da325cd46176cdec3c2fe528b0
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-22 11:04:05 +0100

    Update DccJWK.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/transaction/DccJWK.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/transaction/DccJWK.kt
@@ -3,12 +3,7 @@ package de.rki.coronawarnapp.dccticketing.core.transaction
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccJWK.Purpose
-import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
-import okio.ByteString.Companion.decodeBase64
-import java.security.PublicKey
-import java.security.cert.CertificateFactory
-import java.security.cert.X509Certificate
 
 @Parcelize
 data class DccJWK(
@@ -49,18 +44,4 @@ data class DccJWK(
         @SerializedName("enc")
         ENCRYPTION
     }
-
-    @IgnoredOnParcel
-    private val certificateFactory by lazy {
-        CertificateFactory.getInstance("X.509")
-    }
-
-    val publicKey: PublicKey?
-        get() = x5c.first()
-            .decodeBase64()
-            ?.toByteArray()
-            ?.inputStream()
-            .use {
-                certificateFactory.generateCertificate(it) as X509Certificate
-            }.publicKey
 }
