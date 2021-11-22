6e610262c6bf2e7521228c2857bff6159b35b503
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-22 20:24:15 +0100

    Create ResultTokenRequestProcessorTest.kt

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccticketing/core/service/processor/ResultTokenRequestProcessorTest.kt
@@ -0,0 +1,32 @@
+package de.rki.coronawarnapp.dccticketing.core.service.processor
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ResultTokenRequestProcessorTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun requestResultToken() {
+    }
+
+    @Test
+    fun checkServerCertificate() {
+    }
+
+    @Test
+    fun verifyJWT() {
+    }
+
+    @Test
+    fun resultTokenResponse() {
+    }
+}
