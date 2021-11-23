97336399817a6882e8058df1a6ca5e5b6726fab3
BerndMitter <Berndus@gmx.de>
2021-11-23 22:11:00 +0100

    Added error dialog

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/dialog/DccTicketingDialogFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/dialog/DccTicketingDialogFragment.kt
@@ -9,8 +9,10 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import de.rki.coronawarnapp.util.ui.LazyString
 import kotlinx.parcelize.Parcelize
 import timber.log.Timber
+import java.lang.IllegalArgumentException
 
 class DccTicketingDialogFragment : DialogFragment() {
 
@@ -19,12 +21,19 @@ class DccTicketingDialogFragment : DialogFragment() {
         requireNotNull(config) { "Dialog config is null" }
 
         return config.run {
-            MaterialAlertDialogBuilder(requireContext())
-                .setTitle(titleRes)
-                .setMessage(msgRes)
-                .setPositiveButton(positiveButtonRes) { _, _ -> setAction(Action.PositiveButtonClicked) }
-                .setNegativeButton(negativeButtonRes) { _, _ -> setAction(Action.NegativeButtonClicked) }
-                .create()
+            MaterialAlertDialogBuilder(requireContext()).apply {
+                setTitle(titleRes)
+
+                when {
+                    msgRes != null -> setMessage(msgRes)
+                    msg != null -> setMessage(msg)
+                    else -> throw IllegalArgumentException("msgRes or msg must be set!")
+                }
+
+                setPositiveButton(positiveButtonRes) { _, _ -> setAction(Action.PositiveButtonClicked) }
+
+                negativeButtonRes?.let { setNegativeButton(it) { _, _ -> setAction(Action.NegativeButtonClicked) } }
+            }.create()
         }
     }
 
@@ -47,9 +56,10 @@ class DccTicketingDialogFragment : DialogFragment() {
     @Parcelize
     data class Config(
         @StringRes val titleRes: Int,
-        @StringRes val msgRes: Int,
+        @StringRes val msgRes: Int? = null,
+        val msg: String? = null,
         @StringRes val positiveButtonRes: Int,
-        @StringRes val negativeButtonRes: Int,
+        @StringRes val negativeButtonRes: Int? = null,
     ) : Parcelable
 
     companion object {
