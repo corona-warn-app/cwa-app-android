9c01ccf6d786e83f1754ac6beb07bb118ca53d18
AlexanderAlferov <64849422+AlexanderAlferov@users.noreply.github.com>
2021-11-23 16:19:24 +0300

    DCC Ticketing: navigation to Consent I (#4424)
    * Navigation to consent one
    
    * Dcc ticketing dialog
    
    * Fix
    
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>
    Co-authored-by: Juraj Kusnier <jurajkusnier@users.noreply.github.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/consent/one/DccTicketingConsentOneFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/consent/one/DccTicketingConsentOneFragment.kt
@@ -1,29 +1,49 @@
 package de.rki.coronawarnapp.dccticketing.ui.consent.one
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.navGraphViewModels
 import com.google.android.material.transition.MaterialSharedAxis
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentDccTicketingConsentOneBinding
+import de.rki.coronawarnapp.dccticketing.ui.dialog.DccTicketingDialogType
+import de.rki.coronawarnapp.dccticketing.ui.dialog.show
+import de.rki.coronawarnapp.qrcode.ui.QrcodeSharedViewModel
 import de.rki.coronawarnapp.ui.view.onOffsetChange
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
-import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
+import java.net.URLEncoder
 import javax.inject.Inject
 
+@SuppressLint("SetTextI18n")
 class DccTicketingConsentOneFragment : Fragment(R.layout.fragment_dcc_ticketing_consent_one), AutoInject {
+    private val args by navArgs<DccTicketingConsentOneFragmentArgs>()
 
     @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
-    private val viewModel: DccTicketingConsentOneViewModel by cwaViewModels { viewModelFactory }
-
+    private val viewModel: DccTicketingConsentOneViewModel by cwaViewModelsAssisted(
+        factoryProducer = { viewModelFactory },
+        constructorCall = { factory, _ ->
+            factory as DccTicketingConsentOneViewModel.Factory
+            factory.create(
+                dccTicketingTransactionContext = qrcodeSharedViewModel.dccTicketingTransactionContext(
+                    args.transactionContextIdentifier
+                )
+            )
+        }
+    )
     private val binding: FragmentDccTicketingConsentOneBinding by viewBinding()
+    private val qrcodeSharedViewModel by navGraphViewModels<QrcodeSharedViewModel>(R.id.nav_graph)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,15 +73,29 @@ class DccTicketingConsentOneFragment : Fragment(R.layout.fragment_dcc_ticketing_
             showCloseAlertDialog()
         }
 
+        viewModel.uiState.observe2(this) {
+            with(binding) {
+                provider.text = "\"${it.provider}\""
+                subject.text = "\"${it.subject}\""
+            }
+        }
+
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) { viewModel.goBack() }
     }
 
     private fun showCloseAlertDialog() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(R.string.dcc_ticketing_consent_one_cancel_dialog_title)
-            .setMessage(R.string.dcc_ticketing_consent_one_cancel_dialog_body)
-            .setPositiveButton(R.string.dcc_ticketing_consent_one_cancel_dialog_continue_btn) { _, _ -> }
-            .setNegativeButton(R.string.dcc_ticketing_consent_one_cancel_dialog_cancel_btn) { _, _ -> popBackStack() }
-            .show()
+        DccTicketingDialogType.ConfirmCancelation.show(
+            fragment = this,
+            negativeButtonAction = { popBackStack() }
+        )
+    }
+
+    companion object {
+        fun uri(
+            transactionContextIdentifier: String
+        ): Uri {
+            val encodedTransactionContextId = URLEncoder.encode(transactionContextIdentifier, "UTF-8")
+            return "cwa://dcc.ticketing.consent.one/?transactionContextIdentifier=$encodedTransactionContextId".toUri()
+        }
     }
 }
