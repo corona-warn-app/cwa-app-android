2983cada0dc8049c7cf3aad7d5341286a6dfc5a5
BerndMitter <Berndus@gmx.de>
2021-11-23 22:09:06 +0100

    Get error message with provider name

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/common/DccTicketingException.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/common/DccTicketingException.kt
@@ -1,17 +1,15 @@
 package de.rki.coronawarnapp.dccticketing.core.common
 
-import android.content.Context
 import androidx.annotation.StringRes
 import de.rki.coronawarnapp.R
-import de.rki.coronawarnapp.util.HasHumanReadableError
-import de.rki.coronawarnapp.util.HumanReadableError
 import de.rki.coronawarnapp.util.ui.CachedString
 import de.rki.coronawarnapp.util.ui.LazyString
+import de.rki.coronawarnapp.util.ui.toResolvingString
 
 class DccTicketingException(
     val errorCode: ErrorCode,
     override val cause: Throwable? = null
-) : Exception(errorCode.message, cause), HasHumanReadableError {
+) : Exception(errorCode.message, cause) {
 
     @Suppress("MaxLineLength")
     enum class ErrorCode(val message: String) {
@@ -26,36 +24,37 @@ class DccTicketingException(
 */
         ATR_AUD_INVALID("failure when obtaining Access Token; user may retry"),
         ATR_PARSE_ERR("failure when obtaining Access Token; user may retry"),
-/*
-        ATR_CERT_PIN_MISMATCH("failure when obtaining Access Token; user may retry"),
-        ATR_CERT_PIN_NO_JWK_FOR_KID("failure when obtaining Access Token; user may retry"),
-        ATR_CLIENT_ERR("failure when obtaining Access Token; user may retry"),
-        ATR_JWT_VER_ALG_NOT_SUPPORTED("failure when obtaining Access Token; user may retry"),
-        ATR_JWT_VER_EMPTY_JWKS("failure when obtaining Access Token; user may retry"),
-        ATR_JWT_VER_NO_JWK_FOR_KID("failure when obtaining Access Token; user may retry"),
-        ATR_JWT_VER_NO_KID("failure when obtaining Access Token; user may retry"),
-        ATR_JWT_VER_SIG_INVALID("failure when obtaining Access Token; user may retry"),
-        ATR_NO_NETWORK("failure when obtaining Access Token; user may retry"),
-        ATR_SERVER_ERR("failure when obtaining Access Token; user may retry"),
-        ATR_TYPE_INVALID("failure when obtaining Access Token; user may retry"),
 
-         EC_SIGN_INVALID_KEY("failure while encrypting DCC; user may retry"),
-         EC_SIGN_NOT_SUPPORTED("failure while encrypting DCC; user may retry"),
+        /*
+                ATR_CERT_PIN_MISMATCH("failure when obtaining Access Token; user may retry"),
+                ATR_CERT_PIN_NO_JWK_FOR_KID("failure when obtaining Access Token; user may retry"),
+                ATR_CLIENT_ERR("failure when obtaining Access Token; user may retry"),
+                ATR_JWT_VER_ALG_NOT_SUPPORTED("failure when obtaining Access Token; user may retry"),
+                ATR_JWT_VER_EMPTY_JWKS("failure when obtaining Access Token; user may retry"),
+                ATR_JWT_VER_NO_JWK_FOR_KID("failure when obtaining Access Token; user may retry"),
+                ATR_JWT_VER_NO_KID("failure when obtaining Access Token; user may retry"),
+                ATR_JWT_VER_SIG_INVALID("failure when obtaining Access Token; user may retry"),
+                ATR_NO_NETWORK("failure when obtaining Access Token; user may retry"),
+                ATR_SERVER_ERR("failure when obtaining Access Token; user may retry"),
+                ATR_TYPE_INVALID("failure when obtaining Access Token; user may retry"),
 
-         RSA_ENC_INVALID_KEY("failure while encrypting DCC; user may retry"),
-         RSA_ENC_NOT_SUPPORTED("failure while encrypting DCC; user may retry"),
+                 EC_SIGN_INVALID_KEY("failure while encrypting DCC; user may retry"),
+                 EC_SIGN_NOT_SUPPORTED("failure while encrypting DCC; user may retry"),
 
-         RTR_CERT_PIN_MISMATCH("failure when obtaining Result Token; user may retry"),
-         RTR_CERT_PIN_NO_JWK_FOR_KID("failure when obtaining Result Token; user may retry"),
-         RTR_CLIENT_ERR("failure when obtaining Result Token; user may retry"),
-         RTR_JWT_VER_ALG_NOT_SUPPORTED("failure when obtaining Result Token; user may retry"),
-         RTR_JWT_VER_EMPTY_JWKS("failure when obtaining Result Token; user may retry"),
-         RTR_JWT_VER_NO_JWK_FOR_KID("failure when obtaining Result Token; user may retry"),
-         RTR_JWT_VER_NO_KID("failure when obtaining Result Token; user may retry"),
-         RTR_JWT_VER_SIG_INVALID("failure when obtaining Result Token; user may retry"),
-         RTR_NO_NETWORK("failure when obtaining Result Token; user may retry"),
-         RTR_SERVER_ERR("failure when obtaining Result Token; user may retry"),
- */
+                 RSA_ENC_INVALID_KEY("failure while encrypting DCC; user may retry"),
+                 RSA_ENC_NOT_SUPPORTED("failure while encrypting DCC; user may retry"),
+
+                 RTR_CERT_PIN_MISMATCH("failure when obtaining Result Token; user may retry"),
+                 RTR_CERT_PIN_NO_JWK_FOR_KID("failure when obtaining Result Token; user may retry"),
+                 RTR_CLIENT_ERR("failure when obtaining Result Token; user may retry"),
+                 RTR_JWT_VER_ALG_NOT_SUPPORTED("failure when obtaining Result Token; user may retry"),
+                 RTR_JWT_VER_EMPTY_JWKS("failure when obtaining Result Token; user may retry"),
+                 RTR_JWT_VER_NO_JWK_FOR_KID("failure when obtaining Result Token; user may retry"),
+                 RTR_JWT_VER_NO_KID("failure when obtaining Result Token; user may retry"),
+                 RTR_JWT_VER_SIG_INVALID("failure when obtaining Result Token; user may retry"),
+                 RTR_NO_NETWORK("failure when obtaining Result Token; user may retry"),
+                 RTR_SERVER_ERR("failure when obtaining Result Token; user may retry"),
+         */
         VD_ID_CLIENT_ERR("VD_ID_CLIENT_ERR"),
         VD_ID_NO_ATS_SIGN_KEY("VD_ID_NO_ATS_SIGN_KEY"),
         VD_ID_NO_ATS_SVC_KEY("VD_ID_NO_ATS_SVC_KEY"),
@@ -79,17 +78,24 @@ class DccTicketingException(
     }
 
     // to-do: Add all error codes
-    val errorMessage: LazyString
-        get() = CachedString { context ->
-            when (errorCode) {
-                ErrorCode.VS_ID_SERVER_ERR -> R.string.dcc_ticketing_error_try_again
-                else -> ERROR_MESSAGE_GENERIC
-            }.let { context.getString(it) }
-        }
+    private fun errorMessageRes(serviceProvider: String): LazyString = when (errorCode) {
+        ErrorCode.VS_ID_CERT_PIN_MISMATCH,
+        ErrorCode.VS_ID_CERT_PIN_NO_JWK_FOR_KID,
+        ErrorCode.VS_ID_CLIENT_ERR,
+        ErrorCode.VS_ID_NO_ENC_KEY,
+        ErrorCode.VS_ID_NO_SIGN_KEY,
+        ErrorCode.VS_ID_EMPTY_X5C,
+        ErrorCode.VS_ID_PARSE_ERR ->
+            R.string.dcc_ticketing_error_service_provider_error.toResolvingString(serviceProvider)
 
-    override fun toHumanReadableError(context: Context): HumanReadableError = HumanReadableError(
-        description = errorMessage.get(context) + " ($errorCode)"
-    )
+        ErrorCode.VS_ID_NO_NETWORK,
+        ErrorCode.VS_ID_SERVER_ERR -> R.string.dcc_ticketing_error_try_again.toResolvingString()
+        else -> ERROR_MESSAGE_GENERIC.toResolvingString()
+    }
+
+    fun errorMessage(serviceProvider: String): LazyString = CachedString { context ->
+        "${errorMessageRes(serviceProvider).get(context)} ($errorCode)"
+    }
 }
 
 @StringRes
