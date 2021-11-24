03e366fce1d5a94e13f31c510b8a1a1b72990fc6
BerndMitter <Berndus@gmx.de>
2021-11-24 12:17:27 +0100

    Use right parcelize

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/transaction/DccTicketingResultItem.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/transaction/DccTicketingResultItem.kt
@@ -2,7 +2,7 @@ package de.rki.coronawarnapp.dccticketing.core.transaction
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class DccTicketingResultItem(
