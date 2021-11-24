46d8437d88bed23d399d2a1e5ab71f4dbaeb3f92
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-24 19:01:09 +0100

    Create DccTicketingCertificateFilterTest.kt

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccticketing/core/certificateselection/DccTicketingCertificateFilterTest.kt
@@ -0,0 +1,25 @@
+package de.rki.coronawarnapp.dccticketing.core.certificateselection
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import testhelpers.BaseTest
+
+internal class DccTicketingCertificateFilterTest : BaseTest() {
+
+    @BeforeEach
+    fun setUp() {
+        // TODO
+    }
+
+    @AfterEach
+    fun tearDown() {
+        // TODO
+    }
+
+    @Test
+    fun filter() {
+        // TODO
+    }
+}
