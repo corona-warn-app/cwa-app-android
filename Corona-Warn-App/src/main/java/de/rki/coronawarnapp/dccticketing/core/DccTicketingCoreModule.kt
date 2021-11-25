cd9d8e2194c2aa97d6b077827fc9239e30d58ea9
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-25 12:02:58 +0100

    different http client

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/DccTicketingCoreModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/DccTicketingCoreModule.kt
@@ -3,24 +3,51 @@ package de.rki.coronawarnapp.dccticketing.core
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
+import de.rki.coronawarnapp.BuildConfig
 import de.rki.coronawarnapp.dccticketing.core.server.DccTicketingApiV1
-import de.rki.coronawarnapp.http.HttpClientDefault
+import de.rki.coronawarnapp.http.HttpErrorParser
+import de.rki.coronawarnapp.http.config.HTTPVariables
+import de.rki.coronawarnapp.http.interceptor.RetryInterceptor
+import de.rki.coronawarnapp.http.interceptor.WebSecurityVerificationInterceptor
+import de.rki.coronawarnapp.risk.TimeVariables
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import javax.inject.Qualifier
 
 @Module
 class DccTicketingCoreModule {
 
-    @DccTicketing
+    @DccTicketingHttpClient
     @Provides
-    fun provideHttpClient(@HttpClientDefault client: OkHttpClient) = client
+    fun provideHttpClient(): OkHttpClient {
+        val interceptors: List<Interceptor> = listOf(
+            WebSecurityVerificationInterceptor(),
+            HttpLoggingInterceptor { message -> Timber.tag("OkHttp").v(message) }.apply {
+                setLevel(HttpLoggingInterceptor.Level.BODY)
+            },
+            RetryInterceptor(),
+            HttpErrorParser()
+        )
+
+        return OkHttpClient.Builder().apply {
+            connectTimeout(HTTPVariables.getHTTPConnectionTimeout(), TimeUnit.MILLISECONDS)
+            readTimeout(HTTPVariables.getHTTPReadTimeout(), TimeUnit.MILLISECONDS)
+            writeTimeout(HTTPVariables.getHTTPWriteTimeout(), TimeUnit.MILLISECONDS)
+            callTimeout(TimeVariables.getTransactionTimeout(), TimeUnit.MILLISECONDS)
+
+            interceptors.forEach { addInterceptor(it) }
+        }.build()
+    }
 
     @Reusable
     @Provides
     fun provideDccTicketingValidationApiV1(
-        @DccTicketing client: OkHttpClient,
+        @DccTicketingHttpClient client: OkHttpClient,
         gsonConverterFactory: GsonConverterFactory
     ): DccTicketingApiV1 = Retrofit.Builder()
         .client(client)
