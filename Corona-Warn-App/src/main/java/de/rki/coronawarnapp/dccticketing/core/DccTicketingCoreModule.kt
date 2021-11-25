44225d5373bc0abe0dd282844ecb112c545d34c4
BerndMitter <Berndus@gmx.de>
2021-11-25 13:37:39 +0100

    Better DccTicketingHttpClient

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/DccTicketingCoreModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/DccTicketingCoreModule.kt
@@ -4,44 +4,28 @@ import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import de.rki.coronawarnapp.dccticketing.core.server.DccTicketingApiV1
-import de.rki.coronawarnapp.http.HttpErrorParser
-import de.rki.coronawarnapp.http.config.HTTPVariables
-import de.rki.coronawarnapp.http.interceptor.RetryInterceptor
-import de.rki.coronawarnapp.http.interceptor.WebSecurityVerificationInterceptor
-import de.rki.coronawarnapp.risk.TimeVariables
-import okhttp3.Interceptor
+import de.rki.coronawarnapp.http.HttpClientDefault
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
-import javax.inject.Qualifier
 
 @Module
 class DccTicketingCoreModule {
 
     @DccTicketingHttpClient
     @Provides
-    fun provideHttpClient(): OkHttpClient {
-        val interceptors: List<Interceptor> = listOf(
-            WebSecurityVerificationInterceptor(),
-            HttpLoggingInterceptor { message -> Timber.tag("OkHttp").v(message) }.apply {
-                setLevel(HttpLoggingInterceptor.Level.BODY)
-            },
-            RetryInterceptor(),
-            HttpErrorParser()
-        )
+    fun provideHttpClient(@HttpClientDefault client: OkHttpClient): OkHttpClient = client.newBuilder().apply {
+        // Remove old logger
+        interceptors()
+            .removeAll { it is HttpLoggingInterceptor }
+            .also { Timber.tag(TAG).d("Removed old HttpLoggingInterceptor %s", it) }
 
-        return OkHttpClient.Builder().apply {
-            connectTimeout(HTTPVariables.getHTTPConnectionTimeout(), TimeUnit.MILLISECONDS)
-            readTimeout(HTTPVariables.getHTTPReadTimeout(), TimeUnit.MILLISECONDS)
-            writeTimeout(HTTPVariables.getHTTPWriteTimeout(), TimeUnit.MILLISECONDS)
-            callTimeout(TimeVariables.getTransactionTimeout(), TimeUnit.MILLISECONDS)
-
-            interceptors.forEach { addInterceptor(it) }
-        }.build()
-    }
+        HttpLoggingInterceptor { message -> Timber.tag(TAG).v(message) }
+            .apply { setLevel(HttpLoggingInterceptor.Level.BODY) }
+            .also { addInterceptor(it) }
+    }.build()
 
     @Reusable
     @Provides
@@ -59,7 +43,4 @@ class DccTicketingCoreModule {
 // Dummy base url to satisfy Retrofit ¯\_(ツ)_/¯
 private const val BASE_URL = "https://localhost.de"
 
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
-annotation class DccTicketing
+private const val TAG = "DccTicketingHttpClient"
