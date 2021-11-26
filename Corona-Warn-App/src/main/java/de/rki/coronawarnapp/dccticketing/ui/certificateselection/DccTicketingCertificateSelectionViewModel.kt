e5aace3193080c0dd568f8fcdb136a6940046e8f
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-11-26 19:36:14 +0100

    revert

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/certificateselection/DccTicketingCertificateSelectionViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/certificateselection/DccTicketingCertificateSelectionViewModel.kt
@@ -36,7 +36,7 @@ class DccTicketingCertificateSelectionViewModel @AssistedInject constructor(
 
     private suspend fun uiState(cxt: DccTicketingTransactionContext): UiState {
         val validationCondition = cxt.accessTokenPayload?.vc
-        val certificates = dccTicketingCertificateFilter.filter(validationCondition?.copy(dob = "1972-12-11", fnt = null, gnt = null))
+        val certificates = dccTicketingCertificateFilter.filter(validationCondition)
         val certificateItems = when {
             certificates.isEmpty() -> listOf(
                 DccTicketingNoValidCertificateHeaderCard.Item(validationCondition = validationCondition),
