952e47c2799db40b9e05e88c0a1afca13306eef1
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-11-29 09:57:16 +0100

    Encrypt and sign DCC (EXPOSUREAPP-10658) (#4428)
    * add cryptography
    
    * unit tests
    
    * clean up
    
    * clean up
    
    * error codes
    
    * move exception
    error logging
    
    * error codes
    
    * error codes
    
    * error codes
    
    * missing code

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/security/DccTicketingCryptography.kt
@@ -0,0 +1,75 @@
+package de.rki.coronawarnapp.dccticketing.core.security
+
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException.ErrorCode.AES_CBC_INVALID_IV
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException.ErrorCode.AES_CBC_INVALID_KEY
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException.ErrorCode.AES_CBC_NOT_SUPPORTED
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException.ErrorCode.AES_GCM_INVALID_IV
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException.ErrorCode.AES_GCM_INVALID_KEY
+import de.rki.coronawarnapp.dccticketing.core.common.DccTicketingException.ErrorCode.AES_GCM_NOT_SUPPORTED
+import de.rki.coronawarnapp.util.encryption.aes.AesCryptography
+import okio.ByteString.Companion.decodeBase64
+import timber.log.Timber
+import java.security.InvalidKeyException
+import java.security.SecureRandom
+import javax.crypto.spec.IvParameterSpec
+import javax.inject.Inject
+
+class DccTicketingCryptography @Inject constructor(
+    val aesCryptography: AesCryptography
+) {
+
+    fun encryptWithCBC(
+        key: ByteArray,
+        data: String,
+        iv: String,
+    ): encryptedData {
+        val ivValidated = iv.base64ByteArray().validateIv() ?: throw DccTicketingException(AES_CBC_INVALID_IV)
+        return try {
+            aesCryptography.encryptWithCBC(
+                key = key,
+                data = data.encodeToByteArray(),
+                iv = IvParameterSpec(ivValidated)
+            )
+        } catch (e: InvalidKeyException) {
+            Timber.e(e)
+            throw DccTicketingException(AES_CBC_INVALID_KEY)
+        } catch (e: Exception) {
+            Timber.e(e)
+            throw DccTicketingException(AES_CBC_NOT_SUPPORTED)
+        }
+    }
+
+    fun encryptWithGCM(
+        key: ByteArray,
+        data: String,
+        iv: String,
+    ): encryptedData {
+        val ivValidated = iv.base64ByteArray().validateIv() ?: throw DccTicketingException(AES_GCM_INVALID_IV)
+        return try {
+            aesCryptography.encryptWithGCM(
+                key = key,
+                data = data.encodeToByteArray(),
+                iv = ivValidated
+            )
+        } catch (e: InvalidKeyException) {
+            Timber.e(e)
+            throw DccTicketingException(AES_GCM_INVALID_KEY)
+        } catch (e: Exception) {
+            Timber.e(e)
+            throw DccTicketingException(AES_GCM_NOT_SUPPORTED)
+        }
+    }
+
+    fun generateSecureRandomKey(size: Int = 32): ByteArray {
+        val bytes = ByteArray(size)
+        SecureRandom().nextBytes(bytes)
+        return bytes
+    }
+
+    private fun ByteArray?.validateIv() = if (this != null && size == 16) this else null
+
+    private fun String.base64ByteArray() = decodeBase64()?.toByteArray()
+}
+
+typealias encryptedData = ByteArray
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/encryption/AesCryptographyTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/encryption/AesCryptographyTest.kt
@@ -1,5 +1,6 @@
-package de.rki.coronawarnapp.covidcertificate.common.cryptography
+package de.rki.coronawarnapp.util.encryption
 
+import de.rki.coronawarnapp.util.encryption.aes.AesCryptography
 import io.kotest.matchers.shouldBe
 import okio.ByteString.Companion.decodeBase64
 import org.junit.jupiter.api.Test
@@ -11,7 +12,7 @@ class AesCryptographyTest : BaseTest() {
     fun `decrypt Hello World`() {
         val key = "d56t/juMw5r4qNx1n1igs1pobUjZBT5yq0Ct7MHUuKM=".decodeBase64()!!.toByteArray()
         val encryptedString = "WFOLewp8DWqY/8IWUHEDwg==".decodeBase64()!!.toByteArray()
-        AesCryptography().decrypt(
+        AesCryptography().decryptWithCBC(
             key,
             encryptedData = encryptedString
         ) shouldBe "Hello World".toByteArray()
@@ -21,7 +22,7 @@ class AesCryptographyTest : BaseTest() {
     fun `encrypt Hello World`() {
         val key = "d56t/juMw5r4qNx1n1igs1pobUjZBT5yq0Ct7MHUuKM=".decodeBase64()!!.toByteArray()
         val unencryptedString = "Hello World".toByteArray()
-        AesCryptography().encrypt(
+        AesCryptography().encryptWithCBC(
             key,
             data = unencryptedString
         ) shouldBe "WFOLewp8DWqY/8IWUHEDwg==".decodeBase64()!!.toByteArray()
