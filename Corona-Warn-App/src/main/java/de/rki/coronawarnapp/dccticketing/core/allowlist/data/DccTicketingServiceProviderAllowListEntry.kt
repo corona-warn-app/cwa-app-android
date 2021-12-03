e20e86fe51ee568f9e855e8f7df3c68f5e474019
BerndMitter <Berndus@gmx.de>
2021-12-03 17:49:07 +0100

    SerializedName

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/allowlist/data/DccTicketingServiceProviderAllowListEntry.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/allowlist/data/DccTicketingServiceProviderAllowListEntry.kt
@@ -1,11 +1,12 @@
 package de.rki.coronawarnapp.dccticketing.core.allowlist.data
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import okio.ByteString
 
 @Parcelize
 data class DccTicketingServiceProviderAllowListEntry(
     /** The SHA-256 of the service identity endpoint of the Validation Decorator */
-    val serviceIdentityHash: ByteString
+    @SerializedName("serviceIdentityHash") val serviceIdentityHash: ByteString
 ) : Parcelable
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/allowlist/data/DccTicketingValidationServiceAllowListEntry.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/allowlist/data/DccTicketingValidationServiceAllowListEntry.kt
@@ -1,17 +1,18 @@
 package de.rki.coronawarnapp.dccticketing.core.allowlist.data
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import okio.ByteString
 
 @Parcelize
 data class DccTicketingValidationServiceAllowListEntry(
     /** A display name for the provider of the Validation Service */
-    val serviceProvider: String,
+    @SerializedName("serviceProvider") val serviceProvider: String,
 
     /** The hostname of the Validation Service */
-    val hostname: String,
+    @SerializedName("hostname") val hostname: String,
 
     /** The SHA-256 fingerprint of the certificate of the Validation Service */
-    val fingerprint256: ByteString
+    @SerializedName("fingerprint256") val fingerprint256: ByteString
 ) : Parcelable
