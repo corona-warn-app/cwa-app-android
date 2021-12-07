d58efbf1adca08ed5ee334ce96d46df40a2ce100
Juraj Kusnier <jurajkusnier@gmail.com>
2021-12-07 20:33:45 +0100

    small design fixes

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/home/HomeRulesFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/home/HomeRulesFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentTracingDetailsHomeBinding
+import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
 import setTextWithUrl
 
@@ -14,10 +15,16 @@ class HomeRulesFragment : Fragment(R.layout.fragment_tracing_details_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.faqText.setTextWithUrl(
-            R.string.risk_details_minimize_risk_faq,
-            R.string.risk_details_minimize_risk_faq_label,
-            R.string.risk_details_minimize_risk_faq_link
-        )
+
+        with(binding) {
+            faqText.setTextWithUrl(
+                R.string.risk_details_minimize_risk_faq,
+                R.string.risk_details_minimize_risk_faq_label,
+                R.string.risk_details_minimize_risk_faq_link
+            )
+            toolbar.setNavigationOnClickListener {
+                popBackStack()
+            }
+        }
     }
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/hygiene/HygieneRulesFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/hygiene/HygieneRulesFragment.kt
@@ -1,6 +1,23 @@
 package de.rki.coronawarnapp.tracing.ui.details.hygiene
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FragmentTracingDetailsHygieneBinding
+import de.rki.coronawarnapp.util.ui.popBackStack
+import de.rki.coronawarnapp.util.ui.viewBinding
 
-class HygieneRulesFragment : Fragment(R.layout.fragment_tracing_details_hygiene)
+class HygieneRulesFragment : Fragment(R.layout.fragment_tracing_details_hygiene) {
+    private val binding: FragmentTracingDetailsHygieneBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            toolbar.setNavigationOnClickListener {
+                popBackStack()
+            }
+        }
+    }
+}
