3d887633fce1d7ef407c61fd5da60d1ba13cc125
BerndMitter <Berndus@gmx.de>
2021-12-07 20:27:48 +0100

    Implemented server cert check against an allow list

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/check/DccTicketingServerCertificateChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/core/check/DccTicketingServerCertificateChecker.kt
@@ -1,6 +1,7 @@
 package de.rki.coronawarnapp.dccticketing.core.check
 
 import dagger.Reusable
+import de.rki.coronawarnapp.dccticketing.core.allowlist.data.DccTicketingValidationServiceAllowListEntry
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccJWK
 import de.rki.coronawarnapp.dccticketing.core.check.DccTicketingServerCertificateCheckException.ErrorCode
 import de.rki.coronawarnapp.dccticketing.core.common.DccJWKConverter
@@ -44,7 +45,7 @@ class DccTicketingServerCertificateChecker @Inject constructor(
 
         // 6. Compare fingerprints
         when (requiredFingerprints.contains(leafCertificate.createSha256Fingerprint())) {
-            true -> Timber.d("Certificate check was successful")
+            true -> Timber.d("Certificate check was successful against jwk set=%s", jwkSet)
             false -> throw DccTicketingServerCertificateCheckException(ErrorCode.CERT_PIN_MISMATCH)
         }
     } catch (e: Exception) {
@@ -56,6 +57,61 @@ class DccTicketingServerCertificateChecker @Inject constructor(
             }
         }
     }
+
+    /**
+     * Checks the server certificate against a set of [DccTicketingValidationServiceAllowListEntry]
+     *
+     * @throws [DccTicketingServerCertificateCheckException] if the check fails
+     *
+     * Note that the absence of an error code indicates a successful check
+     */
+    fun checkCertificate(
+        hostname: String,
+        certificateChain: List<Certificate>,
+        allowList: Set<DccTicketingValidationServiceAllowListEntry>
+    ) = try {
+        Timber.d(
+            "checkCertificate(hostname=%s, certificateChain=%s, allowList=%s)",
+            hostname,
+            certificateChain,
+            allowList
+        )
+
+        // 1. Extract leafCertificate
+        val leafCertificate = certificateChain.first()
+
+        // 2. Find requiredFingerprints
+        val requiredFingerprintsHostnameMap = allowList.map { it.fingerprint256 to it.hostname }.toMap()
+
+        // 3. Compare fingerprints
+        val leafCertificateFingerprint = leafCertificate.createSha256Fingerprint()
+        val matchingFingerprintsHostnameMap =
+            requiredFingerprintsHostnameMap.filterKeys { it == leafCertificateFingerprint }
+
+        if (matchingFingerprintsHostnameMap.isEmpty()) {
+            throw DccTicketingServerCertificateCheckException(errorCode = ErrorCode.CERT_PIN_MISMATCH)
+        }
+
+        // 4. Find requiredHostnames
+        val requiredHostnames = matchingFingerprintsHostnameMap.values
+
+        // 5. Compare hostnames
+        when (requiredHostnames.contains(hostname)) {
+            true -> Timber.d("Certificate check was successful against allowlist=%s", allowList)
+            false -> throw DccTicketingServerCertificateCheckException(errorCode = ErrorCode.CERT_PIN_HOST_MISMATCH)
+        }
+    } catch (e: Exception) {
+        throw when (e) {
+            is DccTicketingServerCertificateCheckException -> e
+            else -> {
+                Timber.w(e, "Certificate check failed with an unspecified error. Needs further investigation!")
+                DccTicketingServerCertificateCheckException(errorCode = ErrorCode.CERT_PIN_MISMATCH, cause = e)
+            }
+        }
+    }
+
+    private fun Certificate.createSha256Fingerprint(): ByteString = encoded.toByteString()
+        .sha256()
 
     // Takes the first 8 bytes of the SHA-256 fingerprint of the certificate and encodes them with base64
     private fun Certificate.createKid(): String = createSha256Fingerprint()
@@ -74,7 +130,5 @@ class DccTicketingServerCertificateChecker @Inject constructor(
         return requiredJwkSet
     }
 }
-
-fun Certificate.createSha256Fingerprint(): ByteString = encoded.toByteString().sha256()
 
 private const val BYTE_COUNT: Int = 8
