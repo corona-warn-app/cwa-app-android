453e50789741e1cf0d708adf62fbfd718d8ef18a
BMItr <Berndus@gmx.de>
2021-12-15 17:07:29 +0100

    Advanced root warning (EXPOSUREAPP-10698) (#4550)
    * Added checkbox to root dialog
    
    * Added root detection view model
    
    * Suppress root info if requested
    
    * Whooopsie
    
    * Adjusted test
    
    * Updated test
    
    * ktlint
    
    * Fix launcher test
    
    * Comments
    
    Co-authored-by: Marc Auberer <marc.auberer@chillibits.com>
    Co-authored-by: Juraj Kusnier <jurajkusnier@users.noreply.github.com>
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/rootdetection/ui/RootDetectionDialogFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/rootdetection/ui/RootDetectionDialogFragment.kt
@@ -1,7 +1,8 @@
-package de.rki.coronawarnapp.rootdetection
+package de.rki.coronawarnapp.rootdetection.ui
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
@@ -10,8 +11,15 @@ import androidx.fragment.app.setFragmentResult
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.util.ExternalActionHelper.openUrl
+import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModels
+import javax.inject.Inject
 
-class RootDetectionDialogFragment : DialogFragment() {
+class RootDetectionDialogFragment : DialogFragment(), AutoInject {
+
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val vm: RootDetectionDialogViewModel by cwaViewModels { viewModelFactory }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = MaterialAlertDialogBuilder(requireContext())
         .setTitle(R.string.root_dialog_title)
@@ -20,13 +28,21 @@ class RootDetectionDialogFragment : DialogFragment() {
             setFragmentResult(REQUEST_KEY, bundleOf())
         }
         .setNegativeButton(R.string.root_dialog_faq_link_label, null)
+        .setView(R.layout.root_detection_dialog_checkbox_view)
         .create()
         .also { dialog ->
-            dialog.setOnShowListener {
-                dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
-                    .setOnClickListener { openUrl(R.string.root_dialog_faq_link_url) }
-            }
+            dialog.addListeners()
         }
+
+    private fun AlertDialog.addListeners() = setOnShowListener {
+        getButton(AlertDialog.BUTTON_NEGATIVE)
+            .setOnClickListener { openUrl(R.string.root_dialog_faq_link_url) }
+
+        val checkbox = findViewById<CheckBox>(R.id.checkbox)
+        checkbox?.setOnCheckedChangeListener { _, isChecked ->
+            vm.onSuppressCheckedChanged(isChecked = isChecked)
+        }
+    }
 
     companion object {
         fun newInstance() = RootDetectionDialogFragment()
