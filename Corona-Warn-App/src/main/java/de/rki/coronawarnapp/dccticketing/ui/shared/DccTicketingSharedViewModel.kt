a8c9435c6c259d18eb93665757d5dfde170eae66
Lukas Lechner <office@lukaslechner.com>
2021-12-16 12:55:51 +0100

    Save transaction context in SavedStateHandle of DccTicketingSharedViewModel

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/shared/DccTicketingSharedViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/shared/DccTicketingSharedViewModel.kt
@@ -1,5 +1,7 @@
 package de.rki.coronawarnapp.dccticketing.ui.shared
 
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import de.rki.coronawarnapp.dccticketing.core.transaction.DccTicketingTransactionContext
 import kotlinx.coroutines.flow.Flow
@@ -10,14 +12,31 @@ import timber.log.Timber
 /**
  * Shares the [DccTicketingTransactionContext] between all DccTicketing destinations.
  */
-class DccTicketingSharedViewModel : ViewModel() {
+class DccTicketingSharedViewModel(private val savedState: SavedStateHandle) : ViewModel() {
 
-    private val currentTransactionContext = MutableStateFlow<DccTicketingTransactionContext?>(null)
+    private val currentTransactionContext = MutableStateFlow<DccTicketingTransactionContext?>(
+        getSavedTransactionContext()
+    )
 
     val transactionContext: Flow<DccTicketingTransactionContext> = currentTransactionContext.filterNotNull()
 
     fun updateTransactionContext(ctx: DccTicketingTransactionContext) {
         Timber.d("updateTransactionContext(ctx=%s)", ctx)
-        currentTransactionContext.tryEmit(ctx).also { Timber.d("Update was successful? %s", it) }
+        currentTransactionContext.tryEmit(ctx).also {
+            Timber.d("Update was successful? %s", it)
+            saveTransactionContext(ctx)
+        }
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    fun getSavedTransactionContext(): DccTicketingTransactionContext? =
+        savedState.get(TRANSACTION_CONTEXT_SAVED_STATE_KEY)
+
+    private fun saveTransactionContext(ctx: DccTicketingTransactionContext) {
+        savedState.set(TRANSACTION_CONTEXT_SAVED_STATE_KEY, ctx)
+    }
+
+    companion object {
+        private const val TRANSACTION_CONTEXT_SAVED_STATE_KEY = "transaction_context_saved_state_key"
     }
 }
