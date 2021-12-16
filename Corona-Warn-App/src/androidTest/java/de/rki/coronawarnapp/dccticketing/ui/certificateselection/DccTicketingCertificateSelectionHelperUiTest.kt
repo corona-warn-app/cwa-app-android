916624037e1fd5420ffb134e5889dd387be49b25
Marc Auberer <marc.auberer@chillibits.com>
2021-12-16 11:07:17 +0100

    Fix duplicate certificate list item (EXPOSUREAPP-11052) (#4564)
    * Fix duplicate certificate list item
    
    * Fix wildcard import
    
    * Lint
    
    * Some tests
    
    * Update DccTicketingCertificateSelectionHelperUiTest.kt
    
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>
    Co-authored-by: Juraj Kusnier <jurajkusnier@users.noreply.github.com>

---
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/dccticketing/ui/certificateselection/DccTicketingCertificateSelectionHelperUiTest.kt
@@ -0,0 +1,64 @@
+package de.rki.coronawarnapp.dccticketing.ui.certificateselection
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import de.rki.coronawarnapp.R
+import io.kotest.matchers.shouldBe
+import org.junit.Test
+import testhelpers.BaseTestInstrumentation
+
+class DccTicketingCertificateSelectionHelperUiTest : BaseTestInstrumentation() {
+
+    private val context by lazy { ApplicationProvider.getApplicationContext<Context>() }
+
+    @Test
+    fun certificateTypesText() = with(context) {
+        certificateTypesText(listOf("v", "r", "t", "tp", "tr")) shouldBe
+            listOf(
+                getString(R.string.vaccination_certificate_name),
+                getString(R.string.recovery_certificate_name),
+                getString(R.string.rat_test_certificate),
+                getString(R.string.pcr_test_certificate)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf("v", "r", "tp")) shouldBe
+            listOf(
+                getString(R.string.vaccination_certificate_name),
+                getString(R.string.recovery_certificate_name),
+                getString(R.string.pcr_test_certificate)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf("v", "r", "tr")) shouldBe
+            listOf(
+                getString(R.string.vaccination_certificate_name),
+                getString(R.string.recovery_certificate_name),
+                getString(R.string.rat_test_certificate)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf("v", "r")) shouldBe
+            listOf(
+                getString(R.string.vaccination_certificate_name),
+                getString(R.string.recovery_certificate_name)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf("r")) shouldBe
+            listOf(
+                getString(R.string.recovery_certificate_name)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf("v")) shouldBe
+            listOf(
+                getString(R.string.vaccination_certificate_name)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf("v", "r", "tp", "tr")) shouldBe
+            listOf(
+                getString(R.string.vaccination_certificate_name),
+                getString(R.string.recovery_certificate_name),
+                getString(R.string.pcr_test_certificate),
+                getString(R.string.rat_test_certificate)
+            ).joinToString(", ")
+
+        certificateTypesText(listOf()) shouldBe ""
+    }
+}
