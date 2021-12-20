319ea4fe639ac0206b335a59fe8da7b3c2d2790d
BerndMitter <Berndus@gmx.de>
2021-12-20 14:30:07 +0100

    clean

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/shared/DccTicketingSharedViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccticketing/ui/shared/DccTicketingSharedViewModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 /**
