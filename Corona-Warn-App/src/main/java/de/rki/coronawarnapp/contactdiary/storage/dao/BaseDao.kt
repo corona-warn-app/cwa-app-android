2a4eebcb8eb471f164fc66b67ca9545fab71c2f2
Juraj Kusnier <jurajkusnier@users.noreply.github.com>
2021-12-21 15:46:02 +0100

    FIx: Pop Up in Contact Journal (EXPOSUREAPP-11079) (#4593)
    * fix null pointer exception
    
    * add comments
    
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>
    Co-authored-by: Marc Auberer <marc.auberer@chillibits.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/storage/dao/BaseDao.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/contactdiary/storage/dao/BaseDao.kt
@@ -10,6 +10,6 @@ interface BaseDao<in T, out U> {
     suspend fun delete(entity: T)
     suspend fun delete(entities: List<T>)
     suspend fun deleteAll()
-    suspend fun entityForId(id: Long): U
+    suspend fun entityForId(id: Long): U?
     fun allEntries(): Flow<List<U>>
 }
