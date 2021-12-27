1ceae372b9c6d07acdae60088ec9db63b9cd3fca
Mohamed Metwalli <mohamed.metwalli@sap.com>
2021-12-27 12:50:56 +0100

    Autofocus on older versions

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/qrcode/ui/QrCodeScannerPreviewView.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/qrcode/ui/QrCodeScannerPreviewView.kt
@@ -1,23 +1,36 @@
 package de.rki.coronawarnapp.qrcode.ui
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.RelativeLayout
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.FocusMeteringAction
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceOrientedMeteringPointFactory
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.window.WindowManager
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.tag
+import de.rki.coronawarnapp.util.BuildVersionWrap
+import de.rki.coronawarnapp.util.flow.intervalFlow
+import de.rki.coronawarnapp.util.lessThanAPILevel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
 import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -31,12 +44,14 @@ class QrCodeScannerPreviewView @JvmOverloads constructor(
     private var cameraProvider: ProcessCameraProvider? = null
     private var camera: Camera? = null
     private val cameraExecutor = Executors.newSingleThreadExecutor()
+    private val windowManager: WindowManager
 
     var scanEnabled = true
 
     init {
         inflate(context, R.layout.qr_code_scanner_preview_view, this)
         cameraPreview = findViewById(R.id.camera_preview)
+        windowManager = WindowManager(context)
     }
 
     fun enableTorch(enable: Boolean) {
@@ -53,12 +68,30 @@ class QrCodeScannerPreviewView @JvmOverloads constructor(
             },
             ContextCompat.getMainExecutor(context)
         )
+
+        setupAutofocus(lifecycleOwner)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        Timber.tag(TAG).d("Shutting down camera executor")
+        cameraExecutor.shutdown()
+    }
+
+    private fun setupAutofocus(lifecycleOwner: LifecycleOwner) {
+        if (BuildVersionWrap.lessThanAPILevel(Build.VERSION_CODES.O)) {
+            lifecycleOwner.lifecycleScope.launchWhenStarted {
+                intervalFlow(1_000)
+                    .onEach { autoFocus() }
+                    .catch { Timber.tag(TAG).e(it, "Autofocus failed") }
+                    .collect()
+            }
+        }
     }
 
     private fun bindUseCases(lifecycleOwner: LifecycleOwner, onImageCallback: (ImageProxy) -> Unit) {
         Timber.tag(TAG).d("Binding camera use cases")
         // Get screen metrics used to setup camera for full screen resolution
-        val windowManager = WindowManager(context)
         val metrics = windowManager.getCurrentWindowMetrics().bounds
         Timber.tag(TAG).d("Screen metrics: ${metrics.width()} x ${metrics.height()}")
 
@@ -123,10 +156,47 @@ class QrCodeScannerPreviewView @JvmOverloads constructor(
         return AspectRatio.RATIO_16_9
     }
 
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        Timber.tag(TAG).d("Shutting down camera executor")
-        cameraExecutor.shutdown()
+    private suspend fun autoFocus() {
+        tapToFocus()
+
+        val bounds = windowManager.getCurrentWindowMetrics().bounds
+        val focusPoint = SurfaceOrientedMeteringPointFactory(
+            bounds.width().toFloat(),
+            bounds.height().toFloat()
+        ).createPoint(
+            bounds.exactCenterX(),
+            bounds.exactCenterY()
+        )
+
+        val focusAction = FocusMeteringAction.Builder(focusPoint, FocusMeteringAction.FLAG_AF).build()
+        camera?.cameraControl?.startFocusAndMetering(focusAction)?.let { future ->
+            suspendCoroutine<Unit> { continuation ->
+                future.addListener(
+                    { continuation.resume(Unit) },
+                    ContextCompat.getMainExecutor(context),
+                )
+            }
+        }
+    }
+
+    private fun tapToFocus() {
+        cameraPreview.setOnTouchListener { view, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> true
+                MotionEvent.ACTION_UP -> {
+                    val focusPoint = SurfaceOrientedMeteringPointFactory(view.width.toFloat(), view.height.toFloat())
+                        .createPoint(event.x, event.y)
+
+                    camera?.cameraControl?.startFocusAndMetering(
+                        FocusMeteringAction.Builder(focusPoint, FocusMeteringAction.FLAG_AF)
+                            .disableAutoCancel()
+                            .build()
+                    )
+                    view.performClick()
+                }
+                else -> false
+            }
+        }
     }
 
     companion object {
