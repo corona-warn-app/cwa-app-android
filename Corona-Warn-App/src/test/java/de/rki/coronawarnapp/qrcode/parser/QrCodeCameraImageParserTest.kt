27698798344ead41a52f59a694790bc46cd8d382
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2021-12-28 11:53:12 +0100

    add test image

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/qrcode/parser/QrCodeCameraImageParserTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/qrcode/parser/QrCodeCameraImageParserTest.kt
@@ -2,14 +2,17 @@ package de.rki.coronawarnapp.qrcode.parser
 
 import boofcv.struct.image.GrayU8
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
 import testhelpers.asDispatcherProvider
+import testhelpers.coroutines.runBlockingTest2
+import java.io.File
+import java.nio.file.InvalidPathException
 
 class QrCodeCameraImageParserTest : BaseTest() {
 
@@ -27,11 +30,45 @@ class QrCodeCameraImageParserTest : BaseTest() {
     @Test
     fun `finds nothing`() {
         val image = createEmptyGrayU8(80, 80)
-
-        testDispatcher.runBlockingTest {
+        runBlockingTest2(ignoreActive = true) {
             parser.parseQrCode(image)
             parser.rawResults.firstOrNull() shouldBe null
         }
+    }
+
+    @Test
+    fun `finds certificate`() {
+        val path = File("./src/test/dcc-test-data")
+        if (path.exists()) lookForImagesAndDecode(path)
+        else throw InvalidPathException("./src/test/dcc-test-data", "Directory with test data is missing")
+    }
+
+    private fun lookForImagesAndDecode(file: File) {
+        if (file.isDirectory) {
+            file.listFiles()?.forEach(::lookForImagesAndDecode)
+        } else if (file.isFile && file.isImage()) {
+            val image = Image(file).toGrayU8()
+            runBlockingTest2(ignoreActive = true) {
+                parser.parseQrCode(image)
+                parser.rawResults.first() shouldBe "HC1:6BFOXN*TS0BI\$ZDFRH5TSWK3*93X+0/R8+C1NDC2LE \$C3I9Y1Q20JDJ54-S2UBF/8X*G-O9UVPQRHIY1VS1NQ1 WUQRELS4 CT3SVI\$21\$4Y/K/*K+2T++K+*4KCTO%K4F7TEF+*4U70H:9\$.FE3KZD5CC9T0HM%2CNNK1H--8SGH.+HAMI PQVW5/O16%HAT1Z%PHOP+MMBT16Y5TU1AT1SU9ZIEQKERQ8IY1I\$H3:U8 9QS5/IECN5U.RTOE2QE2K5N%EQJARMA0THWM6J\$7XLH5G6TH9YJA*LA/CJ-LH/CJ6IAXPMHQ1*P14W19UERU9:PIOEG499FQ5VA131A.V56GAM3Q/RQJZI+EBR3E%JTQOL200GTVQWRSAGOD6P+1746.FE%S6Z H V66VEP-1WJHMN1JAVY+M TUBBEQ6C+XCGZEEVU8+N 4FW5B+UVA1M7TUY2V3JH2QPJ2AR8Q14WIUM-J6BYSJ%PVCSPXL9O5+XB6Q7+K8DZHC9JZRKK:3 CD-7J78B%8FS40L%P-3"
+            }
+        }
+    }
+
+    internal fun Image.toGrayU8(): GrayU8 {
+        // Add a white border around the image because BoofCV otherwise fails finding the QR code
+        val pixels = IntArray(width * height)
+        val border = 15
+        val data = createEmptyGrayU8(width + 2 * border, height + 2 * border)
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val argb = pixels[x + y * width].toUInt()
+                data[x + border, y + border] =
+                    if (argb and 0xff000000U == 0U) 0xffffffffU.toInt() else argb.toInt()
+            }
+        }
+
+        return data
     }
 
     private fun createEmptyGrayU8(width: Int, height: Int): GrayU8 {
@@ -43,4 +80,37 @@ class QrCodeCameraImageParserTest : BaseTest() {
         }
         return data
     }
+
+    private fun File.isImage() =
+        listOf(".png", ".jpg", ".jpeg").any { name.endsWith(it) }
 }
+
+internal class Image(private val file: File) {
+    val width: Int
+    val height: Int
+    val pixels: IntArray
+
+    init {
+        // XXX:
+        //  In Android unit tests BitmapFactory doesn't work correctly and reads all-white images.
+        //  ImageIO can't be imported, but reflection works and it's able to read images correctly.
+        val image = readMethod.invoke(imageIO, file.toURI().toURL())
+        val getWidth = image::class.java.getDeclaredMethod("getWidth")
+        val getHeight = image::class.java.getDeclaredMethod("getHeight")
+        val getRGB = image::class.java.getDeclaredMethod("getRGB", Int::class.java, Int::class.java)
+        width = getWidth.invoke(image) as Int
+        height = getHeight.invoke(image) as Int
+        pixels = IntArray(width * height)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                pixels[x + width * y] = getRGB.invoke(image, x, y) as Int
+            }
+        }
+    }
+
+    companion object {
+        val imageIO = Class.forName("javax.imageio.ImageIO")
+        val readMethod = imageIO.getDeclaredMethod("read", java.net.URL::class.java)
+    }
+}
+
