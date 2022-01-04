c9683f1d2859fa261e157011f28af8defee9b7ae
BerndMitter <Berndus@gmx.de>
2022-01-04 14:51:58 +0100

    ktlint test clean

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/qrcode/parser/QrCodeCameraImageParserTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/qrcode/parser/QrCodeCameraImageParserTest.kt
@@ -16,8 +16,8 @@ class QrCodeCameraImageParserTest : BaseTest() {
     @Test
     fun `finds nothing`() {
         runBlockingTest2(ignoreActive = true) {
-            //parser.parseQrCode(emptyImage)
-            //parser.rawResults.firstOrNull() shouldBe null
+            // parser.parseQrCode(emptyImage)
+            // parser.rawResults.firstOrNull() shouldBe null
         }
     }
 
@@ -28,17 +28,16 @@ class QrCodeCameraImageParserTest : BaseTest() {
         else throw InvalidPathException(TEST_DATA_DIRECTORY, "Directory with test data is missing")
     }
 
-
     private fun parseImageFile(file: File) {
         if (file.isDirectory) {
             file.listFiles()?.forEach(::parseImageFile)
         } else if (file.isFile && file.isImage()) {
             val image = Image(file)
             runBlockingTest2(ignoreActive = true) {
-                //parser.parseQrCode(emptyImage)
-                //parser.parseQrCode(image)
-                //parser.parseQrCode(emptyImage)
-                //parser.rawResults.first() shouldBe "HC1:6BFOXN*TS0BI\$ZDFRH5TSWK3*93X+0/R8+C1NDC2LE \$C3I9Y1Q20JDJ54-S2UBF/8X*G-O9UVPQRHIY1VS1NQ1 WUQRELS4 CT3SVI\$21\$4Y/K/*K+2T++K+*4KCTO%K4F7TEF+*4U70H:9\$.FE3KZD5CC9T0HM%2CNNK1H--8SGH.+HAMI PQVW5/O16%HAT1Z%PHOP+MMBT16Y5TU1AT1SU9ZIEQKERQ8IY1I\$H3:U8 9QS5/IECN5U.RTOE2QE2K5N%EQJARMA0THWM6J\$7XLH5G6TH9YJA*LA/CJ-LH/CJ6IAXPMHQ1*P14W19UERU9:PIOEG499FQ5VA131A.V56GAM3Q/RQJZI+EBR3E%JTQOL200GTVQWRSAGOD6P+1746.FE%S6Z H V66VEP-1WJHMN1JAVY+M TUBBEQ6C+XCGZEEVU8+N 4FW5B+UVA1M7TUY2V3JH2QPJ2AR8Q14WIUM-J6BYSJ%PVCSPXL9O5+XB6Q7+K8DZHC9JZRKK:3 CD-7J78B%8FS40L%P-3"
+                // parser.parseQrCode(emptyImage)
+                // parser.parseQrCode(image)
+                // parser.parseQrCode(emptyImage)
+                // parser.rawResults.first() shouldBe "HC1:6BFOXN*TS0BI\$ZDFRH5TSWK3*93X+0/R8+C1NDC2LE \$C3I9Y1Q20JDJ54-S2UBF/8X*G-O9UVPQRHIY1VS1NQ1 WUQRELS4 CT3SVI\$21\$4Y/K/*K+2T++K+*4KCTO%K4F7TEF+*4U70H:9\$.FE3KZD5CC9T0HM%2CNNK1H--8SGH.+HAMI PQVW5/O16%HAT1Z%PHOP+MMBT16Y5TU1AT1SU9ZIEQKERQ8IY1I\$H3:U8 9QS5/IECN5U.RTOE2QE2K5N%EQJARMA0THWM6J\$7XLH5G6TH9YJA*LA/CJ-LH/CJ6IAXPMHQ1*P14W19UERU9:PIOEG499FQ5VA131A.V56GAM3Q/RQJZI+EBR3E%JTQOL200GTVQWRSAGOD6P+1746.FE%S6Z H V66VEP-1WJHMN1JAVY+M TUBBEQ6C+XCGZEEVU8+N 4FW5B+UVA1M7TUY2V3JH2QPJ2AR8Q14WIUM-J6BYSJ%PVCSPXL9O5+XB6Q7+K8DZHC9JZRKK:3 CD-7J78B%8FS40L%P-3"
             }
         }
     }
@@ -71,8 +70,6 @@ class QrCodeCameraImageParserTest : BaseTest() {
 
     private fun File.isImage() =
         listOf(".png", ".jpg", ".jpeg").any { name.endsWith(it) }
-
-
 }
 
 private class Image(file: File) {
