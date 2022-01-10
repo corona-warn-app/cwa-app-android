d5bb28127dbb5275fc16366ebfd8733c92250f9e
Juraj Kusnier <jurajkusnier@gmail.com>
2022-01-10 15:26:55 +0100

    add FindDetailsInJournalBoxIncreasedRisk

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/items/additionalinfos/FindDetailsInJournalBox.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/items/additionalinfos/FindDetailsInJournalBox.kt
@@ -1,11 +1,9 @@
 package de.rki.coronawarnapp.tracing.ui.details.items.additionalinfos
 
-import android.content.Context
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.TracingDetailsFindDetailsInJournalBinding
-import de.rki.coronawarnapp.risk.RiskState
 import de.rki.coronawarnapp.tracing.ui.details.TracingDetailsAdapter
 import de.rki.coronawarnapp.tracing.ui.details.items.additionalinfos.FindDetailsInJournalBox.Item
 
@@ -28,16 +26,7 @@ class FindDetailsInJournalBox(
     override val onBindData: TracingDetailsFindDetailsInJournalBinding.(
         item: Item,
         payloads: List<Any>
-    ) -> Unit = { item, _ ->
-        state = item
-    }
+    ) -> Unit = { _, _ -> }
 
-    data class Item(
-        val riskState: RiskState
-    ) : AdditionalInformationItem {
-        fun getText(context: Context): String = if (riskState == RiskState.INCREASED_RISK)
-            context.getString(R.string.risk_details_find_details_in_journal_increased_risk)
-        else
-            context.getString(R.string.risk_details_find_details_in_journal)
-    }
+    object Item : AdditionalInformationItem
 }
---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/details/items/additionalinfos/FindDetailsInJournalBoxIncreasedRisk.kt
@@ -0,0 +1,32 @@
+package de.rki.coronawarnapp.tracing.ui.details.items.additionalinfos
+
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.TracingDetailsFindDetailsInJournalIncreasedRiskBinding
+import de.rki.coronawarnapp.tracing.ui.details.TracingDetailsAdapter
+import de.rki.coronawarnapp.tracing.ui.details.items.additionalinfos.FindDetailsInJournalBoxIncreasedRisk.Item
+
+class FindDetailsInJournalBoxIncreasedRisk(
+    parent: ViewGroup,
+    @LayoutRes containerLayout: Int = R.layout.tracing_details_find_details_in_journal_increased_risk
+) : TracingDetailsAdapter.DetailsItemVH<Item, TracingDetailsFindDetailsInJournalIncreasedRiskBinding>(
+    containerLayout,
+    parent
+) {
+
+    override val viewBinding = lazy {
+        TracingDetailsFindDetailsInJournalIncreasedRiskBinding.inflate(
+            layoutInflater,
+            itemView.findViewById(R.id.box_container),
+            true
+        )
+    }
+
+    override val onBindData: TracingDetailsFindDetailsInJournalIncreasedRiskBinding.(
+        item: Item,
+        payloads: List<Any>
+    ) -> Unit = { _, _ -> }
+
+    object Item : AdditionalInformationItem
+}
