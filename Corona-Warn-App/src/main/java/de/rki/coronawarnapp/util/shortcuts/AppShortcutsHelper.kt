e821e5056769fe898c4a77f707dd6049fd4aefb4
dodekaphilist <31287077+dodekaphilist@users.noreply.github.com>
2022-01-12 13:15:24 +0100

    Add app shortcuts to primary targets (closes #2858) (#4642)
    * Bump to next Version 1.11.0.8 (DEV) #2220
    
    * Add app shortcuts to primary targets
    
    Fixes #2858, also adds the other targets from the bottom bar menu
    
    Please validate changes for potential problems
    
    * Update AppShortcutsHelper.kt
    
    * Klint fix & labels updated
    
    * Update AppShortcutsHelper.kt
    
    Co-authored-by: Matthias Urhahn <matthias.urhahn@sap.com>
    Co-authored-by: CWA Technical User <67319337+Corona-Warn-App-Technical-User@users.noreply.github.com>
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>
    Co-authored-by: Marc Auberer <marc.auberer@chillibits.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/shortcuts/AppShortcutsHelper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/shortcuts/AppShortcutsHelper.kt
@@ -18,30 +18,67 @@ import javax.inject.Singleton
 class AppShortcutsHelper @Inject constructor(@AppContext private val context: Context) {
 
     suspend fun restoreAppShortcut() = withContext(Dispatchers.IO) {
-        val shortcut = ShortcutInfoCompat.Builder(context, CONTACT_DIARY_SHORTCUT_ID)
+        val shortcutScanner = ShortcutInfoCompat.Builder(context, QR_CODE_SCANNER_SHORTCUT_ID)
+            .setShortLabel(context.getString(R.string.app_shortcut_scanner_title))
+            .setLongLabel(context.getString(R.string.app_shortcut_scanner_title))
+            .setIcon(IconCompat.createWithResource(context, R.drawable.ic_qr_code_scanner_shortcut_icon))
+            .setIntent(createShortcutIntent(AppShortcuts.QR_CODE_SCANNER.toString()))
+            .build()
+
+        val shortcutCertificates = ShortcutInfoCompat.Builder(context, COVID_CERTIFICATES_SHORTCUT_ID)
+            .setShortLabel(context.getString(R.string.app_shortcut_certificates_title))
+            .setLongLabel(context.getString(R.string.app_shortcut_certificates_title))
+            .setIcon(IconCompat.createWithResource(context, R.drawable.ic_certificates_shortcut_icon))
+            .setIntent(createShortcutIntent(AppShortcuts.CERTIFICATES.toString()))
+            .build()
+
+        val shortcutCheckIns = ShortcutInfoCompat.Builder(context, CHECK_INS_SHORTCUT_ID)
+            .setShortLabel(context.getString(R.string.app_shortcut_check_ins_title))
+            .setLongLabel(context.getString(R.string.app_shortcut_check_ins_title))
+            .setIcon(IconCompat.createWithResource(context, R.drawable.ic_check_ins_shortcut_icon))
+            .setIntent(createShortcutIntent(AppShortcuts.CHECK_INS.toString()))
+            .build()
+
+        val shortcutDiary = ShortcutInfoCompat.Builder(context, CONTACT_DIARY_SHORTCUT_ID)
             .setShortLabel(context.getString(R.string.app_shortcut_contact_diary_title))
             .setLongLabel(context.getString(R.string.app_shortcut_contact_diary_title))
             .setIcon(IconCompat.createWithResource(context, R.drawable.ic_contact_diary_shortcut_icon))
-            .setIntent(createContactDiaryIntent())
+            .setIntent(createShortcutIntent(AppShortcuts.CONTACT_DIARY.toString()))
             .build()
 
-        if (ShortcutManagerCompat.getDynamicShortcuts(context).size == 0) {
-            ShortcutManagerCompat.addDynamicShortcuts(context, listOf(shortcut))
-        } else {
-            ShortcutManagerCompat.updateShortcuts(context, listOf(shortcut))
+        val shortcutList = listOf(
+            shortcutDiary,
+            shortcutCheckIns,
+            shortcutCertificates,
+            shortcutScanner
+        )
+
+        for (shortcut in shortcutList) {
+            ShortcutManagerCompat.pushDynamicShortcut(context, shortcut)
         }
     }
 
     fun removeAppShortcut() {
-        ShortcutManagerCompat.removeDynamicShortcuts(context, listOf(CONTACT_DIARY_SHORTCUT_ID))
+        ShortcutManagerCompat.removeDynamicShortcuts(
+            context,
+            listOf(
+                QR_CODE_SCANNER_SHORTCUT_ID,
+                COVID_CERTIFICATES_SHORTCUT_ID,
+                CHECK_INS_SHORTCUT_ID,
+                CONTACT_DIARY_SHORTCUT_ID
+            )
+        )
     }
 
-    private fun createContactDiaryIntent() = Intent(context, LauncherActivity::class.java).apply {
+    private fun createShortcutIntent(shortcut: String) = Intent(context, LauncherActivity::class.java).apply {
         action = Intent.ACTION_VIEW
-        putExtra(SHORTCUT_EXTRA, AppShortcuts.CONTACT_DIARY.toString())
+        putExtra(SHORTCUT_EXTRA, shortcut)
     }
 
     companion object {
+        private const val QR_CODE_SCANNER_SHORTCUT_ID = "scanner_id"
+        private const val COVID_CERTIFICATES_SHORTCUT_ID = "certificates_id"
+        private const val CHECK_INS_SHORTCUT_ID = "check_ins_id"
         private const val CONTACT_DIARY_SHORTCUT_ID = "contact_diary_id"
         const val SHORTCUT_EXTRA = "shortcut_extra"
 
