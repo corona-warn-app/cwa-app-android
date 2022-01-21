8d4ffb9d2c3646758adff8558d16c9bc1d694cb6
Juraj Kusnier <jurajkusnier@users.noreply.github.com>
2022-01-21 11:00:11 +0100

    refresh dscRepository in CovidCertificateOnboardingViewModel (#4698)
    Co-authored-by: BMItr <Berndus@gmx.de>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/ui/onboarding/CovidCertificateOnboardingViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/ui/onboarding/CovidCertificateOnboardingViewModel.kt
@@ -5,12 +5,16 @@ import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.covidcertificate.common.qrcode.DccQrCode
 import de.rki.coronawarnapp.covidcertificate.common.repository.CertificateContainerId
+import de.rki.coronawarnapp.covidcertificate.signature.core.DscRepository
 import de.rki.coronawarnapp.covidcertificate.vaccination.core.CovidCertificateSettings
 import de.rki.coronawarnapp.qrcode.handler.DccQrCodeHandler
+import de.rki.coronawarnapp.util.TimeStamper
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
 import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactory
+import kotlinx.coroutines.flow.first
+import org.joda.time.Duration
 import timber.log.Timber
 
 class CovidCertificateOnboardingViewModel @AssistedInject constructor(
@@ -18,9 +22,22 @@ class CovidCertificateOnboardingViewModel @AssistedInject constructor(
     @Assisted private val dccQrCode: DccQrCode?,
     private val dccQrCodeHandler: DccQrCodeHandler,
     dispatcherProvider: DispatcherProvider,
+    private val dscRepository: DscRepository,
+    private val timeStamper: TimeStamper,
 ) : CWAViewModel(dispatcherProvider) {
 
     val events = SingleLiveEvent<Event>()
+
+    init {
+        launch {
+            val currentDscData = dscRepository.dscData.first()
+            if (Duration(currentDscData.updatedAt, timeStamper.nowUTC) < Duration.standardHours(12)) {
+                Timber.d("Last DSC data refresh was recent: %s", currentDscData.updatedAt)
+                return@launch
+            }
+            dscRepository.refresh()
+        }
+    }
 
     fun onContinueClick() = launch {
         covidCertificateSettings.isOnboarded.update { true }
