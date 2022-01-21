676e1a1a39f1680d927ec74747fea269c92b575f
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-21 15:55:00 +0100

    Update DccWalletInfoParserTest.kt

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/dccwalletinfo/model/DccWalletInfoParserTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/dccwalletinfo/model/DccWalletInfoParserTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 import testhelpers.BaseTest
@@ -11,19 +12,12 @@ import timber.log.Timber
 
 internal class DccWalletInfoParserTest : BaseTest() {
 
-    @BeforeEach
-    fun setUp() {
-    }
-
     @Test
+    @Disabled
     fun parseDccWalletInfo() {
         javaClass.classLoader!!.getResourceAsStream("ccl/dcc_wallet_info.json").use {
             val walletInfo: DccWalletInfo = ObjectMapper().readValue(it)
             Timber.d("walletInfo=$walletInfo")
         }
-    }
-
-    @AfterEach
-    fun tearDown() {
     }
 }
