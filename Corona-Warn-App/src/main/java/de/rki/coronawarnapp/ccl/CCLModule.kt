d8ba99e9efd9e360d264d808bc6c5bed90fdf2ad
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-22 21:49:56 +0100

    DccWallet update task

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/CCLModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/CCLModule.kt
@@ -1,8 +1,20 @@
 package de.rki.coronawarnapp.ccl
 
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import de.rki.coronawarnapp.ccl.dccwalletinfo.update.DccWalletInfoUpdateTask
+import de.rki.coronawarnapp.task.Task
+import de.rki.coronawarnapp.task.TaskFactory
+import de.rki.coronawarnapp.task.TaskTypeKey
 
 @Module
-class CCLModule {
-    // TODO
+abstract class CCLModule {
+
+    @Binds
+    @IntoMap
+    @TaskTypeKey(DccWalletInfoUpdateTask::class)
+    abstract fun dccWalletInfoUpdateTaskFactory(
+        factory: DccWalletInfoUpdateTask.Factory
+    ): TaskFactory<out Task.Progress, out Task.Result>
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/calculation/DccWalletInfoCalculation.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/calculation/DccWalletInfoCalculation.kt
@@ -2,10 +2,14 @@ package de.rki.coronawarnapp.ccl.dccwalletinfo.calculation
 
 import de.rki.coronawarnapp.ccl.configuration.model.CCLConfiguration
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.DccWalletInfo
+import de.rki.coronawarnapp.ccl.dccwalletinfo.model.dummyDccWalletInfo
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccData
 
 class DccWalletInfoCalculation {
-    fun getDccWalletInfo(cclConfiguration: CCLConfiguration, dccList: List<DccData<*>>): DccWalletInfo {
-        error("not implemented yet")
+    fun getDccWalletInfo(
+        cclConfiguration: CCLConfiguration,
+        dccList: List<DccData<*>>
+    ): DccWalletInfo {
+        return dummyDccWalletInfo
     }
 }
