633c0342fd8d2d69561ce41e1d9c4950b5f196a2
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-25 14:49:52 +0100

    Resolve url

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/dccwalletinfo/text/UrlResourceKtTest.kt
@@ -0,0 +1,22 @@
+package de.rki.coronawarnapp.ccl.dccwalletinfo.text
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+import testhelpers.BaseTest
+import java.util.Locale
+
+internal class UrlResourceKtTest : BaseTest() {
+
+    @Test
+    fun urlResource() {
+        val deUrl by urlResource("dcc_admission_state", Locale.GERMAN)
+        deUrl shouldBe "https://www.coronawarn.app/de/faq/#dcc_admission_state"
+
+        val enUrl by urlResource("dcc_admission_state", Locale.FRENCH)
+        enUrl shouldBe "https://www.coronawarn.app/en/faq/#dcc_admission_state"
+
+        val url by urlResource("dcc_admission_state", Locale.ENGLISH)
+        url shouldBe "https://www.coronawarn.app/en/faq/#dcc_admission_state"
+    }
+}
