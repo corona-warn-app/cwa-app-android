cfad3e4795f3f646cd3b5084a135bf361ca7d486
BerndMitter <Berndus@gmx.de>
2022-01-25 14:58:01 +0100

    Added CCLConfigurationStorage

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/storage/CCLConfigurationRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/storage/CCLConfigurationRepository.kt
@@ -1,11 +1,17 @@
 package de.rki.coronawarnapp.ccl.configuration.storage
 
 import de.rki.coronawarnapp.ccl.configuration.model.CCLConfiguration
+import de.rki.coronawarnapp.tag
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
+import timber.log.Timber
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class CCLConfigurationRepository @Inject constructor() {
+@Singleton
+class CCLConfigurationRepository @Inject constructor(
+    private val cclConfigurationStorage: CCLConfigurationStorage
+) {
     val dccConfiguration: Flow<CCLConfiguration> = emptyFlow()
 
     fun getCCLConfiguration(): CCLConfiguration {
@@ -14,4 +20,11 @@ class CCLConfigurationRepository @Inject constructor() {
         return object : CCLConfiguration {
         }
     }
+
+    suspend fun clear() {
+        Timber.tag(TAG).d("Clearing")
+        cclConfigurationStorage.clear()
+    }
 }
+
+private val TAG = tag<CCLConfigurationRepository>()
