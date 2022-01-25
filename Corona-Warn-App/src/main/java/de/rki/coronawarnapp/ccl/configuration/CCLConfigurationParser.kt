c7acf622928ab9f72efe62319f14578c34d22e5f
BerndMitter <Berndus@gmx.de>
2022-01-25 15:02:34 +0100

    Implemented parser

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/CCLConfigurationParser.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/CCLConfigurationParser.kt
@@ -1,11 +1,14 @@
 package de.rki.coronawarnapp.ccl.configuration
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import de.rki.coronawarnapp.ccl.configuration.model.CCLConfiguration
+import de.rki.coronawarnapp.util.serialization.BaseJackson
 import javax.inject.Inject
 
-class CCLConfigurationParser @Inject constructor() {
+class CCLConfigurationParser @Inject constructor(
+    @BaseJackson private val objectMapper: ObjectMapper
+) {
 
-    fun parseCClConfiguration(rawData: String): CCLConfiguration {
-        throw NotImplementedError()
-    }
+    fun parseCClConfiguration(rawData: String): CCLConfiguration = objectMapper.readValue(rawData)
 }
