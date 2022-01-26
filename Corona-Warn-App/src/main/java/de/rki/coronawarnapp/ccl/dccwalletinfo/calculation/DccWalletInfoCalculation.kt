f0d6dc87e1a219a806ca85bf6f83fadd81c2e217
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-01-26 10:40:31 +0100

    add jsonfunctions

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/calculation/DccWalletInfoCalculation.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/calculation/DccWalletInfoCalculation.kt
@@ -4,14 +4,23 @@ import de.rki.coronawarnapp.ccl.configuration.model.CCLConfiguration
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.DccWalletInfo
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.dummyDccWalletInfo
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccData
+import de.rki.jfn.JsonFunctions
 import javax.inject.Inject
 
-class DccWalletInfoCalculation @Inject constructor() {
+class DccWalletInfoCalculation @Inject constructor(
+    private val jsonFunctions: JsonFunctions
+) {
     fun getDccWalletInfo(
         cclConfiguration: CCLConfiguration,
         dccList: List<DccData<*>>
     ): DccWalletInfo {
         val defaultInputParameters = getDefaultInputParameters()
+
+        // register
+        //jsonFunctions.registerFunction(FUNCTION_NAME, logic)
+        //jsonFunctions.evaluateFunction(FUNCTION_NAME, data)
         return dummyDccWalletInfo
     }
 }
+
+private const val FUNCTION_NAME = "getDCCWalletInfo"
