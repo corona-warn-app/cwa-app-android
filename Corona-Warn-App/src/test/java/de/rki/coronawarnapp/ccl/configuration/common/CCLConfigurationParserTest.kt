3a97068ebdd219dce869d2c6dee228e04cacd83f
BerndMitter <Berndus@gmx.de>
2022-01-27 11:56:18 +0100

    Enhance parser test

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/configuration/common/CCLConfigurationParserTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/configuration/common/CCLConfigurationParserTest.kt
@@ -2,6 +2,7 @@ package de.rki.coronawarnapp.ccl.configuration.common
 
 import com.fasterxml.jackson.databind.node.NullNode
 import com.fasterxml.jackson.databind.node.TextNode
+import com.upokecenter.cbor.CBORObject
 import de.rki.coronawarnapp.ccl.configuration.model.CCLConfiguration
 import de.rki.coronawarnapp.ccl.configuration.model.FunctionDefinition
 import de.rki.coronawarnapp.ccl.configuration.model.FunctionParameter
@@ -119,6 +120,15 @@ class CCLConfigurationParserTest : BaseTest() {
             validFrom shouldBe Instant.parse("2021-10-07T00:00:00Z")
             validTo shouldBe Instant.parse("2030-06-01T00:00:00Z")
             logic shouldBe CCLConfiguration.Logic(jfnDescriptors = listOf(jfnDescriptor))
+        }
+    }
+
+    @Test
+    fun `parser creates the same output for raw data and json`() {
+        val rawData = CBORObject.FromJSONString(json).EncodeToBytes()
+
+        with(parser) {
+            parseCClConfigurations(rawData = rawData) shouldBe parseCClConfigurationsJson(json = json)
         }
     }
 }
