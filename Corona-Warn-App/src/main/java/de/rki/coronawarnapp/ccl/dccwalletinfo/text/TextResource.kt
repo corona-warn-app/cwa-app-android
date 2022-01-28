af93f1925735930085401ffe3740eed25e3608dd
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-28 10:36:51 +0100

    Text formatting

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/text/TextResource.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccwalletinfo/text/TextResource.kt
@@ -4,6 +4,9 @@ import de.rki.coronawarnapp.ccl.dccwalletinfo.model.CCLText
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.Parameters
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.PluralText
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.SingleText
+import org.joda.time.Days
+import org.joda.time.Instant
+import org.joda.time.format.DateTimeFormat
 import java.util.Locale
 
 /**
@@ -12,33 +15,111 @@ import java.util.Locale
 fun textResource(
     cclText: CCLText,
     locale: Locale = Locale.getDefault()
-) = lazy { formatCCLText(cclText, locale.language) }
+) = lazy { formatCCLText(cclText, locale) }
 
-internal fun formatCCLText(cclText: CCLText?, lang: String): String? = when (cclText) {
-    is PluralText -> "TODO: Format plural text"
-    is SingleText -> {
-        // DO: provide fallback
-        val text = cclText.localizedText[lang]
-            ?: cclText.localizedText[EN] // Default for other languages
-            ?: cclText.localizedText[DE] // Default for EN
-        text?.replace("%@", "%s")
-            ?.format(cclText.parameters.format())
-    }
+internal fun formatCCLText(
+    cclText: CCLText?,
+    locale: Locale
+): String? = when (cclText) {
+    is PluralText -> cclText.formatPlural(locale)
+    is SingleText -> cclText.formatSingle(locale)
     else -> null
 }
 
-internal fun List<Parameters>.format(): List<Any> = map { parameter ->
-    when (parameter.type) {
-        Parameters.Type.STRING -> parameter.toString()
-        Parameters.Type.NUMBER -> TODO()
-        Parameters.Type.BOOLEAN -> TODO()
-        Parameters.Type.DATE -> TODO()
-        Parameters.Type.LOCAL_DATE -> TODO()
-        Parameters.Type.LOCAL_DATE_TIME -> TODO()
-        Parameters.Type.UTC_DATE -> TODO()
-        Parameters.Type.UTC_DATE_TIME -> TODO()
+private fun SingleText.formatSingle(
+    locale: Locale
+): String? {
+    val text = localizedText[locale.language]
+        ?: localizedText[EN] // Default for other languages
+        ?: localizedText[DE] // Default for EN
+
+    return text
+        ?.replace("%@", "%s")
+        ?.format(*parameters.convertValues(locale))
+}
+
+private fun PluralText.formatPlural(
+    locale: Locale
+): String? {
+    val quantity = quantity(locale)
+    val quantityText = localizedText[locale.language]
+        ?: localizedText[EN]
+        ?: localizedText[DE]
+
+    val text = when (quantity) {
+        0 -> quantityText?.zero
+        1 -> quantityText?.one
+        2 -> quantityText?.two
+        in 3..4 -> quantityText?.few
+        in 5..7 -> quantityText?.many
+        else -> quantityText?.other
+    }
+
+    return text
+        ?.replace("%@", "%s")
+        ?.format(*parameters.convertValues(locale))
+}
+
+private fun PluralText.quantity(locale: Locale): Any {
+    return quantity ?: run {
+        val param = parameters[quantityParameterIndex ?: 0]
+        when (param.format) {
+            Parameters.FormatType.DATE_DIFF_NOW -> when (param.unit) {
+                Parameters.UnitType.DAY ->
+                    Days.daysBetween(
+                        Instant.parse(param.covertValue(locale).toString()),
+                        Instant.now()
+                    ).days
+                else -> param.covertValue(locale)
+            }
+            else -> param.covertValue(locale)
+        }
     }
 }
+
+private fun List<Parameters>.convertValues(locale: Locale): Array<Any> = map { parameter ->
+    parameter.covertValue(locale)
+}.toTypedArray()
+
+private fun Parameters.covertValue(
+    locale: Locale
+) = when (type) {
+    Parameters.Type.STRING -> value.toString()
+    Parameters.Type.NUMBER -> toNumber()
+    Parameters.Type.BOOLEAN -> toBoolean()
+    Parameters.Type.DATE,
+    Parameters.Type.LOCAL_DATE -> toLocalDate(locale)
+    Parameters.Type.LOCAL_DATE_TIME -> toLocalDateTime()
+    Parameters.Type.UTC_DATE -> toUTCDate()
+    Parameters.Type.UTC_DATE_TIME -> toUTCDateTime()
+}
+
+private fun Parameters.toUTCDateTime(): String {
+    return Instant.parse(value.toString()).toString(
+        DateTimeFormat.shortDate()
+    )
+}
+
+private fun Parameters.toUTCDate(): String {
+    return Instant.parse(value.toString()).toString(
+        DateTimeFormat.shortDate()
+    )
+}
+
+private fun Parameters.toLocalDateTime(): String {
+    return Instant.parse(value.toString()).toString(
+        DateTimeFormat.shortDate()
+    )
+}
+
+private fun Parameters.toLocalDate(locale: Locale): String {
+    return Instant.parse(value.toString()).toString(
+        DateTimeFormat.shortDate()
+    )
+}
+
+private fun Parameters.toNumber(): Int = (value as Number).toInt()
+private fun Parameters.toBoolean(): Boolean = value as Boolean
 
 private const val EN = "en"
 private const val DE = "de"
