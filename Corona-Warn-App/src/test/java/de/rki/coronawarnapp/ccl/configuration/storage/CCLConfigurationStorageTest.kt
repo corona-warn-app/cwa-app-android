81d875ece7c41047a1baf06c46c5878d8dfc130c
BerndMitter <Berndus@gmx.de>
2022-01-31 14:53:40 +0100

    Added CCLConfigurationStorageTest

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/configuration/storage/CCLConfigurationStorageTest.kt
@@ -0,0 +1,60 @@
+package de.rki.coronawarnapp.ccl.configuration.storage
+
+import io.kotest.matchers.file.shouldExist
+import io.kotest.matchers.file.shouldNotExist
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseIOTest
+import java.io.File
+
+class CCLConfigurationStorageTest : BaseIOTest() {
+
+    private val cclFile = File(IO_TEST_BASEDIR, "ccl")
+
+    private fun createInstance(file: File = cclFile) = CCLConfigurationStorage(cclFile = file)
+
+    @AfterEach
+    fun cleanup() {
+        cclFile.deleteRecursively()
+    }
+
+    @Test
+    fun `write and read data`() = runBlockingTest {
+        val data = "Some test data".toByteArray()
+
+        with(createInstance()) {
+            load() shouldBe null
+            save(rawData = data)
+            load() shouldBe data
+        }
+
+        val data2 = "Some test data which overrides existing data".toByteArray()
+
+        with(createInstance()) {
+            load() shouldBe data
+            save(rawData = data2)
+            load() shouldNotBe data
+            load() shouldBe data2
+        }
+    }
+
+    @Test
+    fun `clears ccl file completely`() = runBlockingTest {
+        val cacheDir = File(cclFile, "ccl_config_http_cache")
+        val cacheFile = File(cacheDir, "data")
+        cacheFile.parentFile?.mkdirs()
+        val data = "Some random test data".toByteArray()
+        cacheFile.writeBytes(data)
+
+        with(createInstance()) {
+            cacheDir.shouldExist()
+            cacheFile.shouldExist()
+            clear()
+            cacheDir.shouldNotExist()
+            cacheFile.shouldNotExist()
+        }
+    }
+}
