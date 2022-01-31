b01a09f24b0f3793b9363379d499e87947b3397e
Lukas Lechner <office@lukaslechner.com>
2022-01-31 16:03:44 +0100

    Create CCLConfigurationUpdateSchedulerTest.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/update/CCLConfigurationUpdateScheduler.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/update/CCLConfigurationUpdateScheduler.kt
@@ -6,9 +6,12 @@ import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import de.rki.coronawarnapp.util.coroutine.AppScope
 import de.rki.coronawarnapp.util.device.ForegroundState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import timber.log.Timber
@@ -16,6 +19,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class CCLConfigurationUpdateScheduler @Inject constructor(
+    @AppScope private val appScope: CoroutineScope,
     private val foregroundState: ForegroundState,
     private val cclConfigurationUpdater: CCLConfigurationUpdater,
     private val workManager: WorkManager
@@ -29,6 +33,7 @@ class CCLConfigurationUpdateScheduler @Inject constructor(
         foregroundState
             .isInForeground
             .onStart {
+                Timber.d("Schedule daily worker.")
                 scheduleDailyWorker()
             }
             .distinctUntilChanged()
@@ -38,6 +43,7 @@ class CCLConfigurationUpdateScheduler @Inject constructor(
                 Timber.d("Configuration was updated = %s", updated)
                 // dccWalletManager.triggerCalculation(updated)
             }
+            .launchIn(appScope)
     }
 
     private fun scheduleDailyWorker() {
