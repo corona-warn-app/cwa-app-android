aedf195e706612f91b6d870de82e2fe4d5523659
Lukas Lechner <office@lukaslechner.com>
2022-02-01 15:35:16 +0100

    Extend CCLConfigurationUpdaterTest.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/update/CCLConfigurationUpdater.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/configuration/update/CCLConfigurationUpdater.kt
@@ -1,8 +1,10 @@
 package de.rki.coronawarnapp.ccl.configuration.update
 
 import androidx.annotation.VisibleForTesting
+import de.rki.coronawarnapp.ccl.configuration.storage.CCLConfigurationRepository
 import de.rki.coronawarnapp.covidcertificate.booster.BoosterRulesRepository
 import de.rki.coronawarnapp.util.TimeAndDateExtensions.toLocalDateUtc
+import de.rki.coronawarnapp.util.TimeStamper
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
@@ -13,8 +15,10 @@ import javax.inject.Singleton
 
 @Singleton
 class CCLConfigurationUpdater @Inject constructor(
+    private val timeStamper: TimeStamper,
     private val cclSettings: CCLSettings,
     private val boosterRulesRepository: BoosterRulesRepository,
+    private val cclConfigurationRepository: CCLConfigurationRepository,
     // private val dccWalletManager: DccWalletInfoCalculationManager
 ) {
 
@@ -40,22 +44,23 @@ class CCLConfigurationUpdater @Inject constructor(
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    internal suspend fun isUpdateRequired(now: Instant = Instant.now()): Boolean {
+    internal suspend fun isUpdateRequired(now: Instant = timeStamper.nowUTC): Boolean {
         val lastExecution = cclSettings.getLastExecutionTime() ?: return true
 
         // update is needed if the last update was on a different day
         return lastExecution.toLocalDateUtc() != now.toLocalDateUtc()
     }
 
-    private suspend fun updateConfiguration(): Boolean {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    internal suspend fun updateConfiguration(): Boolean {
         return coroutineScope {
             val newBoosterRulesDownloaded = async { boosterRulesRepository.update() }
-            // val newCclConfigDownloaded = async { cclConfigRepository.updateCCLConfig() }
+            val newCclConfigDownloaded = async { cclConfigurationRepository.updateCCLConfiguration() }
 
             return@coroutineScope listOf(
                 newBoosterRulesDownloaded,
-                // newCclConfigDownloaded
-            ).awaitAll().any { true }
+                newCclConfigDownloaded
+            ).awaitAll().any { downloaded -> downloaded }
         }
     }
 }
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/configuration/update/CCLConfigurationUpdaterTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/configuration/update/CCLConfigurationUpdaterTest.kt
@@ -1,10 +1,16 @@
 package de.rki.coronawarnapp.ccl.configuration.update
 
+import de.rki.coronawarnapp.ccl.configuration.storage.CCLConfigurationRepository
 import de.rki.coronawarnapp.covidcertificate.booster.BoosterRulesRepository
+import de.rki.coronawarnapp.util.TimeStamper
 import io.kotest.matchers.shouldBe
+import io.mockk.Called
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
 import kotlinx.coroutines.test.runBlockingTest
 import org.joda.time.Instant
 import org.junit.jupiter.api.BeforeEach
@@ -13,13 +19,69 @@ import testhelpers.BaseTest
 
 internal class CCLConfigurationUpdaterTest : BaseTest() {
 
-    @MockK private lateinit var cclSettings: CCLSettings
+    @MockK private lateinit var timeStamper: TimeStamper
+    @RelaxedMockK private lateinit var cclSettings: CCLSettings
     @MockK private lateinit var boosterRulesRepository: BoosterRulesRepository
+    @MockK private lateinit var cclConfigurationRepository: CCLConfigurationRepository
 
     @BeforeEach
     fun setup() {
         MockKAnnotations.init(this)
     }
+
+    @Test
+    fun `updateIfRequired should update booster rules and ccl configuration if required`() = runBlockingTest {
+        coEvery { cclSettings.getLastExecutionTime() } returns Instant.parse("2000-01-01T00:00:00Z")
+        coEvery { timeStamper.nowUTC } returns Instant.parse("2000-01-02T00:00:00Z")
+
+        coEvery { boosterRulesRepository.update() } returns true
+        coEvery { cclConfigurationRepository.updateCCLConfiguration() } returns false
+
+        getInstance().updateIfRequired()
+
+        coVerify(exactly = 1) { boosterRulesRepository.update() }
+        coVerify(exactly = 1) { cclConfigurationRepository.updateCCLConfiguration() }
+
+        verify(exactly = 1) { cclSettings.setExecutionTimeToNow(any()) }
+
+        // verify(exactly = 1) { dccWalletManager.triggerCalculation(true)}
+    }
+
+    @Test
+    fun `updateIfRequired should NOT update booster rules and ccl configuration if NOT required`() = runBlockingTest {
+        coEvery { cclSettings.getLastExecutionTime() } returns Instant.parse("2000-01-01T00:00:00Z")
+        coEvery { timeStamper.nowUTC } returns Instant.parse("2000-01-01T00:00:00Z")
+
+        getInstance().updateIfRequired()
+
+        verify { boosterRulesRepository wasNot Called }
+        verify { cclConfigurationRepository wasNot Called }
+
+        // verify { dccWalletManager wasNot Called }
+    }
+
+    @Test
+    fun `updateConfiguration() should return true when new booster rules or new configuration was downloaded or false otherwise`() =
+        runBlockingTest {
+
+            val updater = getInstance()
+
+            coEvery { boosterRulesRepository.update() } returns false
+            coEvery { cclConfigurationRepository.updateCCLConfiguration() } returns false
+            updater.updateConfiguration() shouldBe false
+
+            coEvery { boosterRulesRepository.update() } returns true
+            coEvery { cclConfigurationRepository.updateCCLConfiguration() } returns false
+            updater.updateConfiguration() shouldBe true
+
+            coEvery { boosterRulesRepository.update() } returns false
+            coEvery { cclConfigurationRepository.updateCCLConfiguration() } returns true
+            updater.updateConfiguration() shouldBe true
+
+            coEvery { boosterRulesRepository.update() } returns true
+            coEvery { cclConfigurationRepository.updateCCLConfiguration() } returns true
+            updater.updateConfiguration() shouldBe true
+        }
 
     @Test
     fun `isUpdateRequires() should return true after one day`() = runBlockingTest {
@@ -28,13 +90,15 @@ internal class CCLConfigurationUpdaterTest : BaseTest() {
 
         // update is required when none was performed yet
         coEvery { cclSettings.getLastExecutionTime() } returns null
-        updater.isUpdateRequired() shouldBe true
+        updater.isUpdateRequired(Instant.now()) shouldBe true
 
         coEvery { cclSettings.getLastExecutionTime() } returns Instant.parse("2000-01-01T00:00:00Z")
 
         // no update required on the same day
-        val sameDay = Instant.parse("2000-01-01T23:59:59Z")
-        updater.isUpdateRequired(sameDay) shouldBe false
+        val sameDayBeginning = Instant.parse("2000-01-01T00:00:00Z")
+        val sameDayEnd = Instant.parse("2000-01-01T23:59:59Z")
+        updater.isUpdateRequired(sameDayBeginning) shouldBe false
+        updater.isUpdateRequired(sameDayEnd) shouldBe false
 
         // update required on next day
         val nextDay = Instant.parse("2000-01-02T00:00:00Z")
@@ -46,6 +110,6 @@ internal class CCLConfigurationUpdaterTest : BaseTest() {
     }
 
     private fun getInstance(): CCLConfigurationUpdater {
-        return CCLConfigurationUpdater(cclSettings, boosterRulesRepository)
+        return CCLConfigurationUpdater(timeStamper, cclSettings, boosterRulesRepository, cclConfigurationRepository)
     }
 }
