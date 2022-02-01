b7d6c9cb394b5b0630eb121c5f8d5072d268b211
Alexander Alferov <a.alferov@sap.com>
2022-02-01 17:47:54 +0300

    Fix

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/details/items/BoosterCard.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/details/items/BoosterCard.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.BoosterNotification
-import de.rki.coronawarnapp.ccl.dccwalletinfo.text.textResource
+import de.rki.coronawarnapp.ccl.ui.text.format
 import de.rki.coronawarnapp.covidcertificate.person.ui.details.PersonDetailsAdapter
 import de.rki.coronawarnapp.databinding.PersonDetailsBoosterCardBinding
 import de.rki.coronawarnapp.util.lists.diffutil.HasPayloadDiffer
@@ -28,8 +28,8 @@ class BoosterCard(parent: ViewGroup) :
         root.setOnClickListener { curItem.onClick() }
 
         val boosterNotification = curItem.boosterNotification
-        title.text = textResource(boosterNotification.titleText).value
-        subtitle.text = textResource(boosterNotification.subtitleText).value
+        title.text = boosterNotification.titleText.format().orEmpty()
+        subtitle.text = boosterNotification.subtitleText.format().orEmpty()
 
         boosterBadge.isVisible = curItem.isNew
     }
