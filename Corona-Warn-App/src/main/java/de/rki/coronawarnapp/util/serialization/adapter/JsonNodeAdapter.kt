9777bf894234e11fb958cd09a8e6e971a351103b
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-02-01 18:18:14 +0100

    fix adapter new config

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/serialization/adapter/JsonNodeAdapter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/serialization/adapter/JsonNodeAdapter.kt
@@ -25,7 +25,7 @@ class JsonNodeAdapter @Inject constructor(
 
     override fun write(out: JsonWriter, value: JsonNode?) {
         if (value == null) out.nullValue()
-        else out.value(jackson.writeValueAsString(value))
+        else out.value(value.textValue())
     }
 
     override fun read(reader: JsonReader): JsonNode? = when (reader.peek()) {
