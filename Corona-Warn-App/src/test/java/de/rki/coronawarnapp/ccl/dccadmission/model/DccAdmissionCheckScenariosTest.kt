062151ae6dcf856dc119eaacb35fbec54f343273
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-02-09 16:18:47 +0100

    input

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/dccadmission/model/DccAdmissionCheckScenariosTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ccl/dccadmission/model/DccAdmissionCheckScenariosTest.kt
@@ -2,6 +2,7 @@ package de.rki.coronawarnapp.ccl.dccadmission.model
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.SingleText
+import de.rki.coronawarnapp.ccl.dccwalletinfo.model.SystemTime
 import de.rki.coronawarnapp.util.serialization.SerializationModule
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
@@ -135,8 +136,43 @@ class DccAdmissionCheckScenariosTest : BaseTest() {
         )
     )
 
+    private val input = DccAdmissionCheckScenariosInput(
+        os = "android",
+        language = "en",
+        now = SystemTime(
+            timestamp = 1640854800,
+            localDate = "2021-12-30",
+            localDateTime = "2021-12-30T10:00:00+01:00",
+            localDateTimeMidnight = "2021-12-30T00:00:00+01:00",
+            utcDate = "2021-12-30",
+            utcDateTime = "2021-12-30T09:00:00Z",
+            utcDateTimeMidnight = "2021-12-30T00:00:00Z"
+        )
+    )
+
+    private val inputJson = """
+        {
+          "os": "android",
+          "language": "en",
+          "now": {
+            "timestamp": 1640854800,
+            "localDate": "2021-12-30",
+            "localDateTime": "2021-12-30T10:00:00+01:00",
+            "localDateTimeMidnight": "2021-12-30T00:00:00+01:00",
+            "utcDate": "2021-12-30",
+            "utcDateTime": "2021-12-30T09:00:00Z",
+            "utcDateTimeMidnight": "2021-12-30T00:00:00Z"
+          }
+        }
+    """.trimIndent()
+
     @Test
     fun `parse output`() {
         mapper.readValue<DccAdmissionCheckScenarios>(outputJson) shouldBe output
+    }
+
+    @Test
+    fun `parse input`() {
+        mapper.readValue<DccAdmissionCheckScenariosInput>(inputJson) shouldBe input
     }
 }
