ab378b85aeac276147064dd7bc17e03528a6973c
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-02-11 11:51:01 +0100

    Update AdmissionScenariosViewModel.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/admission/AdmissionScenariosViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/admission/AdmissionScenariosViewModel.kt
@@ -1,11 +1,11 @@
 package de.rki.coronawarnapp.covidcertificate.person.ui.admission
 
 import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
-import javax.inject.Inject
 
-class AdmissionScenariosViewModel @Inject constructor() : CWAViewModel() {
+class AdmissionScenariosViewModel @AssistedInject constructor() : CWAViewModel() {
 
     @AssistedFactory
     interface Factory : SimpleCWAViewModelFactory<AdmissionScenariosViewModel>
