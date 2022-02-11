24fd74f1ee1d120c2b8dc95a5349e838eecaf513
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-02-11 15:10:34 +0100

    Expose scenarios

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/admission/AdmissionScenariosViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/admission/AdmissionScenariosViewModel.kt
@@ -2,10 +2,15 @@ package de.rki.coronawarnapp.covidcertificate.person.ui.admission
 
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.ccl.dccadmission.model.storage.DccAdmissionCheckScenariosRepository
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 
-class AdmissionScenariosViewModel @AssistedInject constructor() : CWAViewModel() {
+class AdmissionScenariosViewModel @AssistedInject constructor(
+    admissionCheckScenariosRepository: DccAdmissionCheckScenariosRepository
+) : CWAViewModel() {
+
+    val admissionCheckScenarios = admissionCheckScenariosRepository.admissionCheckScenarios.asLiveData2()
 
     @AssistedFactory
     interface Factory : SimpleCWAViewModelFactory<AdmissionScenariosViewModel>
