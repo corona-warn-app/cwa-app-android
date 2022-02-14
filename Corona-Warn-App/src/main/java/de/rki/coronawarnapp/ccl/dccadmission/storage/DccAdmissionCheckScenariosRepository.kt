15d492ee33aa81b398f1150465190dcd3f1c0f06
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-02-14 14:45:00 +0100

    remove dummy

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccadmission/storage/DccAdmissionCheckScenariosRepository.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ccl/dccadmission/storage/DccAdmissionCheckScenariosRepository.kt
@@ -3,9 +3,6 @@ package de.rki.coronawarnapp.ccl.dccadmission.storage
 import com.fasterxml.jackson.databind.ObjectMapper
 import de.rki.coronawarnapp.ccl.configuration.update.CCLSettings
 import de.rki.coronawarnapp.ccl.dccadmission.model.DccAdmissionCheckScenarios
-import de.rki.coronawarnapp.ccl.dccadmission.model.Scenario
-import de.rki.coronawarnapp.ccl.dccadmission.model.ScenarioSelection
-import de.rki.coronawarnapp.ccl.dccwalletinfo.model.SingleText
 import de.rki.coronawarnapp.util.serialization.BaseJackson
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -42,57 +39,3 @@ class DccAdmissionCheckScenariosRepository @Inject constructor(
         cclSettings.setAdmissionCheckScenarios("")
     }
 }
-
-val dummy = DccAdmissionCheckScenarios(
-    labelText = SingleText(
-        type = "string",
-        localizedText = mapOf("de" to "Status anzeigen für folgendes Bundesland:"),
-        parameters = emptyList()
-    ),
-    scenarioSelection = ScenarioSelection(
-        titleText = SingleText(
-            type = "string",
-            localizedText = mapOf("de" to "Ihr Bundesland"),
-            parameters = emptyList()
-        ),
-        items = listOf(
-            Scenario(
-                identifier = "DE",
-                titleText = SingleText(
-                    type = "string",
-                    localizedText = mapOf("de" to "Bundesweit"),
-                    parameters = emptyList()
-                ),
-                enabled = true
-            ),
-            Scenario(
-                identifier = "BW",
-                titleText = SingleText(
-                    type = "string",
-                    localizedText = mapOf("de" to "Baden-Württemberg"),
-                    parameters = emptyList()
-                ),
-                subtitleText = SingleText(
-                    type = "string",
-                    localizedText = mapOf("de" to "Schön hier"),
-                    parameters = emptyList()
-                ),
-                enabled = true,
-            ),
-            Scenario(
-                identifier = "HE",
-                titleText = SingleText(
-                    type = "string",
-                    localizedText = mapOf("de" to "Hesse"),
-                    parameters = emptyList()
-                ),
-                subtitleText = SingleText(
-                    type = "string",
-                    localizedText = mapOf("de" to "Für dieses Bundesland liegen momentan keine Regeln vor"),
-                    parameters = emptyList()
-                ),
-                enabled = false,
-            ),
-        ),
-    )
-)
