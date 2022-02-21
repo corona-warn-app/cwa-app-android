5713bc67e7235ecaf301aa8fa4c71b0f00e4e5d8
Lukas Lechner <office@lukaslechner.com>
2022-02-21 15:21:33 +0100

    Remove ViewModel and Module of SuccessFragment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/DccReissuanceUiModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/DccReissuanceUiModule.kt
@@ -4,15 +4,10 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import de.rki.coronawarnapp.dccreissuance.ui.consent.DccReissuanceConsentFragment
 import de.rki.coronawarnapp.dccreissuance.ui.consent.DccReissuanceConsentFragmentModule
-import de.rki.coronawarnapp.dccreissuance.ui.success.DccReissuanceSuccessFragment
-import de.rki.coronawarnapp.dccreissuance.ui.success.DccReissuanceSuccessFragmentModule
 
 @Module
 abstract class DccReissuanceUiModule {
 
     @ContributesAndroidInjector(modules = [DccReissuanceConsentFragmentModule::class])
     abstract fun dccReissuanceConsentFragment(): DccReissuanceConsentFragment
-
-    @ContributesAndroidInjector(modules = [DccReissuanceSuccessFragmentModule::class])
-    abstract fun dccReissuanceSuccessFragment(): DccReissuanceSuccessFragment
 }
