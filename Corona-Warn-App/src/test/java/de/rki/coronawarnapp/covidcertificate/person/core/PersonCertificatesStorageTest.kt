cbfef3fbac93e9d0cb68b67cd3f620eaf7a152a1
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-02-22 23:09:42 +0100

    Cover cases

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/person/core/PersonCertificatesStorageTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/person/core/PersonCertificatesStorageTest.kt
@@ -2,6 +2,8 @@ package de.rki.coronawarnapp.covidcertificate.person.core
 
 import android.content.Context
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CertificatePersonIdentifier
+import de.rki.coronawarnapp.covidcertificate.person.core.PersonCertificatesSettings.Companion.CURRENT_PERSON_KEY
+import de.rki.coronawarnapp.covidcertificate.person.core.PersonCertificatesSettings.Companion.PERSONS_SETTINGS_MAP
 import de.rki.coronawarnapp.util.serialization.SerializationModule
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
@@ -9,6 +11,7 @@ import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
+import org.joda.time.Instant
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
@@ -19,22 +22,28 @@ import testhelpers.preferences.FakeDataStore
 @Suppress("MaxLineLength")
 class PersonCertificatesStorageTest : BaseTest() {
     @MockK lateinit var context: Context
-    private lateinit var mockPreferences: FakeDataStore
-    private val personIdentifier = CertificatePersonIdentifier(
+    private lateinit var fakeDataStore: FakeDataStore
+    private val personIdentifier1 = CertificatePersonIdentifier(
         dateOfBirthFormatted = "01.10.2020",
         firstNameStandardized = "fN",
         lastNameStandardized = "lN"
+    )
+
+    private val personIdentifier2 = CertificatePersonIdentifier(
+        dateOfBirthFormatted = "20.10.2020",
+        firstNameStandardized = "ffNN",
+        lastNameStandardized = "llNN"
     )
 
     @BeforeEach
     fun setup() {
         MockKAnnotations.init(this)
 
-        mockPreferences = FakeDataStore()
+        fakeDataStore = FakeDataStore()
     }
 
     private fun createInstance() = PersonCertificatesSettings(
-        dataStore = mockPreferences,
+        dataStore = fakeDataStore,
         mapper = SerializationModule.jacksonBaseMapper,
         appScope = TestCoroutineScope(),
         dispatcherProvider = TestDispatcherProvider()
@@ -49,13 +58,13 @@ class PersonCertificatesStorageTest : BaseTest() {
     fun `clearing deletes all data`() {
 
         createInstance().apply {
-            setCurrentCwaUser(personIdentifier)
-            setBoosterNotifiedAt(personIdentifier)
+            setCurrentCwaUser(personIdentifier1)
+            setBoosterNotifiedAt(personIdentifier1)
             clear()
         }
 
-        mockPreferences[PersonCertificatesSettings.CURRENT_PERSON_KEY] shouldBe null
-        mockPreferences[PersonCertificatesSettings.PERSONS_SETTINGS_MAP] shouldBe null
+        fakeDataStore[CURRENT_PERSON_KEY] shouldBe null
+        fakeDataStore[PERSONS_SETTINGS_MAP] shouldBe null
     }
 
     @Test
@@ -71,7 +80,7 @@ class PersonCertificatesStorageTest : BaseTest() {
             setCurrentCwaUser(testIdentifier)
             currentCwaUser.first() shouldBe testIdentifier
 
-            val raw = mockPreferences[PersonCertificatesSettings.CURRENT_PERSON_KEY] as String
+            val raw = fakeDataStore[CURRENT_PERSON_KEY] as String
             raw.toComparableJsonPretty() shouldBe """
                 {
                   "dateOfBirth": "1999-12-24",
@@ -79,6 +88,245 @@ class PersonCertificatesStorageTest : BaseTest() {
                   "givenNameStandardized": "firstname"
                 }
             """.toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `set booster for a person has not settings`() {
+        createInstance().apply {
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": null,
+                			"lastBoosterNotifiedAt": 0,
+                			"showDccReissuanceBadge": false,
+                			"lastDccReissuanceNotifiedAt": null
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `set booster for a person has settings`() {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": null,
+                			"lastBoosterNotifiedAt": 0,
+                			"showDccReissuanceBadge": true,
+                			"lastDccReissuanceNotifiedAt": 0
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `set dcc reissuance for a person has not settings`() {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": null,
+                			"lastBoosterNotifiedAt": null,
+                			"showDccReissuanceBadge": true,
+                			"lastDccReissuanceNotifiedAt": 0
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `set dcc reissuance for a person has settings`() {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": null,
+                			"lastBoosterNotifiedAt": 0,
+                			"showDccReissuanceBadge": true,
+                			"lastDccReissuanceNotifiedAt": 0
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `dismiss dcc reissuance for a person has not settings`() {
+        createInstance().apply {
+            dismissReissuanceBadge(personIdentifier1)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": null,
+                			"lastBoosterNotifiedAt": null,
+                			"showDccReissuanceBadge": false,
+                			"lastDccReissuanceNotifiedAt": null
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `dismiss dcc reissuance for a person has settings`() {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            dismissReissuanceBadge(personIdentifier1)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": null,
+                			"lastBoosterNotifiedAt": 0,
+                			"showDccReissuanceBadge": false,
+                			"lastDccReissuanceNotifiedAt": 0
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `acknowledge booster for a person has not settings`() {
+        createInstance().apply {
+            acknowledgeBoosterRule(personIdentifier1, "BRN-123")
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": "BRN-123",
+                			"lastBoosterNotifiedAt": null,
+                			"showDccReissuanceBadge": false,
+                			"lastDccReissuanceNotifiedAt": null
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `acknowledge booster for a person has settings`() {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            dismissReissuanceBadge(personIdentifier1)
+            acknowledgeBoosterRule(personIdentifier1, "BRN-123")
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"lastSeenBoosterRuleIdentifier": "BRN-123",
+                			"lastBoosterNotifiedAt": 0,
+                			"showDccReissuanceBadge": false,
+                			"lastDccReissuanceNotifiedAt": 0
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `clear booster for a person has settings`() {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            dismissReissuanceBadge(personIdentifier1)
+            acknowledgeBoosterRule(personIdentifier1, "BRN-123")
+            clearBoosterRuleInfo(personIdentifier1)
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                		"{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                			"showDccReissuanceBadge": false,
+                			"lastDccReissuanceNotifiedAt": 0
+                		}
+                	}
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+        }
+    }
+
+    @Test
+    fun `save settings for many persons`() = runBlockingTest {
+        createInstance().apply {
+            setDccReissuanceNotifiedAt(personIdentifier1, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier1, Instant.EPOCH)
+            dismissReissuanceBadge(personIdentifier1)
+            acknowledgeBoosterRule(personIdentifier1, "BRN-123")
+
+            setDccReissuanceNotifiedAt(personIdentifier2, Instant.EPOCH)
+            setBoosterNotifiedAt(personIdentifier2, Instant.EPOCH)
+            dismissReissuanceBadge(personIdentifier2)
+            acknowledgeBoosterRule(personIdentifier2, "BRN-456")
+
+            fakeDataStore[PERSONS_SETTINGS_MAP].toString().toComparableJsonPretty() shouldBe """
+                {
+                	"settings": {
+                        "{\"dateOfBirth\":\"01.10.2020\",\"familyNameStandardized\":\"lN\",\"givenNameStandardized\":\"fN\"}": {
+                          "lastSeenBoosterRuleIdentifier": "BRN-123",
+                          "lastBoosterNotifiedAt": 0,
+                          "showDccReissuanceBadge": false,
+                          "lastDccReissuanceNotifiedAt": 0
+                        },
+                        "{\"dateOfBirth\":\"20.10.2020\",\"familyNameStandardized\":\"llNN\",\"givenNameStandardized\":\"ffNN\"}": {
+                          "lastSeenBoosterRuleIdentifier": "BRN-456",
+                          "lastBoosterNotifiedAt": 0,
+                          "showDccReissuanceBadge": false,
+                          "lastDccReissuanceNotifiedAt": 0
+                        }
+                    }
+                }
+            """.trimIndent()
+                .toComparableJsonPretty()
+
+            personsSettings.first() shouldBe mapOf(
+                personIdentifier1 to PersonSettings(
+                    lastBoosterNotifiedAt = Instant.EPOCH,
+                    lastSeenBoosterRuleIdentifier = "BRN-123",
+                    showDccReissuanceBadge = false,
+                    lastDccReissuanceNotifiedAt = Instant.EPOCH
+                ),
+                personIdentifier2 to PersonSettings(
+                    lastBoosterNotifiedAt = Instant.EPOCH,
+                    lastSeenBoosterRuleIdentifier = "BRN-456",
+                    showDccReissuanceBadge = false,
+                    lastDccReissuanceNotifiedAt = Instant.EPOCH
+                )
+            )
         }
     }
 }
