a8735a1fb8564625f7f813d4e5fafd039b9c7de0
BerndMitter <Berndus@gmx.de>
2022-02-23 23:28:33 +0100

    Implemented cert validation

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/server/validation/DccReissuanceServerCertificateValidator.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/server/validation/DccReissuanceServerCertificateValidator.kt
@@ -1,18 +1,46 @@
 package de.rki.coronawarnapp.dccreissuance.core.server.validation
 
+import de.rki.coronawarnapp.appconfig.AppConfigProvider
 import de.rki.coronawarnapp.dccreissuance.core.common.DccReissuanceException
 import de.rki.coronawarnapp.tag
+import okio.ByteString.Companion.toByteString
 import timber.log.Timber
 import java.security.cert.Certificate
 import javax.inject.Inject
 
-class DccReissuanceServerCertificateValidator @Inject constructor() {
+class DccReissuanceServerCertificateValidator @Inject constructor(
+    private val appConfigProvider: AppConfigProvider
+) {
 
-    fun validate(certificateChain: List<Certificate>) = try {
+    /**
+     *
+     * @throws [DccReissuanceException] if the check fails
+     *
+     * Note that the absence of an error code indicates a successful check
+     */
+    suspend fun validate(certificateChain: List<Certificate>) = try {
+        val leafCertificate = certificateChain.first()
+        //TODO: Add reissueServicePublicKeyDigest to app config
+        val reissueServicePublicKeyDigest = appConfigProvider.getAppConfig()
 
+        val publicKeyHash256 = leafCertificate.publicKey.encoded.toByteString().sha256()
+
+        if (publicKeyHash256 != reissueServicePublicKeyDigest) throw DccReissuanceException(
+            errorCode = DccReissuanceException.ErrorCode.DCC_RI_PIN_MISMATCH
+        )
+
+        Timber.tag(TAG).d("Certificate check was successful")
     } catch (e: Exception) {
-        Timber.tag(TAG).w(e, "Failed to validate Dcc Reissuance Server Certificate")
-        throw DccReissuanceException(errorCode = DccReissuanceException.ErrorCode.DCC_RI_PIN_MISMATCH, cause = e)
+        throw when (e) {
+            is DccReissuanceException -> e
+            else -> {
+                Timber.tag(TAG).w(
+                    e,
+                    "Certificate validation failed with an unspecified error. Needs further investigation!"
+                )
+                DccReissuanceException(errorCode = DccReissuanceException.ErrorCode.DCC_RI_PIN_MISMATCH, cause = e)
+            }
+        }
     }
 
     companion object {
