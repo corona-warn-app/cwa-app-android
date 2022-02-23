41c55fea42d3409e6f0646e490f0e3931b6bf9c6
BerndMitter <Berndus@gmx.de>
2022-02-23 17:22:08 +0100

    Adjusted module

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/server/DccReissuanceServerModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/server/DccReissuanceServerModule.kt
@@ -4,9 +4,13 @@ import dagger.Module
 import dagger.Provides
 import de.rki.coronawarnapp.environment.dccreissuance.DccReissuanceServerURL
 import de.rki.coronawarnapp.http.HttpClientDefault
+import de.rki.coronawarnapp.http.HttpErrorParser
+import de.rki.coronawarnapp.tag
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import javax.inject.Qualifier
 
 @Module
 object DccReissuanceServerModule {
@@ -14,12 +18,25 @@ object DccReissuanceServerModule {
     @Provides
     fun provideApi(
         @DccReissuanceServerURL url: String,
-        @HttpClientDefault client: OkHttpClient,
+        @HttpClientDefault defaultClient: OkHttpClient,
         gsonConverterFactory: GsonConverterFactory
-    ): DccReissuanceApi = Retrofit.Builder()
-        .baseUrl(url)
-        .client(client)
-        .addConverterFactory(gsonConverterFactory)
-        .build()
-        .create(DccReissuanceApi::class.java)
+    ): DccReissuanceApi {
+        val client = defaultClient.newBuilder()
+            .apply {
+                //Remove http error parser for custom error handling
+                interceptors()
+                    .removeAll { it is HttpErrorParser }
+                    .also { Timber.tag(TAG).d("Removed %s? %b", tag<HttpErrorParser>(), it) }
+            }
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+            .create(DccReissuanceApi::class.java)
+    }
+
+    private val TAG = tag<DccReissuanceServerModule>()
 }
