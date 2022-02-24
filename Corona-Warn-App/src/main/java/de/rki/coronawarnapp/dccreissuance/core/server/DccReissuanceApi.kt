5e8961536b84d72d480cadf5f2f41cb545e3e310
BerndMitter <Berndus@gmx.de>
2022-02-24 16:02:36 +0100

    Added content type header

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/server/DccReissuanceApi.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/server/DccReissuanceApi.kt
@@ -4,10 +4,12 @@ import de.rki.coronawarnapp.dccreissuance.core.server.data.DccReissuanceRequestB
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface DccReissuanceApi {
 
+    @Headers("Content-Type: application/json")
     @POST("/api/certify/v2/reissue")
     suspend fun requestReissuance(
         @Body dccReissuanceRequestBody: DccReissuanceRequestBody
