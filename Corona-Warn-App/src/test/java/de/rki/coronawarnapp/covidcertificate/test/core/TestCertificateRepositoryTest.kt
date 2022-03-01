8b7e75db3600042652f9c1014825bead5667c272
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-03-01 11:33:02 +0100

    unit tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/test/core/TestCertificateRepositoryTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/test/core/TestCertificateRepositoryTest.kt
@@ -353,7 +353,7 @@ class TestCertificateRepositoryTest : BaseTest() {
         val instance = createInstance(this)
         instance.replaceCertificate(
             certificateToReplace = testData.personATest1Container.containerId,
-            testData.personATest2CertContainer.testCertificateQRCode!!
+            newCertificateQrCode = testData.personATest2CertContainer.testCertificateQRCode!!
         )
         with(instance.certificates.first()) {
             size shouldBe 1
@@ -371,7 +371,7 @@ class TestCertificateRepositoryTest : BaseTest() {
         val instance = createInstance(this)
         instance.replaceCertificate(
             certificateToReplace = testData.personATest1Container.containerId,
-            testData.personATest2CertContainer.testCertificateQRCode!!
+            newCertificateQrCode = testData.personATest2CertContainer.testCertificateQRCode!!
         )
         with(instance.certificates.first()) {
             size shouldBe 1
@@ -379,6 +379,42 @@ class TestCertificateRepositoryTest : BaseTest() {
         }
         with(instance.recycledCertificates.first()) {
             size shouldBe 0
+        }
+    }
+
+    @Test
+    fun `replace certificate works if new certificate already exists`() = runBlockingTest2(ignoreActive = true) {
+        coEvery { storage.load() } returns setOf(testData.personATest1StoredData, testData.personATest2StoredData)
+        val instance = createInstance(this)
+        instance.replaceCertificate(
+            certificateToReplace = testData.personATest1Container.containerId,
+            newCertificateQrCode = testData.personATest2CertContainer.testCertificateQRCode!!
+        )
+        with(instance.certificates.first()) {
+            size shouldBe 1
+            this.first().containerId shouldBe testData.personATest2CertContainer.containerId
+        }
+        with(instance.recycledCertificates.first()) {
+            size shouldBe 1
+            this.first().containerId shouldBe testData.personATest1Container.containerId
+        }
+    }
+
+    @Test
+    fun `replace certificate works if old certificate is already recycled`() = runBlockingTest2(ignoreActive = true) {
+        coEvery { storage.load() } returns setOf(testData.personATest1StoredData.copy(recycledAt = nowUTC))
+        val instance = createInstance(this)
+        instance.replaceCertificate(
+            certificateToReplace = testData.personATest1Container.containerId,
+            newCertificateQrCode = testData.personATest2CertContainer.testCertificateQRCode!!
+        )
+        with(instance.certificates.first()) {
+            size shouldBe 1
+            this.first().containerId shouldBe testData.personATest2CertContainer.containerId
+        }
+        with(instance.recycledCertificates.first()) {
+            size shouldBe 1
+            this.first().containerId shouldBe testData.personATest1Container.containerId
         }
     }
 }
