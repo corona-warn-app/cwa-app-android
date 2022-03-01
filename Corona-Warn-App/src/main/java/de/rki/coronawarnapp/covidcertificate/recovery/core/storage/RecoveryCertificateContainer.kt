9b6d591107d0a64b1cb2e95f73c3f118e8cf5b93
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-03-01 11:36:04 +0100

    revert

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/storage/RecoveryCertificateContainer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/recovery/core/storage/RecoveryCertificateContainer.kt
@@ -50,15 +50,6 @@ data class RecoveryCertificateContainer(
 
     override val recycledAt: Instant? = data.recycledAt
 
-//    override fun equals(other: Any?): Boolean {
-//        if (other !is RecoveryCertificateContainer) return false
-//        return data.recoveryCertificateQrCode == other.data.recoveryCertificateQrCode
-//    }
-//
-//    override fun hashCode(): Int {
-//        return data.recoveryCertificateQrCode.hashCode()
-//    }
-
     fun toRecoveryCertificate(
         valueSet: VaccinationValueSets? = null,
         certificateState: State,
