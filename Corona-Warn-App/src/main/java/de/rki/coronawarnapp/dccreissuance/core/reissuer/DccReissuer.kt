189e4316d1e61928af1278cf5f9d23f8bede7d89
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-01 10:49:22 +0100

    Import

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/reissuer/DccReissuer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/reissuer/DccReissuer.kt
@@ -3,6 +3,7 @@ package de.rki.coronawarnapp.dccreissuance.core.reissuer
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.CertificateReissuance
 import de.rki.coronawarnapp.covidcertificate.common.exception.InvalidHealthCertificateException
 import de.rki.coronawarnapp.dccreissuance.core.error.DccReissuanceException
+import de.rki.coronawarnapp.dccreissuance.core.error.DccReissuanceException.ErrorCode
 import de.rki.coronawarnapp.dccreissuance.core.processor.DccReissuanceProcessor
 import javax.inject.Inject
 
@@ -22,7 +23,7 @@ class DccReissuer @Inject constructor(
         val response = dccReissuanceProcessor.requestDccReissuance(dccReissuanceDescriptor)
         val dccReissuance = response.dccReissuances.find { issuance ->
             issuance.relations.any { r -> r.action == "replace" && r.index == 0 }
-        } ?: throw DccReissuanceException(DccReissuanceException.ErrorCode.DCC_RI_NO_RELATION)
+        } ?: throw DccReissuanceException(ErrorCode.DCC_RI_NO_RELATION)
 
         dccSwapper.swap(
             dccReissuance = dccReissuance,
