b07ec930044245c4974d1bb55fff465e251b911f
BerndMitter <Berndus@gmx.de>
2022-03-02 15:44:41 +0100

    Implemented DccReissuanceProcessorTest

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/core/processor/DccReissuanceProcessorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/core/processor/DccReissuanceProcessorTest.kt
@@ -1,6 +1,88 @@
 package de.rki.coronawarnapp.dccreissuance.core.processor
 
+import de.rki.coronawarnapp.ccl.dccwalletinfo.model.Certificate
+import de.rki.coronawarnapp.ccl.dccwalletinfo.model.CertificateRef
+import de.rki.coronawarnapp.ccl.dccwalletinfo.model.CertificateReissuance
+import de.rki.coronawarnapp.dccreissuance.core.error.DccReissuanceException
+import de.rki.coronawarnapp.dccreissuance.core.server.DccReissuanceServer
+import de.rki.coronawarnapp.dccreissuance.core.server.data.DccReissuanceResponse
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
 
-class DccReissuanceProcessorTest: BaseTest() {
+class DccReissuanceProcessorTest : BaseTest() {
+
+    @MockK lateinit var dccReissuanceServer: DccReissuanceServer
+
+    private val instance: DccReissuanceProcessor
+        get() = DccReissuanceProcessor(dccReissuanceServer = dccReissuanceServer)
+
+    private val testAccompanyingCertificate1 = Certificate(
+        certificateRef = CertificateRef(
+            barcodeData = "HC1:1235....",
+        )
+    )
+
+    private val testAccompanyingCertificate2 = Certificate(
+        certificateRef = CertificateRef(
+            barcodeData = "HC1:ABCD...",
+        )
+    )
+
+    private val testCertificateToReissue = Certificate(
+        certificateRef = CertificateRef(
+            barcodeData = "HC1:6789...",
+        )
+    )
+
+    private val dccReissuanceDescriptor: CertificateReissuance = mockk {
+        every { certificateToReissue } returns testCertificateToReissue
+        every { accompanyingCertificates } returns listOf(
+            testAccompanyingCertificate1,
+            testAccompanyingCertificate2
+        )
+    }
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `maps input correctly and forwards response`() = runBlockingTest {
+        val response = DccReissuanceResponse(dccReissuances = emptyList())
+
+        coEvery { dccReissuanceServer.requestDccReissuance(any(), any()) } returns response
+
+        instance.requestDccReissuance(dccReissuanceDescriptor = dccReissuanceDescriptor) shouldBe response
+
+        coVerify {
+            dccReissuanceServer.requestDccReissuance(action = "renew", certificates = listOf(
+                testAccompanyingCertificate1.certificateRef.barcodeData,
+                testAccompanyingCertificate2.certificateRef.barcodeData,
+                testCertificateToReissue.certificateRef.barcodeData
+            ))
+        }
+    }
+
+    @Test
+    fun `forwards errors`() = runBlockingTest {
+        val errorCode = DccReissuanceException.ErrorCode.DCC_RI_400
+        coEvery { dccReissuanceServer.requestDccReissuance(any(), any()) } throws DccReissuanceException(
+            errorCode = errorCode
+        )
+
+        shouldThrow<DccReissuanceException> {
+            instance.requestDccReissuance(dccReissuanceDescriptor = dccReissuanceDescriptor)
+        }.errorCode shouldBe errorCode
+    }
 }
--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/core/server/validation/DccReissuanceServerCertificateValidatorTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/core/server/validation/DccReissuanceServerCertificateValidatorTest.kt
@@ -24,6 +24,8 @@ import java.security.cert.Certificate
 
 class DccReissuanceServerCertificateValidatorTest : BaseTest() {
 
+    @MockK lateinit var appConfigProvider: AppConfigProvider
+
     private val publicKeyEncodedBase64 =
         "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA9UPAy/WbQh/4QaEY18LrEffsbFZQlCxtLpMeXRKGkUsnP21eEJtZIkoGhPAubKNV0xtHmd/AN4vYcOMLdRLwARjQ0uz2MC1UgOStClwD/Aeq8oIvawYgrRKrY5WoiN8kqEONAGecTsHRv/ypDEy6OeZATGQktyFGnuBzGdObbCX97yOYEw1BslSNlO0JC7B7p9by/xPR/1Efa38z6fkXam0lwqKBZ0APVJUkQ/VUQbnPAqgMTGmHMRDf7Mq+nHtrV4zyymwhOOiFrbcgYZ096LO26/+R2GnCPx2p79v4ZJ0/aR/2FdFfnJSqpfppxW54UvxFgRE5/YMqGDPJw7lrT91LoGfR217YJkyveD4g7HOnQaTq7NaOyFURdgDxhngVPgLwNCaW1xV60AvMr3FFCdXd5+Vl4G79bvDPxkwjS0bZlfo6PV2CUVotfvTos4N11/iaLTYZRohdJmwHdYeS2YPHNvEIbMwHA7AZubmIuHjnm3SBRK1GSy/Ats1AG3IMYVdZ6WnD/xfiXMX0knygn+eLx7H7o4t5yGY1YsNMpvGKruVTXR/g1euofK4m/ZWEbwbsPDXjYE4/d6bUGWZc7vUc/X0wW0iMxHVE8Ra4xAhBDc/p/gWB2kN6NzInFTVBERyw5WozDwOW+/8qsBtQYoNPK+I8iao6uEISLOcCXr8CAwEAAQ=="
     private val publicKeyEncoded: ByteArray by lazy { publicKeyEncodedBase64.decodeBase64()!!.toByteArray() }
@@ -35,9 +37,6 @@ class DccReissuanceServerCertificateValidatorTest : BaseTest() {
     private val serverCert: Certificate = mockk {
         every { publicKey } returns mockedPublicKey
     }
-
-    @MockK
-    lateinit var appConfigProvider: AppConfigProvider
 
     private val instance: DccReissuanceServerCertificateValidator
         get() = DccReissuanceServerCertificateValidator(appConfigProvider = appConfigProvider)
