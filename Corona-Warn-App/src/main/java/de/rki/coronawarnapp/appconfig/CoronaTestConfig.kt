87e6b17afe337d258ddee3ac2f1364dcabc498ec
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-03 14:16:03 +0100

    Provide coronaTest parameters

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/CoronaTestConfig.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/appconfig/CoronaTestConfig.kt
@@ -5,19 +5,36 @@ import org.joda.time.Duration
 
 interface CoronaTestConfig {
     val coronaRapidAntigenTestParameters: CoronaRapidAntigenTestParametersContainer
+    val coronaPCRTestParameters: CoronaPCRTestParametersContainer
 
     interface Mapper : ConfigMapper<CoronaTestConfig>
 }
 
 data class CoronaRapidAntigenTestParametersContainer(
-    val hoursToDeemTestOutdated: Duration = Duration.standardHours(DEFAULT_HOURS)
+    val hoursToDeemTestOutdated: Duration = Duration.standardHours(DEFAULT_HOURS),
+    val hoursSinceSampleCollectionToShowRiskCard: Duration = Duration.standardHours(
+        DEFAULT_HOURS_SINCE_SAMPLE_COLLECTION
+    ),
 ) {
     companion object {
         const val DEFAULT_HOURS: Long = 48
+        const val DEFAULT_HOURS_SINCE_SAMPLE_COLLECTION: Long = 48
+    }
+}
+
+data class CoronaPCRTestParametersContainer(
+    val hoursSinceTestRegistrationToShowRiskCard: Duration = Duration.standardHours(
+        DEFAULT_HOURS_SINCE_TEST_REGISTRATION
+    ),
+) {
+    companion object {
+        const val DEFAULT_HOURS_SINCE_TEST_REGISTRATION = 264L // 11 days x 24 hours
     }
 }
 
 data class CoronaTestConfigContainer(
     override val coronaRapidAntigenTestParameters: CoronaRapidAntigenTestParametersContainer =
-        CoronaRapidAntigenTestParametersContainer()
+        CoronaRapidAntigenTestParametersContainer(),
+    override val coronaPCRTestParameters: CoronaPCRTestParametersContainer =
+        CoronaPCRTestParametersContainer()
 ) : CoronaTestConfig
