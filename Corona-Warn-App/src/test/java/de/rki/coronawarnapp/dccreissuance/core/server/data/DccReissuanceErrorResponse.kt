d9084613aebe985db6240769c81563d16025d500
BerndMitter <Berndus@gmx.de>
2022-03-08 15:30:05 +0100

    removed todo

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/core/server/data/DccReissuanceErrorResponse.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/core/server/data/DccReissuanceErrorResponse.kt
@@ -2,7 +2,6 @@ package de.rki.coronawarnapp.dccreissuance.core.server.data
 
 import com.google.gson.annotations.SerializedName
 
-// TODO: Ask Max for clarification if it is error or errorCode
 data class DccReissuanceErrorResponse(
     @SerializedName("error") val error: String,
     @SerializedName("message") val message: String
