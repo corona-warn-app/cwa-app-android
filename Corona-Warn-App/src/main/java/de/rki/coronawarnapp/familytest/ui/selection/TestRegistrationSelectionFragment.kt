71fcf474ce48e3a4900047a9c9e9fd003dd9a231
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-17 14:14:18 +0100

    Setup UI flow from scanner

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/selection/TestRegistrationSelectionFragment.kt
@@ -0,0 +1,9 @@
+package de.rki.coronawarnapp.familytest.ui.selection
+
+import androidx.fragment.app.Fragment
+import de.rki.coronawarnapp.R
+
+class TestRegistrationSelectionFragment : Fragment(R.layout.fragment_test_registration_selection)
+
+
+
