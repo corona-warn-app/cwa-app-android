4936c0a99a194daf6fad05100120404a11130dbd
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-17 14:23:16 +0100

    Lint

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/selection/TestRegistrationSelectionFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/selection/TestRegistrationSelectionFragment.kt
@@ -4,6 +4,3 @@ import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 
 class TestRegistrationSelectionFragment : Fragment(R.layout.fragment_test_registration_selection)
-
-
-
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/testlist/FamilyTestListFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/testlist/FamilyTestListFragment.kt
@@ -1,10 +1,6 @@
 package de.rki.coronawarnapp.familytest.ui.testlist
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import de.rki.coronawarnapp.R
 
 class FamilyTestListFragment : Fragment(R.layout.fragment_family_test_list)
