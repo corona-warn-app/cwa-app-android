22ef6b335d8f7a1d69c5095ae830c2bb950eb883
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-17 09:10:15 +0100

    Rename

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/math/MathTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/util/math/MathTest.kt
@@ -10,28 +10,28 @@ internal class MathTest : BaseTest() {
 
     @Test
     fun roundToDecimal_1() {
-        3.5999999999999996.roundToDecimal(decimalPlacesNumber = 1u) shouldBe 3.6
-        (1.0 * 1.2 * 3.0).roundToDecimal(decimalPlacesNumber = 1u) shouldBe 3.6
+        3.5999999999999996.roundToDecimal(decimalPlaces = 1u) shouldBe 3.6
+        (1.0 * 1.2 * 3.0).roundToDecimal(decimalPlaces = 1u) shouldBe 3.6
 
-        8.9999999999999996.roundToDecimal(decimalPlacesNumber = 1u) shouldBe 9.0
-        (0.6 + 4.8 + 3.5999999999999996).roundToDecimal(decimalPlacesNumber = 1u) shouldBe 9.0
+        8.9999999999999996.roundToDecimal(decimalPlaces = 1u) shouldBe 9.0
+        (0.6 + 4.8 + 3.5999999999999996).roundToDecimal(decimalPlaces = 1u) shouldBe 9.0
     }
 
     @Test
     fun roundToDecimal_2() {
-        3.5999999999999996.roundToDecimal(decimalPlacesNumber = 2u) shouldBe 3.6
-        (1.0 * 1.2 * 3.0).roundToDecimal(decimalPlacesNumber = 2u) shouldBe 3.6
+        3.5999999999999996.roundToDecimal(decimalPlaces = 2u) shouldBe 3.6
+        (1.0 * 1.2 * 3.0).roundToDecimal(decimalPlaces = 2u) shouldBe 3.6
 
-        8.9999999999999996.roundToDecimal(decimalPlacesNumber = 2u) shouldBe 9.0
-        (0.6 + 4.8 + 3.5999999999999996).roundToDecimal(decimalPlacesNumber = 1u) shouldBe 9.0
+        8.9999999999999996.roundToDecimal(decimalPlaces = 2u) shouldBe 9.0
+        (0.6 + 4.8 + 3.5999999999999996).roundToDecimal(decimalPlaces = 1u) shouldBe 9.0
     }
 
     @Test
     fun roundToDecimal_3() {
-        3.5999999999999996.roundToDecimal(decimalPlacesNumber = 3u) shouldBe 3.6
-        (1.0 * 1.2 * 3.0).roundToDecimal(decimalPlacesNumber = 3u) shouldBe 3.6
+        3.5999999999999996.roundToDecimal(decimalPlaces = 3u) shouldBe 3.6
+        (1.0 * 1.2 * 3.0).roundToDecimal(decimalPlaces = 3u) shouldBe 3.6
 
-        8.9999999999999996.roundToDecimal(decimalPlacesNumber = 3u) shouldBe 9.0
-        (0.6 + 4.8 + 3.5999999999999996).roundToDecimal(decimalPlacesNumber = 1u) shouldBe 9.0
+        8.9999999999999996.roundToDecimal(decimalPlaces = 3u) shouldBe 9.0
+        (0.6 + 4.8 + 3.5999999999999996).roundToDecimal(decimalPlaces = 1u) shouldBe 9.0
     }
 }
