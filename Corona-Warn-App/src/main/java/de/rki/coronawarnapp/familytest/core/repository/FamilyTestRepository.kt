eebb6e5f67e87dc9ca98d291341e05971e15e5e1
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-18 09:43:28 +0100

    Repo / model

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/core/repository/FamilyTestRepository.kt
@@ -0,0 +1,67 @@
+package de.rki.coronawarnapp.familytest.core.repository
+
+import de.rki.coronawarnapp.coronatest.TestRegistrationRequest
+import de.rki.coronawarnapp.coronatest.type.TestIdentifier
+import de.rki.coronawarnapp.familytest.core.model.FamilyTest
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class FamilyTestRepository {
+
+    val familyTests: Flow<Set<FamilyTest>> = flowOf()
+    val recycledFamilyTests: Flow<Set<FamilyTest>> = flowOf()
+
+    suspend fun registerTest(
+        request: TestRegistrationRequest,
+        personName: String
+    ): FamilyTest {
+        // TBD
+        throw NotImplementedError()
+    }
+
+    suspend fun restoreTest(
+        identifier: TestIdentifier
+    ) {
+        // TBD
+    }
+
+    suspend fun recycleTest(
+        identifier: TestIdentifier
+    ) {
+        // TBD
+    }
+
+    suspend fun deleteTest(
+        identifier: TestIdentifier
+    ) {
+        // TBD
+    }
+
+    suspend fun refresh() {
+        // TBD
+    }
+
+    suspend fun markBadgeAsViewed(
+        identifier: TestIdentifier
+    ) {
+        // TBD
+    }
+
+    suspend fun updateResultNotification(
+        identifier: TestIdentifier,
+        sent: Boolean
+    ) {
+        // TBD
+    }
+
+    suspend fun markDccAsCreated(
+        identifier: TestIdentifier,
+        created: Boolean
+    ) {
+        // TBD
+    }
+
+    suspend fun clear() {
+        // TBD
+    }
+}
