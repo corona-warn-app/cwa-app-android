9e354ee2ff536ee1c88b3ef99ababb5e14757c63
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-03-29 13:07:22 +0200

    Fix test / cover LocalDate

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/familytest/core/storage/FamilyTestDatabaseTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/familytest/core/storage/FamilyTestDatabaseTest.kt
@@ -14,6 +14,7 @@ import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.joda.time.Instant
+import org.joda.time.LocalDate
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,7 +33,7 @@ class FamilyTestDatabaseTest : BaseTestInstrumentation() {
 
     private val identifier = "familyTest1"
 
-    val test = FamilyCoronaTest(
+    private val test = FamilyCoronaTest(
         personName = "Maria",
         coronaTest = CoronaTest(
             identifier = identifier,
@@ -44,13 +45,21 @@ class FamilyTestDatabaseTest : BaseTestInstrumentation() {
 
     private val identifier2 = "familyTest2"
 
-    val test2 = FamilyCoronaTest(
+    private val test2 = FamilyCoronaTest(
         personName = "Maria",
         coronaTest = CoronaTest(
             identifier = identifier2,
             type = BaseCoronaTest.Type.RAPID_ANTIGEN,
             registeredAt = Instant.parse("2022-03-24T11:00:00.000Z"),
-            registrationToken = "registrationToken1"
+            registrationToken = "registrationToken1",
+            uiState = CoronaTest.UiState(),
+            additionalInfo = CoronaTest.AdditionalInfo(
+                createdAt = Instant.EPOCH,
+                firstName = "Maria",
+                lastName = "Müller",
+                dateOfBirth = LocalDate.now(),
+                sampleCollectedAt = Instant.EPOCH
+            )
         )
     )
 
@@ -68,7 +77,7 @@ class FamilyTestDatabaseTest : BaseTestInstrumentation() {
 
         val entries = dao.getAllActive().first()
         entries.size shouldBe 1
-        entries[0].fromEntity() shouldBe test
+        entries[0]!!.fromEntity() shouldBe test
 
         dao.update(identifier) {
             it.moveToRecycleBin(now)
@@ -86,12 +95,12 @@ class FamilyTestDatabaseTest : BaseTestInstrumentation() {
 
         val entries = dao.getAllActive().first()
         entries.size shouldBe 1
-        entries[0].fromEntity() shouldBe test
+        entries[0]!!.fromEntity() shouldBe test
 
         dao.update(identifier) {
             it.updateTestResult(CoronaTestResult.PCR_NEGATIVE)
         }
-        dao.getAllActive().first()[0].test.testResult shouldBe CoronaTestResult.PCR_NEGATIVE
+        dao.getAllActive().first()[0]!!.test.testResult shouldBe CoronaTestResult.PCR_NEGATIVE
 
         dao.update(identifier) {
             it.moveToRecycleBin(now)
