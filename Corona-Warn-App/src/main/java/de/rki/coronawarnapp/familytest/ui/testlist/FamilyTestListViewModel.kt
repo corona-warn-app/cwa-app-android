96882c27b8c346cb407cb54154b5b42de1d767c4
AlexanderAlferov <64849422+AlexanderAlferov@users.noreply.github.com>
2022-03-31 20:43:37 +0300

    [Feature] Family test list (EXPOSUREAPP-12321, EXPOSUREAPP-12327) (#5014)
    * Initial layouts
    
    * All cards layout + strings
    
    * Cards and adapter
    
    * Cards and view model
    
    * Cards and view model
    
    * View model fix and module
    
    * View model refactoring
    
    * Cards fix
    
    * Mass deletion and refresh
    
    * Notification badge
    
    * Tests and fixes
    
    * Style fix
    
    * Fix
    
    * Night version of outdated and negative icons
    
    * Wire screen to results
    
    * Fix badge
    
    * Lint
    
    * Fix
    
    * Remove -> colliding with badge logic and dismiss badges automatically
    
    * Fix navigation
    
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>
    Co-authored-by: BMItr <Berndus@gmx.de>

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/testlist/FamilyTestListViewModel.kt
@@ -0,0 +1,224 @@
+package de.rki.coronawarnapp.familytest.ui.testlist
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.appconfig.AppConfigProvider
+import de.rki.coronawarnapp.appconfig.CoronaTestConfig
+import de.rki.coronawarnapp.coronatest.type.BaseCoronaTest.Type
+import de.rki.coronawarnapp.familytest.core.model.CoronaTest.State
+import de.rki.coronawarnapp.familytest.core.model.FamilyCoronaTest
+import de.rki.coronawarnapp.familytest.core.repository.FamilyTestRepository
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyPcrTestInvalidCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyPcrTestNegativeCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyPcrTestPendingCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyPcrTestPositiveCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyPcrTestRedeemedCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyRapidTestInvalidCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyRapidTestNegativeCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyRapidTestOutdatedCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyRapidTestPendingCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyRapidTestPositiveCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyRapidTestRedeemedCard
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyTestListItem
+import de.rki.coronawarnapp.util.TimeStamper
+import de.rki.coronawarnapp.util.coroutine.AppScope
+import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import de.rki.coronawarnapp.util.flow.combine
+import de.rki.coronawarnapp.util.ui.SingleLiveEvent
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
+import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+
+class FamilyTestListViewModel @AssistedInject constructor(
+    dispatcherProvider: DispatcherProvider,
+    appConfigProvider: AppConfigProvider,
+    private val familyTestRepository: FamilyTestRepository,
+    private val timeStamper: TimeStamper,
+    @AppScope private val appScope: CoroutineScope,
+) : CWAViewModel(dispatcherProvider) {
+
+    val events = SingleLiveEvent<FamilyTestListEvent>()
+    val error = SingleLiveEvent<Exception>()
+    val refreshComplete = SingleLiveEvent<Unit>()
+
+    fun onRemoveAllTests() {
+        events.postValue(FamilyTestListEvent.ConfirmRemoveAllTests)
+    }
+
+    fun onBackPressed() {
+        events.postValue(FamilyTestListEvent.NavigateBack)
+    }
+
+    fun markAllTestAsViewed() {
+        launch(appScope) {
+            familyTestRepository.familyTests.first().forEach { familyTest ->
+                familyTestRepository.markBadgeAsViewed(familyTest.identifier)
+            }
+        }
+    }
+
+    fun onRemoveTestConfirmed(test: FamilyCoronaTest?) {
+        launch(appScope) {
+            if (test == null) {
+                familyTestRepository.familyTests.first().forEach { familyTest ->
+                    familyTestRepository.moveTestToRecycleBin(familyTest.identifier)
+                }
+            } else {
+                familyTestRepository.moveTestToRecycleBin(test.identifier)
+            }
+        }
+    }
+
+    fun onRefreshTests() {
+        launch(appScope) {
+            val result = familyTestRepository.refresh().also {
+                refreshComplete.postValue(null)
+            }
+            if (result.isNotEmpty()) {
+                error.postValue(result.values.first())
+            }
+        }
+    }
+
+    val familyTests: LiveData<List<FamilyTestListItem>> = combine(
+        familyTestRepository.familyTests,
+        appConfigProvider.currentConfig.map { it.coronaTestParameters }.distinctUntilChanged()
+    ) { familyTests, coronaTestParameters ->
+        familyTests
+            .sortedBy {
+                if (it.type == Type.RAPID_ANTIGEN) it.coronaTest.testTakenAt else it.registeredAt
+            }
+            .map {
+                when (it.coronaTest.type) {
+                    Type.PCR -> it.toPCRTestCardItem(coronaTestParameters)
+                    Type.RAPID_ANTIGEN -> it.toRapidTestCardItem(coronaTestParameters)
+                }
+            }.also {
+                if (it.isEmpty()) {
+                    onBackPressed()
+                }
+            }
+    }.asLiveData(context = dispatcherProvider.Default)
+
+    private fun FamilyCoronaTest.toPCRTestCardItem(coronaTestConfig: CoronaTestConfig): FamilyTestListItem =
+        when (this.coronaTest.getUiState(timeStamper.nowUTC, coronaTestConfig)) {
+            State.PENDING -> FamilyPcrTestPendingCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.NEGATIVE -> FamilyPcrTestNegativeCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.POSITIVE -> FamilyPcrTestPositiveCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.INVALID -> FamilyPcrTestInvalidCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.REDEEMED -> FamilyPcrTestRedeemedCard.Item(
+                familyCoronaTest = this,
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                },
+                onDeleteTest = { events.postValue(FamilyTestListEvent.ConfirmRemoveTest(this)) }
+            )
+            // Should not be possible
+            State.OUTDATED,
+            State.RECYCLED -> FamilyPcrTestInvalidCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {},
+                onSwipeItem = { _, _ -> }
+            )
+        }
+
+    private fun FamilyCoronaTest.toRapidTestCardItem(coronaTestConfig: CoronaTestConfig): FamilyTestListItem =
+        when (this.coronaTest.getUiState(timeStamper.nowUTC, coronaTestConfig)) {
+            State.PENDING -> FamilyRapidTestPendingCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.NEGATIVE -> FamilyRapidTestNegativeCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.POSITIVE -> FamilyRapidTestPositiveCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.INVALID -> FamilyRapidTestInvalidCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {
+                    events.postValue(FamilyTestListEvent.NavigateToDetails(familyCoronaTest = this))
+                },
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                }
+            )
+            State.REDEEMED -> FamilyRapidTestRedeemedCard.Item(
+                familyCoronaTest = this,
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                },
+                onDeleteTest = { events.postValue(FamilyTestListEvent.ConfirmRemoveTest(this)) }
+            )
+            State.OUTDATED -> FamilyRapidTestOutdatedCard.Item(
+                familyCoronaTest = this,
+                onSwipeItem = { familyCoronaTest, position ->
+                    events.postValue(FamilyTestListEvent.ConfirmSwipeTest(familyCoronaTest, position))
+                },
+                onDeleteTest = { events.postValue(FamilyTestListEvent.ConfirmRemoveTest(this)) }
+            )
+            // Should not be possible
+            State.RECYCLED -> FamilyRapidTestInvalidCard.Item(
+                familyCoronaTest = this,
+                onClickAction = {},
+                onSwipeItem = { _, _ -> }
+            )
+        }
+
+    @AssistedFactory
+    interface Factory : SimpleCWAViewModelFactory<FamilyTestListViewModel>
+}
---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/familytest/ui/testlist/items/FamilyPcrTestPositiveCard.kt
@@ -0,0 +1,49 @@
+package de.rki.coronawarnapp.familytest.ui.testlist.items
+
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import de.rki.coronawarnapp.R
+import de.rki.coronawarnapp.databinding.FamilyPcrTestCardPositivBinding
+import de.rki.coronawarnapp.familytest.core.model.FamilyCoronaTest
+import de.rki.coronawarnapp.familytest.ui.testlist.FamilyTestListAdapter
+import de.rki.coronawarnapp.familytest.ui.testlist.items.FamilyPcrTestPositiveCard.Item
+import de.rki.coronawarnapp.util.list.Swipeable
+import de.rki.coronawarnapp.util.lists.diffutil.HasPayloadDiffer
+
+class FamilyPcrTestPositiveCard(parent: ViewGroup) :
+    FamilyTestListAdapter.FamilyTestListVH<Item, FamilyPcrTestCardPositivBinding>(
+        R.layout.home_card_container_layout,
+        parent
+    ),
+    Swipeable {
+
+    private var latestItem: Item? = null
+
+    override fun onSwipe(holder: RecyclerView.ViewHolder, direction: Int) {
+        latestItem?.let { it.onSwipeItem(it.familyCoronaTest, holder.bindingAdapterPosition) }
+    }
+
+    override val viewBinding = lazy {
+        FamilyPcrTestCardPositivBinding
+            .inflate(layoutInflater, itemView.findViewById(R.id.card_container), true)
+    }
+
+    override val onBindData: FamilyPcrTestCardPositivBinding.(
+        item: Item,
+        payloads: List<Any>
+    ) -> Unit = { item, payloads ->
+        latestItem = payloads.filterIsInstance<Item>().lastOrNull() ?: item
+        title.text = latestItem!!.familyCoronaTest.personName
+        val userDate = latestItem!!.familyCoronaTest.coronaTest.getFormattedRegistrationDate()
+        date.text = resources.getString(R.string.family_tests_cards_pcr_date, userDate)
+        notificationBadge.isVisible = latestItem!!.familyCoronaTest.hasBadge
+        itemView.setOnClickListener { latestItem!!.onClickAction(item) }
+    }
+
+    data class Item(
+        override val familyCoronaTest: FamilyCoronaTest,
+        val onClickAction: (Item) -> Unit,
+        val onSwipeItem: (FamilyCoronaTest, Int) -> Unit,
+    ) : FamilyTestListItem, HasPayloadDiffer
+}
