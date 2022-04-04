ec978f1dd824e024ad1450a169e65b9d5a7311d8
BerndMitter <Berndus@gmx.de>
2022-04-04 18:02:36 +0200

    kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/check/RevocationCheck.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/check/RevocationCheck.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 
 class RevocationCheck @Inject constructor(
     private val revocationRepository: RevocationRepository
-){
+) {
 
     suspend fun checkDccAgainstCachedRevocationList(dcc: CwaCovidCertificate): Boolean {
         TODO("Not yet implemented")
