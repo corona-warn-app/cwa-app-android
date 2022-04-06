61786347203dbc82528632113bcc2b2afee4860a
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-04-06 15:47:39 +0200

    skeleton

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/profile/storage/ProfileRepository.kt
@@ -0,0 +1,45 @@
+package de.rki.coronawarnapp.profile.storage
+
+import de.rki.coronawarnapp.profile.model.Profile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import org.joda.time.LocalDate
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ProfileRepository @Inject constructor() {
+    val profileFlow: Flow<Set<Profile>> = flowOf(setOf(dummy1, dummy2))
+
+    fun deleteProfile(id: String) {
+        // to do
+    }
+
+    fun upsertProfile(profile: Profile) {
+        // to do
+    }
+}
+
+val dummy1 = Profile(
+    id = "1",
+    firstName = "First name",
+    lastName = "Last name",
+    birthDate = LocalDate(1981, 3, 20),
+    street = "Main street",
+    zipCode = "12132",
+    city = "London",
+    phone = "111111111",
+    email = "email@example.com"
+)
+
+val dummy2 = Profile(
+    id = "2",
+    firstName = "Jimmy",
+    lastName = "Fallon",
+    birthDate = null,
+    street = "",
+    zipCode = "",
+    city = "New York",
+    phone = "",
+    email = ""
+)
