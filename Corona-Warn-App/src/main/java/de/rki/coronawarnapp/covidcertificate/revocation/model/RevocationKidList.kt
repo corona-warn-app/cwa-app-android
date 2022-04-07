f6e255d17f8e52b7eeb4cca0ef9924414f637f84
BerndMitter <Berndus@gmx.de>
2022-04-07 15:29:06 +0200

    Implemented RevocationKidList

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationKidList.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationKidList.kt
@@ -1,8 +1,16 @@
 package de.rki.coronawarnapp.covidcertificate.revocation.model
 
+import com.google.gson.annotations.SerializedName
+
 data class RevocationKidList(
+    @SerializedName("items")
     val items: List<RevocationKidListItem>
 )
 
-// To Do: Implement
-interface RevocationKidListItem
+data class RevocationKidListItem(
+    @SerializedName("kid")
+    val kid: String,
+
+    @SerializedName("hashTypes")
+    val hashTypes: List<String>
+)
