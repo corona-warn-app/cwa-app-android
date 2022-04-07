86927c7a8d226363e5351f66bae42f79d74b7b5a
Juraj Kusnier <jurajkusnier@users.noreply.github.com>
2022-04-07 13:15:04 +0200

    FIX: Navigation library (EXPOSUREAPP-12571) (#5061)
    * downgrade nav library
    
    * fix SubmissionDeletionWarningFragment
    
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/notifications/NavDeepLinkBuilderFactory.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/notifications/NavDeepLinkBuilderFactory.kt
@@ -1,13 +1,13 @@
 package de.rki.coronawarnapp.util.notifications
 
 import android.content.Context
-import androidx.navigation.NavDeepLinkBuilder
 import dagger.Reusable
+import de.rki.coronawarnapp.util.SafeNavDeepLinkBuilder
 import javax.inject.Inject
 
 @Reusable
 class NavDeepLinkBuilderFactory @Inject constructor() {
-    fun create(context: Context): NavDeepLinkBuilder {
-        return NavDeepLinkBuilder(context)
+    fun create(context: Context): SafeNavDeepLinkBuilder {
+        return SafeNavDeepLinkBuilder(context)
     }
 }
