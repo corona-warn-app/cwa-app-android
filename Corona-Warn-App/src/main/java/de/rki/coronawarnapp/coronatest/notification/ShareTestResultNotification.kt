bf1defffb336522ab8e92e54f8e6a38b23d5dabd
Juraj Kusnier <jurajkusnier@gmail.com>
2022-04-08 11:18:46 +0200

    reminder notification with testIdentifier

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/coronatest/notification/ShareTestResultNotification.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/coronatest/notification/ShareTestResultNotification.kt
@@ -1,15 +1,15 @@
 package de.rki.coronawarnapp.coronatest.notification
 
 import android.content.Context
-import android.os.Bundle
 import dagger.Reusable
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.coronatest.type.BaseCoronaTest
+import de.rki.coronawarnapp.coronatest.type.TestIdentifier
 import de.rki.coronawarnapp.notification.GeneralNotifications
-import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_RESULT_NOTIFICATION_INITIAL_OFFSET
-import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_RESULT_NOTIFICATION_INTERVAL
+import de.rki.coronawarnapp.notification.NotificationConstants
 import de.rki.coronawarnapp.tag
 import de.rki.coronawarnapp.ui.main.MainActivity
+import de.rki.coronawarnapp.ui.submission.warnothers.SubmissionResultPositiveOtherWarningNoConsentFragmentArgs
 import de.rki.coronawarnapp.util.TimeStamper
 import de.rki.coronawarnapp.util.di.AppContext
 import de.rki.coronawarnapp.util.notifications.NavDeepLinkBuilderFactory
@@ -25,19 +25,25 @@ class ShareTestResultNotification @Inject constructor(
     private val navDeepLinkBuilderFactory: NavDeepLinkBuilderFactory,
 ) {
 
-    fun scheduleSharePositiveTestResultReminder(testType: BaseCoronaTest.Type, notificationId: Int) {
+    fun scheduleSharePositiveTestResultReminder(
+        testType: BaseCoronaTest.Type,
+        testIdentifier: TestIdentifier,
+        notificationId: Int
+    ) {
         notificationHelper.scheduleRepeatingNotification(
             testType,
-            timeStamper.nowUTC.plus(POSITIVE_RESULT_NOTIFICATION_INITIAL_OFFSET),
-            POSITIVE_RESULT_NOTIFICATION_INTERVAL,
-            notificationId
+            testIdentifier,
+            timeStamper.nowUTC.plus(NotificationConstants.POSITIVE_RESULT_NOTIFICATION_INITIAL_OFFSET),
+            NotificationConstants.POSITIVE_RESULT_NOTIFICATION_INTERVAL,
+            notificationId,
         )
     }
 
-    fun showSharePositiveTestResultNotification(notificationId: Int, testType: BaseCoronaTest.Type) {
+    fun showSharePositiveTestResultNotification(notificationId: Int, testIdentifier: TestIdentifier) {
         Timber.tag(TAG).d("showSharePositiveTestResultNotification(notificationId=$notificationId)")
 
-        val args = Bundle().apply { putSerializable("testType", testType) }
+        val args = SubmissionResultPositiveOtherWarningNoConsentFragmentArgs(testIdentifier = testIdentifier)
+            .toBundle()
 
         val pendingIntent = navDeepLinkBuilderFactory.create(context)
             .setGraph(R.navigation.nav_graph)
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/notification/NotificationReceiver.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/notification/NotificationReceiver.kt
@@ -10,6 +10,7 @@ import de.rki.coronawarnapp.notification.NotificationConstants.NOTIFICATION_ID
 import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_LEGACY_RESULT_NOTIFICATION_ID
 import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_PCR_RESULT_NOTIFICATION_ID
 import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_RAT_RESULT_NOTIFICATION_ID
+import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_RESULT_NOTIFICATION_TEST_ID
 import de.rki.coronawarnapp.notification.NotificationConstants.POSITIVE_RESULT_NOTIFICATION_TEST_TYPE
 import de.rki.coronawarnapp.tag
 import timber.log.Timber
@@ -27,16 +28,23 @@ class NotificationReceiver : BroadcastReceiver() {
             POSITIVE_LEGACY_RESULT_NOTIFICATION_ID,
             POSITIVE_PCR_RESULT_NOTIFICATION_ID,
             POSITIVE_RAT_RESULT_NOTIFICATION_ID -> {
+                val testIdentifier = intent.getStringExtra(POSITIVE_RESULT_NOTIFICATION_TEST_ID)
                 val testTypeRaw = intent.getStringExtra(POSITIVE_RESULT_NOTIFICATION_TEST_TYPE)
-                val testType = BaseCoronaTest.Type.values().first { it.raw == testTypeRaw }
-                Timber.tag(TAG).v(
-                    "NotificationReceiver received intent to show a positive test result notification for test type %s",
-                    testType
-                )
-                shareTestResultNotificationService.maybeShowSharePositiveTestResultNotification(
-                    notificationId,
-                    testType
-                )
+                val testType = BaseCoronaTest.Type.values().firstOrNull { it.raw == testTypeRaw }
+                if (testType == null || testIdentifier == null) {
+                    Timber.tag(TAG)
+                        .e("Invalid arguments testTypeRaw = %s, testIdentifier = %s", testTypeRaw, testIdentifier)
+                } else {
+                    Timber.tag(TAG).v(
+                        "NotificationReceiver received intent to show a positive test result notification for test type %s",
+                        testType
+                    )
+                    shareTestResultNotificationService.maybeShowSharePositiveTestResultNotification(
+                        notificationId,
+                        testType,
+                        testIdentifier
+                    )
+                }
             }
             else ->
                 Timber.tag(TAG).d("NotificationReceiver received an undefined notificationId: %s", notificationId)
