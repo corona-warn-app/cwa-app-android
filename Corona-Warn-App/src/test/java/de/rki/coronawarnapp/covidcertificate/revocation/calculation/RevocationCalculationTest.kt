b5a2d2a3648e124e114e9837caca94ac1d8612de
BMItr <Berndus@gmx.de>
2022-04-08 11:57:39 +0200

    Revocation calculations (EXPOSUREAPP-12553) (#5060)
    * Added RevocationModule
    
    * Added models
    
    * Added api and server
    
    * Added RevocationEntryCoordinates
    
    * Added update
    
    * Added storage and repo
    
    * Added RevocationCheck
    
    * Added RevocationException
    
    * Update RevocationUpdater.kt
    
    * Adjusted to do
    
    * Added RevocationCalculation
    
    * little clean
    
    * fix ident
    
    * kt
    
    * Update WorkerBinderTest.kt
    
    * satisfy CI - hopefully
    
    * satisfy CI
    
    * missing space
    
    * tests
    
    * Implemented calculateRevocationEntryTypeUCI
    
    * Added RevocationCalculationTest
    
    * Implemented calculateRevocationEntryTypeCOUNTRYCODEUCI
    
    * Implemented calculateRevocationEntryTypeSIGNATURE

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/covidcertificate/revocation/calculation/RevocationCalculationTest.kt
@@ -0,0 +1,66 @@
+package de.rki.coronawarnapp.covidcertificate.revocation.calculation
+
+import com.google.gson.Gson
+import de.rki.coronawarnapp.covidcertificate.DaggerCovidCertificateTestComponent
+import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
+import de.rki.coronawarnapp.covidcertificate.common.certificate.DccQrCodeExtractor
+import de.rki.coronawarnapp.covidcertificate.revocation.model.RevocationEntryCoordinates.Type
+import de.rki.coronawarnapp.util.serialization.BaseGson
+import de.rki.coronawarnapp.util.serialization.fromJson
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+import testhelpers.coroutines.runBlockingTest2
+import java.nio.file.Paths
+import javax.inject.Inject
+
+class RevocationCalculationTest : BaseTest() {
+
+    @Inject lateinit var dccQrCodeExtractor: DccQrCodeExtractor
+    @BaseGson @Inject lateinit var gson: Gson
+
+    private val jsonFile = Paths.get(
+        "src",
+        "test",
+        "resources",
+        "revocation",
+        "RevocationCalculationSampleData.json"
+    ).toFile()
+
+    @BeforeEach
+    fun setup() {
+        DaggerCovidCertificateTestComponent.factory().create().inject(this)
+    }
+
+    @Test
+    fun `run test cases`() = runBlockingTest2 {
+        val testCases: List<RevocationCalculationSampleData> = jsonFile.bufferedReader().use { gson.fromJson(it) }
+        testCases.forEach {
+            val dgc = createMockCert(it.barcodeData)
+
+            val uci = dgc.calculateRevocationEntryForType(Type.UCI)
+            println("calculated uci=$uci, expUCI=${it.expUCI}")
+            uci shouldBe it.expUCI
+
+            val countryUci = dgc.calculateRevocationEntryForType(Type.COUNTRYCODEUCI)
+            println("calculated countryUci=$countryUci, expCOUNTRYCODEUCI=${it.expCOUNTRYCODEUCI}")
+            countryUci shouldBe it.expCOUNTRYCODEUCI
+
+            val signature = dgc.calculateRevocationEntryForType(Type.SIGNATURE)
+            println("calculated signature=$signature, expSIGNATURE=${it.expSIGNATURE}")
+            signature shouldBe it.expSIGNATURE
+        }
+    }
+
+    private suspend fun createMockCert(barcodeData: String): CwaCovidCertificate {
+        val metadata = dccQrCodeExtractor.extract(barcodeData)
+        return mockk {
+            every { uniqueCertificateIdentifier } returns metadata.data.certificate.payload.uniqueCertificateIdentifier
+            every { headerIssuer } returns metadata.data.header.issuer
+            every { dccData.dscMessage } returns metadata.data.dscMessage
+        }
+    }
+}
