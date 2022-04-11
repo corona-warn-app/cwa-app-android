4b84c0367640cbecfd29bcb5ad330d19ddc9a028
BerndMitter <Berndus@gmx.de>
2022-04-11 16:59:30 +0200

    added explanations to EntryCoord types

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationEntryCoordinates.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationEntryCoordinates.kt
@@ -3,8 +3,15 @@ package de.rki.coronawarnapp.covidcertificate.revocation.model
 import okio.ByteString
 
 data class RevocationEntryCoordinates(
+    /** The hex-encoded KID of a DCC (usually 16 characters) */
     val kid: ByteString,
+
+    /** See [RevocationHashType] */
     val type: RevocationHashType,
+
+    /** A hex-encoded byte representing the first byte of the hash */
     val x: ByteString,
+
+    /** A hex-encoded byte representing the second byte of the hash */
     val y: ByteString
 )
