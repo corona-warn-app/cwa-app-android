1c9456eff7b0facbcfe156adb8c85e58ac6df21d
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-04-11 15:56:49 +0200

    No persistence

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationEntryCoordinates.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationEntryCoordinates.kt
@@ -1,18 +1,10 @@
 package de.rki.coronawarnapp.covidcertificate.revocation.model
 
-import com.google.gson.annotations.SerializedName
 import okio.ByteString
 
 data class RevocationEntryCoordinates(
-    /** The hex-encoded KID of a DCC (usually 16 characters) */
-    @SerializedName("kid") val kid: ByteString,
-
-    /** See [RevocationHashType] */
-    @SerializedName("type") val type: RevocationHashType,
-
-    /** A hex-encoded byte representing the first byte of the hash */
-    @SerializedName("x") val x: ByteString,
-
-    /** A hex-encoded byte representing the second byte of the hash */
-    @SerializedName("y") val y: ByteString
+    val kid: ByteString,
+    val type: RevocationHashType,
+    val x: ByteString,
+    val y: ByteString
 )
