b3e873e2138bce6f06a3347f4687cf1eebb5c562
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-04-11 16:36:50 +0200

    Update DccRevocationChecker.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/check/DccRevocationChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/check/DccRevocationChecker.kt
@@ -1,14 +1,14 @@
 package de.rki.coronawarnapp.covidcertificate.revocation.check
 
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccData
-import de.rki.coronawarnapp.covidcertificate.revocation.model.RevocationEntryCoordinates
+import de.rki.coronawarnapp.covidcertificate.revocation.model.CachedRevocationChunk
 import javax.inject.Inject
 
 class DccRevocationChecker @Inject constructor() {
 
     suspend fun isRevoked(
         dccData: DccData<*>,
-        revocationList: List<RevocationEntryCoordinates>,
+        revocationList: List<CachedRevocationChunk>,
     ): Boolean {
 
         return false
