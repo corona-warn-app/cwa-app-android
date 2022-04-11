fed84880551ad49358101c1baaa2c9be00970217
BerndMitter <Berndus@gmx.de>
2022-04-11 11:20:28 +0200

    Added CachedRevocationChunk

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationChunk.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationChunk.kt
@@ -1,7 +1,16 @@
 package de.rki.coronawarnapp.covidcertificate.revocation.model
 
+import com.google.gson.annotations.SerializedName
 import okio.ByteString
 
 data class RevocationChunk(
-    val hashes: List<ByteString>
+    @SerializedName("hashes") val hashes: List<ByteString>
+)
+
+data class CachedRevocationChunk(
+    @SerializedName("kid") val kid: String,
+    @SerializedName("hashType") val hashType: RevocationHashType,
+    @SerializedName("x") val x: String,
+    @SerializedName("y") val y: String,
+    @SerializedName("revocationChunk") val revocationChunk: RevocationChunk
 )
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationKidList.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/revocation/model/RevocationKidList.kt
@@ -3,14 +3,10 @@ package de.rki.coronawarnapp.covidcertificate.revocation.model
 import com.google.gson.annotations.SerializedName
 
 data class RevocationKidList(
-    @SerializedName("items")
-    val items: List<RevocationKidListItem>
+    @SerializedName("items") val items: List<RevocationKidListItem>
 )
 
 data class RevocationKidListItem(
-    @SerializedName("kid")
-    val kid: String,
-
-    @SerializedName("hashTypes")
-    val hashTypes: List<String>
+    @SerializedName("kid") val kid: String,
+    @SerializedName("hashTypes") val hashTypes: List<String>
 )
