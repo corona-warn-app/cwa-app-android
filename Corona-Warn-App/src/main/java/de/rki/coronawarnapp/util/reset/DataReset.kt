0f8b38c5862ca81e91632e2cca4eeddb1737e026
BerndMitter <Berndus@gmx.de>
2022-05-03 23:45:27 +0200

    Added Resettable and new DataReset

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/reset/DataReset.kt
@@ -0,0 +1,28 @@
+package de.rki.coronawarnapp.util.reset
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class DataReset @Inject constructor(
+    private val resettableDataProvider: Provider<Set<@JvmSuppressWildcards Resettable>>
+) {
+
+    private val mutex = Mutex()
+    private val resettableData get() = resettableDataProvider.get()
+
+    suspend fun clearAllLocalData() = mutex.withLock {
+        Timber.w("CWA LOCAL DATA DELETION INITIATED.")
+
+        resettableData.forEach { data ->
+            runCatching { data.reset() }
+                .onFailure { Timber.e(it, "Failed to reset %s", data::class.java.simpleName) }
+        }
+
+        Timber.w("CWA LOCAL DATA DELETION COMPLETED.")
+    }
+}
