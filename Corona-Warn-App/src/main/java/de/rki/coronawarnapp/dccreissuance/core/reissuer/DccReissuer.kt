cf15c31dfbe85ad3e738ed2f53864be565e48743
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-05-05 13:17:14 +0200

    Reissuance API update (EXPOSUREAPP-12860) (#5147)
    * remove replace logic
    
    * refactor handler
    
    * add exception
    
    * adjust tests
    
    * klint
    
    * handler tests
    
    * ccl config version
    
    * fallback v2
    
    * move constant
    
    * json update
    
    * tests
    
    * klint detekt
    
    * parser test
    
    * adjust test to use legacy wallet info
    
    Co-authored-by: Alex Paulescu <alex.paulescu@gmail.com>

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/reissuer/DccReissuer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/dccreissuance/core/reissuer/DccReissuer.kt
@@ -1,33 +1,68 @@
 package de.rki.coronawarnapp.dccreissuance.core.reissuer
 
 import de.rki.coronawarnapp.ccl.dccwalletinfo.model.CertificateReissuance
+import de.rki.coronawarnapp.ccl.dccwalletinfo.model.CertificateReissuanceItem
+import de.rki.coronawarnapp.covidcertificate.common.certificate.DccQrCodeExtractor
 import de.rki.coronawarnapp.covidcertificate.common.exception.InvalidHealthCertificateException
+import de.rki.coronawarnapp.covidcertificate.common.qrcode.QrCodeString
 import de.rki.coronawarnapp.dccreissuance.core.error.DccReissuanceException
-import de.rki.coronawarnapp.dccreissuance.core.error.DccReissuanceException.ErrorCode
-import de.rki.coronawarnapp.dccreissuance.core.processor.DccReissuanceProcessor
+import de.rki.coronawarnapp.dccreissuance.core.server.DccReissuanceServer
+import de.rki.coronawarnapp.qrcode.handler.DccQrCodeHandler
+import timber.log.Timber
 import javax.inject.Inject
 
 class DccReissuer @Inject constructor(
-    private val dccSwapper: DccSwapper,
-    private val dccReissuanceProcessor: DccReissuanceProcessor,
+    private val dccReissuanceServer: DccReissuanceServer,
+    private val dccQrCodeHandler: DccQrCodeHandler,
+    private val dccQrCodeExtractor: DccQrCodeExtractor,
 ) {
 
     /**
-     * Requests new certificate from the server and replaces the old one in the holder's wallet
+     * Requests a new certificate, registers it and moves certificates into the recycle bin as indicated
      */
     @Throws(
         DccReissuanceException::class,
         InvalidHealthCertificateException::class
     )
-    suspend fun startReissuance(dccReissuanceDescriptor: CertificateReissuance) {
-        val response = dccReissuanceProcessor.requestDccReissuance(dccReissuanceDescriptor)
-        val dccReissuance = response.dccReissuances.find { issuance ->
-            issuance.relations.any { r -> r.action == "replace" && r.index == 0 }
-        } ?: throw DccReissuanceException(ErrorCode.DCC_RI_NO_RELATION)
-
-        dccSwapper.swap(
-            dccReissuance = dccReissuance,
-            certificateToRecycle = dccReissuanceDescriptor.certificateToReissue
-        )
+    suspend fun startReissuance(certificateReissuance: CertificateReissuance) {
+        certificateReissuance.migrateLegacyCertificate().certificates?.forEach {
+            reissue(it)
+        }
     }
+
+    suspend fun reissue(item: CertificateReissuanceItem) {
+        val allQrCodes = mutableListOf(item.certificateToReissue) + item.accompanyingCertificates
+        val response = dccReissuanceServer.requestDccReissuance(
+            action = item.action,
+            certificates = allQrCodes.map { it.certificateRef.barcodeData }
+        )
+
+        response.dccReissuances.forEach { issuance ->
+            register(issuance.certificate)
+            issuance.relations.filter { relation ->
+                relation.action == ACTION_REPLACE
+            }.forEach {
+                try {
+                    moveToBin(allQrCodes[it.index].certificateRef.barcodeData)
+                } catch (e: IndexOutOfBoundsException) {
+                    Timber.d(e, "No certificate at index ${it.index}. Size is ${allQrCodes.size}")
+                }
+            }
+        }
+    }
+
+    private suspend fun moveToBin(qrCodeString: QrCodeString) {
+        val qrCode = qrCodeString.extract()
+        dccQrCodeHandler.moveToRecycleBin(qrCode)
+    }
+
+    private suspend fun register(qrCodeString: QrCodeString) {
+        val qrCode = qrCodeString.extract()
+        dccQrCodeHandler.register(qrCode)
+    }
+
+    private suspend fun QrCodeString.extract() = dccQrCodeExtractor.extract(this)
 }
+
+internal const val ACTION_RENEW = "renew"
+internal const val ACTION_REPLACE = "replace"
