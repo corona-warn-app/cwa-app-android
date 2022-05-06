65bc6c8304ac53620f80e816e7904ede32db4acb
BerndMitter <Berndus@gmx.de>
2022-05-06 18:48:45 +0200

    ValueSets resetable

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/valueset/CertificateValueSetModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/valueset/CertificateValueSetModule.kt
@@ -1,14 +1,18 @@
 package de.rki.coronawarnapp.covidcertificate.valueset
 
 import android.content.Context
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
+import dagger.multibindings.IntoSet
 import de.rki.coronawarnapp.covidcertificate.valueset.server.CertificateValueSet
 import de.rki.coronawarnapp.covidcertificate.valueset.server.CertificateValueSetApiV1
+import de.rki.coronawarnapp.covidcertificate.valueset.server.CertificateValueSetServer
 import de.rki.coronawarnapp.environment.download.DownloadCDNHttpClient
 import de.rki.coronawarnapp.environment.download.DownloadCDNServerUrl
 import de.rki.coronawarnapp.util.di.AppContext
+import de.rki.coronawarnapp.util.reset.Resettable
 import okhttp3.Cache
 import okhttp3.CacheControl
 import okhttp3.Interceptor
@@ -18,8 +22,8 @@ import retrofit2.Retrofit
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-@Module
-class CertificateValueSetModule {
+@Module(includes = [CertificateValueSetModule.BindsModule::class])
+object CertificateValueSetModule {
 
     @Reusable
     @CertificateValueSet
@@ -51,25 +55,35 @@ class CertificateValueSetModule {
             .create(CertificateValueSetApiV1::class.java)
     }
 
-    private class CacheInterceptor : Interceptor {
-        override fun intercept(chain: Interceptor.Chain): Response {
-            val response = chain.proceed(chain.request())
+    @Module
+    internal interface BindsModule {
 
-            val cacheControl = CacheControl.Builder()
-                .maxAge(300, TimeUnit.SECONDS)
-                .build()
+        @Binds
+        @IntoSet
+        fun bindResettableValueSetsRepository(resettable: ValueSetsRepository): Resettable
 
-            // We cache as we please
-            val cacheHeader = "Cache-Control"
-            return response.newBuilder()
-                .removeHeader("Pragma")
-                .removeHeader(cacheHeader)
-                .addHeader(cacheHeader, cacheControl.toString())
-                .build()
-        }
+        @Binds
+        @IntoSet
+        fun bindResettableCertificateValueSetServer(resettable: CertificateValueSetServer): Resettable
     }
+}
 
-    companion object {
-        private const val CACHE_SIZE_5MB = 5 * 1024 * 1024L // 5MB
+private const val CACHE_SIZE_5MB = 5 * 1024 * 1024L // 5MB
+
+private class CacheInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+
+        val cacheControl = CacheControl.Builder()
+            .maxAge(300, TimeUnit.SECONDS)
+            .build()
+
+        // We cache as we please
+        val cacheHeader = "Cache-Control"
+        return response.newBuilder()
+            .removeHeader("Pragma")
+            .removeHeader(cacheHeader)
+            .addHeader(cacheHeader, cacheControl.toString())
+            .build()
     }
 }
