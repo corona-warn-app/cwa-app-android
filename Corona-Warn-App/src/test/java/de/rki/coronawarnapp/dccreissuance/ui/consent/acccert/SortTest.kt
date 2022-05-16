e2c682f3a8ee43163711f99f4bcc883bbb1d8c47
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-05-16 15:48:46 +0200

    Reissuance: Accompanying certificates screen (EXPOSUREAPP-13068) (#5171)
    * add accompanying certificates button
    
    * accompanying certs screen
    
    * klint
    
    * strings
    
    * fix test
    
    * fix test
    
    * fix test
    consolidate acc certificates
    
    * rename
    
    * view model test
    
    * fix merge
    
    * sort test
    
    * move adapter declaration
    
    * Update Corona-Warn-App/src/main/res/values-de/dcc_reissuance_strings.xml
    
    Co-authored-by: KathrinLuecke <91470863+KathrinLuecke@users.noreply.github.com>
    
    Co-authored-by: Mohamed Metwalli <mohamed.metwalli@sap.com>
    Co-authored-by: KathrinLuecke <91470863+KathrinLuecke@users.noreply.github.com>

---
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/dccreissuance/ui/consent/acccert/SortTest.kt
@@ -0,0 +1,43 @@
+package de.rki.coronawarnapp.dccreissuance.ui.consent.acccert
+
+import de.rki.coronawarnapp.covidcertificate.recovery.core.qrcode.RecoveryCertificateQRCode
+import de.rki.coronawarnapp.covidcertificate.test.core.qrcode.TestCertificateQRCode
+import de.rki.coronawarnapp.covidcertificate.vaccination.core.qrcode.VaccinationCertificateQRCode
+import de.rki.coronawarnapp.dccreissuance.ui.consent.acccerts.sort
+import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import org.joda.time.Instant
+import org.joda.time.LocalDate
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import testhelpers.BaseTest
+
+class SortTest : BaseTest() {
+
+    @MockK lateinit var vacQrCode: VaccinationCertificateQRCode
+    @MockK lateinit var recQrCode: RecoveryCertificateQRCode
+    @MockK lateinit var testQrCode: TestCertificateQRCode
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `sort works`() {
+        every { vacQrCode.data.certificate.vaccination.vaccinatedOn } returns LocalDate.parse("2022-01-01")
+        every { recQrCode.data.certificate.recovery.testedPositiveOn } returns LocalDate.parse("2022-01-03")
+        every { testQrCode.data.certificate.test.sampleCollectedAt } returns Instant.parse("2022-01-05")
+        listOf(vacQrCode, testQrCode, recQrCode).sort() shouldBe listOf(testQrCode, recQrCode, vacQrCode)
+    }
+
+    @Test
+    fun `sort works 2`() {
+        every { vacQrCode.data.certificate.vaccination.vaccinatedOn } returns LocalDate.parse("2022-02-01")
+        every { recQrCode.data.certificate.recovery.testedPositiveOn } returns LocalDate.parse("2022-01-03")
+        every { testQrCode.data.certificate.test.sampleCollectedAt } returns Instant.parse("2022-03-05")
+        listOf(vacQrCode, testQrCode, recQrCode).sort() shouldBe listOf(testQrCode, vacQrCode, recQrCode)
+    }
+}
