ca5029434c839d169683bc0822fb3243848add1b
Juraj Kusnier <jurajkusnier@gmail.com>
2022-05-20 14:34:12 +0200

    update DccExpirationChecker implementation

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/expiration/DccExpirationChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/expiration/DccExpirationChecker.kt
@@ -3,8 +3,6 @@ package de.rki.coronawarnapp.covidcertificate.expiration
 import dagger.Reusable
 import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
 import de.rki.coronawarnapp.covidcertificate.common.certificate.DccData
-import de.rki.coronawarnapp.util.TimeAndDateExtensions.daysUntil
-import org.joda.time.DateTimeZone
 import org.joda.time.Duration
 import org.joda.time.Instant
 import javax.inject.Inject
@@ -15,24 +13,15 @@ class DccExpirationChecker @Inject constructor() {
     fun getExpirationState(
         dccData: DccData<*>,
         expirationThreshold: Duration,
-        now: Instant,
-        timeZone: DateTimeZone = DateTimeZone.getDefault()
+        now: Instant
     ): CwaCovidCertificate.State = with(dccData) {
         val expiresAt = header.expiresAt
-        val daysUntilExpiration = now.daysUntil(expiresAt, timeZone)
+        val timeDiffUntilExpiration = Duration(now, expiresAt)
 
         return when {
-            daysUntilExpiration < 0 -> CwaCovidCertificate.State.Expired(expiresAt)
-            daysUntilExpiration == 0 -> {
-                if (now.isAfter(expiresAt)) {
-                    CwaCovidCertificate.State.Expired(expiresAt)
-                } else {
-                    CwaCovidCertificate.State.ExpiringSoon(expiresAt)
-                }
-            }
-            daysUntilExpiration <= expirationThreshold.standardDays -> CwaCovidCertificate.State.ExpiringSoon(expiresAt)
-            daysUntilExpiration > expirationThreshold.standardDays -> CwaCovidCertificate.State.Valid(expiresAt)
-            else -> throw IllegalArgumentException() // impossible!
+            expiresAt <= now -> CwaCovidCertificate.State.Expired(expiresAt)
+            timeDiffUntilExpiration <= expirationThreshold -> CwaCovidCertificate.State.ExpiringSoon(expiresAt)
+            else -> CwaCovidCertificate.State.Valid(expiresAt)
         }
     }
 }
