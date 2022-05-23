2f38c52a6e7469c5ddc7d4298b51682e80796a0c
BerndMitter <Berndus@gmx.de>
2022-05-23 14:22:07 +0200

    Added java time module for jackson

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/serialization/SerializationModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/serialization/SerializationModule.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.jsonMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
@@ -46,7 +48,7 @@ class SerializationModule {
                 .registerByteStringSerialization()
 
             jsonMapper {
-                addModules(kotlinModule(), JodaModule(), jacksonSerializationModule)
+                addModules(kotlinModule(), JodaModule(), JavaTimeModule(),jacksonSerializationModule)
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             }
         }
