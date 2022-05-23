9e209b27e2aaa6d8cc968ad11d7cf8a9a589ad08
BerndMitter <Berndus@gmx.de>
2022-05-23 14:11:17 +0200

    Added BaseJsonSerializer

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/datastore/BaseJsonSerializer.kt
@@ -0,0 +1,25 @@
+package de.rki.coronawarnapp.util.datastore
+
+import androidx.datastore.core.Serializer
+import com.fasterxml.jackson.databind.ObjectMapper
+import timber.log.Timber
+import java.io.InputStream
+import java.io.OutputStream
+
+abstract class BaseJsonSerializer<T : Any>(
+    private val objectMapper: ObjectMapper
+) : Serializer<T> {
+
+    private val type get() = defaultValue::class.java
+
+    override suspend fun readFrom(input: InputStream): T = runCatching { objectMapper.readValue(input, type) }
+        .onFailure { Timber.tag(TAG).w(it, "Failed to read data of type=$type") }
+        .getOrThrow()
+
+    override suspend fun writeTo(t: T, output: OutputStream) {
+        runCatching { objectMapper.writeValue(output, t) }
+            .onFailure { Timber.tag(TAG).w(it, "Failed to write data=$t") }
+    }
+}
+
+private const val TAG = "BaseJsonSerializer"
