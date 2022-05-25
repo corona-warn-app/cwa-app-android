6e2771c5a054f63a611f63835ed3074d08223ff9
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-05-25 16:32:40 +0200

    revert

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/ui/details/TestCertificateDetailsViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/test/ui/details/TestCertificateDetailsViewModel.kt
@@ -58,8 +58,11 @@ class TestCertificateDetailsViewModel @AssistedInject constructor(
 
     fun markAsSeen() = launch(scope = appScope) {
         Timber.v("markAsSeen()")
-        if (!fromScanner) testCertificateRepository.markCertificateAsSeenByUser(containerId)
-        testCertificateRepository.acknowledgeState(containerId)
+        if (covidCertificate.value?.isNew == true && !fromScanner) {
+            testCertificateRepository.markCertificateAsSeenByUser(containerId)
+        } else {
+            testCertificateRepository.acknowledgeState(containerId)
+        }
     }
 
     fun onExport() {
