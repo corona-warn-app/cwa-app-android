2ae43c0bfd80cd9d163b1ee431cb03a14161a064
BerndMitter <Berndus@gmx.de>
2022-05-30 13:49:43 +0200

    Implemented ContactDiaryStorageModuleTest

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/contactdiary/storage/ContactDiaryStorageModuleTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/contactdiary/storage/ContactDiaryStorageModuleTest.kt
@@ -2,8 +2,11 @@ package de.rki.coronawarnapp.contactdiary.storage
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import androidx.datastore.migrations.SharedPreferencesMigration
 import de.rki.coronawarnapp.contactdiary.storage.settings.ContactDiarySettings
+import io.kotest.assertions.throwables.shouldNotThrowAnyUnit
+import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
@@ -20,6 +23,8 @@ class ContactDiaryStorageModuleTest : BaseTest() {
     private lateinit var migration: SharedPreferencesMigration<ContactDiarySettings>
     private lateinit var sharedPrefs: SharedPreferences
 
+    private val defaultContactDiarySettings = ContactDiarySettings()
+
     @BeforeEach
     fun setup() {
         MockKAnnotations.init(this)
@@ -31,8 +36,19 @@ class ContactDiaryStorageModuleTest : BaseTest() {
     }
 
     @Test
-    fun `migration success`() = runTest {}
+    fun `migration success`() = runTest {
+        ContactDiarySettings.OnboardingStatus.values().forEach { status ->
+            sharedPrefs.edit { putInt(LEGACY_ONBOARDING_STATUS_KEY, status.order) }
+            migration.migrate(defaultContactDiarySettings) shouldBe ContactDiarySettings(status)
+        }
+    }
 
     @Test
-    fun `returns default if migration fails`() = runTest { }
+    fun `returns default if migration fails`() = runTest {
+        sharedPrefs.edit { putInt(LEGACY_ONBOARDING_STATUS_KEY, Int.MAX_VALUE) }
+
+        shouldNotThrowAnyUnit {
+            migration.migrate(defaultContactDiarySettings) shouldBe defaultContactDiarySettings
+        }
+    }
 }
