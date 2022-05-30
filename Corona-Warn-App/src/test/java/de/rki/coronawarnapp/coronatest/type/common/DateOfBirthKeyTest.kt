9f7d14ef6fde93095079b160c14b3ba3bcb9eb18
Juraj Kusnier <jurajkusnier@gmail.com>
2022-05-30 13:19:41 +0200

    fix contact test unit tests

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/coronatest/type/common/DateOfBirthKeyTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/coronatest/type/common/DateOfBirthKeyTest.kt
@@ -20,7 +20,7 @@ class DateOfBirthKeyTest : BaseTest() {
     fun `test case 1`() {
         DateOfBirthKey(
             testGuid = "E1277F-E1277F24-4AD2-40BC-AFF8-CBCCCD893E4B",
-            dateOfBirth = LocalDate.parse("2000-01-01", DateTimeFormatter.ofPattern("YYYY-MM-dd"))
+            dateOfBirth = LocalDate.parse("2000-01-01", DateTimeFormatter.ofPattern("yyyy-MM-dd"))
         ).key shouldBe "xfa760e171f000ef5a7f863ab180f6f6e8185c4890224550395281d839d85458"
     }
 
@@ -28,7 +28,7 @@ class DateOfBirthKeyTest : BaseTest() {
     fun `test case 2`() {
         DateOfBirthKey(
             testGuid = "F1EE0D-F1EE0D4D-4346-4B63-B9CF-1522D9200915",
-            dateOfBirth = LocalDate.parse("1995-06-07", DateTimeFormatter.ofPattern("YYYY-MM-dd"))
+            dateOfBirth = LocalDate.parse("1995-06-07", DateTimeFormatter.ofPattern("yyyy-MM-dd"))
         ).key shouldBe "x4a7788ef394bc7d52112014b127fe2bf142c51fe1bbb385214280e9db670193"
     }
 }
