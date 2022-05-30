ba62d1db8758f7455fc835040fae6b7d56174da2
BerndMitter <Berndus@gmx.de>
2022-05-30 13:50:38 +0200

    Implemented ContactDiarySettingsSerializerTest

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/contactdiary/storage/settings/ContactDiarySettingsSerializerTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/contactdiary/storage/settings/ContactDiarySettingsSerializerTest.kt
@@ -7,11 +7,18 @@ import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import testhelpers.BaseTest
+import testhelpers.extensions.shouldMatchJson
+import java.io.ByteArrayOutputStream
 
 class ContactDiarySettingsSerializerTest : BaseTest() {
 
     private val objectMapper = SerializationModule.jacksonBaseMapper
     private val serializer = ContactDiarySettingsSerializer(objectMapper)
+
+    private val testContactDiarySettings = ContactDiarySettings(
+        onboardingStatus = ContactDiarySettings.OnboardingStatus.RISK_STATUS_1_12
+    )
+    private val testContactDiarySettingsJson = """{"onboardingStatus":"RISK_STATUS_1_12"}"""
 
     @Test
     fun `check defaultValue`() {
@@ -20,10 +27,16 @@ class ContactDiarySettingsSerializerTest : BaseTest() {
 
     @Test
     fun `read value`() = runTest {
+        testContactDiarySettingsJson
+            .byteInputStream()
+            .use { serializer.readFrom(it) } shouldBe testContactDiarySettings
     }
 
     @Test
     fun `write value`() = runTest {
+        ByteArrayOutputStream()
+            .apply { use { serializer.writeTo(testContactDiarySettings, it) } }
+            .toString() shouldMatchJson testContactDiarySettingsJson
     }
 
     @Test
