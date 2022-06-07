afcea6ed3347addc3552a975f764a61968ee1c7c
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-06-07 17:00:40 +0200

    Progress / error messages

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/pdf/ui/exportAll/DccExportAllOverviewFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/pdf/ui/exportAll/DccExportAllOverviewFragment.kt
@@ -9,11 +9,13 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.core.view.isEmpty
 import androidx.core.view.isVisible
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.bugreporting.ui.toErrorDialogBuilder
 import de.rki.coronawarnapp.covidcertificate.pdf.ui.exportAll.DccExportAllOverviewViewModel.PrintResult
 import de.rki.coronawarnapp.covidcertificate.pdf.ui.exportAll.DccExportAllOverviewViewModel.ShareResult
 import de.rki.coronawarnapp.databinding.FragmentDccExportAllOverviewBinding
+import de.rki.coronawarnapp.util.ExternalActionHelper.openUrl
 import de.rki.coronawarnapp.util.di.AutoInject
 import de.rki.coronawarnapp.util.ui.popBackStack
 import de.rki.coronawarnapp.util.ui.viewBinding
@@ -37,7 +39,8 @@ class DccExportAllOverviewFragment : Fragment(R.layout.fragment_dcc_export_all_o
             true
         }
 
-        binding.setupWebView()
+        setupWebView()
+        cancelButton.setOnClickListener { popBackStack() }
         viewModel.dccData.observe(viewLifecycleOwner) { data ->
             progressBar.isIndeterminate = false
             webView.loadDataWithBaseURL(
@@ -57,7 +60,15 @@ class DccExportAllOverviewFragment : Fragment(R.layout.fragment_dcc_export_all_o
         }
 
         viewModel.error.observe(viewLifecycleOwner) {
-            it.toErrorDialogBuilder(requireContext()).show()
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.export_all_error_title)
+                .setMessage(R.string.export_all_error_message)
+                .setNeutralButton(R.string.export_all_error_faq) { _, _ ->
+                    openUrl(R.string.certificate_export_error_dialog_faq_link)
+
+                }.setPositiveButton(android.R.string.ok) { _, _ -> }
+                .setOnDismissListener { popBackStack() }
+                .show()
         }
     }
 
