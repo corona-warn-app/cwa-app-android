20965a72da3f813844bc1396b20975a24cf72a08
Chilja Gossow <49635654+chiljamgossow@users.noreply.github.com>
2022-06-08 08:15:09 +0200

    DCC export filter and sorting (EXPOSUREAPP-13311) (#5266)
    * filter
    
    * filter
    
    * test

---
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/pdf/core/DccExportFilter.kt
@@ -0,0 +1,52 @@
+package de.rki.coronawarnapp.covidcertificate.pdf.core
+
+import de.rki.coronawarnapp.covidcertificate.common.certificate.CwaCovidCertificate
+import de.rki.coronawarnapp.covidcertificate.recovery.core.RecoveryCertificate
+import de.rki.coronawarnapp.covidcertificate.test.core.TestCertificate
+import de.rki.coronawarnapp.covidcertificate.vaccination.core.VaccinationCertificate
+import de.rki.coronawarnapp.util.TimeAndDateExtensions.toLocalDateUserTz
+import de.rki.coronawarnapp.util.toJavaInstant
+import de.rki.coronawarnapp.util.toJavaTime
+import java.time.Duration
+import java.time.Instant
+
+internal fun List<CwaCovidCertificate>.filterAndSortForExport(
+    nowUtc: Instant
+): List<CwaCovidCertificate> {
+    return filter {
+        it.isIncludedInExport(nowUtc)
+    }.sort()
+}
+
+internal fun CwaCovidCertificate.isIncludedInExport(nowUtc: Instant): Boolean {
+    return state.isIncludedInExport && when (this) {
+        is TestCertificate -> this.isRecent(nowUtc)
+        else -> true
+    }
+}
+
+internal fun List<CwaCovidCertificate>.sort(): List<CwaCovidCertificate> = sortedWith(
+    compareBy(
+        { it.fullName },
+        {
+            when (it) {
+                is TestCertificate -> it.sampleCollectedAt?.toLocalDateUserTz()?.toJavaTime()
+                is VaccinationCertificate -> it.vaccinatedOn?.toJavaTime()
+                is RecoveryCertificate -> it.testedPositiveOn?.toJavaTime()
+                else -> null
+            }
+        }
+    )
+)
+
+internal fun TestCertificate.isRecent(nowUtc: Instant): Boolean {
+    return this.sampleCollectedAt?.let {
+        Duration.between(it.toJavaInstant(), nowUtc) <= Duration.ofHours(72)
+    } ?: false
+}
+
+internal val CwaCovidCertificate.State.isIncludedInExport: Boolean
+    get() = this is CwaCovidCertificate.State.Valid ||
+        this is CwaCovidCertificate.State.Expired ||
+        this is CwaCovidCertificate.State.ExpiringSoon ||
+        this is CwaCovidCertificate.State.Invalid
