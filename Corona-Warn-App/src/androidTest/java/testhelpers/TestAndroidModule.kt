6d9c6eb9eb543d782a496ae3bdb81c0cc2c5de0a
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2022-06-09 15:25:25 +0200

    add Resettable to settings fix tests

--- Corona-Warn-App/src/androidTest/java/testhelpers/TestAndroidModule.kt
+++ Corona-Warn-App/src/androidTest/java/testhelpers/TestAndroidModule.kt
@@ -6,7 +6,6 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.test.platform.app.InstrumentationRegistry
 import dagger.Module
 import dagger.Provides
-import de.rki.coronawarnapp.storage.StorageDataStore
 import de.rki.coronawarnapp.util.di.AppContext
 import io.mockk.mockk
 import javax.inject.Singleton
@@ -18,7 +17,6 @@ class TestAndroidModule {
     @AppContext
     fun context(): Context = InstrumentationRegistry.getInstrumentation().targetContext
 
-    @StorageDataStore
     @Provides
     fun provideDataStore(): DataStore<Preferences> = mockk(relaxed = true)
 }
