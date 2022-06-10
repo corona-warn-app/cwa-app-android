919e4c57e8cbbcc0f26a562b5afed9b336ab5a1f
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2022-06-10 15:22:12 +0200

    fix tests

--- Corona-Warn-App/src/androidTest/java/testhelpers/TestAndroidModule.kt
+++ Corona-Warn-App/src/androidTest/java/testhelpers/TestAndroidModule.kt
@@ -6,6 +6,9 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.test.platform.app.InstrumentationRegistry
 import dagger.Module
 import dagger.Provides
+import de.rki.coronawarnapp.storage.OnboardingSettingsDataStore
+import de.rki.coronawarnapp.storage.TestSettingsDataStore
+import de.rki.coronawarnapp.storage.TracingSettingsDataStore
 import de.rki.coronawarnapp.util.di.AppContext
 import io.mockk.mockk
 import javax.inject.Singleton
@@ -17,6 +20,15 @@ class TestAndroidModule {
     @AppContext
     fun context(): Context = InstrumentationRegistry.getInstrumentation().targetContext
 
+    @OnboardingSettingsDataStore
     @Provides
-    fun provideDataStore(): DataStore<Preferences> = mockk(relaxed = true)
+    fun provideOnboardingSettingsDataStore(): DataStore<Preferences> = mockk(relaxed = true)
+
+    @TestSettingsDataStore
+    @Provides
+    fun provideTestSettingsDataStore(): DataStore<Preferences> = mockk(relaxed = true)
+
+    @TracingSettingsDataStore
+    @Provides
+    fun provideTracingSettingsDataStore(): DataStore<Preferences> = mockk(relaxed = true)
 }
