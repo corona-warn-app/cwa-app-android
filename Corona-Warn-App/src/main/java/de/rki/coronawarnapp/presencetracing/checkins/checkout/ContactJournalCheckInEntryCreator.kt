ca63d60560d3a7c488b27b97a5e5ce5701065f94
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2022-07-21 13:43:45 +0200

    Update comment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/checkins/checkout/ContactJournalCheckInEntryCreator.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/presencetracing/checkins/checkout/ContactJournalCheckInEntryCreator.kt
@@ -58,7 +58,7 @@ class ContactJournalCheckInEntryCreator @Inject constructor(
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     fun CheckIn.toLocationVisit(location: ContactDiaryLocation): ContactDiaryLocationVisit {
         // Duration column is set by calculating the time difference in minutes between Check-in StartDate
-        // and Check-in EndDate and rounding it to the closest 15-minute duration
+        // and Check-in EndDate and rounding it to the closest 10-minute duration
         // Use Seconds for more precision
         val durationInMinutes = Seconds.secondsBetween(checkInStart, checkInEnd).seconds / 60.0
         val duration = (durationInMinutes / 10).roundToLong() * 10
