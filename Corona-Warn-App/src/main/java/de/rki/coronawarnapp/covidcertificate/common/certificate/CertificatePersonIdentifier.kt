95849efe2e84baaa6cff697997528cd8323c2801
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-07-26 16:41:48 +0200

    Lint

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/common/certificate/CertificatePersonIdentifier.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/common/certificate/CertificatePersonIdentifier.kt
@@ -24,17 +24,16 @@ data class CertificatePersonIdentifier(
     private val sanitizedGivenName: List<String> by lazy { firstNameStandardized?.sanitizeName().orEmpty() }
 
     /**
-    Method shall decide whether the DGCs belong to the same holder.
-    Two DCCs shall be considered as belonging to the same holder, if:
-
-    - the sanitized `dob` attributes are the same strings, and
-    - one of:
-    - the intersection/overlap of the name components of sanitized `a.nam.fnt` and `b.nam.fnt` has at least one
-    element, and the intersection/overlap of the name components of sanitized `a.nam.gnt` and `b.nam.gnt` has at least
-    one element or both are empty sets (`gnt` is an optional field)
-    - the intersection/overlap of the name components of sanitized `a.nam.fnt` and `b.nam.gnt` has at least one
-    element, and the intersection/overlap of the name components of sanitized `a.nam.gnt` and `b.nam.fnt` has at least
-    one element
+     * Method shall decide whether the DGCs belong to the same holder.
+     * Two DCCs shall be considered as belonging to the same holder, if:
+     * - the sanitized `dob` attributes are the same strings, and
+     * - one of:
+     * - the intersection/overlap of the name components of sanitized `a.nam.fnt` and `b.nam.fnt` has at least one
+     * element, and the intersection/overlap of the name components of sanitized `a.nam.gnt` and `b.nam.gnt` has at least
+     * one element or both are empty sets (`gnt` is an optional field)
+     * - the intersection/overlap of the name components of sanitized `a.nam.fnt` and `b.nam.gnt` has at least one
+     * element, and the intersection/overlap of the name components of sanitized `a.nam.gnt` and `b.nam.fnt` has at least
+     * one element
      */
     fun belongsToSamePerson(other: CwaCovidCertificate): Boolean = belongsToSamePerson(other.personIdentifier)
     fun belongsToSamePerson(other: CertificatePersonIdentifier?): Boolean {
