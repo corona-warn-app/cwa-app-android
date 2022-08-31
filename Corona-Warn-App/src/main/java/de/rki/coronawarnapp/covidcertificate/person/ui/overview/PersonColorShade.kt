a704f02d24205b82847ac0d35429438f9269d422
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-08-31 11:10:35 +0200

    Remove unused property

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/overview/PersonColorShade.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/covidcertificate/person/ui/overview/PersonColorShade.kt
@@ -14,7 +14,6 @@ enum class PersonColorShade(
     @DrawableRes val maskIcon: Int = R.drawable.ic_mask,
     @DrawableRes val maskLargeBadgeBg: Int = R.drawable.mask_badge_bg,
     @DrawableRes val maskSmallBadge: Int = R.drawable.mask_small_badge,
-    @DrawableRes val noMaskSmallBadge: Int = R.drawable.no_mask_small_badge,
     @ColorRes val maskBadgeTextColor: Int = R.color.maskBadgeTextColorRegular
 ) {
 
