b0997a8b4d0a48c880fd647b1008c6404bb5e807
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-09-06 21:41:44 +0200

    Code smell

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/durationpicker/DurationExtension.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/durationpicker/DurationExtension.kt
@@ -4,33 +4,28 @@ import org.joda.time.Duration
 
 fun Duration.toContactDiaryFormat(): String {
     val minutes = standardMinutes - standardHours * 60
-    return "%02d:%02d".format(standardHours, minutes)
+    return DURATION_FORMAT.format(standardHours, minutes)
 }
 
 fun java.time.Duration.toContactDiaryFormat(): String {
     val minutes = toMinutes() - toHours() * 60
-    return "%02d:%02d".format(toHours(), minutes)
+    return DURATION_FORMAT.format(toHours(), minutes)
 }
 
 // returns readable durations with optional prefix and suffix such as "Dauer 01:30 h"
 fun Duration.toReadableDuration(prefix: String? = null, suffix: String? = null): String {
     val durationInMinutes = standardMinutes
-    val durationString = String.format("%02d:%02d", durationInMinutes / 60, (durationInMinutes % 60))
-
-    return listOfNotNull(
-        prefix,
-        durationString,
-        suffix
-    ).joinToString(separator = " ")
+    val durationString = formatDuration(durationInMinutes)
+    return listOfNotNull(prefix, durationString, suffix).joinToString(separator = " ")
 }
 
 fun java.time.Duration.toReadableDuration(prefix: String? = null, suffix: String? = null): String {
     val durationInMinutes = toMinutes()
-    val durationString = String.format("%02d:%02d", durationInMinutes / 60, (durationInMinutes % 60))
-
-    return listOfNotNull(
-        prefix,
-        durationString,
-        suffix
-    ).joinToString(separator = " ")
+    val durationString = formatDuration(durationInMinutes)
+    return listOfNotNull(prefix, durationString, suffix).joinToString(separator = " ")
 }
+
+private fun formatDuration(durationInMinutes: Long) =
+    DURATION_FORMAT.format(durationInMinutes / 60, (durationInMinutes % 60))
+
+private const val DURATION_FORMAT = "%02d:%02d"
