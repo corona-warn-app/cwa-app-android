e5c86dd839916d27bf373028c0afce6e05d3b829
SamuraiKek <alex.paulescu@gmail.com>
2022-10-04 14:33:43 +0300

    Using the new tracingConsentDialog everywhere.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/TracingConsentDialog.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/tracing/ui/TracingConsentDialog.kt
@@ -1,31 +1,8 @@
 package de.rki.coronawarnapp.tracing.ui
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.ui.dialog.displayDialog
-import de.rki.coronawarnapp.util.DialogHelper
-
-class TracingConsentDialog(private val context: Context) {
-
-    // About to be refactored, use extension function below
-    fun show(
-        onConsentGiven: () -> Unit,
-        onConsentDeclined: () -> Unit
-    ) {
-        val dialog = DialogHelper.DialogInstance(
-            context = context,
-            title = R.string.onboarding_tracing_headline_consent,
-            message = R.string.onboarding_tracing_body_consent,
-            positiveButton = R.string.onboarding_button_enable,
-            negativeButton = R.string.onboarding_button_cancel,
-            cancelable = true,
-            positiveButtonFunction = { onConsentGiven() },
-            negativeButtonFunction = { onConsentDeclined() }
-        )
-        DialogHelper.showDialog(dialog)
-    }
-}
 
 fun Fragment.tracingConsentDialog(
     positiveButton: () -> Unit,
