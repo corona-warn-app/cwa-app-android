f04672b7bbcc4edd4587b90ac2fcaabdda4c7c87
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-10-21 10:19:42 +0200

    lint

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/dialog/DialogFragmentTemplateHelper.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/dialog/DialogFragmentTemplateHelper.kt
@@ -1,9 +1,7 @@
 package de.rki.coronawarnapp.ui.dialog
 
 import android.content.DialogInterface
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import de.rki.coronawarnapp.R
