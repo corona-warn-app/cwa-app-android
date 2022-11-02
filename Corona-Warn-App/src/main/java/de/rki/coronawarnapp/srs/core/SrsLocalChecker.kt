98b3bbeadc2fdfb071d0102b2270d9ecca5a91be
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-11-02 18:19:42 +0100

    Update SrsLocalChecker.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/core/SrsLocalChecker.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/core/SrsLocalChecker.kt
@@ -2,7 +2,10 @@ package de.rki.coronawarnapp.srs.core
 
 import dagger.Reusable
 import de.rki.coronawarnapp.appconfig.AppConfigProvider
+import de.rki.coronawarnapp.appconfig.ConfigData
+import de.rki.coronawarnapp.datadonation.safetynet.SafetyNetException
 import de.rki.coronawarnapp.srs.core.error.SrsSubmissionException
+import de.rki.coronawarnapp.srs.core.error.SrsSubmissionException.ErrorCode
 import de.rki.coronawarnapp.srs.core.storage.SrsSubmissionSettings
 import javax.inject.Inject
 
@@ -16,7 +19,14 @@ class SrsLocalChecker @Inject constructor(
      * @throws SrsSubmissionException
      */
     suspend fun check() {
+        val appConfig = appConfigProvider.getAppConfig()
+        when (appConfig.deviceTimeState) {
+            ConfigData.DeviceTimeState.ASSUMED_CORRECT -> throw SrsSubmissionException(ErrorCode.DEVICE_TIME_UNVERIFIED)
+            ConfigData.DeviceTimeState.INCORRECT -> throw SrsSubmissionException(ErrorCode.DEVICE_TIME_INCORRECT)
+            ConfigData.DeviceTimeState.CORRECT ->
+        }
+
         // TODo
-        throw SrsSubmissionException(SrsSubmissionException.ErrorCode.SUBMISSION_TOO_EARLY)
+        throw SrsSubmissionException(ErrorCode.SUBMISSION_TOO_EARLY)
     }
 }
