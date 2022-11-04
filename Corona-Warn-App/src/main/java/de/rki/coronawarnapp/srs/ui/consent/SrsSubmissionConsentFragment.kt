f04a4e29d982a6fb3c38a65b668cc9972035ce9f
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2022-11-04 11:36:26 +0100

    Add SrsSubmissionConsentFragment

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/ui/consent/SrsSubmissionConsentFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/ui/consent/SrsSubmissionConsentFragment.kt
@@ -3,15 +3,71 @@ package de.rki.coronawarnapp.srs.ui.consent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentSrsSubmissionConsentBinding
+import de.rki.coronawarnapp.ui.dialog.displayDialog
 import de.rki.coronawarnapp.util.di.AutoInject
+import de.rki.coronawarnapp.util.ui.observe2
 import de.rki.coronawarnapp.util.ui.viewBinding
+import de.rki.coronawarnapp.util.viewmodel.CWAViewModelFactoryProvider
+import de.rki.coronawarnapp.util.viewmodel.cwaViewModelsAssisted
+import javax.inject.Inject
 
 class SrsSubmissionConsentFragment : Fragment(R.layout.fragment_srs_submission_consent), AutoInject {
+
+    @Inject lateinit var viewModelFactory: CWAViewModelFactoryProvider.Factory
+    private val navArgs by navArgs<SrsSubmissionConsentFragmentArgs>()
+    private val viewModel: SrsSubmissionConsentFragmentViewModel by cwaViewModelsAssisted(
+        factoryProducer = { viewModelFactory },
+        constructorCall = { factory, _ ->
+            factory as SrsSubmissionConsentFragmentViewModel.Factory
+            factory.create(navArgs.srsSubmissionType, navArgs.unregisteredTest)
+        }
+    )
     private val binding by viewBinding<FragmentSrsSubmissionConsentBinding>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setNavigationOnClickListener {
+            viewModel.onConsentCancel()
+        }
+
+        binding.srsSubmissionConsentMoreInfo.setOnClickListener {
+            viewModel.onDataPrivacyClick()
+        }
+
+        binding.srsSubmissionConsentButtonNext.setOnClickListener {
+            displayDialog {
+                title(R.string.srs_submission_consent_request_dialog_title)
+                message(R.string.srs_submission_consent_request_dialog_body)
+                positiveButton(R.string.srs_submission_consent_request_dialog_positive_button_text) {
+                    viewModel.onConsentDialogConfirmed()
+                }
+                negativeButton(R.string.srs_submission_consent_request_dialog_negative_button_text)
+            }
+        }
+
+        viewModel.routeToScreen.observe2(this) {
+            when (it) {
+                SrsSubmissionConsentNavigationEvents.NavigateToDataPrivacy -> TODO()
+                SrsSubmissionConsentNavigationEvents.NavigateToMainScreen -> TODO()
+                SrsSubmissionConsentNavigationEvents.NavigateToShareCheckins ->
+                    findNavController().navigate(
+                        SrsSubmissionConsentFragmentDirections.actionSrsSubmissionConsentFragmentToSrsCheckinsFragment()
+                    )
+                SrsSubmissionConsentNavigationEvents.NavigateToShareSymptoms ->
+                    findNavController().navigate(
+                        SrsSubmissionConsentFragmentDirections.actionSrsSubmissionConsentFragmentToSrsSymptomsFragment()
+                    )
+                SrsSubmissionConsentNavigationEvents.NavigateToTestType ->
+                    findNavController().navigate(
+                        SrsSubmissionConsentFragmentDirections
+                            .actionSrsSubmissionConsentFragmentToSrsSubmissionTypeSelectionFragment()
+                    )
+            }
+        }
     }
 }
--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/ui/consent/SrsSubmissionConsentFragmentViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/ui/consent/SrsSubmissionConsentFragmentViewModel.kt
@@ -1,15 +1,59 @@
 package de.rki.coronawarnapp.srs.ui.consent
 
+import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import de.rki.coronawarnapp.presencetracing.checkins.CheckInRepository
+import de.rki.coronawarnapp.presencetracing.checkins.common.completedCheckIns
+import de.rki.coronawarnapp.srs.core.model.SrsSubmissionType
 import de.rki.coronawarnapp.util.coroutine.DispatcherProvider
+import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
+import kotlinx.coroutines.flow.first
+import timber.log.Timber
 
 class SrsSubmissionConsentFragmentViewModel @AssistedInject constructor(
+    @Assisted private val srsSubmissionType: SrsSubmissionType?,
+    @Assisted private val inAppResult: Boolean,
+    private val checkInRepository: CheckInRepository,
     dispatcherProvider: DispatcherProvider
 ) : CWAViewModel(dispatcherProvider) {
 
+    val routeToScreen = SingleLiveEvent<SrsSubmissionConsentNavigationEvents>()
+    fun onDataPrivacyClick() {
+        routeToScreen.postValue(SrsSubmissionConsentNavigationEvents.NavigateToDataPrivacy)
+    }
+
+    fun onConsentDialogConfirmed() = launch {
+        if (!inAppResult) {
+            Timber.tag(TAG).d("Navigate to TestType")
+            routeToScreen.postValue(SrsSubmissionConsentNavigationEvents.NavigateToTestType)
+        } else {
+            val completedCheckInsExist = checkInRepository.completedCheckIns.first().isNotEmpty()
+            val navDirections = if (completedCheckInsExist) {
+                Timber.tag(TAG).d("Navigate to ShareCheckins")
+                SrsSubmissionConsentNavigationEvents.NavigateToShareCheckins
+            } else {
+                Timber.tag(TAG).d("Navigate to ShareSymptoms")
+                SrsSubmissionConsentNavigationEvents.NavigateToShareSymptoms
+            }
+            routeToScreen.postValue(navDirections)
+        }
+    }
+
+    fun onConsentCancel() {
+        routeToScreen.postValue(SrsSubmissionConsentNavigationEvents.NavigateToMainScreen)
+    }
+
     @AssistedFactory
-    interface Factory : SimpleCWAViewModelFactory<SrsSubmissionConsentFragmentViewModel>
+    interface Factory : SimpleCWAViewModelFactory<SrsSubmissionConsentFragmentViewModel> {
+        fun create(
+            srsSubmissionType: SrsSubmissionType?,
+            inAppResult: Boolean
+        ): SrsSubmissionConsentFragmentViewModel
+    }
+    companion object {
+        private const val TAG = "SrsSubmissionConsentFragmentViewModel"
+    }
 }
