1b2d44bc9c97759cd003730c71c5e32041ad89e3
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-11-09 11:59:24 +0100

    Reusable

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/core/SubmissionReporter.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/core/SubmissionReporter.kt
@@ -1,11 +1,13 @@
 package de.rki.coronawarnapp.srs.core
 
+import dagger.Reusable
 import de.rki.coronawarnapp.contactdiary.storage.repo.ContactDiaryRepository
 import de.rki.coronawarnapp.srs.core.storage.SrsSubmissionSettings
 import timber.log.Timber
 import java.time.Instant
 import javax.inject.Inject
 
+@Reusable
 class SubmissionReporter @Inject constructor(
     private val srsSubmissionSettings: SrsSubmissionSettings,
     private val contactDiaryRepository: ContactDiaryRepository,
