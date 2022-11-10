047d269bba491372d1af09ba62ac48a8b752699f
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-11-10 11:55:53 +0100

    Delegate selection to viewmodel / checked text

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/ui/typeselection/SrsTypeSelectionFragmentViewModel.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/ui/typeselection/SrsTypeSelectionFragmentViewModel.kt
@@ -1,10 +1,10 @@
 package de.rki.coronawarnapp.srs.ui.typeselection
 
-import androidx.lifecycle.asLiveData
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.presencetracing.checkins.CheckInRepository
 import de.rki.coronawarnapp.presencetracing.checkins.common.completedCheckIns
+import de.rki.coronawarnapp.srs.core.model.SrsSubmissionType
 import de.rki.coronawarnapp.srs.ui.typeselection.SrsTypeSelectionNavigationEvents.NavigateToCloseDialog
 import de.rki.coronawarnapp.srs.ui.typeselection.SrsTypeSelectionNavigationEvents.NavigateToMainScreen
 import de.rki.coronawarnapp.srs.ui.typeselection.SrsTypeSelectionNavigationEvents.NavigateToShareCheckins
@@ -14,18 +14,34 @@ import de.rki.coronawarnapp.util.ui.SingleLiveEvent
 import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import timber.log.Timber
 
 class SrsTypeSelectionFragmentViewModel @AssistedInject constructor(
     private val checkInRepository: CheckInRepository,
     dispatcherProvider: DispatcherProvider
 ) : CWAViewModel(dispatcherProvider) {
+    private val srsTypes = flow {
+        emit(
+            SrsSubmissionType.values().filterNot { it == SrsSubmissionType.SRS_SELF_TEST }
+        )
+    }
 
+    private val selectedType = MutableStateFlow<SrsSubmissionType?>(null)
     val navigation = SingleLiveEvent<SrsTypeSelectionNavigationEvents>()
-
-    private val srsTestTypeInternal = MutableStateFlow(SrsTypeSelectionItem(0, 0, null))
-    val srsTestType = srsTestTypeInternal.asLiveData(context = dispatcherProvider.Default)
+    val types = combine(
+        srsTypes,
+        selectedType
+    ) { srsSubmissionTypes, selectedType ->
+        srsSubmissionTypes.map { type ->
+            SrsTypeSelectionItem(
+                checked = type == selectedType,
+                submissionType = type
+            )
+        }
+    }.asLiveData2()
 
     fun onCancel() {
         Timber.tag(TAG).d("onCancel()")
@@ -34,12 +50,16 @@ class SrsTypeSelectionFragmentViewModel @AssistedInject constructor(
 
     fun onNextClicked() = launch {
         val completedCheckInsExist = checkInRepository.completedCheckIns.first().isNotEmpty()
+        val type = selectedType.value ?: run {
+            Timber.tag(TAG).e("No type was selected")
+            return@launch
+        }
         if (completedCheckInsExist) {
             Timber.tag(TAG).d("Navigate to ShareCheckins")
-            navigation.postValue(NavigateToShareCheckins)
+            navigation.postValue(NavigateToShareCheckins(type))
         } else {
             Timber.tag(TAG).d("Navigate to ShareSymptoms")
-            navigation.postValue(NavigateToShareSymptoms)
+            navigation.postValue(NavigateToShareSymptoms(type))
         }
     }
 
@@ -48,12 +68,8 @@ class SrsTypeSelectionFragmentViewModel @AssistedInject constructor(
         navigation.postValue(NavigateToMainScreen)
     }
 
-    private fun updateTypeSelection(typeSelection: SrsTypeSelectionItem) {
-        srsTestTypeInternal.value = typeSelection
-    }
-
     fun selectTypeListItem(selectionItem: SrsTypeSelectionItem) {
-        updateTypeSelection(selectionItem)
+        selectedType.value = selectionItem.submissionType
     }
 
     @AssistedFactory
