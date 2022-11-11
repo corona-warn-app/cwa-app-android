f2fa4ab693d50d0e75a9c5a1ccb358e32227f9d1
Mohamed <mohamed.metwalli@sap.com>
2022-11-11 16:18:47 +0100

    Move Android ID to payload (DEV) (#5703)
    * Move Android ID to payload
    
    * Import right proto buf
    
    * lint

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/core/server/SrsAuthorizationServer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/srs/core/server/SrsAuthorizationServer.kt
@@ -10,8 +10,7 @@ import de.rki.coronawarnapp.exception.http.CwaUnknownHostException
 import de.rki.coronawarnapp.exception.http.NetworkConnectTimeoutException
 import de.rki.coronawarnapp.exception.http.NetworkReadTimeoutException
 import de.rki.coronawarnapp.server.protocols.internal.ppdd.PpacAndroid
-import de.rki.coronawarnapp.server.protocols.internal.ppdd.SrsOtp
-import de.rki.coronawarnapp.server.protocols.internal.ppdd.SrsOtpRequestAndroid
+import de.rki.coronawarnapp.server.protocols.internal.ppdd.SrsOtpRequestAndroid.SRSOneTimePasswordRequestAndroid
 import de.rki.coronawarnapp.srs.core.error.SrsSubmissionException
 import de.rki.coronawarnapp.srs.core.error.SrsSubmissionException.ErrorCode
 import de.rki.coronawarnapp.srs.core.model.SrsAuthorizationRequest
@@ -54,13 +53,16 @@ class SrsAuthorizationServer @Inject constructor(
 
     private suspend fun authoriseRequest(request: SrsAuthorizationRequest): Instant {
         Timber.tag(TAG).d("authorize(request=%s)", request)
-        val srsOtpRequest = SrsOtpRequestAndroid.SRSOneTimePasswordRequestAndroid.newBuilder()
+        val srsOtpRequest = SRSOneTimePasswordRequestAndroid.newBuilder()
             .setPayload(
-                SrsOtp.SRSOneTimePassword.newBuilder().setOtp(request.srsOtp.uuid.toString()).build()
+                SRSOneTimePasswordRequestAndroid.SRSOneTimePassword
+                    .newBuilder()
+                    .setOtp(request.srsOtp.uuid.toString())
+                    .setAndroidId(request.androidId)
+                    .build()
             )
             .setAuthentication(
                 PpacAndroid.PPACAndroid.newBuilder()
-                    .setAndroidId(request.androidId)
                     .setSafetyNetJws(request.safetyNetJws)
                     .setSalt(request.salt)
                     .build()
