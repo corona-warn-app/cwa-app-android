7fcc98c0691bcb5475b4581051cbcfacd2fe26a2
SamuraiKek <alex.paulescu@gmail.com>
2022-12-15 15:22:23 +0200

    Fix tests.

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/srs/core/repository/SrsSubmissionRepositoryTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/srs/core/repository/SrsSubmissionRepositoryTest.kt
@@ -36,6 +36,7 @@ import io.mockk.coVerifySequence
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
+import io.mockk.runs
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
@@ -97,6 +98,7 @@ internal class SrsSubmissionRepositoryTest : BaseTest() {
         coEvery { tekStorage.reset() } just Runs
         coEvery { deviceAttestation.attest(any()) } returns attestationContainer
         coEvery { srsSubmissionSettings.getOtp() } returns null
+        coEvery { srsSubmissionSettings.resetOtp() } just Runs
         coEvery { srsSubmissionSettings.setOtp(any()) } just Runs
         coEvery { srsSubmissionSettings.setMostRecentSubmissionTime(any()) } just Runs
         coEvery { appConfigProvider.getAppConfig() } returns configData
@@ -126,6 +128,7 @@ internal class SrsSubmissionRepositoryTest : BaseTest() {
             checkInsRepo.updatePostSubmissionFlags(any<List<CheckIn>>())
             timeStamper.nowUTC
             submissionReporter.reportAt(any())
+            srsSubmissionSettings.resetOtp()
         }
 
         coVerify(exactly = 0) {
@@ -156,6 +159,7 @@ internal class SrsSubmissionRepositoryTest : BaseTest() {
             checkInsRepo.updatePostSubmissionFlags(any<List<CheckIn>>())
             timeStamper.nowUTC
             submissionReporter.reportAt(any())
+            srsSubmissionSettings.resetOtp()
         }
 
         coVerify(exactly = 0) {
@@ -181,6 +185,7 @@ internal class SrsSubmissionRepositoryTest : BaseTest() {
             checkInsRepo.updatePostSubmissionFlags(any<List<CheckIn>>())
             timeStamper.nowUTC
             submissionReporter.reportAt(any())
+            srsSubmissionSettings.resetOtp()
         }
 
         coVerify(exactly = 0) {
