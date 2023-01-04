97288da4d7fdb013f48fdd8a810f018803dc8fa1
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2023-01-04 08:45:50 +0100

    fix deserialisation issue

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/serialization/jackson/CoronaTestResultSerializer.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/util/serialization/jackson/CoronaTestResultSerializer.kt
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.addDeserializer
 import com.fasterxml.jackson.module.kotlin.addSerializer
 import de.rki.coronawarnapp.coronatest.server.CoronaTestResult
+import timber.log.Timber
 
 class CoronaTestResultSerializer : JsonSerializer<CoronaTestResult>() {
     override fun serialize(value: CoronaTestResult?, gen: JsonGenerator?, serializers: SerializerProvider?) {
@@ -22,7 +25,12 @@ class CoronaTestResultSerializer : JsonSerializer<CoronaTestResult>() {
 class CoronaTestResultDeSerializer : JsonDeserializer<CoronaTestResult>() {
     override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): CoronaTestResult {
         requireNotNull(p) { "JsonParser is null" }
-        return CoronaTestResult.fromInt(p.intValue)
+        val node = ObjectMapper().readValue(p, JsonNode::class.java)
+        return if (node.isTextual) {
+            CoronaTestResult.valueOf(node.asText())
+        } else {
+            CoronaTestResult.fromInt(p.intValue)
+        }
     }
 }
 
