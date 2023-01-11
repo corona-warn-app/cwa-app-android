0a4c2c6a9757bb2d58812e6fd4355969ad028e5d
SamuraiKek <alex.paulescu@gmail.com>
2023-01-11 13:02:33 +0200

    Lint.

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/vaccination/CovidCertificateOnboardingFragmentTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/ui/vaccination/CovidCertificateOnboardingFragmentTest.kt
@@ -3,7 +3,6 @@ package de.rki.coronawarnapp.ui.vaccination
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import de.rki.coronawarnapp.covidcertificate.common.qrcode.DccQrCode
 import de.rki.coronawarnapp.covidcertificate.ui.onboarding.CovidCertificateOnboardingFragment
 import de.rki.coronawarnapp.covidcertificate.ui.onboarding.CovidCertificateOnboardingFragmentArgs
 import de.rki.coronawarnapp.covidcertificate.ui.onboarding.CovidCertificateOnboardingViewModel
