76ce33a081529571ba91376ac6450cc41b015125
Mohamed <mohamed.metwalli@sap.com>
2023-01-31 13:06:12 +0100

    Trigger calc on configuration loading (#5819)

--- Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/main/home/rampdown/RampDownDataProviderTest.kt
+++ Corona-Warn-App/src/test/java/de/rki/coronawarnapp/ui/main/home/rampdown/RampDownDataProviderTest.kt
@@ -1,5 +1,6 @@
 package de.rki.coronawarnapp.ui.main.home.rampdown
 
+import de.rki.coronawarnapp.ccl.configuration.storage.CclConfigurationRepository
 import de.rki.coronawarnapp.ccl.rampdown.calculation.RampDownCalculation
 import de.rki.coronawarnapp.ccl.rampdown.model.RampDownOutput
 import de.rki.coronawarnapp.ccl.ui.text.CclTextFormatter
@@ -7,9 +8,11 @@ import de.rki.coronawarnapp.util.serialization.SerializationModule
 import io.kotest.matchers.shouldBe
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 
 import org.junit.jupiter.api.BeforeEach
@@ -20,20 +23,21 @@ import testhelpers.BaseTest
 internal class RampDownDataProviderTest : BaseTest() {
 
     @MockK lateinit var rampDownCalculation: RampDownCalculation
+    @MockK lateinit var cclConfigurationRepository: CclConfigurationRepository
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
 
-        coEvery {
-            rampDownCalculation.getStatusTabNotice(any())
-        } returns RampDownOutput(
+        coEvery { rampDownCalculation.getStatusTabNotice(any()) } returns RampDownOutput(
             visible = false,
             titleText = null,
             subtitleText = null,
             faqAnchor = null,
             longText = null
         )
+
+        every { cclConfigurationRepository.cclConfigurations } returns flowOf(emptyList())
     }
 
     @Test
@@ -43,7 +47,8 @@ internal class RampDownDataProviderTest : BaseTest() {
                 cclJsonFunctions = mockk(),
                 mapper = SerializationModule.jacksonBaseMapper
             ),
-            rampDownCalculation = rampDownCalculation
+            rampDownCalculation = rampDownCalculation,
+            cclConfigurationRepository = cclConfigurationRepository
         ).rampDownNotice.first() shouldBe RampDownNotice(
             visible = false,
             title = "",
@@ -64,7 +69,8 @@ internal class RampDownDataProviderTest : BaseTest() {
                 cclJsonFunctions = mockk(),
                 mapper = SerializationModule.jacksonBaseMapper
             ),
-            rampDownCalculation = rampDownCalculation
+            rampDownCalculation = rampDownCalculation,
+            cclConfigurationRepository = cclConfigurationRepository
         ).rampDownNotice.first() shouldBe null
     }
 }
