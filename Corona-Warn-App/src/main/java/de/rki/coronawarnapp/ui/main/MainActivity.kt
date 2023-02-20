b39e815f06df3b77d5824d76710792377f1e3ce4
Mohamed <mohamed.metwalli@sap.com>
2023-02-20 12:31:36 +0100

    Remove unwanted trigger

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/MainActivity.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/MainActivity.kt
@@ -317,15 +317,7 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
         Timber.i("Uri:$uriString")
         viewModel.onNavigationUri(uriString)
     }
-
-    /**
-     * Register callbacks.
-     */
-    override fun onResume() {
-        super.onResume()
-        dataDonationAnalyticsScheduler.initialize()
-    }
-
+    
     private fun showEnergyOptimizedEnabledForBackground() = displayDialog {
         title(R.string.onboarding_energy_optimized_dialog_headline)
         message(R.string.onboarding_energy_optimized_dialog_body)
