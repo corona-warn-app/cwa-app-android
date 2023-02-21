5dbbad564eea28b0a2334fa1928c48033b777303
Mohamed <mohamed.metwalli@sap.com>
2023-02-21 22:29:45 +0100

    Update EolModule.kt

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eol/EolModule.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/eol/EolModule.kt
@@ -7,16 +7,19 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
-import de.rki.coronawarnapp.util.di.AppContext
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
 object EolModule {
     @Singleton
     @Provides
     @EolSettingsDataStore
-    fun provideEolSettingsDataStore(@AppContext context: Context): DataStore<Preferences> =
+    fun provideEolSettingsDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
         PreferenceDataStoreFactory.create {
             context.preferencesDataStoreFile("app_eol_dev_settings")
         }
