9fe84452832ab93b84b54b07b356c8ec7e23366d
Mohamed <mohamed.metwalli@sap.com>
2023-02-22 00:15:25 +0100

    Delete MainActivityTestModule.kt

--- Corona-Warn-App/src/device/java/de/rki/coronawarnapp/ui/main/MainActivityTestModule.kt
+++
@@ -1,9 +0,0 @@
-package de.rki.coronawarnapp.ui.main
-
-import dagger.Module
-
-@Module
-abstract class MainActivityTestModule {
-
-    // This is an empty placeholder so we can use the same one, but not empty via deviceForTesters
-}
