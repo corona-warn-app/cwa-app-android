5e4e632fe65be92802de577553b7148a3446222f
Mohamed <mohamed.metwalli@sap.com>
2023-02-22 00:15:04 +0100

    Update BugReportingModule.kt

--- Corona-Warn-App/src/device/java/de/rki/coronawarnapp/bugreporting/BugReportingModule.kt
+++ Corona-Warn-App/src/device/java/de/rki/coronawarnapp/bugreporting/BugReportingModule.kt
@@ -2,10 +2,13 @@ package de.rki.coronawarnapp.bugreporting
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import de.rki.coronawarnapp.bugreporting.loghistory.LogHistoryTree
 import timber.log.Timber
 import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
 class BugReportingModule {
 
