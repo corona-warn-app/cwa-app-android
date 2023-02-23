d3cce0ac1774169928e107f1eb3ab42c2d4a37f2
Mohamed <Mohamed.Metwalli@sap.com>
2023-02-23 17:19:22 +0100

    Lint

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/source/DefaultStatsSource.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/source/DefaultStatsSource.kt
@@ -2,13 +2,7 @@ package de.rki.coronawarnapp.statistics.source
 
 import android.content.Context
 import dagger.Reusable
-import de.rki.coronawarnapp.appconfig.ConfigData
-import de.rki.coronawarnapp.appconfig.internal.ConfigDataContainer
-import de.rki.coronawarnapp.appconfig.mapping.ConfigParser
-import de.rki.coronawarnapp.srs.core.storage.SrsDevSettings
 import de.rki.coronawarnapp.util.di.AppContext
-import java.time.Duration
-import java.time.Instant
 import javax.inject.Inject
 
 @Reusable
