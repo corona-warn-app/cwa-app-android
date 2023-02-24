a27ccdc1e986e6897412fe06b9e65eb450a25c6f
Mohamed <Mohamed.Metwalli@sap.com>
2023-02-24 10:56:32 +0100

    Update DebugLogFragmentTest.kt

--- Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/bugreporting/DebugLogFragmentTest.kt
+++ Corona-Warn-App/src/androidTest/java/de/rki/coronawarnapp/bugreporting/DebugLogFragmentTest.kt
@@ -1,53 +1,45 @@
 package de.rki.coronawarnapp.bugreporting
 
 import androidx.lifecycle.MutableLiveData
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import de.rki.coronawarnapp.bugreporting.debuglog.DebugLogger
-import de.rki.coronawarnapp.bugreporting.debuglog.internal.LogSnapshotter
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import de.rki.coronawarnapp.bugreporting.debuglog.ui.DebugLogFragment
 import de.rki.coronawarnapp.bugreporting.debuglog.ui.DebugLogViewModel
-import de.rki.coronawarnapp.bugreporting.debuglog.upload.history.storage.UploadHistoryStorage
-import de.rki.coronawarnapp.nearby.ENFClient
-import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.spyk
+import io.mockk.mockk
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import testhelpers.BaseUITest
 import testhelpers.Screenshot
-import testhelpers.TestDispatcherProvider
-import testhelpers.launchFragment2
 import testhelpers.launchFragmentInContainer2
 import testhelpers.takeScreenshot
 
-@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class DebugLogFragmentTest : BaseUITest() {
 
-    @MockK lateinit var debugLogger: DebugLogger
-    @MockK lateinit var enfClient: ENFClient
-    @MockK lateinit var uploadHistoryStorage: UploadHistoryStorage
-    @MockK lateinit var logSnapshotter: LogSnapshotter
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
-    private lateinit var inactiveViewModel: DebugLogViewModel
-    private lateinit var activeViewModel: DebugLogViewModel
+    @BindValue
+    @JvmField
+    val viewModel = mockk<DebugLogViewModel>(relaxed = true)
 
     @Before
     fun setup() {
-        MockKAnnotations.init(this, relaxed = true)
-        inactiveViewModel = setupViewModels(false, 0)
-        activeViewModel = setupViewModels(true, 9410)
+        every { viewModel.state } returns mutableLiveData()
     }
 
     @Test
     fun launch_fragment() {
-        launchFragment2<DebugLogFragment>()
+        launchFragmentInContainer2<DebugLogFragment>()
     }
 
     @Screenshot
     @Test
     fun capture_inactive_screenshot() {
+        every { viewModel.state } returns mutableLiveData(false)
         launchFragmentInContainer2<DebugLogFragment>()
         takeScreenshot<DebugLogFragment>()
     }
@@ -55,36 +47,16 @@ class DebugLogFragmentTest : BaseUITest() {
     @Screenshot
     @Test
     fun capture_active_screenshot() {
-
         launchFragmentInContainer2<DebugLogFragment>()
         takeScreenshot<DebugLogFragment>()
     }
 
-    private fun setupViewModels(
-        isRecording: Boolean,
-        currentSize: Long,
-        isLowStorage: Boolean = false,
-        isActionInProgress: Boolean = false,
-    ): DebugLogViewModel {
-        val vm = spyk(
-            DebugLogViewModel(
-                debugLogger = debugLogger,
-                dispatcherProvider = TestDispatcherProvider(),
-                enfClient = enfClient,
-                uploadHistoryStorage = uploadHistoryStorage,
-                logSnapshotter = logSnapshotter
-            )
+    private fun mutableLiveData(active: Boolean = true) = MutableLiveData(
+        DebugLogViewModel.State(
+            isRecording = active,
+            currentSize = 12,
+            isLowStorage = false,
+            isActionInProgress = false
         )
-        with(vm) {
-            every { state } returns MutableLiveData(
-                DebugLogViewModel.State(
-                    isRecording = isRecording,
-                    currentSize = currentSize,
-                    isLowStorage = isLowStorage,
-                    isActionInProgress = isActionInProgress
-                )
-            )
-        }
-        return vm
-    }
+    )
 }
