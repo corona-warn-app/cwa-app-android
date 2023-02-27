fc710d45430f66ab17217762fe4078a9a3d45056
SamuraiKek <alex.paulescu@gmail.com>
2023-02-27 16:58:17 +0200

    Unused import.

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/overview/MainOverviewFragment.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/ui/main/overview/MainOverviewFragment.kt
@@ -2,7 +2,6 @@ package de.rki.coronawarnapp.ui.main.overview
 
 import android.os.Bundle
 import android.view.View
-import android.view.accessibility.AccessibilityEvent
 import androidx.fragment.app.Fragment
 import de.rki.coronawarnapp.R
 import de.rki.coronawarnapp.databinding.FragmentMainOverviewBinding
