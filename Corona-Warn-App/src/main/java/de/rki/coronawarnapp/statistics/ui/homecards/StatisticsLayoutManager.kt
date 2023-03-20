35dedbeabd30f9b93cd14c258311a84b49ae7b7e
Alex Paulescu <alex.paulescu@gmail.com>
2023-03-20 16:54:45 +0200

    Fix statistics tiles height. (#5904)

--- Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/StatisticsLayoutManager.kt
+++ Corona-Warn-App/src/main/java/de/rki/coronawarnapp/statistics/ui/homecards/StatisticsLayoutManager.kt
@@ -5,26 +5,19 @@ import android.util.AttributeSet
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.roundToInt
 
 /**
  * A custom RecyclerView.LayoutManager implementation that extends
  * the height of items to match RecyclerView. 
-**/
-class StatisticsLayoutManager : LinearLayoutManager {
-    constructor(context: Context?) : super(context)
-
-    constructor(
-        context: Context?,
-        @RecyclerView.Orientation orientation: Int,
-        reverseLayout: Boolean
-    ) : super(context, orientation, reverseLayout)
-
-    constructor(
-        context: Context?,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes)
+ **/
+class StatisticsLayoutManager(
+    context: Context?,
+    @RecyclerView.Orientation orientation: Int,
+    reverseLayout: Boolean,
+    private val fontScale: Float
+) :
+    LinearLayoutManager(context, orientation, reverseLayout) {
 
     override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
         return spanLayoutSize(super.generateDefaultLayoutParams())
@@ -38,10 +31,23 @@ class StatisticsLayoutManager : LinearLayoutManager {
         return spanLayoutSize(super.generateLayoutParams(lp))
     }
 
+    override fun checkLayoutParams(lp: RecyclerView.LayoutParams?): Boolean {
+        if (lp != null) {
+            if (fontScale > 1.0F) {
+                lp.height = (HEIGHT_MULTIPLIER * fontScale).roundToInt()
+            }
+        }
+        return true
+    }
+
     private fun spanLayoutSize(layoutParams: RecyclerView.LayoutParams): RecyclerView.LayoutParams {
         return RecyclerView.LayoutParams(
             layoutParams.width,
             RecyclerView.LayoutParams.MATCH_PARENT
         )
+    }
+
+    companion object {
+        const val HEIGHT_MULTIPLIER = 1050
     }
 }
