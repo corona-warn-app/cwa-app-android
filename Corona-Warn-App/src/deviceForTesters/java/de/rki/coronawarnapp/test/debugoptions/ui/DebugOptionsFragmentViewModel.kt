2a1f046a4b39d677d986038e8b0518cd888f41f4
Alex Paulescu <alex.paulescu@gmail.com>
2023-03-30 14:18:32 +0300

    Fix invalid certs wru z (EXPOSUREAPP-14998) (#5912)
    * Prevent reset when switching to wru-z after eol.
    
    * Add eol to test.
    
    * Lint
    
    * Rename function.

--- Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/debugoptions/ui/DebugOptionsFragmentViewModel.kt
+++ Corona-Warn-App/src/deviceForTesters/java/de/rki/coronawarnapp/test/debugoptions/ui/DebugOptionsFragmentViewModel.kt
@@ -5,6 +5,7 @@ import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import de.rki.coronawarnapp.environment.EnvironmentSetup
 import de.rki.coronawarnapp.environment.EnvironmentSetup.Type.Companion.toEnvironmentType
+import de.rki.coronawarnapp.eol.AppEol
 import de.rki.coronawarnapp.eol.EolSetting
 import de.rki.coronawarnapp.test.debugoptions.ui.EnvironmentState.Companion.toEnvironmentState
 import de.rki.coronawarnapp.util.coroutine.AppScope
@@ -14,11 +15,13 @@ import de.rki.coronawarnapp.util.viewmodel.CWAViewModel
 import de.rki.coronawarnapp.util.viewmodel.SimpleCWAViewModelFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class DebugOptionsFragmentViewModel @AssistedInject constructor(
     private val envSetup: EnvironmentSetup,
     private val eolSetting: EolSetting,
+    private val eol: AppEol,
     dispatcherProvider: DispatcherProvider,
     private val environmentSunset: EnvironmentSunset,
     @AppScope private val appScope: CoroutineScope,
@@ -36,7 +39,7 @@ class DebugOptionsFragmentViewModel @AssistedInject constructor(
                 environmentStateFlow.value = it
                 environmentStateChange.postValue(it)
             }
-        cleanCachedData()
+        cleanCachedDataIfNotEol()
     }
 
     fun selectEnvironmentType(type: String) {
@@ -44,15 +47,17 @@ class DebugOptionsFragmentViewModel @AssistedInject constructor(
         envSetup.toEnvironmentState().let {
             environmentStateFlow.value = it
         }
-        cleanCachedData()
+        cleanCachedDataIfNotEol()
     }
 
     fun setAllowedFlag(flag: Boolean) = launch {
         eolSetting.setLoggerAllowed(flag)
     }
 
-    private fun cleanCachedData() = appScope.launch {
-        environmentSunset.reset()
+    private fun cleanCachedDataIfNotEol() = appScope.launch {
+        if (!eol.isEol.first()) {
+            environmentSunset.reset()
+        }
     }
 
     @AssistedFactory
